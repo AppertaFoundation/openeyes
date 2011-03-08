5eeccbaf61b111fb50deaf249bc55b30a5afc6b0
Chris Raettig <chris@raettig.org>
2011-03-08 18:16:18 +0000

    case fixes

--- protected/commands/GenerateSiteElementTypeDataCommand.php
+++ protected/commands/GenerateSiteElementTypeDataCommand.php
@@ -25,30 +25,30 @@ class GenerateSiteElementTypeDataCommand extends CConsoleCommand
 			echo $specialty->name . "\n";
 			foreach ($possibleElementTypes as $pet) {
 				// if there's not an existing record, where first_in_episode is false, make one
-				if (!$existing_site_element_type = SiteElementType::Model()->findAllByAttributes(array('possible_element_type_id' => $pet->id, 'specialty_id' => $specialty->id, 'first_in_episode' => false))) {
+				if (!$existingSiteElementType = SiteElementType::Model()->findAllByAttributes(array('possible_element_type_id' => $pet->id, 'specialty_id' => $specialty->id, 'first_in_episode' => false))) {
 					echo "\tCreating non-first-in-episode site_element_type entry for: possible_element_type id: " . $pet->id . " specialty: " . $specialty->name . " (event type is: " . $pet->eventType->name . ")\n";
-					$new_site_element_type = new SiteElementType;
-					$new_site_element_type->possible_element_type_id = $pet->id;	
-					$new_site_element_type->specialty_id = $specialty->id;
-					$new_site_element_type->view_number = 1;	
-					$new_site_element_type->required = 0;
-					$new_site_element_type->first_in_episode = 0;
-					$new_site_element_type->save();	
+					$newSiteElementType = new SiteElementType;
+					$newSiteElementType->possible_element_type_id = $pet->id;	
+					$newSiteElementType->specialty_id = $specialty->id;
+					$newSiteElementType->view_number = 1;	
+					$newSiteElementType->required = 0;
+					$newSiteElementType->first_in_episode = 0;
+					$newSiteElementType->save();	
 				}
 
 				// if it's possible for the first in episode to be different:
 				// echo "FIEP: " . $pet->eventType->name ."=".$pet->eventType->first_in_episode_possible."\n";
 				if ($pet->eventType->first_in_episode_possible == 1) {
 					// if there's not an existing record, where first_in_episode is true, make one
-					if (!$existing_site_element_type = SiteElementType::Model()->findAllByAttributes(array('possible_element_type_id' => $pet->id, 'specialty_id' => $specialty->id, 'first_in_episode' => true))) {
+					if (!$existingSiteElementType = SiteElementType::Model()->findAllByAttributes(array('possible_element_type_id' => $pet->id, 'specialty_id' => $specialty->id, 'first_in_episode' => true))) {
 					echo "\tCreating first-in-episode site_element_type entry for: possible_element_type id: " . $pet->id . " specialty: " . $specialty->name . " (event type is: " . $pet->eventType->name . ")\n";
-						$new_site_element_type = new SiteElementType;
-						$new_site_element_type->possible_element_type_id = $pet->id;	
-						$new_site_element_type->specialty_id = $specialty->id;
-						$new_site_element_type->view_number = 1;	
-						$new_site_element_type->required = 0;
-						$new_site_element_type->first_in_episode = 1;
-						$new_site_element_type->save();	
+						$newSiteElementType = new SiteElementType;
+						$newSiteElementType->possible_element_type_id = $pet->id;	
+						$newSiteElementType->specialty_id = $specialty->id;
+						$newSiteElementType->view_number = 1;	
+						$newSiteElementType->required = 0;
+						$newSiteElementType->first_in_episode = 1;
+						$newSiteElementType->save();	
 					}
 				}
 			}
