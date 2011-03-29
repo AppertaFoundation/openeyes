afc17720787c108e0ef37cb57f547ed5bfb624ae
andersonprl <Paul.Anderson@moorfields.nhs.uk>
2011-03-29 10:31:16 +0100

    Fixed migration scripts again.

--- protected/migrations/m110328_135455_alter_element_visual_acuity.php
+++ protected/migrations/m110328_135455_alter_element_visual_acuity.php
@@ -4,9 +4,6 @@ class m110328_135455_alter_element_visual_acuity extends CDbMigration
 {
     public function up()
     {
-		$command = Yii::app()->db->createCommand("INSERT INTO `event_type` (`id`, `name`, `first_in_episode_possible`) VALUES (24, 'example', 0)");
-		$command->execute();
-
 		$this->dropColumn('element_visual_acuity', 'format');
 		$this->dropColumn('element_visual_acuity', 'aid');
    		$this->addColumn('element_visual_acuity', 'right_aid', 'TINYINT(1) UNSIGNED');
@@ -15,9 +12,6 @@ class m110328_135455_alter_element_visual_acuity extends CDbMigration
 
     public function down()
     {
-		$command = Yii::app()->db->createCommand("DELETE FROM `event_type` WHERE `id` = 24");
-		$command->execute();
-
 		$this->dropColumn('element_visual_acuity', 'right_aid');
 		$this->dropColumn('element_visual_acuity', 'left_aid');
 		$this->addColumn('element_visual_acuity', 'aid', 'TINYINT(1) UNSIGNED');
---
+++ protected/migrations/m110329_092922_add_event_type_data.php
@@ -0,0 +1,16 @@
+<?php
+
+class m110329_092922_add_event_type_data extends CDbMigration
+{
+    public function up()
+    {
+		$command = Yii::app()->db->createCommand("INSERT INTO `event_type` (`id`, `name`, `first_in_episode_possible`) VALUES (24, 'example', 0)");
+		$command->execute();
+    }
+
+    public function down()
+    {
+		$command = Yii::app()->db->createCommand("DELETE FROM `event_type` WHERE `id` = 24");
+		$command->execute();
+    }
+}
