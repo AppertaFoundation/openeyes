51bd40acd71324830e4b2f1b4db19999a1c3a8b0
andersonprl <Paul.Anderson@moorfields.nhs.uk>
2011-04-06 17:23:33 +0100

    Updated some method definitions to work with E_STRICT error reporting.

--- protected/modules/admin/controllers/FirmController.php
+++ protected/modules/admin/controllers/FirmController.php
@@ -2,46 +2,16 @@
 
 class FirmController extends Controller
 {
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
 	public $layout='column2';
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
+	protected function beforeAction($action)
 	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-		);
-	}
+		// Sample code to be used when RBAC is fully implemented.
+		if (!Yii::app()->user->checkAccess('admin')) {
+			throw new CHttpException(403, 'You are not authorised to perform this action.');
+		}
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
+		return parent::beforeAction($action);
 	}
 
 	/**
