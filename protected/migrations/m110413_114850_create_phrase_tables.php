5d00eb0d8b9e74507751aafeb911b5b9b373f5c1
Chris Raettig <chris@raettig.org>
2011-04-14 12:02:43 +0100

    created additional element table to keep things separate.

--- protected/migrations/m110413_114850_create_phrase_tables.php
+++ protected/migrations/m110413_114850_create_phrase_tables.php
@@ -4,7 +4,7 @@ class m110413_114850_create_phrase_tables extends CDbMigration
 {
 	public function up()
 	{
-		$this->createTable('section', array(
+		$this->createTable('section_by_specialty', array(
 			'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
 			'name' => 'varchar(255) COLLATE utf8_bin DEFAULT NULL',
 			'PRIMARY KEY (`id`)'
@@ -14,26 +14,32 @@ class m110413_114850_create_phrase_tables extends CDbMigration
 			'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
 			'name' => 'varchar(255) COLLATE utf8_bin DEFAULT NULL',
 			'phrase' => 'text COLLATE utf8_bin DEFAULT NULL',
-			'section_id' => 'int(10) unsigned NOT NULL',
+			'section_by_specialty_id' => 'int(10) unsigned NOT NULL',
 			'display_order' => 'int(10) unsigned',
 			'specialty_id' => 'int(10) unsigned NOT NULL',
 			'PRIMARY KEY (`id`)'
 			), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin'
 		);
-		$this->addForeignKey('section_fk', 'phrase_by_specialty', 'section_id', 'section', 'id');
-		$this->addForeignKey('specialty_fk', 'phrase_by_specialty', 'specialty_id', 'specialty', 'id');
+		$this->addForeignKey('phrase_by_specialty_section_fk', 'phrase_by_specialty', 'section_by_specialty_id', 'section_by_specialty', 'id');
+		$this->addForeignKey('phrase_by_specialty_specialty_fk', 'phrase_by_specialty', 'specialty_id', 'specialty', 'id');
 
+		$this->createTable('section_by_firm', array(
+			'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
+			'name' => 'varchar(255) COLLATE utf8_bin DEFAULT NULL',
+			'PRIMARY KEY (`id`)'
+			), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin'
+		);
 		$this->createTable('phrase_by_firm', array(
 			'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
 			'name' => 'varchar(255) COLLATE utf8_bin DEFAULT NULL',
 			'phrase' => 'text COLLATE utf8_bin DEFAULT NULL',
-			'section_id' => 'int(10) unsigned NOT NULL',
+			'section_by_firm_id' => 'int(10) unsigned NOT NULL',
 			'display_order' => 'int(10) unsigned',
 			'firm_id' => 'int(10) unsigned NOT NULL',
 			'PRIMARY KEY (`id`)'
 			), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin'
 		);
-		$this->addForeignKey('phrase_by_firm_section_fk', 'phrase_by_firm', 'section_id', 'section', 'id');
+		$this->addForeignKey('phrase_by_firm_section_fk', 'phrase_by_firm', 'section_by_firm_id', 'section_by_firm', 'id');
 		$this->addForeignKey('phrase_by_firm_firm_fk', 'phrase_by_firm', 'firm_id', 'firm', 'id');
 
 		// FIXME
