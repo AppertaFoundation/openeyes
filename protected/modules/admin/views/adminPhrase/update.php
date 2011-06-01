7a4d409ddbe2f38072cb0e66046f475606b000ba
Chris Raettig <chris@raettig.org>
2011-06-01 13:31:08 +0100

    .

--- protected/modules/admin/views/adminPhrase/update.php
+++ protected/modules/admin/views/adminPhrase/update.php
@@ -1,7 +1,7 @@
 <?php
 $this->breadcrumbs=array(
 	'Global phrases'=>array('index'),
-	$model->name=>array('view','id'=>$model->id),
+	 $model->id => array('view','id'=>$model->id),
 	'Update',
 );
 
