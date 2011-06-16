d57dd2fa6133f5b0cf6cdf2c35c2290e7f552a3e
Chris Raettig <chris@raettig.org>
2011-06-16 15:28:35 +0100

    initial styling for element letterout

--- protected/views/elements/ElementLetterOut/_form/1.php
+++ protected/views/elements/ElementLetterOut/_form/1.php
@@ -1,34 +1,62 @@
-Letter out: <br />
+<h2 class="element_letterout">Letter out</h2>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'from_address'); ?>
+<div class="row_element_letterout">
+	<h3 class="element_letterout"><?php echo $form->labelEx($model,'from_address'); ?></h3 class="element_letterout">
+	<span class="element_letterout_left">...
+	</span>
+	<span class="element_letterout_right">
 		<?php echo $form->textArea($model,'from_address',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'from_address'); ?>
-	</div>
+	</span>
+</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'date'); ?>
+<div class="row_element_letterout">
+	<h3 class="element_letterout"><?php echo $form->labelEx($model,'date'); ?></h3 class="element_letterout">
+	<span class="element_letterout_left">...
+	</span>
+	<span class="element_letterout_right">
 		<?php echo $form->textField($model, 'date'); ?>
 		<?php echo $form->error($model,'date'); ?>
-	</div>
-	<div class="row">
-		<?php echo $form->labelEx($model,'dear'); ?>
+	</span>
+</div>
+	
+<div class="row_element_letterout">
+	<h3 class="element_letterout"><?php echo $form->labelEx($model,'dear'); ?></h3 class="element_letterout">
+	<span class="element_letterout_left">...
+	</span>
+	<span class="element_letterout_right">
 		<?php echo $form->textField($model, 'dear'); ?>
 		<?php echo $form->error($model,'dear'); ?>
-	</div>
-	<div class="row">
-		<?php echo $form->labelEx($model,'re'); ?>
+	</span>
+</div>
+
+<div class="row_element_letterout">
+	<h3 class="element_letterout"><?php echo $form->labelEx($model,'re'); ?></h3 class="element_letterout">
+	<span class="element_letterout_left">...
+	</span>
+	<span class="element_letterout_right">
 		<?php echo $form->textField($model, 're'); ?>
 		<?php echo $form->error($model,'re'); ?>
-	</div>
+	</span>
+</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'value'); ?>
+<div class="row_element_letterout">
+	<h3 class="element_letterout"><?php echo $form->labelEx($model,'value'); ?></h3 class="element_letterout">
+	<span class="element_letterout_left">...
+	</span>
+	<span class="element_letterout_right">
 		<?php echo $form->textArea($model,'value',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'value'); ?>
-	</div>
-	<div class="row">
-		<?php echo $form->labelEx($model,'to_address'); ?>
+	</span>
+</div>
+
+<div class="row_element_letterout">
+	<h3 class="element_letterout"><?php echo $form->labelEx($model,'to_address'); ?></h3 class="element_letterout">
+	<span class="element_letterout_left">...
+	</span>
+	<span class="element_letterout_right">
 		<?php echo $form->textArea($model,'to_address',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'to_address'); ?>
-	</div>
+	</span>
+</div>
+
