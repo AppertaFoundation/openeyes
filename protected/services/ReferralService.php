6c47b98fc0d3521b37fd28b79bcd05f97b0fcf94
Shanthi Hittler <shanthi.hittler@openeyes.org.uk>
2011-08-17 12:06:09 +0100

    Improving BookingServiceTest and general code cleanup

--- protected/services/ReferralService.php
+++ protected/services/ReferralService.php
@@ -1,6 +1,8 @@
 <?php
+
 class ReferralService
 {
+
 	/**
 	 * Perform a search based on the patient pas key
 	 *
@@ -53,85 +55,85 @@ class ReferralService
 		}
 
 		// Check for an open episode for this patient and firm's service with a referral
-                $episode = Yii::app()->db->createCommand()
-                        ->select('referral_id AS rid')
-                        ->from('referral_episode_assignment r_e_a')
-                        ->join('episode e', 'e.id = r_e_a.episode_id')
+		$episode = Yii::app()->db->createCommand()
+			->select('referral_id AS rid')
+			->from('referral_episode_assignment r_e_a')
+			->join('episode e', 'e.id = r_e_a.episode_id')
 			->join('firm f', 'e.firm_id = f.id')
 			->join('service_specialty_assignment s_s_a', 'f.service_specialty_assignment_id = s_s_a.id')
-                        ->where('e.end_date IS NULL AND e.patient_id = :patient_id AND s_s_a.service_id = :service_id', array(
+			->where('e.end_date IS NULL AND e.patient_id = :patient_id AND s_s_a.service_id = :service_id', array(
 				':patient_id' => $patientId, ':service_id' => $firm->serviceSpecialtyAssignment->service_id
 			))
-                        ->queryRow();
+			->queryRow();
 
 		if (isset($episode['rid'])) {
 			// There is an open episode and it has a referral, no action required
 			return false;
 		}
 
-                // Look for open referrals of this specialty
-                // @todo - change this to just get the top one
-                // @todo - is refno DESC the correct way of determining the most recent referral?
-                $referrals = Referral::model()->findAll(
-                        array(
-                                'order' => 'refno DESC',
-                                'condition' => 'patient_id = :p AND service_id = :s AND closed = 0',
-                                'params' => array(
-                                        ':p' => $patientId,
-                                        ':s' => $firm->serviceSpecialtyAssignment->service_id
-                                )
-                        )
-                );
+		// Look for open referrals of this specialty
+		// @todo - change this to just get the top one
+		// @todo - is refno DESC the correct way of determining the most recent referral?
+		$referrals = Referral::model()->findAll(
+			array(
+				'order' => 'refno DESC',
+				'condition' => 'patient_id = :p AND service_id = :s AND closed = 0',
+				'params' => array(
+					':p' => $patientId,
+					':s' => $firm->serviceSpecialtyAssignment->service_id
+				)
+			)
+		);
 
-                if (count($referrals)) {
+		if (count($referrals)) {
 			// There is at least one open referral for this service, so return that.
-                        return array($referrals[0]);
-                }
+			return array($referrals[0]);
+		}
 
-                // There are no open referrals for this specialty, try and find open referrals for a different
-                // specialty
-                $referrals = Referral::model()->findAll(
-                        array(
-                                'order' => 'refno DESC',
-                                'condition' => 'patient_id = :p AND closed = 0',
-                                'params' => array(':p' => $patientId)
-                        )
-                );
+		// There are no open referrals for this specialty, try and find open referrals for a different
+		// specialty
+		$referrals = Referral::model()->findAll(
+			array(
+				'order' => 'refno DESC',
+				'condition' => 'patient_id = :p AND closed = 0',
+				'params' => array(':p' => $patientId)
+			)
+		);
 
-                if (count($referrals)) {
-                        // There are referrals, use the newest one
-                        return $referrals;
-                }
+		if (count($referrals)) {
+			// There are referrals, use the newest one
+			return $referrals;
+		}
 
 		// There are no open referrals so no referral can be associated.
-                return false;
+		return false;
 	}
 
 	public function assignReferral($eventId, $firm, $patientId)
 	{
-                $referrals = $this->getReferralsList($firm, $patientId);
+		$referrals = $this->getReferralsList($firm, $patientId);
 
-                if (!isset($referrals) || !$referrals) {
-                        // Either there is already a referral for the episode or there are no open referrals
-                        // for this patient, so do nothing
-                        return;
-                }
+		if (!isset($referrals) || !$referrals) {
+			// Either there is already a referral for the episode or there are no open referrals
+			// for this patient, so do nothing
+			return;
+		}
 
-                if (is_array($referrals)) {
-                        // There is at least one referral - check to see if the referral_id provided by the user is in it.
-                        // If not, assign the first referral to the episode.
-                        if (isset($_REQUEST['referral_id'])) {
-                                foreach ($referrals as $referral) {
-                                        if ($referral->id = $_REQUEST['referral_id']) {
+		if (is_array($referrals)) {
+			// There is at least one referral - check to see if the referral_id provided by the user is in it.
+			// If not, assign the first referral to the episode.
+			if (isset($_REQUEST['referral_id'])) {
+				foreach ($referrals as $referral) {
+					if ($referral->id = $_REQUEST['referral_id']) {
 						$this->addReferral($eventId, $referral->id);
 						return;
-                                        }
-                                }
-                        }
+					}
+				}
+			}
 
-                        // No referral_id provided, or doesn't match, so assign the first referral in the list to the episode
+			// No referral_id provided, or doesn't match, so assign the first referral in the list to the episode
 			$this->addReferral($eventId, $referrals[0]->id);
-                }
+		}
 	}
 
 	public function addReferral($eventId, $referralId)
@@ -147,6 +149,6 @@ class ReferralService
 
 		$rea->episode_id = $event->episode_id;
 		$rea->referral_id = $referralId;
-		$rea->save();	
+		$rea->save();
 	}
 }
