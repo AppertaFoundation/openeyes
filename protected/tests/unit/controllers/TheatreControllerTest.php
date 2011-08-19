3c63fef7431cb73b8451a807d9c8767d5c5ffb5c
Shanthi Hittler <shanthi.hittler@openeyes.org.uk>
2011-08-19 15:34:12 +0100

    Ajax-ing the search action for theatres, so that less data gets passed around and the page doesn't reload
    - Moving all results-based html/javascript into its own partial -
    Attaching ajax action for the search form submit button so that it uses
    the partial - Altering controller code/tests to have a new action
    (actionSearch) that uses the partial (_list) to render results -
    Updating CSS

--- protected/tests/unit/controllers/TheatreControllerTest.php
+++ protected/tests/unit/controllers/TheatreControllerTest.php
@@ -22,7 +22,7 @@ class TheatreControllerTest extends CDbTestCase
 		parent::setUp();
 		ob_start();
 	}
-	
+
 	protected function tearDown()
 	{
 		ob_end_clean();
@@ -34,253 +34,126 @@ class TheatreControllerTest extends CDbTestCase
 			array('TheatreController'));
 		$mockController->expects($this->any())
 			->method('render')
-			->with('index', array(
-				'theatres'=>array(),
-				'siteId' => null,
-				'serviceId' => null,
-				'firmId' => null,
-				'theatreId' => null,
-				'wardId' => null,
-				'dateFilter' => null,
-				'theatreList' => array(),
-				'firmList' => array(),
-				'wardList' => array(),
-				'dateStart' => null,
-				'dateEnd' => null,
-			));
+			->with('index');
 		$this->assertNull($mockController->actionIndex());
 	}
 
-	public function testActionIndex_WithSiteId_RendersIndexView()
+	public function testActionSearch_NoPostData_RendersIndexView()
 	{
-		$_POST['site-id'] = $this->sites['site1']['id'];
-		
-		$theatre1 = $this->theatres['theatre1'];
-		$theatre2 = $this->theatres['theatre2'];
-		
-		$theatreList = array(
-			$theatre1['id'] => $theatre1['name'],
-			$theatre2['id'] => $theatre2['name'],
-		);
-		
-		$ward1 = $this->wards['ward1'];
-		$ward2 = $this->wards['ward2'];
-		$ward3 = $this->wards['ward3'];
-		$ward4 = $this->wards['ward4'];
-		
-		$wardList = array(
-			$ward1['id'] => $ward1['name'],
-			$ward2['id'] => $ward2['name'],
-			$ward3['id'] => $ward3['name'],
-			$ward4['id'] => $ward4['name']
-		);
-		
-		$mockController = $this->getMock('TheatreController', array('render'),
+		$mockController = $this->getMock('TheatreController', array('renderPartial'),
 			array('TheatreController'));
 		$mockController->expects($this->any())
-			->method('render')
-			->with('index', array(
-				'theatres'=>array(),
-				'siteId' => $_POST['site-id'],
-				'serviceId' => null,
-				'firmId' => null,
-				'theatreId' => null,
-				'wardId' => null,
-				'dateFilter' => null,
-				'theatreList' => $theatreList,
-				'firmList' => array(),
-				'wardList' => $wardList,
-				'dateStart' => null,
-				'dateEnd' => null,
-			));
-		$this->assertNull($mockController->actionIndex());
+			->method('renderPartial')
+			->with('_list', array('theatres'=>array()), false, true);
+		$this->assertNull($mockController->actionSearch());
 	}
 
-	public function testActionIndex_WithTheatreId_RendersIndexView()
+	public function testActionSearch_WithSiteId_RendersIndexView()
+	{
+		$_POST['site-id'] = $this->sites['site1']['id'];
+
+		$mockController = $this->getMock('TheatreController', array('renderPartial'),
+			array('TheatreController'));
+		$mockController->expects($this->any())
+			->method('renderPartial')
+			->with('_list', array('theatres'=>array()), false, true);
+		$this->assertNull($mockController->actionSearch());
+	}
+
+	public function testActionSearch_WithTheatreId_RendersIndexView()
 	{
 		$theatreId = $this->theatres['theatre1']['id'];
 		$_POST['theatre-id'] = $theatreId;
-		
-		$mockController = $this->getMock('TheatreController', array('render'),
+
+		$mockController = $this->getMock('TheatreController', array('renderPartial'),
 			array('TheatreController'));
 		$mockController->expects($this->any())
-			->method('render')
-			->with('index', array(
-				'theatres'=>array(),
-				'siteId' => null,
-				'serviceId' => null,
-				'firmId' => null,
-				'theatreId' => $theatreId,
-				'wardId' => null,
-				'dateFilter' => null,
-				'theatreList' => array(),
-				'firmList' => array(),
-				'wardList' => array(),
-				'dateStart' => null,
-				'dateEnd' => null,
-			));
-		$this->assertNull($mockController->actionIndex());
+			->method('renderPartial')
+			->with('_list', array('theatres'=>array()), false, true);
+		$this->assertNull($mockController->actionSearch());
 	}
 
-	public function testActionIndex_WithServiceId_RendersIndexView()
+	public function testActionSearch_WithServiceId_RendersIndexView()
 	{
 		$serviceId = $this->serviceSpecialtyAssignments['servicespecialtyassignment1']['service_id'];
 		$_POST['service-id'] = $serviceId;
-		
-		$firmList = array($this->firms['firm1']['id'] => $this->firms['firm1']['name']);
-		
-		$mockController = $this->getMock('TheatreController', array('render'),
+
+		$mockController = $this->getMock('TheatreController', array('renderPartial'),
 			array('TheatreController'));
 		$mockController->expects($this->any())
-			->method('render')
-			->with('index', array(
-				'theatres'=>array(),
-				'siteId' => null,
-				'serviceId' => $serviceId,
-				'firmId' => null,
-				'theatreId' => null,
-				'wardId' => null,
-				'dateFilter' => null,
-				'theatreList' => array(),
-				'firmList' => $firmList,
-				'wardList' => array(),
-				'dateStart' => null,
-				'dateEnd' => null,
-			));
-		$this->assertNull($mockController->actionIndex());
+			->method('renderPartial')
+			->with('_list', array('theatres'=>array()), false, true);
+		$this->assertNull($mockController->actionSearch());
 	}
 
-	public function testActionIndex_WithFirmId_RendersIndexView()
+	public function testActionSearch_WithFirmId_RendersIndexView()
 	{
 		$firmId = $this->firms['firm1']['id'];
 		$_POST['firm-id'] = $firmId;
-		
-		$mockController = $this->getMock('TheatreController', array('render'),
+
+		$mockController = $this->getMock('TheatreController', array('renderPartial'),
 			array('TheatreController'));
 		$mockController->expects($this->any())
-			->method('render')
-			->with('index', array(
-				'theatres'=>array(),
-				'siteId' => null,
-				'serviceId' => null,
-				'firmId' => $firmId,
-				'theatreId' => null,
-				'wardId' => null,
-				'dateFilter' => null,
-				'theatreList' => array(),
-				'firmList' => array(),
-				'wardList' => array(),
-				'dateStart' => null,
-				'dateEnd' => null,
-			));
-		$this->assertNull($mockController->actionIndex());
+			->method('renderPartial')
+			->with('_list', array('theatres'=>array()), false, true);
+		$this->assertNull($mockController->actionSearch());
 	}
 
-	public function testActionIndex_WithWardId_RendersIndexView()
+	public function testActionSearch_WithWardId_RendersIndexView()
 	{
 		$wardId = $this->wards['ward1']['id'];
 		$_POST['ward-id'] = $wardId;
-		
-		$mockController = $this->getMock('TheatreController', array('render'),
+
+		$mockController = $this->getMock('TheatreController', array('renderPartial'),
 			array('TheatreController'));
 		$mockController->expects($this->any())
-			->method('render')
-			->with('index', array(
-				'theatres'=>array(),
-				'siteId' => null,
-				'serviceId' => null,
-				'firmId' => null,
-				'theatreId' => null,
-				'wardId' => $wardId,
-				'dateFilter' => null,
-				'theatreList' => array(),
-				'firmList' => array(),
-				'wardList' => array(),
-				'dateStart' => null,
-				'dateEnd' => null,
-			));
-		$this->assertNull($mockController->actionIndex());
+			->method('renderPartial')
+			->with('_list', array('theatres'=>array()), false, true);
+		$this->assertNull($mockController->actionSearch());
 	}
 
-	public function testActionIndex_DailyDateFilter_RendersIndexView()
+	public function testActionSearch_DailyDateFilter_RendersIndexView()
 	{
 		$dateFilter = 'today';
 		$_POST['date-filter'] = $dateFilter;
-		
-		$mockController = $this->getMock('TheatreController', array('render'),
+
+		$mockController = $this->getMock('TheatreController', array('renderPartial'),
 			array('TheatreController'));
 		$mockController->expects($this->any())
-			->method('render')
-			->with('index', array(
-				'theatres'=>array(),
-				'siteId' => null,
-				'serviceId' => null,
-				'firmId' => null,
-				'theatreId' => null,
-				'wardId' => null,
-				'dateFilter' => $dateFilter,
-				'theatreList' => array(),
-				'firmList' => array(),
-				'wardList' => array(),
-				'dateStart' => null,
-				'dateEnd' => null,
-			));
-		$this->assertNull($mockController->actionIndex());
+			->method('renderPartial')
+			->with('_list', array('theatres'=>array()), false, true);
+		$this->assertNull($mockController->actionSearch());
 	}
 /*
-	public function testActionIndex_WeeklyDateFilter_RendersIndexView()
+	public function testActionSearch_WeeklyDateFilter_RendersIndexView()
 	{
 		$dateFilter = 'week';
 		$_POST['date-filter'] = $dateFilter;
-		
-		$mockController = $this->getMock('TheatreController', array('render'),
+
+		$mockController = $this->getMock('TheatreController', array('renderPartial'),
 			array('TheatreController'));
 		$mockController->expects($this->any())
-			->method('render')
-			->with('index', array(
-				'theatres'=>array(),
-				'siteId' => null,
-				'serviceId' => null,
-				'firmId' => null,
-				'theatreId' => null,
-				'wardId' => null,
-				'dateFilter' => $dateFilter,
-				'theatreList' => array(),
-				'wardList' => array(),
-				'firmList' => array(),
-				'dateStart' => null,
-				'dateEnd' => null,
-			));
-		$this->assertNull($mockController->actionIndex());
+			->method('renderPartial')
+			->with('_list', array('theatres'=>array()), false, true);
+		$this->assertNull($mockController->actionSearch());
 	}
 */
 	// @todo: figure out how to generate expected theatre data so this test reliably passes
-//	public function testActionIndex_MonthlyDateFilter_RendersIndexView()
+//	public function testActionSearch_MonthlyDateFilter_RendersIndexView()
 //	{
 //		$dateFilter = 'month';
 //		$_POST['date-filter'] = $dateFilter;
-//		
-//		$mockController = $this->getMock('TheatreController', array('render'),
+//
+//		$mockController = $this->getMock('TheatreController', array('renderPartial'),
 //			array('TheatreController'));
 //		$mockController->expects($this->any())
-//			->method('render')
-//			->with('index', array(
-//				'theatres'=>array(),
-//				'siteId' => null,
-//				'serviceId' => null,
-//				'firmId' => null,
-//				'theatreId' => null,
-//				'dateFilter' => $dateFilter,
-//				'theatreList' => array(),
-//				'firmList' => array(),
-//				'dateStart' => null,
-//				'dateEnd' => null,
-//			));
-//		$this->assertNull($mockController->actionIndex());
+//			->method('renderPartial')
+//			->with('_list', array('theatres'=>array()), false, true);
+//		$this->assertNull($mockController->actionSearch());
 //	}
 
 	// @todo: figure out how to generate expected theatre data so this test reliably passes
-//	public function testActionIndex_CustomDateFilter_RendersIndexView()
+//	public function testActionSearch_CustomDateFilter_RendersIndexView()
 //	{
 //		$dateFilter = 'custom';
 //		$dateStart = date('Y-m-d');
@@ -288,166 +161,155 @@ class TheatreControllerTest extends CDbTestCase
 //		$_POST['date-filter'] = $dateFilter;
 //		$_POST['date-start'] = $dateStart;
 //		$_POST['date-end'] = $dateEnd;
-//		
-//		$mockController = $this->getMock('TheatreController', array('render'),
+//
+//		$mockController = $this->getMock('TheatreController', array('renderPartial'),
 //			array('TheatreController'));
 //		$mockController->expects($this->any())
-//			->method('render')
-//			->with('index', array(
-//				'theatres'=>array(),
-//				'siteId' => null,
-//				'serviceId' => null,
-//				'firmId' => null,
-//				'theatreId' => null,
-//				'dateFilter' => $dateFilter,
-//				'theatreList' => array(),
-//				'firmList' => array(),
-//				'dateStart' => $dateStart,
-//				'dateEnd' => $dateEnd,
-//			));
-//		$this->assertNull($mockController->actionIndex());
+//			->method('renderPartial')
+//			->with('_list', array('theatres'=>array()), false, true);
+//		$this->assertNull($mockController->actionSearch());
 //	}
-	
+
 	public function testActionFilterFirms_NoPostData_ListsOneFirm()
 	{
 		$mockController = $this->getMock('TheatreController', array('getFilteredFirms'),
 			array('TheatreController'));
-		
+
 		$mockController->expects($this->never())
 			->method('getFilteredFirms');
-		
+
 		$this->assertNull($mockController->actionFilterFirms());
 		$results = ob_get_contents();
-		
-		$expected = CHtml::tag('option', array('value'=>''), 
+
+		$expected = CHtml::tag('option', array('value'=>''),
 			CHtml::encode('All firms'), true);
-		
+
 		$this->assertEquals($expected, $results, 'Output should match.');
 	}
-	
+
 	public function testActionFilterFirms_ValidServiceId_ListsAllFirms()
 	{
 		$serviceId = $this->serviceSpecialtyAssignments['servicespecialtyassignment1']['service_id'];
 		$_POST['service_id'] = $serviceId;
 		$mockController = $this->getMock('TheatreController', array('getFilteredFirms'),
 			array('TheatreController'));
-		
+
 		$firmList = array($this->firms['firm1']['id'] => $this->firms['firm1']['name']);
-		
+
 		$mockController->expects($this->once())
 			->method('getFilteredFirms')
 			->with($serviceId)
 			->will($this->returnValue($firmList));
 		$this->assertNull($mockController->actionFilterFirms());
 		$results = ob_get_contents();
-		
-		$expected = CHtml::tag('option', array('value'=>''), 
+
+		$expected = CHtml::tag('option', array('value'=>''),
 			CHtml::encode('All firms'), true);
 		foreach ($firmList as $id => $name) {
-			$expected .= CHtml::tag('option', array('value'=>$id), 
+			$expected .= CHtml::tag('option', array('value'=>$id),
 				CHtml::encode($name), true);
 		}
-		
+
 		$this->assertEquals($expected, $results, 'Output should match.');
 	}
-	
+
 	public function testActionFilterTheatres_NoPostData_ListsOneTheatre()
 	{
 		$mockController = $this->getMock('TheatreController', array('getFilteredTheatres'),
 			array('TheatreController'));
-		
+
 		$mockController->expects($this->never())
 			->method('getFilteredTheatres');
 		$this->assertNull($mockController->actionFilterTheatres());
 		$results = ob_get_contents();
-		
-		$expected = CHtml::tag('option', array('value'=>''), 
+
+		$expected = CHtml::tag('option', array('value'=>''),
 			CHtml::encode('All theatres'), true);
-		
+
 		$this->assertEquals($expected, $results, 'Output should match.');
 	}
-	
+
 	public function testActionFilterTheatres_ValidSiteId_ListsAllTheatres()
 	{
 		$_POST['site_id'] = $this->sites['site1']['id'];
-		
+
 		$theatre1 = $this->theatres['theatre1'];
 		$theatre2 = $this->theatres['theatre2'];
-		
+
 		$theatreList = array(
 			$theatre1['id'] => $theatre1['name'],
 			$theatre2['id'] => $theatre2['name'],
 		);
-		
+
 		$mockController = $this->getMock('TheatreController', array('getFilteredTheatres'),
 			array('TheatreController'));
-		
+
 		$mockController->expects($this->once())
 			->method('getFilteredTheatres')
 			->with($_POST['site_id'])
 			->will($this->returnValue($theatreList));
 		$this->assertNull($mockController->actionFilterTheatres());
 		$results = ob_get_contents();
-		
-		$expected = CHtml::tag('option', array('value'=>''), 
+
+		$expected = CHtml::tag('option', array('value'=>''),
 			CHtml::encode('All theatres'), true);
 		foreach ($theatreList as $id => $name) {
-			$expected .= CHtml::tag('option', array('value'=>$id), 
+			$expected .= CHtml::tag('option', array('value'=>$id),
 				CHtml::encode($name), true);
 		}
-		
+
 		$this->assertEquals($expected, $results, 'Output should match.');
 	}
-	
+
 	public function testActionFilterWards_NoPostData_ListsOneWard()
 	{
 		$mockController = $this->getMock('TheatreController', array('getFilteredWards'),
 			array('TheatreController'));
-		
+
 		$mockController->expects($this->never())
 			->method('getFilteredWards');
 		$this->assertNull($mockController->actionFilterWards());
 		$results = ob_get_contents();
-		
-		$expected = CHtml::tag('option', array('value'=>''), 
+
+		$expected = CHtml::tag('option', array('value'=>''),
 			CHtml::encode('All wards'), true);
-		
+
 		$this->assertEquals($expected, $results, 'Output should match.');
 	}
-	
+
 	public function testActionFilterWards_ValidSiteId_ListsAllWards()
 	{
 		$_POST['site_id'] = $this->sites['site1']['id'];
-		
+
 		$ward1 = $this->wards['ward1'];
 		$ward2 = $this->wards['ward2'];
 		$ward3 = $this->wards['ward3'];
 		$ward4 = $this->wards['ward4'];
-		
+
 		$wardList = array(
 			$ward1['id'] => $ward1['name'],
 			$ward2['id'] => $ward2['name'],
 			$ward3['id'] => $ward3['name'],
 			$ward4['id'] => $ward4['name']
 		);
-		
+
 		$mockController = $this->getMock('TheatreController', array('getFilteredWards'),
 			array('TheatreController'));
-		
+
 		$mockController->expects($this->once())
 			->method('getFilteredWards')
 			->with($_POST['site_id'])
 			->will($this->returnValue($wardList));
 		$this->assertNull($mockController->actionFilterWards());
 		$results = ob_get_contents();
-		
-		$expected = CHtml::tag('option', array('value'=>''), 
+
+		$expected = CHtml::tag('option', array('value'=>''),
 			CHtml::encode('All wards'), true);
 		foreach ($wardList as $id => $name) {
-			$expected .= CHtml::tag('option', array('value'=>$id), 
+			$expected .= CHtml::tag('option', array('value'=>$id),
 				CHtml::encode($name), true);
 		}
-		
+
 		$this->assertEquals($expected, $results, 'Output should match.');
 	}
 }
---
+++ protected/views/theatre/_list.php
@@ -0,0 +1,119 @@
+<div id="theatreList">
+<?php
+$baseUrl = Yii::app()->baseUrl;
+$cs = Yii::app()->getClientScript();
+$cs->registerCSSFile('/css/theatre.css', 'all');
+$cs->registerCoreScript('jquery');
+$cs->registerCoreScript('jquery.ui');
+$cs->registerCSSFile('/css/jqueryui/theme/jquery-ui.css', 'all');
+$cs->registerScriptFile($baseUrl.'/js/jquery.multi-open-accordion-1.5.2.min.js');
+
+if (empty($theatres)) { ?>
+<h2 class="theatre">No theatre schedules match your search criteria.</h2>
+</div>
+<?php
+} else { ?>
+    <div id="multiOpenAccordion">
+<?php
+    $panels = array();
+	$firstTheatreShown = false;
+    foreach ($theatres as $name => $dates) { ?>
+<h2 class="theatre<?php if (!$firstTheatreShown) { echo ' firstTheatre'; } ?>"><?php echo $name; ?></h2>
+<?php   $firstTheatreShown = true;
+		foreach ($dates as $date => $sessions) {
+            $timestamp = strtotime($date); ?>
+<h3 class="date"><a href="#"><?php echo date('d ', $timestamp);
+            echo substr(date('F', $timestamp), 0, 3);
+            echo date(' Y', $timestamp);
+            echo ' - ' . date('l', $timestamp); ?></a></h3>
+<div>
+    <table>
+    <tr>
+        <th class="first">Session</th>
+        <th class="repeat leftAlign">Patient (Age)</th>
+        <th class="repeat leftAlign">[Eye] Operation</th>
+        <th class="repeat">Duration</th>
+        <th class="repeat">Ward</th>
+        <th class="repeat">Anaesthetic</th>
+        <th class="last">Alerts</th>
+    </tr>
+<?php        $lastSession = $sessions[0];
+            foreach ($sessions as $session) {
+                if ($session['sessionId'] != $lastSession['sessionId']) { ?>
+    <tr>
+        <th class="footer" colspan="7">Time unallocated: <?php
+                    echo '<span';
+                    if ($lastSession['timeAvailable'] < 0) {
+                        echo ' class="full"';
+                    }
+                    echo ">{$lastSession['timeAvailable']}"; ?>min</span></th>
+    </tr>
+<?php                $lastSession = $session;
+                } ?>
+    <tr>
+        <td class="session"><?php echo substr($session['startTime'], 0, 5) . '-' . substr($session['endTime'], 0, 5); ?></td>
+        <td class="patient leftAlign"><?php echo $session['patientName'] . ' (' . $session['patientAge'] . ')'; ?></td>
+        <td class="operation leftAlign">[<?php echo $session['eye']; ?>] <?php echo !empty($session['procedures']) ? $session['procedures'] : 'No procedures'; ?></td>
+        <td class="duration"><?php echo $session['operationDuration']; ?></td>
+        <td class="ward"><?php echo $session['ward']; ?></td>
+        <td class="anaesthetic"><?php echo $session['anaesthetic']; ?></td>
+        <td class="alerts"><div class="alert gender invisible <?php echo $session['patientGender']; ?>"></div><?php
+        if (!empty($session['operationComments'])) { ?><div class="alert comments invisble"><img class="invisible" src="/images/icon_comments.gif" alt="comments"title="<?php echo $session['operationComments']; ?>" /></div><?php
+        } ?></td>
+    </tr>
+<?php
+            } ?>
+    <tr>
+        <th class="footer" colspan="7">Time unallocated: <?php
+                    echo '<span';
+                    if ($session['timeAvailable'] < 0) {
+                       echo ' class="full"';
+                    }
+                    echo ">{$session['timeAvailable']}"; ?>min</span></th>
+    </tr>
+    </table>
+</div>
+<?php
+        }
+    }
+    ?>
+    </div>
+</div>
+<div id="alertOptions">
+    <input type="checkbox" name="theatre_alerts" value="comments" /> Comments<br />
+    <input type="checkbox" name="theatre_alerts" value="gender" /> Gender<br />
+    <input type="checkbox" name="theatre_alerts" value="latex" disabled="true" /> Latex allergy<br />
+    <input type="checkbox" name="theatre_alerts" value="consultant" disabled="true" /> Consultant required
+</div>
+<div class="clear"></div>
+<script type="text/javascript">
+	$('input[name=theatre_alerts][value=comments]').click(function() {
+		if ($(this).is(':checked')) {
+			$('.comments').removeClass('invisible');
+			$('.comments img').removeClass('invisible');
+		} else {
+			$('.comments').addClass('invisible');
+			$('.comments img').addClass('invisible');
+		}
+	});
+	$('input[name=theatre_alerts][value=gender]').click(function() {
+		if ($(this).is(':checked')) {
+			$('.gender').removeClass('invisible');
+		} else {
+			$('.gender').addClass('invisible');
+		}
+	});
+	$('#multiOpenAccordion').multiOpenAccordion({
+		autoHeight: false,
+		clearStyle: true });
+	// if we've selected today, or a same-day custom date range, show expanded
+	if ('today' == $('input[name=date-filter]:checked').val() ||
+		($('input[name=date-filter]:checked').val() == 'custom' &&
+		$('input[id=date-start]').val() == $('input[id=date-end]').val())) {
+		$('#multiOpenAccordion').multiOpenAccordion("option", "active", "all");
+	} else {
+		$('#multiOpenAccordion').multiOpenAccordion("option", "active", "none");
+	}
+</script>
+<?php
+} ?>
