99a76c21c5ab85cd36f61c9b7341b15441814e5b
Shanthi Hittler <shanthi.hittler@openeyes.org.uk>
2011-09-07 11:47:09 +0100

    Add tests for Contact model (OE-37)

--- protected/tests/fixtures/contact.php
+++ protected/tests/fixtures/contact.php
@@ -3,11 +3,29 @@
 return array(
 	'contact1' => array(
 		'nick_name' => 'Aylward',
+		'address_id' => 1,
+		'primary_phone' => '07123 456789',
+		'title' => 'Mr',
+		'first_name' => 'Jim',
+		'last_name' => 'Aylward',
+		'qualifications' => ''
 	),
 	'contact2' => array(
 		'nick_name' => 'Collin',
+		'address_id' => 2,
+		'primary_phone' => '07234 567890',
+		'title' => 'Mr',
+		'first_name' => 'Bob',
+		'last_name' => 'Collin',
+		'qualifications' => ''
 	),
 	'contact3' => array(
 		'nick_name' => 'Allan',
+		'address_id' => 3,
+		'primary_phone' => '07987 654321',
+		'title' => 'Mr',
+		'first_name' => 'Edward',
+		'last_name' => 'Allan',
+		'qualifications' => ''
 	),
 );
---
+++ protected/tests/unit/models/ContactFormTest.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: shanthi
+ * Date: 07/09/2011
+ * Time: 11:45
+ * To change this template use File | Settings | File Templates.
+ */
+
