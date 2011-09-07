81c9a05c8ca9d2ce65e7e232e91c2f8d9ccb0573
Shanthi Hittler <shanthi.hittler@openeyes.org.uk>
2011-09-07 11:47:22 +0100

    Add tests for ContactForm model (OE-37)

--- protected/tests/unit/models/ContactFormTest.php
+++ protected/tests/unit/models/ContactFormTest.php
@@ -1,9 +1,20 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: shanthi
- * Date: 07/09/2011
- * Time: 11:45
- * To change this template use File | Settings | File Templates.
- */
+class ContactFormTest extends CDbTestCase
+{
+	public $model;
 
+	public function setUp()
+	{
+		parent::setUp();
+		$this->model = new ContactForm;
+	}
+
+	public function testAttributeLabels()
+	{
+		$expected = array(
+			'verifyCode'=>'Verification Code',
+		);
+
+		$this->assertEquals($expected, $this->model->attributeLabels(), 'Attribute labels should match.');
+	}
+}
