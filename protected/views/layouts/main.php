2fd5d205d9a801bc22931b404825e437caa16b79
Mark Wadham <mark.wadham@openeyes.org.uk>
2011-09-14 15:14:55 +0100

    initial work on getting boilerplate into yii and beginning to integrate Toby's layout changes into the template layouts.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,31 +1,27 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
-
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-</head>
-
-<body>
-
-<div class="container" id="page">
-
-	<div id="header">
-		<div id="logo"><a href="/site/index"><img src="/images/logo_colour.png" alt="OpenEyes Logo" /></a></div>
-
+<!doctype html> 
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]--> 
+<!--[if IE 7]>		<html class="no-js ie7 oldie" lang="en"> <![endif]--> 
+<!--[if IE 8]>		<html class="no-js ie8 oldie" lang="en"> <![endif]--> 
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]--> 
+<head> 
+	<meta charset="utf-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+ 
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title> 
+	<meta name="viewport" content="width=device-width"> 
+	<link rel="icon" href="favicon.ico" type="image/x-icon" /> 
+	<link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/favicon.ico"/> 
+	<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css"> 
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/libs/modernizr-2.0.6.min.js"></script> 
+</head> 
+ 
+<body> 
+ 
+	<div id="container"> 
+	<div id="header" class="clearfix"> 
+		<div id="brand" class="ir"><h1>OpenEyes</h1></div> 
 		<?php echo $this->renderPartial('//base/_form', array()); ?>
-	</div><!-- header -->
+	</div> <!-- #header --> 
 
 	<!--div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
@@ -40,32 +36,39 @@
 		)); ?>
 	</div--><!-- mainmenu -->
 
-	<div id="outer_content">
-		<div id="content">
+	<div id="content"> 
 		<?php echo $content; ?>
-		</div>
-	</div>
-
-	<div id="footer">
-		Copyright OpenEyes Foundation 2011<br/>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
-<script type="text/javascript">
-	$('select[id=selected_firm_id]').live('change', function() {
-		var firmId = $('select[id=selected_firm_id]').val();
-		$.ajax({
-			type: 'post',
-			url: '<?php echo Yii::app()->createUrl('site'); ?>',
-			data: {'selected_firm_id': firmId },
-			success: function(data) {
-				console.log(data);
-				window.location.href = '<?php echo Yii::app()->createUrl('site'); ?>';
-			}
+	</div> <!-- #content --> 
+	<div id="help" class="clearfix"> 
+	</div> <!-- #help --> 
+	</div> 
+	<!--#container --> 
+	
+	<div id="footer"> 
+		<h6>&copy; Copyright OpenEyes Foundation 2011 &nbsp;&nbsp;|&nbsp;&nbsp; Terms of Use &nbsp;&nbsp;|&nbsp;&nbsp; Legals</h6> 
+	</div> <!-- #footer --> 
+ 
+ 
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script> 
+	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script> 
+ 
+	<script defer src="<?php echo Yii::app()->request->baseUrl; ?>/js/plugins.js"></script>
+	<script defer src="<?php echo Yii::app()->request->baseUrl; ?>/js/script.js"></script>
+	
+	<script type="text/javascript">
+		$('select[id=selected_firm_id]').live('change', function() {
+			var firmId = $('select[id=selected_firm_id]').val();
+			$.ajax({
+				type: 'post',
+				url: '<?php echo Yii::app()->createUrl('site'); ?>',
+				data: {'selected_firm_id': firmId },
+				success: function(data) {
+					console.log(data);
+					window.location.href = '<?php echo Yii::app()->createUrl('site'); ?>';
+				}
+			});
 		});
-	});
-</script>
+	</script>
 
-</body>
+</body> 
 </html>
--- protected/views/layouts/simple.php
+++ protected/views/layouts/simple.php
@@ -1,44 +1,49 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+<!doctype html> 
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]--> 
+<!--[if IE 7]>		<html class="no-js ie7 oldie" lang="en"> <![endif]--> 
+<!--[if IE 8]>		<html class="no-js ie8 oldie" lang="en"> <![endif]--> 
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]--> 
+<head> 
+	<meta charset="utf-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+ 
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title> 
+	<meta name="viewport" content="width=device-width"> 
+	<link rel="icon" href="favicon.ico" type="image/x-icon" /> 
+	<link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/favicon.ico"/> 
+	<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css"> 
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/libs/modernizr-2.0.6.min.js"></script> 
+</head> 
+ 
+<body> 
+ 
+	<div id="container"> 
+	<div id="header" class="clearfix"> 
+		<div id="brand" class="ir"><h1>OpenEyes</h1></div> 
+	</div> <!-- #header --> 
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-</head>
-
-<body>
-
-<div class="container" id="page">
-	<div id="header">
-		<div id="logo"><img src="/images/logo_colour.png" alt="OpenEyes Logo" /></div>
-	</div>
-	
 	<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 		'links'=>$this->breadcrumbs,
 	)); ?><!-- breadcrumbs -->
 
-	<div id="outer_content">
-		<div id="content">
-			<?php echo $content; ?>
-		</div>
-	</div>
-
-	<div id="footer">
-		Copyright OpenEyes Foundation 2011<br/>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
-</body>
-</html>
+	<div id="content"> 
+		<?php echo $content; ?>
+	</div> <!-- #content --> 
+	<div id="help" class="clearfix"> 
+	</div> <!-- #help --> 
+	</div> 
+	<!--#container --> 
+	
+	<div id="footer"> 
+		<h6>&copy; Copyright OpenEyes Foundation 2011 &nbsp;&nbsp;|&nbsp;&nbsp; Terms of Use &nbsp;&nbsp;|&nbsp;&nbsp; Legals</h6> 
+	</div> <!-- #footer --> 
+ 
+ 
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script> 
+	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script> 
+ 
+	<script defer src="<?php echo Yii::app()->request->baseUrl; ?>/js/plugins.js"></script> 
+	<script defer src="<?php echo Yii::app()->request->baseUrl; ?>/js/script.js"></script> 
+	
+</body> 
+</html> 
