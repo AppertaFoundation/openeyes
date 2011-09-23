ebed389a594d65139e83e95ac61f7e9bb3f77ab0
Mark Wadham <mark.wadham@openeyes.org.uk>
2011-09-23 16:15:04 +0100

    added GPLv3 license to all files owned by OpenEyes.

--- protected/models/elements/ElementAllergies.php
+++ protected/models/elements/ElementAllergies.php
@@ -1,4 +1,16 @@
 <?php
+/*
+_____________________________________________________________________________
+(C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
+(C) OpenEyes Foundation, 2011
+This file is part of OpenEyes.
+OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+_____________________________________________________________________________
+http://www.openeyes.org.uk   info@openeyes.org.uk
+--
+*/
 
 /**
  * This is the model class for table "element_allergies".
