7032bdd9dbd66efce0a2d9b4b9178d7fe546113b
Mark Wadham <mark.wadham@openeyes.org.uk>
2011-10-12 14:02:44 +0100

    adjusted styling of "extra episode summary" block.

--- protected/views/clinical/episodeSummaries/8.php
+++ protected/views/clinical/episodeSummaries/8.php
@@ -12,24 +12,28 @@ http://www.openeyes.org.uk   info@openeyes.org.uk
 --
 */
 
-?>This is the extra episode summary detail page for Medical Retinal.
-<br />
-<br />
-As an example it includes the exampleSummary widget.
-<br />
-<?php $this->widget('application.components.summaryWidgets.ExampleSummary', array(
-   'episode_id' => $episode->id
-)); ?>
-<br />
-<br />
-And a link to view the exampleSummary widget on a different page:
-<br />
-<?php
+?>
+<div class="extraSummary">
+	This is the extra episode summary detail page for Medical Retinal.
+	<br />
+	<br />
+	As an example it includes the exampleSummary widget.
+	<br />
+	<?php $this->widget('application.components.summaryWidgets.ExampleSummary', array(
+		 'episode_id' => $episode->id
+	)); ?>
+	<br />
+	<br />
+	And a link to view the exampleSummary widget on a different page:
+	<br />
+	<?php
 
-echo CHtml::link(
-	'exampleSummary',
-	Yii::app()->createUrl('clinical/summary', array(
-		'id' => $episode->id,
-		'summary' => 'ExampleSummary'
-	))
-);
+	echo CHtml::link(
+		'exampleSummary',
+		Yii::app()->createUrl('clinical/summary', array(
+			'id' => $episode->id,
+			'summary' => 'ExampleSummary'
+		))
+	);
+?>
+</div>
