33cb9f41fa78b3bd4a3deb8b0a24e7f828b8cf44
Mark Wadham <mark.wadham@openeyes.org.uk>
2011-10-20 15:12:42 +0100

    hooked up "close episode" button.

--- protected/views/clinical/episodeSummary.php
+++ protected/views/clinical/episodeSummary.php
@@ -93,11 +93,54 @@ if (!empty($episode)) {
 					$(this).dialog('close');
 				}
 			},
-		    open: function() {
-    			$(this).parents('.ui-dialog-buttonpane button:eq(1)').focus();
-    		}
+				open: function() {
+					$(this).parents('.ui-dialog-buttonpane button:eq(1)').focus();
+				}
 		});
 		return false;
 	});
 	</script>
 </script>
+
+<?php if (empty($episode->end_date)) {?>
+	<div style="margin-top:40px; text-align:right; position:relative; ">
+		<button id="close-episode" type="submit" value="submit" class="wBtn_close-episode ir">Close Episode</button>
+
+		<div id="close-episode-popup" class="popup red" style="display: none;">
+			<p style="text-align:left;">You are closing this episode. This can not be undone. Once an episode is closed it can not be re-opened.</p>
+			<p><strong>Are you sure?</strong></p>
+			<div class="action_options">
+				<span class="aBtn"><a id="yes-close-episode" href="#"><strong>Yes, I am</strong></a></span>
+				<span class="aBtn"><a id="no-close-episode" href="#"><strong>No, cancel this.</strong></a></span>
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript">
+		$('#close-episode').unbind('click').click(function(e) {
+			e.preventDefault();
+			$('#close-episode-popup').slideToggle(100);
+			return false;
+		});
+
+		$('#no-close-episode').unbind('click').click(function(e) {
+			e.preventDefault();
+			$('#close-episode-popup').slideToggle(100);
+			return false;
+		});
+
+		$('#yes-close-episode').unbind('click').click(function(e) {
+			e.preventDefault();
+			$('#close-episode-popup').slideToggle(100);
+			$.ajax({
+				url: '/clinical/closeepisode/<?php echo $episode->id?>',
+				success: function(data) {
+					$('#event_content').html(data);
+					return false;
+				}
+			});
+
+			return false;
+		});
+	</script>
+<?php }?>
