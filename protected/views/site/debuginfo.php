8d4566657251cb45cd403aa01c52660d0f481abb
Mark Wadham <mark.wadham@openeyes.org.uk>
2011-10-25 15:00:53 +0100

    tweaked debug info to use git in the correct location on osx.

--- protected/views/site/debuginfo.php
+++ protected/views/site/debuginfo.php
@@ -1,4 +1,11 @@
 <?php
+
+if (trim(`uname`) == "Darwin") {
+	$git = "/opt/local/bin/git";
+} else {
+	$git = "/usr/bin/git";
+}
+
 if (!empty(Yii::app()->session['user'])) {
 	$user = Yii::app()->session['user'];
 } else {
@@ -25,7 +32,7 @@ if (is_object($user)) {
 		<li>Server: <?php echo $hostname?></li>
 		<li>Docroot: <?php echo @$_SERVER['DOCUMENT_ROOT']?></li>
 		<li>Date: <?php echo date('d.m.Y H:i:s')?></li>
-		<li>Commit: <?php echo trim(`git log |head -n1 |cut -d ' ' -f2`)?></li>
+		<li>Commit: <?php echo trim(`$git log |head -n1 |cut -d ' ' -f2`)?></li>
 		<li>User agent: <?php echo @$_SERVER['HTTP_USER_AGENT']?></li>
 		<li>Client IP: <?php echo @$_SERVER['REMOTE_ADDR']?></li>
 		<li>Username: <?php echo $username?></li>
