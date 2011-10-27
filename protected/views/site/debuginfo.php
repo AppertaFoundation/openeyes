08d47caa46030323ed866ea268421209fefb52e4
chris raettig <chris@raettig.org>
2011-10-27 17:25:56 +0000

    tweaked the debug text

--- protected/views/site/debuginfo.php
+++ protected/views/site/debuginfo.php
@@ -23,14 +23,16 @@ if (is_object($user)) {
 $commit = preg_replace('/[\s\t].*$/s','',@file_get_contents(@$_SERVER['DOCUMENT_ROOT']."/.git/FETCH_HEAD"));
 ?>
 <div id="debug-info-modal">
-	<ul>
-		<li>Server: <?php echo $hostname?></li>
-		<li>Docroot: <?php echo @$_SERVER['DOCUMENT_ROOT']?></li>
-		<li>Date: <?php echo date('d.m.Y H:i:s')?></li>
-		<li>Commit: <?php echo $commit?></li>
-		<li>User agent: <?php echo @$_SERVER['HTTP_USER_AGENT']?></li>
-		<li>Client IP: <?php echo @$_SERVER['REMOTE_ADDR']?></li>
-		<li>Username: <?php echo $username?></li>
-		<li>Firm: <?php echo $firm?></li>
-	</ul>
+	<code>
+		<strong>This information is provided to assist the helpdesk in diagnosing any problems</strong><br />
+		Served, with love, by: <?php echo $hostname?><br />
+		Docroot: <?php echo @$_SERVER['DOCUMENT_ROOT']?><br />
+		Date: <?php echo date('d.m.Y H:i:s')?><br />
+		Commit: <?php echo $commit?><br />
+		User agent: <?php echo wordwrap(@$_SERVER['HTTP_USER_AGENT'], 80, "<br />\n");?>
+		Client IP: <?php echo @$_SERVER['REMOTE_ADDR']?><br />
+		Username: <?php echo $username?><br />
+		Firm: <?php echo $firm?><br />
+		
+	</code>
 </div>
