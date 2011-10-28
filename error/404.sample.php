e0932d33956eba06dbd41352c235eb981b19e009
Mark Wadham <mark.wadham@openeyes.org.uk>
2011-10-28 12:32:13 +0100

    changed error pages to be gitignored

--- error/404.sample.php
+++ error/404.sample.php
@@ -1,4 +1,18 @@
-<?php header('HTTP/1.1 500 Internal Server Error')?>
+<?php header('HTTP/1.1 404 Page Not Found');
+
+class Config {
+	function __construct($data) {
+		$this->config = $data;
+	}
+}
+
+if (file_exists("../protected/config/params.php")) {
+	$t = new Config(require("../protected/config/params.php"));
+
+	$helpdesk_phone = $t->config['params']['helpdesk_phone'];
+	$helpdesk_email = $t->config['params']['helpdesk_email'];
+}
+?>
 <!doctype html> 
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]--> 
 <!--[if IE 7]>		<html class="no-js ie7 oldie" lang="en"> <![endif]--> 
@@ -37,8 +51,8 @@
 			<p>
 				Support Options
 				<ul>
-					<li>Immediate support (8:00am to 8:00pm) - Phone <?php echo Yii::app()->params['helpdesk_phone']?></li>
-					<li>Less urgent issues email <a href="mailto:<?php echo Yii::app()->params['helpdesk_email']?>"><?php echo Yii::app()->params['helpdesk_email']?></a></li>
+					<li>Immediate support (8:00am to 8:00pm) - Phone <?php echo @$helpdesk_phone?></li>
+					<li>Less urgent issues email <a href="mailto:<?php echo @$helpdesk_email?>"><?php echo @$helpdesk_email?></a></li>
 					<li>Log a support call or question at ???????????</li>
 				</ul>
 			</p>
@@ -50,7 +64,7 @@
 	<!--#container --> 
 	
 	<div id="footer"> 
-		<h6>&copy; Copyright OpenEyes Foundation 2011 &nbsp;&nbsp;|&nbsp;&nbsp; Terms of Use &nbsp;&nbsp;|&nbsp;&nbsp; Legals &nbsp;&nbsp;|&nbsp;&nbsp; served by HOSTNAME</h6> 
+		<h6>&copy; Copyright OpenEyes Foundation 2011 &nbsp;&nbsp;|&nbsp;&nbsp; Terms of Use &nbsp;&nbsp;|&nbsp;&nbsp; Legals &nbsp;&nbsp;|&nbsp;&nbsp; served by <?php echo trim(`hostname`)?></h6> 
 	</div> <!-- #footer --> 
 </body> 
 </html> 
