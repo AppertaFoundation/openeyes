8b07b66b18341b42a2b6cd33f0e7bb184ee1436d
Mark Wadham <mark.wadham@openeyes.org.uk>
2011-12-22 17:26:10 +0000

    modified GpService to only pull in GPs for patients in the database as opposed to /all/ GPs. made the script also associate the patient with a gp via the new gp_id field on the patient table. made it send a small report of any errors that occurred when it runs to the alerts email address.

--- protected/services/GpService.php
+++ protected/services/GpService.php
@@ -8,7 +8,7 @@ OpenEyes is free software: you can redistribute it and/or modify it under the te
 OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
 _____________________________________________________________________________
-http://www.openeyes.org.uk   info@openeyes.org.uk
+http://www.openeyes.org.uk	 info@openeyes.org.uk
 --
 */
 
@@ -27,81 +27,109 @@ class GpService
 			exit("Unable to find GP contact type.\n");
 		}
 
-// For testing - getting all the GPs is very slow.
-//		$results = PAS_Gp::model()->findAll(array('limit' => 100));
-		$results = PAS_Gp::model()->findAll();
+		// collect patient ids with no gp
+		$patient_ids = array();
+		$n=0;
+		foreach (Yii::app()->db->createCommand()->select()->from('patient')->queryAll() as $patient) {
+			$patient_ids[$n][] = $patient['id'];
 
-		if (!empty($results)) {
-			foreach ($results as $pasGp) {
-				$gp = Gp::model()->find('obj_prof = ?', array($pasGp->OBJ_PROF));
+			// pas parameter limit is 1000
+			if (count($patient_ids[$n]) == 1000) $n++;
+		}
 
-				if (isset($gp)) {
-					// Update existing GP
-					$contact = Contact::model()->findByPk($gp->contact_id);
+		$errors = array();
 
-					if (isset($contact)) {
-						$this->populateContact($contact, $pasGp);
+		// get last gp date from past table for all these patients
+		//select distinct rm_patient_no as patient_id, max(date_from) as latestGP from silver.patient_gps where rm_patient_no in (16218,16219) group by rm_patient_no order by rm_patient_no
+		foreach ($patient_ids as $ids) {
+			foreach (Yii::app()->db_pas->createCommand("select distinct rm_patient_no as patient_id, max(date_from) as latestGP from silver.patient_gps where rm_patient_no in (".implode(',',$ids).") group by rm_patient_no order by rm_patient_no")->queryAll() as $latestGP) {
+				$gp = Yii::app()->db_pas->createCommand("select * from silver.patient_gps where rm_patient_no = '{$latestGP['PATIENT_ID']}' and date_from = '{$latestGP['LATESTGP']}'")->queryRow();
 
-						$address = Address::model()->findByPk($contact->address_id);
+				if ($pasGp = Yii::app()->db_pas->createCommand("select * from silver.ENV040_PROFDETS where obj_prof = '{$gp['GP_ID']}'")->queryRow()) {
+					if ($gp = Gp::model()->find('obj_prof = ?', array($pasGp['OBJ_PROF']))) {
+						// Update existing GP
+						if ($contact = Contact::model()->findByPk($gp->contact_id)) {
+							$this->populateContact($contact, $pasGp);
 
-						if (isset($address)) {
-							$this->populateAddress($address, $pasGp);
+							if ($address = Address::model()->findByPk($contact->address_id)) {
+								$this->populateAddress($address, $pasGp);
 
-							$this->populateGp($gp, $pasGp);
+								$this->populateGp($gp, $pasGp);
+							} else {
+								$errors[] = "No address for gp contact " . $contact->id;
+								echo "x";
+							}
 						} else {
-							echo "No address for gp contact " . $contact->id . "\n";
+							$errors[] = "Unable to update existing gp contact " . $pasGp['OBJ_PROF'];
+							echo "x";
 						}
 					} else {
-						echo "Unable to update existing gp contact " . $pasGp->OBJ_PROF . "\n";
+						$address = new Address;
+
+						$this->populateAddress($address, $pasGp);
+
+						$contact = new Contact;
+
+						$contact->consultant = 0;
+						$contact->address_id = $address->id;
+
+						$this->populateContact($contact, $pasGp);
+
+						$gp = new Gp;
+
+						$gp->contact_id = $contact->id;
+
+						$this->populateGp($gp, $pasGp);
+					}
+
+					// Update patient
+					if ($patient = Patient::model()->findByPk($latestGP['PATIENT_ID'])) {
+						$patient->gp_id = $gp->id;
+						$patient->save();
+						echo ".";
+					} else {
+						$errors[] = "Unable to find patient {$latestGP['PATIENT_ID']}";
+						echo "x";
 					}
 				} else {
-                                        $address = new Address;
-
-                                        $this->populateAddress($address, $pasGp);
-			
-					$contact = new Contact;
-
-					$contact->consultant = 0;
-					$contact->address_id = $address->id;
-
-					$this->populateContact($contact, $pasGp);
-
-					$gp = new Gp;
-
-					$gp->contact_id = $contact->id;
-
-					$this->populateGp($gp, $pasGp);
+					$errors[] = "Unable to find GP for patient id={$latestGP['PATIENT_ID']}, GP_ID={$gp['GP_ID']}";
+					echo "x";
 				}
 			}
+		}
+		echo "\n";
+
+		if (!empty($errors)) {
+			mail(Yii::app()->params['alerts_email'],"FetchGP errors",implode("\n",$errors));
 		}
 	}
 
 	public function populateContact($contact, $pasGp)
 	{
-		$contact->title = $pasGp->TITLE;
-		$contact->first_name = $pasGp->FN1 . ' ' . $pasGp->FN2;
-		$contact->last_name = $pasGp->SN;
-		$contact->primary_phone = $pasGp->TEL_1;
+		$contact->title = $pasGp['TITLE'];
+		$contact->first_name = $pasGp['FN1'] . ' ' . $pasGp['FN2'];
+		$contact->last_name = $pasGp['SN'];
+		$contact->primary_phone = $pasGp['TEL_1'];
 		$contact->save();
 	}
 
 	public function populateAddress($address, $pasGp)
 	{
-		$address->address1 = $pasGp->ADD_NAM . ' ' . $pasGp->ADD_NUM . ' ' . $pasGp->ADD_ST;
-		$address->address2 = $pasGp->ADD_TWN . ' ' . $pasGp->ADD_DIS;
-		$address->city = $pasGp->ADD_CTY;
-		$address->postcode = $pasGp->PC;
+		$address->address1 = $pasGp['ADD_NAM'] . ' ' . $pasGp['ADD_NUM'] . ' ' . $pasGp['ADD_ST'];
+		$address->address2 = $pasGp['ADD_TWN'] . ' ' . $pasGp['ADD_DIS'];
+		$address->city = $pasGp['ADD_CTY'];
+		$address->postcode = $pasGp['PC'];
 		$address->country_id = 1;
 
 		if (!$address->save()) {
-			exit('failed to save address for ' . $pasGp->OBJ_PROF);
+			exit('failed to save address for ' . $pasGp['OBJ_PROF']);
 		}
 	}
 
 	public function populateGp($gp, $pasGp)
 	{
-		$gp->obj_prof = $pasGp->OBJ_PROF;
-		$gp->nat_id = $pasGp->NAT_ID;
+		$gp->obj_prof = $pasGp['OBJ_PROF'];
+		$gp->nat_id = $pasGp['NAT_ID'];
 		$gp->save();
 	}
 }
