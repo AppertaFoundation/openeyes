0e9fc9ab3395e47571d7683d211f05974c75a3bd
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-01-24 17:18:09 +0000

    bugfix for when there's no data

--- protected/views/transport/_list.php
+++ protected/views/transport/_list.php
@@ -23,59 +23,61 @@ if (empty($bookings)) { ?>
 			</tr>
 <?php
 	$i = 0;
-	foreach ($bookings as $id => $booking) {
-?>
+	if ($bookings) {
+		foreach ($bookings as $id => $booking) {
+	?>
 
-<?php
-	if ($booking['transport'] == null) {
-		if (strtotime($booking['session_date']) <= (strtotime(date('Y-m-d')) + 86400)) {
-			$tablecolour = "Red";
+	<?php
+		if ($booking['transport'] == null) {
+			if (strtotime($booking['session_date']) <= (strtotime(date('Y-m-d')) + 86400)) {
+				$tablecolour = "Red";
+			} else {
+				$tablecolour = "Green";
+			}
 		} else {
-			$tablecolour = "Green";
+			$tablecolour = "Grey";
 		}
-	} else {
-		$tablecolour = "Grey";
-	}
-?>
-    <tr class="waitinglist<?php echo $tablecolour ?>">
-		<?php
-?>
-	<td style="width: 53px;"><?php echo $booking['hos_num'] ?></td>
-	<td class="patient">
-		<?php echo CHtml::link(trim("<b>" . $booking['last_name']) . '</b>, ' . $booking['first_name'], '/patient/episodes/' . $booking['pid'] . '/event/' . $booking['evid'])?>
-	</td>
-	<td style="width: 83px;"><?php echo date('j-M-Y',strtotime($booking['session_date']))?></td>
-	<td style="width: 73px;"><?php echo $booking['session_time']?></td>
-	<td style="width: 95px;"><?php echo $booking['location']?></td>
-	<td style="width: 53px;"><?php echo $booking['method']?></td>
-	<td style="width: 43px;"><?php echo $booking['firm'] ?></td>
-	<td style="width: 53px;"><?php echo $booking['specialty']?></td>
-	<td style="width: 80px;"><?php echo $booking['decision_date'] ?></td>
-	<td><?php echo ($booking['urgent']) ? 'Urgent' : 'Routine' ?></td>
-	<td style="width: 20px;">
-		<?php if ($booking['method'] == 'Cancelled') {?>
-			<input type="checkbox" name="cancelled[]" value="<?php echo $booking['checkid']?>" />
-		<?php }else{?>
-			<input type="checkbox" name="booked[]" value="<?php echo $booking['checkid']?>" />
-		<?php }?>
-	</td>
-</tr>
-
-<?php
-		$i++;
-	}
-
-	if ($i == 0) { ?>
-	<tr>
-		<td colspan="7" style="border: none; padding-top: 10px;">
-			There is no relevant activity for the selected date.
+	?>
+			<tr class="waitinglist<?php echo $tablecolour ?>">
+			<?php
+	?>
+		<td style="width: 53px;"><?php echo $booking['hos_num'] ?></td>
+		<td class="patient">
+			<?php echo CHtml::link(trim("<b>" . $booking['last_name']) . '</b>, ' . $booking['first_name'], '/patient/episodes/' . $booking['pid'] . '/event/' . $booking['evid'])?>
+		</td>
+		<td style="width: 83px;"><?php echo date('j-M-Y',strtotime($booking['session_date']))?></td>
+		<td style="width: 73px;"><?php echo $booking['session_time']?></td>
+		<td style="width: 95px;"><?php echo $booking['location']?></td>
+		<td style="width: 53px;"><?php echo $booking['method']?></td>
+		<td style="width: 43px;"><?php echo $booking['firm'] ?></td>
+		<td style="width: 53px;"><?php echo $booking['specialty']?></td>
+		<td style="width: 80px;"><?php echo $booking['decision_date'] ?></td>
+		<td><?php echo ($booking['urgent']) ? 'Urgent' : 'Routine' ?></td>
+		<td style="width: 20px;">
+			<?php if ($booking['method'] == 'Cancelled') {?>
+				<input type="checkbox" name="cancelled[]" value="<?php echo $booking['checkid']?>" />
+			<?php }else{?>
+				<input type="checkbox" name="booked[]" value="<?php echo $booking['checkid']?>" />
+			<?php }?>
 		</td>
 	</tr>
-	<?php }
-?>
-</tbody>
-</table>
-<?php
+
+	<?php
+			$i++;
+		}
+
+		if ($i == 0) { ?>
+		<tr>
+			<td colspan="7" style="border: none; padding-top: 10px;">
+				There is no relevant activity for the selected date.
+			</td>
+		</tr>
+		<?php }
+	?>
+	</tbody>
+	</table>
+	<?php
+	}
 }
 ?>
 
