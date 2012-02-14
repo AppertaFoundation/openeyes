a903d4f6b860b4e4e34aa416cb83be4d8ec8559e
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-02-14 17:01:57 +0000

    related: OE-827
    prevented multiple clicks on the 'reschedule later' page.

--- protected/views/booking/_reschedule_later.php
+++ protected/views/booking/_reschedule_later.php
@@ -50,15 +50,25 @@ echo '<textarea name="cancellation_comment" rows=6 cols=40></textarea>';
 echo '<div style="height: 0.4em;"></div>'?>
 <div class="clear"></div>
 <button type="submit" class="classy red venti"><span class="button-span button-span-red">Confirm reschedule later</span></button>
+<img src="/img/ajax-loader.gif" alt="loading..." style="display: none;" id="loader" />
 <?php
 echo CHtml::endForm(); ?>
 </div>
 </div>
 <script type="text/javascript">
 	$('#cancelForm button[type="submit"]').click(function () {
-		if ('' == $('#cancellation_reason option:selected').val()) {
-			$('div.alertBox ul li').html('Please select a cancellation reason');
-			$('div.alertBox').show();
+		if ($(this).hasClass('red')) {
+			if ('' == $('#cancellation_reason option:selected').val()) {
+				$('div.alertBox ul li').html('Please select a cancellation reason');
+				$('div.alertBox').show();
+				return false;
+			}
+
+			$(this).removeClass('red').addClass('inactive');
+			$(this).children('span').removeClass('button-span-red').addClass('button-span-inactive');
+			$('#loader').show();
+
+		} else {
 			return false;
 		}
 	});
