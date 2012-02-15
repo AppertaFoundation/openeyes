d026b81404aeee6d9b187a13459c08a8228160fd
Jamie Neil <jamie.neil@openeyes.org.uk>
2012-02-15 16:32:03 +0000

    simple db benchmark

---
+++ protected/controllers/MicController.php
@@ -0,0 +1,41 @@
+<?php
+class MicController extends BaseController {
+
+	public $layout='column2';
+
+	protected function beforeAction($action) {
+		if (!Yii::app()->user->checkAccess('admin')) {
+			throw new CHttpException(403, 'You are not authorised to perform this action.');
+		}
+		return parent::beforeAction($action);
+	}
+	
+	public function actionIndex() {
+		
+		$results = array();
+		
+		// Find
+		$start = microtime(true);
+		for($i = 1; $i <= 100; $i++) {
+			$patients = Patient::model()->findAll(array(
+				'limit' => 100
+			));
+		}
+		$results['Find 100 patients 100 times'] = microtime(true) - $start;
+		
+		// Save
+		$patients = Patient::model()->findAll(array(
+			'limit' => 500
+		));
+		$count = count($patients);
+		$start = microtime(true);
+		foreach($patients as $patient) {
+			$patient->save();
+		}
+		$results["Save $count patients"] = microtime(true) - $start;
+		
+		$this->render('index', array(
+			'results' => $results,
+		));
+	}
+}
---
+++ protected/views/mic/index.php
@@ -0,0 +1,6 @@
+<h1>Performance Test</h1>
+<ul>
+<?php foreach($results as $key => $result) { ?>
+<li><?php echo $key ?>: <?php echo $result ?>s</li> 
+<?php } ?>
+</ul>
