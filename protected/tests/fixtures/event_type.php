1dd3660140747491bf1adec7d6df7395b47e3a8e
chris raettig <chris@raettig.org>
2012-02-23 07:24:16 +0000

    migration to replace the 'diagnosis' event type with an 'operationnote' event type. diagnosis isn't an event level concept and so doesn't belong in there. operationnote is the one event type from the upcoming cataract clump which doesn't have an existing event type.
    removed the first_in_episode_possible column from event_type while i was in there, as we're removing the concept of 'first in episode'
    and replacing it with the more flexible concept of statuses.
    
    a few other changes to support the removal of that first_in_episode_possible column.

--- protected/tests/fixtures/event_type.php
+++ protected/tests/fixtures/event_type.php
@@ -21,94 +21,71 @@
 return array(
 	'eventType1' => array(
 		'name' => 'examination',
-		'first_in_episode_possible' => 1,
 	),
 	'eventType2' => array(
 		'name' => 'refraction',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType3' => array(
 		'name' => 'orthoptics',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType4' => array(
 		'name' => 'diagnosis',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType5' => array(
 		'name' => 'ffa',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType6' => array(
 		'name' => 'icg',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType7' => array(
 		'name' => 'oct',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType8' => array(
 		'name' => 'field',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType9' => array(
 		'name' => 'ultrasound',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType10' => array(
 		'name' => 'xray',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType11' => array(
 		'name' => 'ctscan',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType12' => array(
 		'name' => 'mriscan',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType13' => array(
 		'name' => 'bloodtest',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType14' => array(
 		'name' => 'prescription',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType15' => array(
 		'name' => 'preassess',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType16' => array(
 		'name' => 'anaesth',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType17' => array(
 		'name' => 'amdapplication',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType18' => array(
 		'name' => 'amdinjection',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType19' => array(
 		'name' => 'injection',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType20' => array(
 		'name' => 'laser',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType21' => array(
 		'name' => 'letterin',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType22' => array(
 		'name' => 'letterout',
-		'first_in_episode_possible' => 0,
 	),
 	'eventType23' => array(
 		'name' => 'cvi',
-		'first_in_episode_possible' => 0,
 	),
 );
