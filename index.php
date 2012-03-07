5abd1da5441f6f5bfb9fd248f3fabec2024676ec
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-03-07 14:19:17 +0000

    re-added the functionality back into index.php that looks for environment = 'dev' in local/common.php and sets YII_DEBUG to true if it's there.

--- index.php
+++ index.php
@@ -4,8 +4,18 @@
 $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
-// remove the following lines when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
+$local_config=dirname(__FILE__).'/protected/config/local/common.php';
+
+foreach (@file($local_config) as $line) {
+	if (preg_match('/^[\s\t]+\'environment\'[\s\t]*=>[\s\t]*\'([a-z]+)\'/',$line,$m)) {
+		$environment = $m[1];
+	}
+}
+
+if ($environment == 'dev') {
+	define('YII_DEBUG',true);
+}
+
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
