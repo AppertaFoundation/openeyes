eef512576b7f00931d37865e66fba687883c7ae4
chris raettig <chris@raettig.org>
2012-03-12 14:18:12 +0000

    test

--- protected/modules/OphTrOperationnote/views/default/create.php
+++ protected/modules/OphTrOperationnote/views/default/create.php
@@ -5,12 +5,12 @@ $this->breadcrumbs=array(
 ?>
 <h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
 
-This is the default create view for the <?php echo $this->module->name ?> event type module.
 <p>
-This is the view content for action "<?php echo $this->action->id; ?>".
-The action belongs to the controller "<?php echo get_class($this); ?>"
-in the "<?php echo $this->module->id; ?>" module.
+This is the default <?php echo $this->action->id; ?> view for the <?php echo get_class($this); ?> controller of the <?php echo $this->module->name ?> event type module.
 </p>
 <p>
 You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
+</p>
+<p>
+We need to take a position on AJAX before proceeding much further.  We need to know whether this file needs to handle loading in of the element types, like 'patient/views/episodes.php' or rendering the whole page.  in which case rather than pushing in the element types it ought ot render the page based on a parent template then call an inherited function for grabbing in the element types and renderpartialing them.
 </p>
