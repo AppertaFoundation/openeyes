d2bb9afb66f7bbe0fde1d564020791d13098b40f
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-03-13 10:54:50 +0000

    Moved the javascript that detects firm changes into the generic script.js rather than the two views. Made the diary 'forget' the firm-id and specialty-id from the session when changing firm (requested by Ratna).

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -47,6 +47,7 @@
 	<script type="text/javascript" src="/js/jquery.printElement.min.js"></script>
 	<script type="text/javascript" src="/js/print.js"></script>
 	<script type="text/javascript" src="/js/buttons.js"></script>
+	<script type="text/javascript" src="/js/script.js"></script>
 	<?php if (Yii::app()->params['google_analytics_account']) {?>
 		<script type="text/javascript">
 
@@ -117,21 +118,6 @@
 
 	<?php echo $this->renderPartial('/base/_footer',array())?>
  
-	<script type="text/javascript">
-		$('select[id=selected_firm_id]').die('change').live('change', function() {
-			var firmId = $('select[id=selected_firm_id]').val();
-			$.ajax({
-				type: 'post',
-				url: '<?php echo Yii::app()->createUrl('site'); ?>',
-				data: {'selected_firm_id': firmId },
-				success: function(data) {
-					console.log(data);
-					window.location.href = '<?php echo Yii::app()->createUrl('site'); ?>';
-				}
-			});
-		});
-	</script>
-
 	<?php if (Yii::app()->user->checkAccess('admin')) {?>
 		<div class="h1-watermark-admin"><?php echo Yii::app()->params['watermark_admin']?></div>
 	<?php } else if (Yii::app()->params['watermark']) {?>
