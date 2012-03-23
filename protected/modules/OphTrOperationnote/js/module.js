e73e3a8f8889970a08e093595943b91640dc3558
Mark Wadham <m@rkw.me>
2012-03-23 19:38:12 +0000

    made inserting the vitrectomy element work on the update form.

--- protected/modules/OphTrOperationnote/js/module.js
+++ protected/modules/OphTrOperationnote/js/module.js
@@ -2,7 +2,7 @@
 function callbackAddProcedure(procedure_id) {
 	$.ajax({
 		'type': 'GET',
-		'url': 'loadElementByProcedure?procedure_id='+procedure_id,
+		'url': '/OphTrOperationnote/Default/loadElementByProcedure?procedure_id='+procedure_id,
 		'success': function(html) {
 			if (html.length >0) {
 				$('div.elements').append(html);
@@ -14,7 +14,7 @@ function callbackAddProcedure(procedure_id) {
 function callbackRemoveProcedure(procedure_id) {
 	$.ajax({
 		'type': 'GET',
-		'url': 'getElementsByProcedure?procedure_id='+procedure_id,
+		'url': '/OphTrOperationnote/Default/getElementsByProcedure?procedure_id='+procedure_id,
 		'dataType': 'json',
 		'success': function(data) {
 			$.each(data, function(key, val) {
