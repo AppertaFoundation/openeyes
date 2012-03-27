41cef11ac236c7c666b7e734c3beec9004b09cfb
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-03-27 13:49:36 +0100

    removed spurious label.

--- protected/widgets/views/TextField.php
+++ protected/widgets/views/TextField.php
@@ -2,6 +2,5 @@
 	<div class="label"><?php echo CHtml::encode($element->getAttributeLabel($field)); ?>:</div>
 	<div class="data">
 		<?php echo CHtml::textField($name, $value, $htmlOptions)?>
-		<label for="<?php echo get_class($element)?>[<?php echo $field?>]"><?php echo $value?></label>
 	</div>
 </div>
