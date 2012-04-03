e5a85f4f40b2ad80e99b895a69c6d379eed78c39
Jamie Neil <jamie.neil@gmail.com>
2012-04-03 21:50:29 +0100

    Unfinished

--- protected/extensions/mehpas/components/PasService.php
+++ protected/extensions/mehpas/components/PasService.php
@@ -41,7 +41,7 @@ class PasService {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Check to see if a GP ID (obj_prof) is on our block list
 	 * @param string $gp_id
@@ -64,7 +64,7 @@ class PasService {
 		if($pas_gp = $assignment->external) {
 			Yii::log('Found GP in PAS obj_prof:'.$pas_gp->OBJ_PROF, 'trace');
 			$gp->nat_id = $pas_gp->NAT_ID;
-				
+
 			// Contact
 			if(!$contact = $gp->contact) {
 				$contact = new Contact();
@@ -73,7 +73,7 @@ class PasService {
 			$contact->last_name = $pas_gp->SN;
 			$contact->title = $pas_gp->TITLE;
 			$contact->primary_phone = $pas_gp->TEL_1;
-				
+
 			// Address
 			if(!$address = $contact->address) {
 				$address = new Address();
@@ -85,7 +85,7 @@ class PasService {
 			$address->county = $pas_gp->ADD_CTY;
 			$address->postcode = $pas_gp->PC;
 			$address->country_id = 1;
-				
+
 			// Save
 			$contact->save();
 			$address->parent_id = $contact->id;
@@ -129,14 +129,14 @@ class PasService {
 			if($nhs_number = $pas_patient->nhs_number) {
 				$patient_attrs['nhs_num'] = $nhs_number->NUMBER_ID;
 			}
-			
+
 			// Get primary phone from patient's main address
 			if($pas_patient->address) {
 				$patient_attrs['primary_phone'] = $pas_patient->address->TEL_NO;
 			}
-			
+
 			$patient->attributes = $patient_attrs;
-				
+
 			// Get latest GP mapping from PAS
 			$pas_patient_gp = $pas_patient->PatientGp;
 			if($pas_patient_gp) {
@@ -326,30 +326,9 @@ class PasService {
 
 		foreach ($results as $result) {
 
-			$hos_num = $result['NUM_ID_TYPE'] . $result['NUMBER_ID'];
-				
 			// See if the patient is in openeyes, if not then fetch from PAS
-			if($assignment = PasAssignment::model()->findByExternal('PAS_Patient', $result['RM_PATIENT_NO'])) {
-				// Patient is in OpenEyes and has an existing assignment
-				$patient = $assignment->internal;
-				if($assignment->isStale()) {
-					$this->updatePatientFromPas($patient, $assignment);
-				}
-			} else if($patient = Patient::model()->findByAttributes(array('hos_num' => $hos_num))) {
-				// Patient is in OpenEyes, but doesn't have an assignment
-				// FIXME: Ideally this step should not be necessary, and could be removed if we prefill the assignment table when the module is setup
-				$assignment = new PasAssignment();
-				$assignment->external_id = $result['RM_PATIENT_NO'];
-				$assignment->external_type = 'PAS_Patient';
-				$this->updatePatientFromPas($patient, $assignment);
-			} else {
-				// Patient is not in OpenEyes
-				$patient = new Patient();
-				$assignment = new PasAssignment();
-				$assignment->external_id = $result['RM_PATIENT_NO'];
-				$assignment->external_type = 'PAS_Patient';
-				$this->updatePatientFromPas($patient, $assignment);
-			}
+			$patient_assignment = $this->findPatientAssignment($result['RM_PATIENT_NO'], $result['NUM_ID_TYPE'] . $result['NUMBER_ID']);
+			$patient = $patient_assignment->internal;
 
 			// Check that patient has an address
 			if($patient->address) {
@@ -391,7 +370,7 @@ class PasService {
 				break;
 		}
 
-		// collect all the patients we just created
+		// Collect all the patients we just created
 		$criteria = new CDbCriteria;
 		$criteria->addInCondition('id', $ids);
 		$criteria->order = "$sort_by $sort_dir";
@@ -402,6 +381,37 @@ class PasService {
 		}
 
 		return $criteria;
+	}
+
+	/**
+	 * Try to find patient assignment in OpenEyes and if necessary create a new one and populate it from PAS
+	 * @param string $rm_patient_no
+	 * @param string $hos_num
+	 * @return PasAssignment
+	 */
+	protected function findPatientAssignment($rm_patient_no, $hos_num) {
+		if($assignment = PasAssignment::model()->findByExternal('PAS_Patient', $rm_patient_no)) {
+			// Patient is in OpenEyes and has an existing assignment
+			$patient = $assignment->internal;
+			if($assignment->isStale()) {
+				$this->updatePatientFromPas($patient, $assignment);
+			}
+		} else if($patient = Patient::model()->findByAttributes(array('hos_num' => $hos_num))) {
+			// Patient is in OpenEyes, but doesn't have an assignment
+			// FIXME: Ideally this step should not be necessary, and could be removed if we prefill the assignment table when the module is setup
+			$assignment = new PasAssignment();
+			$assignment->external_id = $rm_patient_no;
+			$assignment->external_type = 'PAS_Patient';
+			$this->updatePatientFromPas($patient, $assignment);
+		} else {
+			// Patient is not in OpenEyes
+			$patient = new Patient();
+			$assignment = new PasAssignment();
+			$assignment->external_id = $rm_patient_no;
+			$assignment->external_type = 'PAS_Patient';
+			$this->updatePatientFromPas($patient, $assignment);
+		}
+		return $assignment;
 	}
 
 	/**
@@ -554,11 +564,61 @@ class PasService {
 	}
 
 	/**
-	 * Fetch referral from PAS
+	 * Find and associate a referral to an episode if it doesn't already have one
 	 * @param Episode $episode
 	 */
-	public function fetchReferral($episode) {
-		Yii::log('Fetching referral from PAS', 'trace');
+	public function matchReferralToEpisode($episode) {
+		Yii::log("Trying to match referral to episode_id $episode->id", 'trace');
+		$patient_id = $episode->patient_id;
+		$ssa_id = $episode->firm->service_specialty_assignment_id;
+
+		// First try to match patient _and_ service specialty
+		$referrals = Referral::model()->findAll(array(
+				'condition' => 'patient_id = :patient_id AND service_specialty_assignment_id = :ssa_id AND closed = 0',
+				'order' => 'refno DESC',
+				'params' => array(
+						':patient_id' => $patient_id,
+						':ssa_id' => $ssa_id
+				),
+		));
+		if(count($referrals) == 1) {
+			// Found one matching referral, so we assume this is the right one
+			$referral = $referrals[0];
+		} else if(count($referrals) > 1) {
+			// Found more than one candidate, cannot continue
+			return false;
+		} else {
+			// No referrals found
+			$referral = false;
+		}
+
+		if(!$referral) {
+			// Fall back to trying a looser match on just the patient
+			$referrals = Referral::model()->findAll(array(
+					'condition' => 'patient_id = :patient_id AND closed = 0',
+					'order' => 'refno DESC',
+					'params' => array(
+							':patient_id' => $patient_id,
+							':ssa_id' => $ssa_id
+					),
+			));
+			if(count($referrals) == 1) {
+				// Found one matching referral, so we assume this is the right one
+				Yii::log('One referral found on loose match');
+				$referral = $referrals[0];
+			} else if(count($referrals) > 1) {
+				// Found more than one candidate, cannot continue
+				Yii::log('More than one referral found on loose match');
+				return false;
+			} else {
+				// No referrals found
+				Yii::log('No referrals found on loose match');
+				return false;
+			}
+
+		}
+
+
 		$assignment = PasAssignment::model()->findByInternal('Patient', $episode->patient_id);
 		if(!$assignment) {
 			throw new CException("Patient has no PAS assignment, cannot fetch referral");
@@ -572,7 +632,7 @@ class PasService {
 						':ref_spec' => $ref_spec,
 				),
 		));
-		
+
 		// Only create referral if there is a single matching referral in PAS as otherwise we cannot determine which on matches
 		if($pas_referrals && count($pas_referrals) == 1) {
 			Yii::log("Found referral for patient id $episode->patient_id (rm_patient_no $rm_patient_no)", 'trace');
@@ -585,7 +645,7 @@ class PasService {
 			if (!$referral->save()) {
 				throw new CException("Failed to save referral for patient id $episode->patient_id (rm_patient_no $rm_patient_no), episode $episode->id: " . print_r($referral->getErrors(), true));
 			}
-			
+
 			$rea = new ReferralEpisodeAssignment();
 			$rea->referral_id = $referral->id;
 			$rea->episode_id = $episode->id;
@@ -597,124 +657,148 @@ class PasService {
 		} else {
 			Yii::log("No referrals found in PAS for patient id $episode->patient_id (rm_patient_no $rm_patient_no)",'trace');
 		}
-		
+
 	}
 
 	/**
-	 * Fetch new referrals from PAS
+	 * Fetch new referrals from PAS and link them to patients
 	 */
 	public function fetchNewReferrals() {
 		Yii::log('Fetching new referrals from PAS', 'trace');
+
+		// Find the last referral that is not linked to an episode. This is required because referrals fetched
+		// on demand may otherwise leave holes (and these will always be linked to an episode).
 		$last_refno = Yii::app()->db->createCommand()
-			->select('MAX(refno) AS mrn')
-			->from('referral')
-			->queryScalar();
-		if (!$last_refno) {
-			echo "There are no referrals in the DB. This would cause every referral from PAS to be fetched.\n";
+		->select('MAX(refno) AS mrn')
+		->from('referral')
+		->leftJoin('referral_episode_assignment', 'referral_episode_assignment.referral_id = referral.id')
+		->where('episode_id IS NULL')
+		->queryScalar();
+
+		// Get new referrals
+		$pas_referrals = PAS_Referral::model()->findAll("REFNO > :last_refno AND REF_SPEC <> 'OP'", array(
+				':last_refno' => $last_refno,
+		));
+		if(count($pas_referrals) > 1000) {
+			echo "There are more than 1000 new referrals to fetch, aborting.\n";
 			Yii::app()->end();
 		}
-	
-		$pas_referrals = PAS_Referral::model()->findAll(array(
-				'condition' => "REFNO > :last_refno AND REF_SPEC <> 'OP'",
-				'params' => array(
-						':last_refno' => $last_refno,
-				),
-		));
+
 		$errors = '';
-	
-		// Put all new PAS referrals in OE
 		foreach ($pas_referrals as $pas_referral) {
-			$specialty = Specialty::model()->find(array(
-					'condition' => 'ref_spec = :ref_spec',
-					'params' => array(
-							':ref_spec' => $pas_referral->REF_SPEC,
-					)
-			));
-	
-			if ($specialty) {
-				$ssa = ServiceSpecialtyAssignment::model()->find(array(
-						'condition' => 'specialty_id = :specialty_id',
-						'params' => array(
-								':specialty_id' => $specialty->id,
-						)
-				));
-				$referral = new Referral;
-				$referral->service_specialty_assignment_id = $ssa->id;
-				$referral->refno = $pas_referral->REFNO;
-	
-				if ($pas_referral->X_CN) {
-					$external_id = $pas_referral->X_CN;
-				} else {
-					/*
-					 * Due to a dodgy migration by some third party company we don't always have the X_CN field containing
-					 * the patient number, so this is an alternative way to obtain it.
-					 * TODO: We should probably do something similar for the referral event handler
-					 */
-					$sql = "select distinct b.X_CN from SILVER.OUT040_REFDETS a left join SILVER.OUT031_OUTAPPT b on a.REFNO = b.REFNO and a.REF_SPEC = b.CLI_SPEC where a.REFNO = {$pas_referral->REFNO}";
-					$connection = Yii::app()->db_pas;
-					$command = $connection->createCommand($sql);
-					$extended_referrals = $command->queryAll();
-					if (count($extended_referrals) && $patient_id = $extended_referrals[0]['X_CN']) {
-						$external_id = $patient_id;
-					} else {
-						$errors .= "Unable to save referral refno $referral->refno: due to missing X_CN, and unable to infer from SILVER.OUT031_OUTAPPT too.";
-						continue;
-					}
-				}
-				// TODO: Check/pull in patient before getting referral
-				die;
-				$assignment = PasAssignment::model()->findByExternal('PAS_Patient', $external_id);
-				$referral->patient_id = $assignment->internal_id;
-	
-				if (!empty($pas_referral->DT_CLOSE)) {
-					$referral->closed = 1;
-				}
-	
-				if ($referral->save()) {
-					//echo 'Added referral refo ' . $referral->refno . "\n";
-				} else {
-					$errors .= "Unable to save referral refno $referral->refno: ".print_r($referral->getErrors(),true)."\n";
-				}
-			} else {
-				Yii::log('No specialty for ref_spec ' . $pas_referral->REF_SPEC, 'trace');
+
+			// First check that we haven't already imported this referral (on demand)
+			if($referral = Referral::model()->findByAttributes(array('refno' => $pas_referral->REFNO))) {
+				Yii::log("Imported referral already exists for REFNO $pas_referral->REFNO, skipping", 'trace');
+				continue;
 			}
+
+			// Find matching specialty
+			$specialty = Specialty::model()->find('ref_spec = :ref_spec', array(
+					':ref_spec' => $pas_referral->REF_SPEC,
+			));
+			if(!$specialty) {
+				Yii::log("Cannot find specialty for REF_SPEC $pas_referral->REF_SPEC, REFNO $pas_referral->REFNO, skipping", 'trace');
+				continue;
+			}
+			$ssa = ServiceSpecialtyAssignment::model()->find('specialty_id = :specialty_id', array(
+					':specialty_id' => $specialty->id,
+			));
+
+			// Match referral to a Patient
+			if ($pas_referral->X_CN) {
+				$external_id = $pas_referral->X_CN;
+			} else {
+				/**
+				 * Due to a dodgy migration by some third party company we don't always have the X_CN field containing
+				 * the patient number, so this is an alternative way to obtain it.
+				 * TODO: We should probably do something similar for the referral event handler
+				 */
+				$external_id = Yii::app()->db_pas->createCommand()
+				->select('distinct b.X_CN')
+				->from('SILVER.OUT040_REFDETS a')
+				->leftJoin('SILVER.OUT031_OUTAPPT b', 'a.REFNO = b.REFNO and a.REF_SPEC = b.CLI_SPEC')
+				->where("a.REFNO = '$pas_referral->REFNO'")
+				->queryScalar();
+				if(!$external_id) {
+					Yii::log("Cannot find patient (external_id) for REFNO $referral->refno, skipping", 'trace');
+					continue;
+				}
+			}
+
+			// Find patient
+			Yii::log("Looking for patient with external_id $external_id", 'trace');
+			$pas_patient_num = PAS_PatientNumber::model()->find("RM_PATIENT_NO = :rm_patient_no AND REGEXP_LIKE(NUM_ID_TYPE, '[[:digit:]]')", array(
+					':rm_patient_no' => $external_id,
+			));
+			$patient_assignment = $this->findPatientAssignment($external_id, $pas_patient_num->NUM_ID_TYPE . $pas_patient_num->NUMBER_ID);
+
+			// Create new referral
+			Yii::log("Creating new referral REFNO $referral->refno", 'trace');
+			$referral = new Referral();
+			$referral->service_specialty_assignment_id = $ssa->id;
+			$referral->refno = $pas_referral->REFNO;
+			$referral->patient_id = $patient_assignment->internal_id;
+			if(!empty($pas_referral->DT_CLOSE)) {
+				$referral->closed = 1;
+			}
+			$referral->save();
+
 		}
-	
-		//echo "\nREFERRAL CREATION COMPLETE.\n\n";
-	
+
+		$this->matchReferrals();
+
+	}
+
+	/**
+	 * Match referrals to episodes that don't currently have one
+	 */
+	public function matchReferrals() {
 		// Find all the open episodes with no referral
-		$command = Yii::app()->db->createCommand()
-		->select('p.id AS pid, ep.id AS epid, rea.id AS reaid, ssa.id AS ssaid')
-		->from('patient p')
-		->join('episode ep', 'ep.patient_id = p.id')
+		$episodes = Yii::app()->db->createCommand()
+		->select('ep.id AS episode_id, ep.patient_id AS patient_id, rea.id AS rea_id, ssa.id AS ssa_id')
+		->from('episode ep')
 		->join('firm f', 'f.id = ep.firm_id')
 		->join('service_specialty_assignment ssa', 'ssa.id = f.service_specialty_assignment_id')
 		->leftJoin('referral_episode_assignment rea', 'rea.episode_id = ep.id')
-		->where('ep.end_date IS NULL');
-	
-		foreach ($command->queryAll() as $result) {
-			if (empty($result['reaid'])) {
-				$referralId = $this->getReferral($result['pid'], $result['ssaid']);
-	
-				if ($referralId) {
-					$rea = new ReferralEpisodeAssignment;
-					$rea->episode_id = $result['epid'];
-					$rea->referral_id = $referralId;
-					if (!$rea->save()) {
-						$errors .= 'Unable to save referral for epid ' . $result['epid'] . ' and referral ' . $referralId . "\n";
-					} else {
-						//echo 'Assignment rea id ' . $rea->id . ' to patient ' . $result['epid'] . ' and referral ' . $referralId . "\n";
-					}
-				}
+		->where('ep.end_date IS NULL AND rea_id IS NULL')
+		->queryAll();
+
+		foreach($episodes as $episode) {
+			if ($referral = $this->getReferral($episode['patient_id'], $episode['ssa_id'])) {
+				Yii::log("Found referral_id $referral->id for episode_id ".$episode['episode_id'].", creating assignment", 'trace');
+				$rea = new ReferralEpisodeAssignment();
+				$rea->episode_id = $episode['episode_id'];
+				$rea->referral_id = $referral->id;
+				$rea->save();
+			} else {
+				Yii::log("Cannot find a referral for episode_id ".$episode['episode_id'], 'trace');
 			}
 		}
-	
-		if ($errors) {
-			$hostname = trim(`/bin/hostname`);
-			mail(Yii::app()->params['alerts_email'],"[$hostname] Referrals crontab failed",$errors);
-		}
-	
-		//echo "\nREFERRAL ASSIGNMENT COMPLETE.\n\n";
 	}
-	
+
+	protected function getReferral($patient_id, $ssa_id) {
+
+		// Look for open referrals of this service
+		$referral = Referral::model()->find(array(
+				'condition' => 'patient_id = :p AND service_specialty_assignment_id = :s AND closed = 0',
+				'order' => 'refno DESC',
+				'params' => array(':p' => $patientId, ':s' => $ssaId),
+		)
+		);
+		if ($referral) {
+			return $referral;
+		}
+
+		// There are no open referrals for this specialty, try and find open referrals for a different specialty
+		$referral = Referral::model()->find(array(
+				'order' => 'refno DESC',
+				'condition' => 'patient_id = :p AND closed = 0',
+				'params' => array(':p' => $patientId),
+		)
+		);
+
+		return $referral;
+	}
+
 }
