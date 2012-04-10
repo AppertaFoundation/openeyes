08dcaac46df2bbc82e1c4042aa6d71de7c677e6b
Jamie Neil <jamie.neil@openeyes.org.uk>
2012-04-10 14:45:17 +0100

    user doesn't exist in console app

--- protected/extensions/mehpas/components/PasService.php
+++ protected/extensions/mehpas/components/PasService.php
@@ -47,8 +47,8 @@ class PasService {
 	 */
 	public function flashPasDown() {
 		Yii::log('PAS is not available, displayed data may be out of date', 'trace');
-		if($user = Yii::app()->user) {
-			$user->setFlash('warning.pas_unavailable', 'PAS is not available');
+		if(Yii::app()->hasComponent('user')) {
+			Yii::app()->user->setFlash('warning.pas_unavailable', 'PAS is not available');
 		}
 	}
 	
