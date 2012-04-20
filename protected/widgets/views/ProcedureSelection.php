d9fb6c7aca06922106df2424dd25691ef97d0818
Adrian Duke <adrian.duke@openeyes.org.uk>
2012-04-20 14:16:41 +0100

    Fixed issue with remove button on procedure list. resolves OE-1113

--- protected/widgets/views/ProcedureSelection.php
+++ protected/widgets/views/ProcedureSelection.php
@@ -166,11 +166,11 @@ $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
 	}
 
 	$('a.removeProcedure').live('click',function() {
-		var len = $(this).parent().parent().children('div').length;
+		var len = $(this).parent().parent().parent().children('div').length;
 
-		var procedure_id = $(this).parent().children('input:first').val();
-
-		$(this).parent().remove();
+		var procedure_id = $(this).parent().parent().children('input:first').val();
+		
+		$(this).parent().parent().remove();
 
 		<?php if ($durations) {?>
 			updateTotalDuration();
