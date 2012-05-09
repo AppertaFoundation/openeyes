bc03ad309bb8731543dc3e6b7033db8aa5e42b95
Mark Wadham <m@rkw.me>
2012-05-09 11:18:11 +0100

    bugfix: add new event icons were being displayed inconsistently.

--- protected/views/patient/add_new_event.php
+++ protected/views/patient/add_new_event.php
@@ -5,7 +5,7 @@
 		if ($eventType->class_name == 'OphTrOperation') {?>
 			<p><a href="/clinical/create?event_type_id=25&patient_id=<?php echo $this->patient->id?>&firm_id=<?php echo $this->firm->id?>"><img src="/img/_elements/icons/event/small/treatment_operation.png" alt="operation" width="16" height="16" /> - <strong><?php echo $eventType->name ?></strong></a></p>
 		<?php }else{
-			$assetpath = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('application.modules.'.$eventType->class_name.'.img'),true).'/';
+			$assetpath = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('application.modules.'.$eventType->class_name.'.img')).'/';
 			?>
 			<p><a href="/<?php echo $eventType->class_name?>/Default/create?patient_id=<?php echo $this->patient->id?>"><img src="<?php echo $assetpath?>small.png" alt="operation" width="16" height="16" /> - <strong><?php echo $eventType->name ?></strong></a></p>
 		<?php }?>
