21eff7fca0c6ad6f84aeb8454e145fb1c729b9ee
Mark Wadham <m@rkw.me>
2012-05-10 11:41:54 +0100

    bugfix: prevent dupliate icons in the add new event list.

--- protected/views/patient/add_new_event.php
+++ protected/views/patient/add_new_event.php
@@ -3,11 +3,11 @@
 	<p><strong>Select event to add:</strong></p>
 	<?php foreach ($eventTypes as $eventType) {
 		if ($eventType->class_name == 'OphTrOperation') {?>
-			<p><a href="/clinical/create?event_type_id=25&patient_id=<?php echo $this->patient->id?>&firm_id=<?php echo $this->firm->id?>"><img src="/img/_elements/icons/event/small/treatment_operation.png" alt="operation" width="16" height="16" /> - <strong><?php echo $eventType->name ?></strong></a></p>
+			<p><a href="/clinical/create?event_type_id=25&patient_id=<?php echo $this->patient->id?>&firm_id=<?php echo $this->firm->id?>"><img src="/img/_elements/icons/event/small/treatment_operation.png" alt="operation" /> - <strong><?php echo $eventType->name ?></strong></a></p>
 		<?php }else{
-			$assetpath = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('application.modules.'.$eventType->class_name.'.img')).'/';
+			$assetpath = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('application.modules.'.$eventType->class_name.'.img').'/').'/';
 			?>
-			<p><a href="/<?php echo $eventType->class_name?>/Default/create?patient_id=<?php echo $this->patient->id?>"><img src="<?php echo $assetpath?>small.png" alt="operation" width="16" height="16" /> - <strong><?php echo $eventType->name ?></strong></a></p>
+			<p><a href="/<?php echo $eventType->class_name?>/Default/create?patient_id=<?php echo $this->patient->id?>"><img src="<?php echo $assetpath?>small.png" alt="operation" /> - <strong><?php echo $eventType->name ?></strong></a></p>
 		<?php }?>
 	<?php }?>
 </div>
