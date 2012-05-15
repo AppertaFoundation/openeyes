766e5f4c010e1a6fcf5d4b1175e97e37722b0f62
Mark Wadham <m@rkw.me>
2012-05-15 11:45:31 +0100

    bugfix: editable status should be taken from the controller rather than inferred from the event.

--- protected/views/patient/edit_controls.php
+++ protected/views/patient/edit_controls.php
@@ -8,9 +8,9 @@
 		<?php }else{
 			if (is_object($this->event)) {
 				if ($this->event->eventType->class_name == 'OphTrOperation') {?>
-					You can: <span class="aBtn_inactive">View</span><span class="aBtn edit-event"<?php if (!$this->event->editable) {?> style="display: none;"<?php }?>><a class="edit-event" href="/clinical/update/<?php echo $this->event->id?>">Edit</a></span>
+					You can: <span class="aBtn_inactive">View</span><span class="aBtn edit-event"<?php if (!$this->editable) {?> style="display: none;"<?php }?>><a class="edit-event" href="/clinical/update/<?php echo $this->event->id?>">Edit</a></span>
 				<?php }else{?>
-					You can: <span class="aBtn_inactive">View</span><span class="aBtn edit-event"<?php if (!$this->event->editable) {?> style="display: none;"<?php }?>><a class="edit-event" href="/<?php echo $this->event->eventType->class_name?>/Default/update/<?php echo $this->event->id?>">Edit</a></span>
+					You can: <span class="aBtn_inactive">View</span><span class="aBtn edit-event"<?php if (!$this->editable) {?> style="display: none;"<?php }?>><a class="edit-event" href="/<?php echo $this->event->eventType->class_name?>/Default/update/<?php echo $this->event->id?>">Edit</a></span>
 				<?php }
 			}?>
 		<?php }?>
