b1b6f2abde0d83e0c5e29cdce41bb0b614d15135
Mark Wadham <m@rkw.me>
2012-05-15 13:31:42 +0100

    enabled nowrapper mode for the dropdownlist widget.

--- protected/widgets/views/DropDownList.php
+++ protected/widgets/views/DropDownList.php
@@ -1,6 +1,10 @@
-<div id="div_<?php echo get_class($element)?>_<?php echo $field?>" class="eventDetail">
-	<div class="label"><?php echo $element->getAttributeLabel($field)?>:</div>
-	<div class="data">
-		<?php echo CHtml::activeDropDownList($element,$field,$data,$htmlOptions)?>
+<?php if (@$htmlOptions['nowrapper']) {?>
+	<?php echo CHtml::activeDropDownList($element,$field,$data,$htmlOptions)?>
+<?php }else{?>
+	<div id="div_<?php echo get_class($element)?>_<?php echo $field?>" class="eventDetail">
+		<div class="label"><?php echo $element->getAttributeLabel($field)?>:</div>
+		<div class="data">
+			<?php echo CHtml::activeDropDownList($element,$field,$data,$htmlOptions)?>
+		</div>
 	</div>
-</div>
+<?php }?>
