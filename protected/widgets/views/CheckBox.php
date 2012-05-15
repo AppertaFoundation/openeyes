cfe73fd62f7dd0e97ad4ea34afc911e1767cba6d
Mark Wadham <m@rkw.me>
2012-05-15 16:05:53 +0100

    fixed the checkbox bug in the checkbox widget (if checkbox isn't checked, no value is sent in the post).

--- protected/widgets/views/CheckBox.php
+++ protected/widgets/views/CheckBox.php
@@ -3,6 +3,7 @@
 		<div class="label"><?php echo CHtml::encode($element->getAttributeLabel($field))?>:</div>
 		<div class="datacol1">
 			<?php }?>
+			<?php echo CHtml::hiddenField(get_class($element)."[$field]",'0')?>
 			<?php echo CHtml::checkBox(get_class($element)."[$field]",$checked[$field])?>
 		<?php if (!@$options['nowrapper']) {?>
 		</div>
