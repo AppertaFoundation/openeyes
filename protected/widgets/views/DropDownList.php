9617ed88aa7ed96a1262809862a2e2e0d6a07cdc
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-06-20 14:45:35 +0100

    enabled displaying a dropdownlist widget with no label.

--- protected/widgets/views/DropDownList.php
+++ protected/widgets/views/DropDownList.php
@@ -2,7 +2,7 @@
 	<?php echo CHtml::activeDropDownList($element,$field,$data,$htmlOptions)?>
 <?php }else{?>
 	<div id="div_<?php echo get_class($element)?>_<?php echo $field?>" class="eventDetail"<?php if (@$hidden) {?> style="display: none;"<?php }?>>
-		<div class="label"><?php echo $element->getAttributeLabel($field)?>:</div>
+		<?php if (!@$htmlOptions['nolabel']){?><div class="label"><?php echo $element->getAttributeLabel($field)?>:</div><?php }?>
 		<div class="data">
 			<?php echo CHtml::activeDropDownList($element,$field,$data,$htmlOptions)?>
 		</div>
