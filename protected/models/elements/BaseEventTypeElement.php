84f02ca512080c07b7f522db851bce4e619f411b
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-06-20 14:45:51 +0100

    settings bugfixes

--- protected/models/elements/BaseEventTypeElement.php
+++ protected/models/elements/BaseEventTypeElement.php
@@ -62,52 +62,52 @@ class BaseEventTypeElement extends BaseElement
 	function getSetting($key) {
 		$element_type = ElementType::model()->find('class_name=?',array(get_class($this)));
 
-		if (!$metadata = SettingMetadata::model()->find('element_type_id=? and key=?',array($element_type->id,$key))) {
+		if (!$metadata = SettingMetadata::model()->find('element_type_id=? and `key`=?',array($element_type->id,$key))) {
 			return false;
 		}
 
-		if ($setting = SettingInstallation::model()->find('element_type_id=? and key=?',array($element_type->id,$key))) {
+		if ($setting = SettingInstallation::model()->find('element_type_id=? and `key`=?',array($element_type->id,$key))) {
 			return $this->parseSetting($setting, $metadata);
 		}
 
 		$site = Site::model()->findByPk(Yii::app()->session['selected_site_id']);
 
-		if ($setting = SettingInstitution::model()->find('institution_id=? and element_type_id=? and key=?',array($site->institution_id,$element_type->id,$key))) {
+		if ($setting = SettingInstitution::model()->find('institution_id=? and element_type_id=? and `key`=?',array($site->institution_id,$element_type->id,$key))) {
 			return $this->parseSetting($setting, $metadata);
 		}
 
-		if ($setting = SettingSite::model()->find('site_id=? and element_type_id=? and key=?',array($site->id,$element_type->id,$key))) {
+		if ($setting = SettingSite::model()->find('site_id=? and element_type_id=? and `key`=?',array($site->id,$element_type->id,$key))) {
 			return $this->parseSetting($setting, $metadata);
 		}
 
 		$firm = Firm::model()->findByPk(Yii::app()->session['selected_firm_id']);
 
-		if ($setting = SettingSpecialty::model()->find('specialty_id=? and element_type_id=? and key=?',array($firm->serviceSubspecialtyAssignment->subspecialty->specialty_id,$element_type->id,$key))) {
+		if ($setting = SettingSpecialty::model()->find('specialty_id=? and element_type_id=? and `key`=?',array($firm->serviceSubspecialtyAssignment->subspecialty->specialty_id,$element_type->id,$key))) {
 			return $this->parseSetting($setting, $metadata);
 		}
 
-		if ($setting = SettingSubspecialty::model()->find('subspecialty_id=? and element_type_id=? and key=?',array($firm->serviceSubspecialtyAssignment->subspecialty_id,$element_type->id,$key))) {
+		if ($setting = SettingSubspecialty::model()->find('subspecialty_id=? and element_type_id=? and `key`=?',array($firm->serviceSubspecialtyAssignment->subspecialty_id,$element_type->id,$key))) {
 			return $this->parseSetting($setting, $metadata);
 		}
 
-		if ($setting = SettingFirm::model()->find('firm_id=? and element_type_id=? and key=?',array($firm->id,$element_type->id,$key))) {
+		if ($setting = SettingFirm::model()->find('firm_id=? and element_type_id=? and `key`=?',array($firm->id,$element_type->id,$key))) {
 			return $this->parseSetting($setting, $metadata);
 		}
 
 		$user = Yii::app()->session['user'];
 
-		if ($setting = SettingUser::model()->find('user_id=? and element_type_id=? and key=?',array($user->id,$element_type->id,$key))) {
+		if ($setting = SettingUser::model()->find('user_id=? and element_type_id=? and `key`=?',array($user->id,$element_type->id,$key))) {
 			return $this->parseSetting($setting, $metadata);
 		}
 
-		return false;
+		return $metadata->default_value;
 	}
 
 	function parseSetting($setting, $metadata) {
-		if (@$data = unserialize($setting->data)) {
+		if (@$data = unserialize($metadata->data)) {
 			if (isset($data['model'])) {
 				$model = $data['model'];
-				return $model::model()->find($setting->value);
+				return $model::model()->findByPk($setting->value);
 			}
 		}
 
