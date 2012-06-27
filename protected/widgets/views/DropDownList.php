c55455f03f318673fc67028d1abbe417112f81b1
Mark Wadham <m@rkw.me>
2012-06-27 08:41:09 +0100

    updated dropdownlist widget to work better with optgroups.

--- protected/widgets/views/DropDownList.php
+++ protected/widgets/views/DropDownList.php
@@ -2,7 +2,15 @@
 	<?php echo CHtml::activeDropDownList($element,$field,$data,$htmlOptions)?>
 <?php }else{?>
 	<div id="div_<?php echo get_class($element)?>_<?php echo $field?>" class="eventDetail"<?php if (@$hidden) {?> style="display: none;"<?php }?>>
-		<div class="label"><?php echo $element->getAttributeLabel($field)?>:</div>
+		<?php if (@$htmlOptions['layout'] == 'vertical') {?>
+			<div class="label"></div>
+			<div class="DropDownLabelVertical">
+				<?php echo $element->getAttributeLabel($field)?>
+			</div>
+			<div class="label"></div>
+		<?php }else{?>
+			<?php if (!@$htmlOptions['nolabel']){?><div class="label"><?php echo $element->getAttributeLabel($field)?>:</div><?php }?>
+		<?php }?>
 		<div class="data">
 			<?php if (@$htmlOptions['divided']) {?>
 				<select name="<?php echo get_class($element)?>[<?php echo $field?>]" id="<?php echo get_class($element)?>_<?php echo $field?>">
@@ -10,15 +18,11 @@
 						<option value=""><?php echo $htmlOptions['empty']?></option>
 					<?php }?>
 					<?php foreach ($data as $i => $optgroup) {?>
-						<?php if ($i>0) {?>
-							<optgroup label="--------">
-						<?php }?>
+						<optgroup label="---------------">
 							<?php foreach ($optgroup as $id => $option) {?>
 								<option value="<?php echo $id?>"<?php if ($id == $value) {?> selected="selected"<?php }?>><?php echo $option?></option>
 							<?php }?>
-						<?php if ($i>0) {?>
-							</optgroup>
-						<?php }?>
+						</optgroup>
 					<?php }?>
 				</select>
 			<?php }else{?>
