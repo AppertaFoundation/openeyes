858ff6137ea60b40264c97b47f4d00348d189525
Jamie Neil <jamie.neil@gmail.com>
2012-07-19 17:33:10 +0100

    Move postop drugs

--- protected/models/PostopDrug.php
+++ protected/models/PostopDrug.php
@@ -18,49 +18,69 @@
  */
 
 /**
- * This is the model class for table "site_subspecialty_drug".
+ * This is the model class for table "postop_drug".
  *
- * The followings are the available columns in table 'site_subspecialty_drug':
- * @property string $id
- * @property integer $site_id
- * @property integer $subspecialty_id
- * @property integer $drug_id
+ * The followings are the available columns in table 'postop_drug':
+ * @property integer $id
+ * @property string $name
+ *
  */
-class SiteSubspecialtyDrug extends BaseActiveRecord
-{
+class PostopDrug extends BaseActiveRecord {
 	/**
 	 * Returns the static model of the specified AR class.
-	 * @return SiteSubspecialtyDrug the static model class
+	 * @return PostopDrug the static model class
 	 */
-	public static function model($className=__CLASS__)
-	{
+	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
 
 	/**
 	 * @return string the associated database table name
 	 */
-	public function tableName()
-	{
-		return 'site_subspecialty_drug';
+	public function tableName() {
+		return 'postop_drug';
 	}
 
 	/**
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules()
-	{
+	public function rules() {
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
+			array('name', 'required'),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, name', 'safe', 'on'=>'search'),
 		);
 	}
 
 	/**
 	 * @return array relational rules.
 	 */
-	public function relations()
+	public function relations() {
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
 	{
-		return array();
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id,true);
+		$criteria->compare('name',$this->name,true);
+
+		return new CActiveDataProvider(get_class($this), array(
+			'criteria'=>$criteria,
+		));
 	}
 }
