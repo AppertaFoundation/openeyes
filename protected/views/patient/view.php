efd1e1a2fbaac4b7988e697754bf737eadff4625
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-08-08 08:43:42 +0100

    removed GP filter option from patient summary contact filters and prevented GPs from being returned by the search.

--- protected/views/patient/view.php
+++ protected/views/patient/view.php
@@ -291,7 +291,6 @@ if (!empty($address)) {
 								<option value="">- Filter -</option>
 								<option value="consultant" selected="selected">Consultant Ophthalmologist</option>
 								<option value="specialist">Non-ophthalmic specialist</option>
-								<option value="gp">GP</option>
 							</select>
 							&nbsp;
 							<img src="/img/ajax-loader.gif" class="loader" alt="loading..." style="display: none;" />
