f89cc0736756561e2b4b6e9789c03c5fcd6568c2
Mark Wadham <m@rkw.me>
2012-09-20 08:57:38 +0100

    [OE-2051] typo

--- protected/commands/SetCorrespondenceDirectLineNumbersCommand.php
+++ protected/commands/SetCorrespondenceDirectLineNumbersCommand.php
@@ -23,7 +23,7 @@ class SetCorrespondenceDirectLineNumbersCommand extends CConsoleCommand {
 
 		foreach (ElementLetter::model()->findAll() as $element_letter) {
 			$event = Yii::app()->db->createCommand("select * from event where id = $element_letter->event_id")->queryRow();
-			$episode = Yii::app()->db->createCommand("select * from episode where id = $event->episode_id")->queryRow();
+			$episode = Yii::app()->db->createCommand("select * from episode where id = {$event['episode_id']}")->queryRow();
 
 			if ($dl = FirmSiteSecretary::model()->find('firm_id=? and site_id=?',array($episode['firm_id'],$element_letter->site_id))) {
 				Yii::app()->db->createCommand("update et_ophcocorrespondence_letter set direct_line = '$dl->direct_line' where id = $element_letter->id")->query();
