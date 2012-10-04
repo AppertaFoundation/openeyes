f49a497c189850de83bd90ec9a3a573f1b99c26c
mark <mark@mb.(none)>
2012-10-04 15:21:54 +0100

    [OE-2162] logic to remove secondary diagnoses if they are set as the principal diagnosis for an episode.

--- protected/models/Episode.php
+++ protected/models/Episode.php
@@ -299,4 +299,15 @@ class Episode extends BaseActiveRecord
 	 
 		return TRUE;
 	}
+
+	protected function afterSave() {
+		foreach (SecondaryDiagnosis::model()->findAll('patient_id=? and disorder_id=?',$this->patient_id,$this->disorder_id) as $sd) {
+			if ($this->eye_id == $sd->eye_id || ($this->eye_id == 3 && in_array($sd->eye_id,array(1,2)))) {
+				$sd->delete();
+			} else if (in_array($this->eye_id,array(1,2)) && $sd->eye_id == 3) {
+				$sd->eye_id = ($this->eye_id == 1 ? 2 : 1);
+				$sd->save();
+			}
+		}
+	}
 }
