1ebc37b7097af969b6919c0c1225635582a705ea
Jamie Neil <jamie.neil@gmail.com>
2012-10-26 13:43:45 +0100

    Make NHS number parser more robust

--- protected/models/Patient.php
+++ protected/models/Patient.php
@@ -901,7 +901,8 @@ class Patient extends BaseActiveRecord {
 	}
 
 	public function getNhsnum() {
-		return $this->nhs_num ? substr($this->nhs_num,0,3).' '.substr($this->nhs_num,3,3).' '.substr($this->nhs_num,6,4) : 'not known';
+		$nhs_num = preg_replace('/[^0-9]/', '', $this->nhs_num);
+		return $nhs_num ? substr($nhs_num,0,3).' '.substr($nhs_num,3,3).' '.substr($nhs_num,6,4) : 'not known';
 	}
 
 	public function hasLegacyLetters() {
