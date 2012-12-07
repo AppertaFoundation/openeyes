6db85850466a16d5a9d1863bf793dabb1fb69b22
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-12-07 09:49:33 +0000

    [OE-2444] fixed migration that removes the booking tables

--- protected/migrations/m121114_132152_remove_booking_tables.php
+++ protected/migrations/m121114_132152_remove_booking_tables.php
@@ -4,6 +4,7 @@ class m121114_132152_remove_booking_tables extends CDbMigration
 {
 	public function up()
 	{
+		/*
 		$element_operation = ElementType::model()->find('event_type_id=? and class_name=?',array(25,'ElementOperation'));
 		$element_diagnosis = ElementType::model()->find('event_type_id=? and class_name=?',array(25,'ElementDiagnosis'));
 		$this->delete('element_type_anaesthetic_type','element_type_id='.$element_operation->id);
@@ -25,9 +26,10 @@ class m121114_132152_remove_booking_tables extends CDbMigration
 		$this->dropTable('sequence');
 		$this->dropTable('theatre_ward_assignment');
 		$this->dropTable('ward');
+		$this->dropTable('cancelled_booking');
+		*/
 		$this->dropTable('theatre');
 		$this->dropTable('cancelled_operation');
-		$this->dropTable('cancelled_booking');
 		$this->dropTable('date_letter_sent');
 		$this->dropTable('element_operation');
 		$this->dropTable('cancellation_reason');
