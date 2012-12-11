eb58134821e5213fcb7b64b20952ac44242cd1e6
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-12-11 08:34:23 +0000

    [OE-2473] allow overriding the label for a textfield

--- protected/widgets/views/TextField.php
+++ protected/widgets/views/TextField.php
@@ -1,5 +1,5 @@
 <div id="div_<?php echo get_class($element)?>_<?php echo $field?>" class="eventDetail">
-	<div class="label"><?php echo CHtml::encode($element->getAttributeLabel($field))?>:</div>
+	<div class="label"><?php echo empty($htmlOptions['label']) ? CHtml::encode($element->getAttributeLabel($field)) : $htmlOptions['label']?>:</div>
 	<div class="data">
 		<?php echo CHtml::textField($name, $value, $htmlOptions)?>
 	</div>
