529c1b17a567db0b08e6840c57e8e23f1f65b193
splatEric <mike.smith@camc-ltd.co.uk>
2012-12-12 14:48:28 +0000

    + add ability to define arbitary html options on the event type form HiddenField widget in line with many of the others.

--- protected/widgets/views/HiddenField.php
+++ protected/widgets/views/HiddenField.php
@@ -1,1 +1,1 @@
-<?php echo CHtml::hiddenField(get_class($element)."[".$field."]", $value, array('id' => get_class($element).'_'.$field))?>
+<?php echo CHtml::hiddenField(get_class($element)."[".$field."]", $value, $htmlOptions)?>
