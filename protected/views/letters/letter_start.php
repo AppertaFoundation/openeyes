370c7b5202e1381eb9eac7cbda34f548e451c02f
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-12-13 09:34:10 +0000

    [OE-2493] made all patient details bold in the letter_start partial.

--- protected/views/letters/letter_start.php
+++ protected/views/letters/letter_start.php
@@ -4,11 +4,11 @@
 	<?php if(@$patient_ref) { 
 		echo $patient->fullname . ', ';
 	} ?>
-	<strong>Hospital Reference Number: <?php echo $patient->hos_num; ?></strong>
-	<?php if($patient->nhsnum) { ?><br/> NHS Number: <?php echo $patient->nhsnum; } ?>
+	<strong>Hospital Reference Number: <?php echo $patient->hos_num; ?>
+	<?php if($patient->nhsnum) { ?><br/> NHS Number: <?php echo $patient->nhsnum; }?>
 	<?php if(@$patient_ref) { ?>
 	<br /><?php echo $patient->correspondAddress->letterline ?>
 	<br />DOB: <?php echo $patient->NHSDate('dob') ?>, <?php echo ($patient->gender == 'M') ? 'Male' : 'Female'; ?>
-	<?php } ?>
+	<?php } ?></strong>
 </p>
 
