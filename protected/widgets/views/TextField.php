f42058b02324a065678fb627836850c0151d07b2
Mark <m@rkw.me>
2013-02-13 13:24:58 +0000

    [OE-2635] enabled hiding of textfield inputs.

--- protected/widgets/views/TextField.php
+++ protected/widgets/views/TextField.php
@@ -1,7 +1,7 @@
 <?php if (@$htmlOptions['nowrapper']) {?>
 	<?php echo CHtml::textField($name, $value, $htmlOptions)?>
 <?php }else{?>
-	<div id="div_<?php echo get_class($element)?>_<?php echo $field?>" class="eventDetail">
+	<div id="div_<?php echo get_class($element)?>_<?php echo $field?>" class="eventDetail" <?php if (@$htmlOptions['hide']) {?> style="display: none;"<?php }?>>
 		<div class="label"><?php echo empty($htmlOptions['label']) ? CHtml::encode($element->getAttributeLabel($field)) : $htmlOptions['label']?>:</div>
 		<div class="data">
 			<?php echo CHtml::textField($name, $value, $htmlOptions)?>
