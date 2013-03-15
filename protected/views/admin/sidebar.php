5fd19d21ccac52a09348160870f064bcde877539
Mark <m@rkw.me>
2013-03-15 13:52:23 +0000

    menu selection for admin system

--- protected/views/admin/sidebar.php
+++ protected/views/admin/sidebar.php
@@ -12,8 +12,12 @@
 			'Sessions' => '/admin/sessions',
 			'Episode status' => '/admin/episodeStatus',
 		) as $title => $uri) {?>
-			<li<?php if (false) {?> class="active"<?php }?>>
-				<?php echo CHtml::link($title,array($uri))?>
+			<li<?php if (Yii::app()->getController()->action->id == preg_replace('/^\/admin\//','',$uri)) {?> class="active"<?php }?>>
+				<?php if (Yii::app()->getController()->action->id == preg_replace('/^\/admin\//','',$uri)) {?>
+					<span class="viewing"><?php echo $title?></span>
+				<?php }else{?>
+					<?php echo CHtml::link($title,array($uri))?>
+				<?php }?>
 			</li>
 		<?php }?>
 	</ul>
@@ -22,9 +26,15 @@
 	<?php foreach (ModuleAdmin::getAll() as $module => $items) {?>
 		<h4><?php echo $module?></h4>
 		<ul>
-			<?php foreach ($items as $item => $uri) {?>
-				<li>
-					<?php echo CHtml::link($item,array($uri))?>
+			<?php foreach ($items as $item => $uri) {
+				$e = explode('/',$uri);
+				$action = array_pop($e)?>
+				<li<?php if (Yii::app()->getController()->action->id == $action){?> class="active"<?php }?>>
+					<?php if (Yii::app()->getController()->action->id == $action){?>
+						<span class="viewing"><?php echo $item?></span>
+					<?php }else{?>
+						<?php echo CHtml::link($item,array($uri))?>
+					<?php }?>
 				</li>
 			<?php }?>
 		</ul>
