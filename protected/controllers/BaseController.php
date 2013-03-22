11ecc0be190e17ec513a6609d90380241b81faa5
Mark <m@rkw.me>
2013-03-22 13:57:49 +0000

    fixed error when clicking cancel on edit user form.

--- protected/controllers/BaseController.php
+++ protected/controllers/BaseController.php
@@ -29,6 +29,7 @@ class BaseController extends Controller
 	public $showForm = false;
 	public $patientId;
 	public $patientName;
+	public $jsVars = array();
 
 	/**
 	 * Check to see if user's level is high enough
@@ -202,5 +203,16 @@ class BaseController extends Controller
 
 		Yii::log($message . ' from ' . $addr, "user", "userActivity");
 	}
-	
+
+	protected function beforeRender($view) {
+		$this->processJsVars();
+		return parent::beforeRender($view);
+	}
+
+	public function processJsVars() {
+		foreach ($this->jsVars as $key => $value) {
+			$value = CJavaScript::encode($value);
+			Yii::app()->getClientScript()->registerScript('scr_'.$key, "$key = $value;",CClientScript::POS_READY);
+		}
+	}
 }
