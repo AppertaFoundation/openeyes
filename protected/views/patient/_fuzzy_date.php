62f6791163586cd148311958b9e93a0ba71fe30c
Mark <m@rkw.me>
2013-04-03 14:07:14 +0100

    [OE-2876] fixed fuzzy date defaults

--- protected/views/patient/_fuzzy_date.php
+++ protected/views/patient/_fuzzy_date.php
@@ -17,16 +17,6 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
-<?php 
-	if (!@$fuzzy_date) 
-	{
-		// default to today
-		$fuzzy_date = date('Y-m-d');
-	}
-	$_year = (integer)substr($fuzzy_date,0,4);
-	$_month = (integer)substr($fuzzy_date,5,2);
-	$_day = (integer)substr($fuzzy_date,8,2);
-	?>
 							<div class="fuzzy_date <?php echo @$class?>">
 								<span class="fuzzy_date_label label">
 									Date:
@@ -34,18 +24,18 @@
 								<select name="fuzzy_day" class="fuzzy_date_field">
 									<option value="">Day (optional)</option>
 									<?php for ($i=1;$i<=31;$i++) {?>
-										<option value="<?php echo $i?>"<?php if ($_day == $i) {?> selected="selected"<?php }?>><?php echo $i?></option>
+										<option value="<?php echo $i?>"><?php echo $i?></option>
 									<?php }?>
 								</select>
 								<select name="fuzzy_month" class="fuzzy_date_field">
 									<option value="">Month (optional)</option>
 									<?php foreach (array('January','February','March','April','May','June','July','August','September','October','November','December') as $i => $month) {?>
-										<option value="<?php echo $i+1?>"<?php if ($_month == $i+1) {?> selected="selected"<?php }?>><?php echo $month?></option>
+										<option value="<?php echo $i+1?>"><?php echo $month?></option>
 									<?php }?>
 								</select>
 								<select name="fuzzy_year" class="fuzzy_date_field">
 									<?php for ($i=date('Y')-50;$i<=date('Y');$i++) {?>
-										<option value="<?php echo $i?>"<?php if ($_year == $i) {?> selected="selected"<?php }?>><?php echo $i?></option>
+										<option value="<?php echo $i?>"<?php if (date('Y') == $i) {?> selected="selected"<?php }?>><?php echo $i?></option>
 									<?php }?>
 								</select>
 							</div>
