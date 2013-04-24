dc2d317a11d7920cfbe4d2231bd4b50d3eb7f6ea
Jamie Neil <jamie.neil@openeyes.org.uk>
2013-04-24 14:01:51 +0100

    [OE-2760] Fix admission contact string and logic (External)

--- protected/models/elements/ElementOperation.php
+++ protected/models/elements/ElementOperation.php
@@ -1187,8 +1187,8 @@ class ElementOperation extends BaseEventTypeElement
 		}
 
 		# OE-2259 special case for Allan Bruce in External Theatre 9 or CXL
-		if ($this->event->episode->firm_id == 19
-			&& ($this->booking->session->theatre_id == 9 || $this->booking->session->theatre_id == 25)) {
+		if ($this->event->episode->firm->serviceSubspecialtyAssignment->subspecialty->ref_spec == 'EX'
+			&& ($this->booking->session->theatre->code == 'CR9' || $this->booking->session->theatre->code == 'CXL')) {
 			$contact['refuse'] = $subspecialty->name . ' Admission Coordinator on 020 7566 2205';
 		}
 
