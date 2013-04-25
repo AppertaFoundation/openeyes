9c2f47084752a92e65030e0ebf8d428456ce74ad
Jamie Neil <jamie.neil@openeyes.org.uk>
2013-04-25 14:05:56 +0100

    Add URI.js lib for cleaner handling of urls

--- protected/views/layouts/patientMode/event_header.php
+++ protected/views/layouts/patientMode/event_header.php
@@ -42,6 +42,7 @@
 	<?php $cs->registerCSSFile($cs->getCoreScriptUrl().'/jui/css/base/jquery-ui.css', 'screen')?>
 	<?php $cs->registerScriptFile(Yii::app()->createUrl('js/jquery.watermark.min.js'))?>
 	<?php $cs->registerScriptFile(Yii::app()->createUrl('js/mustache.js'))?>
+	<?php $cs->registerScriptFile(Yii::app()->createUrl('js/libs/uri-1.10.2.js'))?>
 	<?php $cs->registerScriptFile(Yii::app()->createUrl('js/waypoints.min.js'))?>
 	<?php $cs->registerScriptFile(Yii::app()->createUrl('js/waypoints-sticky.min.js'))?>
 	<?php $cs->registerScriptFile(Yii::app()->createUrl('js/libs/modernizr-2.0.6.min.js'))?>
