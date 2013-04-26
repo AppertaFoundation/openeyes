9428c076b67629bdd706ae7c8f4c71ffb98322e6
Mark <m@rkw.me>
2013-04-26 09:16:05 +0100

    [OE-2623 fixed crash when saving medication with a route option

--- protected/controllers/PatientController.php
+++ protected/controllers/PatientController.php
@@ -1192,7 +1192,7 @@ class PatientController extends BaseController
 			$patient->addMedication(array(
 				'drug_id' => $drug->id,
 				'route_id' => $route->id,
-				'option_id' => $option ? $option->id : null,
+				'option_id' => @$option ? $option->id : null,
 				'frequency_id' => $frequency->id,
 				'start_date' => $_POST['start_date'],
 			));
