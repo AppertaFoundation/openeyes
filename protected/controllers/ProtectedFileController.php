d170fed29a99b735ad42ea1d125ee4981236074e
Jamie Neil <jamie.neil@gmail.com>
2013-04-30 18:16:57 +0100

    Rename Assets to ProtectedFiles Add auto thumbnail feature

---
+++ protected/controllers/ProtectedFileController.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * OpenEyes
+ *
+ * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
+ * (C) OpenEyes Foundation, 2011-2012
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2008-2011, Moorfields Eye Hospital NHS Foundation Trust
+ * @copyright Copyright (c) 2011-2012, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
+ */
+
+class ProtectedFileController extends BaseController {
+
+	public function accessRules() {
+		return array(
+				// Level 3 or above can do anything
+				array('allow',
+						'actions' => array('download'),
+						'expression' => 'BaseController::checkUserLevel(3)',
+				),
+				array('deny'),
+		);
+	}
+
+	public function actionDownload($id) {
+		if(!$file = ProtectedFile::model()->findByPk($id)) {
+			throw new CHttpException(404, "File not found");
+		}
+		if(!file_exists($file->getPath())) {
+			throw new CException("File not found on filesystem: ".$file->getPath());
+		}
+		header('Content-Description: File Transfer');
+		header('Content-Type: ' . $file->mimetype);
+		header('Content-Disposition: attachment; filename="' . $file->name . '"');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Content-Length: ' . $file->size);
+		ob_clean();
+		flush();
+		readfile($file->getPath());
+	}
+	
+	public function actionView($id, $name) {
+		if(!$file = ProtectedFile::model()->findByPk($id)) {
+			throw new CHttpException(404, "File not found");
+		}
+		if(!file_exists($file->getPath())) {
+			throw new CException("File not found on filesystem: ".$file->getPath());
+		}
+		header('Content-Type: ' . $file->mimetype);
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Content-Length: ' . $file->size);
+		ob_clean();
+		flush();
+		readfile($file->getPath());
+	}
+	
+	public function actionThumbnail($id, $dimensions, $name) {
+		if(!$file = ProtectedFile::model()->findByPk($id)) {
+			throw new CHttpException(404, "File not found");
+		}
+		if(!$thumbnail = $file->getThumbnail($dimensions, true)) {
+			throw new CHttpException(404, "Thumbnail not available");
+		}
+		header('Content-Type: ' . $file->mimetype);
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Content-Length: ' . $thumbnail['size']);
+		ob_clean();
+		flush();
+		readfile($thumbnail['path']);
+	}
+	
+	public function actionImport() {
+		echo "<h1>Importing files:</h1>";
+		foreach(glob(Yii::app()->basePath.'/data/test/*') as $file) {
+			$file = ProtectedFile::createFromFile($file);
+			if(!$file->save()) {
+				throw new CException('Cannot save file'.print_r($file->getErrors(), true));
+			}
+			unlink($file);
+			echo("<p>Imported ".$file->uid . ' - '. $file->name."</p>");
+		}
+		echo "<p>Done!</p>";
+	}
+
+}
---
+++ protected/models/ProtectedFile.php
@@ -0,0 +1,264 @@
+<?php
+/**
+ * OpenEyes
+ *
+ * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
+ * (C) OpenEyes Foundation, 2011-2012
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2008-2011, Moorfields Eye Hospital NHS Foundation Trust
+ * @copyright Copyright (c) 2011-2012, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
+ */
+
+/**
+ * This is the model class for table "protected_file".
+ *
+ * The followings are the available columns in table 'protected_file':
+ * @property string $id
+ * @property string $uid
+ * @property string $name
+ * @property string $title
+ * @property string $description
+ * @property string $mimetype
+ * @property integer $size
+ */
+class ProtectedFile extends BaseActiveRecord {
+	
+	const THUMBNAIL_QUALITY = 85;
+
+	protected $_source_path;
+
+	protected $_thumbnail = array();
+
+	/**
+	 * Create a new protected file from an existing file
+	 * @param string $path Path to file
+	 * @return ProtectedFile
+	*/
+	public static function createFromFile($path) {
+		$file = new ProtectedFile();
+		$file->setSource($path);
+		return $file;
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @return ProtectedFile the static model class
+	 */
+	public static function model($className=__CLASS__) {
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName() {
+		return 'protected_file';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules() {
+		return array(
+				array('uid, name, mimetype, size', 'required'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations() {
+		return array(
+		);
+	}
+
+	/**
+	 * Path to protected file storage
+	 * @return string
+	 */
+	public static function getBasePath() {
+		return Yii::app()->basePath . '/files';
+	}
+
+	/**
+	 * Path to file
+	 * @return string
+	 */
+	public function getPath() {
+		return self::getBasePath() . '/' . substr($this->uid, 0, 1)
+		. '/' . substr($this->uid, 1, 1) . '/' . substr($this->uid, 2, 1)
+		. '/' . $this->uid;
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see BaseActiveRecord::beforeSave()
+	 */
+	protected function beforeSave() {
+		if($this->_source_path) {
+			mkdir(dirname($this->getPath()), 0777, true);
+			copy($this->_source_path, $this->getPath());
+			$this->_source_path = null;
+		}
+		return true;
+	}
+
+	/**
+	 * Initialise protected file from a source file
+	 * @param string $path
+	 * @throws CException
+	 */
+	public function setSource($path) {
+
+		if(!file_exists($path) || is_dir($path)) {
+			throw new CException("File doesn't exist: ".$path);
+		}
+		$this->_source_path = $path;
+
+		$this->name = basename($path);
+
+		// Set MIME type
+		$path_parts = pathinfo($this->name);
+		$this->mimetype = $this->lookupMimetype($path);
+			
+		// Set size
+		$this->size = filesize($path);
+
+		// Set UID
+		$this->uid = sha1(microtime().$this->name);
+		while(file_exists($this->getPath())) {
+			$this->uid = sha1(microtime().$this->name);
+		}
+
+	}
+
+	/**
+	 * Get the mime type of the file
+	 * @param string $path
+	 */
+	protected function lookupMimetype($path) {
+		$finfo = new finfo(FILEINFO_MIME_TYPE);
+		return $finfo->file($path);
+	}
+
+	/**
+	 * Has the file got a thumbnail 
+	 * @return boolean
+	 */
+	public function hasThumbnail() {
+		return in_array($this->mimetype, array(
+				'image/jpeg',
+				'image/gif',
+				'image/png',
+		));
+	}
+
+	/**
+	 * Get thumbnail of image (generated automatically if not already created)
+	 * @param string $dimensions
+	 * @throws CException
+	 * @return boolean|array:
+	 */
+	public function getThumbnail($dimensions, $regenerate = false) {
+		preg_match('/\d+(x\d+)?/', $dimensions, $matches);
+		if(!$matches) {
+			throw new CException('Invalid thumbnail dimensions');
+		}
+		if($regenerate || !isset($this->_thumbnail[$dimensions])) {
+			$path = $this->getThumbnailPath($dimensions);
+			if($regenerate || !file_exists($path)) {
+				if(!$this->generateThumbnail($dimensions)) {
+					return false;
+				}
+			}
+			$this->_thumbnail[$dimensions] = array(
+					'path' => $path,
+					'size' => filesize($path),
+			);
+		}
+		return $this->_thumbnail[$dimensions];
+	}
+
+	/**
+	 * Get the path for a thumbnail 
+	 * @param string $dimensions
+	 * @return string
+	 */
+	protected function getThumbnailPath($dimensions) {
+		return self::getBasePath() . '/' . substr($this->uid, 0, 1)
+		. '/' . substr($this->uid, 1, 1) . '/' . substr($this->uid, 2, 1)
+		. '/' . $dimensions . '/' . $this->uid;
+	}
+
+	/**
+	 * Generate a thumbnail
+	 * @param string $dimensions
+	 * @throws CException
+	 * @return boolean
+	 */
+	protected function generateThumbnail($dimensions) {
+
+		// Setup source image
+		$image_info = getimagesize($this->getPath());
+		$src_width = $image_info[0];
+		$src_height = $image_info[1];
+		$ratio = $src_width/$src_height;
+		$image_type = $image_info[2];
+		switch($image_type) {
+			case IMAGETYPE_JPEG:
+				$src_image = imagecreatefromjpeg($this->getPath());
+				break;
+			case IMAGETYPE_PNG:
+				$src_image = imagecreatefrompng($this->getPath());
+				break;
+			case IMAGETYPE_GIF:
+				$src_image = imagecreatefromgif($this->getPath());
+				break;
+			default:
+				return false;
+		}
+
+		// Work out thumbnail width/height
+		$dimensions_parts = explode('x', $dimensions);
+		if(!$width = (int) $dimensions_parts[0]) {
+			throw new CException('Bad width');
+		}
+		if(isset($dimensions_parts[1])) {
+			if(!$height = (int) $dimensions_parts[1]) {
+				throw new CException('Bad height');
+			}
+		} else {
+			$height = floor($width / $ratio);
+		}
+
+		// Generate thumbnail
+		$thumbnail = imagecreatetruecolor($width, $height);
+		imagecopyresampled($thumbnail, $src_image, 0, 0, 0, 0, $width, $height, $src_width, $src_height);
+		$thumbnail_path = $this->getThumbnailPath($dimensions);
+		if(!file_exists(dirname($thumbnail_path))) {
+			mkdir(dirname($thumbnail_path), 0777, true);
+		}
+		switch($image_type) {
+			case IMAGETYPE_JPEG:
+				imagejpeg($thumbnail, $thumbnail_path, self::THUMBNAIL_QUALITY);
+				break;
+			case IMAGETYPE_PNG:
+				imagepng($thumbnail, $thumbnail_path, self::THUMBNAIL_QUALITY);
+				break;
+			case IMAGETYPE_GIF:
+				imagegif($thumbnail, $thumbnail_path);
+				break;
+		}
+
+		return true;
+
+	}
+}
