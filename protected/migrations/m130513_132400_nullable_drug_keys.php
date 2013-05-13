d0e5e864d0c75a193479f4b4b6c6a9c5017c3f68
Jamie Neil <jamie.neil@openeyes.org.uk>
2013-05-13 12:45:19 +0000

    Some drug columns should be nullable

---
+++ protected/migrations/m130513_132400_nullable_drug_keys.php
@@ -0,0 +1,14 @@
+<?php
+
+class m130513_132400_nullable_drug_keys extends CDbMigration {
+
+	public function up() {
+		$this->alterColumn('drug', 'default_duration_id', 'int(10) UNSIGNED');
+		$this->alterColumn('drug', 'default_route_id', 'int(10) UNSIGNED');
+		$this->alterColumn('drug', 'default_frequency_id', 'int(10) UNSIGNED');
+	}
+
+	public function down() {
+	}
+
+}
