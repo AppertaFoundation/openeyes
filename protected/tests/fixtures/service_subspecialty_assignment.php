82f5a0c8ac90bad2e7b2fed2b766facc2cb5c1c1
abinrax <reda.benjli@openeyes.org.uk>
2013-06-07 14:30:38 +0100

    ADDED BASH SCRIPT TO AUTO GENERATE SKELETON TEST CLASSES

---
+++ protected/tests/fixtures/service_subspecialty_assignment.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * OpenEyes
+ *
+ * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
+ * (C) OpenEyes Foundation, 2011-2013
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2008-2011, Moorfields Eye Hospital NHS Foundation Trust
+ * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
+ */
+
+
+return array(
+	'servicespecialtyassignment1' => array(
+		'service_id' => 1,
+		'specialty_id' => 1,
+                                                      'subspecialty_id' => 1,
+	),
+	'servicespecialtyassignment2' => array(
+		'service_id' => 2,
+		'specialty_id' => 2,
+                                                    'subspecialty_id' => 2,
+	),
+	'servicespecialtyassignment3' => array(
+		'service_id' => 3,
+		'specialty_id' => 3,
+                                                    'subspecialty_id' => 3,
+	),
+	'servicespecialtyassignment4' => array(
+		'service_id' => 4,
+		'specialty_id' => 4,
+                                                    'subspecialty_id' => 4,
+	),
+);
+
