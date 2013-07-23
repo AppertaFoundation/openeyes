58c9cccf23eef007e2989a9690c9a7fa020fb6e7
Richard Willis <willis.rh@gmail.com>
2013-07-23 13:52:36 +0100

    Responsive header work

--- templates/components/header_logged_in.php
+++ templates/components/header_logged_in.php
@@ -4,10 +4,10 @@
 		  OpenEyes
 		</div>
 	</div>
-	<div class="large-4 columns">
+	<div class="large-4 medium-5 columns">
 		<? include 'patient_panel.php'; ?>
 	</div>
-	<div class="large-6 columns">
+	<div class="large-6 medium-7 columns">
 		<? include 'user_panel.php'; ?>
 	</div>
 </header>
