ed7a5ee9ef1f57dee72b66d6da758f35fa4cd1ae
Richard Willis <willis.rh@gmail.com>
2013-07-30 13:05:08 +0100

    Split the waiting list template into components; renamed filters-panel to actions-panel

---
+++ templates/components/waiting-list-button-bar.php
@@ -0,0 +1,19 @@
+<button id="btn_print_all" class="small">Print all</button>
+<button id="btn_print" class="small">Print selected</button>
+<div class="panel panel-orange">
+        <label for="adminconfirmdate">Set latest letter sent to be:</label>
+        <input class="small" type="text" style="width:110px" />
+</div>
+<div class="panel panel-orange">
+    <select name="adminconfirmto" id="adminconfirmto">
+        <option value="OFF">Off</option>
+        <option value="noletters">No letters sent</option>
+        <option value="0">Invitation letter</option>
+        <option value="1">1st reminder letter</option>
+        <option value="2">2nd reminder letter</option>
+        <option value="3">GP letter</option>
+    </select>
+</div>
+<button class="small secondary">
+    Confirm selected
+</button>
---
+++ templates/components/waiting-list-filters.php
@@ -0,0 +1,53 @@
+<div class="row">
+	<div class="large-12 column">
+		<h2>Search partial bookings waiting lists by:</h2>
+	</div>
+</div>
+<div class="row">
+	<div class="large-12 column">
+		<table class="grid">
+			<thead>
+				<tr>
+					<th>Service:</th>
+					<th>Firm:</th>
+					<th>Next letter due:</th>
+					<th>Site:</th>
+					<th>Hospital no:</th>
+					<th>&nbsp;</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						<select>
+							<option>All specialties</option>
+						</select>
+					</td>
+					<td>
+						<select>
+							<option>All firms</option>
+						</select>
+					</td>
+					<td>
+						<select>
+							<option>Any</option>
+						</select>
+					</td>
+					<td>
+						<select>
+							<option>All sites</option>
+						</select>
+					</td>
+					<td>
+						<input type="text" />
+					</td>
+					<td class="text-right">
+						<button class="secondary">
+							Search
+						</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
