52252ac92ab57507ba0b6a597098674832c745bb
Richard Willis <willis.rh@gmail.com>
2013-07-30 13:05:58 +0100

    Added base theatre diaries template

---
+++ templates/theatre-diaries.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<? include 'components/head.php'; ?>
+</head>
+<body>
+	<div class="container" role="main">
+
+		<? include 'components/header-logged-in-no-patient.php'; ?>
+
+		<div class="content">
+			<h1 class="badge">Partial bookings waiting list</h1>
+
+			<div class="box-content">
+
+				<div class="panel actions-panel row">
+					<div class="large-3 column">
+						<div class="actions-label">
+							Use the filters below to find patients:
+						</div>
+					</div>
+					<div class="large-9 column text-right">
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+		<? include 'components/footer.php'; ?>
+	</div>
+</body>
+</html>
