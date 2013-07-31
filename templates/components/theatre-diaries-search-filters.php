9e160bff0b073958f926e0403a79c57fb77c565b
Richard Willis <willis.rh@gmail.com>
2013-07-31 11:14:35 +0100

    Theatre diaries extra search fields completed

--- templates/components/theatre-diaries-search-filters.php
+++ templates/components/theatre-diaries-search-filters.php
@@ -3,86 +3,100 @@
 		<h2>Search schedules by:</h2>
 	</div>
 </div>
-<div class="row search-filters theatre-diaries">
+<div class="search-filters theatre-diaries">
 
-	<div class="large-12 column">
+	<div class="row">
+		<div class="large-12 column">
 
-		<table class="grid">
-			<thead>
-				<tr>
-					<th>Site:</th>
-					<th>Theatre:</th>
-					<th>Subspeciality:</th>
-					<th>Firm:</th>
-					<th>Ward:</th>
-					<th>Emergency list:</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>
-						<select>
-							<option>All sites</option>
-						</select>
-					</td>
-					<td>
-						<select>
-							<option>All theatres</option>
-						</select>
-					</td>
-					<td>
-						<select>
-							<option>All specialities</option>
-						</select>
-					</td>
-					<td>
-						<select>
-							<option>All firms</option>
-						</select>
-					</td>
-					<td>
-						<select>
-							<option>All wards</option>
-						</select>
-					</td>
-					<td>
-						<input type="checkbox" />
-					</td>
-				</tr>
-			</tbody>
-		</table>
+			<table class="grid">
+				<thead>
+					<tr>
+						<th>Site:</th>
+						<th>Theatre:</th>
+						<th>Subspeciality:</th>
+						<th>Firm:</th>
+						<th>Ward:</th>
+						<th>Emergency list:</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							<select>
+								<option>All sites</option>
+							</select>
+						</td>
+						<td>
+							<select>
+								<option>All theatres</option>
+							</select>
+						</td>
+						<td>
+							<select>
+								<option>All specialities</option>
+							</select>
+						</td>
+						<td>
+							<select>
+								<option>All firms</option>
+							</select>
+						</td>
+						<td>
+							<select>
+								<option>All wards</option>
+							</select>
+						</td>
+						<td>
+							<input type="checkbox" />
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 
-		<div class="search-filters-extra">
+	<div class="row">
 
-			<label class="highlight">
-				<input type="radio" name="date-filter" />
-				Today
-			</label>
+		<div class="large-10 column">
 
-			<label class="highlight">
-				<input type="radio" name="date-filter" />
-				Next 7 days
-			</label>
+			<div class="search-filters-extra clearfix">
 
-			<label class="highlight">
-				<input type="radio" name="date-filter" />
-				Next 30 days
-			</label>
-
-			<fieldset class="highlight">
-				<label>
+				<label class="highlight">
 					<input type="radio" name="date-filter" />
-					or select date range:
+					Today
 				</label>
 
-				<input class="small fixed-width" type="text" name="date-start" /> to
-				<input class="small fixed-width" type="text" name="date-end" />
+				<label class="highlight">
+					<input type="radio" name="date-filter" />
+					Next 7 days
+				</label>
 
-				<ul class="button-group">
-          <li><a href="#" class="small button">Last week</a></li>
-          <li><a href="#" class="small button">Next week</a></li>
-        </ul>
-			</fieldset>
+				<label class="highlight">
+					<input type="radio" name="date-filter" />
+					Next 30 days
+				</label>
+
+				<fieldset class="highlight">
+					<label>
+						<input type="radio" name="date-filter" />
+						Or select date range:
+					</label>
+					<input class="small fixed-width" type="text" name="date-start" />
+					<span class="to">to</span>
+					<input class="small fixed-width" type="text" name="date-end" />
+
+					<ul class="button-group">
+	          <li><a href="#" class="small button">Last week</a></li>
+	          <li><a href="#" class="small button">Next week</a></li>
+	        </ul>
+				</fieldset>
+			</div>
+		</div>
+
+		<div class="large-2 column text-right">
+			<button class="secondary">
+				Search
+			</button>
 		</div>
 	</div>
 </div>
