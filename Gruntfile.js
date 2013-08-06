519da21faac7ee8a9fe9fe3885a0b14c72beb56c
Richard Willis <willis.rh@gmail.com>
2013-08-06 13:36:18 +0100

    Using minimatch to auto-load grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,8 @@ module.exports = function(grunt) {
   /* Set the config */
   grunt.initConfig(require('./grunt/config')(grunt));
 
-  /* Load the node packaged grunt tasks */
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-styleguide');
-  grunt.loadNpmTasks('grunt-jsdoc');
-  grunt.loadNpmTasks('grunt-modernizr');
+  /* Load the npm grunt tasks */
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   /* Load our custom grunt tasks */
   grunt.loadTasks('./grunt/tasks');
