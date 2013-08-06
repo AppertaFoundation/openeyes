41a93aa2a76048d8ad5cac2828436bc8b443b501
splatEric <mike.smith@camc-ltd.co.uk>
2013-08-06 14:42:12 +0100

    + [OE-2474] clinical access check for patient warnings

--- protected/models/Patient.php
+++ protected/models/Patient.php
@@ -1096,29 +1096,46 @@ class Patient extends BaseActiveRecord
 		}
 	}
 	
+	// storage of warning data
+	protected $_clinical_warnings = null;
+	protected $_nonclinical_warnings = null;
+	
 	/**
-	 * return the patient warnings that have been defined
+	 * return the patient warnings that have been defined for the patient. If $clinical is false
+	 * only non-clinical warnings will be returned.
 	 * 
-	 * return {'short_msg' => string, 'long_msg' => string}[]
+	 * @param boolean $clinical
+	 * @return {'short_msg' => string, 'long_msg' => string}[]
 	 */
-	public function getWarnings()
+	public function getWarnings($clinical=true)
 	{
 		// At the moment, we only warn for diabetes, so this is quite lightweight and hard coded
 		// but this should serve as a wrapper function for configuring warnings (i.e. a system setting could 
 		// define what should be warned on, and then we return a structure that is determined from this)
+		
 		$res = array();
 		
-		if ($diabetic_disorders = $this->getDisordersOfType(Disorder::$SNOMED_DIABETES_SET) ) {
-			$terms = array();
-			foreach ($diabetic_disorders as $disorder) {
-				$terms[] = $disorder->term;
+		if ($clinical) {
+			if ($this->_nonclinical_warnings == null) {
+				// this should be expanded with any future clinical disorders
+				if ($diabetic_disorders = $this->getDisordersOfType(Disorder::$SNOMED_DIABETES_SET) ) {
+					$terms = array();
+					foreach ($diabetic_disorders as $disorder) {
+						$terms[] = $disorder->term;
+					}
+					$res[] = array(
+							'short_msg' => 'Diabetes',
+							'long_msg' => 'Patient is Diabetic',
+							'details' => implode(', ', $terms)
+					);
+				}
+				
+				$this->_nonclinical_warnings = $res;
 			}
-			$res[] = array(
-				'short_msg' => 'Diabetes',
-				'long_msg' => 'Patient is Diabetic',
-				'details' => implode(', ', $terms)
-			);
+			return $this->_nonclinical_warnings;
 		}
+		
+		
 		return $res;
 	}
 }
