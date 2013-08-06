8678726f35a84b5b8b3d268f68af4564742135d7
Richard Willis <willis.rh@gmail.com>
2013-08-06 15:40:15 +0100

    Consistant defining of new methods on Function.prototype

--- js/util.js
+++ js/util.js
@@ -53,25 +53,28 @@ Object.defineProperty(Function.prototype, 'inherits', {
  * @name Function.prototype#bind
  */
 if (!Function.prototype.bind) {
-  Function.prototype.bind = function (oThis) {
-    if (typeof this !== "function") {
-      // closest thing possible to the ECMAScript 5 internal IsCallable function
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+  Object.defineProperty(Function.prototype, 'bind', {
+    value: function(oThis) {
+
+      if (typeof this !== "function") {
+        // closest thing possible to the ECMAScript 5 internal IsCallable function
+        throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+      }
+
+      var aArgs = Array.prototype.slice.call(arguments, 1);
+      var fToBind = this;
+      var fNOP = function () {};
+      var fBound = function () {
+        return fToBind.apply((this instanceof fNOP && oThis ? this : oThis),
+          aArgs.concat(Array.prototype.slice.call(arguments)));
+      };
+
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+
+      return fBound;
     }
-
-    var aArgs = Array.prototype.slice.call(arguments, 1);
-    var fToBind = this;
-    var fNOP = function () {};
-    var fBound = function () {
-      return fToBind.apply((this instanceof fNOP && oThis ? this : oThis),
-        aArgs.concat(Array.prototype.slice.call(arguments)));
-    };
-
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
-
-    return fBound;
-  };
+  });
 }
 
 (function(window, namespace) {
