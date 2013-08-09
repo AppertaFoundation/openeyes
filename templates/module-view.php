a3c75634bfb4f0a323d171efecc99598acd0210f
Richard Willis <willis.rh@gmail.com>
2013-08-09 16:01:04 +0100

    Completed the episodes panel and other style amends

--- templates/module-view.php
+++ templates/module-view.php
@@ -40,32 +40,41 @@
 										Adnexal
 									</a>
 								</h4>
-								<button class="button secondary small add-event" type="button">
-									<span class="icon-button-small-plus-sign"></span>
-									Add event
-								</button>
-								<ol class="events">
-									<li class="selected">
-										<a href="#">
-											<span class="event-type alert">
-												<img src="/img/new/operationbooking/small.png" alt="op" width="19" height="19" />
-											</span>
-											<span class="event-date">
-												1 Jan 2000
-											</span>
-										</a>
-									</li>
+								<ol class="events-overview">
 									<li>
 										<a href="#">
-											<span class="event-type alert">
-												<img src="/img/new/operationbooking/small.png" alt="op" width="19" height="19" />
-											</span>
-											<span class="event-date">
-												1 Jan 2000
-											</span>
+											<img src="/img/new/operationbooking/small.png" alt="op" width="19" height="19" />
 										</a>
 									</li>
 								</ol>
+								<div class="events-container">
+									<button class="button secondary tiny add-event" type="button">
+										<span class="icon-button-small-plus-sign"></span>
+										Add event
+									</button>
+									<ol class="events">
+										<li class="selected">
+											<a href="#">
+												<span class="event-type alert">
+													<img src="/img/new/operationbooking/small.png" alt="op" width="19" height="19" />
+												</span>
+												<span class="event-date">
+													1 Jan 2000
+												</span>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<span class="event-type alert">
+													<img src="/img/new/operationbooking/small.png" alt="op" width="19" height="19" />
+												</span>
+												<span class="event-date">
+													1 Jan 2000
+												</span>
+											</a>
+										</li>
+									</ol>
+								</div>
 							</section>
 
 							<!-- Episode panel -->
@@ -78,7 +87,7 @@
 								</a>
 								<h4 class="episode-title">
 									<a href="#">
-										Adnexal
+										Cataract
 									</a>
 								</h4>
 								<ol class="events-overview">
@@ -88,32 +97,34 @@
 										</a>
 									</li>
 								</ol>
-								<button class="button secondary disabled small add-event" type="button">
-									<span class="icon-button-small-plus-sign"></span>
-									Add event
-								</button>
-								<ol class="events">
-									<li>
-										<a href="#">
-											<span class="event-type alert">
-												<img src="/img/new/operationbooking/small.png" alt="op" width="19" height="19" />
-											</span>
-											<span class="event-date">
-												1 Jan 2000
-											</span>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<span class="event-type alert">
-												<img src="/img/new/operationbooking/small.png" alt="op" width="19" height="19" />
-											</span>
-											<span class="event-date">
-												1 Jan 2000
-											</span>
-										</a>
-									</li>
-								</ol>
+								<div class="events-container">
+									<button class="button secondary disabled tiny add-event" type="button">
+										<span class="icon-button-small-plus-sign"></span>
+										Add event
+									</button>
+									<ol class="events">
+										<li>
+											<a href="#">
+												<span class="event-type alert">
+													<img src="/img/new/operationbooking/small.png" alt="op" width="19" height="19" />
+												</span>
+												<span class="event-date">
+													1 Jan 2000
+												</span>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<span class="event-type alert">
+													<img src="/img/new/operationbooking/small.png" alt="op" width="19" height="19" />
+												</span>
+												<span class="event-date">
+													1 Jan 2000
+												</span>
+											</a>
+										</li>
+									</ol>
+								</div>
 							</section>
 						</div>
 
