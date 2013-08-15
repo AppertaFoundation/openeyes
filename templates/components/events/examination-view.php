ee13cc4008be45a880776b00451a6ea0996092c2
Richard Willis <willis.rh@gmail.com>
2013-08-15 14:06:34 +0100

    Added colapsed grid and nested sub-panels to the examination view

--- templates/components/events/examination-view.php
+++ templates/components/events/examination-view.php
@@ -17,6 +17,69 @@
 	<div class="box event content view examination">
 		<h2 class="event-title">Examination</h2>
 
+		<div class="row">
+			<div class="large-12 column">
 
+				<!-- Element -->
+				<div class="element">
+					<h3 class="element-title">History</h3>
+					<div class="panel element">
+						<div class="element-value">History test 123</div>
+
+						<!-- Sub (optional) element -->
+						<div class="sub-element">
+							<h4 class="sub-element-title">Comorbidities</h4>
+							<div class="panel sub-element">
+								<div class="element-value">
+									Angina, Ethnicity
+									<br />
+									Test 123
+								</div>
+							</div>
+						</div>
+						<!-- / Sub (optional) element -->
+
+					</div>
+				</div>
+				<!-- / Element -->
+
+				<div class="element">
+					<h3 class="element-title">Refraction</h3>
+
+					<div class="row collapse">
+						<div class="large-6 column">
+							<div class="element panel">
+								<div class="row">
+									<div class="large-3 column">
+										<img src="/img/new/tmp/eyedraw-small.png" class="canvas" />
+									</div>
+									<div class="large-9 column">
+										<div class="element-value">
+											0.00/0.00 @ 20° Auto-refraction <br />
+											Spherical equivalent: 0.00
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="large-6 column">
+							<div class="element panel">
+								<div class="row">
+									<div class="large-3 column">
+										<img src="/img/new/tmp/eyedraw-small.png" class="canvas" />
+									</div>
+									<div class="large-9 column">
+										<div class="element-value">
+											0.00/0.00 @ 20° Auto-refraction <br />
+											Spherical equivalent: 0.00
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
