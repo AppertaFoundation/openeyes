5f06ee08cb755bca73baf749295e3be5060c53e7
Richard Willis <willis.rh@gmail.com>
2013-08-27 15:35:22 +0100

    Fixed login view

--- templates/login.php
+++ templates/login.php
@@ -13,7 +13,7 @@
 			<div class="row">
 				<div class="large-11 small-11 small-centered large-centered column">
 					<form class="form panel login">
-						<div class="row">
+						<div class="row field-row">
 							<div class="small-4 column">
 								<label for="username" class="inline">Username:</label>
 							</div>
@@ -21,7 +21,7 @@
 								<input type="text" id="username" placeholder="Enter username...">
 							</div>
 						</div>
-						<div class="row">
+						<div class="row field-row">
 							<div class="small-4 column">
 								<label for="password" class="inline">Password:</label>
 							</div>
@@ -29,7 +29,7 @@
 								<input type="password" id="password" placeholder="Enter password...">
 							</div>
 						</div>
-						<div class="row text-right">
+						<div class="row field-row text-right">
 							<div class="small-12 column">
 								<button type="submit">
 									Login
