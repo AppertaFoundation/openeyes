41efbdaf08e20180072829c56c8880954dc41f8f
Richard Willis <willis.rh@gmail.com>
2013-09-06 15:03:07 +0100

    Removed debug

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+	'use strict';
+
 	/* Set the config */
 	grunt.initConfig(require('./grunt/config')(grunt));
 
@@ -8,33 +10,4 @@ module.exports = function(grunt) {
 
 	/* Load our custom grunt tasks */
 	grunt.loadTasks('./grunt/tasks');
-
-	var _ = grunt.util._;
-
-
-	grunt.registerTask(
-		'expose', "Expose available tasks as JSON object.", function () {
-			var tasks = grunt.task._tasks;
-			_.each( tasks, function( value, key, list ) {
-				var targets = grunt.config.getRaw( key ) || {};
-				delete targets['options'];
-				list[ key ].targets = Object.keys(targets);
-			});
-			grunt.log.write("EXPOSE_BEGIN" + JSON.stringify(tasks, '', 2) + "EXPOSE_END");
-		}
-	);
-
-	grunt.registerTask('hello', 'test', function() {
-
-		var done = this.async();
-
- 		var child = grunt.util.spawn({
-      cmd: 'node',
-      args: [ 'test.js' ]
-    }, function (err, result, code) {
-      done();
-    });
-    child.stdout.pipe(process.stdout);
-    child.stderr.pipe(process.stderr);
-	})
 };
