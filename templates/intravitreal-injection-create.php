aea40bddc01f0d17c34b840ec7a4d5a76aede5b2
Richard Willis <willis.rh@gmail.com>
2013-09-11 16:24:03 +0100

    Added initial Intravitreal injection create template

---
+++ templates/intravitreal-injection-create.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<? include 'components/head.php'; ?>
+<link rel="stylesheet" href="/module_assets/intravitreal-injection/css/module_new.css" />
+</head>
+<body>
+	<div class="container main" role="main">
+
+		<? include 'components/header-logged-in.php'; ?>
+
+		<div class="container content">
+			<h1 class="badge">Episodes and events</h1>
+
+			<div class="box content">
+				<div class="row">
+					<? include 'components/events/sidebar.php'; ?>
+					<? include 'components/events/intravitreal-injection-create.php'; ?>
+				</div>
+			</div>
+		</div>
+		<? include 'components/footer.php'; ?>
+	</div>
+</body>
+</html>
