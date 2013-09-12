d4e85bbfaa4fe20a20626df8dad0ab8c148f7e11
Richard Willis <willis.rh@gmail.com>
2013-09-12 09:11:23 +0100

    Added based Intravitreal Injection view template

---
+++ templates/components/events/intravitreal-injection-view.php
@@ -0,0 +1,36 @@
+<div class="large-10 column event view intravitreal-injection">
+	<header class="event-header">
+		<ul class="inline-list tabs event-actions">
+			<li class="selected">
+				<a href="#">View</a>
+			</li>
+			<li>
+				<a href="#">Edit</a>
+			</li>
+		</ul>
+		<div class="button-bar right">
+			<a href="#" class="button small">
+				Print
+			</a>
+		</div>
+	</header>
+	<div class="event-content">
+		<h2 class="event-title">Intravitreal injection</h2>
+
+		<div class="row">
+			<div class="large-12 column">
+
+
+
+				<div class="metadata">
+					<span class="info">
+						Examination created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+					</span>
+					<span class="info">
+						Examination created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
