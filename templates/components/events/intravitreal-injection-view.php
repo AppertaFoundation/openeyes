e70d2f72810de6eb7d7d454e74c501e833b2cc3c
Richard Willis <willis.rh@gmail.com>
2013-09-12 09:34:03 +0100

    Added additional fields for intravitreal injection view

--- templates/components/events/intravitreal-injection-view.php
+++ templates/components/events/intravitreal-injection-view.php
@@ -20,7 +20,258 @@
 		<div class="row">
 			<div class="large-12 column">
 
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Site</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="element-label">
+									Site:
+								</div>
+							</div>
+							<div class="large-10 column">
+								<div class="element-value">
+									City Road
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
 
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Anaesthetic</h3>
+					</header>
+					<div class="element-data element-eyes row">
+						<div class="element-eye right-eye column">
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Anaesthetic Type:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										Topical
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Anaesthetic Agent:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										G Amethocaine
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="element-eye left-eye column">
+							<div class="element-value">Not recorded</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Treatment</h3>
+					</header>
+					<div class="element-data element-eyes row">
+						<div class="element-eye right-eye column">
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Pre Injection Antiseptic:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										Iodine 5%
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Pre Injection Skin Cleanser:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										Chlorhexidine
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Pre Injection IOP Lowering Therapy Required:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										Yes
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Pre Injection IOP Lowering Therapy:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										Iopidine 1.0%
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Drug:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										Avastin
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Number of Injections:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										1
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Batch Number:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										1212
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Batch Expiry Date:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										12 Sep 2013
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Injection Given By:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										Root Enoch
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Injection Time:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										7:42am
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Post Injection IOP Lowering Therapy Required:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										No
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="element-eye left-eye column">
+							<div class="element-value">Not recorded</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Post Injection Examination</h3>
+					</header>
+					<div class="element-data element-eyes">
+						<div class="element-eye right-eye column">
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Counting Fingers Checked?:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										Yes
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										IOP:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										IOP should be checked
+									</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="element-label">
+										Post Injection Drops:
+									</div>
+								</div>
+								<div class="large-8 column">
+									<div class="element-value">
+										G. Levofloxacin four times daily for 5 days
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="element-eye left-eye column">
+							<div class="element-value">
+								Not recorded
+							</div>
+						</div>
+					</div>
+				</section>
 
 				<div class="metadata">
 					<span class="info">
