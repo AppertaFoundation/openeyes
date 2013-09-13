3fd0f7b33e2828f93115e374c9c2f72af0067cdb
Richard Willis <willis.rh@gmail.com>
2013-09-13 13:21:30 +0100

    Added Anaesthetic Satisfaction Audit create fields

--- templates/components/events/anaesthetic-satisfaction-audit-create.php
+++ templates/components/events/anaesthetic-satisfaction-audit-create.php
@@ -17,6 +17,169 @@
 		<div class="row">
 			<div class="large-12 column">
 
+				<!-- Anaesthetist -->
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Anaesthetist</h3>
+					</header>
+					<div class="element-fields">
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Anaesthetist</label>
+							</div>
+							<div class="large-10 column">
+								<select>
+									<option>-- Select --</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- Satisfaction -->
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Satisfaction</h3>
+					</header>
+					<div class="element-fields">
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Pain:</label>
+							</div>
+							<div class="large-5 column end">
+								<div class="field-row">
+									<input type="range" min="0" max="10" step="1" />
+								</div>
+								<div class="field-row">
+									<img class="field_key" id="pain_key" src="/module_assets/anaesthetic-satisfaction-audit/img/new/painscale_adult.png" />
+								</div>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Nausea:</label>
+							</div>
+							<div class="large-3 column end">
+								<input type="range" />
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-10 large-offset-2 column">
+								<div class="field-info">
+									<em>0 - none, 1 - mild, 2 - moderate, 3 - severe</em>
+								</div>
+							</div>
+						</div>
+
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Vomited:</label>
+							</div>
+							<div class="large-10 column">
+								<input type="checkbox" />
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- Vital Signs -->
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Vital Signs</h3>
+					</header>
+					<div class="element-fields">
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Respiratory Rate:</label>
+							</div>
+							<div class="large-10 column">
+								<select>
+									<option>-- Select --</option>
+								</select>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Oxygen Saturation:</label>
+							</div>
+							<div class="large-10 column">
+								<select>
+									<option>-- Select --</option>
+								</select>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Systolic Blood Pressure:</label>
+							</div>
+							<div class="large-10 column">
+								<select>
+									<option>-- Select --</option>
+								</select>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Body Temperature:</label>
+							</div>
+							<div class="large-10 column">
+								<select>
+									<option>-- Select --</option>
+								</select>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Heart Rate:</label>
+							</div>
+							<div class="large-10 column">
+								<select>
+									<option>-- Select --</option>
+								</select>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Conscious Level AVPU:</label>
+							</div>
+							<div class="large-10 column">
+								<select>
+									<option>-- Select --</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- Notes -->
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Notes</h3>
+					</header>
+					<div class="element-fields">
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Comments:</label>
+							</div>
+							<div class="large-10 column">
+								<textarea placeholder="Enter comments..."></textarea>
+							</div>
+						</div>
+						<fieldset class="row field-row">
+							<legend class="large-2 column">
+								Ready for discharge from recovery:
+							</legend>
+							<div class="large-10 column">
+								<label class="inline highlight">
+									<input type="radio"> Yes
+								</label>
+								<label class="inline highlight">
+									<input type="radio" /> No
+								</label>
+							</div>
+						</fieldset>
+					</div>
+				</section>
 			</div>
 		</div>
 	</div>
