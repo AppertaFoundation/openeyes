9ce452cf9b3e747263cc8b291da7ccfa4ebb438e
Richard Willis <willis.rh@gmail.com>
2013-09-13 10:35:12 +0100

    Added initial laser event templates

---
+++ templates/components/events/laser-create.php
@@ -0,0 +1,24 @@
+<div class="large-10 column event laser edit">
+	<header class="event-header">
+		<ul class="inline-list tabs event-actions">
+			<li class="selected">
+				<a href="#">Create</a>
+			</li>
+		</ul>
+		<div class="button-bar right">
+			<a href="#" class="button small secondary">
+				Save
+			</a>
+		</div>
+	</header>
+	<div class="event-content">
+		<h2 class="event-title">Laser</h2>
+
+		<div class="row">
+			<div class="large-12 column">
+
+
+			</div>
+		</div>
+	</div>
+</div>
