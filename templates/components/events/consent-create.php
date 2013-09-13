d2e765b3be3ee81ef6b88bfec4980993dcd0ae80
Richard Willis <willis.rh@gmail.com>
2013-09-13 14:11:25 +0100

    Added base consent event templates

---
+++ templates/components/events/consent-create.php
@@ -0,0 +1,23 @@
+<div class="large-10 column event consent edit">
+	<header class="event-header">
+		<ul class="inline-list tabs event-actions">
+			<li class="selected">
+				<a href="#">Create</a>
+			</li>
+		</ul>
+		<div class="button-bar right">
+			<a href="#" class="button small secondary">
+				Save
+			</a>
+		</div>
+	</header>
+	<div class="event-content">
+		<h2 class="event-title">Anaesthetic Satisfaction Audit</h2>
+
+		<div class="row">
+			<div class="large-12 column">
+
+			</div>
+		</div>
+	</div>
+</div>
---
+++ templates/components/events/consent-view.php
@@ -0,0 +1,29 @@
+<div class="large-10 column event view consent">
+	<header class="event-header">
+		<ul class="inline-list tabs event-actions">
+			<li class="selected">
+				<a href="#">View</a>
+			</li>
+			<li>
+				<a href="#">Edit</a>
+			</li>
+		</ul>
+	</header>
+	<div class="event-content">
+		<h2 class="event-title">Consent</h2>
+
+		<div class="row">
+			<div class="large-12 column">
+
+				<div class="metadata">
+					<span class="info">
+						Anaesthetic Satisfaction Audit  created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+					</span>
+					<span class="info">
+						Anaesthetic Satisfaction Audit  last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
