a20704e036a2332099b98f1a4aa3b93ad4dc7dc1
Richard Willis <willis.rh@gmail.com>
2013-09-13 15:43:13 +0100

    Added consent form view fields

--- templates/components/events/consent-view.php
+++ templates/components/events/consent-view.php
@@ -10,17 +10,174 @@
 		</ul>
 	</header>
 	<div class="event-content">
-		<h2 class="event-title">Consent</h2>
+		<h2 class="event-title">Consent form</h2>
 
 		<div class="row">
 			<div class="large-12 column">
 
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Type</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Type:</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">1. Patient agreement to investigation or treatment</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Leaflets</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Leaflets provided:</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">None</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Procedure</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Eye:</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">Left</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Procedures:</div>
+							</div>
+							<div class="large-9 column">
+								<ul class="field-value procedures">
+									<li>Punctoplasty</li>
+								</ul>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Anaesthetic type:</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">Topical</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Additional procedures:</div>
+							</div>
+							<div class="large-9 column">
+								<ul class="field-value procedures">
+									<li>Botulinum toxin injection to eyelid</li>
+									<li>Some other procedure</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Permissions for images</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Agree for use in audit, education and publication</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">Not applicable</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Other</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Consultant</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">Mr Yassir Abou-Rayyah</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Consultant</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">Mr Yassir Abou-Rayyah</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">An information leaflet has been provided</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">Yes</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Anaesthetic leaflet has been provided</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">Yes</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Witness required</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">No</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Interpreter required</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">No</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-3 column">
+								<div class="field-label">Include supplementary consent form</div>
+							</div>
+							<div class="large-9 column">
+								<div class="field-value">No</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
 				<div class="metadata">
 					<span class="info">
-						Anaesthetic Satisfaction Audit  created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+						Consent form created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
 					</span>
 					<span class="info">
-						Anaesthetic Satisfaction Audit  last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+						Consent form last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
 					</span>
 				</div>
 			</div>
