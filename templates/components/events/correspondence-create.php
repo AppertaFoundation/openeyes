4d0b6d2588cce5784dd8fc5368682e0157db3793
Richard Willis <willis.rh@gmail.com>
2013-09-13 16:30:32 +0100

    Added initial field to correspondence create view

--- templates/components/events/correspondence-create.php
+++ templates/components/events/correspondence-create.php
@@ -18,6 +18,156 @@
 			<div class="large-12 column">
 
 
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Letter</h3>
+					</header>
+					<div class="element-fields">
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Site:</label>
+							</div>
+							<div class="large-10 column">
+								<select>
+									<option>-- Select --</option>
+								</select>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Address:</label>
+							</div>
+							<div class="large-10 column">
+								<div class="field-row">
+									<label class="inline">Recipient:</label>
+									<select>
+										<option>-- Select --</option>
+										<option selected>Dr A Who (GP)</option>
+									</select>
+								</div>
+								<div class="field-row">
+									<textarea class="address">Dr A Who
+83 Wintour Lane
+Northop
+Heald Green
+Lothian
+GH14 2DH</textarea>
+								</div>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Clinic Date:</label>
+							</div>
+							<div class="large-2 column end">
+								<input type="text" />
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Direct Line:</label>
+							</div>
+							<div class="large-2 column end">
+								<input type="text" />
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Introduction:</label>
+							</div>
+							<div class="large-10 column">
+								<div class="row field-row">
+									<div class="large-8 column">
+										<textarea>Dear Dr Who,</textarea>
+									</div>
+									<div class="large-4 column">
+										<label>
+											<input type="checkbox" />
+											Nickname
+										</label>
+									</div>
+								</div>
+								<div class="field-row">
+									<textarea></textarea>
+								</div>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+
+							</div>
+							<div class="large-10 column">
+								<textarea class="letter-body"></textarea>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">From:</label>
+							</div>
+							<div class="large-10 column">
+								<div class="row field-row">
+									<div class="large-6 column end">
+										<input type="text" placeholder="Type to search for users..." />
+									</div>
+								</div>
+								<div class="row field-row">
+									<div class="large-8 column end">
+										<textarea></textarea>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">CC</label>
+							</div>
+							<div class="large-10 column">
+								<div class="field-row">
+									<label class="inline">Recipient:</label>
+									<select>
+										<option>-- Select --</option>
+										<option selected>Dr A Who (GP)</option>
+									</select>
+								</div>
+								<div class="field-row">
+									<textarea class="cc"></textarea>
+								</div>
+							</div>
+						</div>
+						<fieldset class="row field-row">
+							<legend class="large-2 column">
+								Enclosures:
+							</legend>
+							<div class="large-10 column">
+								<div class="field-row row collapse">
+									<div class="large-8 column">
+										<input type="text" />
+									</div>
+									<div class="large-4 column">
+										<div class="postfix align">
+											<a href="#" class="field-info">Remove</a>
+										</div>
+									</div>
+								</div>
+								<div class="field-row row collapse">
+									<div class="large-8 column">
+										<input type="text" />
+									</div>
+									<div class="large-4 column">
+										<div class="postfix align">
+											<a href="#" class="field-info">Remove</a>
+										</div>
+									</div>
+								</div>
+								<div class="field-row">
+									<button class="secondary small">
+										Add
+									</button>
+								</div>
+							</div>
+						</fieldset>
+					</div>
+				</section>
 
 			</div>
 		</div>
