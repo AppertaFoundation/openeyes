504665d7de7ba19df2c5a79efe8c8464b837982f
Richard Willis <willis.rh@gmail.com>
2013-09-13 13:58:23 +0100

    Added Anaesthetic Satisfaction Audit view fields

--- templates/components/events/anaesthetic-satisfaction-audit-view.php
+++ templates/components/events/anaesthetic-satisfaction-audit-view.php
@@ -8,11 +8,6 @@
 				<a href="#">Edit</a>
 			</li>
 		</ul>
-		<div class="button-bar right">
-			<a href="#" class="button small">
-				Print
-			</a>
-		</div>
 	</header>
 	<div class="event-content">
 		<h2 class="event-title">Anaesthetic Satisfaction Audit</h2>
@@ -20,14 +15,148 @@
 		<div class="row">
 			<div class="large-12 column">
 
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Anaesthetist</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Anaesthetist</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">Non-consultant</div>
+							</div>
+						</div>
+					</div>
+				</section>
 
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Satisfaction</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Pain</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">7</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Nausea</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">1</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Vomited</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">No</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Vital Signs</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Respiratory Rate</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">8 or less</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Oxygen Saturation</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">90-94</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Systolic Blood Pressure</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">70 or lower</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Body Temperature</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">36.1-37.4</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Heart Rate</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">41-50</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Conscious Level AVPU</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">Unresponsive</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Calculated MEWS</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">11</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Notes</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Comments</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">Comments here...</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Ready for discharge from recovery</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">Yes</div>
+							</div>
+						</div>
+					</div>
+				</section>
 
 				<div class="metadata">
 					<span class="info">
-						Therapy application created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+						Anaesthetic Satisfaction Audit  created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
 					</span>
 					<span class="info">
-						Therapy application last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+						Anaesthetic Satisfaction Audit  last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
 					</span>
 				</div>
 			</div>
