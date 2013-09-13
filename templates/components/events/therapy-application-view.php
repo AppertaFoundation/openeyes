406a8b4b58b18514e674cfea852e30573393c17d
Richard Willis <willis.rh@gmail.com>
2013-09-13 10:25:16 +0100

    Added data to therapy application view template

--- templates/components/events/therapy-application-view.php
+++ templates/components/events/therapy-application-view.php
@@ -1,4 +1,4 @@
-<div class="large-10 column event view intravitreal-injection">
+<div class="large-10 column event view therapy-application">
 	<header class="event-header">
 		<ul class="inline-list tabs event-actions">
 			<li class="selected">
@@ -10,7 +10,10 @@
 		</ul>
 		<div class="button-bar right">
 			<a href="#" class="button small">
-				Print
+				Preview Application
+			</a>
+			<a href="#" class="button small">
+				Submit Application
 			</a>
 		</div>
 	</header>
@@ -20,14 +23,127 @@
 		<div class="row">
 			<div class="large-12 column">
 
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Diagnosis</h3>
+					</header>
+					<div class="element-data element-eyes row">
+						<div class="element-eye left-eye column">
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="field-label">Diagnosis:</div>
+								</div>
+								<div class="large-8 column">
+									<div class="field-value">Choroidal retinal neovascularisation</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="field-label">Secondary To:</div>
+								</div>
+								<div class="large-8 column">
+									<div class="field-value">Age related macular degeneration</div>
+								</div>
+							</div>
+						</div>
+						<div class="element-eye right-eye column">
+							<div class="field-value">Not recorded</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Patient Suitability</h3>
+					</header>
+					<div class="element-data element-eyes row">
+						<div class="element-eye left-eye column">
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="field-label">Treatment:</div>
+								</div>
+								<div class="large-8 column">
+									<div class="field-value">Avastin</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="field-label">Angiogram Baseline Date:</div>
+								</div>
+								<div class="large-8 column">
+									<div class="field-value">13 Sep 2013</div>
+								</div>
+							</div>
+							<div class="row element-data-row">
+								<div class="large-4 column">
+									<div class="field-label">NICE Compliance:</div>
+								</div>
+								<div class="large-8 column">
+									<div class="field-value">No</div>
+								</div>
+							</div>
+						</div>
+						<div class="element-eye right-eye column">
+							<div class="field-value">Not recorded</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Relative ContraIndications</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Cerebrovascular accident:</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">Yes</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Ischaemic Attack:</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">Yes</div>
+							</div>
+						</div>
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Myocardial Infarction:</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">Yes</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">MR Service Information</h3>
+					</header>
+					<div class="element-data">
+						<div class="row element-data-row">
+							<div class="large-2 column">
+								<div class="field-label">Consultant:</div>
+							</div>
+							<div class="large-10 column">
+								<div class="field-value">Andrews Richard</div>
+							</div>
+						</div>
+					</div>
+				</section>
 
 
 				<div class="metadata">
 					<span class="info">
-						Examination created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+						Therapy application created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
 					</span>
 					<span class="info">
-						Examination created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+						Therapy application last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
 					</span>
 				</div>
 			</div>
