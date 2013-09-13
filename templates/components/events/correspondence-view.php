6e86503bce1dbedd6c11229197705053fbe456b6
Richard Willis <willis.rh@gmail.com>
2013-09-13 15:53:24 +0100

    Added base correspondence templates

---
+++ templates/components/events/correspondence-view.php
@@ -0,0 +1,30 @@
+<div class="large-10 column event view correspondence">
+	<header class="event-header">
+		<ul class="inline-list tabs event-actions">
+			<li class="selected">
+				<a href="#">View</a>
+			</li>
+			<li>
+				<a href="#">Edit</a>
+			</li>
+		</ul>
+	</header>
+	<div class="event-content">
+		<h2 class="event-title">Correspondence</h2>
+
+		<div class="row">
+			<div class="large-12 column">
+
+
+				<div class="metadata">
+					<span class="info">
+						Consent form created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+					</span>
+					<span class="info">
+						Consent form last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
