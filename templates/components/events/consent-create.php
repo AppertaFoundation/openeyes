dba41c69afaefc3c4354d0938273fa51e313293d
Richard Willis <willis.rh@gmail.com>
2013-09-13 15:07:04 +0100

    Renamed consent create to consent select booking, created consent create view

--- templates/components/events/consent-create.php
+++ templates/components/events/consent-create.php
@@ -12,10 +12,249 @@
 		</div>
 	</header>
 	<div class="event-content">
-		<h2 class="event-title">Anaesthetic Satisfaction Audit</h2>
+		<h2 class="event-title">Consent form</h2>
 
 		<div class="row">
 			<div class="large-12 column">
+
+				<!-- Type -->
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Type</h3>
+					</header>
+					<div class="element-fields">
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Type:</label>
+							</div>
+							<div class="large-10 column">
+								<select>
+									<option>-- Select --</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- Leaflets -->
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Leaflets</h3>
+					</header>
+					<div class="element-fields">
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Leaflets provided:</label>
+							</div>
+							<div class="large-10 column">
+								<select>
+									<option>-- Select --</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- Procedure -->
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Procedure</h3>
+					</header>
+					<div class="element-fields">
+						<fieldset class="row field-row">
+							<legend class="large-2 column">
+								Leaflets provided:
+							</legend>
+							<div class="large-10 column">
+								<label class="inline highlight">
+									<input type="radio" /> Right
+								</label>
+								<label class="inline highlight">
+									<input type="radio" /> Both
+								</label>
+								<label class="inline highlight">
+									<input type="radio" /> Left
+								</label>
+							</div>
+						</fieldset>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<div class="field-label">Procedures:</div>
+							</div>
+							<div class="large-4 column end">
+								<div class="panel element-field">
+									Punctoplasty - 3 snp
+								</div>
+							</div>
+						</div>
+						<fieldset class="row field-row">
+							<legend class="large-2 column">
+								Anaesthetic type:
+							</legend>
+							<div class="large-10 column">
+								<label class="inline highlight">
+									<input type="radio" /> Topical
+								</label>
+								<label class="inline highlight">
+									<input type="radio" /> LA
+								</label>
+								<label class="inline highlight">
+									<input type="radio" /> LAC
+								</label>
+								<label class="inline highlight">
+									<input type="radio" /> LAS
+								</label>
+								<label class="inline highlight">
+									<input type="radio" /> GA
+								</label>
+							</div>
+						</fieldset>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label>
+									Procedures:
+								</label>
+							</div>
+							<div class="large-4 column">
+								<div class="field-row">
+									<div class="field-info">
+										Any extra procedures which may become necessary during the procedure.
+									</div>
+								</div>
+								<fieldset>
+									<legend>Add a procedure:</legend>
+									<div class="field-row">
+										<select>
+											<option>Select a subsection</option>
+										</select>
+									</div>
+									<div class="field-row">
+										<input type="text" placeholder="or enter procedure here..." />
+									</div>
+								</fieldset>
+							</div>
+							<div class="large-6 column">
+								<div class="panel element-field">
+									<table class="plain">
+										<thead>
+											<tr>
+												<td>Procedure</td>
+												<td>Actions</td>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>Punctoplasty - 3 snp</td>
+												<td><a href="#">Remove</a></td>
+											</tr>
+											<tr>
+												<td>Blepharoplasty of lower lid - Bleph lower lid</td>
+												<td><a href="#">Remove</a></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- Benefits and risks -->
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Benefits and risks</h3>
+					</header>
+					<div class="element-fields">
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Benefits:</label>
+							</div>
+							<div class="large-10 column">
+								<textarea></textarea>
+							</div>
+						</div>
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Risks:</label>
+							</div>
+							<div class="large-10 column">
+								<textarea></textarea>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- Permissions for images -->
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Permissions for images</h3>
+					</header>
+					<div class="element-data">
+						<fieldset class="row field-row">
+							<legend class="large-2 column">
+								Agree for use in audit, education and publication:
+							</legend>
+							<div class="large-10 column">
+								<label class="inline highlight">
+									<input type="radio" />
+									Yes
+								</label>
+								<label class="inline highlight">
+									<input type="radio" />
+									No
+								</label>
+								<label class="inline highlight">
+									<input type="radio" />
+									Not applicable
+								</label>
+							</div>
+						</fieldset>
+					</div>
+				</section>
+
+				<!-- Other -->
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Other</h3>
+					</header>
+					<div class="element-fields">
+						<div class="row field-row">
+							<div class="large-2 column">
+								<label for="">Consultant</label>
+							</div>
+							<div class="large-6 column end">
+								<div class="field-row">
+									<input type="text" placeholder="Type to search for users..." />
+								</div>
+								<div class="field-row">
+									<input readonly="readonly" type="text" value="Mr Yassir Abou-Rayyah FRCS FRCOphth" />
+								</div>
+							</div>
+						</div>
+						<fieldset class="row field-row">
+							<legend class="large-2 column">
+								Other information:
+							</legend>
+							<div class="large-10 column">
+								<label class="field-row">
+									<input type="checkbox"> An information leaflet has been provided
+								</label>
+								<label class="field-row">
+									<input type="checkbox"> Anaesthetic leaflet has been provided
+								</label>
+								<label class="field-row">
+									<input type="checkbox"> Witness required
+								</label>
+								<label class="field-row">
+									<input type="checkbox"> Interpreter required
+								</label>
+								<label class="field-row">
+									<input type="checkbox"> Include supplementary consent form
+								</label>
+							</div>
+						</fieldset>
+					</div>
+				</section>
 
 			</div>
 		</div>
---
+++ templates/components/events/consent-select-booking.php
@@ -0,0 +1,71 @@
+<div class="large-10 column event consent edit">
+	<header class="event-header">
+		<ul class="inline-list tabs event-actions">
+			<li class="selected">
+				<a href="#">Select a booking</a>
+			</li>
+		</ul>
+		<div class="button-bar right">
+			<a href="#" class="button small secondary">
+				Create consent form
+			</a>
+		</div>
+	</header>
+	<div class="event-content">
+		<h2 class="event-title">Consent</h2>
+
+		<div class="row">
+			<div class="large-12 column">
+
+				<section class="element">
+					<header class="element-header">
+						<h3 class="element-title">Create Consent Form</h3>
+					</header>
+					<div class="element-fields">
+						<div class="field-row">
+							<div class="field-info">
+								Please indicate whether this Consent Form is for an existing booking or for unbooked procedures:
+							</div>
+						</div>
+						<fieldset class="row field-row">
+							<legend class="large-2 column">
+								Select:
+							</legend>
+							<div class="large-5 column end">
+								<label class="highlight">
+									<span class="row">
+										<span class="large-1 column">
+											<input type="radio" />
+										</span>
+										<span class="large-1 column">
+											<img src="/module_assets/operation-booking/img/new/small.png" alt="op" width="19" height="19" />
+										</span>
+										<span class="large-3 column">
+											19 Sep 2013
+										</span>
+										<span class="large-3 column">
+											Operation
+										</span>
+										<span class="large-4 column">
+											Punctoplasty
+										</span>
+									</span>
+								</label>
+								<label class="highlight">
+									<span class="row">
+										<span class="large-1 column">
+											<input type="radio" />
+										</span>
+										<span class="large-11 column">
+											Unbooked procedures
+										</span>
+									</span>
+								</label>
+							</div>
+						</fieldset>
+					</div>
+				</section>
+			</div>
+		</div>
+	</div>
+</div>
