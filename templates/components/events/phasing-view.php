c786b244be11f765b1fb1d7cfa4321e3ff703169
Richard Willis <willis.rh@gmail.com>
2013-09-16 11:43:35 +0100

    Added phasing view template

--- templates/components/events/phasing-view.php
+++ templates/components/events/phasing-view.php
@@ -15,13 +15,65 @@
 		<div class="row">
 			<div class="large-12 column">
 
+				<section class="element">
+					<div class="element-data element-eyes row">
+						<div class="element-eye right-eye column">
+							<div class="element-data-row">
+								<div class="field-value">
+									Tono-pen (dilated)
+								</div>
+							</div>
+							<div class="element-data-row">
+								<table class="blank">
+									<tbody>
+										<tr>
+											<td>9:16am - </td>
+											<td>23 mm Hg</td>
+										</tr>
+										<tr>
+											<td>9:16am - </td>
+											<td>23 mm Hg</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+							<div class="element-data-row">
+								<div class="field-value">
+									(sdfdsfsdf3 sdfdsf)
+								</div>
+							</div>
+						</div>
+						<div class="element-eye left-eye column">
+							<div class="element-data-row">
+								<div class="field-value">
+									Other
+								</div>
+							</div>
+							<div class="element-data-row">
+								<table class="blank">
+									<tbody>
+										<tr>
+											<td>9:16am - </td>
+											<td>32432 mm Hg</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+							<div class="element-data-row">
+								<div class="field-value">
+									(sdfdsfsdf)
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
 
 				<div class="metadata">
 					<span class="info">
-						Consent form created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+						Phasing created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
 					</span>
 					<span class="info">
-						Consent form last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+						Phasing last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
 					</span>
 				</div>
 			</div>
