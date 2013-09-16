030239697dbc0d3bfaff206c20ca782702823d26
Richard Willis <willis.rh@gmail.com>
2013-09-16 11:55:49 +0100

    Added base templates for prescription module

---
+++ templates/components/events/prescription-view.php
@@ -0,0 +1,30 @@
+<div class="large-10 column event view phasing">
+	<header class="event-header">
+		<ul class="inline-list tabs event-actions">
+			<li class="selected">
+				<a href="#">View</a>
+			</li>
+			<li>
+				<a href="#">Edit</a>
+			</li>
+		</ul>
+	</header>
+	<div class="event-content">
+		<h2 class="event-title">Prescription</h2>
+
+		<div class="row">
+			<div class="large-12 column">
+
+
+				<div class="metadata">
+					<span class="info">
+						Prescription created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+					</span>
+					<span class="info">
+						Prescription last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
