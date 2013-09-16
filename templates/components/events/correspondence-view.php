a0f73857e915051b33ca0db685ee5ed120753f8e
Richard Willis <willis.rh@gmail.com>
2013-09-16 09:48:53 +0100

    Added markup for correspondence view (letter layout)

--- templates/components/events/correspondence-view.php
+++ templates/components/events/correspondence-view.php
@@ -15,6 +15,73 @@
 		<div class="row">
 			<div class="large-12 column">
 
+				<!-- Letter begin -->
+				<div class="correspondence-letter draft">
+					<header>
+						<div class="banner clearfix">
+							<div class="seal">
+								<img src="/img/_print/letterhead_seal.jpg" alt="letterhead_seal">
+							</div>
+							<div class="logo">
+								<img src="/img/_print/letterhead_Moorfields_NHS.jpg" alt="letterhead_Moorfields_NHS">
+							</div>
+						</div>
+						<div class="from-address">
+							Moorfields at City Road
+							<br />162 City Road
+							<br />London
+							<br />EC1V 2PD
+							<br />Tel: 020 7253 3411
+							<br />Direct line: 34234324
+							<div class="date">16 Sep 2013 (clinic date 3 Sep 2013)</div>
+						</div>
+						<div class="to-address">Dr A Who
+							<br />83 Wintour Lane
+							<br />Northop
+							<br />Heald Green
+							<br />Lothian
+							<br />GH14 2DH
+						</div>
+						<br /><br />
+					</header>
+
+					<div class="reply-address">
+					</div>
+
+					<p class="accessible">
+						Dear Dr Who,
+					</p>
+					<p class="accessible"><strong>Re: Ellen Kendall, 68, Pink Lane, Larbert, IQ68 5SD<br />
+						DOB: 23 Jan 1937, Hosp No: 1000440, NHS No: 973 475 9943</strong>
+					</p>
+
+					<p class="accessible">
+						History: Difficulty with reading,
+						<br />Clinical management: . Visual acuity:
+						<br />left: 6/3 Unaided
+						<br />right: 6/3 Unaided
+					</p>
+					<br />
+					<p class="accessible" nobr="true">
+						Yours sincerely
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />Mr Bill Aylward FRCS FRCOphth MD
+						<br />admin role
+						<br />Consultant: Mr Yassir Abou-Rayyah
+					</p>
+
+					<p nobr="true">
+						To:
+						Dr A Who, 83 Wintour Lane, Northop, Heald Green, Lothian, GH14 2DH
+						<br />CC: Patient: Mrs Ellen Kendall, 68 Pink Lane, Larbert, Essex, IQ68 5SD
+						<br />Enc: sdfsd
+						<br />Enc: sdfdsf
+					</p>
+
+				</div>
 
 				<div class="metadata">
 					<span class="info">
