827959e582d97d06198cf44501977861e1368a9e
Richard Willis <willis.rh@gmail.com>
2013-09-16 11:37:15 +0100

    Added phasing create view

--- templates/components/events/phasing-create.php
+++ templates/components/events/phasing-create.php
@@ -17,7 +17,148 @@
 		<div class="row">
 			<div class="large-12 column">
 
-
+				<section class="element">
+					<div class="element-fields element-eyes row">
+						<div class="element-eye right-eye column">
+							<a href="#" class="icon-remove-side">Remove side</a>
+							<div class="row field-row">
+								<div class="large-2 column">
+									<label for="">Instrument:</label>
+								</div>
+								<div class="large-10 column">
+									<select>
+										<option>-- Select --</option>
+									</select>
+								</div>
+							</div>
+							<fieldset class="row field-row">
+								<legend class="large-2 column">
+									Dilated:
+								</legend>
+								<div class="large-10 column">
+									<label class="inline highlight">
+										<input type="radio" />
+										Yes
+									</label>
+									<label class="inline highlight">
+										<input type="radio" />
+										No
+									</label>
+								</div>
+							</fieldset>
+							<fieldset class="row field-row">
+								<legend class="large-2 column">
+									Readings:
+								</legend>
+								<div class="large-10 column">
+									<table class="blank">
+										<thead>
+											<tr>
+												<th>Time (HH:MM)</th>
+												<th>mm Hg</th>
+												<th><div class="hide-offscreen">Actions</div></th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td><input type="text" class="small" /></td>
+												<td><input type="text" class="small"></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="small" /></td>
+												<td><input type="text" class="small"></td>
+												<td><a href="#">Remove</a></td>
+											</tr>
+										</tbody>
+										<tfoot>
+											<tr>
+												<td colspan="3"><button class="secondary small">Add</button></td>
+											</tr>
+										</tfoot>
+									</table>
+								</div>
+							</fieldset>
+							<div class="row field-row">
+								<div class="large-2 column">
+									<label for="">Comments:</label>
+								</div>
+								<div class="large-10 column">
+									<textarea placeholder="Enter comments..."></textarea>
+								</div>
+							</div>
+						</div>
+						<div class="element-eye left-eye column">
+							<a href="#" class="icon-remove-side">Remove side</a>
+							<div class="row field-row">
+								<div class="large-2 column">
+									<label for="">Instrument:</label>
+								</div>
+								<div class="large-10 column">
+									<select>
+										<option>-- Select --</option>
+									</select>
+								</div>
+							</div>
+							<fieldset class="row field-row">
+								<legend class="large-2 column">
+									Dilated:
+								</legend>
+								<div class="large-10 column">
+									<label class="inline highlight">
+										<input type="radio" />
+										Yes
+									</label>
+									<label class="inline highlight">
+										<input type="radio" />
+										No
+									</label>
+								</div>
+							</fieldset>
+							<fieldset class="row field-row">
+								<legend class="large-2 column">
+									Readings:
+								</legend>
+								<div class="large-10 column">
+									<table class="blank">
+										<thead>
+											<tr>
+												<th>Time (HH:MM)</th>
+												<th>mm Hg</th>
+												<th><div class="hide-offscreen">Actions</div></th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td><input type="text" class="small" /></td>
+												<td><input type="text" class="small"></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="small" /></td>
+												<td><input type="text" class="small"></td>
+												<td><a href="#">Remove</a></td>
+											</tr>
+										</tbody>
+										<tfoot>
+											<tr>
+												<td colspan="3"><button class="secondary small">Add</button></td>
+											</tr>
+										</tfoot>
+									</table>
+								</div>
+							</fieldset>
+							<div class="row field-row">
+								<div class="large-2 column">
+									<label for="">Comments:</label>
+								</div>
+								<div class="large-10 column">
+									<textarea placeholder="Enter comments..."></textarea>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
 
 			</div>
 		</div>
