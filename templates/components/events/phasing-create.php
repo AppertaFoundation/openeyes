306850bba26116a920f8089a93f93a9e37e3d02b
Richard Willis <willis.rh@gmail.com>
2013-09-16 10:04:55 +0100

    Added base phasing templates

---
+++ templates/components/events/phasing-create.php
@@ -0,0 +1,25 @@
+<div class="large-10 column event phasing edit">
+	<header class="event-header">
+		<ul class="inline-list tabs event-actions">
+			<li class="selected">
+				<a href="#">Create</a>
+			</li>
+		</ul>
+		<div class="button-bar right">
+			<a href="#" class="button small secondary">
+				Save
+			</a>
+		</div>
+	</header>
+	<div class="event-content">
+		<h2 class="event-title">Phasing</h2>
+
+		<div class="row">
+			<div class="large-12 column">
+
+
+
+			</div>
+		</div>
+	</div>
+</div>
---
+++ templates/phasing-view.php
@@ -0,0 +1,26 @@
+<? include 'components/common.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<? include 'components/head.php'; ?>
+<link rel="stylesheet" href="/module_assets/phasing/css/module_new.css" />
+</head>
+<body>
+	<div class="container main" role="main">
+
+		<? include 'components/header-logged-in.php'; ?>
+
+		<div class="container content">
+			<h1 class="badge">Episodes and events</h1>
+
+			<div class="box content">
+				<div class="row">
+					<? include 'components/events/sidebar.php'; ?>
+					<? include 'components/events/phasing-view.php'; ?>
+				</div>
+			</div>
+		</div>
+		<? include 'components/footer.php'; ?>
+	</div>
+</body>
+</html>
