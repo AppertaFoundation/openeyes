ecf805e708eae85ea20094f0bfae68bbebde2db0
Richard Willis <willis.rh@gmail.com>
2013-09-17 10:08:55 +0100

    Added operation note select booking template

---
+++ templates/components/events/operation-note-select-booking.php
@@ -0,0 +1,73 @@
+<div class="large-10 column event phasing edit">
+	<header class="event-header">
+		<ul class="inline-list tabs event-actions">
+			<li class="selected">
+				<a href="#">Select a booking</a>
+			</li>
+		</ul>
+		<div class="button-bar right">
+			<a href="#" class="button small secondary">
+				Create Operation Note
+			</a>
+		</div>
+	</header>
+	<div class="event-content">
+		<h2 class="event-title">Operation note</h2>
+
+			<div class="row">
+				<div class="large-12 column">
+
+					<section class="element">
+						<header class="element-header">
+							<h3 class="element-title">Create Operation note</h3>
+						</header>
+						<div class="element-fields">
+							<div class="field-row">
+								<div class="field-value">
+									Please indicate whether this operation note relates to a booking or an unbooked emergency:
+								</div>
+							</div>
+							<fieldset class="row field-row">
+								<legend class="large-2 column">
+									Select:
+								</legend>
+								<div class="large-5 column end">
+									<label class="highlight booking">
+										<span class="row">
+											<span class="large-1 column">
+												<input type="radio" />
+											</span>
+											<span class="large-1 column">
+												<img src="/module_assets/operation-booking/img/new/small.png" />
+											</span>
+											<span class="large-3 column">
+												19 Sep 2013
+											</span>
+											<span class="large-3 column">
+												Operation
+											</span>
+											<span class="large-4 column">
+												Punctoplasty
+											</span>
+										</span>
+									</label>
+									<label class="highlight booking">
+										<span class="row">
+											<span class="large-1 column">
+												<input type="radio" />
+											</span>
+											<span class="large-11 column">
+												Emergency
+											</span>
+										</span>
+									</label>
+								</div>
+							</fieldset>
+						</div>
+					</section>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
