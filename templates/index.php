f59989baef291638741bf7046b95afe5de6a0247
Richard Willis <willis.rh@gmail.com>
2013-09-17 15:32:25 +0100

    Added remaining templates to index

--- templates/index.php
+++ templates/index.php
@@ -4,31 +4,28 @@
 <head>
 <? include 'components/head.php'; ?>
 <style type="text/css">
-.complete {
-	color: green;
-}
-table {
-	background: #e1eef9
-}
-table.grid tr td {
-	border: 0;
-	border-top: 1px solid #ddd;
-}
-table.grid tr th {
-	border: 0;
-}
-table tr:nth-of-type(even) {
-	background: #e1eef9
-}
-tr.heading, tr.heading:nth-of-type(even) {
-	background: none;
-}
-tr.heading h3 {
-	font-size: inherit;
-	margin: 0;
-	padding: 0;
-	font-weight: bold;
-}
+	.complete {
+		color: green;
+		white-space: nowrap;
+	}
+	.complete:before {
+		content: "✓ ";
+	}
+	table {
+		background: #e1eef9
+	}
+	table tr td {
+		border-top: 1px solid #ddd;
+	}
+	table tr:nth-of-type(even) {
+		background: #e1eef9
+	}
+	tr.heading h3 {
+		font-size: inherit;
+		margin: 0;
+		padding: 0;
+		font-weight: bold;
+	}
 </style>
 </head>
 <body>
@@ -53,7 +50,7 @@ tr.heading h3 {
 
 								<p>Select the template you'd like to view:</p>
 
-								<table class="grid">
+								<table class="">
 									<thead>
 										<tr>
 											<th>Template</th>
@@ -69,14 +66,21 @@ tr.heading h3 {
 											<td>
 												<a href="login.php">Home login</a>
 											</td>
-											<td>The main login page.</td>
+											<td>The main login template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="login-validation.php">Home login validation</a>
+											</td>
+											<td>The main login template with validation errors.</td>
 											<td><span class="complete">Complete</span></td>
 										</tr>
 										<tr>
 											<td>
 												<a href="search.php">Home search (logged in)</a>
 											</td>
-											<td>The main search page.</td>
+											<td>The main search template.</td>
 											<td><span class="complete">Complete</span></td>
 										</tr>
 										<tr class="heading">
@@ -96,7 +100,7 @@ tr.heading h3 {
 											<td>
 												<a href="theatre-diaries.php">Theatre diaries</a>
 											</td>
-											<td>The main theatre diaries page.</td>
+											<td>The main theatre diaries template.</td>
 											<td><span class="complete">Complete</span></td>
 										</tr>
 										<tr class="heading">
@@ -106,7 +110,7 @@ tr.heading h3 {
 											<td>
 												<a href="waiting-list.php">Waiting list</a>
 											</td>
-											<td>The main waiting list page.</td>
+											<td>The main waiting list template.</td>
 											<td><span class="complete">Complete</span></td>
 										</tr>
 										<tr class="heading">
@@ -130,14 +134,154 @@ tr.heading h3 {
 											<td>
 												<a href="operation-booking-edit.php">Operation booking edit</a>
 											</td>
-											<td>The operation booking edit page.</td>
+											<td>The operation booking edit template - shows an example of adding and listing procedures.</td>
 											<td><span class="complete">Complete</span></td>
 										</tr>
 										<tr>
 											<td>
 												<a href="operation-booking-view.php">Operation booking view</a>
 											</td>
-											<td>The operation booking view page.</td>
+											<td>The operation booking view template - shows an example of highlighting element fields.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="consent-select-booking.php">Consent select booking</a>
+											</td>
+											<td>The consent select booking template - shows an example of selecting a booking prior to creating.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="consent-create.php">Consent create</a>
+											</td>
+											<td>The consent create template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="consent-view.php">Consent view</a>
+											</td>
+											<td>The consent view template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="anaesthetic-satisfaction-audit-create.php">Anaesthetic satisfaction audit create</a>
+											</td>
+											<td>The anaesthetic satisfaction audit create template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="anaesthetic-satisfaction-audit-view.php">Anaesthetic satisfaction audit view</a>
+											</td>
+											<td>The anaesthetic satisfaction audit view template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="correspondence-create.php">Correspondence create</a>
+											</td>
+											<td>The correspondence create template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="correspondence-view.php">Correspondence view</a>
+											</td>
+											<td>The correspondence view template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="intravitreal-injection-create.php">Intravitreal injection create</a>
+											</td>
+											<td>The intravitreal injection create template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="intravitreal-injection-view.php">Intravitreal injection view</a>
+											</td>
+											<td>The intravitreal injection view template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="laser-create.php">Laser create</a>
+											</td>
+											<td>The laser create template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="laser-view.php">Laser view</a>
+											</td>
+											<td>The laser view template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="operation-note-select-booking.php">Operation note select booking</a>
+											</td>
+											<td>The operation note select booking template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="operation-note-create.php">Operation note create</a>
+											</td>
+											<td>The operation note create template - shows an example of displaying "on-demand" elements.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="operation-note-view.php">Operation note view</a>
+											</td>
+											<td>The operation note view template - shows an example of highlighting element fields.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="phasing-create.php">Phasing create</a>
+											</td>
+											<td>The phasing create template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="phasing-view.php">Phasing view</a>
+											</td>
+											<td>The phasing view template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="prescription-create.php">Prescription create</a>
+											</td>
+											<td>The prescription create template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="prescription-view.php">Prescription view</a>
+											</td>
+											<td>The prescription view template.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="therapy-application-create.php">Therapy application create</a>
+											</td>
+											<td>The therapy application create template - shows an example of sub-form panels.</td>
+											<td><span class="complete">Complete</span></td>
+										</tr>
+										<tr>
+											<td>
+												<a href="therapy-application-view.php">Therapy application view</a>
+											</td>
+											<td>The therapy application view template.</td>
 											<td><span class="complete">Complete</span></td>
 										</tr>
 									</tbody>
