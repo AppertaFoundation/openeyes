68382cd6133a6987eeeb9367c75aef9b6f4e3b38
Richard Willis <willis.rh@gmail.com>
2013-09-17 09:31:52 +0100

    Added initial operation note templates

---
+++ templates/components/events/operation-note-view.php
@@ -0,0 +1,29 @@
+<div class="large-10 column event view phasing">
+	<header class="event-header">
+		<ul class="inline-list tabs event-actions">
+			<li class="selected">
+				<a href="#">View</a>
+			</li>
+			<li>
+				<a href="#">Edit</a>
+			</li>
+		</ul>
+	</header>
+	<div class="event-content">
+		<h2 class="event-title">Operation note</h2>
+
+		<div class="row">
+			<div class="large-12 column">
+
+				<div class="metadata">
+					<span class="info">
+						Operation note created by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+					</span>
+					<span class="info">
+						Operation note last modified by <span class="user">Enoch Root</span> on 1 Jan 2000 at 00:00
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
