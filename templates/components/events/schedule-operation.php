6ce9855988e7687da48a2b7c61bef75a6ef7345d
Richard Willis <willis.rh@gmail.com>
2013-09-19 09:10:17 +0100

    Schedule operation data

--- templates/components/events/schedule-operation.php
+++ templates/components/events/schedule-operation.php
@@ -1,4 +1,9 @@
-<div class="large-10 column event view consent">
-
-	<p>Schedule operation</p>
+<div class="large-10 column event view booking schedule">
+	<div class="event-content">
+		<div class="panel patient-reminder">
+			<span class="surname">KENDALL</span>, Ellen (1000440)
+		</div>
+		<h3 class="event-title">Schedule Operation</h3>
+		<div class="alert-box alert with-icon">You are booking into the Emergency list</div>
+	</div>
 </div>
