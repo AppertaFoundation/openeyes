0f6c007a6c4f658dc221ca3a12769deedbadecfc
Richard Willis <willis.rh@gmail.com>
2013-09-23 13:42:00 +0100

    [OE-3495] New search results template

---
+++ templates/search-results.php
@@ -0,0 +1,133 @@
+<? include 'components/common.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<? include 'components/head.php'; ?>
+</head>
+<body>
+	<div class="container main" role="main">
+
+		<? include 'components/header-logged-in-no-patient.php'; ?>
+
+		<div class="container content">
+
+			<h1 class="badge">Search</h1>
+
+			<div class="row">
+				<div class="large-9 column">
+					<div class="box generic">
+						<p>
+							<strong>50 patients found</strong>,
+							based on FIRST NAME: <strong>"violet"</strong>
+						</p>
+					</div>
+					<div class="box generic">
+						<h2>Results. You are viewing patients 1 - 20 of 50</h2>
+						<table class="grid">
+							<thead>
+								<tr>
+									<th><a href="#">Hospital Number</a></th>
+									<th><a href="#">Title</a></th>
+									<th><a href="#">First name</a></th>
+									<th><a href="#">Last name</a></th>
+									<th><a href="#">Date of birth</a></th>
+									<th><a href="#">Gender</a></th>
+									<th><a href="#">NHS number</a></th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr class="clickable">
+									<td>1000005</td>
+									<td>Mrs</td>
+									<td>Violet</td>
+									<td>Hodgkinson</td>
+									<td>1958-01-10</td>
+									<td>F</td>
+									<td>853 986 5548</td>
+								</tr>
+								<tr class="clickable">
+									<td>1000005</td>
+									<td>Mrs</td>
+									<td>Violet</td>
+									<td>Hodgkinson</td>
+									<td>1958-01-10</td>
+									<td>F</td>
+									<td>853 986 5548</td>
+								</tr>
+								<tr class="clickable">
+									<td>1000005</td>
+									<td>Mrs</td>
+									<td>Violet</td>
+									<td>Hodgkinson</td>
+									<td>1958-01-10</td>
+									<td>F</td>
+									<td>853 986 5548</td>
+								</tr>
+								<tr class="clickable">
+									<td>1000005</td>
+									<td>Mrs</td>
+									<td>Violet</td>
+									<td>Hodgkinson</td>
+									<td>1958-01-10</td>
+									<td>F</td>
+									<td>853 986 5548</td>
+								</tr>
+								<tr class="clickable">
+									<td>1000005</td>
+									<td>Mrs</td>
+									<td>Violet</td>
+									<td>Hodgkinson</td>
+									<td>1958-01-10</td>
+									<td>F</td>
+									<td>853 986 5548</td>
+								</tr>
+								<tr class="clickable">
+									<td>1000005</td>
+									<td>Mrs</td>
+									<td>Violet</td>
+									<td>Hodgkinson</td>
+									<td>1958-01-10</td>
+									<td>F</td>
+									<td>853 986 5548</td>
+								</tr>
+								<tr class="clickable">
+									<td>1000005</td>
+									<td>Mrs</td>
+									<td>Violet</td>
+									<td>Hodgkinson</td>
+									<td>1958-01-10</td>
+									<td>F</td>
+									<td>853 986 5548</td>
+								</tr>
+							</tbody>
+							<tfoot class="pagination-container">
+								<tr>
+									<td colspan="7">
+										<div class="clearfix">
+											<ul class="pagination right">
+												<li class="label">Viewing patients:</li>
+												<li class="current"><a href="#">1-20</a></li>
+												<li><a href="#">21-40</a></li>
+												<li><a href="#">41-50</a></li>
+											</ul>
+										</div>
+									</td>
+								</tr>
+							</tfoot>
+						</table>
+					</div>
+				</div>
+				<div class="large-3 column">
+					<div class="box generic">
+						<p>
+							<a href="#">Clear this search and <span class="highlight">start a new search</span>.</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<? include 'components/footer.php'; ?>
+	</div>
+</body>
+</html>
