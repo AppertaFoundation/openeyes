9a97efbc90ca20069a1341c074bda7c832dcd35c
Richard Willis <willis.rh@gmail.com>
2013-09-24 10:45:53 +0100

    Consistant column classes

--- themes/rewrite/views/layouts/main.php
+++ themes/rewrite/views/layouts/main.php
@@ -32,19 +32,19 @@
 		<header class="header row">
 
 			<!-- Branding (logo) -->
-			<div class="large-2 columns">
+			<div class="large-2 column">
 				<?php $this->renderPartial('//base/_brand'); ?>
 			</div>
 
 			<!-- Patient panel -->
-			<div class="large-4 medium-5 columns">
+			<div class="large-4 medium-5 column">
 				<?php if ($this->renderPatientPanel === true) {
 					$this->renderPartial('//patient/_patient_id');
 				}?>
 			</div>
 
 			<!-- User panel (with site navigation) -->
-			<div class="large-6 medium-7 columns">
+			<div class="large-6 medium-7 column">
 				<?php $this->renderPartial('//base/_form'); ?>
 			</div>
 		</header><!-- /.header -->
