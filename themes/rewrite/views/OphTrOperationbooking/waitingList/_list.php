a76a836f9ecbe6d78145025ba6a5f334caf1e686
Adam Dyer <progen91@gmail.com>
2013-10-02 10:55:07 +0100

    [OE-3622] waiting list new markup

--- themes/rewrite/views/OphTrOperationbooking/waitingList/_list.php
+++ themes/rewrite/views/OphTrOperationbooking/waitingList/_list.php
@@ -17,148 +17,178 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
-<div id="pas_warnings" class="alertBox" style="display: none;">
-	<div class="no_gp" style="display: none;">One or more patients has no GP practice address, please correct in PAS before printing GP letter.</div>
-	<div class="no_address" style="display: none;">One or more patients has no Address, please correct in PAS before printing a letter for them.</div>
-</div>
-<div id="waitingList" class="grid-view">
-	<table class="waiting-list">
+<div class="panel">
+
+	<div id="pas_warnings" class="alertBox">
+		<ul>
+			<li>One or more patients has no GP practice address, please correct in PAS before printing GP letter.</li>
+			<li>One or more patients has no Address, please correct in PAS before printing a letter for them.</li>
+		</ul>
+		<a href="#" class="close">×</a>
+	</div>
+	<table class="grid waiting-list">
+		<thead>
+		<tr>
+			<th>Letters sent</th>
+			<th style="width: 120px;">Patient</th>
+			<th style="width: 53px;">Hospital number</th>
+			<th style="width: 95px;">Location</th>
+			<th>Procedure</th>
+			<th>Eye</th>
+			<th>Firm</th>
+			<th style="width: 80px;">Decision date</th>
+			<th>Priority</th>
+			<th>Book status (requires...)</th>
+			<th><input style="margin-top: 0.4em;" type="checkbox" id="checkall" value="" /> All</th>
+		</tr>
+		</thead>
 		<tbody>
+		<?php if (empty($operations)) {?>
 			<tr>
-				<th>Letters sent</th>
-				<th style="width: 120px;">Patient</th>
-				<th style="width: 53px;">Hospital number</th>
-				<th style="width: 95px;">Location</th>
-				<th>Procedure</th>
-				<th>Eye</th>
-				<th>Firm</th>
-				<th style="width: 80px;">Decision date</th>
-				<th>Priority</th>
-				<th>Book status (requires...)</th>
-				<th><input style="margin-top: 0.4em;" type="checkbox" id="checkall" value="" /> All</th>
+				<td>
+					There are no patients who match the specified criteria.
+				</td>
 			</tr>
-			<?php if (empty($operations)) {?>
+		<?php } else {?>
+			<?php
+			$i = 0;
+			foreach ($operations as $eo) {
+				$patient = $eo->event->episode->patient;
+				$contact = $patient->contact;
+				if (isset($_POST['status']) and $_POST['status'] != '') {
+					if ($eo->getNextLetter() != $_POST['status']) {
+						continue;
+					}
+				}?>
+
+				<?php if ($eo->getWaitingListStatus() == Element_OphTrOperationbooking_Operation::STATUS_PURPLE) {
+					$tablecolour = "Purple";
+				} elseif ($eo->getWaitingListStatus() == Element_OphTrOperationbooking_Operation::STATUS_GREEN1) {
+					$tablecolour = "Green";
+				} elseif ($eo->getWaitingListStatus() == Element_OphTrOperationbooking_Operation::STATUS_GREEN2) {
+					$tablecolour = "Green";
+				} elseif ($eo->getWaitingListStatus() == Element_OphTrOperationbooking_Operation::STATUS_ORANGE) {
+					$tablecolour = "Orange";
+				} elseif ($eo->getWaitingListStatus() == Element_OphTrOperationbooking_Operation::STATUS_RED) {
+					$tablecolour = "Red";
+				} else {
+					$tablecolour = "White";
+				}?>
+
+				<tr class="waitinglist <?php echo ($i % 2 == 0) ? 'Even' : 'Odd'; ?>">
+					<td class="letter-status <?php echo $tablecolour ?>">
+						<?php if ($eo->sentInvitation()) {?>
+							<img src="<?php echo $assetPath?>/img/letterIcons/invitation.png" alt="Invitation" width="17" height="17" />
+						<?php }?>
+						<?php if ($eo->sent1stReminder()) {?>
+							<img src="<?php echo $assetPath?>/img/letterIcons/letter1.png" alt="1st reminder" width="17" height="17" />
+						<?php }?>
+						<?php if ($eo->sent2ndReminder()) {?>
+							<img src="<?php echo $assetPath?>/img/letterIcons/letter2.png" alt="2nd reminder" width="17" height="17" />
+						<?php }?>
+						<?php if ($eo->sentGPLetter()) {?>
+							<img src="<?php echo $assetPath?>/img/letterIcons/GP.png" alt="GP" width="17" height="17" />
+						<?php }?>
+					</td>
+					<td class="patient">
+						<?php echo CHtml::link("<strong>" . trim(strtoupper($contact->last_name)) . '</strong>, ' . $contact->first_name, Yii::app()->createUrl('/OphTrOperationbooking/default/view/'.$eo->event_id))?>
+					</td>
+					<td><?php echo $patient->hos_num ?></td>
+					<td><?php echo $eo->site->short_name?></td>
+					<td><?php echo $eo->getProceduresCommaSeparated('short_format') ?></td>
+					<td><?php echo $eo->eye->name ?></td>
+					<td><?php echo $eo->event->episode->firm->name ?> (<?php echo $eo->event->episode->firm->serviceSubspecialtyAssignment->subspecialty->name?>)</td>
+					<td><?php echo $eo->NHSDate('decision_date') ?></td>
+					<td><?php echo $eo->priority->name?></td>
+					<td><?php echo ucfirst(preg_replace('/^Requires /','',$eo->status->name)) ?></td>
+					<td<?php if ($tablecolour == 'White' && Yii::app()->user->checkAccess('admin')) { ?> class="admin-td"<?php } ?>>
+
+						<?php if (($patient && $patient->contact->correspondAddress)
+							&& $eo->id
+							&& ($eo->getDueLetter() != Element_OphTrOperationbooking_Operation::LETTER_GP
+								|| ($eo->getDueLetter() == Element_OphTrOperationbooking_Operation::LETTER_GP && $patient->practice && $patient->practice->contact->address)
+							)) {?>
+							<div>
+								<input<?php if ($tablecolour == 'White' && !Yii::app()->user->checkAccess('admin')) { ?> disabled="disabled"<?php } ?> type="checkbox" id="operation<?php echo $eo->id ?>" value="1" />
+							</div>
+						<?php }?>
+
+						<?php if (!$patient->practice || !$patient->practice->contact->address) { ?>
+							<script type="text/javascript">
+								$('#pas_warnings').show();
+								$('#pas_warnings .no_gp').show();
+							</script>
+							<span class="no-GP">No GP</span>
+						<?php }?>
+
+						<?php if ($patient && !$patient->contact->correspondAddress) { ?>
+							<script type="text/javascript">
+								$('#pas_warnings').show();
+								$('#pas_warnings .no_address').show();
+							</script>
+							<span class="no-Address">No Address</span>
+						<?php }?>
+					</td>
+				</tr>
+				<?php
+				$i++;
+			}
+
+			if ($i == 0) {?>
 				<tr>
 					<td colspan="7" style="border: none; padding-top: 10px;">
 						There are no patients who match the specified criteria.
 					</td>
 				</tr>
-			<?php } else {?>
-				<?php
-				$i = 0;
-				foreach ($operations as $eo) {
-					$patient = $eo->event->episode->patient;
-					$contact = $patient->contact;
-					if (isset($_POST['status']) and $_POST['status'] != '') {
-						if ($eo->getNextLetter() != $_POST['status']) {
-							continue;
-						}
-					}?>
-
-					<?php if ($eo->getWaitingListStatus() == Element_OphTrOperationbooking_Operation::STATUS_PURPLE) {
-						$tablecolour = "Purple";
-					} elseif ($eo->getWaitingListStatus() == Element_OphTrOperationbooking_Operation::STATUS_GREEN1) {
-						$tablecolour = "Green";
-					} elseif ($eo->getWaitingListStatus() == Element_OphTrOperationbooking_Operation::STATUS_GREEN2) {
-						$tablecolour = "Green";
-					} elseif ($eo->getWaitingListStatus() == Element_OphTrOperationbooking_Operation::STATUS_ORANGE) {
-						$tablecolour = "Orange";
-					} elseif ($eo->getWaitingListStatus() == Element_OphTrOperationbooking_Operation::STATUS_RED) {
-						$tablecolour = "Red";
-					} else {
-						$tablecolour = "White";
-					}?>
-					<tr class="waitinglist<?php echo ($i % 2 == 0) ? 'Even' : 'Odd'; ?>">
-						<td class="letterStatus waitinglist<?php echo $tablecolour ?>">
-							<?php if ($eo->sentInvitation()) {?>
-								<img src="<?php echo $assetPath?>/img/letterIcons/invitation.png" alt="Invitation" width="17" height="17" />
-							<?php }?>
-							<?php if ($eo->sent1stReminder()) {?>
-								<img src="<?php echo $assetPath?>/img/letterIcons/letter1.png" alt="1st reminder" width="17" height="17" />
-							<?php }?>
-							<?php if ($eo->sent2ndReminder()) {?>
-								<img src="<?php echo $assetPath?>/img/letterIcons/letter2.png" alt="2nd reminder" width="17" height="17" />
-							<?php }?>
-							<?php if ($eo->sentGPLetter()) {?>
-								<img src="<?php echo $assetPath?>/img/letterIcons/GP.png" alt="GP" width="17" height="17" />
-							<?php }?>
-						</td>
-						<td class="patient">
-							<?php echo CHtml::link("<strong>" . trim(strtoupper($contact->last_name)) . '</strong>, ' . $contact->first_name, Yii::app()->createUrl('/OphTrOperationbooking/default/view/'.$eo->event_id))?>
-						</td>
-						<td><?php echo $patient->hos_num ?></td>
-						<td><?php echo $eo->site->short_name?></td>
-						<td><?php echo $eo->getProceduresCommaSeparated('short_format') ?></td>
-						<td><?php echo $eo->eye->name ?></td>
-						<td><?php echo $eo->event->episode->firm->name ?> (<?php echo $eo->event->episode->firm->serviceSubspecialtyAssignment->subspecialty->name?>)</td>
-						<td><?php echo $eo->NHSDate('decision_date') ?></td>
-						<td><?php echo $eo->priority->name?></td>
-						<td><?php echo ucfirst(preg_replace('/^Requires /','',$eo->status->name)) ?></td>
-						<td<?php if ($tablecolour == 'White' && Yii::app()->user->checkAccess('admin')) { ?> class="admin-td"<?php } ?>>
-
-							<?php if (($patient && $patient->contact->correspondAddress)
-								&& $eo->id
-								&& ($eo->getDueLetter() != Element_OphTrOperationbooking_Operation::LETTER_GP
-									|| ($eo->getDueLetter() == Element_OphTrOperationbooking_Operation::LETTER_GP && $patient->practice && $patient->practice->contact->address)
-								)) {?>
-							<div>
-								<input<?php if ($tablecolour == 'White' && !Yii::app()->user->checkAccess('admin')) { ?> disabled="disabled"<?php } ?> type="checkbox" id="operation<?php echo $eo->id ?>" value="1" />
-							</div>
-							<?php }?>
-
-							<?php if (!$patient->practice || !$patient->practice->contact->address) { ?>
-								<script type="text/javascript">
-									$('#pas_warnings').show();
-									$('#pas_warnings .no_gp').show();
-								</script>
-								<span class="no-GP">No GP</span>
-							<?php }?>
-
-							<?php if ($patient && !$patient->contact->correspondAddress) { ?>
-								<script type="text/javascript">
-									$('#pas_warnings').show();
-									$('#pas_warnings .no_address').show();
-								</script>
-								<span class="no-Address">No Address</span>
-							<?php }?>
-						</td>
-					</tr>
-				<?php
-					$i++;
-				}
-
-				if ($i == 0) {?>
-					<tr>
-						<td colspan="7" style="border: none; padding-top: 10px;">
-							There are no patients who match the specified criteria.
-						</td>
-					</tr>
-				<?php }?>
 			<?php }?>
+		<?php }?>
 		</tbody>
 		<tfoot>
-			<tr>
-				<td colspan="11">
-					<div id="key">
-						<span>Colour Key:</span>
-						<div class="container" id="sendflag-invitation"><div class="color_box"></div><div class="label">Send invitation letter</div></div>
-						<div class="container" id="sendflag-reminder"><div class="color_box"></div><div class="label">Send another reminder (2 weeks)</div></div>
-						<div class="container" id="sendflag-GPremoval"><div class="color_box"></div><div class="label">Send GP removal letter</div></div>
-						<div class="container" id="sendflag-remove"><div class="color_box"></div><div class="label">Patient is due to be removed</div></div>
-					</div>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="11" class="small">
-					<div id="letters-key">
-						<span>Letters sent out:</span>&nbsp;&nbsp;
-						<img src="<?php echo $assetPath?>/img/letterIcons/invitation.png" alt="Invitation" height="17" width="17"> - Invitation
-						<img src="<?php echo $assetPath?>/img/letterIcons/letter1.png" alt="1st reminder" height="17" width="17"> - 1<sup>st</sup> Reminder
-						<img src="<?php echo $assetPath?>/img/letterIcons/letter2.png" alt="2nd reminder" height="17" width="17"> - 2<sup>nd</sup> Reminder
-						<img src="<?php echo $assetPath?>/img/letterIcons/GP.png" alt="GP" height="17" width="17"> - GP Removal
-					</div>
-				</td>
-			</tr>
+		<tr>
+			<td colspan="11" class="waiting-list-key">
+				<h3>Colour Key:</h3>
+				<ul class="inline-list">
+					<li>
+						<span class="key-box send-invitation-letter"></span>
+						Send invitation letter
+					</li>
+					<li>
+						<span class="key-box send-another-reminder"></span>
+						Send another reminder (2 weeks)
+					</li>
+					<li>
+						<span class="key-box send-gp-removal-letter"></span>
+						Send GP removal letter
+					</li>
+					<li>
+						<span class="key-box patient-due-removed"></span>
+						Patient is due to be removed
+					</li>
+				</ul>
+		</tr>
+		<tr>
+			<td colspan="11" class="letters-sent-out">
+				<h3>Letters sent out:</h3>
+				<ul class="inline-list">
+					<li>
+						<img src="<?php echo $assetPath?>/img/letterIcons/invitation.png" alt="Invitation" height="17" width="17">
+						- Invitation
+					</li>
+					<li>
+						<img src="<?php echo $assetPath?>/img/letterIcons/letter1.png" alt="1st reminder" height="17" width="17">
+						- 1 <sup>st</sup> Reminder
+					</li>
+					<li>
+						<img src="<?php echo $assetPath?>/img/letterIcons/letter2.png" alt="2nd reminder" height="17" width="17">
+						- 2 <sup>nd</sup> Reminder
+					</li>
+					<li>
+						<img src="<?php echo $assetPath?>/img/letterIcons/GP.png" alt="GP" height="17" width="17">
+						- GP Removal
+					</li>
+				</ul>
+			</td>
+		</tr>
 		</tfoot>
 	</table>
 </div>
