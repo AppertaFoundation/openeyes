61c2d2cfb0b453d48bb51930ecf412451de3607c
Richard Willis <willis.rh@gmail.com>
2013-10-23 14:21:08 +0100

    [OE-3635] Added cancel operation styles

--- themes/rewrite/views/OphTrOperationbooking/default/cancel.php
+++ themes/rewrite/views/OphTrOperationbooking/default/cancel.php
@@ -19,36 +19,60 @@
 
 $this->beginContent('//patient/event_container'); ?>
 
-	<div id="schedule">
-		<p>
-			Patient: <?php echo $patient->getDisplayName() . ' (' . $patient->hos_num . ')'; ?>
-		</p>
-		<div id="operation">
-			<h1>Cancel operation</h1>
+	<h2 class="event-title">
+		Cancel operation
+	</h2>
+
 	<?php
-	echo CHtml::form(Yii::app()->createUrl('/'.$operation->event->eventType->class_name.'/default/cancel'), 'post', array('id' => 'cancelForm'));
+	echo CHtml::form(Yii::app()->createUrl('/'.$operation->event->eventType->class_name.'/default/cancel'), 'post', array('id' => 'cancelForm', 'class' => 'edit cancel'));
 	echo CHtml::hiddenField('operation_id', $operation->id); ?>
-	<?php echo CHtml::label('Cancellation reason: ', 'cancellation_reason'); ?>
-	<?php if (!empty($operation->booking) && (strtotime($operation->booking->session->date) <= strtotime('now'))) {
-		$listIndex = 3;
-	} else {
-		$listIndex = 2;
-	} ?>
-	<?php echo CHtml::dropDownList('cancellation_reason', '', OphTrOperationbooking_Operation_Cancellation_Reason::getReasonsByListNumber($listIndex),
-		array('empty'=>'Select a reason')
-	); ?>
-			<br/>
-	<?php echo CHtml::label('Comments: ', 'cancellation_comment'); ?>
-			<div style="height: 0.4em;"></div>
-			<textarea name="cancellation_comment" rows=6 cols=40></textarea>
-			<div style="height: 0.4em;"></div>
-			<div class="buttonwrapper">
-				<button type="submit" class="classy red venti" id="cancel"><span class="button-span button-span-red">Cancel operation</span></button>
+
+	<div class="alert-box alert with-icon hide">
+		<p>Please fix the following input errors:</p>
+	</div>
+
+	<div class="element-fields">
+		<div class="row field-row">
+			<div class="large-2 column">
+				<div class="field-label">
+					Patient:
+				</div>
+			</div>
+			<div class="large-10 column end">
+				<div class="field-value">
+					<?php echo $patient->getDisplayName() . ' (' . $patient->hos_num . ')'; ?>
+				</div>
+			</div>
+		</div>
+		<div class="row field-row">
+			<div class="large-2 column">
+				<?php echo CHtml::label('Cancellation reason: ', 'cancellation_reason'); ?>
+			</div>
+			<div class="large-5 column end">
+				<?php if (!empty($operation->booking) && (strtotime($operation->booking->session->date) <= strtotime('now'))) {
+					$listIndex = 3;
+				} else {
+					$listIndex = 2;
+				} ?>
+				<?php echo CHtml::dropDownList('cancellation_reason', '', OphTrOperationbooking_Operation_Cancellation_Reason::getReasonsByListNumber($listIndex),
+					array('empty'=>'Select a reason')
+				); ?>
+			</div>
+		</div>
+		<div class="row field-row">
+			<div class="large-2 column">
+				<?php echo CHtml::label('Comments: ', 'cancellation_comment'); ?>
+			</div>
+			<div class="large-5 column end">
+				<textarea id="cancellation_comment" name="cancellation_comment" rows="6"></textarea>
+			</div>
+		</div>
+		<div class="row field-row">
+			<div class="large-10 column large-offset-2">
+				<button type="submit" class="warning" id="cancel">Cancel operation</button>
 				<img class="loader" src="<?php echo Yii::app()->createUrl('img/ajax-loader.gif')?>" alt="loading..." style="display: none;" />
 			</div>
-	<?php echo CHtml::endForm(); ?>
 		</div>
 	</div>
-	<div class="alertBox" style="margin-top: 10px; display:none"><p>Please fix the following input errors:</p>
-	<ul><li>&nbsp;</li></ul></div>
+	<?php echo CHtml::endForm(); ?>
 <?php $this->endContent();?>
