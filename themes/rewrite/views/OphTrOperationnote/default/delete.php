e70ee875dfa17e9e37b2feef374d6503321b8634
Richard Willis <willis.rh@gmail.com>
2013-10-29 16:32:28 +0000

    [OE-3643] Added delete view

--- themes/rewrite/views/OphTrOperationnote/default/delete.php
+++ themes/rewrite/views/OphTrOperationnote/default/delete.php
@@ -17,31 +17,29 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
-<?php $this->header() ?>
+<?php $this->beginContent('//patient/event_container'); ?>
 
-<h3 class="withEventIcon"><?php echo $this->event_type->name ?></h3>
+	<h2 class="event-title"><?php echo $this->event_type->name ?></h2>
 
-<div>
-	<div class="cleartall"></div>
-</div>
-
-<div id="delete_event">
-	<h1>Delete event</h1>
-	<div class="alertBox" style="margin-top: 10px;">
-		<strong>WARNING: This will permanently delete the event and remove it from view.<br><br>THIS ACTION CANNOT BE UNDONE.</strong>
-	</div>
-	<p>
-		<strong>Are you sure you want to proceed?</strong>
-	</p>
-	<?php
-	echo CHtml::form(array('Default/delete/'.$this->event->id), 'post', array('id' => 'deleteForm'));
-		echo CHtml::hiddenField('event_id', $this->event->id); ?>
-		<div class="buttonwrapper">
-			<button type="submit" class="classy red venti" id="et_deleteevent" name="et_deleteevent"><span class="button-span button-span-red">Delete event</span></button>
-			<button type="submit" class="classy green venti" id="et_canceldelete" name="et_canceldelete"><span class="button-span button-span-green">Cancel</span></button>
-			<img class="loader" src="<?php echo Yii::app()->createUrl('img/ajax-loader.gif')?>" alt="loading..." style="display: none;" />
+	<div id="delete_event">
+		<h3>Delete event</h3>
+		<div class="alert-box alert with-icon">
+			<strong>WARNING: This will permanently delete the event and remove it from view.<br><br>THIS ACTION CANNOT BE UNDONE.</strong>
 		</div>
-	<?php echo CHtml::endForm(); ?>
-</div>
+		<p>
+			<strong>Are you sure you want to proceed?</strong>
+		</p>
+		<?php
+		echo CHtml::form(array('Default/delete/'.$this->event->id), 'post', array('id' => 'deleteForm'));
+			echo CHtml::hiddenField('event_id', $this->event->id); ?>
+			<button type="submit" class="warning" id="et_deleteevent" name="et_deleteevent">
+				Delete event
+			</button>
+			<button type="submit" class="secondary" id="et_canceldelete" name="et_canceldelete">
+				Cancel
+			</button>
+			<img class="loader" src="<?php echo Yii::app()->createUrl('img/ajax-loader.gif')?>" alt="loading..." style="display: none;" />
+		<?php echo CHtml::endForm(); ?>
+	</div>
 
-<?php $this->footer() ?>
+<?php $this->endContent() ?>
