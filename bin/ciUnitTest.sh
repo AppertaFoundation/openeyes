160975846cc9da4aaa62f0b1f66cea0a0c1f7a48
Diego Gullo <diego_gullo@bizmate.biz>
2013-11-05 15:15:00 +0000

    [OES-120] - change 3 CI shell script

--- bin/ciUnitTest.sh
+++ bin/ciUnitTest.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 mesg n
-vagrant up
 ssh -t -t vagrant@127.0.0.1 -p 2222 'pwd; cd /var/www/protected/tests/; cp -f ../config/local/console.sample.php  ../config/local/console.php; pwd; ../yiic migrate --connectionID=testdb --interactive=0;../../bin/phpunit .'
