14f894aa0e839f28444e7ac9f5685300773495ff
Richard Willis <willis.rh@gmail.com>
2013-11-12 14:46:58 +0000

    Fixed form errors markup

--- protected/views/admin/_form_errors.php
+++ protected/views/admin/_form_errors.php
@@ -1,14 +1,14 @@
 <?php if (isset($errors) && !empty($errors)) {?>
 	<div class="alert-box alert with-icon">
 		<p>Please fix the following input errors:</p>
-		<?php foreach ($errors as $field => $errs) {?>
-			<?php foreach ($errs as $err) {?>
-				<ul>
+		<ul>
+			<?php foreach ($errors as $field => $errs) {?>
+				<?php foreach ($errs as $err) {?>
 					<li>
 						<?php echo $err?>
 					</li>
-				</ul>
+				<?php }?>
 			<?php }?>
-		<?php }?>
+		</ul>
 	</div>
 <?php }?>
