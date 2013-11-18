7fdbeb9b85453c10e224ba27bb2cfef92203985c
Diego Gullo <diego_gullo@bizmate.biz>
2013-11-18 12:10:51 +0000

    [OES-125] - try full path to composer in install step

--- puppet/classes/composer.pp
+++ puppet/classes/composer.pp
@@ -1,15 +1,15 @@
 class composer {
-    exec { "install_composer":
+    exec { "download_composer":
         command => "/usr/bin/curl -sS https://getcomposer.org/installer | /usr/bin/php; mv composer.phar /usr/local/bin/composer",
         path    => "/usr/local/bin/:/bin/",
         require => Package['curl'],
         # path    => [ "/usr/local/bin/", "/bin/" ],  # alternative syntax
     }
 
-    exec { "run_composer_build":
-        command => "composer install -vvv",
+    exec { "run_composer_install":
+        command => "/usr/local/bin/composer install -vvv",
         path    => "/usr/local/bin/:/bin/:/usr/bin/",
         cwd => '/var/www',
-        require  => Exec["install_composer"]
+        require  => Exec["download_composer"]
     }
 }
