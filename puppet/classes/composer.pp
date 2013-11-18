9eb63488beeccabb3e09a59d1f35f5ba6cb0aac2
Jamie Neil <zora.j13.me>
2013-11-18 13:29:49 +0100

    Composer tweaks

--- puppet/classes/composer.pp
+++ puppet/classes/composer.pp
@@ -1,13 +1,17 @@
 class composer {
+    package { 'git':
+        ensure  => present,
+        require => Exec['apt-update']
+    }
+
     exec { "install_composer":
         command => "/usr/bin/curl -sS https://getcomposer.org/installer | /usr/bin/php; mv composer.phar /usr/local/bin/composer",
         path    => "/usr/local/bin/:/bin/",
-        require => Package['curl'],
-        # path    => [ "/usr/local/bin/", "/bin/" ],  # alternative syntax
+        require => Package['curl','git']
     }
 
     exec { "run_composer_build":
-        command => "composer install",
+        command => "composer install --prefer-source --verbose --no-interaction",
         path    => "/usr/local/bin/:/bin/:/usr/bin/",
         cwd => '/var/www',
         require  => Exec["install_composer"]
