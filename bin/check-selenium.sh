9cc937c7bb392f04e1206596daa6de67e9422ccb
Diego Gullo <diego_gullo@bizmate.biz>
2013-12-02 15:44:57 +0000

    [OES-125] - separate selenium check from ciBDD

---
+++ bin/check-selenium.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+#make sure selenium is running before going ahead
+SELENIUM=`ps aux | grep -c selenium`
+if [ $SELENIUM -gt 1 ]
+  then
+    echo GOOD - Looks like selenium is running
+  else
+    echo ERR - looks like selenium is not running. PLS run ./bin/run-selenium.sh
+    exit 1
+fi
--- bin/ciBDD.sh
+++ bin/ciBDD.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 #make sure selenium is running before going ahead
-SELENIUM=`ps aux | grep -c selenium`
-if [ $SELENIUM -gt 1 ]
-  then
-    echo GOOD - Looks like selenium is running
-  else
-    echo ERR - looks like selenium is not running. PLS run ./bin/run-selenium.sh
-    exit 1
-fi
-
+#SELENIUM=`ps aux | grep -c selenium`
+#if [ $SELENIUM -gt 1 ]
+#  then
+#    echo GOOD - Looks like selenium is running
+#  else
+#    echo ERR - looks like selenium is not running. PLS run ./bin/run-selenium.sh
+#    exit 1
+#fi
+#
 # define all modules to test
 echo "OphCiExamination
 OphDrPrescription
