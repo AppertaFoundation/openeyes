e10ffa1bb39e0fa51913adf2af587daded131bb7
Ben Spencer <ben.spencer@openeyes.org.uk>
2013-12-17 15:12:26 +0000

    Custom episode summary rendering.

--- protected/widgets/EpisodeSummaryWidget.php
+++ protected/widgets/EpisodeSummaryWidget.php
@@ -1,9 +1,6 @@
 <?php
 /**
- * OpenEyes
- *
- * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
- * (C) OpenEyes Foundation, 2011-2013
+ * (C) OpenEyes Foundation, 2013
  * This file is part of OpenEyes.
  * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -12,36 +9,12 @@
  * @package OpenEyes
  * @link http://www.openeyes.org.uk
  * @author OpenEyes <info@openeyes.org.uk>
- * @copyright Copyright (c) 2008-2011, Moorfields Eye Hospital NHS Foundation Trust
- * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
+ * @copyright Copyright (C) 2013, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 
-/*
-?>
-<div class="extraSummary">
-	This is the extra episode summary detail page for Medical Retinal.
-	<br />
-	<br />
-	As an example it includes the exampleSummary widget.
-	<br />
-	<?php $this->widget('application.components.summaryWidgets.ExampleSummary', array(
-		 'episode_id' => $episode->id
-	)); ?>
-	<br />
-	<br />
-	And a link to view the exampleSummary widget on a different page:
-	<br />
-	<?php
-
-	echo CHtml::link(
-		'exampleSummary',
-		Yii::app()->createUrl('clinical/summary', array(
-			'id' => $episode->id,
-			'summary' => 'ExampleSummary'
-		))
-	);
-?>
-</div>
-*/
-?>
+abstract class EpisodeSummaryWidget extends CWidget
+{
+	public $episode;
+	public $event_type;
+}
