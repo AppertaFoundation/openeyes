18ee2363855f948b6bdaeb3eaf39b6e5ab4cb8d7
Diego Gullo <diego_gullo@bizmate.biz>
2014-01-07 17:19:41 +0000

    Added echo message in Vagrant file and fixed Xdebug class and configuration method so it is configured differently for CI and Dev modes

--- puppet/modules/dev/manifests/xdebug.pp
+++ puppet/modules/dev/manifests/xdebug.pp
@@ -1,10 +1,11 @@
 class dev::xdebug {
 	case $operatingsystem {
-		Debian,Ubuntu:  { include xdebug::debian}
+		Debian,Ubuntu:  { include dev::xdebug::debian}
 		default: { fail "Unsupported operatingsystem ${operatingsystem}" }
 	}
+}
 
-	define config (
+define dev::xdebug::config (
 		#Template variables
 		$ini_file_path    = '',
 		$default_enable   = '',
@@ -18,88 +19,86 @@ class dev::xdebug {
 		$profiler_output_name = '',
 		$idekey = '',
 	)
-	{
-		notice("Define xdebug config \$ini_file_path ${ini_file_path}, \$remote_enable ${remote_enable}, \$remote_host ${remote_host}, ")
+{
+	notice("Define xdebug config \$ini_file_path ${ini_file_path}, \$remote_enable ${remote_enable}, \$remote_host ${remote_host}, ")
 
-		#Template variables default values
-		$xdebug_ini_file_path = $ini_file_path ? {
-			''      => '/etc/php5/conf.d/xdebug_config.ini',
-			default => $ini_file_path,
-		}
+	#Template variables default values
+	$xdebug_ini_file_path = $ini_file_path ? {
+		''      => '/etc/php5/conf.d/xdebug_config.ini',
+		default => $ini_file_path,
+	}
 
-		$xdebug_default_enable = $default_enable ? {
-			''      => '1',
-			default => $default_enable,
-		}
+	$xdebug_default_enable = $default_enable ? {
+		''      => '1',
+		default => $default_enable,
+	}
 
-		$xdebug_remote_enable = $remote_enable ? {
-			''      => '1',
-			default => $remote_enable,
-		}
+	$xdebug_remote_enable = $remote_enable ? {
+		''      => '1',
+		default => $remote_enable,
+	}
 
-		$xdebug_remote_handler = $remote_handler ? {
-			''      => 'dbgp',
-			default => $remote_handler,
-		}
+	$xdebug_remote_handler = $remote_handler ? {
+		''      => 'dbgp',
+		default => $remote_handler,
+	}
 
-		$xdebug_remote_host = $remote_host ? {
-			''      => 'localhost',
-			default => $remote_host,
-		}
+	$xdebug_remote_host = $remote_host ? {
+		''      => 'localhost',
+		default => $remote_host,
+	}
 
-		$xdebug_remote_connect_back = $remote_connect_back ? {
-			''      => '0',
-			default => $remote_connect_back,
-		}
+	$xdebug_remote_connect_back = $remote_connect_back ? {
+		''      => '0',
+		default => $remote_connect_back,
+	}
 
-		$xdebug_remote_port = $remote_port ? {
-			''      => '9000',
-			default => $remote_port,
-		}
+	$xdebug_remote_port = $remote_port ? {
+		''      => '9000',
+		default => $remote_port,
+	}
 
-		$xdebug_remote_autostart = $remote_autostart ? {
-			''      => '0',
-			default => $remote_autostart,
-		}
+	$xdebug_remote_autostart = $remote_autostart ? {
+		''      => '0',
+		default => $remote_autostart,
+	}
 
-		$xdebug_profiler_enable = $profiler_enable ? {
-			''      => '0',
-			default => $profiler_enable,
-		}
+	$xdebug_profiler_enable = $profiler_enable ? {
+		''      => '0',
+		default => $profiler_enable,
+	}
 
-		$xdebug_profiler_output_name = $profiler_output_name ? {
-			''      => '',
-			default => $profiler_output_name,
-		}
+	$xdebug_profiler_output_name = $profiler_output_name ? {
+		''      => '',
+		default => $profiler_output_name,
+	}
 
-		$xdebug_idekey = $idekey ? {
-			''      => 'XDEBUG_ECLIPSE',
-			default => $idekey,
-		}
+	$xdebug_idekey = $idekey ? {
+		''      => 'XDEBUG_ECLIPSE',
+		default => $idekey,
+	}
 
-		file { "$xdebug_ini_file_path" :
-			content => inline_template("\
-			xdebug.default_enable=<%= default_enable %>
-			xdebug.remote_enable=<%= remote_enable %>
-			xdebug.remote_handler=<%= remote_handler %>
-			xdebug.remote_connect_back=<%= remote_connect_back %>
-			xdebug.remote_host=<%= remote_host %>
-			xdebug.remote_port=<%= remote_port %>
-			xdebug.remote_autostart=<%= remote_autostart %>
-			xdebug.profiler_enable=<%= profiler_enable %>
-			xdebug.profiler_output_name=<%= profiler_output_name %>
-			xdebug.idekey=<%= idekey %>
-			xdebug.remote_handler=dbgp"),
-			ensure  => present,
-			require => Package['xdebug'],
-			notify  => Service['apache2'],
-		}
+	file { "$xdebug_ini_file_path" :
+		content => inline_template("\
+		xdebug.default_enable=<%= default_enable %>
+		xdebug.remote_enable=<%= remote_enable %>
+		xdebug.remote_handler=<%= remote_handler %>
+		xdebug.remote_connect_back=<%= remote_connect_back %>
+		xdebug.remote_host=<%= remote_host %>
+		xdebug.remote_port=<%= remote_port %>
+		xdebug.remote_autostart=<%= remote_autostart %>
+		xdebug.profiler_enable=<%= profiler_enable %>
+		xdebug.profiler_output_name=<%= profiler_output_name %>
+		xdebug.idekey=<%= idekey %>
+		xdebug.remote_handler=dbgp"),
+		ensure  => present,
+		require => Package['xdebug'],
+		notify  => Service['apache2'],
 	}
 }
 
-class xdebug::debian {
-
-	include xdebug::params
+class dev::xdebug::debian {
+	include dev::xdebug::params
 
 	package { "xdebug":
 		name   => $xdebug::params::pkg,
@@ -108,7 +107,7 @@ class xdebug::debian {
 	}
 }
 
-class xdebug::params {
+class dev::xdebug::params {
 
 	$pkg = $operatingsystem ? {
 		/Debian|Ubuntu/ => 'php5-xdebug',
@@ -119,8 +118,5 @@ class xdebug::params {
 		/Debian|Ubuntu/ => 'php5-cli',
 	}
 }
-notice("Running advanced xdebug config")
-xdebug::config { 'default':
-	profiler_output_name => 'xdebug.log',
-}
+
 
