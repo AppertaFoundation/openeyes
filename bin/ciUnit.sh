1bc365611fef6707521e2e6e30cb77d0b3ea5226
Diego Gullo <diego_gullo@bizmate.biz>
2014-02-03 15:17:01 +0000

    Set some CI scripts

---
+++ bin/ciUnit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+CSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Current script dir: $CSDIR"
+
+$CSDIR/modules_yii_config.sh
+
+# Run tests
+vagrant ssh -c 'cd /var/www/protected/tests/; /var/www/bin/phpunit'
