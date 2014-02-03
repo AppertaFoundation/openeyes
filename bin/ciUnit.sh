d6144b79fdd2a2636291e20dc64522a453500da0
Diego Gullo <diego_gullo@bizmate.biz>
2014-02-03 15:52:00 +0000

    setting Ci scripts executables

--- bin/ciUnit.sh
+++ bin/ciUnit.sh
@@ -4,5 +4,8 @@ echo "Current script dir: $CSDIR"
 
 $CSDIR/modules_yii_config.sh
 
+vagrant ssh -c 'cd /var/www;  echo "running oe-migrate"; /var/www/protected/yiic migrate --interactive=0; \
+/var/www/protected/yiic migratemodules --interactive=0;exit;'
+
 # Run tests
 vagrant ssh -c 'cd /var/www/protected/tests/; /var/www/bin/phpunit --configuration phpunit_ci.xml --testsuite all '
