41115db1f8ac68226346f0839dc21559db32e01b
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-02-04 15:47:10 +0000

    [OEM-124] Systemic diagnoses shortcode.

---
+++ protected/tests/fixtures/secondary_diagnosis.php
@@ -0,0 +1,32 @@
+<?php
+
+return array(
+	array(
+		'disorder_id' => 1,
+		'eye_id' => 1,
+		'patient_id' => 2,
+		'date' => '2004',
+	),
+	array(
+		'disorder_id' => 2,
+		'eye_id' => 2,
+		'patient_id' => 2,
+		'date' => '2006',
+	),
+	array(
+		'disorder_id' => 3,
+		'eye_id' => 3,
+		'patient_id' => 2,
+		'date' => '2005',
+	),
+	array(
+		'disorder_id' => 5,
+		'patient_id' => 2,
+		'date' => '2008',
+	),
+	array(
+		'disorder_id' => 6,
+		'patient_id' => 2,
+		'date' => '2007',
+	),
+);
---
+++ protected/tests/fixtures/specialty.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	array(
+		'id' => 1,
+		'name' => 'Ophthamology',
+		'code' => 130,
+	),
+);
