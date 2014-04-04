452077dbd781e92f6cb19806a22cd4a08d5583d1
Richard Willis <willis.rh@gmail.com>
2014-04-04 14:09:12 +0100

    [OE-4171] Fixed HTML printing and removed legacy print.css file. Refs 4f16ee548ec371ccf95774251bd886db2fc88989 that was added for 1.6

--- protected/controllers/BaseController.php
+++ protected/controllers/BaseController.php
@@ -80,11 +80,9 @@ class BaseController extends Controller
 		//FIXME: currently we are resetting the assetmanager list for PDFs because of the TCPDF processing of
 		// stylesheets. Ideally we should suppress the inclusion here. (Or we should be using a different approach
 		// to render the HTML template for the TCPDF engine)
-		
+
 		// Register the main stylesheet without pre-registering to ensure it's always output first.
-		if (!$this->isPrintAction($this->action->id)) {
-			$assetManager->registerCssFile('css/style.css', null, null, AssetManager::OUTPUT_ALL, false);
-		}
+		$assetManager->registerCssFile('css/style.css', null, null, AssetManager::OUTPUT_ALL, false);
 
 		// Prevent certain assets from being outputted in certain conditions.
 		$assetManager->adjustScriptMapping();
