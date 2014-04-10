7307e9fb57f279352cd3c903cc871d2267f13baf
Diego Gullo <diego_gullo@bizmate.biz>
2014-04-10 10:28:36 +0100

    moved module setup in dedicated bash function

--- bin/clone-modules.sh
+++ bin/clone-modules.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env sh
 
+function setupModule {
+    $modules_path=$1
+    $module=$2
+    $running_path=$3
+    if [ ! -d "$modules_path/$module" ]; then
+            echo "clone-modules.sh: Cloning $module module..." git@github.com:openeyes/$module.git
+            #git clone git@github.com:openeyes/$module.git $modules_path/$module
+            git clone https://github.com/openeyes/$module $modules_path/$module
+    else
+        cd $modules_path/$module
+        echo "clone-modules.sh: Switching module $module branch to $current_branch..."
+        git checkout $current_branch
+        echo "clone-modules.sh: git reset --hard origin/$current_branch and then pull"
+        git reset --hard origin/$current_branch
+        git pull
+        echo "clone-modules.sh: git clean -xdf"
+        git clean -xdf
+    fi
+    echo "clone-modules.sh: cd $running_path"
+    cd $running_path
+}
+
 modules_path="protected/modules"
 running_path=${PWD}
 enabled_modules="$running_path/.enabled-modules"
@@ -22,25 +44,12 @@ fi
 
 while read module
 do
-    if [ ! -e $module ]; then 
-        if [ ! -d "$modules_path/$module" ]; then
-            echo "clone-modules.sh: Cloning $module module..." git@github.com:openeyes/$module.git
-            #git clone git@github.com:openeyes/$module.git $modules_path/$module
-            git clone https://github.com/openeyes/$module $modules_path/$module
-        else
-            cd $modules_path/$module
-            echo "clone-modules.sh: Switching module $module branch to $current_branch..."
-            git checkout $current_branch
-            echo "clone-modules.sh: git reset --hard origin/$current_branch and then pull"
-            git reset --hard origin/$current_branch
-            git pull
-            echo "clone-modules.sh: git clean -xdf"
-            git clean -xdf
-        fi
-        echo "clone-modules.sh: cd $running_path"
-        cd $running_path
+    if [ ! -e $module ]; then
+        setupModule $modules_path $module $running_path
     fi
 done < $enabled_modules
+
+
 
 
 
