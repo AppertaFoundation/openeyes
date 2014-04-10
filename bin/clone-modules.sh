469eff1d5f09541cad3921378937988b5e022167
Diego Gullo <diego_gullo@bizmate.biz>
2014-04-10 15:29:15 +0100

    enabled install of dependencies in clone-modules.sh

--- bin/clone-modules.sh
+++ bin/clone-modules.sh
@@ -1,25 +1,26 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 function setupModule {
-    $modules_path=$1
-    $module=$2
-    $running_path=$3
-    if [ ! -d "$modules_path/$module" ]; then
-            echo "clone-modules.sh: Cloning $module module..." git@github.com:openeyes/$module.git
-            #git clone git@github.com:openeyes/$module.git $modules_path/$module
-            git clone https://github.com/openeyes/$module $modules_path/$module
+    loc_modules_path=$1
+    loc_module=$2
+    loc_running_path=$3
+    loc_current_branch=$4
+    if [ ! -d "$loc_modules_path/$loc_module" ]; then
+        echo "clone-modules.sh: Cloning $loc_module module..." git@github.com:openeyes/$module.git
+        #git clone git@github.com:openeyes/$module.git $modules_path/$module
+        git clone https://github.com/openeyes/$loc_module $loc_modules_path/$loc_module
     else
-        cd $modules_path/$module
-        echo "clone-modules.sh: Switching module $module branch to $current_branch..."
-        git checkout $current_branch
-        echo "clone-modules.sh: git reset --hard origin/$current_branch and then pull"
-        git reset --hard origin/$current_branch
+        cd $loc_modules_path/$loc_module
+        echo "clone-modules.sh: Switching module $loc_module branch to $loc_current_branch..."
+        git checkout $loc_current_branch
+        echo "clone-modules.sh: git reset --hard origin/$loc_current_branch and then pull"
+        git reset --hard origin/$loc_current_branch
         git pull
         echo "clone-modules.sh: git clean -xdf"
         git clean -xdf
     fi
-    echo "clone-modules.sh: cd $running_path"
-    cd $running_path
+    echo "clone-modules.sh: cd $loc_running_path"
+    cd $loc_running_path
 }
 
 modules_path="protected/modules"
@@ -45,7 +46,16 @@ fi
 while read module
 do
     if [ ! -e $module ]; then
-        setupModule $modules_path $module $running_path
+        setupModule $modules_path $module $running_path $current_branch
+        if [ -r $modules_path/$module/moduledeps ]; then
+            moduledeps="$modules_path/$module/moduledeps"
+            echo "clone-modules.sh: Setting up $module dependencies: $moduledeps "
+            while read -r moduledep || [[ -n "$moduledep" ]]
+            do
+                echo "clone-modules.sh: configuring dependency: setupModule $modules_path $moduledep $running_path $current_branch"
+                setupModule $modules_path $moduledep $running_path $current_branch
+            done < "$modules_path/$module/moduledeps"
+        fi
     fi
 done < $enabled_modules
 
