bf8e2361dfe7f1b27b1522b763836b14f9f18830
Diego Gullo <diego_gullo@bizmate.biz>
2014-04-22 11:57:32 +0100

    Set Europe/London as default timezone in Vagrant php5 setup

--- puppet/modules/core/manifests/php5.pp
+++ puppet/modules/core/manifests/php5.pp
@@ -1,24 +1,4 @@
 class core::php5 {
-
-	# NOTE: this repository was recently updated to include PHP 5.5 and Apache 2.4,
-	# which is not compatible with the OpenEyes application.
-	# (PHP5.5 needs dh-apache2 which is provided only by apache2.4.)
-
-	# exec { 'apt-repository-php5':
-	#   command => '/usr/bin/add-apt-repository ppa:ondrej/php5',
-	#   require => Package['python-software-properties'],
-	# }
-
-	# exec { 'apt-update-php5':
-	#   command => '/usr/bin/apt-get update',
-	#   require => Exec['apt-repository-php5']
-	# }
-
-	# package { 'python-software-properties':
-	#   ensure  => present,
-	#   require => Exec['apt-update'],
-	# }
-
 	package { 'php5':
 		ensure  => present,
 		require => Exec['apt-update'],
@@ -34,26 +14,6 @@ class core::php5 {
 	file {'/etc/php5/cli/conf.d/buffering_settings.ini':
 		ensure => present,
 		owner => root, group => root, mode => 444,
-		content => "output_buffering = On \nzend.enable_gc = 0 \n",
+		content => "output_buffering = On \nzend.enable_gc = 0 \ndate.timezone = Europe/London",
 	}
-
-	#define set_php_var_cli($value, $path = 'default') {
-	#    if $path == 'default'
-	#       $path= "/etc/php5/apache/php.ini"
-	#    else
-	#       $path= "/etc/php5/cli/php.ini"
-	#    end
-	#
-	#    exec { "sed -i 's/^;*[[:space:]]*$name[[:space:]]*=.*$/$name = $value/g' $path":
-	#      unless  => "grep -xqe '$name[[:space:]]*=[[:space:]]*$value' -- $path",
-	#      path    => "/bin:/usr/bin",
-	#      require => Package['php5'],
-	#      notify  => Service['apache2'];
-	#    }
-	#}
-
-	#set_php_var_cli {
-	#    "auto_detect_line_endings":       value => 'On', path => 'cli' ;
-	#}
-
 }
