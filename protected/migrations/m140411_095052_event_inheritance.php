2890b62d2cf118993f464b22850cc58a0b12f9a9
Mike Smith <mike.smith@openeyes.org.uk>
2014-05-05 15:24:33 +0100

    + [OE-4173] parent column on event_type_version

--- protected/migrations/m140411_095052_event_inheritance.php
+++ protected/migrations/m140411_095052_event_inheritance.php
@@ -6,10 +6,12 @@ class m140411_095052_event_inheritance extends CDbMigration
 	{
 		$this->addColumn('event_type', 'parent_event_type_id', 'int(10) unsigned');
 		$this->addForeignKey('event_type_pevid_fk', 'event_type', 'parent_event_type_id', 'event_type', 'id');
+		$this->addColumn('event_type_version', 'parent_event_type_id', 'int(10) unsigned');
 	}
 
 	public function down()
 	{
+		$this->dropColumn('event_type_version', 'parent_event_type_id');
 		$this->dropForeignKey('event_type_pevid_fk', 'event_type');
 		$this->dropColumn('event_type', 'parent_event_type_id');
 	}
