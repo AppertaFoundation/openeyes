816a156f506a81e3db6f51270e2785a500b55cd1
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-05-12 11:32:12 +0100

    Data integrity checks for MeasurementReference.

--- protected/models/MeasurementReference.php
+++ protected/models/MeasurementReference.php
@@ -15,4 +15,23 @@ class MeasurementReference extends BaseActiveRecordVersioned
 			'patientMeasurement' => array(self::BELONGS_TO, 'PatientMeasurement', 'patient_measurement_id'),
 		);
 	}
+
+	protected function beforeSave()
+	{
+		foreach ($this->findAll('patient_measurement_id = ?', array($this->patient_measurement_id)) as $existing) {
+			if ($this->episode_id && $this->episode_id == $existing->episode_id) {
+				throw new Exception("Measurement reference already exists from episode {$this->episode_id} to patient measurement {$this->patient_measurement_id}");
+			}
+
+			if ($this->event_id && $this->event_id == $existing->event_id) {
+				throw new Exception("Measurement reference already exists from event {$this->event_id} to patient measurement {$this->patient_measurement_id}");
+			}
+
+			if ($this->origin && $existing->origin) {
+				throw new Exception("Origin reference already exists for patient measurement {$this->patient_measurement_id}");
+			}
+		}
+
+		return parent::beforeSave();
+	}
 }
---
+++ protected/tests/fixtures/measurement_reference.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	'height_episode' => array(
+		'patient_measurement_id' => 1,
+		'episode_id' => 1,
+		'origin' => true,
+	),
+	'height_event' => array(
+		'patient_measurement_id' => 1,
+		'event_id' => 1,
+		'origin' => true,
+	),
+);
