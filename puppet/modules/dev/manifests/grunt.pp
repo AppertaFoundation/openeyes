3478d493ea864d88fd4f97783bd5a34d1b307d87
jam13 <jamie.neil@gmail.com>
2014-06-04 18:20:14 +0100

    Fix vagrant dev build

--- puppet/modules/dev/manifests/grunt.pp
+++ puppet/modules/dev/manifests/grunt.pp
@@ -1,6 +1,6 @@
 class dev::grunt {
 	exec { 'grunt-install':
-		command => '/home/vagrant/.nvm/v0.10.25/bin/npm --registry http://registry.npmjs.eu/ install -g grunt-cli@0.1.13',
+		command => '/bin/bash -c "source /home/vagrant/.nvm/nvm.sh && npm install -g grunt-cli@0.1.13"',
 		user => 'vagrant',
 		environment => 'HOME=/home/vagrant',
 		require => Exec['node-install']
