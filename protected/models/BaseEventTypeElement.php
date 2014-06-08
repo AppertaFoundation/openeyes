68ee73035099a2d7cf4b4269b8fe4a6edb7c53a6
Mark Wadham <mark.wadham@openeyes.org.uk>
2014-06-08 09:38:23 +0000

    bugfix

--- protected/models/BaseEventTypeElement.php
+++ protected/models/BaseEventTypeElement.php
@@ -327,7 +327,7 @@ class BaseEventTypeElement extends BaseElement
 	}
 
 	public function textWithLineBreaks($field) {
-		return CHtml::encode($this->$field);
+		return str_replace("\n",'<br/>',CHtml::encode($this->$field));
 	}
 
 	/**
