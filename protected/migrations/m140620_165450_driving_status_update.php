712f836ca363a661e478818a9aba12b6ef14f1a2
adamdyer <progen91@gmail.com>
2014-06-25 13:51:59 +0100

    [OE-4365] driving status update

--- protected/migrations/m140620_165450_driving_status_update.php
+++ protected/migrations/m140620_165450_driving_status_update.php
@@ -4,11 +4,11 @@ class m140620_165450_driving_status_update extends CDbMigration
 {
 	public function up()
 	{
-		Yii::app()->db->createCommand('update socialhistory_driving_status set name =\'HGV\' where name=\'HGV, Taxi, Train\'')->query();
+		$this->dbConnection->createCommand('update socialhistory_driving_status set name =\'HGV\' where name=\'HGV, Taxi, Train\'')->query();
 	}
 
 	public function down()
 	{
-		Yii::app()->db->createCommand('update socialhistory_driving_status set name =\'HGV, Taxi, Train\' where name=\'HGV\'')->query();
+		$this->dbConnection->createCommand('update socialhistory_driving_status set name =\'HGV, Taxi, Train\' where name=\'HGV\'')->query();
 	}
 }
