36fc4fecd14b327fbafb73d1bfa968fcf3f5414d
adamdyer <progen91@gmail.com>
2014-07-09 14:17:54 +0100

    [OE-4474] disabled drugs admin

--- protected/views/admin/sidebar.php
+++ protected/views/admin/sidebar.php
@@ -7,7 +7,6 @@
 			'Contacts' => '/admin/contacts',
 			'Contact labels' => '/admin/contactlabels',
 			'Data sources' => '/admin/datasources',
-			'Drugs' => '/admin/drugs',
 			'Institutions' => '/admin/institutions',
 			'Sites' => '/admin/sites',
 			'Commissioning bodies' => '/admin/commissioning_bodies',
