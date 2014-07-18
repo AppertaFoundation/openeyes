9836b8198edde2c7db7d7c1018cfc824845ede16
adamdyer <progen91@gmail.com>
2014-07-18 14:55:33 +0100

    [OE-4515] yob moved to patient migration

--- protected/migrations/m140718_131304_genetics_extra_contact_fields.php
+++ protected/migrations/m140718_131304_genetics_extra_contact_fields.php
@@ -5,12 +5,10 @@ class m140718_131304_genetics_extra_contact_fields extends CDbMigration
 	public function up()
 	{
 		$this->addColumn('contact','maiden_name','varchar(100) COLLATE utf8_bin NOT NULL');
-		$this->addColumn('patient','yob','int(2) unsigned NULL');
 	}
 
 	public function down()
 	{
-		$this->dropColumn('patient','yob');
 		$this->dropColumn('contact','maiden_name');
 	}
 }
