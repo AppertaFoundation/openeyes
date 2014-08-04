55df8dc27be3b0192911b8c25c192fd29b42a3a5
Richard Willis <willis.rh@gmail.com>
2014-08-04 11:24:12 +0100

    Removed print view generation from gii as print views now use the same "view" views.
    Custom print views can still be specified for elements by adjusting the name of the print view file in the model.

--- protected/gii/EventTypeModule/templates/default/views/default/print.php
+++ protected/gii/EventTypeModule/templates/default/views/default/print.php
@@ -18,7 +18,7 @@
  */
 <?php echo "?>\n"?>
 
-<?php echo '<?php'?> $this->renderDefaultElements($this->action->id)<?php echo "?>\n"?>
-
-
-
+<?php echo '<?php'?> $this->renderPartial('//print/event', array(
+	'hide_modified' => @$hide_modified,
+	'hide_created' => @$hide_created
+));<?php echo "?>\n"?>
