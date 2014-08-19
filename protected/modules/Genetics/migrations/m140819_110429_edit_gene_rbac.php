ca9cc30c7d7ff6402695af6a7019cf159329c499
adamdyer <progen91@gmail.com>
2014-08-19 12:12:07 +0100

    gene data rbac permission migration

---
+++ protected/modules/Genetics/migrations/m140819_110429_edit_gene_rbac.php
@@ -0,0 +1,29 @@
+<?php
+
+class m140819_110429_edit_gene_rbac extends CDbMigration
+{
+	public function up()
+	{
+
+
+		$this->insert('authitem',array('name'=>'TaskEditGeneData', 'type' => 1));
+		$this->insert('authitem',array('name'=>'TaskViewGeneData', 'type' => 1));
+
+		$this->insert('authitemchild',array('parent'=>'Genetics Admin','child'=>'TaskEditGeneData'));
+		$this->insert('authitemchild',array('parent'=>'Genetics Admin','child'=>'TaskViewGeneData'));
+		$this->insert('authitemchild',array('parent'=>'Genetics User','child'=>'TaskViewGeneData'));
+
+		$this->insert('authitem',array('name'=>'OprnViewGene', 'type' => 0));
+		$this->insert('authitem',array('name'=>'OprnEditGene', 'type' => 0));
+
+		$this->insert('authitemchild',array('parent'=>'TaskEditPedigreeData','child'=>'OprnViewGene'));
+		$this->insert('authitemchild',array('parent'=>'TaskViewPedigreeData','child'=>'OprnEditGene'));
+
+	}
+
+	public function down()
+	{
+
+	}
+
+}
