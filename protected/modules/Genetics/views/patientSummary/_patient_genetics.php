9a94e30766977326952915ea27ad4239fcb57312
adamdyer <progen91@gmail.com>
2014-08-26 15:46:18 +0100

    blood sample event icons

--- protected/modules/Genetics/views/patientSummary/_patient_genetics.php
+++ protected/modules/Genetics/views/patientSummary/_patient_genetics.php
@@ -49,5 +49,23 @@
 				</div>
 			</div>
 		<?php }?>
+		<?php
+		if ($api = Yii::app()->moduleAPI->get('OphInBloodsample')) {
+			foreach ($api->getEventsByPatient($this->patient) as $event) {
+				$event_path = Yii::app()->createUrl($event->eventType->class_name . '/default/view') . '/'; ?>
+				<a href="<?php echo $event_path . $event->id ?>" data-id="<?php echo $event->id ?>">
+					<?php
+					if (file_exists(Yii::getPathOfAlias('application.modules.' . $event->eventType->class_name . '.assets'))) {
+						$assetpath = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('application.modules.' . $event->eventType->class_name . '.assets')) . '/';
+					} else {
+						$assetpath = '/assets/';
+					}
+					?>
+					<img src="<?php echo $assetpath . 'img/small.png' ?>" alt="op" width="19" height="19" />
+				</a>
+			<?php
+			}
+		}
+		?>
 	</div>
 </section>
