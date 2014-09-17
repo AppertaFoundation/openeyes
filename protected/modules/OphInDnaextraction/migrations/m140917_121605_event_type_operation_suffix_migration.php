e87e8ad900dcdd83e28803bc551074175027169d
adamdyer <progen91@gmail.com>
2014-09-17 13:36:17 +0100

    register gentics rbac suffix

--- protected/modules/OphInDnaextraction/migrations/m140917_121605_event_type_operation_suffix_migration.php
+++ protected/modules/OphInDnaextraction/migrations/m140917_121605_event_type_operation_suffix_migration.php
@@ -5,7 +5,7 @@ class m140917_121605_event_type_operation_suffix_migration extends CDbMigration
 	public function up()
 	{
 		$event_type_id=$this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name'=>'OphInDnaextraction'))->queryScalar();
-		$this->update('event_type',array('rbac_operation_suffix'=>'Genetics'),"id = $event_type_id");
+		$this->update('event_type',array('rbac_operation_suffix'=>'DnaExtraction'),"id = $event_type_id");
 	}
 
 	public function down()
