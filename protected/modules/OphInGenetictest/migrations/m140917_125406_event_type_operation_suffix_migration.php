1cbecb36306aa8145c429acc98503c15507c99a4
adamdyer <progen91@gmail.com>
2014-09-17 14:33:29 +0100

    update rbac suffix

--- protected/modules/OphInGenetictest/migrations/m140917_125406_event_type_operation_suffix_migration.php
+++ protected/modules/OphInGenetictest/migrations/m140917_125406_event_type_operation_suffix_migration.php
@@ -4,7 +4,7 @@ class m140917_125406_event_type_operation_suffix_migration extends  OEMigration
 {
 	public function up()
 	{
-		$this->setEventTypeRBACSuffix('OphInGenetictest','Genetictest');
+		$this->setEventTypeRBACSuffix('OphInGenetictest','GeneticTest');
 	}
 
 	public function down()
