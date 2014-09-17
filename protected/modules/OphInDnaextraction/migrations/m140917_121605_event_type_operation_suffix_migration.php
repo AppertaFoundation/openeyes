e31caaed1171633e2cfb77588728a60d4f8a16b3
adamdyer <progen91@gmail.com>
2014-09-17 13:48:17 +0100

    set event type rbac suffix

--- protected/modules/OphInDnaextraction/migrations/m140917_121605_event_type_operation_suffix_migration.php
+++ protected/modules/OphInDnaextraction/migrations/m140917_121605_event_type_operation_suffix_migration.php
@@ -1,16 +1,14 @@
 <?php
 
-class m140917_121605_event_type_operation_suffix_migration extends CDbMigration
+class m140917_121605_event_type_operation_suffix_migration extends OEMigration
 {
 	public function up()
 	{
-		$event_type_id=$this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name'=>'OphInDnaextraction'))->queryScalar();
-		$this->update('event_type',array('rbac_operation_suffix'=>'DnaExtraction'),"id = $event_type_id");
+		$this->setEventTypeRBACSuffix('OphInDnaExtraction','DnaExtraction');
 	}
 
 	public function down()
 	{
-		$event_type_id=$this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name'=>'OphInDnaextraction'))->queryScalar();
-		$this->update('event_type',array('rbac_operation_suffix'=>NULL),"id = $event_type_id");
+		$this->setEventTypeRBACSuffix('OphInDnaExtraction',NULL);
 	}
 }
