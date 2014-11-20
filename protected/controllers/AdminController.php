1a0764044a45f718141f7842aadad5c445cbded8
adamdyer <progen91@gmail.com>
2014-11-20 17:36:58 +0000

    [OE-5213] common opthalmic admin new row

--- protected/controllers/AdminController.php
+++ protected/controllers/AdminController.php
@@ -72,6 +72,16 @@ class AdminController extends BaseAdminController
 				'filter_fields' => array(
 					array('field' => 'subspecialty_id', 'model' => 'Subspecialty'),
 				),
+				'extra_fields' => array(
+					array(
+						'field' => 'alternate_disorder_id',
+						'relation' => 'alternate_disorder',
+						'type' => 'search_lookup',
+						'options' => \CHtml::listData(\CommonOphthalmicDisorder::model()->findAll(), 'id', 'name'),
+					),
+					array('field' => 'alternate_disorder_label', 'type' => 'text',
+						'model' => 'CommonOphthalmicDisorder'),
+				),
 			),
 			$key
 		);
