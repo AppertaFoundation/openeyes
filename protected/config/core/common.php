c19766094a534de32daff4e2f6d3dd4d5a984fd4
jam13 <jamie.neil@gmail.com>
2014-12-05 11:47:17 +0000

    [OE-5240] Fix margins and include wkhtmltopdf in vagrant

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -295,8 +295,8 @@ return array(
 		'wkhtmltopdf_footer_middle' => 'Page {{PAGE}} of {{PAGES}}',
 		'wkhtmltopdf_footer_right' => 'OpenEyes',
 		'wkhtmltopdf_top_margin' => '10mm',
-		'wkhtmltopdf_bottom_margin' => '12mm',
-		'wkhtmltopdf_left_margin' => '17mm',
-		'wkhtmltopdf_right_margin' => '17mm',
+		'wkhtmltopdf_bottom_margin' => '18mm',
+		'wkhtmltopdf_left_margin' => '20mm',
+		'wkhtmltopdf_right_margin' => '20mm',
 	),
 );
