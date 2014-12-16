bd8e0319f885b689b09d75b47b789c3a84322dab
Mark Wadham <mark.wadham@openeyes.org.uk>
2014-12-16 10:42:21 +0000

    fix for blank nhs_num values.

--- protected/migrations/m140815_135553_unique_patient_identifiers.php
+++ protected/migrations/m140815_135553_unique_patient_identifiers.php
@@ -5,6 +5,8 @@ class m140815_135553_unique_patient_identifiers extends OEMigration
 	public function safeUp()
 	{
 		$this->createIndex('patient_hos_num_unique', 'patient', 'hos_num', true);
+
+		$this->update('patient',array('nhs_num' => null),"nhs_num = ''");
 		$this->createIndex('patient_nhs_num_unique', 'patient', 'nhs_num', true);
 	}
 
