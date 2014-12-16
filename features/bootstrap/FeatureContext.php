54305d66f74f6c83d429e880cc1c5125970f96d1
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-12-16 15:50:24 +0000

    Don't overwrite variable.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -101,8 +101,8 @@ class FeatureContext extends PageObjectContext implements YiiAwareContextInterfa
 			} else {
 				throw new Exception("Couldn't figure out how to select option '$option' in fieldset '$label'");
 			}
-		} else if(($label = $page->find('xpath', ".//label[contains(normalize-space(string(.)), '${label}')]")) &&
-			($id = $label->getAttribute('for')) && ($select = $page->find('css', "select#{$id}"))) {
+		} else if(($label_el = $page->find('xpath', ".//label[contains(normalize-space(string(.)), '${label}')]")) &&
+			($id = $label_el->getAttribute('for')) && ($select = $page->find('css', "select#{$id}"))) {
 			$select->selectOption($option);
 		} else {
 			throw new Exception("Couldn't find option field '$label'");
