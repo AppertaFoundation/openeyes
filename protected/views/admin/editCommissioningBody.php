0b30ea6958d0d5665eac1d9989b851c505c3bd8e
Peter Gallagher <peter.s.gallagher@gmail.com>
2015-02-19 12:36:11 +0000

    Fixing further syntax errors

--- protected/views/admin/editCommissioningBody.php
+++ protected/views/admin/editCommissioningBody.php
@@ -36,9 +36,9 @@
 		<?php echo $form->textField($cb,'code',array('autocomplete'=>Yii::app()->params['html_autocomplete']),null,array('field' => 2))?>
 		<?php echo $form->textField($address,'address1',array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
 		<?php echo $form->textField($address,'address2', array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
-		<?php echo $form->textField($address,'city'array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
-		<?php echo $form->textField($address,'county'array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
-		<?php echo $form->textField($address,'postcode'array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
+		<?php echo $form->textField($address,'city', array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
+		<?php echo $form->textField($address,'county', array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
+		<?php echo $form->textField($address,'postcode', array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
 		<?php echo $form->dropDownList($address,'country_id','Country')?>
 		<?php echo $form->formActions(array('cancel-uri' => '/admin/commissioning_bodies'));?>
 	<?php $this->endWidget()?>
