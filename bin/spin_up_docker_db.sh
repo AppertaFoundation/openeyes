f2abce7b1951d175cd054aada21db2f5b5bfdeef
Peter Gallagher <peter.s.gallagher@gmail.com>
2015-02-23 10:17:57 +0000

    Replacing port and adding script to build file.

--- bin/spin_up_docker_db.sh
+++ bin/spin_up_docker_db.sh
@@ -4,4 +4,6 @@
 sudo docker run -d --name $1 -p 3306 openeyesdb
 
 #Fix on Monday ;)
-sudo docker ps -a | grep $1 | grep -Po '(\d+)->' | grep -o "[0-9]*"
+mysqlport=`sudo docker ps -a | grep $1 | grep -Po '(\d+)->' | grep -o "[0-9]*"`
+
+sudo sed -i.bak 's/port=3306/port='$mysqlport'/g' protected/config/local/common.php
