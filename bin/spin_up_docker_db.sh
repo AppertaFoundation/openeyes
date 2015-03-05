da05dd386a694386ab26e07bffd51bc7639b03f2
Peter Gallagher <peter.s.gallagher@gmail.com>
2015-03-05 13:51:15 +0000

    Removing another sudo

--- bin/spin_up_docker_db.sh
+++ bin/spin_up_docker_db.sh
@@ -3,8 +3,7 @@
 
 docker run -d --name $1 -p 3306 openeyesdb
 
-#Fix on Monday ;)
-mysqlport=`sudo docker ps -a | grep $1 | grep -Po '(\d+)->' | grep -o "[0-9]*"`
+mysqlport=`docker ps -a | grep $1 | grep -Po '(\d+)->' | grep -o "[0-9]*"`
 
 echo "setting config port to be $mysqlport"
 
