221e7800405f5feb2eba10507fbe890f2a59b733
Peter Gallagher <peter.s.gallagher@gmail.com>
2015-03-27 17:09:31 +0000

    OE-5400: Starting validator for checking if contained in another model

---
+++ protected/extensions/validators/NotContainedInValidator.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: petergallagher
+ * Date: 27/03/15
+ * Time: 16:00
+ */
+
+class NotContainedInValidator extends CValidator
+{
+
+	/**
+	 * @var string Name of the model to be checked against
+	 */
+	public $model;
+
+	/**
+	 * @var string Foreign key if not standard tablename_id
+	 */
+	public $foreignKey;
+
+	/**
+	 * Validates a single attribute.
+	 * This method should be overridden by child classes.
+	 * @param CModel $object the data object being validated
+	 * @param string $attribute the name of the attribute to be validated.
+	 */
+	protected function validateAttribute($object, $attribute)
+	{
+		if($this->model === null){
+			//currently unsupported
+			return;
+		}
+
+		if($this->message === null){
+			$this->message = '{attribute} can not be edited because it is already in use';
+		}
+
+		if($this->foreignKey === null){
+			$this->foreignKey = $object->tableName().'_id';
+		}
+
+		 if($this->validateContainedInModel($object->id)){
+			 $this->addError($object, $attribute, $this->message);
+		 }
+	}
+
+	/**
+	 * @param $value
+	 * @return CActiveRecord|null
+	 */
+	protected function validateContainedInModel($value)
+	{
+		if(!class_exists($this->model) || is_subclass_of($this->model, 'BaseActiveRecord')){
+			return;
+		}
+		$instance = new $this->model;
+
+		return $instance::model()->findByAttributes(array("$this->foreignKey" => $value));
+	}
+
+}
