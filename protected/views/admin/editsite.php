d8855a660d7df5999db19e5b34565eeb4144a0f9
Himanshu Patel <contacttohimanshu@yahoo.com>
2015-04-13 12:06:36 +0100

    OE-5392 :: Added 'Short Name' field to Edit Site screen

--- protected/views/admin/editsite.php
+++ protected/views/admin/editsite.php
@@ -33,6 +33,7 @@
 	))?>
 		<?php echo $form->dropDownList($site,'institution_id','Institution',array('style'=>'margin-bottom:6px;'))?>
 		<?php echo $form->textField($site,'name',array('autocomplete'=>Yii::app()->params['html_autocomplete'],'size'=>'50'))?>
+		<?php echo $form->textField($site,'short_name',array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
 		<?php echo $form->textField($site,'remote_id',array('autocomplete'=>Yii::app()->params['html_autocomplete']),null,array('field' => 2))?>
 		<?php echo $form->textField($address,'address1',array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
 		<?php echo $form->textField($address,'address2',array('autocomplete'=>Yii::app()->params['html_autocomplete']))?>
