b695126961aa1af0585afd85eb22cbbe854b502e
Peter Gallagher <peter.s.gallagher@gmail.com>
2015-04-16 10:37:13 +0100

    OE-5426: Fixing case issue

--- protected/models/Procedure.php
+++ protected/models/Procedure.php
@@ -78,7 +78,7 @@ class Procedure extends BaseActiveRecordVersioned
 			//'operations' => array(self::MANY_MANY, 'ElementOperation', 'operation_procedure_assignment(proc_id, operation_id)'),
 			'specialties' => array(self::MANY_MANY, 'Subspecialty', 'proc_subspecialty_assignment(proc_id, subspecialty_id)'),
 			'subspecialtySubsections' => array(self::MANY_MANY, 'SubspecialtySubsection', 'proc_subspecialty_subsection_assignment(proc_id, subspecialty_subsection_id)'),
-			'opcsCodes' => array(self::MANY_MANY, 'OpcsCode', 'proc_opcs_assignment(proc_id, opcs_code_id)'),
+			'opcsCodes' => array(self::MANY_MANY, 'OPCSCode', 'proc_opcs_assignment(proc_id, opcs_code_id)'),
 			'additional' => array(self::MANY_MANY, 'Procedure', 'procedure_additional(proc_id, additional_proc_id)'),
 			'benefits' => array(self::MANY_MANY, 'Benefit', 'procedure_benefit(proc_id, benefit_id)'),
 			'complications' => array(self::MANY_MANY, 'Complication', 'procedure_complication(proc_id, complication_id)'),
