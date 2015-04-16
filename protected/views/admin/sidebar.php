bcd6988e6dcf34743ced5c52d63ad29866cad96b
vetusko@gmail.com <bazdmeg1>
2015-04-16 18:08:47 +0100

    Menu for admin page

--- protected/views/admin/sidebar.php
+++ protected/views/admin/sidebar.php
@@ -66,6 +66,23 @@
 			</li>
 		<?php }?>
 </div>
+<div class="box admin">
+	<h2>Drugs</h2>
+	<ul class="navigation admin">
+		<?php foreach (array(
+						   'Medication List' => '/oeadmin/medication/list'
+					   ) as $title => $uri) { ?>
+			<li<?php if (Yii::app()->getController()->action->id == preg_replace('/^\/admin\//', '', $uri)
+			) { ?> class="selected"<?php } ?>>
+				<?php if (Yii::app()->getController()->action->id == preg_replace('/^\/admin\//', '', $uri)) { ?>
+					<?php echo CHtml::link($title, array($uri), array('class' => 'selected')) ?>
+				<?php } else { ?>
+					<?php echo CHtml::link($title, array($uri)) ?>
+				<?php } ?>
+			</li>
+		<?php } ?>
+</div>
+
 <?php foreach (ModuleAdmin::getAll() as $module => $items) {?>
 	<div class="admin box">
 		<h2><?php echo $module?></h2>
