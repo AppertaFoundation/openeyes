1a068cfcb933500a1b32443bb879efd00dc3d61f
Peter Gallagher <peter.s.gallagher@gmail.com>
2015-06-25 14:50:57 +0100

    Fixing display order when key is provided.

--- protected/widgets/views/GenericAdmin.php
+++ protected/widgets/views/GenericAdmin.php
@@ -77,7 +77,7 @@
 if (!$get_row && $filters_ready) {
 				if (!$this->new_row_url) {
 					$this->render('_generic_admin_row', array('row_class' => 'newRow', 'row_style' => 'display: none;', 'disabled' => true,
-							'i' => '{{key}}', 'row' => new $model, 'label_field' => $label_field, 'extra_fields' => $extra_fields, 'model' => $model));
+							'i' => '{{key}}', 'row' => new $model, 'label_field' => $label_field, 'extra_fields' => $extra_fields, 'model' => $model, 'display_order' => $display_order ));
 				} ?>
 			</tbody>
 			<?php if ($model::model()->hasAttribute('default')) {?>
