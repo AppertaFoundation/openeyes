ba046494269d52f4a89a88517c6042e6c52f4071
Antony Penn <antony.penn@gmail.com>
2015-10-22 15:11:49 +0100

    clear out

--- docs/src/jsdoc/README.md
+++
@@ -1,1 +0,0 @@
-## OpenEyes Javascript API Documentation.
--- docs/src/static-templates/components/header-logged-in-no-patient.php
+++
@@ -1,9 +0,0 @@
-<header class="header row">
-	<div class="large-2 columns">
-		<div class="logo">OpenEyes</div>
-	</div>
-	<div class="large-4 column"></div>
-	<div class="large-6 column">
-		<? include 'user-panel.php'; ?>
-	</div>
-</header>
