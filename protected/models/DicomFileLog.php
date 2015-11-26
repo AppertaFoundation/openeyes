a31b5cde4d9ca7813a7b253bfc92dc84d5477d56
himanshupatel79 <contacttohimanshu@yahoo.com>
2015-11-26 17:46:52 +0000

    OE-5735 :: Added First Draft of Dicom File Viewer

---
+++ protected/models/DicomFileLog.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: PATELH3
+ * Date: 26/11/2015
+ * Time: 14:03
+ */
+class DicomFileLog extends BaseActiveRecordVersioned
+{
+    protected $auto_update_relations = true;
+
+    /**
+     * Returns the static model of the specified AR class.
+     * @return Benefit the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'dicom_file_log';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('filename', 'required'),
+            array(
+                'event_date_time, filename, status, process_name',
+                'safe'
+            ),
+        );
+    }
+}
+
+
