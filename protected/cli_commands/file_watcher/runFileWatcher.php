71c1c5ee1bfd3af1304ea34d2b95fd320d194cfe
vetusko <vetusko@gmail.com>
2015-12-03 17:11:27 +0000

    removed fam_resume_monitor call

--- protected/cli_commands/file_watcher/runFileWatcher.php
+++ protected/cli_commands/file_watcher/runFileWatcher.php
@@ -17,18 +17,27 @@ checkExistingFiles();
 while (TRUE)
 {
 	$newfile = false;
-	fam_resume_monitor($fam_res, $dir_res);
+	//fam_resume_monitor($fam_res, $dir_res);
 	while(fam_pending($fam_res)){
 		$arr = fam_next_event($fam_res);
-				
+		
 		// FAMCreated == 5
 		if($arr["code"] == 5){
 			echo 'New file arrived: '.$arr["filename"]."\n";
 			$newfile = true;
+			$filedata = stat($dicomConfig["biometry"]["inputFolder"]."/".$arr["filename"]);
+			if(! $fileid = fileEntryExists($dicomConfig["biometry"]["inputFolder"]."/".$arr["filename"], $filedata)){
+				$fileid = createFileEntry($dicomConfig["biometry"]["inputFolder"]."/".$arr["filename"], $filedata);
+			}
+			var_dump("Fileid: ".$fileid);
 			$mysqli->query("INSERT INTO dicom_file_queue (filename, detected_date, last_modified_date, status_id) VALUES ('".$dicomConfig["biometry"]["inputFolder"]."/".$arr["filename"]."', now(), now(), (SELECT id FROM dicom_process_status WHERE name = 'new'))");
 			$logger->addLogEntry($dicomConfig["biometry"]["inputFolder"]."/".$arr["filename"], 'new', basename($_SERVER["SCRIPT_FILENAME"]));
 			// add log entry
 			
+		}else if($arr["code"] == 1){
+			echo 'File has been changed: '.$arr["filename"]."\n";
+			echo "What we should do now? :)\n";
+			var_dump(stat($dicomConfig["biometry"]["inputFolder"]."/".$arr["filename"]));
 		}
 	}
 	
@@ -42,6 +51,27 @@ while (TRUE)
 	sleep(5);
 
 }
+
+function fileEntryExists($fullfilename, $filedata){
+	global $mysqli;
+	
+	var_dump($filedata);
+	$checkQuery = $mysqli->query("SELECT id FROM dicom_files WHERE filename='".$fullfilename."' AND filesize ='".$filedata["size"]."' AND filedate='".date("Y-m-d H:i:s",$filedata["mtime"])."'");
+	if($isFileEntryExists = $checkQuery->fetch_row()){
+		return $isFileEntryExists[0];
+	}else{
+		return false;
+	}
+	
+}
+
+function createFileEntry($fullfilename, $filedata){
+	global $mysqli;
+	// todo: add insert here! :)
+	$mysqli->query("INSERT INTO dicom_files (filename, filesize, filedate, processor_id) VALUES ('".$fullfilename."', '".$filedata["size"]."','".date("Y-m-d H:i:s",$filedata["mtime"])."','".php_uname("n")."')");
+	return $mysqli->insert_id;
+}
+
 
 function checkExistingFiles(){
 	global $fam_res;
@@ -62,7 +92,7 @@ function checkExistingFiles(){
 
 	if(is_array($existingFiles) && count($existingFiles) > 0){
 		$processedFiles = array();
-		$processedFilesQ = $mysqli->query("SELECT id,filename FROM dicom_file_queue");
+		$processedFilesQ = $mysqli->query("SELECT id,filename, filesize, filedate FROM dicom_files");
 		while($filerow = $processedFilesQ->fetch_assoc()){
 			$processedFiles[] = $filerow["filename"];
 		}
