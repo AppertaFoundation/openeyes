02b48843677985e977e92063091edb6faecf282e
Mike Smith <mike.smith@camc-ltd.co.uk>
2015-12-22 15:56:28 +0000

    + make dashboard items sortable (not stored)

--- protected/views/base/_dashboard.php
+++ protected/views/base/_dashboard.php
@@ -1,6 +1,8 @@
+<div class="dashboard-container">
 <?php foreach ($items as $item) { ?>
 <section class="box dashboard js-toggle-container">
     <h3 class="box-title"><?= $item['title'] ?></h3>
+        <span class="sortable-anchor fa fa-arrows"></span>
     <a href="#" class="toggle-trigger toggle-hide js-toggle">
 		<span class="icon-showhide">
 			Show/hide this section
@@ -10,5 +12,10 @@
     <?= $item['content']; ?>
     </div>
 </section>
-
 <?php } ?>
+</div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.dashboard-container').sortable({handle: '.sortable-anchor'});
+    })
+</script>
