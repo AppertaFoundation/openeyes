e3e2ea9d1ee917c76c60de1752177d26c62223fe
Mike Smith <mike.smith@camc-ltd.co.uk>
2015-12-22 13:48:13 +0000

    + updated dashboard layout and style to support collapsing display and titles

--- protected/views/base/_dashboard.php
+++ protected/views/base/_dashboard.php
@@ -1,5 +1,14 @@
-<?php
-    foreach ($items as $item) {
-        echo $item;
-    }
-?>
+<?php foreach ($items as $item) { ?>
+<section class="box dashboard js-toggle-container">
+    <h3 class="box-title"><?= $item['title'] ?></h3>
+    <a href="#" class="toggle-trigger toggle-hide js-toggle">
+		<span class="icon-showhide">
+			Show/hide this section
+		</span>
+    </a>
+    <div class="js-toggle-body">
+    <?= $item['content']; ?>
+    </div>
+</section>
+
+<?php } ?>
