e90c0e197c2a07790d5d60aa734e979c56edaca7
tobyoe <toby.bisco@openeyes.org.uk>
2016-02-05 12:46:48 +0000

    Added Dr Prescription

---
+++ protected/modules/OphDrPrescription/migrations/m131121_114638_prescription_authitems.php
@@ -0,0 +1,38 @@
+<?php
+
+class m131121_114638_prescription_authitems extends CDbMigration
+{
+	private $authitems = array(
+		array('name' => 'OprnCreatePrescription', 'type' => 0, 'bizrule' => 'canCreateEvent'),
+		array('name' => 'OprnEditPrescription', 'type' => 0, 'bizrule' => 'canEditEvent'),
+		array('name' => 'OprnPrintPrescription', 'type' => 0),
+	);
+
+	private $parents = array(
+		'OprnCreatePrescription' => 'TaskPrescribe',
+		'OprnEditPrescription' => 'TaskPrescribe',
+		'OprnPrintPrescription' => 'TaskPrescribe',
+	);
+
+	public function up()
+	{
+		foreach ($this->authitems as $authitem) {
+			$this->insert('authitem', $authitem);
+		}
+
+		foreach ($this->parents as $child => $parent) {
+			$this->insert('authitemchild', array('parent' => $parent, 'child' => $child));
+		}
+	}
+
+	public function down()
+	{
+		foreach ($this->parents as $child => $parent) {
+			$this->delete('authitemchild', 'parent = ? and child = ?', array($parent, $child));
+		}
+
+		foreach ($this->authitems as $authitem) {
+			$this->delete('authitem', 'name = ?', array($authitem['name']));
+		}
+	}
+}
---
+++ protected/modules/OphDrPrescription/migrations/m150522_103956_addItemContinueByGp.php
@@ -0,0 +1,27 @@
+<?php
+
+class m150522_103956_addItemContinueByGp extends CDbMigration
+{
+	public function up()
+	{
+		$this->addColumn('ophdrprescription_item', 'continue_by_gp', 'tinyint(1) not null default 0');
+		$this->addColumn('ophdrprescription_item_version', 'continue_by_gp', 'tinyint(1) not null default 0');
+	}
+
+	public function down()
+	{
+		$this->dropColumn('ophdrprescription_item', 'continue_by_gp');
+		$this->dropColumn('ophdrprescription_item_version', 'continue_by_gp');
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
