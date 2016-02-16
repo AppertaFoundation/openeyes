69ae19459136bf60d4626d65df83994d2d450dc4
vetusko <vetusko@gmail.com>
2016-02-16 14:57:41 +0000

    IOL and predicted refraction fields are placed next to each other

--- protected/modules/OphTrOperationnote/views/default/form_Element_OphTrOperationnote_Cataract_OEEyeDraw_fields.php
+++ protected/modules/OphTrOperationnote/views/default/form_Element_OphTrOperationnote_Cataract_OEEyeDraw_fields.php
@@ -28,8 +28,21 @@
 		CHtml::listData(OphTrOperationnote_IOLType::model()->activeOrPk($element->iol_type_id)->findAll(array('condition'=>'private=1','order'=>'display_order')),'id','name'),
 	),
 	array('empty' => '- Please select -','divided' => true),$element->iol_hidden,array('field' => 4))?>
-<?php echo $form->textField($element, 'predicted_refraction',array(),array(),array_merge($form->layoutColumns,array('field'=>2)))?>
-<?php echo $form->textField($element, 'iol_power', array('hide' => $element->iol_hidden),array(),array_merge($form->layoutColumns,array('field'=>2)))?>
+<div id="div_Element_OphTrOperationnote_Cataract_iol_power" class="row field-row">
+     <div class="large-3 column">
+         <label for="Element_OphTrOperationnote_Cataract_iol_power">IOL power:</label>
+     </div>
+     <div class="large-2 column end">
+         <input id="Element_OphTrOperationnote_Cataract_iol_power" type="text" name="Element_OphTrOperationnote_Cataract[iol_power]" autocomplete="off" hide="" value="<?php echo $element->iol_power; ?>">
+     </div>
+     <div class="large-3 column">
+         <label for="Element_OphTrOperationnote_Cataract_predicted_refraction">Predicted refraction:</label>
+     </div>
+     <div class="large-2 column end">
+         <input id="Element_OphTrOperationnote_Cataract_predicted_refraction" type="text" name="Element_OphTrOperationnote_Cataract[predicted_refraction]" autocomplete="off" value="<?php echo $element->predicted_refraction; ?>">
+     </div>
+</div>
+
 <?php echo $form->dropDownList($element, 'iol_position_id', 'OphTrOperationnote_IOLPosition', array('empty'=>'- Please select -'),$element->iol_hidden,array('field'=>4))?>
 <?php echo $form->multiSelectList($element, 'OphTrOperationnote_CataractOperativeDevices', 'operative_devices', 'id', $this->getOperativeDeviceList($element), $this->getOperativeDeviceDefaults(), array('empty' => '- Devices -', 'label' => 'Devices'),false,false,null,false,false,array('field'=>4))?>
 <?php echo $form->multiSelectList($element, 'OphTrOperationnote_CataractComplications', 'complications', 'id', CHtml::listData(OphTrOperationnote_CataractComplications::model()->activeOrPk($element->cataractComplicationValues)->findAll(array('order'=>'display_order asc')), 'id', 'name'), null, array('empty' => '- Complications -', 'label' => 'Complications'),false,false,null,false,false,array('field'=>4))?>
