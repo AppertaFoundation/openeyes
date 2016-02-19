c07b77362df2ce623ec05d91a0f6d3ebe314f453
himanshupatel79 <contacttohimanshu@yahoo.com>
2016-02-19 15:45:56 +0000

    Centralised Duplicate prescription warning

--- protected/modules/OphInBiometry/assets/js/module.js
+++ protected/modules/OphInBiometry/assets/js/module.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
 
 	if(show_disable_manual_warning) {
 		var warning_message = 'No new biometry reports are available for this patient. Please generate a new event on your linked device first (e.g., IOL Master).';
-		var dialog_msg = '<div class="ui-dialog ui-widget ui-widget-content ui-corner-all dialog" id="dialog-msg" tabindex="-1" role="dialog" aria-labelledby="ui-id-1" style="outline: 0px; z-index: 10002; height: auto; width: 600px; top: 450px; left: 600px; display: block;">' +
+		var dialog_msg = '<div class="ui-dialog ui-widget ui-widget-content ui-corner-all dialog" id="dialog-msg" tabindex="-1" role="dialog" aria-labelledby="ui-id-1" style="outline: 0px; z-index: 10002; height: auto; width: 600px; position: fixed; top: 50%; left: 50%; margin-top: -110px; margin-left: -200px;">' +
 			'<div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix">' +
 			'<span id="ui-id-1" class="ui-dialog-title">No New Biometry Reports</span>' +
 			'</div><div id="site-and-firm-dialog" class="ui-dialog-content ui-widget-content" scrolltop="0" scrollleft="0" style="display: block; width: auto; min-height: 0px; height: auto;">' +
