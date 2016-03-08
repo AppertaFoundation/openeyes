901b8e3d807bba49db01d3e0f03b849b7f63b8be
Veta Veta <Veta Veta>
2016-03-08 21:30:44 +0000

    added time interval function + 2 new exports

--- protected/controllers/NodExportController.php
+++ protected/controllers/NodExportController.php
@@ -24,11 +24,16 @@ class NodExportController extends BaseController
 	 */
 	public $layout='//layouts/main';
         
-        protected $export_path;
-        protected $zip_name;
+    protected $export_path;
+    protected $zip_name;
         
-        protected $institution_code = "000001";
+    protected $institution_code = "000001";
 
+	private	$startDate;
+	private	$endDate;
+
+	private $allEpisodeIds;
+	
 	public function accessRules()
 	{
 		// TODO need to add NOD Export RBAC rule here!! now we restrict to admin only
@@ -52,7 +57,13 @@ class NodExportController extends BaseController
             if (!file_exists($this->export_path)) {
                 mkdir($this->export_path, 0777, true);
             }
+			
+			//TODO: need to set start date and end date
+			// TODO: we need to use values from the POST array
 
+			$this->startDate = '2015-01-01';
+			$this->endDate = '2016-03-08';
+			
             parent::init();
         }
 
@@ -164,5 +175,193 @@ EOL;
             fclose($df);
             return ob_get_clean();
         }
-        
+      
+	
+	public function actionGetAllEpisodeId(){
+		// TODO: we need to call all extraction functions from here!
+		$this->allEpisodeIds = array_merge($this->getEpisodePostOpComplication(), $this->actionEpisodeOperationCoPathology());
+		
+		print_r($this->allEpisodeIds);
+	}
+
+	private function getDateWhere($tablename){
+		if($this->startDate != ""){
+			$dateWhereStart = $tablename.".last_modified_date >= '".$this->startDate."'";
+		}
+		if($this->endDate != ""){
+			$dateWhereEnd = $tablename.".last_modified_date <= '".$this->endDate."'";
+		}
+		
+		$dateWhere = "";
+		if(isset($dateWhereStart) && isset($dateWhereEnd)){
+			$dateWhere = "WHERE ".$dateWhereStart." AND ".$dateWhereEnd;
+		}else if(isset($dateWhereStart)){
+			$dateWhere = "WHERE ".$dateWhereStart;
+		}else if(isset($dateWhereEnd)){
+			$dateWhere = "WHERE ".$dateWhereEnd;
+		}
+		
+		return $dateWhere;
+	}
+	
+	private function getEpisodePostOpComplication(){
+		
+		$dateWhere = $this->getDateWhere('et_ophciexamination_postop_complications');
+		
+		$dataQuery = "SELECT 
+                        episode.id AS EpisodeId, 
+                        ophciexamination_postop_et_complications.`operation_note_id` AS OperationId, 
+                        (SELECT CASE WHEN ophciexamination_postop_et_complications.`eye_id` = 1 THEN 'L' WHEN ophciexamination_postop_et_complications.`eye_id` = 2 THEN 'R' END ) AS Eye,
+                        ophciexamination_postop_complications.`code` AS ComplicationTypeId
+                        FROM episode
+                        JOIN `event` ON episode.id = `event`.`episode_id`
+                        JOIN et_ophciexamination_postop_complications ON `event`.id = et_ophciexamination_postop_complications.`event_id`
+                        JOIN ophciexamination_postop_et_complications ON et_ophciexamination_postop_complications.id = ophciexamination_postop_et_complications.`element_id`
+                        JOIN ophciexamination_postop_complications ON ophciexamination_postop_et_complications.`complication_id` = ophciexamination_postop_complications.id 
+						".$dateWhere;
+		
+		$data = Yii::app()->db->createCommand($dataQuery)->queryAll();
+		$csv = $this->array2Csv($data);
+          
+        file_put_contents($this->export_path . '/EpisodePostOpComplication.csv' , $csv);
+		
+		foreach($data as $row){
+			$episodeIds[] = $row["EpisodeId"];
+		}
+		
+		return $episodeIds;
+	}
+	
+	public function actionEpisodeOperationCoPathology(){
+		$tempTableQuery = <<<EOL
+			DROP TEMPORARY TABLE IF EXISTS tmp_pathology_type;
+			CREATE TEMPORARY TABLE tmp_pathology_type (
+				`nodcode` INT(10) UNSIGNED NOT NULL,
+				`term` VARCHAR(100)
+			);
+			INSERT INTO tmp_pathology_type (`nodcode`, `term`)
+			VALUES
+				(0, 'None'),
+				(1, 'Age related macular degeneration'),
+				(2, 'Amblyopia'),
+				(4, 'Diabetic retinopathy'),
+				(5, 'Glaucoma'),
+				(7, 'Degenerative progressive high myopia'),
+				(8, 'Ocular Hypertension'),
+				(11, 'Stickler Syndrome'),
+				(12, 'Uveitis'),
+				(13, 'Pseudoexfoliation'),
+				(13, 'phacodonesis'),
+				(18, 'macular hole'),
+				(19, 'epiretinal membrane'),
+				(20, 'retinal detachment ');
+EOL;
+		
+	
+        Yii::app()->db->createCommand($tempTableQuery)->execute();
+		
+		$dataQuery = "(SELECT
+                        op_event.id AS OperationId,
+                        (SELECT
+                                CASE
+                                        WHEN (proc_list.eye_id = 3) THEN 'B'
+                                        WHEN (proc_list.eye_id = 2) THEN 'R'
+                                        WHEN (proc_list.eye_id = 1) THEN 'L'
+                                    END
+                            ) AS Eye,
+                        IF(element_type.`name` = 'Trabeculectomy', 25,23)  AS ComplicationTypeId
+                    FROM
+                        `event` AS op_event
+                            JOIN
+                        `episode` ON op_event.episode_id = episode.id
+                            JOIN
+                        `event` AS previous_op_event ON previous_op_event.episode_id = episode.id
+                            AND previous_op_event.event_type_id = (SELECT id FROM event_type WHERE `name` = 'Operation Note')
+                            AND previous_op_event.created_date <= op_event.created_date
+                            JOIN
+                        `et_ophtroperationnote_procedurelist` AS proc_list ON proc_list.event_id = previous_op_event.id
+                            JOIN
+                        `ophtroperationnote_procedurelist_procedure_assignment` AS proc_list_asgn ON proc_list_asgn.procedurelist_id = proc_list.id
+                            JOIN
+                        proc ON proc_list_asgn.proc_id = proc.id
+                            JOIN
+                        ophtroperationnote_procedure_element ON ophtroperationnote_procedure_element.procedure_id = proc.id
+                            JOIN
+                        element_type ON ophtroperationnote_procedure_element.element_type_id = element_type.id
+                    WHERE
+                        element_type.`name` in ('Vitrectomy', 'Trabeculectomy')
+                        AND op_event.event_type_id = (SELECT id FROM event_type WHERE `name` = 'Operation Note'))
+                    UNION
+                    (SELECT
+                    op_event.id AS OperationId,
+                    (SELECT
+                            CASE
+                                    WHEN (proc_list.eye_id = 3) THEN 'B'
+                                    WHEN (proc_list.eye_id = 2) THEN 'R'
+                                    WHEN (proc_list.eye_id = 1) THEN 'L'
+                                END
+                        ) AS Eye,
+                    21 AS ComplicationTypeId
+                    FROM
+                        `event` AS op_event
+                            JOIN
+                        `episode` ON op_event.episode_id = episode.id
+                            JOIN
+                        `event` AS previous_op_event ON previous_op_event.episode_id = episode.id
+                            AND previous_op_event.event_type_id = (SELECT id FROM event_type WHERE `name` = 'Operation Note')
+                            AND previous_op_event.created_date <= op_event.created_date
+                            JOIN `et_ophtroperationnote_procedurelist` AS proc_list ON proc_list.event_id = previous_op_event.id
+                            JOIN `ophtroperationnote_procedurelist_procedure_assignment` AS proc_list_asgn ON proc_list_asgn.procedurelist_id = proc_list.id
+                            JOIN proc ON proc_list_asgn.proc_id = proc.id
+                            JOIN procedure_benefit ON procedure_benefit.proc_id = proc.id
+                            JOIN benefit ON procedure_benefit.benefit_id = benefit.id
+                    WHERE
+                        benefit.`name` = 'to prevent retinal detachment'
+                        AND op_event.event_type_id = (SELECT id FROM event_type WHERE `name` = 'Operation Note'))
+                    UNION
+                    (SELECT op_event.id AS OperationId,
+						(SELECT CASE
+							WHEN (left_cortical_id = 4 OR left_nuclear_id = 4) AND (right_cortical_id = 4 OR right_nuclear_id = 4) THEN 'B'
+							WHEN (left_cortical_id = 4 OR left_nuclear_id = 4) THEN 'L'
+							WHEN (right_cortical_id = 4 OR right_nuclear_id = 4) THEN 'R'
+							END
+						) AS Eye,
+                    14 AS ComplicationTypeId
+                    From et_ophciexamination_anteriorsegment
+                    JOIN `event` AS exam_event on et_ophciexamination_anteriorsegment.event_id = exam_event.id
+                    JOIN `episode` ON exam_event.episode_id = episode.id
+                    JOIN `event` AS op_event
+                    ON episode.id = op_event.episode_id
+                    AND op_event.event_type_id = (select id from event_type where `name` = 'Operation Note')
+                    AND op_event.created_date >= exam_event.created_date
+                    HAVING Eye IS NOT NULL)
+                    UNION
+                    (SELECT
+                        event.id AS OperationId,
+                        (SELECT CASE
+                            WHEN secondary_diagnosis.eye_id = 1 THEN 'L'
+                            WHEN secondary_diagnosis.eye_id = 2 THEN 'R'
+                            WHEN secondary_diagnosis.eye_id = 3 THEN 'B'
+                            END
+                        ) AS Eye,
+                        tmp_pathology_type.nodcode as ComplicationTypeId
+                    FROM `event`
+                    JOIN `episode` ON `event`.episode_id = episode.id
+                    JOIN secondary_diagnosis ON episode.`patient_id` = secondary_diagnosis.`patient_id`
+                    JOIN `disorder` ON  secondary_diagnosis.`disorder_id` = `disorder`.id
+                    JOIN tmp_pathology_type on LOWER(disorder.term) = LOWER(tmp_pathology_type.term)
+                    WHERE event_type_id = (SELECT id from event_type where `name` = 'Operation Note'))";
+		
+		$data = Yii::app()->db->createCommand($dataQuery)->queryAll();
+		$csv = $this->array2Csv($data);
+          
+        file_put_contents($this->export_path . '/EpisodeOperationCoPathology.csv' , $csv);
+		
+		foreach($data as $row){
+			$episodeIds[] = $row["OperationId"];
+		}
+		
+		return $episodeIds;
+	}
+
 }
