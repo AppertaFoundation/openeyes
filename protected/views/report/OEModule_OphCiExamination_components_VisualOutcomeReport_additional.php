ca01875d93856ab5721a5a2fb7b627f900c45f5a
Sabi <szabolcs.potondi@gmail.com>
2016-03-31 10:57:52 +0100

    OE-5650 : plural or singular form of the 'month' in the yAxis based on the number of months

--- protected/views/report/OEModule_OphCiExamination_components_VisualOutcomeReport_additional.php
+++ protected/views/report/OEModule_OphCiExamination_components_VisualOutcomeReport_additional.php
@@ -3,7 +3,7 @@
         var months = $('#visual-acuity-months').val();
         var type = $('input[name="type"]:checked').val();
         var type_text = type.charAt(0).toUpperCase() + type.slice(1);
-        OpenEyes.Dash.reports['<?=$report->graphId();?>'].yAxis[0].setTitle({ text: "Visual acuity " + months + " months after surgery (LogMAR)" });
+        OpenEyes.Dash.reports['<?=$report->graphId();?>'].yAxis[0].setTitle({ text: "Visual acuity " + months + " month" + (months > 1 ? 's' : '') + " after surgery (LogMAR)" });
         OpenEyes.Dash.reports['<?=$report->graphId();?>'].setTitle({ text: "Visual Acuity (" + type_text + ")" });
     }
 </script>
