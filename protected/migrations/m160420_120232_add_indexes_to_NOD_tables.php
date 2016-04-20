612be756a120ed3e9d616f347d41406097f322e0
Sabi <szabolcs.potondi@gmail.com>
2016-04-20 14:15:05 +0100

    NOD Export : add indexes to EpisodeVisualAcuity tables

--- protected/migrations/m160420_120232_add_indexes_to_NOD_tables.php
+++ protected/migrations/m160420_120232_add_indexes_to_NOD_tables.php
@@ -7,6 +7,14 @@ class m160420_120232_add_indexes_to_NOD_tables extends CDbMigration
             //EpisodeDrug
             $this->createIndex('medication_prescription_item_id', 'medication', 'prescription_item_id');
             $this->createIndex('medication_last_modified_date', 'medication', 'last_modified_date');
+            
+            //EpisodeVisualAcuity
+            $this->createIndex('ophciexamination_visual_acuity_unit_value_base_value', 'ophciexamination_visual_acuity_unit_value', 'base_value');
+            $this->createIndex('ophciexamination_visual_acuity_unit_name', 'ophciexamination_visual_acuity_unit', 'name');
+            $this->createIndex('ophciexamination_visualacuity_method_name', 'ophciexamination_visualacuity_method', 'name');
+            $this->createIndex('ophciexamination_visualacuity_reading_side', 'ophciexamination_visualacuity_reading', 'side');
+            $this->createIndex('et_ophciexamination_visualacuity_last_modified_date', 'et_ophciexamination_visualacuity', 'last_modified_date');
+            
 	}
 
 	public function down()
@@ -14,5 +22,13 @@ class m160420_120232_add_indexes_to_NOD_tables extends CDbMigration
             //EpisodeDrug
             $this->dropIndex('medication_prescription_item_id', 'medication');
             $this->dropIndex('medication_last_modified_date', 'medication');
+            
+            //EpisodeVisualAcuity
+            $this->dropIndex('ophciexamination_visual_acuity_unit_value_base_value', 'ophciexamination_visual_acuity_unit_value');
+            $this->dropIndex('ophciexamination_visual_acuity_unit_name', 'ophciexamination_visual_acuity_unit');
+            $this->dropIndex('ophciexamination_visualacuity_method_name', 'ophciexamination_visualacuity_method');
+            $this->dropIndex('ophciexamination_visualacuity_reading_side', 'ophciexamination_visualacuity_reading');
+            $this->dropIndex('et_ophciexamination_visualacuity_last_modified_date', 'et_ophciexamination_visualacuity');
+            
 	}
 }
