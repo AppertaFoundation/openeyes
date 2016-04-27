c6fce17d7a3e49d643daa96ee0a39b52372c10eb
Poornimapriya <poornimapriyas@across.co.uk>
2016-04-27 08:29:33 +0000

    Changed the column name for the unique_codes table

--- protected/migrations/m160426_222411_unique_codes.php
+++ protected/migrations/m160426_222411_unique_codes.php
@@ -7,7 +7,7 @@ class m160426_222411_unique_codes extends CDbMigration
             $this->createTable('unique_codes', array(
                 'id' => 'int unsigned NOT NULL AUTO_INCREMENT',
                 'code' => 'varchar(6) NOT NULL',
-                'status' => 'int(1) unsigned NOT NULL default 1',
+                'active' => 'int(1) unsigned NOT NULL default 1',
                 'PRIMARY KEY (`id`)',
                 'UNIQUE KEY `code` (`code`)',
             ), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci');
@@ -15,7 +15,6 @@ class m160426_222411_unique_codes extends CDbMigration
 
 	public function down()
 	{
-		$this->dropTable('unique_codes');
+            $this->dropTable('unique_codes');
 	}
-
 }
