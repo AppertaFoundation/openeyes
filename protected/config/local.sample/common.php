65a5bf1472c1076c2fb88b98a6e934d8379407dc
PoornimapriyaSevugarajan <poornimapriya@live.com>
2016-05-13 10:38:37 +0100

    Configuration for the salt and the check for episode in latest opnote event detection

--- protected/config/local.sample/common.php
+++ protected/config/local.sample/common.php
@@ -148,7 +148,8 @@ $config = array(
 				'client_id' => 'f3d259ddd3ed8ff3843839b',
 				'client_secret' => '4c7f6f8fa93d59c45502c0ae8c4a95b',
 			)
-		)
+		),
+            'salt' => 'SALTFORUNIQUEODES',
 	),
 );
 
