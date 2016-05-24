123a7b7e3e139bbfe8fd52b2006bd7fc731dcb0f
PoornimapriyaSevugarajan <poornimapriya@live.com>
2016-05-24 11:22:15 +0100

    Changes to log

--- protected/modules/OphCiExamination/migrations/m160519_102017_automatic_examination_event_log.php
+++ protected/modules/OphCiExamination/migrations/m160519_102017_automatic_examination_event_log.php
@@ -7,28 +7,25 @@ class m160519_102017_automatic_examination_event_log extends OEMigration {
             'id' => 'pk',
             'event_id' => 'int(10) unsigned',
             'unique_code' => 'varchar(6) NOT NULL',
-            'examination_data' => 'blob',
+            'examination_data' => 'text',
             'examination_date' => "datetime DEFAULT '1900-01-01 00:00:00'",
-            'import_success' => 'int(1) unsigned NOT NULL default 0',
+            'import_success' => 'int(11) NOT NULL default 3',
                 ), true);
         $this->createTable('import_status', array(
             'id' => 'pk',
-            'status' => 'int(1) unsigned not null',
             'status_value' => 'varchar(30) NOT NULL',
                 ));
         $this->insert('import_status', array(
-            'status' => '0',
-            'status_value' => 'Duplicate/Unfound Event',
-        ));
-        $this->insert('import_status', array(
-            'status' => '1',
             'status_value' => 'Success Event',
         ));
         $this->insert('import_status', array(
-            'status' => '2',
             'status_value' => 'Import Failure',
         ));
+        $this->insert('import_status', array(
+            'status_value' => 'Duplicate/Unfound Event',
+        ));
         $this->addForeignKey('automatic_examination_event_log_event_id_fk', 'automatic_examination_event_log', 'event_id', 'event', 'id');
+        $this->addForeignKey('import_status_automatic_examination_event_log_id_fk', 'automatic_examination_event_log', 'import_success', 'import_status', 'id');
     }
 
     public function down() {
