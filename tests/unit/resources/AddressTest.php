f7683c689244a6f36935db4e5146992d9804d216
petergallagher <peter.s.gallagher@gmail.com>
2016-06-13 09:07:00 +0100

    Fix code.

--- tests/unit/resources/AddressTest.php
+++ tests/unit/resources/AddressTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * OpenEyes
+ * OpenEyes.
  *
  * (C) OpenEyes Foundation, 2016
  * This file is part of OpenEyes.
@@ -9,8 +9,8 @@
  * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
  *
- * @package OpenEyes
  * @link http://www.openeyes.org.uk
+ *
  * @author OpenEyes <info@openeyes.org.uk>
  * @copyright Copyright (c) 2016, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
@@ -34,7 +34,7 @@ class AddressTest extends \PHPUnit_Framework_TestCase
 
         $test = new \OEModule\PASAPI\resources\Address('V1');
         $this->assertNull($test->saveModel($address));
-        $this->assertEquals(array("field: Test Error"), $test->errors);
+        $this->assertEquals(array('field: Test Error'), $test->errors);
     }
 
     public function test_saveModelValid()
--- views/admin/list_RemapValue.php
+++ views/admin/list_RemapValue.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * OpenEyes
+ * OpenEyes.
  *
  * (C) OpenEyes Foundation, 2016
  * This file is part of OpenEyes.
@@ -8,23 +8,22 @@
  * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
  *
- * @package OpenEyes
  * @link http://www.openeyes.org.uk
+ *
  * @author OpenEyes <info@openeyes.org.uk>
  * @copyright Copyright (c) 2016, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
-
 ?>
 
 <?php $this->renderPartial('//base/_messages'); ?>
 
 <div class="box admin">
     <header class="box-header">
-        <h2 class="box-title"><?php echo $title ? $title : "PASAPI Admin" ?></h2>
+        <h2 class="box-title"><?php echo $title ? $title : 'PASAPI Admin' ?></h2>
         <div class="box-actions">
-            <a class="button small warning" href="<?php echo Yii::app()->createUrl($this->module->getName() . '/admin/viewXpathRemaps'); ?>">Back to Index</a>
-            <a class="button small" href="<?php echo Yii::app()->createUrl($this->module->getName() . '/admin/create' . $model_class, array('id' => $remap->id)); ?>">Add New Value</a>
+            <a class="button small warning" href="<?php echo Yii::app()->createUrl($this->module->getName().'/admin/viewXpathRemaps'); ?>">Back to Index</a>
+            <a class="button small" href="<?php echo Yii::app()->createUrl($this->module->getName().'/admin/create'.$model_class, array('id' => $remap->id)); ?>">Add New Value</a>
         </div>
     </header>
 
@@ -42,16 +41,17 @@
             ?>
             <tr data-attr-id="<?php echo $model->id?>">
                 <td>
-                    <a href="<?php echo Yii::app()->createUrl($this->module->getName() . '/admin/update' . Helper::getNSShortname($model), array('id'=> $model->id)) ?>"><?php echo $model->input ?></a>
+                    <a href="<?php echo Yii::app()->createUrl($this->module->getName().'/admin/update'.Helper::getNSShortname($model), array('id' => $model->id)) ?>"><?php echo $model->input ?></a>
                 </td>
                 <td>
                     <?= $model->output ?>
                 </td>
                 <td>
-                    <a href="<?php echo Yii::app()->createUrl($this->module->getName() . '/admin/deleteRemapValue', array('id'=> $model->id)) ?>">Delete</a>
+                    <a href="<?php echo Yii::app()->createUrl($this->module->getName().'/admin/deleteRemapValue', array('id' => $model->id)) ?>">Delete</a>
                 </td>
             </tr>
             <?php
+
         }?>
         </tbody>
     </table>
