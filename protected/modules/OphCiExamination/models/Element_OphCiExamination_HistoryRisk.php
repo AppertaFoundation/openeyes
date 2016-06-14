7cd2daf0f8a86eb108f27517f047af846febd5e2
PoornimapriyaSevugarajan <poornimapriya@live.com>
2016-06-14 14:57:28 +0100

    Changed the relation for the model to save.

--- protected/modules/OphCiExamination/models/Element_OphCiExamination_HistoryRisk.php
+++ protected/modules/OphCiExamination/models/Element_OphCiExamination_HistoryRisk.php
@@ -57,6 +57,7 @@ class Element_OphCiExamination_HistoryRisk extends \BaseEventTypeElement
         // NOTE: you should only define rules for those attributes that
         // will receive user inputs.
         return array(
+                array('id, event_id,anticoagulant,alphablocker', 'safe'),
                 // The following rule is used by search().
                 // Please remove those attributes that should not be searched.
                 array('id, event_id,anticoagulant,alphablocker', 'safe', 'on' => 'search'),
