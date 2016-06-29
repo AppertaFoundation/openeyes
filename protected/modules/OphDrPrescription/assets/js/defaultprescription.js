ec93067b8f4c726678344ca40969fb9c4bc15313
Prasath Kumar Sagadevan <prasathkumars@yahoo.co.in>
2016-06-29 13:53:55 +0000

    Client validation for atleast one item in prescriptions update

--- protected/modules/OphDrPrescription/assets/js/defaultprescription.js
+++ protected/modules/OphDrPrescription/assets/js/defaultprescription.js
@@ -70,6 +70,40 @@ $('#prescription_items').delegate('a.removeItem', 'click', function () {
     var row = $(this).closest('tr');
     var drug_id = row.find('input[name$="[drug_id]"]').first().val();
     var key = row.attr('data-key');
+     if(key == 0)
+    {
+        var warning_message = 'Items cannot be blank';
+        
+        var p = $('#event-content');
+        var position = p.position();
+       // alert ('L->'+position.left+ ' T '+position.top);
+        var topdist = position.left+400;
+        var leftdist = position.top + 500;
+
+        var dialog_msg = '<div class="ui-dialog ui-widget ui-widget-content ui-corner-all dialog" id="dialog-msg" tabindex="-1" role="dialog" aria-labelledby="ui-id-1" style="outline: 0px; z-index: 10002; height: auto; width: 600px;  position: fixed; top: 50%; left: 50%; margin-top: -110px; margin-left: -200px;">' +
+            '<div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix">' +
+            '<span id="ui-id-1" class="ui-dialog-title">Confirm Prescription</span>' +
+            '</div><div id="site-and-firm-dialog" class="ui-dialog-content ui-widget-content" scrolltop="0" scrollleft="0" style="display: block; width: auto; min-height: 0px; height: auto;">' +
+            '<div class="alert-box alert with-icon"> <strong>WARNING: ' +  warning_message + ' </strong></div>' +
+            '<p>Do you want to continue with a new item?</p>' +
+            '<div style = "margin-top:20px; float:right">'+
+            '<input class="secondary small" id="prescription-yes" type="submit" name="yt0" style = "margin-right:10px" value="Yes" onclick="hide_dialog()">' +
+            '<input class="warning small" id="prescription-no" type="submit" name="yt0" value="No" onclick="goBack()">' +
+            '</div>';
+
+        var blackout_box = '<div id="blackout-box" style="position:fixed;top:0;left:0;width:100%;height:100%;background-color:black;opacity:0.6;z-index:10000">';
+
+
+        $(dialog_msg).prependTo("body");
+        $(blackout_box).prependTo("body");
+        $('div#blackout_box').css.opacity = 0.6;
+        $("input#prescription-no").focus();
+        $("input#prescription-yes").keyup(function (e) {
+            hide_dialog();
+        });
+        
+    }
+    else{
     $('#prescription_items tr[data-key="' + key + '"]').remove();
     decorateRows();
     var option = $('#common_drug_id option[value="' + drug_id + '"]');
@@ -78,6 +112,7 @@ $('#prescription_items').delegate('a.removeItem', 'click', function () {
         applyFilter();
     }
     return false;
+    }
 });
 
 // Add taper to item
