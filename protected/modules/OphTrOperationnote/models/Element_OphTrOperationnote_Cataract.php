a26e8e0b56d2f8a5db1934aca7a0f274071cd820
Prasath Kumar Sagadevan <prasathkumars@yahoo.co.in>
2016-07-05 12:57:44 +0000

    Changed the iol power value might be an optional with 2 decimal

--- protected/modules/OphTrOperationnote/models/Element_OphTrOperationnote_Cataract.php
+++ protected/modules/OphTrOperationnote/models/Element_OphTrOperationnote_Cataract.php
@@ -91,16 +91,14 @@ class Element_OphTrOperationnote_Cataract extends Element_OnDemand {
      public function validateIolPower() {
 
         $value = $this->iol_power;
-        if (!is_numeric($value)) {
-            $message = $this->addError('iol_power', 'IOL Power must be a number.');
+        if (!preg_match('/^\-?[0-9]{1,2}(\.[0-9]{1,2})?$/', $value)) {
+                $message = $this->addError('iol_power', 'IOL power must be a number with an optional 2 decimal places.');
         }
         elseif ($value < -10 || $value > 40) {
             $message = $this->addError('iol_power', 'IOL Power must be between -10 to 40');
             
         }
-        elseif (!preg_match('/^\-?[0-9]{1,2}(\.[0-9]{1,2})?$/', $value)) {
-                $message = $this->addError('iol_power', 'IOL power must be a number with an optional 2 decimal places.');
-        }        
+                
         
     }
    
