60d77e23cab41e5d40732708a0658ac83ca488f4
Tamas Vedelek <vetusko@gmail.com>
2016-07-29 16:32:55 +0100

    - added base admin controller for local authorities

--- controllers/LocalAuthoritiesAdminController.php
+++ controllers/LocalAuthoritiesAdminController.php
@@ -2,14 +2,69 @@
 /**
  * Created by PhpStorm.
  * User: veta
- * Date: 29/07/2016
- * Time: 15:55
+ * Date: 06/05/15
+ * Time: 11:30
  */
-
 namespace OEModule\OphCoCvi\controllers;
 
 
-class LocalAuthoritiesAdminController
+class LocalAuthoritiesAdminController extends \BaseAdminController
 {
 
+    /**
+     * @var int
+     */
+    public $itemsPerPage = 100;
+
+    /**
+     * Lists lens types
+     *
+     * @throws CHttpException
+     */
+    public function actionList()
+    {
+        $admin = new \Admin(\CommissioningBodyService::model(), $this);
+        $admin->setListFields(array(
+            'id',
+            'name',
+            'code'
+        ));
+        $admin->setModelDisplayName('Local Authorities');
+        $admin->searchAll();
+        $admin->getSearch()->addActiveFilter();
+        $admin->getSearch()->setItemsPerPage($this->itemsPerPage);
+        $admin->listModel();
+    }
+
+    /**
+     * Edits or adds a lens type
+     *
+     * @param bool|int $id
+     * @throws CHttpException
+     */
+    public function actionEdit($id = false)
+    {
+        $admin = new \Admin(\CommissioningBodyService::model(), $this);
+        if($id){
+            $admin->setModelId($id);
+        }
+        $admin->setModelDisplayName('Local Authorities');
+        $admin->setEditFields(array(
+            'name' => 'text',
+            'code' => 'text',
+            'address' => 'text'
+        ));
+        $admin->editModel();
+    }
+
+    /**
+     * Deletes rows for the model
+     */
+    public function actionDelete()
+    {
+        $admin = new \Admin(\CommissioningBodyService::model(), $this);
+        $admin->deleteModel();
+    }
+
 }
+
