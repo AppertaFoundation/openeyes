763acc685ae2ac456b0d28ed3b9972f06b6c2609
Tamas Vedelek <vetusko@gmail.com>
2016-07-29 16:32:19 +0100

    - Added the basic admin controller

---
+++ controllers/LocalAuthoritiesAdminController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: veta
+ * Date: 29/07/2016
+ * Time: 15:55
+ */
+
+namespace OEModule\OphCoCvi\controllers;
+
+
+class LocalAuthoritiesAdminController
+{
+
+}
