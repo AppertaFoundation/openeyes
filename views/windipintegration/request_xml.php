928596aca1d4a85d0f59330e2d20652d1da649c8
Mike Smith <mike.smith@camc-ltd.co.uk>
2016-08-02 12:16:46 +0100

    + windip request template tidy up

--- views/windipintegration/request_xml.php
+++ views/windipintegration/request_xml.php
@@ -1,5 +1,3 @@
-<?xml version="1.0" encoding="utf-8" ?><linkparameters> <security><authentication>#AuthenticationHash#</authentication> <timestamp>2007-11-13 15:44:36</timestamp><xmlid>5E6B4D07-636C-4e23- 94C6802E9D1842B9</xmlid></security><revision number="3.1" /><applicationid>FE6164CC-3EDA-405c-8A59- 2F6358B21AF9</applicationid><user><id>PCN</id><name>Norton, Paul</name></user><primarylink><process>0</process> <uniquereference type="I">UQ17357239946</uniquereference><typeid>REF01</typeid> <eventdate>2007-03-29</eventdate><eventtime>13:27:21</eventtime></primarylink> <additionalindexes count="2"><index_1><id>DocumentID</id><value>DOC12345678</value> </index_1><index_2><id>Client Name</id><value>Gateway Computing Ltd</value></index_2> </additionalindexes><event><status>0</status><failuredescription></failuredescription></event> </linkparameters>
-
 <?xml version="1.0" encoding="utf-8" ?>
 <linkparameters>
 	<security><authentication><?=$authentication_hash?></authentication>
