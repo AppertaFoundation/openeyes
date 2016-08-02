90081a2ca544e16f7489d02903a0f4029848a1b6
petergallagher <peter.s.gallagher@gmail.com>
2016-08-02 09:50:01 +0100

    Adding back the build file Fixing npm/composer dependencies Changing docker to work with official mysql

--- bin/spin_up_docker_db.sh
+++ bin/spin_up_docker_db.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
+mkdir -p docker
+wget https://raw.githubusercontent.com/openeyes/Sample/master/sql/openeyes_sample_data.sql -N -O $PWD/docker/data.sql
 
-docker run -d --name $1 -p 3306 openeyestestdb
-sleep 5
-docker exec $1 /usr/local/bin/import.sh
+docker run --name $1 -v $PWD/docker:/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=rootpass -e MYSQL_DATABASE=openeyes -e MYSQL_USER=openeyes -e MYSQL_PASSWORD=oe_test -p 3306 -d mysql
 
 mysqlport=`docker ps -a | grep $1 | grep -Po '(\d+)->' | grep -o "[0-9]*"`
 
@@ -14,3 +14,6 @@ if [ "$mysqlport" ]; then
     sed -i.bak 's/port=[0-9]\+;/port='$mysqlport';/g' protected/config/local/test.php
     sed -i.bak 's/port=[0-9]\+;/port='$mysqlport';/g' protected/config/local/console.php
 fi
+
+#sleep so the database is ready when the script exits.
+sleep 10
