8b6444b826cee8df700accd375a111fc9d3aae48
petergallagher <peter.s.gallagher@gmail.com>
2016-08-02 10:43:54 +0100

    Change to mariadb

--- bin/spin_up_docker_db.sh
+++ bin/spin_up_docker_db.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 mkdir -p docker
-wget https://raw.githubusercontent.com/openeyes/Sample/master/sql/openeyes_sample_data.sql -N -O $PWD/docker/data.sql
+wget https://raw.githubusercontent.com/openeyes/Sample/master/sql/openeyes_sample_data.sql -N $PWD/docker/data.sql
 
-docker run --name $1 -v $PWD/docker:/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=rootpass -e MYSQL_DATABASE=openeyes -e MYSQL_USER=openeyes -e MYSQL_PASSWORD=oe_test -p 3306 -d mysql
+docker run --name $1 -v $PWD/docker:/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=rootpass -e MYSQL_DATABASE=openeyes -e MYSQL_USER=openeyes -e MYSQL_PASSWORD=oe_test -p 3306 -d mariadb
 
 mysqlport=`docker ps -a | grep $1 | grep -Po '(\d+)->' | grep -o "[0-9]*"`
 
