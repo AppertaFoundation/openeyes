4383246e0d4c176e889ebdcd29feab3670d1f198
Mike Smith <mike.smith@camc-ltd.co.uk>
2016-08-04 17:59:21 +0100

    + prescription view file further clean up

--- protected/modules/OphDrPrescription/views/default/view_Element_OphDrPrescription_Details.php
+++ protected/modules/OphDrPrescription/views/default/view_Element_OphDrPrescription_Details.php
@@ -18,31 +18,34 @@
  */
 ?>
 <table class="prescriptions">
-	<thead>
-		<tr>
-			<th>Drug</th>
-			<th>Dose</th>
-			<th>Route</th>
-			<th>Frequency</th>
-			<th>Duration</th>
-			<th>Continue by GP</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach ($element->items as $key => $item) { ?>
-		<tr	class="prescription-item<?php if ($this->patient->hasDrugAllergy($item->drug_id)) { ?> allergyWarning<?php } ?> <?php echo (($key % 2) == 0) ? 'even' : 'odd'; ?>">
-			<td class="prescription-label"><?php echo $item->drug->tallmanlabel; ?></td>
-			<td><?php echo $item->dose ?></td>
-			<td><?php echo $item->route->name ?>
-				<?php if ($item->route_option) { 
-					echo ' ('.$item->route_option->name.')'; 
-				} ?>
-			</td>
-			<td><?php echo $item->frequency->name ?></td>
-			<td><?php echo $item->duration->name ?></td>
-			<td><?php echo ($item->continue_by_gp) ? 'Yes' : 'No'; ?></td>
-		</tr>
-		<?php foreach ($item->tapers as $taper) {?>
+    <thead>
+    <tr>
+        <th>Drug</th>
+        <th>Dose</th>
+        <th>Route</th>
+        <th>Frequency</th>
+        <th>Duration</th>
+        <th>Continue by GP</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($element->items as $key => $item) {
+        $additional_class = $this->patient->hasDrugAllergy($item->drug_id) ? ' allergyWarning' : '';
+        $additional_class .= (($key % 2) == 0) ? 'even' : ' odd';
+        ?>
+        <tr class="prescription-item<?= $additional_class ?>">
+            <td class="prescription-label"><?php echo $item->drug->tallmanlabel; ?></td>
+            <td><?php echo $item->dose ?></td>
+            <td><?php echo $item->route->name ?>
+                <?php if ($item->route_option) {
+                    echo ' (' . $item->route_option->name . ')';
+                } ?>
+            </td>
+            <td><?php echo $item->frequency->name ?></td>
+            <td><?php echo $item->duration->name ?></td>
+            <td><?php echo ($item->continue_by_gp) ? 'Yes' : 'No'; ?></td>
+        </tr>
+        <?php foreach ($item->tapers as $taper) { ?>
             <tr class="prescription-tapier <?php echo (($key % 2) == 0) ? 'even' : 'odd'; ?>">
                 <td class="prescription-label"><span>then</span></td>
                 <td><?php echo $taper->dose ?></td>
@@ -50,23 +53,23 @@
                 <td><?php echo $taper->frequency->name ?></td>
                 <td><?php echo $taper->duration->name ?></td>
             </tr>
-		<?php }
-        } ?>
-	</tbody>
+        <?php }
+    } ?>
+    </tbody>
 </table>
 
-<input type="hidden" id="et_ophdrprescription_draft" value="<?php echo $element->draft?>" />
-<input type="hidden" id="et_ophdrprescription_print" value="<?php echo $element->print?>" />
+<input type="hidden" id="et_ophdrprescription_draft" value="<?php echo $element->draft ?>"/>
+<input type="hidden" id="et_ophdrprescription_print" value="<?php echo $element->print ?>"/>
 
-<?php if ($element->comments) {?>
-	<h3 class="element-title">
-		<?php echo CHtml::encode($element->getAttributeLabel('comments'))?>
-	</h3>
-	<div class="element-data">
-		<div class="data-row">
-			<div class="data-value">
-				<?php echo $element->textWithLineBreaks('comments')?>
-			</div>
-		</div>
-	</div>
+<?php if ($element->comments) { ?>
+    <h3 class="element-title">
+        <?php echo CHtml::encode($element->getAttributeLabel('comments')) ?>
+    </h3>
+    <div class="element-data">
+        <div class="data-row">
+            <div class="data-value">
+                <?php echo $element->textWithLineBreaks('comments') ?>
+            </div>
+        </div>
+    </div>
 <?php } ?>
