96e3e04785441b94ad74dd3d9b5d2fa93cd3d265
Mike Smith <mike.smith@camc-ltd.co.uk>
2016-08-17 12:16:16 +0100

    + fix migration for disorder id mapping

--- migrations/m160816_132509_add_disorder_mapping.php
+++ migrations/m160816_132509_add_disorder_mapping.php
@@ -4,7 +4,7 @@ class m160816_132509_add_disorder_mapping extends CDbMigration
 {
 	public function up()
 	{
-        $this->addColumn('ophcocvi_clinicinfo_disorder', 'disorder_id','int(10) unsigned NOT NULL');
+        $this->addColumn('ophcocvi_clinicinfo_disorder', 'disorder_id','int(10) unsigned');
         $this->addForeignKey('ophcocvi_clinicinfo_disorder_disorder_fk',
             'ophcocvi_clinicinfo_disorder','disorder_id',
             'disorder', 'id');
