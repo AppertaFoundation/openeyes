6af1f1196e4c9fe2d7ea2cc24e7cc4f4d98e31c4
Poornimapriya <poornimapriya@live.com>
2016-08-17 12:04:50 +0100

    OE-6119 : Added the column to the disorder version

--- migrations/m160816_132509_add_disorder_mapping.php
+++ migrations/m160816_132509_add_disorder_mapping.php
@@ -8,6 +8,7 @@ class m160816_132509_add_disorder_mapping extends CDbMigration
         $this->addForeignKey('ophcocvi_clinicinfo_disorder_disorder_fk',
             'ophcocvi_clinicinfo_disorder','disorder_id',
             'disorder', 'id');
+        $this->addColumn('ophcocvi_clinicinfo_disorder_version', 'disorder_id','int(10) unsigned NOT NULL');
 	}
 
 	public function down()
