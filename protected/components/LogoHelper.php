0516d614218962edc0149c92bf8c126bd2e46952
petergallagher <peter.s.gallagher@gmail.com>
2016-09-02 14:15:25 +0100

    OE-6178: Reverting changes as they neither solve the problem or meet quality standards.

--- protected/components/LogoHelper.php
+++ protected/components/LogoHelper.php
@@ -3,54 +3,42 @@
  * Created by PhpStorm.
  * User: PrasathKumar Sagadevan
  * Date: 07/06/16
- * Time: 15:45
+ * Time: 15:45.
  */
+class LogoHelper
+{
+    public function __construct()
+    {
+    }
 
-class LogoHelper {
-	public function __construct()
-	{
-		
-	}
+    public function render()
+    {
+        return Yii::app()->controller->renderPartial(
+            '//base/_logo',
+            array(
+                    'logo' => $this->getLogo(),
+            ),
+            true
+        );
+    }
 
-    /**
-     * @return mixed
-     */
-	public function render()
-	{
-		return Yii::app()->controller->renderPartial(
-			'//base/_logo',
-			array(
-			        'logo'=>$this->getLogo()
-			),
-			true
-		);
-	}
-
-    /**
-     * @return array
-     */
     protected function getLogo()
     {
         $logo = array();
-        if(Yii::app()->params['allow_logo_admin']===false)
-        {
-            $path = Yii::app()->basePath . '/assets/img/_print/';
-        }
-        else
-        {
-            $path = Yii::app()->basePath . '/runtime/';
-        }
+
+        $path = Yii::app()->basePath.'/runtime/';
         $yourImageUrl = Yii::app()->assetManager->publish($path);
         $imageLists = scandir($path, 1);
+
         foreach ($imageLists as $imageList) {
-            if(strpos($imageList,"header") !== false) {
-                $logo['headerLogo'] = $yourImageUrl."/".$imageList;
+            if (strpos($imageList, 'header') !== false) {
+                $logo['headerLogo'] = $yourImageUrl.'/'.$imageList;
             }
-            if(strpos($imageList,"secondary") !== false)  {
-                $logo['secondaryLogo'] = $yourImageUrl."/".$imageList;
+            if (strpos($imageList, 'secondary') !== false) {
+                $logo['secondaryLogo'] = $yourImageUrl.'/'.$imageList;
             }
         }
+
         return $logo;
     }
-
 }
--- protected/views/admin/sidebar.php
+++ protected/views/admin/sidebar.php
@@ -3,55 +3,44 @@
 		<h2>Admin</h2>
 	</div>
 </div>
-<?php
-$disableLink = array();
-foreach(Yii::app()->params['menu_disable_url'] as $menuKey => $menuValue)
-{
-	if(Yii::app()->params[$menuKey] === false)
-	{
-		$disableLink[] = $menuValue;
-	}
-}
-foreach (Yii::app()->params['admin_structure'] as $adminBoxTitle => $adminBoxElements) {
-	?>
+<?php foreach (Yii::app()->params['admin_structure'] as $adminBoxTitle => $adminBoxElements) { ?>
 	<div class="box admin">
 		<div class="box_admin_header">
 			<h2><?php echo $adminBoxTitle ?></h2>
 		</div>
 		<div class="box_admin_elements">
 			<ul class="navigation admin">
-				<?php
-				foreach ($adminBoxElements as $title => $uri) {
-					// need to check if function depends on a module
-					if (is_array($uri)) {
-						foreach ($uri as $moduleName => $adminUri) {
-							if (array_key_exists($moduleName, Yii::app()->modules)) {
-								$uri = $adminUri;
-							} else {
-								$uri = null;
-							}
-						}
-					}
-					if ($uri && !in_array($uri,$disableLink)) { ?>
+				<?php foreach ($adminBoxElements as $title => $uri) {
+    // need to check if function depends on a module
+                    if (is_array($uri)) {
+                        foreach ($uri as $moduleName => $adminUri) {
+                            if (array_key_exists($moduleName, Yii::app()->modules)) {
+                                $uri = $adminUri;
+                            } else {
+                                $uri = null;
+                            }
+                        }
+                    }
+                    if ($uri) { ?>
 						<li<?php
-						$requestUriArray = explode('?', Yii::app()->getController()->request->requestUri);
-						$requestUri = $requestUriArray[0];
-						if ($requestUri == $uri) { ?> class="selected"<?php } ?>>
+                        $requestUriArray = explode('?', Yii::app()->getController()->request->requestUri);
+        $requestUri = $requestUriArray[0];
+                        if ($requestUri == $uri) { ?> class="selected"<?php } ?>>
 							<?php if ($requestUri == $uri) { ?>
 								<script type="text/javascript">
 									$(document).ready(function () {
-											$('#<?php echo str_replace(' ','_',$title) ?>').closest('.box_admin_elements').show();
+											$('#<?php echo str_replace(' ', '_', $title) ?>').closest('.box_admin_elements').show();
 										}
 									);
 								</script>
 								<?php echo CHtml::link($title, array($uri),
-									array('class' => 'selected', 'id' => str_replace(' ', '_', $title))); ?>
+                                    array('class' => 'selected', 'id' => str_replace(' ', '_', $title))); ?>
 							<?php } else { ?>
 								<?php echo CHtml::link($title, array($uri)) ?>
 							<?php } ?>
 						</li>
 					<?php }
-				}  ?>
+                } ?>
 			</ul>
 		</div>
 	</div>
@@ -65,18 +54,18 @@ foreach (Yii::app()->params['admin_structure'] as $adminBoxTitle => $adminBoxEle
 		<div class="box_admin_elements">
 			<ul class="navigation admin">
 				<?php foreach ($items as $item => $uri) {
-					$e = explode('/', $uri);
-					$action = array_pop($e) ?>
+                    $e = explode('/', $uri);
+                    $action = array_pop($e) ?>
 					<li<?php if ($requestUri == $uri) { ?> class="selected"<?php } ?>>
 						<?php if ($requestUri == $uri) { ?>
 							<script type="text/javascript">
 								$(document).ready(function () {
-										$('#<?php echo str_replace(' ','_',$title) ?>').closest('.box_admin_elements').show();
+										$('#<?php echo str_replace(' ', '_', $title) ?>').closest('.box_admin_elements').show();
 									}
 								);
 							</script>
 							<?php echo CHtml::link($item, array($uri),
-								array('class' => 'selected', 'id' => str_replace(' ', '_', $title))) ?>
+                                array('class' => 'selected', 'id' => str_replace(' ', '_', $title))) ?>
 						<?php } else { ?>
 							<?php echo CHtml::link($item, array($uri)) ?>
 						<?php } ?>
