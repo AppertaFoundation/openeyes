cafab7ed0e7f7a3c8b8940838b30527e9a24a689
Mike Smith <mike.smith@camc-ltd.co.uk>
2016-09-08 12:17:45 +0100

    + code style

--- components/WinDipIntegration.php
+++ components/WinDipIntegration.php
@@ -31,185 +31,188 @@ namespace OEModule\Internalreferral\components;
  */
 class WinDipIntegration extends \CApplicationComponent implements ExternalIntegration
 {
-	protected $yii;
-	protected $required_params =  array(
-		'launch_uri',
-		'application_id',
-		'hashing_function',
-		'form_id'
-	);
+    protected $yii;
+    protected $required_params = array(
+        'launch_uri',
+        'application_id',
+        'hashing_function',
+        'form_id'
+    );
 
-	public $launch_uri;
-	public $application_id;
-	public $hashing_function;
-	public $form_id;
+    public $launch_uri;
+    public $application_id;
+    public $hashing_function;
+    public $form_id;
 
-	/**
-	 * Template path for the WinDip request
-	 *
-	 * @var string
-	 */
-	protected $request_template = 'Internalreferral.views.windipintegration.request_xml';
-	protected $new_event_template = 'Internalreferral.views.windipintegration.popup_newreferral';
+    /**
+     * Template path for the WinDip request
+     *
+     * @var string
+     */
+    protected $request_template = 'Internalreferral.views.windipintegration.request_xml';
+    protected $new_event_template = 'Internalreferral.views.windipintegration.popup_newreferral';
 
-	/**
-	 * WinDipIntegration constructor.
-	 *
-	 * @param \CApplication|null $yii
-	 * @param array $params
-	 * @throws \Exception
-	 */
-	public function init()
-	{
-		if (is_null($this->yii))
-			$this->yii = \Yii::app();
+    /**
+     * WinDipIntegration constructor.
+     *
+     * @param \CApplication|null $yii
+     * @param array $params
+     * @throws \Exception
+     */
+    public function init()
+    {
+        if (is_null($this->yii)) {
+            $this->yii = \Yii::app();
+        }
 
-		foreach ($this->required_params as $p) {
-			if (!isset($this->$p) || is_null($this->$p))
-				throw new \Exception("Missing required parameter {$p}");
-		}
-	}
+        foreach ($this->required_params as $p) {
+            if (!isset($this->$p) || is_null($this->$p)) {
+                throw new \Exception("Missing required parameter {$p}");
+            }
+        }
+    }
 
-	/**
-	 * Convenience wrapper to allow template rendering.
-	 *
-	 * @param $view
-	 * @param array $parameters
-	 * @return mixed
-	 */
-	public function renderPartial($view, $parameters = array())
-	{
-		return $this->yii->controller->renderPartial($view, $parameters, true);
-	}
+    /**
+     * Convenience wrapper to allow template rendering.
+     *
+     * @param $view
+     * @param array $parameters
+     * @return mixed
+     */
+    public function renderPartial($view, $parameters = array())
+    {
+        return $this->yii->controller->renderPartial($view, $parameters, true);
+    }
 
-	/**
-	 * Build a request for the given event
-	 *
-	 * @param \Event $event
-	 * @param \DateTime $when
-	 * @param $message_id
-	 * @return array
-	 */
-	protected function constructRequestData(\Event $event, \DateTime $when, $message_id)
-	{
-		//TODO: better way of handling mysql date to datetime
-		$event_date = \DateTime::createFromFormat('Y-m-d H:i:s', $event->event_date);
+    /**
+     * Build a request for the given event
+     *
+     * @param \Event $event
+     * @param \DateTime $when
+     * @param $message_id
+     * @return array
+     */
+    protected function constructRequestData(\Event $event, \DateTime $when, $message_id)
+    {
+        //TODO: better way of handling mysql date to datetime
+        $event_date = \DateTime::createFromFormat('Y-m-d H:i:s', $event->event_date);
 
-		$indexes = array();
-		$indexes[] = array(
-			'id'=>'hosnum',
-			'value'=> $event->episode->patient->hos_num
-		);
+        $indexes = array();
+        $indexes[] = array(
+            'id' => 'hosnum',
+            'value' => $event->episode->patient->hos_num
+        );
 
-		$user = \User::model()->findByPk(\Yii::app()->user->id);
+        $user = \User::model()->findByPk(\Yii::app()->user->id);
 
-		return array(
-			'timestamp' => $when->format('Y-m-d H:i:s'),
-			'message_id' => $message_id,
-			'application_id' => $this->application_id,
-			'username' => $user->username,
-			'user_displayname' => $user->getReversedFullName(),
-			'event_id' => $event->id,
-			// TODO: make this dynamic
-			'windip_type_id' => $this->form_id,
-			'event_date' => $event_date->format('Y-m-d'),
-			'event_time' => $event_date->format('H:i:s'),
-			'additional_indexes' => $indexes
-		);
-	}
+        return array(
+            'timestamp' => $when->format('Y-m-d H:i:s'),
+            'message_id' => $message_id,
+            'application_id' => $this->application_id,
+            'username' => $user->username,
+            'user_displayname' => $user->getReversedFullName(),
+            'event_id' => $event->id,
+            // TODO: make this dynamic
+            'windip_type_id' => $this->form_id,
+            'event_date' => $event_date->format('Y-m-d'),
+            'event_time' => $event_date->format('H:i:s'),
+            'additional_indexes' => $indexes
+        );
+    }
 
-	/**
-	 * Generate a unique ID for the event message to be sent to WinDip
-	 *
-	 * @TODO: determine if ID should be stored with the event and maintained for subsequent links
-	 *
-	 * @param \Event $event
-	 * @return string
-	 */
-	protected function getMessageId(\Event $event)
-	{
-		return \Helper::generateUuid();
-	}
+    /**
+     * Generate a unique ID for the event message to be sent to WinDip
+     *
+     * @TODO: determine if ID should be stored with the event and maintained for subsequent links
+     *
+     * @param \Event $event
+     * @return string
+     */
+    protected function getMessageId(\Event $event)
+    {
+        return \Helper::generateUuid();
+    }
 
-	/**
-	 * Generate the authentication hash for the WinDip request.
-	 *
-	 * @param $data
-	 * @return mixed
-	 * @throws \Exception
-	 */
-	private function generateAuthenticationHash($data)
-	{
-		if (!is_callable($this->hashing_function)) {
-			throw new \Exception("A hashing function must be provided to generate the authentication hash for the WinDip integration.");
-		}
+    /**
+     * Generate the authentication hash for the WinDip request.
+     *
+     * @param $data
+     * @return mixed
+     * @throws \Exception
+     */
+    private function generateAuthenticationHash($data)
+    {
+        if (!is_callable($this->hashing_function)) {
+            throw new \Exception("A hashing function must be provided to generate the authentication hash for the WinDip integration.");
+        }
 
-		return call_user_func($this->hashing_function, $this, $data, $this->request_template);
-	}
+        return call_user_func($this->hashing_function, $this, $data, $this->request_template);
+    }
 
-	/**
-	 *
-	 * @param \Event $event
-	 * @return mixed
-	 * @throws \Exception
-	 */
-	public function generateXmlRequest(\Event $event)
-	{
-		$when = new \DateTime();
-		$message_id = $this->getMessageId($event);
+    /**
+     *
+     * @param \Event $event
+     * @return mixed
+     * @throws \Exception
+     */
+    public function generateXmlRequest(\Event $event)
+    {
+        $when = new \DateTime();
+        $message_id = $this->getMessageId($event);
 
-		$data = $this->constructRequestData($event, $when, $message_id);
+        $data = $this->constructRequestData($event, $when, $message_id);
 
-		$data['authentication_hash'] = $this->generateAuthenticationHash($data);
+        $data['authentication_hash'] = $this->generateAuthenticationHash($data);
 
-		$request = $this->renderPartial($this->request_template, $data, true);
-		return $this->cleanRequest($request);
-	}
+        $request = $this->renderPartial($this->request_template, $data, true);
+        return $this->cleanRequest($request);
+    }
 
-	/**
-	 * Generate the external application URL for a new WinDip referral event.
-	 *
-	 * @param \Event $event
-	 * @return string
-	 */
-	public function generateUrlForNewEvent(\Event $event)
-	{
-		return $this->launch_uri . '?XML=' . urlencode($this->generateXmlRequest($event));
-	}
+    /**
+     * Generate the external application URL for a new WinDip referral event.
+     *
+     * @param \Event $event
+     * @return string
+     */
+    public function generateUrlForNewEvent(\Event $event)
+    {
+        return $this->launch_uri . '?XML=' . urlencode($this->generateXmlRequest($event));
+    }
 
-	/**
-	 * parsing function that ensure consistent clean up of request XML
-	 *
-	 * @param $request
-	 * @return string
-	 */
-	public function cleanRequest($request)
-	{
-		$request = preg_replace('/>\s+</', '><', $request);
-		$request = preg_replace('/[\n\r]/', '', $request);
-		return \CHtml::encode($request);
-	}
+    /**
+     * parsing function that ensure consistent clean up of request XML
+     *
+     * @param $request
+     * @return string
+     */
+    public function cleanRequest($request)
+    {
+        $request = preg_replace('/>\s+</', '><', $request);
+        $request = preg_replace('/[\n\r]/', '', $request);
+        return \CHtml::encode($request);
+    }
 
-	/**
-	 * Generates the HTML/JS to be inserted into the view page of the referral event.
-	 *
-	 * @param \Event $event
-	 * @return string
-	 */
-	public function renderEventView(\Event $event)
-	{
-		return $this->renderPartial($this->new_event_template, array(
-				'external_link' => $this->generateUrlForNewEvent($event),
-				'event' => $event)
-		);
-	}
+    /**
+     * Generates the HTML/JS to be inserted into the view page of the referral event.
+     *
+     * @param \Event $event
+     * @return string
+     */
+    public function renderEventView(\Event $event)
+    {
+        return $this->renderPartial($this->new_event_template, array(
+                'external_link' => $this->generateUrlForNewEvent($event),
+                'event' => $event
+            )
+        );
+    }
 
-	/**
-	 * @param array $data
-	 * @return array
-	 */
-	public function processExternalResponse($data = array())
-	{
-		return array(200, 'OK');
-	}
+    /**
+     * @param array $data
+     * @return array
+     */
+    public function processExternalResponse($data = array())
+    {
+        return array(200, 'OK');
+    }
 }
