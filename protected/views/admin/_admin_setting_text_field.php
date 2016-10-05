c0c4f60061f8aea60273e79b1fbc89f9116632e0
Prasath Kumar Sagadevan <prasathkumars@yahoo.co.in>
2016-10-05 12:20:48 +0530

    OE-6197 UI render crash when any combinations of words is entered in admin settings - user/admin banner

--- protected/views/admin/_admin_setting_text_field.php
+++ protected/views/admin/_admin_setting_text_field.php
@@ -17,7 +17,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
-<?php $currentValue = html_entity_decode($metadata->getSettingName());
+<?php $currentValue = $metadata->getSettingName();
       if (!isset($currentValue) || $currentValue == null) {
           // we need this to prevent HTML value without =
             $currentValue = '';
