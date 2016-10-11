c4db60f2121ca29a96da210f441edda9b9490b99
Poornimapriya Sevugarajan <poornimapriya@live.com>
2016-10-11 15:52:09 +0530

    OE-5376 : Configure devices for (cataract) op note via admin interface

---
+++ protected/modules/OphTrOperationnote/controllers/OperativeDeviceMappingController.php
@@ -0,0 +1,164 @@
+<?php
+
+/**
+ * OpenEyes.
+ *
+ * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
+ * (C) OpenEyes Foundation, 2011-2012
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.openeyes.org.uk
+ *
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2008-2011, Moorfields Eye Hospital NHS Foundation Trust
+ * @copyright Copyright (c) 2011-2012, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
+ */
+
+class OperativeDeviceMappingController  extends BaseAdminController
+{
+    public function actionList()
+    {
+        $admin = new AdminListAutocomplete(SiteSubspecialtyOperativeDevice::model(), $this);
+
+        $admin->setListFields(array(
+            'id',
+            'devices.name',
+            'default',
+        ));
+
+        $admin->setCustomDeleteURL('/OphTrOperationnote/OperativeDeviceMapping/delete');
+        $admin->setCustomSaveURL('/OphTrOperationnote/OperativeDeviceMapping/add');
+        $admin->setCustomSetDefaultURL('/OphTrOperationnote/OperativeDeviceMapping/setDefault');
+        $admin->setCustomRemoveDefaultURL('/OphTrOperationnote/OperativeDeviceMapping/removeDefault');
+        $admin->setModelDisplayName('Operation Note Operative Device Mapping');
+        $admin->setFilterFields(
+            array(
+                array(
+                    'label' => 'Site',
+                    'dropDownName' => 'site_id',
+                    'defaultValue' => Yii::app()->session['selected_site_id'],
+                    'listModel' => Site::model(),
+                    'listIdField' => 'id',
+                    'listDisplayField' => 'short_name',
+                ),
+                array(
+                    'label' => 'Subspecialty',
+                    'dropDownName' => 'subspecialty_id',
+                    'defaultValue' => Firm::model()->findByPk(Yii::app()->session['selected_firm_id'])->serviceSubspecialtyAssignment->subspecialty_id,
+                    'listModel' => Subspecialty::model(),
+                    'listIdField' => 'id',
+                    'listDisplayField' => 'name',
+                ),
+            )
+        );
+
+        // we set default search options
+        if ($this->request->getParam('search') == '') {
+            $admin->getSearch()->initSearch(array(
+                    'filterid' => array(
+                        'subspecialty_id' => Firm::model()->findByPk(Yii::app()->session['selected_firm_id'])->serviceSubspecialtyAssignment->subspecialty_id,
+                        'site_id' => Yii::app()->session['selected_site_id'],
+                    ),
+                )
+            );
+        }
+
+        $admin->setAutocompleteField(
+            array(
+                'fieldName' => 'operative_device_id',
+                'allowBlankSearch' => 1,
+                'jsonURL' => '/OphTrOperationnote/OperativeDeviceMapping/search',
+                'placeholder' => 'search for adding operative devices',
+            )
+        );
+        //$admin->searchAll();
+        $admin->listModel();
+    }
+
+    public function actionDelete($itemId)
+    {
+        /*
+        * We make sure to not allow deleting directly with the URL, user must come from the commondrugs list page
+        */
+        if (!Yii::app()->request->isAjaxRequest) {
+            $this->render('errorpage', array('errorMessage' => 'notajaxcall'));
+        } else {
+            if ($leafletSubspecialy = SiteSubspecialtyOperativeDevice::model()->findByPk($itemId)) {
+                $leafletSubspecialy->delete();
+                echo 'success';
+            } else {
+                $this->render('errorpage', array('errormessage' => 'recordmissing'));
+            }
+        }
+    }
+
+    public function actionAdd()
+    {
+        $subspecialtyId = $this->request->getParam('subspecialty_id');
+        $siteId = $this->request->getParam('site_id');
+        $operativeDeviceId = $this->request->getParam('operative_device_id');
+        if (!Yii::app()->request->isAjaxRequest) {
+            $this->render('errorpage', array('errormessage' => 'notajaxcall'));
+        } else {
+            if (!is_numeric($subspecialtyId) || !is_numeric($siteId) || !is_numeric($operativeDeviceId)) {
+                echo 'error';
+            } else {
+                $criteria = new CDbCriteria();
+                $criteria->condition = 'operative_device_id=:operative_device_id AND site_id=:site_id AND subspecialty_id=:subspecialty_id';
+                $criteria->params = array(
+                    ':operative_device_id' => $operativeDeviceId,
+                    ':site_id' => $siteId,
+                    ':subspecialty_id' => $subspecialtyId,
+                );
+                $currentSSAA = SiteSubspecialtyOperativeDevice::model()->findall($criteria);
+                if (!$currentSSAA) {
+                    $newSSAA = new SiteSubspecialtyOperativeDevice();
+                    $newSSAA->subspecialty_id = $subspecialtyId;
+                    $newSSAA->site_id = $siteId;
+                    $newSSAA->operative_device_id = $operativeDeviceId;
+                    if ($newSSAA->save()) {
+                        echo 'success added to SSAA';
+                    } else {
+                        echo 'error';
+                    }
+                } else {
+                    echo 'success';
+                }
+            }
+        }
+    }
+
+    public function actionSearch()
+    {
+        if (Yii::app()->request->isAjaxRequest) {
+            $criteria = new CDbCriteria();
+            if (isset($_GET['term'])) {
+                $term = $_GET['term'];
+                $criteria->addCondition(array('LOWER(name) LIKE :term'),
+                    'OR');
+                $params[':term'] = '%'.strtolower(strtr($term, array('%' => '\%'))).'%';
+            }
+
+            $criteria->order = 'name';
+            $criteria->select = 'id, name';
+            $criteria->params = $params;
+
+            $results = OperativeDevice::model()->active()->findAll($criteria);
+
+            $return = array();
+            foreach ($results as $resultRow) {
+                $return[] = array(
+                    'label' => $resultRow->name,
+                    'value' => $resultRow->name,
+                    'id' => $resultRow->id,
+                );
+            }
+            echo CJSON::encode($return);
+        }
+    }
+
+}
