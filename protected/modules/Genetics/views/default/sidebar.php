cf8507586e3c8574131656e7216119f961139e93
petergallagher <peter.s.gallagher@gmail.com>
2016-10-20 10:15:17 +0100

    Tidies up code.

--- protected/modules/Genetics/views/default/sidebar.php
+++ protected/modules/Genetics/views/default/sidebar.php
@@ -1,17 +1,18 @@
 <div class="box genetics panel">
-	<h2>Menu</h2>
-	<ul class="navigation">
-		<?php foreach (array(
-			'Pedigrees' => Yii::app()->createUrl('/Genetics/default/pedigrees'),
-			'Genes' => Yii::app()->createUrl('/Genetics/default/genes'),
-		) as $title => $uri) {?>
-			<li<?php if (Yii::app()->getController()->action->id == $uri) {?> class="selected"<?php }?>>
-				<?php if (Yii::app()->getController()->action->id == $uri) {?>
-					<?php echo CHtml::link($title,array($uri),array('class' => 'selected'))?>
-				<?php } else {?>
-					<?php echo CHtml::link($title,array($uri))?>
-				<?php }?>
-			</li>
-		<?php }?>
-	</ul>
+    <h2>Menu</h2>
+    <ul class="navigation">
+        <?php $sidebarLinks = array(
+            'Pedigrees' => Yii::app()->createUrl('/Genetics/default/pedigrees'),
+            'Genes' => Yii::app()->createUrl('/Genetics/default/genes'),
+        );
+        foreach ($sidebarLinks as $title => $uri) { ?>
+            <li<?php if (Yii::app()->getController()->action->id == $uri) { ?> class="selected"<?php } ?>>
+                <?php if (Yii::app()->getController()->action->id == $uri) { ?>
+                    <?php echo CHtml::link($title, array($uri), array('class' => 'selected')) ?>
+                <?php } else { ?>
+                    <?php echo CHtml::link($title, array($uri)) ?>
+                <?php } ?>
+            </li>
+        <?php } ?>
+    </ul>
 </div>
