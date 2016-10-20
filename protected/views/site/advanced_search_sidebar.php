724544e052e0072eb67839c3c1a8f7f7fa4e9e51
petergallagher <peter.s.gallagher@gmail.com>
2016-10-20 15:12:57 +0100

    Fixes a whole new other sidebar

--- protected/views/site/advanced_search_sidebar.php
+++ protected/views/site/advanced_search_sidebar.php
@@ -1,26 +1,26 @@
-<div class="box admin">
-	<h2>Core</h2>
-	<ul class="navigation admin">
-	</ul>
+<div class="box search panel">
+    <h2>Core</h2>
+    <ul class="navigation search">
+    </ul>
 </div>
 <?php foreach (Yii::app()->params['advanced_search'] as $module => $pages) {
-	if ($et = EventType::model()->find('class_name=?',array($module))) {
-		$name = $et->name;
-	} else {
-		$name = $module;
-	}?>
-	<div class="box admin">
-		<h2><?php echo $name?></h2>
-		<ul class="navigation admin">
-			<?php foreach ($pages as $title => $uri) {?>
-				<li<?php if (Yii::app()->getController()->action->id == preg_replace('/^\/admin\//','',$uri)) {?> class="selected"<?php }?>>
-					<?php if (Yii::app()->getController()->action->id == preg_replace('/^\/admin\//','',$uri)) {?>
-						<?php echo CHtml::link($title,Yii::app()->createUrl('/'.$module.'/search/'.$uri),array('class' => 'selected'))?>
-					<?php } else {?>
-						<?php echo CHtml::link($title,Yii::app()->createUrl('/'.$module.'/search/'.$uri))?>
-					<?php }?>
-				</li>
-			<?php }?>
-		</ul>
-	</div>
-<?php }?>
+    if ($et = EventType::model()->find('class_name=?', array($module))) {
+        $name = $et->name;
+    } else {
+        $name = $module;
+    } ?>
+    <div class="box search panel">
+        <h2><?php echo $name ?></h2>
+        <ul class="navigation search">
+            <?php foreach ($pages as $title => $uri) {
+                $class = '';
+                if(Yii::app()->getController()->action->id ==  $uri){
+                    $class = 'selected';
+                }?>
+                <li class="<?=$class?>">
+                    <?php echo CHtml::link($title, Yii::app()->createUrl('/' . $module . '/search/' . $uri), array('class' => $class)) ?>
+                </li>
+            <?php } ?>
+        </ul>
+    </div>
+<?php } ?>
