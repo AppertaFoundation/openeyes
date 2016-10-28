6d90df9bddf0924d6a793af92693965f15abe269
Mike Pargeter <mike.pargeter@across.co.uk>
2016-10-28 10:33:55 +0100

    OE-6239 Fix migration script to include _version table.

--- protected/modules/OphInBiometry/migrations/m161021_110716_add_eye_comments.php
+++ protected/modules/OphInBiometry/migrations/m161021_110716_add_eye_comments.php
@@ -2,26 +2,30 @@
 
 class m161021_110716_add_eye_comments extends CDbMigration
 {
-	public function up()
-	{
+    public function up()
+    {
         $this->addColumn('et_ophinbiometry_calculation', 'comments_right', 'text');
         $this->addColumn('et_ophinbiometry_calculation', 'comments_left', 'text');
-	}
+        $this->addColumn('et_ophinbiometry_calculation_version', 'comments_right', 'text');
+        $this->addColumn('et_ophinbiometry_calculation_version', 'comments_left', 'text');
+    }
 
-	public function down()
-	{
+    public function down()
+    {
         $this->dropColumn('et_ophinbiometry_calculation', 'comments_right');
         $this->dropColumn('et_ophinbiometry_calculation', 'comments_left');
-	}
+        $this->dropColumn('et_ophinbiometry_calculation_version', 'comments_right');
+        $this->dropColumn('et_ophinbiometry_calculation_version', 'comments_left');
+    }
 
-	/*
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-	}
+    /*
+    // Use safeUp/safeDown to do migration with transaction
+    public function safeUp()
+    {
+    }
 
-	public function safeDown()
-	{
-	}
-	*/
+    public function safeDown()
+    {
+    }
+    */
 }
