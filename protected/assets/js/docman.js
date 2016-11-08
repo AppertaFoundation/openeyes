15a012774d57d920872ef9d09fd26c1179d83b23
Sabi <szabolcs.potondi@gmail.com>
2016-11-08 15:19:49 +0000

    OE-6313 : replace tabs to spaces and fix some indents

--- protected/assets/js/docman.js
+++ protected/assets/js/docman.js
@@ -1,78 +1,78 @@
 var docman = (function() {
-	var data;		// holds the entire document data structure
-	var macros;		// macro names and ids
-	var dom_id;		// the ID of the div to contain the docman table
-	var prefix;		// prefix added to all DOM elements created by docman
-	var event_id;	// event id that this docset relates to
-	var baseUrl;
-	var allowAddDocument;
-	var module_correspondence=0;
+    var data;        // holds the entire document data structure
+    var macros;        // macro names and ids
+    var dom_id;        // the ID of the div to contain the docman table
+    var prefix;        // prefix added to all DOM elements created by docman
+    var event_id;    // event id that this docset relates to
+    var baseUrl;
+    var allowAddDocument;
+    var module_correspondence=0;
 
-	return {
+    return {
 
-                init: function(){
-                    $('#docman_block').on("click", '#docman_add_new', function(){
-                        // insert new edit row after the dm_table last TR
-                        var lastrow = $('#dm_table tr:last');
-                        this.createNewEntry(lastrow);
-                    });
-                    $('#docman_block').on("click", '#docman_add_new_recipient', function(e){
-                        e.preventDefault();
-                        docman.createNewRecipientEntry();
-                    });
-                    
-                    this.addHandlers();
-                },
+        init: function(){
+            $('#docman_block').on("click", '#docman_add_new', function(){
+                // insert new edit row after the dm_table last TR
+                var lastrow = $('#dm_table tr:last');
+                this.createNewEntry(lastrow);
+            });
+            $('#docman_block').on("click", '#docman_add_new_recipient', function(e){
+                e.preventDefault();
+                docman.createNewRecipientEntry();
+            });
 
-		//-------------------------------------------------------------
-		//  Sets the id of the containing DOM object, and the prefix
-		//  used for all DOM elements created within that.
-		//-------------------------------------------------------------
+            this.addHandlers();
+        },
 
-		setDOMid: function(id,prefix,event_id) {
-			this.dom_id = id;
-			this.prefix = prefix;
-			this.event_id = event_id;
-			this.allowAddDocument = '1';
+        //-------------------------------------------------------------
+        //  Sets the id of the containing DOM object, and the prefix
+        //  used for all DOM elements created within that.
+        //-------------------------------------------------------------
 
-		},
+        setDOMid: function(id,prefix,event_id) {
+            this.dom_id = id;
+            this.prefix = prefix;
+            this.event_id = event_id;
+            this.allowAddDocument = '1';
 
-
-		//-------------------------------------------------------------
-		//  Sets the event id
-		//-------------------------------------------------------------
-
-		setEventId: function(event_id) {
-			this.event_id = event_id;
-		},
+        },
 
 
-		//-------------------------------------------------------------
-		//  Clears the DOM ready to create the new table
-		//-------------------------------------------------------------
+        //-------------------------------------------------------------
+        //  Sets the event id
+        //-------------------------------------------------------------
 
-		clearDOM: function() {
-			var myNode = document.getElementById(this.dom_id);
-			var fc = myNode.firstChild;
-			while(fc) {
-				myNode.removeChild(fc);
-				fc = myNode.firstChild;
-			}
-			$("#"+this.dom_id).append("<table id='"+this.prefix+"table'>" +
-				"<tr id='"+this.prefix+"0'><th>Document Type</th><th>To/CC</th><th>Recipient</th>" + 
-				"<th>Delivery Method(s)</th><th>Delivery Status</th></tr></table>");
-		},
+        setEventId: function(event_id) {
+            this.event_id = event_id;
+        },
 
 
-		changeSelectedMacro: function(macro_id, element) {
-			if(macro_id !== undefined) {
-				this.fetchDocumentRecipients(macro_id, element);
-				if(this.module_correspondence == 1)
-				{
+        //-------------------------------------------------------------
+        //  Clears the DOM ready to create the new table
+        //-------------------------------------------------------------
+
+        clearDOM: function() {
+            var myNode = document.getElementById(this.dom_id);
+            var fc = myNode.firstChild;
+            while(fc) {
+                myNode.removeChild(fc);
+                fc = myNode.firstChild;
+            }
+            $("#"+this.dom_id).append("<table id='"+this.prefix+"table'>" +
+                "<tr id='"+this.prefix+"0'><th>Document Type</th><th>To/CC</th><th>Recipient</th>" + 
+                "<th>Delivery Method(s)</th><th>Delivery Status</th></tr></table>");
+        },
+
+
+        changeSelectedMacro: function(macro_id, element) {
+            if(macro_id !== undefined) {
+                this.fetchDocumentRecipients(macro_id, element);
+                if(this.module_correspondence == 1)
+                {
                     updateCorrespondence( macro_id );
-				}
-			}
-		},
+                }
+            }
+        },
 
 
         addMacroHandler: function(){
@@ -101,20 +101,20 @@ var docman = (function() {
             });
         },
 
-		addRemoveHandler: function(){
-			$('#docman_block').on("click", '.remove_recipient', function(event)
-			{
+        addRemoveHandler: function(){
+            $('#docman_block').on("click", '.remove_recipient', function(event)
+            {
                 event.target.closest('tr').remove();
                 //$('#dm_table tr:has(td[rowspan])').attr("rowspan", $(this).attr("rowspan")-1);
-			});
-		},
+            });
+        },
 
-		addHandlers: function(){
-			this.addMacroHandler();
-			this.addNewRecipientHandler();
-			this.addRemoveHandler();
+        addHandlers: function(){
+            this.addMacroHandler();
+            this.addNewRecipientHandler();
+            this.addRemoveHandler();
             this.addDocmanMethodMandatory();
-		},
+        },
 
         addDocmanMethodMandatory: function()
         {
@@ -154,8 +154,8 @@ var docman = (function() {
         },
 
         fetchDocumentRecipients: function (macroId, element) {
-			// perform AJAX call to fetch recipients, target options and output statuses
-			// write data to DOM
+            // perform AJAX call to fetch recipients, target options and output statuses
+            // write data to DOM
             $.ajax(
                 {
                     'type': 'GET',
@@ -169,47 +169,47 @@ var docman = (function() {
                     }
                 }
             );
-		},
+        },
 
 
-		//-------------------------------------------------------------
-		//  Fetches all the data for the entire document set, in JSON
-		//-------------------------------------------------------------
+        //-------------------------------------------------------------
+        //  Fetches all the data for the entire document set, in JSON
+        //-------------------------------------------------------------
 
-		getDocSet: function(eventId) {
-			$.ajax({
-				'type': 'GET',
-				'url': this.baseUrl + 'ajaxGetDocSet?id='+eventId,
-				'data':  null,
-				context: this,
-				'success': function(resp) {
-					this.data = resp;
-				}
-			});
-		},
+        getDocSet: function(eventId) {
+            $.ajax({
+                'type': 'GET',
+                'url': this.baseUrl + 'ajaxGetDocSet?id='+eventId,
+                'data':  null,
+                context: this,
+                'success': function(resp) {
+                    this.data = resp;
+                }
+            });
+        },
 
-		//-------------------------------------------------------------
-		//  Fetches all the data for the entire document set, in JSON
-		//-------------------------------------------------------------
+        //-------------------------------------------------------------
+        //  Fetches all the data for the entire document set, in JSON
+        //-------------------------------------------------------------
 
-		getDocTable: function(event_id, macro_id) {
-			var correspondence_mode = '';
+        getDocTable: function(event_id, macro_id) {
+            var correspondence_mode = '';
             if(this.module_correspondence == 1)
             {
                 correspondence_mode = '&in_correspondence=1';
             }
 
             $.ajax({
-				'type': 'GET',
-				'url': this.baseUrl + 'ajaxGetDocTable?id='+event_id+correspondence_mode,
-				'data':  null,
-				context: this,
-				dataType: 'html',
-				'success': function(resp) {
+                'type': 'GET',
+                'url': this.baseUrl + 'ajaxGetDocTable?id='+event_id+correspondence_mode,
+                'data':  null,
+                context: this,
+                dataType: 'html',
+                'success': function(resp) {
                     this.setDocTableToHTML(resp);
-				}
+                }
                     });
-		},
+        },
 
         setDocTableToHTML: function(data){
             $('#docman_block').html(data);
@@ -217,58 +217,57 @@ var docman = (function() {
                     if(macro_id > 0){
                         $('#macro_id').val(macro_id).change();
                     }
-		},
+        },
 
-		getRecipientData: function(contact_id, element) {
-			$.ajax({
-				'type': 'GET',
-				'url': this.baseUrl + 'ajaxGetContactData?contact_id='+contact_id+'&patient_id='+OE_patient_id,
-				context: this,
-				dataType: 'json',
-				'success': function(resp) {
-                                    rowindex = $(element).data("rowindex");
-                                    $('#address_'+rowindex).val(resp.address);
-                                    $('#contact_type_'+rowindex).val(resp.contact_type);
-                                    docman.setDeliveryMethods(rowindex);
-                                    
-				}
-			});
-		},
-
-		//------------------------------------------------------------
-		//  Create a new entry row at the end of the table
-		//------------------------------------------------------------
-		createNewEntry: function(element) {
-			$.ajax({
-				'type': 'GET',
-				'url': this.baseUrl + 'ajaxGetDocTableEditRow?patient_id='+OE_patient_id,
-				'data':  null,
-				context: this,
-				'success': function(resp) {
-					//console.log(resp);
-					element.before(resp);
-                    $('#docman_add_new').hide();
-				}
-			});
-		},
-
-		//------------------------------------------------------------
-		//  Create a new recipient entry row at the end of the table
-		//------------------------------------------------------------
-		createNewRecipientEntry: function()
-        {
-			last_row_index = this.getLastRowIndex();
+        getRecipientData: function(contact_id, element) {
             $.ajax({
-				'type': 'GET',
-				'url': this.baseUrl + 'ajaxGetDocTableRecipientRow?patient_id='+OE_patient_id+'&last_row_index='+last_row_index,
-				'context': this,
-				'success': function(resp) {
+                'type': 'GET',
+                'url': this.baseUrl + 'ajaxGetContactData?contact_id='+contact_id+'&patient_id='+OE_patient_id,
+                context: this,
+                dataType: 'json',
+                'success': function(resp) {
+                    rowindex = $(element).data("rowindex");
+                    $('#address_'+rowindex).val(resp.address);
+                    $('#contact_type_'+rowindex).val(resp.contact_type);
+                    docman.setDeliveryMethods(rowindex);                
+                }
+            });
+        },
+
+        //------------------------------------------------------------
+        //  Create a new entry row at the end of the table
+        //------------------------------------------------------------
+        createNewEntry: function(element) {
+            $.ajax({
+                'type': 'GET',
+                'url': this.baseUrl + 'ajaxGetDocTableEditRow?patient_id='+OE_patient_id,
+                'data':  null,
+                context: this,
+                'success': function(resp) {
+                    //console.log(resp);
+                    element.before(resp);
+                    $('#docman_add_new').hide();
+                }
+            });
+        },
+
+        //------------------------------------------------------------
+        //  Create a new recipient entry row at the end of the table
+        //------------------------------------------------------------
+        createNewRecipientEntry: function()
+        {
+            last_row_index = this.getLastRowIndex();
+            $.ajax({
+                'type': 'GET',
+                'url': this.baseUrl + 'ajaxGetDocTableRecipientRow?patient_id='+OE_patient_id+'&last_row_index='+last_row_index,
+                'context': this,
+                'success': function(resp) {
                     $('#dm_table tr:last').before(resp);
                     this.addRemoveHandler();
                     this.addDocmanMethodMandatory();
-				}
-			});
-		},
+                }
+            });
+        },
 
         getLastRowIndex: function()
         {
@@ -282,47 +281,47 @@ var docman = (function() {
             return last_row_index;
         },
 
-		//-------------------------------------------------------------
-		//  Process change of letter macro select box
-		//-------------------------------------------------------------
+        //-------------------------------------------------------------
+        //  Process change of letter macro select box
+        //-------------------------------------------------------------
 
 
-		//-------------------------------------------------------------
-		//  Return the count of document instances defined
-		//-------------------------------------------------------------
+        //-------------------------------------------------------------
+        //  Return the count of document instances defined
+        //-------------------------------------------------------------
 
-		getDocCount: function() {
-			if(this.data) {
-				return this.data.docinst.length;
-			} else {
-				return 0;
-			}
-		},
-
-
-		//-------------------------------------------------------------
-		//  Finds the last numbered row in the doc table
-		//-------------------------------------------------------------
-
-		getLastTableRowId: function() {
-			var n = 0;
-			while(1) {
-				var x = this.prefix+"tablerow_"+n;
-				if(!document.getElementById(x)) break;
-			}
-			return n;
-		},
+        getDocCount: function() {
+            if(this.data) {
+                return this.data.docinst.length;
+            } else {
+                return 0;
+            }
+        },
 
 
-		editAddress: function(addressId) {
+        //-------------------------------------------------------------
+        //  Finds the last numbered row in the doc table
+        //-------------------------------------------------------------
+
+        getLastTableRowId: function() {
+            var n = 0;
+            while(1) {
+                var x = this.prefix+"tablerow_"+n;
+                if(!document.getElementById(x)) break;
+            }
+            return n;
+        },
+
+
+        editAddress: function(addressId) {
             $('#docman_edit_button_'+addressId).hide();
-			data = $('#docman_address_'+addressId).html();
-			$('#docman_address_'+addressId).html('<textarea rows="3" cols="10" id="docman_address_edit_'+addressId+'">'+data+'</textarea><button onclick="docman2.saveAddress(event, '+addressId+')" class="secondary small right" >Save</button>');
-		},
+            data = $('#docman_address_'+addressId).html();
+            $('#docman_address_'+addressId).html('<textarea rows="3" cols="10" id="docman_address_edit_'+addressId+'">'+data+'</textarea><button onclick="docman2.saveAddress(event, '+addressId+')" class="secondary small right" >Save</button>');
+        },
 
         saveAddress: function(event, addressId){
             event.preventDefault();
-			$.ajax(
+            $.ajax(
                 {
                     // TODO: should be POST but we need the YII_TOKEN for that!!!
                     'type': 'GET',
@@ -337,25 +336,25 @@ var docman = (function() {
             );
         },
 
-		//-------------------------------------------------------------
-		//  test / junk
-		//-------------------------------------------------------------
+        //-------------------------------------------------------------
+        //  test / junk
+        //-------------------------------------------------------------
 
-		getEventId: function() {
+        getEventId: function() {
             return 1001;
-			//return this.data.data.docset[0].event_id;
-		},
-				
-		f1: function() {
-		  return 6;
-		},
+            //return this.data.data.docset[0].event_id;
+        },
+                
+        f1: function() {
+          return 6;
+        },
 
-		f2: function() {
-		  var x = this.f1() /2;
-		  alert(x);
-		  return x;
-		},
+        f2: function() {
+          var x = this.f1() /2;
+          alert(x);
+          return x;
+        },
 
-	};
+    };
 })();
 
