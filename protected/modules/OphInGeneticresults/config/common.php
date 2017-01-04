b40dbd705e108e9e7aac001e95768bce914c9c93
Ervin Kovacs <kovacs.ervin87@gmail.com>
2017-01-04 12:35:15 +0100

    Rename Genetic Test Result module to Genetic Result

---
+++ protected/modules/OphInGeneticresults/config/common.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'params' => array(
+        'advanced_search' => array(
+            'OphInGeneticresults' => array(
+                'Genetic Results' => 'geneticResults',
+            ),
+        ),
+    ),
+);
