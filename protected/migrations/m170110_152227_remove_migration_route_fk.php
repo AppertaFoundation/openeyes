01b883b97abdcb470fda091a55f245e18603ac9c
paulhooperAcross <paul.hooper@across.co.uk>
2017-01-11 15:55:58 +0000

    OE-5621 - Making Route optional

--- protected/migrations/m170110_152227_remove_migration_route_fk.php
+++ protected/migrations/m170110_152227_remove_migration_route_fk.php
@@ -4,18 +4,12 @@ class m170110_152227_remove_migration_route_fk extends CDbMigration
 {
 +	public function up()
  +	{
- +        if (isset(Yii::app()->params['enable_concise_med_history']) && Yii::app()->params['enable_concise_med_history'])
- +        {
- +            $this->alterColumn('medication', 'route_id', 'int(10) unsigned DEFAULT NULL');
- +        }
+        $this->alterColumn('medication', 'route_id', 'int(10) unsigned DEFAULT NULL');
 +	}
  +
  +	public function down()
  +	{
-    +        if (isset(Yii::app()->params['enable_concise_med_history']) && Yii::app()->params['enable_concise_med_history'])
-        +        {
-        +            $this->alterColumn('medication', 'route_id', 'int(10) unsigned NOT NULL');
-        +        }
+        $this->alterColumn('medication', 'route_id', 'int(10) unsigned NOT NULL');
  +	}
 	/*
 	// Use safeUp/safeDown to do migration with transaction
