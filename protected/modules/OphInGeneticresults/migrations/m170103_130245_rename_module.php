9db0764f122a1c9b7fe3f46694a9fc8867dd7f08
Ervin Kovacs <kovacs.ervin87@gmail.com>
2017-01-11 11:54:46 +0100

    Fixed down function

--- protected/modules/OphInGeneticresults/migrations/m170103_130245_rename_module.php
+++ protected/modules/OphInGeneticresults/migrations/m170103_130245_rename_module.php
@@ -62,7 +62,51 @@ class m170103_130245_rename_module extends OEMigration
 	public function down()
 	{
 		
-		return true;
+		if ($this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name' => 'OphInGeneticresults'))->queryRow()) {
+            $group = $this->dbConnection->createCommand()->select('id')->from('event_group')->where('name=:name', array(':name' => 'Investigation events'))->queryRow();
+            $rowID = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name' => 'OphInGeneticresults'))->queryRow();
+            $this->update('event_type', array('class_name' => 'OphInGenetictest', 'name' => 'Genetic Results') , 'id = '.$rowID['id'].' AND event_group_id = '.$group['id']);
+        }
+        
+        $this->update("element_type", array( "class_name" => "Element_OphInGenetictest_Test"), "class_name = 'Element_OphInGeneticresults_Test'");
+        
+        $this->renameTable('ophingeneticresults_test_method', 'ophingenetictest_test_method');
+        $this->renameTable('ophingeneticresults_test_effect', 'ophingenetictest_test_effect');
+        $this->renameTable('et_ophingeneticresults_test', 'et_ophingenetictest_test');
+        
+        $this->delete('authitemchild', "parent = 'Genetics Admin' AND child = 'TaskEditGeneticResults'");
+        $this->delete('authitemchild', "parent = 'Genetics Admin' AND child = 'TaskViewGeneticResults'");
+        $this->delete('authitemchild', "parent = 'Genetics User' AND child = 'TaskViewGeneticResults'");
+        
+        $this->delete('authitemchild', "parent = 'TaskEditGeneticResults' AND child = 'OprnEditGeneticResults'");
+        $this->delete('authitemchild', "parent = 'TaskViewGeneticResults' AND child = 'OprnViewGeneticResults'");
+        
+        $this->update('authitem', array('name' => 'OprnViewGeneticTest', 'type' => 1), "name = 'OprnViewGeneticResults'");
+        $this->update('authitem', array('name' => 'OprnEditGeneticTest', 'type' => 1), "name = 'OprnEditGeneticResults'");
+        
+        $this->insert('authitemchild', array('parent' => 'Genetics Admin', 'child' => 'TaskEditGeneticTest'));
+        $this->insert('authitemchild', array('parent' => 'Genetics Admin', 'child' => 'TaskViewGeneticTest'));
+        $this->insert('authitemchild', array('parent' => 'Genetics User', 'child' => 'TaskViewGeneticTest'));
+        
+        $this->insert('authitemchild', array('parent' => 'TaskEditGeneticTest', 'child' => 'OprnEditGeneticTest'));
+        $this->insert('authitemchild', array('parent' => 'TaskViewGeneticTest', 'child' => 'OprnViewGeneticTest'));
+        
+        $this->setEventTypeRBACSuffix('OphInGenetictest', 'GeneticTest');
+        
+        $this->delete('authitemchild', "parent = 'Genetics User' AND child = 'TaskCreateGeneticResults'");    
+        $this->delete('authitemchild', "parent = 'TaskCreateGeneticResults' AND child = 'OprnCreateGeneticResults'");
+        
+        $this->update('authitem', array('name' => 'TaskCreateGeneticTest', 'type' => 1), "name = 'TaskCreateGeneticResults'");
+        $this->insert('authitemchild', array('parent' => 'Genetics User', 'child' => 'TaskCreateGeneticTest'));
+        $this->update('authitem', array('name' => 'OprnCreateGeneticTest', 'type' => 0), "name = 'OprnCreateGeneticResults'");
+        $this->insert('authitemchild', array('parent' => 'TaskCreateGeneticTest', 'child' => 'OprnCreateGeneticTest'));
+        
+        $this->renameTable('ophingeneticresults_external_source', 'ophingenetictest_external_source');
+        
+        $this->versionExistingTable('ophingenetictest_test_method');
+        $this->versionExistingTable('ophingenetictest_test_effect');
+        
+        $this->renameTable('et_ophingeneticresults_test_version', 'et_ophingenetictest_test_version');
 	}
 
 	/*
