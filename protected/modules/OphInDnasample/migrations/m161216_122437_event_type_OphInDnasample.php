9270e3d8f7965635cd952685c5bea1fab5b0dde6
Ervin Kovacs <kovacs.ervin87@gmail.com>
2017-01-11 12:35:35 +0100

    Fixed down functions in DNA sample modul

--- protected/modules/OphInDnasample/migrations/m161216_122437_event_type_OphInDnasample.php
+++ protected/modules/OphInDnasample/migrations/m161216_122437_event_type_OphInDnasample.php
@@ -21,8 +21,21 @@ class m161216_122437_event_type_OphInDnasample extends CDbMigration
 
 	public function down()
 	{
-		return true;
-	}
+		if ($this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name' => 'OphInDnasample'))->queryRow()) {
+            $group = $this->dbConnection->createCommand()->select('id')->from('event_group')->where('name=:name', array(':name' => 'Investigation events'))->queryRow();
+            $rowID = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name' => 'OphInDnasample'))->queryRow();
+            $this->update('event_type', array('class_name' => 'OphInBloodsample', 'name' => 'DNA sample') , 'id = '.$rowID['id'].' AND event_group_id = '.$group['id']);
+        }
+        
+        $event_type = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name' => 'OphInBloodsample'))->queryRow();
+        
+        $element_type_row = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('class_name=:class_name', array(':class_name' => 'Element_OphInDnasample_Sample'))->queryRow();
+        $this->update('element_type', array( 'name' => 'Sample', 'class_name' => 'Element_OphInBloodsample_Sample', 'event_type_id' => $event_type['id'], 'display_order' => 1), 'id = '.$element_type_row['id'].' AND '.$event_type['id']);
+        
+        
+        $this->renameTable('ophindnasample_sample_type', 'ophinbloodsample_sample_type');
+        $this->renameTable('et_ophindnasample_sample', 'et_ophinbloodsample_sample');
+    }
 
 	/*
 	// Use safeUp/safeDown to do migration with transaction
