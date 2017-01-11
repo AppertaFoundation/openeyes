36b4971278d8b34a33b5c886c0cb0eb85b6d668d
Mike Pargeter <mike.pargeter@across.co.uk>
2017-01-11 16:03:47 +0000

    OE-5621 fix some errant + signs in migration script.

--- protected/migrations/m170110_152227_remove_migration_route_fk.php
+++ protected/migrations/m170110_152227_remove_migration_route_fk.php
@@ -2,15 +2,15 @@
 
 class m170110_152227_remove_migration_route_fk extends CDbMigration
 {
-+	public function up()
- +	{
-        $this->alterColumn('medication', 'route_id', 'int(10) unsigned DEFAULT NULL');
-+	}
- +
- +	public function down()
- +	{
-        $this->alterColumn('medication', 'route_id', 'int(10) unsigned NOT NULL');
- +	}
+	public function up()
+ 	{
+        	$this->alterColumn('medication', 'route_id', 'int(10) unsigned DEFAULT NULL');
+	}
+ 
+ 	public function down()
+ 	{
+        	$this->alterColumn('medication', 'route_id', 'int(10) unsigned NOT NULL');
+ 	}
 	/*
 	// Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
