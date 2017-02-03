5f748b8d2111e4f40ec2ca3d89e85101f04fbfd6
paulhooperAcross <paul.hooper@across.co.uk>
2017-02-03 09:24:28 +0000

    Keratoconus

---
+++ protected/modules/OphCiExamination/migrations/m170202_145813_create_specular_microscopy.php
@@ -0,0 +1,90 @@
+<?php
+
+class m170202_145813_create_specular_microscopy extends CDbMigration
+{
+
+    public function up()
+    {
+        $this->createTable('et_ophciexamination_specular_microscopy', array(
+            'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
+            'event_id' => 'int(10) unsigned NOT NULL',
+            'eye_id' => 'int(10) unsigned NOT NULL DEFAULT 3',
+            'microscope_id' => 'int(10) unsigned NOT NULL',
+            'scan_quality_id' => 'int(10) unsigned NOT NULL',
+            'endothelial_cell_density_value' => 'int(10) unsigned NOT NULL',
+            'coefficient_variation_value' => 'decimal(5,2) NOT NULL',
+            'last_modified_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
+            'last_modified_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
+            'created_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
+            'created_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
+            'PRIMARY KEY (`id`)',
+            'KEY `et_ophciexamination_specular_microscopy_lmui_fk` (`last_modified_user_id`)',
+            'KEY `et_ophciexamination_specular_microscopy_cui_fk` (`created_user_id`)',
+            'CONSTRAINT `et_ophciexamination_specular_microscopy_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)',
+            'CONSTRAINT `et_ophciexamination_specular_microscopy_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
+        ), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci');
+
+        $this->createTable('ophciexamination_specular_microscope', array(
+            'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
+            'value' => 'varchar NOT NULL',
+            'eye_id' => 'int(10) unsigned NOT NULL DEFAULT 3',
+            'microscope_id' => 'int(10) unsigned NOT NULL',
+            'scan_quality_id' => 'int(10) unsigned NOT NULL',
+            'endothelial_cell_density_value' => 'int(10) unsigned NOT NULL',
+            'coefficient_variation_value' => 'decimal(5,2) NOT NULL',
+            'last_modified_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
+            'last_modified_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
+            'created_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
+            'created_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
+            'PRIMARY KEY (`id`)',
+            'KEY `et_ophciexamination_specular_microscopy_lmui_fk` (`last_modified_user_id`)',
+            'KEY `et_ophciexamination_specular_microscopy_cui_fk` (`created_user_id`)',
+            'CONSTRAINT `et_ophciexamination_specular_microscopy_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)',
+            'CONSTRAINT `et_ophciexamination_specular_microscopy_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
+        ), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci');
+
+
+        $this->createTable('et_ophciexamination_keratometry', array(
+            'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
+            'event_id' => 'int(10) unsigned NOT NULL',
+            'eye_id' => 'int(10) unsigned NOT NULL DEFAULT 3',
+            'microscope_id' => 'int(10) unsigned NOT NULL',
+            'scan_quality_id' => 'int(10) unsigned NOT NULL',
+            'endothelial_cell_density_value' => 'int(10) unsigned NOT NULL',
+            'coefficient_variation_value' => 'decimal(5,2) NOT NULL',
+            'last_modified_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
+            'last_modified_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
+            'created_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
+            'created_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
+            'PRIMARY KEY (`id`)',
+            'KEY `et_ophciexamination_specular_microscopy_lmui_fk` (`last_modified_user_id`)',
+            'KEY `et_ophciexamination_specular_microscopy_cui_fk` (`created_user_id`)',
+            'CONSTRAINT `et_ophciexamination_specular_microscopy_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)',
+            'CONSTRAINT `et_ophciexamination_specular_microscopy_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
+        ), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci');
+
+
+//        $this->insert('ophciexamination_oct_fluidstatus', array('name' => 'New', 'display_order' => '1'));
+//        $this->insert('ophciexamination_oct_fluidstatus', array('name' => 'Improving', 'display_order' => '2'));
+//        $this->insert('ophciexamination_oct_fluidstatus', array('name' => 'Persistent', 'display_order' => '3'));
+
+
+    }
+
+    public function down()
+    {
+        $this->dropTable('et_ophciexamination_specular_microscopy');
+
+    }
+
+    /*
+    // Use safeUp/safeDown to do migration with transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
