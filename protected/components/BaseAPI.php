556d72640903bb9669f3dc3c74e8ac2ed9c3a179
Ervin Kovacs <kovacs.ervin87@gmail.com>
2017-02-22 17:08:54 +0100

    Missing function comment

--- protected/components/BaseAPI.php
+++ protected/components/BaseAPI.php
@@ -68,6 +68,14 @@ class BaseAPI
         }
     }
     
+    /**
+     * gets all element of type $element for the given patient in the given episode.
+     *
+     * @param Episode $episode - the episode
+     * @param string  $element - the element class
+     *
+     * @return unknown - the element type requested, or null
+     */
     public function getElementForAllEventInEpisode($episode, $element)
     {
         $event_type = $this->getEventType();
