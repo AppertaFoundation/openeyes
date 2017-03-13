894c8979eeaaaebf3d41854435960ebab40dcede
Peter Kecskes <kecsokoma@gmail.com>
2017-03-13 15:31:20 +0100

    patient general information to header

---
+++ protected/assets/js/patientGeneralInformations.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+	var patientGeneralInfoToolTip = new OpenEyes.UI.Tooltip({
+		offset: {
+			x: 16,
+			y: 16
+		},
+		viewPortOffset: {
+			x: 0,
+			y: 32 // height of sticky footer
+		}
+	});
+	
+	var patient_info_content = $('#patient_general_informations').html();
+	patientGeneralInfoToolTip.setContent(patient_info_content);
+    
+    $('.icon-patient-panel-info').on('mouseover', function() {
+    	var offsets = $(this).offset();
+        patientGeneralInfoToolTip.show(offsets.left, offsets.top);
+    }).mouseout(function (e) {
+		patientGeneralInfoToolTip.hide();
+	}); 
+});
