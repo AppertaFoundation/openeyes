3f413dc985014810c5fb03f0aa7f90b20a3af8ba
paulhooperAcross <paul.hooper@across.co.uk>
2017-03-29 09:05:48 +0100

    Keratoconus

--- protected/modules/OphCiExamination/widgets/OphCiExamination_Episode_KeratometryChartRight.php
+++ protected/modules/OphCiExamination/widgets/OphCiExamination_Episode_KeratometryChartRight.php
@@ -61,7 +61,7 @@ class OphCiExamination_Episode_KeratometryChartRight extends \EpisodeSummaryWidg
             ->configureYAxis("KmaxY", array('position' => 'right', 'min' => 30, 'max' => 80))
             ->configureSeries('Kmax', array('yaxis' => "KmaxY", 'lines' => array('show' => true), 'points' => array('show' => true)))
             ->configureSeries('K1', array('yaxis' => $this->kera_axis, 'lines' => array('show' => true), 'points' => array('show' => true)))
-            ->configureSeries('CXL Op', array('yaxis' => $this->kera_axis, 'lines' => array('show' => true), 'points' => array('show' => true)))
+            ->configureSeries('Intervention', array('yaxis' => $this->kera_axis, 'lines' => array('show' => true), 'points' => array('show' => true)))
             ->configureSeries('K2', array('yaxis' => $this->kera_axis, 'lines' => array('show' => true), 'points' => array('show' => true)));
 
         return $chart;
@@ -77,10 +77,12 @@ class OphCiExamination_Episode_KeratometryChartRight extends \EpisodeSummaryWidg
                     $this->addKeraReading($event, $chart, $reading);
                 }
         }
-        // For Operation Date line - just need to get the date from Op Note for this patient for Cross-Linking
-        // And also uncomment CXL Op series above.
-        $chart->addPoint("CXL Op", Helper::mysqlDate2JsTimestamp('2017-03-02 00:00:00'), '1', "Op");
-        $chart->addPoint("CXL Op", Helper::mysqlDate2JsTimestamp('2017-03-02 00:00:00'), '180', "Op");
+        if ($api = Yii::app()->moduleAPI->get('OphTrOperationnote')) {
+            $interventionDate = $api->getLastOperationDate($this->episode->patient);
+            echo $interventionDate;
+            $chart->addPoint("Intervention", Helper::mysqlDate2JsTimestamp($interventionDate), '1', "Intervention");
+            $chart->addPoint("Intervention", Helper::mysqlDate2JsTimestamp($interventionDate), '180', "Intervention");
+        }
 
     }
 
