bc2cd5bfe8347343dcc64d7e9a1f33dfde5c3cf9
Ervin Kovacs <kovacs.ervin87@gmail.com>
2017-04-10 16:21:09 +0200

    Table field position fix

--- protected/modules/OphCiExamination/migrations/m170407_160941_reposition_version_table_fields.php
+++ protected/modules/OphCiExamination/migrations/m170407_160941_reposition_version_table_fields.php
@@ -4,6 +4,8 @@ class m170407_160941_reposition_version_table_fields extends CDbMigration
 {
 	public function up()
 	{
+        $this->execute("ALTER TABLE `automatic_examination_event_log` MODIFY `invoice_status_id` text AFTER `comment` ");
+
         $this->execute("ALTER TABLE `automatic_examination_event_log_version` MODIFY `comment` text AFTER `created_date` ");
         $this->execute("ALTER TABLE `automatic_examination_event_log_version` MODIFY  `invoice_status_id` text AFTER `comment` ");
         $this->execute("ALTER TABLE `automatic_examination_event_log_version` MODIFY  `optometrist` text AFTER `invoice_status_id` ");
