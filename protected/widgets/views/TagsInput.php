c994e0921b166a02e9d68f255c68e27348dafa1b
feketecs <csaba.fekete@wecoonline.hu>
2017-05-09 08:57:46 +0200

    OE-6445 TagsInput widget

--- protected/widgets/views/TagsInput.php
+++ protected/widgets/views/TagsInput.php
@@ -27,12 +27,14 @@
         </label>
     </div>
     <div class="large-<?php echo $layoutColumns['field'];?> column end">
-        <input
-            name="<?php echo $field?>" id="tags"
-            value="<?php echo implode(',', $this->default_tags); ?>"
-            type="text" class="tagsinput"
-            <?php if($this->autocomplete_url){ echo 'data-autocomplete-url = "'.$this->autocomplete_url.'"';} ?>
-        />
+        <div class="oe-tagsinput-wrapper">
+            <input
+                name="<?php echo $field?>" id="tags"
+                value="<?php echo implode(',', $this->default_tags); ?>"
+                type="text" class="tagsinput"
+                <?php if($this->autocomplete_url){ echo 'data-autocomplete-url = "'.$this->autocomplete_url.'"';} ?>
+            />
+        </div>
     </div>
 </div>
 
