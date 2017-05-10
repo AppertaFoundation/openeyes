3ddf8bbc90286320fe833824aeb1dbfbe60d7303
Mike Smith <mike.smith@camc-ltd.co.uk>
2017-05-10 20:12:23 +0100

    patient allergy tracking view definitions for backward compatibility WIP

--- protected/modules/OphCiExamination/migrations/m170510_131532_event_allergy_record.php
+++ protected/modules/OphCiExamination/migrations/m170510_131532_event_allergy_record.php
@@ -14,6 +14,7 @@ class m170510_131532_event_allergy_record extends OEMigration
      * @param $source
      * @param $dest
      * @param $cols
+     * @todo: move to OEMigration
      */
     public function duplicateTable($source, $dest, $cols)
     {
@@ -27,6 +28,16 @@ class m170510_131532_event_allergy_record extends OEMigration
         foreach ($source_rows as $row) {
             $this->insert($dest, $row);
         }
+    }
+
+    public function createView($view_name, $select)
+    {
+        $this->dbConnection->createCommand('create view ' . $view_name . ' as ' . $select)->execute();
+    }
+
+    public function dropView($view_name)
+    {
+        $this->dbConnection->createCommand('drop view ' . $view_name)->execute();
     }
 
 	public function up()
@@ -86,30 +97,42 @@ class m170510_131532_event_allergy_record extends OEMigration
         $this->renameColumn('patient', 'no_allergies_date', static::$archive_prefix . 'no_allergies_date');
         $this->renameColumn('patient_version', 'no_allergies_date', static::$archive_prefix . 'no_allergies_date');
 
-        // TODO: create the following views for the patient relations to still work
-        /*
-        // event ids for most recent exam with allergy in it:
-        create view latest_allergy_examination_events as select distinct event_id, patient_id from et_ophciexamination_history et
-        join event on et.event_id = event.id
-        join episode on event.episode_id = episode.id
-        group by episode.patient_id
-        order by event.event_date desc, event.created_date desc
-
-        // actual assignments
-        create view patient_allergy_assignment as
-        select allergy_assignment.id, latest.patient_id as patient_id, allergy_assignment.allergy_id, allergy_assignment.other, allergy_assignment.comments from ophciexamination_allergy_entry as allergy_assignment
-            join et_ophciexamination_allergies element on allergy_assignment.element_id = element.id
-            join latest_allergy_examination_events latest on element.event_id = latest.event_id
-
-        // look up of allergies
-        create view allergy as select * from ophciexamination_allergy;
-         */
+        $this->createView('allergy_examination_events', <<<EOSQL
+select event_date, event.created_date, event_id, patient_id from et_ophciexamination_allergies et
+join event on et.event_id = event.id
+join episode on event.episode_id = episode.id
+EOSQL
+);
+        $this->createView('latest_allergy_examination_events', <<<EOSQL
+select t1.event_id, t1.patient_id from allergy_examination_events t1
+left outer join allergy_examination_events t2
+on t1.patient_id = t2.patient_id
+   and (t1.event_date < t2.event_date
+   		or (t1.event_date = t2.event_date and t1.created_date < t2.created_date))
+where t2.patient_id is null
+EOSQL
+);
+        $this->createView('patient_allergy_assignment', <<<EOSQL
+select aa.id, latest.patient_id as patient_id, aa.allergy_id, aa.other, aa.comments, aa.last_modified_user_id,
+aa.last_modified_date,
+aa.created_user_id,
+aa.created_date from ophciexamination_allergy_entry as aa 
+	join et_ophciexamination_allergies element on aa.element_id = element.id
+	join latest_allergy_examination_events latest on element.event_id = latest.event_id
+EOSQL
+);
+        $this->createView('allergy', 'select * from ophciexamination_allergy');
     }
 
 	public function down()
 	{
-        $this->renameColumn('patient', static::$archive_prefix . 'no_allergies_date', 'no_allergies_date');
-        $this->renameColumn('patient_version', static::$archive_prefix . 'no_allergies_date', 'no_allergies_date');
+	    $this->dropView('allergy');
+	    $this->dropView('patient_allergy_assignment');
+	    $this->dropView('latest_allergy_examination_events');
+	    $this->dropView('allergy_examination_events');
+
+//        $this->renameColumn('patient', static::$archive_prefix . 'no_allergies_date', 'no_allergies_date');
+//        $this->renameColumn('patient_version', static::$archive_prefix . 'no_allergies_date', 'no_allergies_date');
         foreach (static::$archive_tables as $table) {
             $this->renameTable(static::$archive_prefix . $table, $table);
             $this->renameTable(static::$archive_prefix . $table . '_version', $table . '_version');
