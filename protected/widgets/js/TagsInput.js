e4fce7cea513944b51554a522e3d02d801e58511
neilsf <feketecsaba@gmail.com>
2017-05-19 08:42:18 +0200

    OE-6445 Updated license headers

--- protected/widgets/js/TagsInput.js
+++ protected/widgets/js/TagsInput.js
@@ -1,7 +1,6 @@
 /**
  * OpenEyes
  *
- * (C) Moorfields Eye Hospital NHS Foundation Trust, 2017
  * (C) OpenEyes Foundation, 2017
  * This file is part of OpenEyes.
  * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -11,7 +10,6 @@
  * @package OpenEyes
  * @link http://www.openeyes.org.uk
  * @author OpenEyes <info@openeyes.org.uk>
- * @copyright Copyright (c) 2017, Moorfields Eye Hospital NHS Foundation Trust
  * @copyright Copyright (c) 2017, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
