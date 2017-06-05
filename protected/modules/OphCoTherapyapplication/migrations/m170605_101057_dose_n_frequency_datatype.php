2dc36e14672cfaa9730817045bd2b87273633230
Sabi <szabolcs.potondi@gmail.com>
2017-06-05 11:17:03 +0100

    OE-6627 : Dose and Frequency field to be expended

---
+++ protected/modules/OphCoTherapyapplication/migrations/m170605_101057_dose_n_frequency_datatype.php
@@ -0,0 +1,14 @@
+<?php
+
+class m170605_101057_dose_n_frequency_datatype extends CDbMigration
+{
+	public function up()
+	{
+	    $this->alterColumn('ophcotherapya_treatment', 'dose_and_frequency', 'TEXT');
+	}
+
+	public function down()
+	{
+        $this->alterColumn('ophcotherapya_treatment', 'dose_and_frequency', 'VARCHAR(256)');
+	}
+}
