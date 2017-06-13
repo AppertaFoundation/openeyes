d977bd99aa29d6a1f78daa5f5743d5e78fc18541
Sabi <szabolcs.potondi@gmail.com>
2017-06-13 18:51:59 +0100

    bugfix : Genetics : patient lookup field submit on enter, study

--- protected/modules/Genetics/assets/js/module.js
+++ protected/modules/Genetics/assets/js/module.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $('#search_patient_disorder_id_0, #search_disorder_id_0').keydown(function(event){
+    $('#search_patient_disorder_id_0, #search_disorder_id_0, #genetics_patient_lookup').keydown(function(event){
         if(event.keyCode == 13) {
             event.preventDefault();
             return false;
--- protected/modules/Genetics/models/GeneticsStudy.php
+++ protected/modules/Genetics/models/GeneticsStudy.php
@@ -108,9 +108,7 @@ class GeneticsStudy extends BaseActiveRecordVersioned
      */
     public function beforeSave()
     {
-        //Set the date to null if it's not truthy so empty strings don't convert to 0000-00-00....
-
-        if (!$this->end_date) {
+        if (!$this->end_date || $this->end_date == '0000-00-00 00:00:00') {
             $this->end_date = null;
         }
         return parent::beforeSave();
