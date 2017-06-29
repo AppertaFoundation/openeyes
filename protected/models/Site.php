21c362dd081fd7cda1fb15d7ce263b2b8d274219
neilsf <feketecsaba@gmail.com>
2017-06-29 22:10:48 +0200

    OE-6311 Work In Progress

--- protected/models/Site.php
+++ protected/models/Site.php
@@ -139,10 +139,27 @@ class Site extends BaseActiveRecordVersioned
         ));
     }
 
-    public function getListForCurrentInstitution($field = 'short_name')
+    public function getListForCurrentInstitution($field = 'short_name', $having_theatres_only = false)
     {
         $result = array();
+        $sites_with_theatres = array();
+
+        if($having_theatres_only)
+        {
+            foreach (OphTrOperationbooking_Operation_Theatre::model()->findAll() as $theatre)
+            {
+                if(!in_array($theatre->site_id, $sites_with_theatres))
+                {
+                    $sites_with_theatres[] = $theatre->site_id;
+                }
+            }
+        }
+
         foreach (Institution::model()->getCurrent()->sites as $site) {
+            if($having_theatres_only && !in_array($site->id, $sites_with_theatres))
+            {
+                continue;
+            }
             $result[$site->id] = $site->$field;
         }
 
