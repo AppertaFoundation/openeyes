9fae6a3df7a9b07263e52a98ee7fa66576de4a5d
neilsf <feketecsaba@gmail.com>
2017-06-30 21:02:35 +0200

    OE-6311 Site list in op booking and booking waiting list filtered to sites w theatres

--- protected/modules/OphTrOperationbooking/views/default/form_Element_OphTrOperationbooking_Operation.php
+++ protected/modules/OphTrOperationbooking/views/default/form_Element_OphTrOperationbooking_Operation.php
@@ -49,7 +49,12 @@
 	<?php $form->radioBoolean($element, 'stop_medication') ?>
 	<?php $form->textArea($element, 'stop_medication_details', array('rows' => 4), true, array(), array_merge($form->layoutColumns, array('field' => 4))) ?>
 	<?php echo $form->radioBoolean($element, 'overnight_stay')?>
-	<?php echo $form->dropDownList($element, 'site_id', Site::model()->getListForCurrentInstitution('short_name', true), array('empty' => '--- Please select ---'), false, array('field' => 2))?>
+    <?php
+        $options = array(
+            $this->selectedSiteId=>array('selected'=>true)
+        );
+    ?>
+	<?php echo $form->dropDownList($element, 'site_id', Site::model()->getListForCurrentInstitution('short_name', true), array('empty' => '- Please select -', 'options' => $options), false, array('field' => 2))?>
 	<?php echo $form->radioButtons($element, 'priority_id', CHtml::listData(OphTrOperationbooking_Operation_Priority::model()->notDeletedOrPk($element->priority_id)->findAll(array('order' => 'display_order asc')), 'id', 'name'))?>
 	<?php
         if (Yii::app()->params['ophtroperationbooking_referral_link']) {
