7448336a56c4371ccc876b8c33b4c5e1546250bd
Mike Smith <mike.smith@camc-ltd.co.uk>
2017-07-19 11:05:13 +0100

    [OE-6527] red banner for deceased patients

--- protected/widgets/views/PatientPanel.php
+++ protected/widgets/views/PatientPanel.php
@@ -23,7 +23,7 @@ $warnings = $this->patient->getWarnings($allow_clinical);
 Yii::app()->assetManager->registerCssFile('components/font-awesome/css/font-awesome.css', null, 10);
 ?>
 
-<div class="panel patient<?php if ($warnings): echo ' warning'; endif; ?>" id="patientID">
+<div class="panel patient<?php if ($warnings): echo ' warning'; endif; ?><?= $this->patient->isDeceased() ? ' patient-deceased' : ''?>" id="patientID">
     <div class="patient-details">
         <?php echo CHtml::link($this->patient->getDisplayName(), array('/patient/view/'.$this->patient->id)) ?>
         <span class="patient-title">
