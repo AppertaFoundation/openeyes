a2a19961cd0500582c7ab1e9e55c9085cb930bfa
Mike Smith <mike.smith@camc-ltd.co.uk>
2017-07-19 15:05:00 +0100

    [OE-6465] WIP processing of eyedraw config

--- protected/commands/EyedrawConfigLoadCommand.php
+++ protected/commands/EyedrawConfigLoadCommand.php
@@ -30,8 +30,18 @@ class EyedrawConfigLoadCommand extends CConsoleCommand
 
     public function getHelp()
     {
-        return "yiic eyedrawconfigload <filename>\n\n".
+        return "yiic eyedrawconfigload --filename=<filename>\n\n".
             "load/update the eyedraw configuration from the definition file <filename>";
+    }
+
+    /**
+     * Abstraction to the db connection
+     * @return mixed
+     */
+    protected function getDb()
+    {
+        // TODO: stop using the static Yii call here
+        return Yii::app()->db;
     }
 
     public function actionLoad($filename)
@@ -41,27 +51,72 @@ class EyedrawConfigLoadCommand extends CConsoleCommand
         }
 
         $data = json_decode(file_get_contents($filename));
+        if ($data === null) {
+            $this->usageError($filename . ' is not in a valid format');
+        }
 
-        foreach ($data['canvases'] as $canvas) {
+        foreach ($data->canvases as $canvas) {
             $this->processCanvasDefinition($canvas);
         }
 
         // iterate through the data structure, performing update/insert statements as appropriate
-        foreach ($data['doodles'] as $doodle) {
+        foreach ($data->doodles as $doodle) {
             $this->processDoodleDefinition($doodle);
         }
+    }
+
+    private function insertOrUpdateCanvas($canvas, $element_type)
+    {
+        $current = $this->getDb()
+            ->createCommand('SELECT count(*) FROM ' . static::CANVS_TBL . ' WHERE container_element_type_id = :eid')
+            ->bindValue(':eid', $element_type->id)
+            ->queryScalar();
+        if ($current) {
+            $cmd = $this->getDb()
+                ->createCommand('UPDATE '
+                    . static::CANVS_TBL .
+                    ' SET canvas_mnemonic = :cvmn, canvas_name = :cvname where container_element_type_id = :eid');
+        } else {
+            $cmd = $this->getDb()
+                ->createCommand('INSERT INTO ' . static::CANVS_TBL .
+                    '(canvas_mnemonic, canvas_name, container_element_type_id) VALUES (:cvmn, :cvname, :eid)');
+        }
+        $cmd->bindValue(':cvmn', $canvas->mnemonic)
+            ->bindValue(':cvname', $canvas->name)
+            ->bindValue(':eid', $element_type->id)
+            ->query();
     }
 
     protected function processCanvasDefinition($canvas)
     {
         // verify that the element type exists for this definition
+        if ($element_type = ElementType::model()->findByAttributes(array('class_name' => $canvas->oe_class))) {
+            $this->insertOrUpdateCanvas($canvas, $element_type);
+        }
+    }
 
-        // insert/update as appropriate
+    private function insertOrUpdateDoodle($definition)
+    {
+        $current = $this->getDb()
+            ->createCommand('SELECT count(*) FROM ' . static::DOODLE_TBL . ' WHERE eyedraw_class_mnemonic = :mnm')
+            ->bindValue(':mnm', $definition->mnemonic)
+            ->queryScalar();
+        if ($current) {
+            $cmd = $this->getDb()->createCommand('UPDATE ' . static::DOODLE_TBL . ' SET init_doodle_json = :init');
+        } else {
+            $cmd = $this->getDb()->createCommand('INSERT INTO '
+                . static::DOODLE_TBL .
+                '(eyedraw_class_mnemonic, init_doodle_json) VALUES (:mnm, :init)')
+                ->bindValue(':mnm', $definition->mnemonic);
+        }
+        $cmd->bindValue(':init', json_encode($definition->initial))
+            ->query();
     }
 
     protected function processDoodleDefinition($doodle)
     {
         // update or create doodle entry
+        $this->insertOrUpdateDoodle($doodle->definition);
 
         // iterate through doodle canvas definitions. Use the canvas mnemonic to confirm
         // whether or not it should be setup in the db.
