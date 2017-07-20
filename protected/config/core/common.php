2c6a0296f0350f7d243dc100b6b3f4aa47cae653
neilsf <feketecsaba@gmail.com>
2017-07-20 21:38:02 +0200

    OE-6311 WIP

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -79,7 +79,7 @@ return array(
         ),
         'cacheBuster' => array(
             'class' => 'CacheBuster',
-            'time' => '20170627',
+            'time' => '201706720',
         ),
         'clientScript' => array(
             'class' => 'ClientScript',
