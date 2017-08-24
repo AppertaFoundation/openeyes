784b174e85e02cd18af958f7f7e94bbd5141df24
Your Namels <tom.ayoola@abehr.com>
2017-08-24 17:54:47 +0100

    s

--- protected/widgets/js/IndexSearch.js
+++ protected/widgets/js/IndexSearch.js
@@ -139,7 +139,7 @@ $(document).ready(function(){
     index_clicked($(this));
   });
 
-  $('body').append('<div id="dim_rest" class="ui-widget-overlay" style="display : none; width: 1280px; height: 835px; z-index: 180;"></div>');
+  $('body').append('<div id="dim_rest" class="ui-widget-overlay" style="display : none; width: 100%; height: 100%; z-index: 180;"></div>');
   $('body').append("<div id=\"is_loading\"style=\"display : none; position: fixed; background-color: black; width: 100%; height: 1000px; z-index: 1000; opacity: 0.8; top:0px; \"><img src=\"https://itopia.com/wp-content/themes/itopia/img-cloud/loader.gif\" style=\" position: fixed; z-index: 1000; height: 64px; width: 64px; top: 33%; left: 50%;\"></div>");
   $('#description_toggle').change(function(){
     if (this.checked) {
