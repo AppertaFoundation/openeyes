9e74fd9c307f9738c661fb87b1ae8e1fe6c70c1c
Your Namels <tom.ayoola@abehr.com>
2017-08-30 11:15:49 +0100

    Fixed error for lids medical in edit mode.

--- protected/migrations/m170830_100559_remove_STFB_lids_medical.php
+++ protected/migrations/m170830_100559_remove_STFB_lids_medical.php
@@ -1,16 +1,18 @@
 <?php
 
-class m170829_120523_LidsMedicalSFTB extends CDbMigration
+class m170830_100559_remove_STFB_lids_medical extends CDbMigration
 {
 	public function up()
 	{
 		$this->dropColumn('et_ophciexamination_medical_lids', 'right_stfb');
 		$this->dropColumn('et_ophciexamination_medical_lids', 'left_stfb');
+		$this->dropColumn('et_ophciexamination_medical_lids_version', 'right_stfb');
+		$this->dropColumn('et_ophciexamination_medical_lids_version', 'left_stfb');
 	}
 
 	public function down()
 	{
-		echo "m170829_120523_LidsMedicalSFTB does not support migration down.\n";
+		echo "m170830_100559_remove_STFB_lids_medical does not support migration down.\n";
 		return false;
 	}
 
