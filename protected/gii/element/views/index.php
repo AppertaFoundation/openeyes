90b36d5bb3d8fba11cb8aa0fb1e2da027d569b99
Biskyt <github@gettheitguy.com>
2017-09-19 15:45:16 +0000

    OE-6863 - update license text in all files

--- protected/gii/element/views/index.php
+++ protected/gii/element/views/index.php
@@ -12,7 +12,7 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with OpenEyes in a file
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file
  * titled COPYING. If not, see <http://www.gnu.org/licenses/>.
  *
  * _____________________________________________________________________________
