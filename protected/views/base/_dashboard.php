bf144ff98db45c3c30940f5b5c056726dab5500d
tomayoola <tom.ayoola@abehr.com>
2017-09-19 17:08:25 +0100

    added tour and overlay

--- protected/views/base/_dashboard.php
+++ protected/views/base/_dashboard.php
@@ -1,141 +1,163 @@
 <div class="dashboard-container" id="tour2">
-<?php foreach ($items as $box_number => $item) {?>
+  <?php foreach ($items as $box_number => $item) {?>
 
-<?php
+    <?php
     $container_id = isset($item['options']['container-id']) ? $item['options']['container-id'] : "js-toggle-container-$box_number";
     $is_open = isset($item['options']['js-toggle-open']) && $item['options']['js-toggle-open'];
     ?>
 
-<section id="<?php echo $container_id; ?>" class="box dashboard js-toggle-container">
-    <h3 class="box-title"><?= $item['title'] ?></h3>
-    <?php if ($sortable) { ?><span class="sortable-anchor fa fa-arrows"></span><?php }?>
-    <a href="#" class="toggle-trigger <?php echo  $is_open ? 'toggle-hide' : 'toggle-show' ?> js-toggle">
+    <section id="<?php echo $container_id; ?>" class="box dashboard js-toggle-container">
+      <h3 class="box-title"><?= $item['title'] ?></h3>
+      <?php if ($sortable) { ?><span class="sortable-anchor fa fa-arrows"></span><?php }?>
+      <a href="#" class="toggle-trigger <?php echo  $is_open ? 'toggle-hide' : 'toggle-show' ?> js-toggle">
         <span class="icon-showhide">
-            Show/hide this section
+          Show/hide this section
         </span>
-    </a>
-    <div class="js-toggle-body" style="<?php echo  $is_open ? 'display:block' : 'display:none' ?>">
-    <?= $item['content']; ?>
-    </div>
-</section>
-<?php } ?>
+      </a>
+      <div class="js-toggle-body" style="<?php echo  $is_open ? 'display:block' : 'display:none' ?>">
+        <?= $item['content']; ?>
+      </div>
+    </section>
+  <?php } ?>
 </div>
 <?php if ($sortable) { ?>
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('.dashboard-container').sortable({handle: '.sortable-anchor'});
-    });
-</script>
+  <script type="text/javascript">
+  $(document).ready(function() {
+    $('.dashboard-container').sortable({handle: '.sortable-anchor'});
+  });
+  </script>
 <?php }?>
 
 <div id="help-container" style="z-index: 100000;">
-    <div id="help-trigger">
-    </div>
-    <div id="help-overlay" class="action">
-      Show Help Overlay
-    </div>
-    <div id="help-tour" class="action">
-      Take a Tour
-    </div>
-    <div id="help-download" class="action">
-      Download a PDF
-    </div>
+  <div id="help-trigger">
   </div>
+  <div id="help-overlay" class="action">
+    Show Help Overlay
+  </div>
+  <div id="help-tour" class="action">
+    Take a Tour
+  </div>
+  <div id="help-download" class="action">
+    Download a PDF
+  </div>
+</div>
+
 
 <script>
-  $('#help-trigger').click(function(){
-    const $this = $(this).parent();
+$(document).ready(function(){
+
+  function Steps(){ //init tour
+    this._addListeners();
+    this._addOverlays();
+    this.tour = new Tour({backdrop: true});
+    this.elements = []; //for z-index
+    this.contentType = []; //selector short long content
+  }
+  Steps.prototype._addListeners = function () {
+    let self = this;
+    $('#help-trigger').click(function(){
+      self._helpTrigger($(this).parent());
+    });
+    $('#help-overlay').click(function(){
+      self._toggleOverlay($(this));
+    });
+    $('#help-tour').click(function(){
+      self._showLongContent();
+      self.tour.init(); // TODO: see why cannot who all first
+      self.tour.restart(); //sometimes invalid could keep steps array and pass it in so create tour each time and remove all attributes first
+    });
+  }
+  Steps.prototype._addOverlays = function () {
+    this.header_overlay = $('body').append('<div id="help-body-overlay" hidden="true"></div>');
+    this.body_overlay = $('header').append('<div id="help-header-overlay" hidden="true"></div>');
+    this.overlay_toggle = 0b0;
+
+  }
+  Steps.prototype._helpTrigger = function ($this) {
     if ($this.hasClass('help-active')) {
       $this.removeClass('help-active');
     }
     else {
       $this.addClass('help-active');
     }
-  });
-</script>
-
-<script>
-$(document).ready(function(){
-
-  $('body').append('<div id="feature-help-overlay" hidden="true"></div>');
-  let button_state = 0b0;
-  const step_1_selc = ".large-6.medium-7.column";
-  const step_1_title = "User Panel";
-  const step_1_content = "This is where....";
-  $(step_1_selc).attr('data-toggle','popover');
-  $(step_1_selc).attr('title',step_1_title);
-  $(step_1_selc).attr('data-content',step_1_content);
-  $(step_1_selc).attr('data-trigger',"manual");
-  $(step_1_selc).attr('data-placement',"auto right");
-
-
-  const step_2_selc = ".oe-find-patient:first";
-  const step_2_title = "Patient Search";
-  const step_2_content = "This is where....";
-  $(step_2_selc).attr('data-toggle','popover');
-  $(step_2_selc).attr('title',step_2_title);
-  $(step_2_selc).attr('data-content',step_2_content);
-  $(step_2_selc).attr('data-trigger',"manual");
-
-  const step_3_selc = "#"+$('section .box-title:contains(Messages):not(:contains(Sent Messages))').parent().prop('id');
-  const step_3_title = "Messages";
-  const step_3_content = "This is where....";
-
-
-  //add popovers to elements
-  $(step_1_selc);
-  $(step_2_selc);
-  $(step_3_selc);
-  //show popovers
-  //show download
-  //show take a tour
-  //show cancel
-
-  // Instance the tour
-
-
-var tour = new Tour({
-  backdrop: true,
-  steps: [
-  {
-    element: ".large-6.medium-7.column",
-    backdropContainer: 'header',
-    title: "User Info Panel",
-    content: "This is where user info"
-  },
-  {
-    element: ".oe-find-patient:first",
-    title: "Paitent Search",
-    content: "Search for patients here"
-  },
-  {
-    element: step_3_selc,
-    title: "Paitent Search",
-    content: "Search for patients here"
+    if (this.overlay_toggle) {
+      this._toggleOverlay($('#help-overlay'));
+    }
   }
-]});
-
-// Initialize the tour
-tour.init();
-
-// Start the tour
-$('#help-overlay').click(function(){
-  const $this = $(this);
-  if (button_state = ~button_state) { //add different class?
-    window.scrollTo(0,0);
-    $this.html("Hide Help Overlay");
-    $("#feature-help-overlay").show();
-    //have an overlay for each context
-    //think about header stack context cannot append to non static positioned elements think of solution maybe jsut append to body?
-    $('[data-toggle="popover"]').popover('show');
-  } else {
-    $this.html("Show Help Overlay");
-    $('[data-toggle="popover"]').popover('hide');
-    $("#feature-help-overlay").hide();
+  Steps.prototype._addStep = function (params) {
+    this.tour.addStep(params);
+    const $this = $(params.element);
+    $this.attr('data-toggle','popover');
+    $this.attr('title',params.title);
+    $this.attr('data-content',params.contentLess);
+    $this.attr('data-trigger',"manual");
+    $this.attr('data-placement',params.placement ? params.placement : "auto right");
+    if (params.showParent) {
+     this.elements.push($this.parent());
+   } else {
+     this.elements.push($this);
+   }
+   this.contentType.push({element: params.element, shortContent: params.contentLess, longContent: params.content});
+  };
+  Steps.prototype.addSteps = function (params) {
+    params.forEach((el) => {
+      this._addStep(el);
+    });
+  }
+  Steps.prototype._showShortContent = function () {
+    this.contentType.forEach((el) => {
+      $(el.element).attr('data-content',el.shortContent);
+    });
+  }
+  Steps.prototype._showLongContent = function () {
+    this.contentType.forEach((el) => {
+      $(el.element).attr('data-content',el.longContent);
+    });
+  }
+  Steps.prototype._toggleOverlay = function ($this) {
+    if (this.overlay_toggle = ~this.overlay_toggle) {
+      this._showShortContent();
+      window.scrollTo(0,0);
+      $this.html("Hide Help Overlay");
+      this.elements.forEach(function(elem){
+        elem.css('z-index', 160);
+      });
+      $("#help-body-overlay").show();
+      $("#help-header-overlay").show();
+      $('[data-toggle="popover"]').popover('show');
+    } else {
+      $this.html("Show Help Overlay");
+      this.elements.forEach(function(elem){
+        elem.css('z-index', 'auto');
+      });
+      $('[data-toggle="popover"]').popover('hide');
+      $("#help-body-overlay").hide();
+      $("#help-header-overlay").hide();
+    }
   }
 
-  //tour.restart(true);
-});
+  const steps = new Steps();
+  steps.addSteps([{
+                  element: ".large-6.medium-7.column",
+                  title: "User Panel",
+                  content: "This is where....",
+                  contentLess: "This is....",
+                  backdropContainer: "header"
+                 },
+                 {
+                  element: ".oe-find-patient:first",
+                  title: "Paitent Search",
+                  content: "This is where....",
+                  contentLess: "This is....",
+                  showParent: "true"
+                 },
+                 {
+                  element: "#"+$('section .box-title:contains(Messages):not(:contains(Sent Messages))').parent().prop('id'),
+                  title: "Messages",
+                  content: "This is where....",
+                  contentLess: "This is....",
+                  placement: "bottom"
+                }]);
 });
 
 </script>
