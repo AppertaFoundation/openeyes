1d23077b7c03081e9876a5b7611cf246bd2887a2
tomayoola <tom.ayoola@abehr.com>
2017-09-20 12:34:31 +0100

    Working Steps object.

--- protected/views/base/_dashboard.php
+++ protected/views/base/_dashboard.php
@@ -46,45 +46,70 @@
 <script>
 $(document).ready(function(){
 
-  function Steps(){ //init tour
+  function Steps() {
+    this.elements = [];
+    this.elementsContent = [];
+    this.toggles = {overlay: 0b0, tour: 0b0};
+    this.buttons = {help: $('#help-trigger'), menu: $('#help-trigger').parent(),overlay: $('#help-overlay'), tour: $('#help-tour')};
     this._addListeners();
     this._addOverlays();
-    this.tour = new Tour({backdrop: true});
-    this.elements = []; //for z-index
-    this.contentType = []; //selector short long content
+    this.tour = new Tour({backdrop: true, debug: true, onEnd: this._tourEnd.bind(this), onStart: this._tourStart.bind(this)});
+  }
+  Steps.prototype._tourStart = function(tour) {
+    this.buttons.tour.html('End Tour');
+  }
+  Steps.prototype._tourEnd = function(tour) {
+    this.buttons.tour.html('Take a Tour');
+    this.toggles.tour = 0b0;
   }
   Steps.prototype._addListeners = function () {
-    let self = this;
-    $('#help-trigger').click(function(){
-      self._helpTrigger($(this).parent());
-    });
-    $('#help-overlay').click(function(){
-      self._toggleOverlay($(this));
-    });
-    $('#help-tour').click(function(){
-      self._showLongContent();
-      self.tour.init(); // TODO: see why cannot who all first
-      self.tour.restart(); //sometimes invalid could keep steps array and pass it in so create tour each time and remove all attributes first
-    });
+    this.buttons.help.click(() => {this.toggleHelpMenu();});
+    this.buttons.overlay.click(() => {this.toggleOverlay();});
+    this.buttons.tour.click(() => {this.toggleTour();});
+  }
+  Steps.prototype.toggleTour = function () {
+    if (!this.toggles.overlay) {
+      if (this.toggles.tour = ~this.toggles.tour) {
+        this._startTour();
+      } else {
+        this._endTour();
+      }
+    }
+  }
+  Steps.prototype._startTour = function () {
+    this._showLongContent();
+    localStorage.clear();
+    this.tour.start(true);
+    this.tour.next();
+    this.tour.next();
+    this.tour.next();
+    this.tour.next();
+    this.tour.goTo(0);
+  }
+  Steps.prototype._endTour = function () {
+    this.tour.end();
   }
   Steps.prototype._addOverlays = function () {
-    this.header_overlay = $('body').append('<div id="help-body-overlay" hidden="true"></div>');
-    this.body_overlay = $('header').append('<div id="help-header-overlay" hidden="true"></div>');
-    this.overlay_toggle = 0b0;
-
+    $('body').append('<div id="help-body-overlay" hidden="true"></div>');
+    $('header').append('<div id="help-header-overlay" hidden="true"></div>');
+    this.header_overlay = $('#help-body-overlay');
+    this.body_overlay = $('#help-header-overlay');
   }
-  Steps.prototype._helpTrigger = function ($this) {
-    if ($this.hasClass('help-active')) {
-      $this.removeClass('help-active');
+  Steps.prototype.toggleHelpMenu = function () {
+    if (this.buttons.menu.hasClass('help-active')) {
+      this.buttons.menu.removeClass('help-active');
     }
     else {
-      $this.addClass('help-active');
+      this.buttons.menu.addClass('help-active');
     }
-    if (this.overlay_toggle) {
-      this._toggleOverlay($('#help-overlay'));
+    if (this.toggles.overlay) {
+      this.toggleOverlay();
+    }
+    if (this.toggles.tour) {
+      this.toggleTour();
     }
   }
-  Steps.prototype._addStep = function (params) {
+  Steps.prototype.addStep = function (params) {
     this.tour.addStep(params);
     const $this = $(params.element);
     $this.attr('data-toggle','popover');
@@ -97,42 +122,44 @@ $(document).ready(function(){
    } else {
      this.elements.push($this);
    }
-   this.contentType.push({element: params.element, shortContent: params.contentLess, longContent: params.content});
+   this.elementsContent.push({element: params.element, shortContent: params.contentLess, longContent: params.content});
   };
   Steps.prototype.addSteps = function (params) {
     params.forEach((el) => {
-      this._addStep(el);
+      this.addStep(el);
     });
   }
   Steps.prototype._showShortContent = function () {
-    this.contentType.forEach((el) => {
+    this.elementsContent.forEach((el) => {
       $(el.element).attr('data-content',el.shortContent);
     });
   }
   Steps.prototype._showLongContent = function () {
-    this.contentType.forEach((el) => {
+    this.elementsContent.forEach((el) => {
       $(el.element).attr('data-content',el.longContent);
     });
   }
-  Steps.prototype._toggleOverlay = function ($this) {
-    if (this.overlay_toggle = ~this.overlay_toggle) {
-      this._showShortContent();
-      window.scrollTo(0,0);
-      $this.html("Hide Help Overlay");
-      this.elements.forEach(function(elem){
-        elem.css('z-index', 160);
-      });
-      $("#help-body-overlay").show();
-      $("#help-header-overlay").show();
-      $('[data-toggle="popover"]').popover('show');
-    } else {
-      $this.html("Show Help Overlay");
-      this.elements.forEach(function(elem){
-        elem.css('z-index', 'auto');
-      });
-      $('[data-toggle="popover"]').popover('hide');
-      $("#help-body-overlay").hide();
-      $("#help-header-overlay").hide();
+  Steps.prototype.toggleOverlay = function ($this) {
+    if (!this.toggles.tour) {
+      if (this.toggles.overlay = ~this.toggles.overlay) {
+        this._showShortContent();
+        window.scrollTo(0,0);
+        this.buttons.overlay.html("Hide Help Overlay");
+        this.elements.forEach(function(elem){
+          elem.css('z-index', 160);
+        });
+        this.header_overlay.show();
+        this.body_overlay.show();
+        $('[data-toggle="popover"]').popover('show');
+      } else {
+        this.buttons.overlay.html("Show Help Overlay");
+        this.elements.forEach(function(elem){
+          elem.css('z-index', '');
+        });
+        $('[data-toggle="popover"]').popover('hide');
+        this.header_overlay.hide();
+        this.body_overlay.hide();
+      }
     }
   }
 
