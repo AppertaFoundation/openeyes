cd9674ac6de8ea1cc89fb779969c36f99786f909
tomayoola <tom.ayoola@abehr.com>
2017-09-22 14:55:06 +0100

    Completed NewFeatureHelpController.

--- protected/widgets/js/NewFeatureHelp.js
+++ protected/widgets/js/NewFeatureHelp.js
@@ -1,34 +1,96 @@
 function NewFeatureHelpController(splashScreen, tours, downloadLinks) {
   this.tours = [];
-  this.splashScreen = [];
-  this.downloadLinks = [];
-
+  this.splashScreenElements = [];
+  this.downloadLinks = downloadLinks;
   this._initTours(tours);
   this._initSplashScreen(splashScreen);
 }
+
+NewFeatureHelpController.prototype.showSplashScreen = function() {
+  this.endAllTours();
+  window.scrollTo(0,0);
+  this._showSplashScreen();
+}
+
+NewFeatureHelpController.prototype.hideSplashScreen = function() {
+  this.endAllTours();
+  this._hideSplashScreen();
+}
+
+NewFeatureHelpController.prototype._showSplashScreen = function() {
+  this.splashScreenElements.forEach(($element) => {
+    if ($element.attr('data-showparent') === 'true') {
+      $element.parent().css('z-index','161');
+    }
+    $element.css('z-index','161');
+  });
+  $('#help-body-overlay').show();
+  $('#help-header-overlay').show();
+  $('[data-toggle="popover"]').popover('show');
+}
+
+NewFeatureHelpController.prototype._hideSplashScreen = function() {
+  this.splashScreenElements.forEach(($element) => {
+    if ($element.attr('data-showparent') === 'true') {
+      $element.parent().css('z-index','');
+    }
+    $element.css('z-index','');
+  });
+  $('#help-body-overlay').hide();
+  $('#help-header-overlay').hide();
+  $('[data-toggle="popover"]').popover('hide');
+}
+
+NewFeatureHelpController.prototype.startTour = function(tourName) {
+  this.hideSplashScreen();
+  this.endAllTours();
+  this.tours[tourName].restart();
+}
+
+NewFeatureHelpController.prototype.endAllTours = function() {
+  for (var tour in this.tours) {
+    if (this.tours.hasOwnProperty(tour) && !this.tours[tour].ended()) {
+      this.tours[tour].end();
+    }
+  }
+}
+
 NewFeatureHelpController.prototype._initTours = function(tours) {
-  for (var tourName in tours) { //tourName is tour id
+  for (var tourName in tours) { //tourName is tour id has own but prefixed
     var tourSteps = tours[tourName];
     this.tours[tourName] = new Tour(
       {
         name: tourName,
         backdrop: true,
         steps: tourSteps,
-        onEnd: this._tourEnd.bind(this,tourName),
-        onStart: this._tourStart.bind(this,tourName)
+        onEnd: this._tourEnded.bind(this,tourName),
+        onStart: this._tourStarted.bind(this,tourName)
       }
     );
   }
 }
+
 NewFeatureHelpController.prototype._initSplashScreen = function(splashScreen) {
-  console.log(splashScreen);
-  //add attributes to all elements required may interfer with rest and cause similar bug so use old fix
+  splashScreen.forEach((popup) => {
+    var $element = $(popup.element);
+    $element.attr('title', popup.title);
+    for (var data in popup) {
+      if (!['element','title'].includes(data)) {
+        $element.attr(`data-${data}`,popup[data]);
+      }
+    }
+    this.splashScreenElements.push($element);
+  });
 }
-NewFeatureHelpController.prototype._tourEnd = function(tourName) {
-  console.log(tourName);
-  console.log("has ended");
+
+NewFeatureHelpController.prototype._tourEnded = function(tourName) {
+  $('.popover').remove();
 }
-NewFeatureHelpController.prototype._tourStart = function(tourName) {
-  console.log(tourName);
-  console.log("has started");
+
+NewFeatureHelpController.prototype._tourStarted = function(tourName) {
+  this.tours[tourName].goTo(0);
+  for (var i = 0; i < this.tours[tourName]._options.steps.length; i++) {
+    this.tours[tourName].next();
+  }
+  this.tours[tourName].goTo(0);
 }
