9c394ea9451a3932f221cafc29ea3091b779afb5
Biskyt <github@gettheitguy.com>
2017-10-03 10:01:20 +0000

    updated copyright in header for components

--- protected/components/CoreAPI.php
+++ protected/components/CoreAPI.php
@@ -2,7 +2,8 @@
 /**
  * OpenEyes
  *
- * (C) OpenEyes Foundation, 2017
+ * Copyright OpenEyes Foundation, 2017
+ *
  * This file is part of OpenEyes.
  * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
