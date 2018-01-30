a4a5c19e6258ee24f4b5e63976c66ab624b4364f
Andrew Pigram <andrew.pigram@fivium.com.au>
2018-01-30 14:52:40 +1100

    Upgraded TextField widget to v3 styling.

--- protected/widgets/views/TextField.php
+++ protected/widgets/views/TextField.php
@@ -21,7 +21,7 @@
 <?php } else {?>
     <div id="div_<?php echo CHtml::modelName($element) ?>_<?php echo $field ?>"
          class="row field-row"<?php if (@$htmlOptions['hide']) { ?> style="display: none;"<?php } ?>>
-		<div class="large-<?php echo $layoutColumns['label'];?> column">
+		<div class="cols-<?php echo $layoutColumns['label'];?> column">
 			<?php
             $labelText = empty($htmlOptions['label']) ? CHtml::encode($element->getAttributeLabel($field)) : $htmlOptions['label'];
     $labelText .= ':';
@@ -29,7 +29,7 @@
     ?>
 		</div>
         <div
-            class="large-<?php echo $layoutColumns['field']; ?> column<?php if (empty($htmlOptions['append-text']) || empty($layoutColumns['append-text'])) { ?> end<?php } ?>">
+            class="cols-<?php echo $layoutColumns['field']; ?> column">
             <?php
             switch ($htmlOptions['type']) {
                 case 'password':
@@ -53,7 +53,7 @@
         }?>
 		</div>
         <?php if(!empty($htmlOptions['append-text']) && !empty($layoutColumns['append-text'])){?>
-            <div class="large-<?php echo $layoutColumns['append-text'];?> column collapse in end">
+            <div class="cols-<?php echo $layoutColumns['append-text'];?> column collapse">
                 <span class="field-info"><?php echo $htmlOptions['append-text'];?></span>
             </div>
         <?php }?>
