5d2975ddc6b52f3d3d304c2f7fb425c84bdd343f
Zhaolian-Zhou <zhaolian.zhou@fivium.com.au>
2018-02-16 16:37:12 +1100

    v3-ui, examination, VA, tooltip content

--- protected/assets/js/events_and_episodes.js
+++ protected/assets/js/events_and_episodes.js
@@ -174,7 +174,6 @@ $(document).ready(function(){
     var text = $(this).data('tooltip-content');
     var offset = $(this).offset();
     var html = '<div class="oe-tooltip" style="position:fixed; left:'+(offset.left + 20)+'px; top:'+(offset.top + - 10)+'px;">'+ text +'</div>';
-    $(this).data( "tooltip", html );
     $('body').append(html);
   });
 
