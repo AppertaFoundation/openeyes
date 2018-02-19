54bcaaa1028dd33e55c4fa02e1d6fbc3245edb5e
Jackson <mrisaacjackson@gmail.com>
2018-02-20 09:19:43 +1100

    changed view mode to new UI

--- protected/modules/OphCiExamination/views/default/view_Element_OphCiExamination_OverallManagementPlan.php
+++ protected/modules/OphCiExamination/views/default/view_Element_OphCiExamination_OverallManagementPlan.php
@@ -17,73 +17,75 @@
  */
 ?>
 <div class="element-data">
-	<div class="row data-row">
-		<div class="large-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('clinic_interval_id'))?></div></div>
-		<div class="large-7 column end"><div class="data-value"><?php echo $element->clinic_interval ? : 'None'?></div></div>
-	</div>
-	<div class="row data-row">
-		<div class="large-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('photo_id'))?></div></div>
-		<div class="large-7 column end"><div class="data-value"><?php echo $element->photo ? $element->photo->name : 'None'?></div></div>
-	</div>
-	<div class="row data-row">
-		<div class="large-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('oct_id'))?></div></div>
-		<div class="large-7 column end"><div class="data-value"><?php echo $element->oct ? $element->oct->name : 'None'?></div></div>
-	</div>
-	<div class="row data-row">
-		<div class="large-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('hfa_id'))?></div></div>
-		<div class="large-7 column end"><div class="data-value"><?php echo $element->hfa ? $element->hfa->name : 'None'?></div></div>
-	</div>
-	<div class="row data-row">
-		<div class="large-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('gonio_id'))?></div></div>
-		<div class="large-7 column end"><div class="data-value"><?php echo $element->gonio ? $element->gonio->name : 'None'?></div></div>
-	</div>
-	<div class="row data-row">
-		<div class="large-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('hrt_id'))?></div></div>
-		<div class="large-7 column end"><div class="data-value"><?php echo $element->hrt ? $element->hrt->name : 'None'?></div></div>
-	</div>
-	<div class="row data-row">
-		<div class="large-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('comments'))?></div></div>
-		<div class="large-7 column end"><div class="data-value"><?php echo $element->textWithLineBreaks('comments')?></div></div>
-	</div>
+  <table>
+    <tbody>
+    <tr>
+      <td>
+        <div class="cols-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('clinic_interval_id'))?></div></div>
+        <div class="cols-7 column end"><div class="data-value"><?php echo $element->clinic_interval ? : 'None'?></div></div>
+      </td>
+      <td>
+        <div class="cols-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('photo_id'))?></div></div>
+        <div class="cols-7 column end"><div class="data-value"><?php echo $element->photo ? $element->photo->name : 'None'?></div></div>
+      </td>
+      <td>
+        <div class="cols-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('oct_id'))?></div></div>
+        <div class="cols-7 column end"><div class="data-value"><?php echo $element->oct ? $element->oct->name : 'None'?></div></div>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <div class="cols-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('hfa_id'))?></div></div>
+        <div class="cols-7 column end"><div class="data-value"><?php echo $element->hfa ? $element->hfa->name : 'None'?></div></div>
+      </td>
+      <td>
+
+        <div class="cols-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('gonio_id'))?></div></div>
+        <div class="cols-7 column end"><div class="data-value"><?php echo $element->gonio ? $element->gonio->name : 'None'?></div></div>
+      </td>
+      <td>
+        <div class="cols-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('hrt_id'))?></div></div>
+        <div class="cols-7 column end"><div class="data-value"><?php echo $element->hrt ? $element->hrt->name : 'None'?></div></div>
+      </td>
+    </tr>
+
+    </tbody>
+  </table>
+  <br/>
+  <div class="cols-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('comments'))?></div></div>
+  <div class="cols-full column end"><div class="data-value"><?php echo $element->textWithLineBreaks('comments')?></div></div>
+  <br/>
 </div>
 <div class="element-data element-eyes row">
-	<div class="element-eye right-eye column">
+<?php foreach(['left' => 'right', 'right' => 'left'] as $side => $eye):
+  $hasEyeFunc = 'has'.ucfirst($eye);
+  $targetIOP = $eye."_target_iop";
+?>
+	<div class="element-eye <?=$eye?>-eye column">
 		<div class="data-row">
 			<div class="data-value">
-				<?php if ($element->hasRight()) {
-    ?>
+				<?php if ($element->$hasEyeFunc()):?>
 					<div class="row data-row">
-						<div class="large-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('right_target_iop_id'))?></div></div>
-						<div class="large-7 column end"><div class="data-value" id="OEModule_OphCiExamination_models_Element_OphCiExamination_OverallManagementPlan_right_target_iop_id"><?php echo $element->right_target_iop->name?> mmHg</div></div>
+						<div class="cols-5 column">
+              <div class="data-label">
+                  <?php echo CHtml::encode($element->getAttributeLabel($eye.'_target_iop_id'))?>
+              </div>
+            </div>
+						<div class="cols-7 column end">
+              <div
+                  class="data-value"
+                  id="OEModule_OphCiExamination_models_Element_OphCiExamination_OverallManagementPlan_<?=$eye?>_target_iop_id"
+              >
+                  <?php echo $element->$targetIOP->name?> mmHg
+              </div>
+            </div>
 					</div>
 
-				<?php
-
-} else {
-    ?>
+				<?php else:?>
 					Not recorded
-				<?php 
-}?>
+				<?php endif;?>
 			</div>
 		</div>
 	</div>
-	<div class="element-eye left-eye column">
-		<div class="data-row">
-			<div class="data-value">
-				<?php if ($element->hasLeft()) {
-    ?>
-					<div class="row data-row">
-						<div class="large-5 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('left_target_iop_id'))?></div></div>
-						<div class="large-7 column end"><div class="data-value" id="OEModule_OphCiExamination_models_Element_OphCiExamination_OverallManagementPlan_left_target_iop_id"><?php echo $element->left_target_iop->name?> mmHg</div></div>
-					</div>
-				<?php
-
-} else {
-    ?>
-					Not recorded
-				<?php 
-}?>
-			</div>
-		</div>
-	</div>
+<?php endforeach;?>
 </div>
