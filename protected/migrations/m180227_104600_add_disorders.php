08cc07ebef07309f434fbffe1dc5c06584f842bc
Mike Pargeter <mike.pargeter@abehr.com>
2018-02-27 13:09:13 +0000

    FD-1740 Adds systemic disorders

---
+++ protected/migrations/m180227_104600_add_disorders.php
@@ -0,0 +1,26 @@
+<?php
+
+class m180227_104600_add_disorders extends OEMigration
+{
+	public function up()
+	{
+	    $this->execute("INSERT IGNORE INTO disorder (id, fully_specified_name, term, specialty_id) VALUES (48867003, 'Bradycardia (finding)', 'Bradycardia', NULL);");
+
+	    $this->execute("INSERT IGNORE INTO disorder (id, fully_specified_name, term, specialty_id) VALUES (3424008, 'Tachycardia (finding)', 'Tachycardia', NULL);");
+
+	    $this->execute("INSERT IGNORE INTO disorder (id, fully_specified_name, term, specialty_id) VALUES (88610006, 'Heart murmur (finding)', 'Heart murmur', NULL);");
+
+	    $this->execute("INSERT IGNORE INTO disorder (id, fully_specified_name, term, specialty_id) VALUES (60845006, 'Dyspnea on exertion (finding)', 'Dyspnea on exertion', NULL);");
+
+	    $this->execute("INSERT IGNORE INTO disorder (id, fully_specified_name, term, specialty_id) VALUES (399153001, 'Vertigo (finding)', 'Vertigo', NULL);");
+
+	    $this->execute("INSERT IGNORE INTO disorder (id, fully_specified_name, term, specialty_id) VALUES (60862001, 'Tinnitus (finding)', 'Tinnitus', NULL);");
+
+	}
+
+	public function down()
+	{
+		echo 'Down method not supported';
+	}
+
+}
