8613fd095cc8140ff7219710d9e0c0af665ff48e
Liam Marshall <marshl@live.com.au>
2018-02-28 14:54:25 +1100

    v3: Added a flag to the top of the login page to explain the brand menu

--- protected/views/base/_header.php
+++ protected/views/base/_header.php
@@ -1,8 +1,19 @@
+<?php
+/**
+ * @var SiteController $this
+ */
+?>
 
 <header class="oe-header">
+    <?php if ($this->action->id === 'login'): ?>
+      <div class="oe-logo-flag-help">
+        <i class="oe-i direction-left pro-theme no-click"></i> Click for Help, Theme change, Tours &amp; Feedback
+      </div>
+    <?php endif; ?>
+
     <?php if ($this->renderPatientPanel === true) {
         $this->renderPartial('//patient/_patient_id');
-    }?>
+    } ?>
 
     <?php $this->renderPartial('//base/_form'); ?>
 </header><!-- /.header -->
