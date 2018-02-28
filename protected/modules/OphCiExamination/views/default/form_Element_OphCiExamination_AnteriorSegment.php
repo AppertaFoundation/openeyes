adc7f5d42bfea4af4d1b99bf60cddef5d5209657
Liam Marshall <marshl@live.com.au>
2018-03-01 10:08:15 +1100

    v3: Changed the side/eye variables in the Anterior Segment element to be clearer

--- protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_AnteriorSegment.php
+++ protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_AnteriorSegment.php
@@ -20,33 +20,33 @@
 <div class="element-fields element-eyes">
     <?php echo $form->hiddenInput($element, 'eye_id', false, array('class' => 'sideField')); ?>
 
-    <?php foreach (array('right' => 'left', 'left' => 'right') as $eye => $side): ?>
-      <div class="element-eye column <?= $eye ?>-eye <?= $side ?> side
-    <?php if (!$element->hasEye($eye)) { ?> inactive<?php } ?>" data-side="<?= $eye ?>">
+    <?php foreach (array('left' => 'right', 'right' => 'left') as $page_side => $eye_side): ?>
+      <div class="element-eye column <?= $eye_side ?>-eye <?= $page_side ?> side
+    <?php if (!$element->hasEye($eye_side)) { ?> inactive<?php } ?>" data-side="<?= $eye_side ?>">
 
-        <div class="active-form" style="<?= !$element->hasEye($eye) ? 'display: none;' : '' ?>">
+        <div class="active-form" style="<?= !$element->hasEye($eye_side) ? 'display: none;' : '' ?>">
           <a class="remove-side"><i class="oe-i remove-circle small"></i></a>
           <br/>
           <div class="eyedraw-row flex-layout flex-top anterior-segment">
               <?php $this->renderPartial($element->form_view . '_OEEyeDraw', array(
                   'form' => $form,
-                  'side' => $eye,
+                  'side' => $eye_side,
                   'element' => $element,
               )) ?>
           </div>
           <div class="eyedraw-data stack">
               <?php $this->renderPartial($element->form_view . '_OEEyeDraw_fields', array(
                   'form' => $form,
-                  'side' => $eye,
+                  'side' => $eye_side,
                   'element' => $element,
               ));
               ?>
           </div>
         </div>
-        <div class="inactive-form" style="<?= $element->hasEye($eye) ? 'display: none;' : '' ?>">
+        <div class="inactive-form" style="<?= $element->hasEye($eye_side) ? 'display: none;' : '' ?>">
           <div class="add-side">
             <a href="#">
-              Add <?= $eye ?> side <span class="icon-add-side"></span>
+              Add <?= $eye_side ?> side <span class="icon-add-side"></span>
             </a>
           </div>
         </div>
