fdcb72db559c77c7474e8cb85e10e94b2a84cb91
Liam Marshall <marshl@live.com.au>
2018-03-02 10:55:55 +1100

    v3: Updated the Examination: Lids Surgical element to the new layout

--- protected/modules/OphCiExamination/views/default/view_SurgicalLids_OEEyeDraw.php
+++ protected/modules/OphCiExamination/views/default/view_SurgicalLids_OEEyeDraw.php
@@ -15,33 +15,21 @@
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 ?>
-<div class="row">
-    <div class="column large-6">
-        <?php $this->widget('application.modules.eyedraw.OEEyeDrawWidget', array(
-            'idSuffix' => $side.'_'.$element->elementType->id.'_'.$element->id,
-            'side' => ($side == 'right') ? 'R' : 'L',
-            'mode' => 'view',
-            'width' => 200,
-            'height' => 200,
-            'model' => $element,
-            'attribute' => $side.'_eyedraw',
-        ))?>
-    </div>
-    <div class="column large-6">
-        <div class="data-row">
-            <div class="data-value">
-                <?= Yii::app()->format->Ntext($element->{$side.'_ed_report'}) ?>
-            </div>
-        </div>
-        <?php if ($element->{$side.'_comments'}) {
-            ?>
-            <div class="data-row">
-                <div class="data-value">
-                    <?= Yii::app()->format->Ntext($element->{$side.'_comments'}) ?>
-                </div>
-            </div>
-            <?php
-        }?>
-    </div>
+<div class="eyedraw-canvas">
+    <?php $this->widget('application.modules.eyedraw.OEEyeDrawWidget', array(
+        'idSuffix' => $side . '_' . $element->elementType->id . '_' . $element->id,
+        'side' => ($side == 'right') ? 'R' : 'L',
+        'mode' => 'view',
+        'width' => 200,
+        'height' => 200,
+        'model' => $element,
+        'attribute' => $side . '_eyedraw',
+    )) ?>
 </div>
 
+<div class="eyedraw-data stack">
+  <div class="data-value"><?= Yii::app()->format->Ntext($element->{$side . '_ed_report'}) ?></div>
+    <?php if ($element->{$side . '_comments'}): ?>
+      <div class="data-value"><?= Yii::app()->format->Ntext($element->{$side . '_comments'}) ?></div>
+    <?php endif; ?>
+</div>
