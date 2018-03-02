368cebaf9fc6ac3265b27169dc9ae3fce8f61d20
Mike Pargeter <mike.pargeter@abehr.com>
2018-03-02 08:28:32 +0000

    FD-1740 Added dry eyes finding.

--- protected/migrations/m180227_104600_add_disorders.php
+++ protected/migrations/m180227_104600_add_disorders.php
@@ -16,6 +16,8 @@ class m180227_104600_add_disorders extends OEMigration
 
 	    $this->execute("INSERT IGNORE INTO disorder (id, fully_specified_name, term, specialty_id) VALUES (60862001, 'Tinnitus (finding)', 'Tinnitus', NULL);");
 
+	    $this->execute("INSERT IGNORE INTO disorder (id, fully_specified_name, term, specialty_id) VALUES (162290004, 'Dry eyes (finding)', 'Dry eyes', 109);");
+
 	}
 
 	public function down()
