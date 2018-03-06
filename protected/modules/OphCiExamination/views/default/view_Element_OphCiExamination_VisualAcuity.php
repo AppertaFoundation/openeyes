bf95f2536ad2d7b0af85a470d5c7627e84fae38c
Liam Marshall <marshl@live.com.au>
2018-03-06 12:33:19 +1100

    v3: Updated the Visual Acuity view to have the new layout and reduced its complexity

--- protected/modules/OphCiExamination/views/default/view_Element_OphCiExamination_VisualAcuity.php
+++ protected/modules/OphCiExamination/views/default/view_Element_OphCiExamination_VisualAcuity.php
@@ -18,135 +18,73 @@
 ?>
 <?php
 
-    $all_units = $element->getUnits($element->unit->id, false);
-    $va_tooltip_right = "";
-    $va_tooltip_left = "";
-    foreach($all_units as $unit) {
-        $va_tooltip_right.='<b>'.$unit->name.'</b>:<br/> '.$element->getCombined('right', $unit->id).'<br/>';
-        $va_tooltip_left.='<b>'.$unit->name.'</b>:<br/> '.$element->getCombined('left', $unit->id).'<br/>';
-    }
+$all_units = $element->getUnits($element->unit->id, false);
+$va_tooltip_right = "";
+$va_tooltip_left = "";
+foreach($all_units as $unit) {
+    $va_tooltip_right.='<b>'.$unit->name.'</b>:<br/> '.$element->getCombined('right', $unit->id).'<br/>';
+    $va_tooltip_left.='<b>'.$unit->name.'</b>:<br/> '.$element->getCombined('left', $unit->id).'<br/>';
+}
 
 
-    $cvi_api = Yii::app()->moduleAPI->get('OphCoCvi');
-    if ($cvi_api) {
-        echo $cvi_api->renderAlertForVA($this->patient, $element, true);
-    }
+$cvi_api = Yii::app()->moduleAPI->get('OphCoCvi');
+if ($cvi_api) {
+    echo $cvi_api->renderAlertForVA($this->patient, $element, true);
+}
 
 ?>
 
 <?php echo CHtml::hiddenField('element_id', $element->id, array('class' => 'element_id')); ?>
 
 <div class="element-data element-eyes">
-    <div class="element-eye right-eye">
-        <?php if ($element->hasRight()) {
-            ?>
-            <?php if ($element->getCombined('right')) {
-                ?>
-                <div class="data-row">
-                    <div class="data-value">
-                      <span class="large-text">
-                        <?php echo $element->unit->name ?>
-                      </span>
-                      <i class="oe-i info small pad"></i>
-                    </div>
-                </div>
-                <div class="data-row">
-                    <div class="data-value">
-                      <span class="large-text">
-                        <?php echo $element->getCombined('right') ?>
-                      </span>
-                      <i class="oe-i info small pad"></i>
-                    </div>
-                </div>
-                <?php
-            } else {
-                ?>
-                <div class="data-row">
-                    <div class="data-value">
-                        Not recorded
-                        <?php if ($element->right_unable_to_assess) {
-                            ?>
-                            (Unable to assess<?php if ($element->right_eye_missing) {
-                                ?>, eye missing<?php
-                            }
-                            ?>)
-                            <?php
-                        } elseif ($element->right_eye_missing) {
-                            ?>
-                            (Eye missing)
-                            <?php
-                        }
-                        ?>
-                    </div>
-                </div>
-                <?php
-            }
-            ?>
-            <?php
-        } else {
-            ?>
-            <div class="data-row">
+    <?php foreach (array('left' => 'right', 'right' => 'left') as $page_side => $eye_side): ?>
+      <div class="element-eye <?= $eye_side ?>-eye">
+          <?php if ($element->hasEye($eye_side)) {
+              ?>
+              <?php if ($element->getCombined($eye_side)) {
+                  ?>
+              <div class="data-row">
                 <div class="data-value">
-                    Not recorded
-                </div>
-            </div>
-            <?php
-        } ?>
-    </div>
-    <div class="element-eye left-eye">
-        <?php if ($element->hasLeft()) {
-            ?>
-            <?php if ($element->getCombined('left')) {
-                ?>
-                <div class="data-row">
-                    <div class="data-value">
-                      <span class="large-text">
-                        <?php echo $element->unit->name ?>
+                      <span class="priority-text">
+                        <?php echo $element->getCombined($eye_side) ?>
                       </span>
-                      <i class="oe-i info small pad"></i>
-                    </div>
+                  <i class="oe-i info small pad"></i>
                 </div>
-                <div class="data-row">
-                    <div class="data-value">
-                      <span class="large-text">
-                        <?php echo $element->getCombined('left') ?>
-                      </span>
-                      <i class="oe-i info small pad"></i>
-                    </div>
-                </div>
-                <?php
-            } else {
-                ?>
-                <div class="data-row">
-                    <div class="data-value">
-                        Not recorded
-                        <?php if ($element->left_unable_to_assess) {
-                            ?>
-                            (Unable to assess<?php if ($element->left_eye_missing) {
-                                ?>, eye missing<?php
-                            }
-                            ?>)
-                            <?php
-                        } elseif ($element->left_eye_missing) {
-                            ?>
-                            (Eye missing)
-                            <?php
-                        }
-                        ?>
-                    </div>
-                </div>
-                <?php
-            }
-            ?>
-            <?php
-        } else {
-            ?>
-            <div class="data-row">
+              </div>
+                  <?php
+              } else {
+                  ?>
+              <div class="data-row">
                 <div class="data-value">
-                    Not recorded
+                  Not recorded
+                    <?php if ($element->{$eye_side . '_unable_to_assess'}) {
+                        ?>
+                      (Unable to assess<?php if ($element->{$eye_side . '_eye_missing'}) {
+                            ?>, eye missing<?php
+                        }
+                        ?>)
+                        <?php
+                    } elseif ($element->{$eye_side . '_eye_missing'}) {
+                        ?>
+                      (Eye missing)
+                        <?php
+                    }
+                    ?>
                 </div>
+              </div>
+                  <?php
+              }
+              ?>
+              <?php
+          } else {
+              ?>
+            <div class="data-row">
+              <div class="data-value">
+                Not recorded
+              </div>
             </div>
-            <?php
-        } ?>
-    </div>
+              <?php
+          } ?>
+      </div>
+    <?php endforeach; ?>
 </div>
