ac614108b2e58a7502e00ebb78ec9261cfecc639
Unknown <mrisaacjackson@gmail.com>
2018-03-07 15:50:52 +1100

    Changed examination injection management view for new ui
    Refactored eyes to use foreach loop rather than duplication
    Changed elements to a table format

--- protected/modules/OphCiExamination/views/default/view_Element_OphCiExamination_InjectionManagementComplex_fields.php
+++ protected/modules/OphCiExamination/views/default/view_Element_OphCiExamination_InjectionManagementComplex_fields.php
@@ -16,116 +16,80 @@
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 ?>
-<?php if ($element->{$side.'_no_treatment'}) {
-    ?>
-	<div class="row data-row">
-		<div class="large-4 column">
-			<div class="data-label">
-				<?php echo $element->getAttributeLabel($side.'_no_treatment_reason_id')?>:
-			</div>
-		</div>
-		<div class="large-8 column">
-			<div class="data-value">
-				<?= Yii::app()->format->Ntext($element->{"{$side}NoTreatmentReasonName"}) ?>
-			</div>
-		</div>
-	</div>
-<?php 
-} else {
-    ?>
-	<div class="row data-row">
-		<div class="large-4 column">
-			<div class="data-label">
-				<?php echo $element->getAttributeLabel($side.'_diagnosis1_id')?>:
-			</div>
-		</div>
-		<div class="large-8 column">
-			<div class="data-value">
-				<?php echo $element->{$side.'_diagnosis1'}->term?>
-			</div>
-		</div>
-	</div>
-	<?php if ($element->{$side.'_diagnosis2_id'}) {
-    ?>
-		<div class="row data-row">
-			<div class="large-4 column">
-				<div class="data-label">
-					<?php echo $element->getAttributeLabel($side.'_diagnosis2_id')?>:
-				</div>
-			</div>
-			<div class="large-8 column">
-				<div class="data-value">
-					<?php echo $element->{$side.'_diagnosis2'}->term?>
-				</div>
-			</div>
-		</div>
-	<?php 
-}
-    ?>
-	<?php foreach ($element->{$side.'_answers'} as $answer) {
-    ?>
-		<div class="row data-row">
-			<div class="large-4 column">
-				<div class="data-label">
-					<?php echo $answer->question->question?>
-				</div>
-			</div>
-			<div class="large-8 column">
-				<div class="data-value">
-					<?php echo ($answer->answer) ? 'Yes' : 'No'?>
-				</div>
-			</div>
-		</div>
-	<?php 
-}
-    ?>
-	<?php if ($element->{$side.'_treatment'}) {
-    ?>
-		<div class="row data-row">
-			<div class="large-4 column">
-				<div class="data-label">
-					<?php echo $element->getAttributeLabel($side.'_treatment_id')?>:
-				</div>
-			</div>
-			<div class="large-8 column">
-				<div class="data-value">
-					<?php echo $element->{$side.'_treatment'}->name?>
-				</div>
-			</div>
-		</div>
-	<?php 
-}
-    ?>
-	<div class="row data-row">
-		<div class="large-4 column">
-			<div class="data-label">
-				<?php echo $element->getAttributeLabel($side.'_risks')?>:
-			</div>
-		</div>
-		<div class="large-8 column">
-			<div class="data-value">
-				<?php if (!$element->{$side.'_risks'}) {
-    echo 'None';
-} else {
-    foreach ($element->{$side.'_risks'} as $item) {
-        echo $item->name.'<br />';
-    }
-}
-    ?>
-			</div>
-		</div>
-	</div>
-	<div class="row data-row">
-		<div class="large-4 column">
-			<div class="data-label">
-				<?php echo $element->getAttributeLabel($side.'_comments')?>:
-			</div>
-		</div>
-		<div class="large-8 column">
-			<div class="data-value">
-				<?= Yii::app()->format->Ntext($element->{"{$side}_comments"}) ?>
-			</div>
-		</div>
-	</div>
-<?php 
-}?>
+<table class="cols-full">
+  <tbody>
+    <?php if ($element->{$side.'_no_treatment'}) :?>
+      <tr class="flex-layout">
+        <td class="cols-4">
+            <?= $element->getAttributeLabel($side.'_no_treatment_reason_id')?>:
+        </td>
+        <td class="cols-8">
+            <?= Yii::app()->format->Ntext($element->{"{$side}NoTreatmentReasonName"}) ?>
+        </td>
+      </tr>
+    <?php else: ?>
+      <tr>
+        <td class="cols-4">
+            <?= $element->getAttributeLabel($side.'_diagnosis1_id')?>:
+        </td>
+        <td class="cols-8">
+            <?= $element->{$side.'_diagnosis1'}->term?>
+        </td>
+      </tr>
+      <?php if ($element->{$side.'_diagnosis2_id'}) {?>
+        <tr>
+          <td class="cols-4">
+              <?= $element->getAttributeLabel($side.'_diagnosis2_id')?>:
+          </td>
+          <td class="cols-8">
+              <?php echo $element->{$side.'_diagnosis2'}->term?>
+          </td>
+        </tr>
+      <?php }
+      foreach ($element->{$side.'_answers'} as $answer) {?>
+        <tr>
+          <td class="cols-4">
+              <?php echo $answer->question->question?>
+          </td>
+          <td class="cols-8">
+              <?php echo ($answer->answer) ? 'Yes' : 'No'?>
+          </td>
+        </tr>
+      <?php }?>
+      <?php if ($element->{$side.'_treatment'}) {?>
+        <tr>
+          <td class="cols-4">
+              <?php echo $element->getAttributeLabel($side.'_treatment_id')?>:
+          </td>
+          <td class="cols-8">
+              <?php echo $element->{$side.'_treatment'}->name?>
+          </td>
+        </tr>
+      <?php }?>
+      <tr>
+        <td class="cols-4">
+            <?php echo $element->getAttributeLabel($side.'_risks')?>:
+        </td>
+        <td class="cols-8">
+            <?php
+            if (!$element->{$side.'_risks'}) {
+              echo 'None';
+              } else {
+                foreach ($element->{$side.'_risks'} as $item) {
+                    echo $item->name.'<br />';
+                }
+            }
+            ?>
+        </td>
+      </tr>
+      <tr>
+        <td class="cols-4">
+            <?php echo $element->getAttributeLabel($side.'_comments')?>:
+        </td>
+        <td class="cols-8">
+            <?= Yii::app()->format->Ntext($element->{"{$side}_comments"}) ?>
+        </td>
+      </tr>
+    <?php endif;?>
+  </tbody>
+</table>
