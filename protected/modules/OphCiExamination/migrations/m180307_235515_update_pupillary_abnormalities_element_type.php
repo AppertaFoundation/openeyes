1979b0adb64b731d29547ce6f06ba62c58dd9154
Zhaolian-Zhou <zhaolian.zhou@fivium.com.au>
2018-03-09 04:21:30 +0000

    pupillary abnormalities, change display order and path

---
+++ protected/modules/OphCiExamination/migrations/m180307_235515_update_pupillary_abnormalities_element_type.php
@@ -0,0 +1,23 @@
+<?php
+
+class m180307_235515_update_pupillary_abnormalities_element_type extends CDbMigration
+{
+    // Use safeUp/safeDown to do migration with transaction
+    public function safeUp()
+    {
+        $parent_id = $this->getDbConnection()->createCommand("select id from element_type where class_name='OEModule\\\OphCiExamination\\\models\\\Element_OphCiExamination_VisualFunction'")->queryRow();
+        if (isset($parent_id['id']) && $parent_id['id']) {
+            $this->update('element_type',
+                array('name' => 'Pupils', 'display_order' => 5, 'parent_element_type_id' => $parent_id['id']),
+                "class_name='OEModule\\\OphCiExamination\\\models\\\Element_OphCiExamination_PupillaryAbnormalities'");
+        }
+    }
+
+    public function safeDown()
+    {
+        $this->update('element_type',
+            array('name' => 'Pupillary Abnormalities', 'display_order' => 45, 'parent_element_type_id' => null),
+            "class_name='OEModule\\\OphCiExamination\\\models\\\Element_OphCiExamination_PupillaryAbnormalities'");
+    }
+
+}
