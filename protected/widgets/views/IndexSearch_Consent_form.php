6e5aed6f4a3f989c3fcee33605ddd0dda46b0020
Zhaolian-Zhou <zhaolian.zhou@fivium.com.au>
2018-03-20 04:01:17 +0000

    consent form, code clean

--- protected/widgets/views/IndexSearch_Consent_form.php
+++ protected/widgets/views/IndexSearch_Consent_form.php
@@ -1,11 +1,4 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Zhaolian
- * Date: 16/03/2018
- * Time: 1:34 PM
- */
-?>
+
 <nav class="sidebar-header">
     <span class="title">Consent Form</span>
 </nav>
