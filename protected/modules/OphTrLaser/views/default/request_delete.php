cb719cfec7bc5245b2cab8bb96794060575b39a8
Liam Marshall <marshl@live.com.au>
2018-03-22 09:40:21 +1100

    v3: Updated the Laser event Delete and Delete Request pages to the layout

--- protected/modules/OphTrLaser/views/default/request_delete.php
+++ protected/modules/OphTrLaser/views/default/request_delete.php
@@ -15,36 +15,40 @@
  * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
- ?>
+?>
 <?php $this->beginContent('//patient/event_container'); ?>
 
-	<?php echo CHtml::form(array('Default/requestDeletion/'.$this->event->id), 'post', array('id' => 'deleteForm'))?>
-	<div id="delete_event">
-		<h3>Request event deletion</h3>
-		<div class="alert-box issue with-icon">
-			<strong>This will send a request to delete the event to an admin user.</strong>
-		</div>
-		<?php $this->displayErrors($errors)?>
-		<div style="width:300px; margin-bottom: 0.6em;">
-			<p>Reason for deletion:</p>
-			<?php echo CHtml::textArea('delete_reason', '')?>
-		</div>
-		<p>
-			<strong>Are you sure you want to proceed?</strong>
-		</p>
-		<?php
-            echo CHtml::hiddenField('event_id', $this->event->id); ?>
-			<button type="submit" class="warning" id="et_deleteevent" name="et_deleteevent">
-				Request deletion
-			</button>
-			<button type="submit" class="secondary" id="et_canceldelete" name="et_canceldelete">
-				Cancel
-			</button>
-			<img class="loader" src="<?php echo Yii::app()->createUrl('img/ajax-loader.gif')?>" alt="loading..." style="display: none;" />
-		<?php echo CHtml::endForm(); ?>
-	</div>
-
+<section class="element">
+  <section class="element-fields full-width">
+      <?php echo CHtml::form(array('Default/requestDeletion/' . $this->event->id), 'post',
+          array('id' => 'deleteForm')) ?>
+    <div id="delete_event">
+      <h3>Request event deletion</h3>
+      <div class="alert-box issue with-icon">
+        <strong>This will send a request to delete the event to an admin user.</strong>
+      </div>
+        <?php $this->displayErrors($errors) ?>
+      <div style="width:300px; margin-bottom: 0.6em;">
+        <p>Reason for deletion:</p>
+          <?php echo CHtml::textArea('delete_reason', '', array('cols' => 40)) ?>
+      </div>
+      <p>
+        <strong>Are you sure you want to proceed?</strong>
+      </p>
+        <?php
+        echo CHtml::hiddenField('event_id', $this->event->id); ?>
+      <button type="submit" class="button red" id="et_deleteevent" name="et_deleteevent">
+        Request deletion
+      </button>
+      <button type="submit" class="button" id="et_canceldelete" name="et_canceldelete">
+        Cancel
+      </button>
+      <i class="loader spinner" style="display: none;"></i>
+        <?php echo CHtml::endForm(); ?>
+    </div>
+  </section>
+</section>
 <?php $this->endContent() ?>
 <script type="text/javascript">
-	$('#delete_reason').focus();
+  $('#delete_reason').focus();
 </script>
