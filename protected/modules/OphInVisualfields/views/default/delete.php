20e4f0a67167e82cbc572f06e85f489575cee0ee
Liam Marshall <marshl@live.com.au>
2018-04-10 11:36:08 +1000

    v3: Updated the Visual Fields delete page to the new layout

--- protected/modules/OphInVisualfields/views/default/delete.php
+++ protected/modules/OphInVisualfields/views/default/delete.php
@@ -18,30 +18,38 @@
 $this->beginContent('//patient/event_container', array('no_face'=>true));
 ?>
 
-	<?php echo CHtml::form(array('Default/delete/'.$this->event->id), 'post', array('id' => 'deleteForm'))?>
-	<div id="delete_event">
-		<h3>Delete event</h3>
-		<div class="alert-box alert with-icon">
-			<strong>WARNING: This will permanently delete the event and remove it from view.<br><br>THIS ACTION CANNOT BE UNDONE.</strong>
-		</div>
-		<?php $this->displayErrors(@$errors)?>
-		<div style="width:300px; margin-bottom: 0.6em;">
-			<p>Reason for deletion:</p>
-			<?php echo CHtml::textArea('delete_reason', '')?>
-		</div>
-		<p>
-			<strong>Are you sure you want to proceed?</strong>
-		</p>
-		<?php
-            echo CHtml::hiddenField('event_id', $this->event->id); ?>
-			<button type="submit" class="warning" id="et_deleteevent" name="et_deleteevent">
-				Delete event
-			</button>
-			<button type="submit" class="secondary" id="et_canceldelete" name="et_canceldelete">
-				Cancel
-			</button>
-			<img class="loader" src="<?php echo Yii::app()->assetManager->createUrl('img/ajax-loader.gif')?>" alt="loading..." style="display: none;" />
-		<?php echo CHtml::endForm(); ?>
-	</div>
+
+<section class="element">
+  <section class="element-fields full-width">
+
+      <?php echo CHtml::form(array('Default/delete/' . $this->event->id), 'post', array('id' => 'deleteForm')) ?>
+    <div id="delete_event">
+      <h3>Delete event</h3>
+      <div class="alert-box alert with-icon">
+        <strong>WARNING: This will permanently delete the event and remove it from view.<br><br>THIS ACTION CANNOT BE
+          UNDONE.</strong>
+      </div>
+        <?php $this->displayErrors(@$errors) ?>
+      <div style="width:300px; margin-bottom: 0.6em;">
+        <p>Reason for deletion:</p>
+          <?php echo CHtml::textArea('delete_reason', '', array('cols' => 40)) ?>
+      </div>
+      <p>
+        <strong>Are you sure you want to proceed?</strong>
+      </p>
+        <?php
+        echo CHtml::hiddenField('event_id', $this->event->id); ?>
+      <button type="submit" class="button red" id="et_deleteevent" name="et_deleteevent">
+        Delete event
+      </button>
+      <button type="submit" class="button" id="et_canceldelete" name="et_canceldelete">
+        Cancel
+      </button>
+      <i class="spinner loader" style="display: none;"></i>
+        <?php echo CHtml::endForm(); ?>
+    </div>
+  </section>
+</section>
 
 <?php $this->endContent() ?>
+
