cc8d2d15efd63a5e82e70d3deb7e59366b9fe558
Zhaolian-Zhou <zhaolian.zhou@fivium.com.au>
2018-04-22 23:00:24 +0000

    v3-ui, remove history medication, end date default value

--- protected/modules/OphCiExamination/widgets/views/HistoryMedicationsEntry_prescription_event_edit.php
+++ protected/modules/OphCiExamination/widgets/views/HistoryMedicationsEntry_prescription_event_edit.php
@@ -67,16 +67,14 @@ if (isset($entry->end_date) && strtotime($entry->end_date)) {
   </td>
   <td>
         <fieldset class="row field-row fuzzy-date">
-            <div class="large-11 column end">
+            <div class="cols-11">
                 <?php if (!$entry->end_date) { ?>
-                  <input id="datepicker_2_<?=$row_count?>"
-                         name="<?= $field_prefix ?>[end_date]" value="<?= $entry->end_date? $entry->end_date: date('Y-m-d') ?>"
-                         style="width:80px" placeholder="yyyy-mm-dd">
+                  <input id="datepicker_2_<?=$row_count?>" name="<?= $field_prefix ?>[end_date]" value="<?= $entry->end_date ?>" style="width:80px" placeholder="yyyy-mm-dd">
                   <i class="js-has-tooltip oe-i info small pad right" data-tooltip-content="You can enter date format as yyyy-mm-dd, or yyyy-mm or yyyy."></i>
                 <?php } else { ?>
                   <i class="oe-i start small pad"></i>
                     <?=Helper::formatFuzzyDate($entry->end_date) ?>
-                  <input type="hidden" name="<?= $field_prefix ?>[end_date]" value="<?= $entry->end_date? $entry->end_date: date('Y-m-d') ?>" />
+                  <input type="hidden" name="<?= $field_prefix ?>[end_date]" value="<?= $entry->end_date ?>" />
                 <?php } ?>
             </div>
         </fieldset>
