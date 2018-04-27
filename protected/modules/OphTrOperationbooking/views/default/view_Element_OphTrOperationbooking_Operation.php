65c0ecaae2ee833245515f09e494197466a8ee06
neilsf <feketecsaba@gmail.com>
2018-04-27 08:59:32 +0200

    Simplify specification of complexity rating in Operation Booking - any_grade_of_doctor field is now nullable - added new field "complexity" - senior fellow to do and any grade of doctor fields are hidden - renamed named consultant and consultant required attr labels

--- protected/modules/OphTrOperationbooking/views/default/view_Element_OphTrOperationbooking_Operation.php
+++ protected/modules/OphTrOperationbooking/views/default/view_Element_OphTrOperationbooking_Operation.php
@@ -16,6 +16,7 @@
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 ?>
+
 <section class="element element-data">
     <h3 class="data-title">Procedure<?php if (count($element->procedures) != 1) echo 's' ?></h3>
     <ul class="data-value highlight important">
@@ -26,6 +27,14 @@
 </section>
 
 <section class="element element-data">
+    <?php if(!is_null($element->complexity)): ?>
+    <div class="row">
+        <div class="large-6 column">
+            <h3 class="data-title"><?php echo $element->getAttributeLabel('complexity') ?>:</h3>
+            <div class="data-value"><?php echo $element->getComplexityCaption(); ?></div>
+        </div>
+    </div>
+    <?php endif; ?>
     <div class="row">
         <div class="large-6 column">
             <h3 class="data-title">Consultant required?</h3>
@@ -44,12 +53,6 @@
                 <div class="data-value"><?= $element->senior_fellow_to_do ? 'Yes' : 'No' ?></div>
             </div>
         <?php endif; ?>
-    </div>
-    <div class="row">
-        <div class="large-6 column">
-            <h3 class="data-title"><?= CHtml::encode($element->getAttributeLabel('any_grade_of_doctor')) ?>?</h3>
-            <div class="data-value"><?php echo $element->any_grade_of_doctor ? 'Yes' : 'No' ?></div>
-        </div>
     </div>
     <div class="row">
         <div class="large-6 column">
