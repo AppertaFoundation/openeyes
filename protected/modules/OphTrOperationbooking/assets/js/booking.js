cbb6f0d285e054ae3f6d0a191464595f71175a52
Liam Marshall <marshl@live.com.au>
2018-04-30 14:33:29 +1000

    v3: Fixed an issue where the user couldn't schedule a Booking now because the event wasn't being triggered

--- protected/modules/OphTrOperationbooking/assets/js/booking.js
+++ protected/modules/OphTrOperationbooking/assets/js/booking.js
@@ -103,19 +103,18 @@ $(document).ready(function() {
         return true;
 	});
 
+  $(this).on('click', '#et_save_and_schedule', function () {
+    $('#schedule_now').val(1);
+  });
 
-	handleButton($('#et_save_and_schedule'),function() {
-		$('#schedule_now').val(1);
-	});
-
-	handleButton($('#et_cancel'),function(e) {
-		if (m = window.location.href.match(/\/update\/[0-9]+/)) {
-			window.location.href = window.location.href.replace('/update/','/view/');
-		} else {
-			window.location.href = baseUrl+'/patient/episodes/'+OE_patient_id;
-		}
-		e.preventDefault();
-	});
+  $(this).on('click', '#et_cancel', function (e) {
+    if (m = window.location.href.match(/\/update\/[0-9]+/)) {
+      window.location.href = window.location.href.replace('/update/', '/view/');
+    } else {
+      window.location.href = baseUrl + '/patient/episodes/' + OE_patient_id;
+    }
+    e.preventDefault();
+  });
 
 	handleButton($('#et_deleteevent'));
 
