86c0b4e161855e7ecc30127dfc90e062fb68c9d5
Liam Marshall <marshl@live.com.au>
2018-05-15 10:31:00 +1000

    v3: Moved the eyedraw fields in the Operation Note - Buckle element to the right of the eyedraw widget

--- protected/modules/OphTrOperationnote/views/default/form_Element_OphTrOperationnote_Buckle.php
+++ protected/modules/OphTrOperationnote/views/default/form_Element_OphTrOperationnote_Buckle.php
@@ -22,14 +22,14 @@ $layoutColumns = $form->layoutColumns;
 $form->layoutColumns = array('label' => 3, 'field' => 9);
 ?>
 <div class="element-fields full-width">
-  <div class="eyedraw-row row buckle">
-    <div class="fixed column">
+  <div class="cols-11 flex-layout col-gap eyedraw-row buckle">
+    <div class="cols-6">
         <?php $this->renderPartial($element->form_view . '_OEEyeDraw', array(
             'element' => $element,
             'form' => $form,
         )); ?>
     </div>
-    <div class="fluid column">
+    <div class="cols-6">
         <?php $this->renderPartial($element->form_view . '_OEEyeDraw_fields', array(
             'form' => $form,
             'element' => $element,
