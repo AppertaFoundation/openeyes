1eb08e449589ce6fa3565e0299f7efde259ae9ea
Arpit Arora <arpit.arora89@live.com>
2018-05-15 14:26:15 +1000

    v3: virtual clinic migration to new version 3 of OE

--- protected/modules/PatientTicketing/views/default/form_queueset_select.php
+++ protected/modules/PatientTicketing/views/default/form_queueset_select.php
@@ -29,54 +29,45 @@ $form = $this->beginWidget('CActiveForm', array(
 ));
 ?>
 
-    <div <?php if (!$queueset) { ?> style="display: none;"<?php } ?>>
-        <button class="button blue hint" id="js-virtual-clinic-btn">Change <?= $category->name ?></button>
-    </div>
-    <div id="js-virtual-clinic" class="oe-popup-wrap" style="display: none"><!-- generic oe-popup -->
-        <div class="oe-popup">
-            <div class="title">Select <?= $category->name ?>:</div>
-            <div class="close-icon-btn"><i class="oe-i remove-circle pro-theme"></i></div>
-            <div class="oe-popup-content previous-elements">
-                <ul class="oe-btn-list">
-                    <?php foreach ($queueset_list as $clinic): ?>
-                        <li><?= $clinic ?></li>
-                    <?php endforeach; ?>
-                </ul>
-
-            </div>
-
+<div <?php if (!$queueset) { ?> style="display: none;"<?php } ?>>
+    <button class="button blue hint" id="js-virtual-clinic-btn">Change <?= $category->name ?></button>
+</div>
+<div class="oe-popup-wrap" style="display: none">
+    <div class="oe-popup">
+        <div class="title">Change Virtual Clinic</div>
+        <div class="close-icon-btn">
+            <i class="oe-i remove-circle pro-theme" id="close-btn"></i>
         </div>
-    </div>
-
-    <div id="queueset-select-toggle-wrapper"<?php if (!$queueset) {?> style="display: none;"<?php }?>><button class="secondary small" id="queueset-select-toggle">Change <?=$category->name?></button></div>
-    <div id="queueset-form" class="row field-row"<?php if ($queueset) { ?> style="display: none;"<?php } ?>>
-        <div class="title">Select <?= $category->name ?>:</div>
-        <div class="large-3 column"><?php echo CHtml::dropDownList('queueset_id', ($queueset ? $queueset->getId() : null), $queueset_list, array('empty' => '- Please Select -')) ?></div>
-        <div class="large-2 column end">
+        <div class="oe-popup-content previous-elements">
             <input type="hidden" name="cat_id" value="<?= $cat_id; ?>"/>
-            <input type="submit" class="secondary small" value="Select"/>
-            <?php if ($queueset) { ?>
-                <button class="small warning" id="queueset-select-cancel">Cancel</button>
-            <?php } ?>
+            <?php echo CHtml::hiddenField('queueset_id', ($queueset ? $queueset->getId() : null)) ?>
+            <ul class="oe-btn-list">
+                <?php foreach ($queueset_list as $item) {?>
+                    <li><a id="<?php echo $item?>"><?php echo $item ?></a> </li>
+                <?php }?>
+            </ul>
         </div>
     </div>
+</div>
+
+
 <?php
-$this->endWidget();?>
+$this->endWidget(); ?>
 <script>
-    $(document).on('click', '#js-virtual-clinic-btn', function(e) {
-
+    $(document).on('click', '#js-virtual-clinic-btn', function (e) {
         e.preventDefault();
-        var returnUrl = window.location.href;
-
-        new OpenEyes.UI.Dialog($.extend({}, {
-            url: baseUrl + '/site/changesiteandfirm',
-            width: 500,
-            data: {
-                returnUrl: returnUrl,
-                patient_id: window.OE_patient_id || null
-            }
-        })).open();
+        $('.oe-popup-wrap').css('display', 'flex');
     });
 
+    $(document).on('click', '#close-btn', function (e) {
+        e.preventDefault();
+        $('.oe-popup-wrap').css('display', 'none');
+    });
+
+    $(document).ready(function() {
+        $("a").click(function(event) {
+           $('#ticket-filter').submit();
+        });
+    });
 </script>
 
