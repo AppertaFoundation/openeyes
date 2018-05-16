e6de7ac89c0ff8359892dc49154eb0eb0ae474fe
Sabi Potondi <sabi.potondi@abehr.com>
2018-05-16 18:21:14 +0000

    OE-7253 - Adding RPEAtrophy to Posteriol Pole

--- protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_PosteriorPole_OEEyeDraw.php
+++ protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_PosteriorPole_OEEyeDraw.php
@@ -23,7 +23,7 @@
             array('HardDrusen', 'Geographic', 'CNV', 'RPEDetachment', 'EpiretinalMembrane', 'MacularHole', 'MacularDystrophy', 'Macroaneurysm', 'RetinalVeinOcclusionPostPole', 'RetinalArteryOcclusionPostPole'),
             array('Microaneurysm', 'BlotHaemorrhage', 'HardExudate', 'IRMA', 'Circinate', 'MacularThickening', 'CystoidMacularOedema', 'PreRetinalHaemorrhage', 'CottonWoolSpot', 'DiabeticNV', 'VitreousOpacity', 'FibrousProliferation', 'TractionRetinalDetachment'),
             array('SwollenDisc', 'Telangiectasis', 'ChoroidalHaemorrhage', 'ChoroidalNaevus'),
-            array('LaserSpot', 'FocalLaser', 'MacularGrid', 'SectorPRPPostPole', 'PRPPostPole'),
+            array('LaserSpot', 'FocalLaser', 'MacularGrid', 'SectorPRPPostPole', 'PRPPostPole', 'RPEAtrophy'),
         ),
         'onReadyCommandArray' => array(
             array('addDoodle', ['MaculaPostPole']),
