50c576cde9291a4259323eff3a248e51e7ec1e76
Arpit Arora <arpit.arora89@live.com>
2018-05-24 14:10:48 +1000

    v3-ui: restyled the view observation tile as per the v3-ui styles

--- protected/modules/OphCiExamination/views/default/view_Element_OphCiExamination_Observations.php
+++ protected/modules/OphCiExamination/views/default/view_Element_OphCiExamination_Observations.php
@@ -16,56 +16,106 @@
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 ?>
+<div class="element-fields full-width">
+    <div class="field-row flex-layout flex-left col-gap">
+        <div class="cols-4">
+            <table class="cols-full">
+                <colgroup>
+                    <col class="cols-4">
+                </colgroup>
+                <tbody>
+                <tr>
+                    <td>
+                        <span class="data-label"><?= $element->getAttributeLabel('blood_pressure') ?>:</span>
+                    </td>
+                    <td>
+                        <span class="data-value"><?php echo (!empty($element->blood_pressure_systolic)) ? $element->blood_pressure_systolic . '/' . $element->blood_pressure_diastolic . ' mmHg' : '' ?>
+                            <span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <span class="data-label"><?= $element->getAttributeLabel('blood_glucose') ?>:</span>
+                    </td>
+                    <td>
+                        <span class="data-value"><?php echo (!empty($element->blood_glucose)) ? $element->blood_glucose . ' mmol/l' : '' ?></span>
+                    </td>
 
-<div class="element-data">
-	<div class="large-4 column">
-        <div class="large-12 column">
-            <span class="data-label"><?= $element->getAttributeLabel('blood_pressure')?>:</span>&emsp;<span class="data-value"><?php echo (!empty($element->blood_pressure_systolic)) ? $element->blood_pressure_systolic.'/'. $element->blood_pressure_diastolic.' mmHg' :  '' ?><span>
+                </tr>
+                <tr>
+                    <td>
+                        <span class="data-label"><?= $element->getAttributeLabel('weight') ?>:</span>
+                    </td>
+                    <td>
+                        <span class="data-value"><?php echo (!empty($element->weight)) ? $element->weight . ' kg' : '' ?></span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
         </div>
-
-        <div class="large-12 column">
-            <span class="data-label"><?= $element->getAttributeLabel('blood_glucose')?>:</span>&emsp;<span class="data-value"><?php echo (!empty($element->blood_glucose)) ? $element->blood_glucose.' mmol/l': '' ?></span>
-        </div>
-
-        <div class="large-12 column">
-            <span class="data-label"><?= $element->getAttributeLabel('weight')?>:</span>&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;<span class="data-value"><?php echo (!empty($element->weight)) ? $element->weight.' kg': '' ?></span>
-        </div>
-    </div>
-
-    <div class="large-4 column">
-
-        <div class="large-12 column">
-            <span class="data-label"><?= $element->getAttributeLabel('o2_sat')?>:</span>&emsp;<span class="data-value"><?php echo (!empty($element->o2_sat)) ? $element->o2_sat.' %' : '' ?></span>
-        </div>
-
-        <div class="large-12 column">
-            <span class="data-label"><?= $element->getAttributeLabel('hba1c')?>:</span>&emsp;<span class="data-value"><?php echo (!empty($element->hba1c)) ? $element->hba1c.' mmol/mol' : '' ?></span>
-        </div>
-
-        <div class="large-12 column" >
-            <span class="data-label"><?= $element->getAttributeLabel('height')?>:</span>&emsp;<span class="data-value"><?php echo (!empty($element->height)) ? $element->height.' cm' : '' ?></span>
-        </div>
-    </div>
-
-    <div class="large-4 column">
-        <div class="large-12 column">
-            <span class="data-label"><?= $element->getAttributeLabel('pulse')?>:</span>&emsp;<span class="data-value"><?php echo (!empty($element->pulse)) ? $element->pulse.' BPM' : ''; ?></span>
-        </div>
-
-        <div class="large-12 column">&nbsp;</div>
-
-        <div class="large-12 column">
-            <span class="data-label">BMI:</span>&emsp;&nbsp;&nbsp;
-            <div id="bmi-container" class="data-value" style="display:inline-block;">
-                <?php
-                    if(ceil($element->weight) > 0 && ceil($element->height) > 0){
-                        echo $element->bmiCalculator( $element->weight, $element->height) ;
-                    } else {
-                        echo 'N/A';
-                    }
-                ?>
+            <div class="cols-4">
+                <table class="cols-full">
+                    <colgroup>
+                        <col class="cols-4">
+                    </colgroup>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <span class="data-label"><?= $element->getAttributeLabel('o2_sat') ?>:</span>
+                        </td>
+                        <td>
+                            <span class="data-value"><?php echo (!empty($element->o2_sat)) ? $element->o2_sat . ' %' : '' ?></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span class="data-label"><?= $element->getAttributeLabel('hba1c') ?>:</span>
+                        </td>
+                        <td>
+                            <span class="data-value"><?php echo (!empty($element->hba1c)) ? $element->hba1c . ' mmol/mol' : '' ?></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span class="data-label"><?= $element->getAttributeLabel('height') ?>:</span>
+                        </td>
+                        <td>
+                            <span class="data-value"><?php echo (!empty($element->height)) ? $element->height . ' cm' : '' ?></span>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
-        </div>
-
+            <div class="cols-4">
+                <table class="cols-full">
+                    <colgroup>
+                        <col class="cols-4">
+                    </colgroup>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <span class="data-label"><?= $element->getAttributeLabel('pulse') ?>:</span>
+                        </td>
+                        <td>
+                            <span class="data-value"><?php echo (!empty($element->pulse)) ? $element->pulse . ' BPM' : ''; ?></span>
+                        </td>
+                        <td>
+                            <span class="data-label">BMI:</span>&emsp;&nbsp;&nbsp;
+                        </td>
+                        <td>
+                            <div id="bmi-container" class="data-value" style="display:inline-block;">
+                                <?php
+                                if (ceil($element->weight) > 0 && ceil($element->height) > 0) {
+                                    echo $element->bmiCalculator($element->weight, $element->height);
+                                } else {
+                                    echo 'N/A';
+                                }
+                                ?>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
     </div>
 </div>
