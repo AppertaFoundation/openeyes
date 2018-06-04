fbcbcb6da3551b7228de3ab0861249040c147f3d
Tamas Vedelek <vetusko@gmail.com>
2018-06-04 11:49:17 +0000

    changed PDF viewer size in document module

--- protected/modules/OphCoDocument/views/default/view_object.php
+++ protected/modules/OphCoDocument/views/default/view_object.php
@@ -1,3 +1,3 @@
-<object width="100%" height="700px" data="/file/view/<?php echo $element->{$index}->id ?>/image<?php echo strrchr ($element->{$index}->name, '.') ?>" type="application/pdf">
+<object width="100%" height="1200px" data="/file/view/<?php echo $element->{$index}->id ?>/image<?php echo strrchr ($element->{$index}->name, '.') ?>" type="application/pdf">
     <embed src="/file/view/<?php echo $element->{$index}->id ?>/image<?php echo strrchr ($element->{$index}->name, '.') ?>" type="application/pdf" />
 </object>
