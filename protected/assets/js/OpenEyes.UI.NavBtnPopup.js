937a56fb41de952a5580fcf9b09eaa954d8ba691
Liam Marshall <marshl@live.com.au>
2018-06-08 10:47:52 +1000

    v3: Changed the buttons in the patient panel to be "latchable", so the user can mouse over them to show them temporarily, or click on them to show them permanently.

--- protected/assets/js/OpenEyes.UI.NavBtnPopup.js
+++ protected/assets/js/OpenEyes.UI.NavBtnPopup.js
@@ -1,123 +1,164 @@
 (function (exports, Util, EventEmitter) {
-    function NavBtnPopup(id, $btn, $content) {
+  function NavBtnPopup(id, $btn, $content) {
 
-        // private
-        var id = id,
-            eventObj = $btn,
-            useMouseEvents = false,
-            isGrouped = false, 		// e.g. patient popups
-            groupController = null,
-            isFixed = false,
-            css = {
-                active: 'active', 	// hover
-                open: 'open' 		// clicked
-            };
-        init(); // all are initiated but useWrapperEvents modifies the eventObj then re-initiates
+    var popup = this;
 
-        function init() {
-            // Events
-            eventObj.click(function (e) {
-                e.stopPropagation();
-                // use $btn class as boolean
-                changeContent($btn.hasClass(css.open));
-            }).mouseenter(function () {
-              $btn.addClass(css.active);
-              if (useMouseEvents){
-                  show(); }
-            })
-              .mouseleave(function () {
-                $btn.removeClass(css.active);
-                if (useMouseEvents) {
-                  hide();
-                }
-              });
+    // private
+    this.id = id;
+    this.eventObj = $btn;
+    this.useMouseEvents = false;
+    this.isGrouped = false; 		// e.g. patient popups
+    this.groupController = null;
+    this.isFixed = false;
+    this.latchable = false;
+    this.isLatched = false;
+    this.css = {
+      active: 'active', 	// hover
+      open: 'open' 		// clicked
+    };
+    init(); // all are initiated but useWrapperEvents modifies the eventObj then re-initiates
+
+    function init() {
+      // Events
+      popup.eventObj.click(function (e) {
+        e.stopPropagation();
+        // use $btn class as boolean
+        changeContent($btn.hasClass(popup.css.open));
+      }).mouseenter(function () {
+        if (popup.isLatched) return;
+        $btn.addClass(popup.css.active);
+        if (popup.useMouseEvents) {
+          show();
         }
-
-        /**
-         public methods
-         **/
-        this.hide = hide;
-        this.useWrapper = useWrapperEvents;
-        this.fixed = fixed;
-        this.inGroup = inGroup;
-
-        /**
-         provide a way for shortcuts to re-assign
-         the Events to the DOM wrapper
-         **/
-
-
-        function changeContent(isOpen) {
-            if (isFixed) return; // if popup is fixed
-
-            if (isOpen) {
-                hide();
-            } else {
-                if (isGrouped) groupController.closeAll();
-                show();
-            }
+      }).mouseleave(function () {
+        if (popup.isLatched) return;
+        $btn.removeClass(popup.css.active);
+        if (popup.useMouseEvents) {
+          hide();
         }
-
-        function show() {
-            $btn.addClass(css.open);
-            $content.show();
-            if (useMouseEvents && !isFixed) addContentEvents();
-        }
-
-        function hide() {
-            $btn.removeClass(css.open);
-            $content.hide();
-        }
-
-        /**
-         Enhance $content behaviour for non-touch users
-         Allow mouseLeave to close $content popup
-         **/
-        function addContentEvents() {
-            $content.mouseenter(function () {
-                $(this).off('mouseenter'); // clean up
-                $(this).mouseleave(function () {
-                    $(this).off('mouseleave'); // clean up
-                    hide();
-                });
-            });
-        }
-
-        /**
-         DOM structure for the Shortcuts dropdown list is different
-         Need to shift the events to the wrapper DOM rather than the $btn
-         **/
-        function useWrapperEvents(DOMwrapper) {
-            eventObj.off('click mouseenter mouseleave');
-            eventObj = DOMwrapper;
-            css.open = css.active; // wrap only has 1 class
-            useMouseEvents = true;
-            init(); // re initiate with new eventObj
-        }
-
-        /**
-         Activity Panel needs to be fixable when the browsers is wide enough
-         (but not in oescape mode)
-         **/
-        function fixed(b) {
-            isFixed = b;
-            if (b) {
-                $content.off('mouseenter mouseleave');
-                show();
-            } else {
-                hide();
-            }
-        }
-
-        /**
-         Group popups to stop overlapping
-         **/
-        function inGroup( controller ){
-            isGrouped = true;
-            groupController = controller;
-        }
+      });
     }
-    Util.inherits(EventEmitter, NavBtnPopup);
 
-    exports.NavBtnPopup = NavBtnPopup;
+    /**
+     public methods
+     **/
+    this.init = init;
+    this.hide = hide;
+    this.useWrapper = useWrapperEvents;
+    this.fixed = fixed;
+    this.inGroup = inGroup;
+    this.show = show;
+    this.hide = hide;
+    this.latch = latch;
+    this.unlatch = unlatch;
+
+    /**
+     provide a way for shortcuts to re-assign
+     the Events to the DOM wrapper
+     **/
+
+
+    function changeContent(isOpen) {
+      if (popup.isFixed) return; // if popup is fixed
+
+      if (popup.latchable) {
+        if (popup.isLatched) {
+          if (popup.isGrouped) {
+            popup.groupController.closeAll();
+          }
+          popup.unlatch();
+        } else {
+          if (popup.isGrouped) {
+            popup.groupController.closeAll();
+          }
+          popup.latch()
+        }
+      } else if (isOpen) {
+        popup.hide();
+      } else {
+        if (popup.isGrouped) {
+          popup.groupController.closeAll();
+        }
+        popup.show();
+      }
+    }
+
+    function show() {
+      $btn.addClass(popup.css.open);
+      $content.show();
+      if (popup.useMouseEvents && !popup.isFixed) addContentEvents();
+    }
+
+    function hide() {
+      $btn.removeClass(popup.css.open);
+      $content.hide();
+    }
+
+    /**
+     Enhance $content behaviour for non-touch users
+     Allow mouseLeave to close $content popup
+     **/
+    function addContentEvents() {
+      $content.mouseenter(function () {
+        $(this).off('mouseenter'); // clean up
+        $(this).mouseleave(function () {
+          $(this).off('mouseleave'); // clean up
+          popup.hide();
+        });
+      });
+    }
+
+    /**
+     DOM structure for the Shortcuts dropdown list is different
+     Need to shift the events to the wrapper DOM rather than the $btn
+     **/
+    function useWrapperEvents(DOMwrapper) {
+      popup.eventObj.off('click mouseenter mouseleave');
+      popup.eventObj = DOMwrapper;
+      popup.css.open = popup.css.active; // wrap only has 1 class
+      popup.useMouseEvents = true;
+      popup.init(); // re initiate with new eventObj
+    }
+
+    /**
+     Activity Panel needs to be fixable when the browsers is wide enough
+     (but not in oescape mode)
+     **/
+    function fixed(b) {
+      popup.isFixed = b;
+      if (b) {
+        $content.off('mouseenter mouseleave');
+        popup.show();
+      } else {
+        popup.hide();
+      }
+    }
+
+    function latch() {
+      popup.groupController.lockAll();
+      popup.isLatched = true;
+      popup.show();
+      $content.off('mouseenter mouseleave');
+    }
+
+    function unlatch() {
+      popup.groupController.unlockAll();
+      popup.isLatched = false;
+      popup.hide();
+      $content.on('mouseenter mouseleave');
+      $btn.removeClass(popup.css.active);
+    }
+
+    /**
+     Group popups to stop overlapping
+     **/
+    function inGroup(controller) {
+      popup.isGrouped = true;
+      popup.groupController = controller;
+    }
+  }
+
+  Util.inherits(EventEmitter, NavBtnPopup);
+
+  exports.NavBtnPopup = NavBtnPopup;
 }(OpenEyes.UI, OpenEyes.Util, OpenEyes.Util.EventEmitter));
--- protected/widgets/js/PatientPanelPopup.js
+++ protected/widgets/js/PatientPanelPopup.js
@@ -1,28 +1,48 @@
 var PatientPanel = PatientPanel || {};
 PatientPanel.patientPopups = {
-  init:function(){
-    if( $('#oe-patient-details').length == 0 ) return;
+  init: function () {
+    if ($('#oe-patient-details').length == 0) return;
 
     // patient popups
-    var quicklook 		= new OpenEyes.UI.NavBtnPopup( 'quicklook', $('#js-quicklook-btn'), $('#patient-summary-quicklook') );
-    var demographics 	= new OpenEyes.UI.NavBtnPopup( 'demographics', $('#js-demographics-btn'), $('#patient-popup-demographics') );
-    var demographics2 	= new OpenEyes.UI.NavBtnPopup( 'management', $('#js-management-btn'), $('#patient-popup-management') );
-    var risks 			= new OpenEyes.UI.NavBtnPopup( 'risks', $('#js-allergies-risks-btn'), $('#patient-popup-allergies-risks') );
+    var quicklook = new OpenEyes.UI.NavBtnPopup('quicklook', $('#js-quicklook-btn'), $('#patient-summary-quicklook'));
+    var demographics = new OpenEyes.UI.NavBtnPopup('demographics', $('#js-demographics-btn'), $('#patient-popup-demographics'));
+    var management = new OpenEyes.UI.NavBtnPopup('management', $('#js-management-btn'), $('#patient-popup-management'));
+    var risks = new OpenEyes.UI.NavBtnPopup('risks', $('#js-allergies-risks-btn'), $('#patient-popup-allergies-risks'));
 
+    var all = [quicklook, demographics, management, risks];
 
-    var all = [ quicklook, demographics, demographics2, risks ];
-
-    for( pBtns in all ) {
-      all[pBtns].inGroup( this ); // register group with PopupBtn
+    for (pBtns in all) {
+      var popup = all[pBtns];
+      popup.inGroup(this); // register group with PopupBtn
+      popup.latchable = true;
+      popup.useMouseEvents = true;
     }
 
     this.popupBtns = all;
 
   },
 
-  closeAll:function(){
-    for( pBtns in this.popupBtns ){
-      this.popupBtns[pBtns].hide();  // close all patient popups
+  closeAll: function () {
+    for (var i in this.popupBtns) {
+      var popup = this.popupBtns[i];
+      popup.hide();  // close all patient popups
+      if(popup.latchable) {
+        popup.unlatch();
+      }
+    }
+  },
+
+  lockAll: function() {
+    for (var i in this.popupBtns) {
+      var popup = this.popupBtns[i];
+      popup.isLatched = true;
+    }
+  },
+
+  unlockAll: function() {
+    for (i in this.popupBtns) {
+      var popup = this.popupBtns[i];
+      popup.isLatched = false;
     }
   }
 
