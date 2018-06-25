5d3aa402fe8c30364a929879f7048e9e4fc8eeea
Liam Marshall <marshl@live.com.au>
2018-06-25 10:27:40 +1000

    OE-7283: Fixed the event ID argument route of the CreateEventImageCommand

--- protected/commands/CreateEventImageCommand.php
+++ protected/commands/CreateEventImageCommand.php
@@ -15,60 +15,58 @@ class CreateEventImageCommand extends CConsoleCommand
             if ($event === null) {
                 throw new InvalidArgumentException('Could not find event with id: "' . $args[0] . '"');
             }
-
-            EventImageController::createImageForEvent($event);
-
         } else {
             $event = EventImage::model()->getNextEventToImage();
             if ($event === null) {
                 echo "No out of date events found\n";
+
                 return;
             }
-
-            echo 'Creating event image for event ' . $event->id . "\n";
-
-            $login_page = Yii::app()->params['docman_login_url'];
-            $username = Yii::app()->params['docman_user'];
-            $password = Yii::app()->params['docman_password'];
-            $image_url = 'http://localhost/eventImage/create/';
-
-            $ch = curl_init();
-
-            curl_setopt($ch, CURLOPT_URL, $login_page);
-            // disable SSL certificate check for locally issued certificates
-            if (Yii::app()->params['disable_ssl_certificate_check']) {
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            }
-            curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
-            curl_setopt($ch, CURLOPT_COOKIESESSION, true);
-            curl_setopt($ch, CURLOPT_COOKIEJAR, '/tmp/cookie.txt');
-            curl_setopt($ch, CURLOPT_COOKIEFILE, '/tmp/cookie.txt');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-            $response = curl_exec($ch);
-            if (curl_errno($ch)) {
-                die(curl_error($ch));
-            }
-
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
-            curl_setopt($ch, CURLOPT_POST, true);
-
-            $params = array(
-                'LoginForm[username]' => $username,
-                'LoginForm[password]' => $password,
-            );
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-
-            curl_exec($ch);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POST, false);
-            echo 'Curling URL "' . $image_url . $event->id . "\"\n";
-            curl_setopt($ch, CURLOPT_URL, $image_url . $event->id);
-            $content = curl_exec($ch);
-
-            $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            curl_close($ch);
-            echo 'Result: ' . $http_code . "\n";
         }
+
+        echo 'Creating event image for event ' . $event->id . "\n";
+
+        $login_page = Yii::app()->params['docman_login_url'];
+        $username = Yii::app()->params['docman_user'];
+        $password = Yii::app()->params['docman_password'];
+        $image_url = 'http://localhost/eventImage/create/';
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $login_page);
+        // disable SSL certificate check for locally issued certificates
+        if (Yii::app()->params['disable_ssl_certificate_check']) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        }
+        curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+        curl_setopt($ch, CURLOPT_COOKIESESSION, true);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, '/tmp/cookie.txt');
+        curl_setopt($ch, CURLOPT_COOKIEFILE, '/tmp/cookie.txt');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        if (curl_errno($ch)) {
+            die(curl_error($ch));
+        }
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+
+        $params = array(
+            'LoginForm[username]' => $username,
+            'LoginForm[password]' => $password,
+        );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+
+        curl_exec($ch);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, false);
+        echo 'Curling URL "' . $image_url . $event->id . "\"\n";
+        curl_setopt($ch, CURLOPT_URL, $image_url . $event->id);
+        $content = curl_exec($ch);
+
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        echo 'Result: ' . $http_code . "\n";
     }
 }
