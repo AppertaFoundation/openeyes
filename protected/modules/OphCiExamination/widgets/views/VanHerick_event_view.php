ac297fab01226c9789f949bc839afb51128de4c0
biskyt <github@gettheitguy.com>
2018-07-09 11:02:45 +0100

    OE-7212 - Removed uneccessary label from view

--- protected/modules/OphCiExamination/widgets/views/VanHerick_event_view.php
+++ protected/modules/OphCiExamination/widgets/views/VanHerick_event_view.php
@@ -19,7 +19,7 @@
 <div class="element-data element-eyes row">
     <div class="element-eye right-eye column">
         <?php if ($element->hasRight()) {
-            echo "Grade: {$element->right_van_herick->name}";
+            echo "{$element->right_van_herick->name}";
         } else {
             ?>
             <div class="data-row">
@@ -30,7 +30,7 @@
     </div>
     <div class="element-eye left-eye column">
         <?php if ($element->hasLeft()) {
-            echo "Grade: {$element->left_van_herick->name}";
+            echo "{$element->left_van_herick->name}";
         } else {
             ?>
             <div class="data-row">
