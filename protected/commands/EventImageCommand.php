bb51fdeaefeebf690d333376699f2a864220cfe7
Liam Marshall <marshl@live.com.au>
2018-07-11 08:42:33 +1000

    OE-7283: Renamed CreateEventImageCommand to EventImageCommand and changed it to have an argument of a number of events to image

--- protected/commands/EventImageCommand.php
+++ protected/commands/EventImageCommand.php
@@ -1,35 +1,24 @@
 <?php
 
 
-class CreateEventImageCommand extends CConsoleCommand
+class EventImageCommand extends CConsoleCommand
 {
     public function getHelp()
     {
         return 'Creates the preview image for the given event ID, or creates the preview image for the latest event without an image if no event ID is specified';
     }
 
-    public function run($args)
+    public function actionCreate($args)
     {
-        if (isset($args[0])) {
-            $event = Event::model()->findByPk($args[0]);
-            if ($event === null) {
-                throw new InvalidArgumentException('Could not find event with id: "' . $args[0] . '"');
-            }
-        } else {
-            $event = EventImage::model()->getNextEventToImage();
-            if ($event === null) {
-                echo "No out of date events found\n";
+        $count = isset($args[0]) ? (int)$args[0] : 0;
+        return $this->createEventImages($count);
+    }
 
-                return;
-            }
-        }
-
-        echo 'Creating event image for event ' . $event->id . "\n";
-
+    public function openCurlConnection()
+    {
         $login_page = Yii::app()->params['docman_login_url'];
         $username = Yii::app()->params['docman_user'];
         $password = Yii::app()->params['docman_password'];
-        $image_url = 'http://localhost/eventImage/create/';
 
         $ch = curl_init();
 
@@ -61,12 +50,30 @@ class CreateEventImageCommand extends CConsoleCommand
         curl_exec($ch);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, false);
-        echo 'Curling URL "' . $image_url . $event->id . "\"\n";
-        curl_setopt($ch, CURLOPT_URL, $image_url . $event->id);
-        $content = curl_exec($ch);
 
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        return $ch;
+    }
+
+    public function createEventImages($imageCount)
+    {
+        $image_url = 'http://localhost/eventImage/create/';
+
+        $ch = $this->openCurlConnection();
+        for ($i = 0; $i < $imageCount; ++$i) {
+            $event = EventImage::model()->getNextEventToImage();
+            if ($event === null) {
+                echo "No out of date events found\n";
+
+                return;
+            }
+
+            echo 'Curling URL "' . $image_url . $event->id . "\"\n";
+            curl_setopt($ch, CURLOPT_URL, $image_url . $event->id);
+            $content = curl_exec($ch);
+            $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            echo 'Result: ' . $http_code . "\n";
+        }
+
         curl_close($ch);
-        echo 'Result: ' . $http_code . "\n";
     }
 }
