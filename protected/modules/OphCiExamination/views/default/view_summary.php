9740a445532e92fc10d0e02f8a51ff921aa50699
Liam Marshall <marshl@live.com.au>
2018-07-12 09:19:26 +1000

    v3: Tidied up the examination summary view file

--- protected/modules/OphCiExamination/views/default/view_summary.php
+++ protected/modules/OphCiExamination/views/default/view_summary.php
@@ -5,28 +5,25 @@ use OEModule\OphCiExamination\models;
 /**
  * @var \OEModule\OphCiExamination\controllers\DefaultController $this
  * @var string $action
- * @var $form
- * @var $data
+ * @var BaseEventTypeCActiveForm $form
+ * @var array $data
  */
 
-// Find the elements for each tile, or create dummy elements so they will still render, but without any data
+// The history element won't be displayed if it doesn't exist
 $historyElement = $this->event->getElementByClass(models\Element_OphCiExamination_History::class);
+
+// Find the elements for each tile, or create dummy elements so they will still render, but without any data
 $pastOphthalmicSurgeryElement = $this->event->getElementByClass(models\PastSurgery::class) ?: new models\PastSurgery();
 $pastSystemicSurgeryElement = $this->event->getElementByClass(models\PastSurgery::class) ?: new models\PastSurgery();
 $systemicDiagnosesElement = $this->event->getElementByClass(models\SystemicDiagnoses::class) ?: new models\SystemicDiagnoses();
 $diagnosesElement = $this->event->getElementByClass(models\Element_OphCiExamination_Diagnoses::class) ?: new models\Element_OphCiExamination_Diagnoses();
-$familyHistoryElement = $this->event->getElementByClass(models\FamilyHistory::class) ?: new models\FamilyHistory();
-$socialHistoryElement = $this->event->getElementByClass(models\SocialHistory::class) ?: new models\SocialHistory();
 $medicationsElement = $this->event->getElementByClass(models\HistoryMedications::class) ?: new models\HistoryMedications();
 ?>
-
-<!--<?php $assetPath = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('application.modules.' . $this->getModule()->name . '.assets'));
-Yii::app()->clientScript->registerScriptFile($assetPath . '/js/HistoryMedications.js'); ?>
--->
 
 <?php if ($historyElement): ?>
     <?php $this->renderElement($historyElement, $action, $form, $data) ?>
 <?php endif; ?>
+
 <div class="element-tile-group" id="tile-group-exam-eyes" data-collapse="expanded">
     <?php $this->renderElement($diagnosesElement, $action, $form, $data) ?>
 
@@ -162,7 +159,6 @@ Yii::app()->clientScript->registerScriptFile($assetPath . '/js/HistoryMedication
       </div>
     </div>
   </section>
-
 
   <div class="collapse-tile-group">
     <i class="oe-i small collapse js-tiles-collapse-btn" data-group="tile-group-exam-eyes"></i>
