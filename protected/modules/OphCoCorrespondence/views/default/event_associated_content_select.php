d9e3d79ef30fb6c21b87a99f48a2bf0c2bfa3b36
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2018-07-12 12:58:21 +0100

    OE-7252 Show subtypes when macro selected instead of document

--- protected/modules/OphCoCorrespondence/views/default/event_associated_content_select.php
+++ protected/modules/OphCoCorrespondence/views/default/event_associated_content_select.php
@@ -1,39 +1,43 @@
 <?php
-if(empty($patient)){
+if (empty($patient)) {
     $patient = $this->patient;
 }
 ?>
 <div class="row field-row">
-    <div  class="large-2 column">
+    <div class="large-2 column">
         <label>Attachments:</label>
     </div>
     <div class="large-10 column end">
         <table id="correspondence_attachments_table">
             <thead>
-                <tr>
-                    <th>Attachment type</th>
-                    <th>Title</th>
-                    <th>Event Date</th>
-                    <th>Action</th>
-                </tr>
+            <tr>
+                <th>Attachment type</th>
+                <th>Title</th>
+                <th>Event Date</th>
+                <th>Action</th>
+            </tr>
             </thead>
             <tbody>
-                <tr id="correspondence_attachments_table_last_row" data-id="1">
-                    <td colspan="2"><td>
-                    <td>
-                        <?php
+            <tr id="correspondence_attachments_table_last_row" data-id="1">
+                <td colspan="2">
+                <td>
+                <td>
+                    <?php
 
-                        $events = $this->getAttachableEvents($patient);
-                        ?>
+                    $events = $this->getAttachableEvents($patient);
+                    ?>
                     <?= CHtml::dropDownList(
-                            'attachment_events',
-                            ' ',
-                            CHtml::listData($events,'id',function($events) {
-                                return CHtml::encode($events->eventType->name. ' - '.Helper::convertDate2NHS($events->event_date));
-                            }), array('empty' => '- Select -')) ;
-                            ?>
-                    </td>
-                </tr>
+                        'attachment_events',
+                        ' ',
+                        CHtml::listData($events, 'id', function ($events) {
+                            $event_type_name = $events->eventType->name;
+                            $document_model = Element_OphCoDocument_Document::model()->findByAttributes(["event_id" => $events->id]);
+                            $event_type_name = isset($document_model->sub_type) ? $document_model->sub_type->name : '';
+                            return CHtml::encode($event_type_name . ' - ' . Helper::convertDate2NHS($events->event_date));
+                        }), array('empty' => '- Select -'));
+                    ?>
+                </td>
+            </tr>
             </tbody>
         </table>
     </div>
