933893f540a5eaf57b6245a308f5512b4a036acc
Toby Bisco <biskyt@users.noreply.github.com>
2018-07-12 14:34:47 +0100

    fix case in model name

--- protected/modules/OphCoCorrespondence/views/default/init_method_row.php
+++ protected/modules/OphCoCorrespondence/views/default/init_method_row.php
@@ -1,6 +1,6 @@
 <tr data-id="">
     <input class="attachments_event_id" value="<?= $event->id ?>" type="hidden">
-    <td><?php $document_model = Element_ophcodocument_document::model()->findByAttributes(["event_id" => $event->id]);
+    <td><?php $document_model = Element_OphCoDocument_document::model()->findByAttributes(["event_id" => $event->id]);
             echo isset($document_model->sub_type) ? $document_model->sub_type->name : ''; ?></td>
     <td><input type="text" class="attachments_display_title" name="attachments_display_title[]"   value="<?= $event->eventType->name ?>" /></td>
     <td><?= Helper::convertDate2NHS($event->event_date) ?></td>
