f7467b4fcabfcd468ec62c93304cd160e7c99461
Liam Marshall <marshl@live.com.au>
2018-07-17 09:46:06 +1000

    OE-7283: Added "eye_id" to the event image table to facilitate sided document events

--- protected/migrations/m180619_032208_create_event_image_table.php
+++ protected/migrations/m180619_032208_create_event_image_table.php
@@ -9,13 +9,19 @@ class m180619_032208_create_event_image_table extends OEMigration
             array(
                 'id' => 'pk',
                 'event_id' => 'int(10) unsigned NOT NULL',
+                'eye_id' => 'int(10) unsigned',
                 'image_data' => 'mediumblob',
                 'status_id' => 'int(11)  NOT NULL',
             ));
 
-        $this->createTable('event_image_status', array('id' => 'pk', 'name' => 'varchar(50)'));
+        $this->createOETable('event_image_status',
+            array(
+                'id' => 'pk',
+                'name' => 'varchar(50)',
+            ));
 
         $this->addForeignKey('event_image_event_id_fk', 'event_image', 'event_id', 'event', 'id');
+        $this->addForeignKey('event_image_eye_id_fk', 'event_image', 'eye_id', 'eye', 'id');
         $this->addForeignKey('event_image_status_id_fk', 'event_image', 'status_id', 'event_image_status', 'id');
 
         $this->insert('event_image_status', array('name' => 'NOT_CREATED'));
