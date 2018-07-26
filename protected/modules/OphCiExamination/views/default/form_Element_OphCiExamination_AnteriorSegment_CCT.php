5ceb14c61defd8194752817d3a953fd2b16791a8
Zhaolian <zhaolian.zhou@toukanlabs.com>
2018-07-26 15:42:42 +1000

    v3-ui, examination, CCT css fix

--- protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_AnteriorSegment_CCT.php
+++ protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_AnteriorSegment_CCT.php
@@ -20,14 +20,24 @@
 <div class="element-eyes sub-element-fields">
 	<?php echo $form->hiddenInput($element, 'eye_id', false, array('class' => 'sideField'))?>
   <?php foreach(['left' => 'right', 'right' => 'left'] as $side => $eye):
-      $hasEyeFunc = 'has'.ucfirst($eye);
-      ?>
-    <div class="element-eye <?=$eye?>-eye column <?=$side?> side" data-side="<?=$eye?>">
+      $hasEyeFunc = 'has'.ucfirst($eye); ?>
+      <div class="element-eye <?=$eye?>-eye column <?=$side?> side" data-side="<?=$eye?>">
       <div class="active-form" style="<?= !$element->$hasEyeFunc() ? "display: none;" : ""?>">
-        <a class="remove-side"><i class="oe-i remove-circle small"></i></a>
-        <div class="cols-full">
-            <?php echo $form->textField($element, $eye.'_value', array('autocomplete' => Yii::app()->params['html_autocomplete'], 'nowrapper' => true, 'class' => 'cct_value')) ?>
-          &micro;m, using <?php echo $form->dropDownList($element, $eye.'_method_id', $methods, array('nowrapper' => true, 'class' => 'inline')) ?>
+      <a class="remove-side"><i class="oe-i remove-circle small"></i></a>
+      <div class="cols-full flex-layout">
+        <?php echo $form->textField(
+            $element,
+            $eye.'_value',
+            array('autocomplete' =>
+            Yii::app()->params['html_autocomplete'],
+            'nowrapper' => true,
+            'append-text'=>'&micro;m, using',
+            'class' => 'cct_value')) ?>
+        <?php echo $form->dropDownList(
+            $element,
+            $eye.'_method_id', 
+            $methods,
+            array('nowrapper' => true, 'class' => 'inline')) ?>
         </div>
       </div>
       <div class="inactive-form side" style="<?= $element->$hasEyeFunc() ? "display: none;" : ""?>">
