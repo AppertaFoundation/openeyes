5d8e2b40d5565b5c155117bd8f11cecde1682284
Liam Marshall <marshl@live.com.au>
2018-08-09 16:36:49 +1000

    OE-7339: Fixed some code format issues in the va_alert view

--- protected/modules/OphCoCvi/views/patient/_va_alert.php
+++ protected/modules/OphCoCvi/views/patient/_va_alert.php
@@ -15,8 +15,10 @@
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 ?>
-<div class="align-right cvi-alert alert-box issue" data-alert="CVI may be applicable"
-     data-threshold="<?= $threshold ?>" <?php if (!$visible): ?>style="display: none;"<?php endif; ?>>
+<div class="align-right cvi-alert alert-box issue"
+     data-alert="CVI may be applicable"
+     data-threshold="<?= $threshold ?>"
+     style="<?php if (!$visible): ?>display: none;<?php endif; ?>">
   This patient may be eligible for a CVI
   <i class="oe-i info pad-left small js-has-tooltip"
      data-tooltip-content="You'll be able to create a CVI after saving this Examination"></i>
@@ -28,6 +30,5 @@
       </a>
     <?php endif; ?>
   <i class="oe-i remove-circle small dismiss_cva_alert dismiss right"></i>
-  </span>
 </div>
 
