0fcafa50e610b42dbbf268db6253b325768d388c
Arpit Arora <arpit.arora89@live.com>
2018-08-10 13:35:09 +1000

    OE-7499 removing redundant terms from alerts and allergies popup

--- protected/modules/OphCiExamination/widgets/views/HistoryRisksEntry_patient_mode.php
+++ protected/modules/OphCiExamination/widgets/views/HistoryRisksEntry_patient_mode.php
@@ -19,9 +19,9 @@
  */
 ?>
 
-<tr  style="background-color: <?= $entry->getDisplayHasRisk()=='Not present' ? '#101925': '' ?>">
-  <td><?= $entry->getDisplayRisk() ?></td>
-  <td><?= $entry->getDisplayHasRisk() ?></td>
-  <td><?= $entry->comments ?></td>
-  <td></td>
+<tr style="background-color: <?= $entry->getDisplayHasRisk() == 'Not present' ? '#101925' : '' ?>">
+    <td><?= $entry->getDisplayRisk() ?></td>
+    <td></td>
+    <td><?= $entry->comments ?></td>
+    <td></td>
 </tr>
