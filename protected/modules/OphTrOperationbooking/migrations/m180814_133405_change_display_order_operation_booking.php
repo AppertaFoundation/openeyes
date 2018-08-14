7f05810f3528edd32ee71ed0ff1de7aadc8e1d9a
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2018-08-14 14:35:26 +0100

    OE-7326-develop Change the display order of operation booking

---
+++ protected/modules/OphTrOperationbooking/migrations/m180814_133405_change_display_order_operation_booking.php
@@ -0,0 +1,12 @@
+<?php
+class m180706_105034_change_display_order_operation_booking extends CDbMigration
+{
+    public function up()
+    {
+        $this->update('element_type' , array('display_order' => 30) , 'name="Schedule operation"');
+    }
+    public function down()
+    {
+        $this->update('element_type' , array('display_order' => 1) , 'name="Schedule operation"');
+    }
+}
