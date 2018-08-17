d8d31cdae0f82b59d7fdf97662fa4254c5088edb
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2018-08-17 16:46:01 +0100

    OE-7547 Fix the migration and add refraction icons

--- protected/modules/OphCoDocument/migrations/m180816_151734_add_refraction_to_document_sub_types.php
+++ protected/modules/OphCoDocument/migrations/m180816_151734_add_refraction_to_document_sub_types.php
@@ -9,8 +9,6 @@ class m180816_151734_add_refraction_to_document_sub_types extends CDbMigration
 
 	public function down()
 	{
-		$this->delete('ophcodocument_sub_types', array('display_order'=>12, 'name'=>'Refraction'));
+		$this->delete('ophcodocument_sub_types','name = ?' , array('Refraction'));
 	}
-
-
 }
