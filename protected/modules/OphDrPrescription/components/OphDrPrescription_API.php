bb9c5b731f02fd5a4fe867311597648fb37c0b49
neilsf <feketecsaba@gmail.com>
2018-08-21 08:12:35 +0200

    OE-7023 Fixed \OphDrPrescription_API::getPrescriptionItemsForPatient

--- protected/modules/OphDrPrescription/components/OphDrPrescription_API.php
+++ protected/modules/OphDrPrescription/components/OphDrPrescription_API.php
@@ -89,7 +89,7 @@ class OphDrPrescription_API extends BaseAPI
         $prescriptionCriteria->addCondition('prescription.draft = 0');
         $prescriptionCriteria->addNotInCondition('t.id', $exclude);
         $prescriptionCriteria->params = array_merge($prescriptionCriteria->params, array(':id' => $patient->id));
-        $prescriptionItems = OphDrPrescription_Item::model()->with('prescription', 'drug', 'duration', 'prescription.event', 'prescription.event.episode')->findAll($prescriptionCriteria);
+        $prescriptionItems = OphDrPrescription_Item::model()->with('prescription', 'refMedication', 'drugDuration', 'prescription.event', 'prescription.event.episode')->findAll($prescriptionCriteria);
 
         return $prescriptionItems;
     }
