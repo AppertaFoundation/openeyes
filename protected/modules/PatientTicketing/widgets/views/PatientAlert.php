3a4e32c2cd17f00f64700bf0af38af1d48c5f96c
Liam Marshall <marshl@live.com.au>
2018-08-31 12:18:26 +1000

    OE-7308: Fixed an issue where the patient ticket box would appear if there are tickets for the patient, but none of them were being reviewed (causing only the box to appear, but not any of the tickets)

--- protected/modules/PatientTicketing/widgets/views/PatientAlert.php
+++ protected/modules/PatientTicketing/widgets/views/PatientAlert.php
@@ -29,11 +29,26 @@ if ($closing_flsh = Yii::app()->user->getFlash('patient-ticketing-closing')) {
   <div class="alert-box with-icon success">
       <?= $closing_flsh; ?>
   </div>
-<?php }
-if (count($tickets) && Yii::app()->user->checkAccess('OprnViewClinical')) {
-    $qs_svc = Yii::app()->service->getService('PatientTicketing_QueueSet');
-    if ($this->assetFolder) { ?>
-      <script type="text/javascript" src="<?php echo $this->assetFolder ?>/<?php echo $this->shortName ?>.js"></script>
+<?php } ?>
+
+<?php
+$patient_ticket_in_review = null;
+
+if (isset(Yii::app()->session['patientticket_ticket_in_review'])) {
+    $patient_ticket_in_review = Yii::app()->session['patientticket_ticket_in_review'];
+    if ($patient_ticket_in_review['patient_id'] != $this->patient->id) {
+        unset(Yii::app()->session['patientticket_ticket_in_review']);
+    }
+}
+
+$tickets = array_filter($tickets, function ($ticket) use ($patient_ticket_in_review) {
+    return @$patient_ticket_in_review['ticket_id'] == $ticket->id;
+});
+
+if (count($tickets) && Yii::app()->user->checkAccess('OprnViewClinical')) { ?>
+
+    <?php if ($this->assetFolder) { ?>
+    <script type="text/javascript" src="<?php echo $this->assetFolder ?>/<?php echo $this->shortName ?>.js"></script>
     <?php } ?>
 
   <div id="oe-vc-scratchpad" class="oe-vc-mode vc-floating-input" draggable="true" style="display:none;">
@@ -72,83 +87,62 @@ if (count($tickets) && Yii::app()->user->checkAccess('OprnViewClinical')) {
        style="display: block;">
     <div class="oe-vc-mode">
       <div class="vc-content">
-          <?php
-          if (isset(Yii::app()->session['patientticket_ticket_in_review'])) {
-              $patient_ticket_in_review = Yii::app()->session['patientticket_ticket_in_review'];
-              if ($patient_ticket_in_review['patient_id'] != $this->patient->id) {
-                  unset(Yii::app()->session['patientticket_ticket_in_review']);
-              }
-          }
-
-          foreach ($tickets as $ticket) {
-              if (@$patient_ticket_in_review['ticket_id'] == $ticket->id) {
-                  $cat = $t_svc->getCategoryForTicket($ticket);
-                  ?>
-                <div data-ticket-id="<?= $ticket->id ?>">
-                    <?php if ($ticket->priority) { ?>
-                      <div class="priority">
-                        <i class="oe-i circle-<?= $ticket->priority->name ?> medium"></i>
-                      </div>
-                    <?php } ?>
-
-                  <div class="scratchpad">
-                    <button id="js-vc-scratchpad" class="blue hint">ScratchPad</button>
+          <?php foreach ($tickets as $ticket) { ?>
+            <div data-ticket-id="<?= $ticket->id ?>">
+                <?php if ($ticket->priority) { ?>
+                  <div class="priority">
+                    <i class="oe-i circle-<?= $ticket->priority->name ?> medium"></i>
                   </div>
-                  <script>
-                    $(document).on('click', '#js-vc-scratchpad', function () {
-                      $('#oe-vc-scratchpad').toggle();
+                <?php } ?>
 
-                      var txt = $('#oe-vc-scratchpad').is(':visible') ? 'Hide ScratchPad' : 'ScratchPad';
-                      $(this).text(txt);
-                    });
-                  </script>
+              <div class="scratchpad">
+                <button id="js-vc-scratchpad" class="blue hint">ScratchPad</button>
+              </div>
+              <script>
+                $(document).on('click', '#js-vc-scratchpad', function () {
+                  $('#oe-vc-scratchpad').toggle();
 
-                  <h3>VC: <b><?= strtoupper(trim($ticket->patient->last_name)) ?>,
-                          <?= $ticket->patient->first_name ?></b></h3>
+                  var txt = $('#oe-vc-scratchpad').is(':visible') ? 'Hide ScratchPad' : 'ScratchPad';
+                  $(this).text(txt);
+                });
+              </script>
 
+              <h3>VC: <b><?= strtoupper(trim($ticket->patient->last_name)) ?>,
+                      <?= $ticket->patient->first_name ?></b></h3>
+
+              <div class="row divider">
+                <ul class="vc-steps">
+                  <li class="selected">1. VC Step one</li>
+                  <li>2. VC Step two</li>
+                </ul>
+              </div>
+
+                <?php if ($ticket->event) { ?>
                   <div class="row divider">
-                    <ul class="vc-steps">
-                      <li class="selected">1. VC Step one</li>
-                      <li>2. VC Step two</li>
-                    </ul>
-                  </div>
-
-                    <?php if ($ticket->event) { ?>
-                      <div class="row divider">
-                        <div class="data-label">Date of <?= $ticket->event->eventType->name ?>
-                          : <?= Helper::convertDate2NHS($ticket->event->event_date) ?></div>
-                        <div class="data-value">
+                    <div class="data-label">Date of <?= $ticket->event->eventType->name ?>
+                      : <?= Helper::convertDate2NHS($ticket->event->event_date) ?></div>
+                    <div class="data-value">
                           <textarea readonly="" rows="1" class="cols-full"
                                     style="overflow: hidden; overflow-wrap: break-word; height: 25px;">No significant issues reported</textarea>
-                        </div>
-                      </div>
-                    <?php } ?>
-
-                  <div data-ticket-id="<?= $ticket->id ?>">
-
-                    <!--<header><strong class="box-title"><?= $cat->name ?>: Patient is
-                      in <?= $ticket->current_queue->queueset->name ?>, <?= $ticket->current_queue->name ?></strong>
-                  </header>-->
-
-                      <?php $this->widget($summary_widget, array('ticket' => $ticket)); ?>
-
-
-                      <?php
-                      $qs_r = $qs_svc->getQueueSetForTicket($ticket->id);
-                      if ($qs_svc->isQueueSetPermissionedForUser($qs_r, Yii::app()->user->id)) {
-                          $this->widget('OEModule\PatientTicketing\widgets\TicketMove', array(
-                                  'ticket' => $ticket,
-                              )
-                          );
-                      }
-                      ?>
-
-
+                    </div>
                   </div>
-                </div>
-              <?php }
-          }
-          ?>
+                <?php } ?>
+
+              <div data-ticket-id="<?= $ticket->id ?>">
+                  <?php $this->widget($summary_widget, array('ticket' => $ticket)); ?>
+                  <?php
+                  $qs_svc = Yii::app()->service->getService('PatientTicketing_QueueSet');
+                  $qs_r = $qs_svc->getQueueSetForTicket($ticket->id);
+                  if ($qs_svc->isQueueSetPermissionedForUser($qs_r, Yii::app()->user->id)) {
+                      $this->widget('OEModule\PatientTicketing\widgets\TicketMove', array(
+                              'ticket' => $ticket,
+                          )
+                      );
+                  }
+                  ?>
+              </div>
+            </div>
+          <?php } ?>
       </div>
     </div>
   </div>
