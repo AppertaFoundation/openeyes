81029ae7b5d680327d41e74fd5174c2f0a512ca4
Liam Marshall <marshl@live.com.au>
2018-09-03 14:35:51 +1000

    OE-7308: Fixed the Save Queue Set button not working in the Patientticketing admin screen

--- protected/modules/PatientTicketing/assets/js/admin.js
+++ protected/modules/PatientTicketing/assets/js/admin.js
@@ -137,9 +137,9 @@
         });
         formDialog.open();
         // suppress default ok behaviour
-        formDialog.content.off('click', '.js-ok');
+        formDialog.content.off('click', '.ok');
         // manage form submission and response
-        formDialog.content.on('click', '.js-ok', function () {
+        formDialog.content.on('click', '.ok', function () {
           this.submitQueueSetForm(formDialog, this.options.editQueueSetURI + '?id=' + queueSetId)
         }.bind(this));
       }.bind(this)
