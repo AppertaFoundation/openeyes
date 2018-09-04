13bd27f68b24bc28646fbc127b180d984d168089
Liam Marshall <marshl@live.com.au>
2018-09-04 16:42:56 +1000

    OE-7606: Fixed various places where Firm::contextLabel() was typed incorrectly

--- protected/modules/OphCoCorrespondence/views/admin/_macros.php
+++ protected/modules/OphCoCorrespondence/views/admin/_macros.php
@@ -26,7 +26,7 @@ if (!empty($macros)) {
                 } elseif ($macro->subspecialty) {
                     echo 'Subspecialty: '.$macro->subspecialty->name;
                 } else {
-                    echo Firm::ContextLabel() . ': '.$macro->firm->getNameAndSubspecialty();
+                    echo Firm::contextLabel() . ': '.$macro->firm->getNameAndSubspecialty();
                 }?>
 			</td>
 			<td><?php echo $macro->name?></td>
