904271bc87be8cc36637c79562c2f1f766d51612
jacksi <mrisaacjackson@gmail.com>
2018-09-05 15:22:53 +1000

    OE-7557 White space fixes

--- protected/modules/OphTrOperationbooking/assets/js/module.js
+++ protected/modules/OphTrOperationbooking/assets/js/module.js
@@ -15,152 +15,156 @@
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 
-(function() {
-	function updateRTTInfo(el) {
-		var show = false;
-		var cs, b;
+(function () {
+    function updateRTTInfo(el) {
+        var show = false;
+        var cs, b;
 
-		var selectedVal = $(el).val();
-		$(el).find('option').each(function() {
-			if ($(this).attr('value') == selectedVal) {
-				cs = $(this).data('clock-start');
-				b = $(this).data('breach');
-				return false;
-			}
-		});
+        var selectedVal = $(el).val();
+        $(el).find('option').each(function () {
+            if ($(this).attr('value') == selectedVal) {
+                cs = $(this).data('clock-start');
+                b = $(this).data('breach');
+                return false;
+            }
+        });
 
-		if (cs) {
-			$('#rtt-clock-start').html(cs);
-			show = true;
-		}
-		else {
-			$('#rtt-clock-start').html('');
-		}
-		if (b) {
-			$('#rtt-breach').html(b);
-			show = true;
-		}
-		else {
-			$('#rtt-breach').html('');
-		}
-		if (show) {
-			$('#rtt-info').show();
-		}
-		else {
-			$('#rtt-info').hide();
-		}
-	}
+        if (cs) {
+            $('#rtt-clock-start').html(cs);
+            show = true;
+        }
+        else {
+            $('#rtt-clock-start').html('');
+        }
+        if (b) {
+            $('#rtt-breach').html(b);
+            show = true;
+        }
+        else {
+            $('#rtt-breach').html('');
+        }
+        if (show) {
+            $('#rtt-info').show();
+        }
+        else {
+            $('#rtt-info').hide();
+        }
+    }
 
-	function ProcedureChecker(options) {
-		this.options = $.extend(true, {}, ProcedureChecker._defaultOptions, options);
-	}
+    function ProcedureChecker(options) {
+        this.options = $.extend(true, {}, ProcedureChecker._defaultOptions, options);
+    }
 
-	ProcedureChecker._defaultOptions = {
-		'procCheckURI': '/OphTrOperationbooking/default/verifyProcedures'
-	}
+    ProcedureChecker._defaultOptions = {
+        'procCheckURI': '/OphTrOperationbooking/default/verifyProcedures'
+    }
 
-	ProcedureChecker.prototype.verifySubmit = function(form) {
-		disableButtons();
-		$.ajax({
-			url: this.options.procCheckURI + '?patient_id=' + OE_patientId,
-			data: $(form).serialize(),
-			type: 'POST',
-			dataType: 'JSON',
-			success: function(response) {
-				if (response.previousProcedures) {
-					var dialog = new OpenEyes.UI.Dialog.Confirm({
-						content: response.message,
-						title: 'Duplicate procedure(s)'
-					});
-					dialog.on('ok', function() {
-						$(form).trigger('submit');
-					}.bind(this));
+    ProcedureChecker.prototype.verifySubmit = function (form) {
+        disableButtons();
+        $.ajax({
+            url: this.options.procCheckURI + '?patient_id=' + OE_patientId,
+            data: $(form).serialize(),
+            type: 'POST',
+            dataType: 'JSON',
+            success: function (response) {
+                if (response.previousProcedures) {
+                    var dialog = new OpenEyes.UI.Dialog.Confirm({
+                        content: response.message,
+                        title: 'Duplicate procedure(s)'
+                    });
+                    dialog.on('ok', function () {
+                        $(form).trigger('submit');
+                    }.bind(this));
 
-					dialog.on('cancel', function() {enableButtons(); });
-					dialog.open();
-				}
-				else {
-					$(form).trigger('submit');
-				}
-			}.bind(this),
-			error: function(jqXHR, status, error) {
-				var alert = new OpenEyes.UI.Dialog.Alert({
-					content: 'An unexpected error has occurred, cannot save at this time.'
-				});
-				alert.on('close', function() { enableButtons(); });
-				alert.open();
+                    dialog.on('cancel', function () {
+                        enableButtons();
+                    });
+                    dialog.open();
+                }
+                else {
+                    $(form).trigger('submit');
+                }
+            }.bind(this),
+            error: function (jqXHR, status, error) {
+                var alert = new OpenEyes.UI.Dialog.Alert({
+                    content: 'An unexpected error has occurred, cannot save at this time.'
+                });
+                alert.on('close', function () {
+                    enableButtons();
+                });
+                alert.open();
 
-			}.bind(this)
-		});
+            }.bind(this)
+        });
 
-	}
+    }
 
-	$(document).ready(function() {
-		updateRTTInfo($('#Element_OphTrOperationbooking_Operation_referral_id'));
-		$('#Element_OphTrOperationbooking_Operation_referral_id').on('change', function() {
-			updateRTTInfo($(this));
-		});
+    $(document).ready(function () {
+        updateRTTInfo($('#Element_OphTrOperationbooking_Operation_referral_id'));
+        $('#Element_OphTrOperationbooking_Operation_referral_id').on('change', function () {
+            updateRTTInfo($(this));
+        });
 
-		var procChecker = new ProcedureChecker();
+        var procChecker = new ProcedureChecker();
 
-		$(this).on('click', '#et_save, #et_save_and_schedule', function(e) {
-			if (OE_confirmProcedures) {
-				e.preventDefault();
-				procChecker.verifySubmit(e.target.form);
-			}
-		});
+        $(this).on('click', '#et_save, #et_save_and_schedule', function (e) {
+            if (OE_confirmProcedures) {
+                e.preventDefault();
+                procChecker.verifySubmit(e.target.form);
+            }
+        });
 
-		function setDependentField(field, dependent_field_selector) {
-			var input_selector = 'input[name=Element_OphTrOperationbooking_Operation\\[' + field + '\\]]';
+        function setDependentField(field, dependent_field_selector) {
+            var input_selector = 'input[name=Element_OphTrOperationbooking_Operation\\[' + field + '\\]]';
 
-			function showHide() {
-				var div = $(dependent_field_selector);
-				var option = $(input_selector + ':checked');
+            function showHide() {
+                var div = $(dependent_field_selector);
+                var option = $(input_selector + ':checked');
 
-				div[parseInt(option.val(), 10) ? 'show' : 'hide']();
-			}
+                div[parseInt(option.val(), 10) ? 'show' : 'hide']();
+            }
 
-			$(document).on('click', input_selector, showHide);
-			showHide();
-		}
+            $(document).on('click', input_selector, showHide);
+            showHide();
+        }
 
         setDependentField('stop_medication', '#tr_stop_medication_details');
-		setDependentField('special_equipment', '#div_Element_OphTrOperationbooking_Operation_special_equipment_details');
-		setDependentField('consultant_required', '#div_Element_OphTrOperationbooking_Operation_named_consultant_id');
+        setDependentField('special_equipment', '#div_Element_OphTrOperationbooking_Operation_special_equipment_details');
+        setDependentField('consultant_required', '#div_Element_OphTrOperationbooking_Operation_named_consultant_id');
 
-		$('.remove_organising_admission_user').live('click',function(e) {
-			e.preventDefault();
+        $('.remove_organising_admission_user').live('click', function (e) {
+            e.preventDefault();
 
-			$('#Element_OphTrOperationbooking_Operation_organising_admission_user_id').val('');
-			$(this).parent().html('None');
-		});
+            $('#Element_OphTrOperationbooking_Operation_organising_admission_user_id').val('');
+            $(this).parent().html('None');
+        });
 
-		$('.autosize').autosize();
+        $('.autosize').autosize();
 
-		$(this).on('click','#et_print_admission_form',function() {
-			printIFrameUrl(baseUrl + '/OphTrOperationbooking/default/admissionForm/' + OE_event_id);
-		});
+        $(this).on('click', '#et_print_admission_form', function () {
+            printIFrameUrl(baseUrl + '/OphTrOperationbooking/default/admissionForm/' + OE_event_id);
+        });
 
 
-		$(this).on('click','#et_print',function(e) {
-			printEvent(null);
-			e.preventDefault();
-		});
+        $(this).on('click', '#et_print', function (e) {
+            printEvent(null);
+            e.preventDefault();
+        });
 
-		$('input[name="Element_OphTrOperationbooking_Operation[priority_id]"]').click(function(e) {
-			var priority_id = $(this).val();
+        $('input[name="Element_OphTrOperationbooking_Operation[priority_id]"]').click(function (e) {
+            var priority_id = $(this).val();
 
-			if (!priority_canschedule[priority_id]) {
-				$('#et_save').show();
-				$('#et_save_and_schedule_later').hide();
-				$('#et_save_and_schedule').hide();
-			} else {
-				if ($('#et_save_and_schedule_later').length >0) {
-					$('#et_save').hide();
-					$('#et_save_and_schedule_later').show();
-					$('#et_save_and_schedule').show();
-				}
-			}
-		});
-	});
+            if (!priority_canschedule[priority_id]) {
+                $('#et_save').show();
+                $('#et_save_and_schedule_later').hide();
+                $('#et_save_and_schedule').hide();
+            } else {
+                if ($('#et_save_and_schedule_later').length > 0) {
+                    $('#et_save').hide();
+                    $('#et_save_and_schedule_later').show();
+                    $('#et_save_and_schedule').show();
+                }
+            }
+        });
+    });
 }());
