5269e19dd7e7ea4ecac33b046315643156029b8c
jacksi <mrisaacjackson@gmail.com>
2018-09-11 08:44:30 +1000

    OE-5715 fixed multiple new lines being squashed

--- protected/migrations/m180910_030837_letter_macros_to_html.php
+++ protected/migrations/m180910_030837_letter_macros_to_html.php
@@ -8,7 +8,7 @@ class m180910_030837_letter_macros_to_html extends OEMigration
                      ->select('*')->from('ophcocorrespondence_letter_macro')
                      ->queryAll() as $i => $macro
         ){
-            $altered_body = preg_replace("/[\n\r]+/", "<br/>", $macro['body']);
+            $altered_body = preg_replace("/\r?\n/", "<br/>", $macro['body']);
 
             $this->update('ophcocorrespondence_letter_macro',
                 array('body' => $altered_body),
