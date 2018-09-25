06a473a7885d6eea00ab8ce38c0db2e99c757703
Arpit Arora <arpit.arora89@live.com>
2018-09-25 10:47:50 +1000

    CERA-7: renaming the patch as per UK standards

--- protected/models/Address.php
+++ protected/models/Address.php
@@ -95,7 +95,7 @@ class Address extends BaseActiveRecordVersioned
             'address2' => 'Address2',
             'city' => 'City',
             'postcode' => 'Postcode',
-            'county' => Yii::app()->params['state_label'],
+            'county' => Yii::app()->params['county_label'],
             'country_id' => 'Country',
             'email' => 'Email',
             'address_type_id' => 'Address Type',
