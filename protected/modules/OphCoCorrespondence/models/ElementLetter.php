75bcd4bfac86c220b49a6d5f6e57d7ea5e623d56
Vikas <envikas@gmail.com>
2018-09-26 14:56:59 +1000

    CERA-5 Parameterized the name NHS in ElementLetter

--- protected/modules/OphCoCorrespondence/models/ElementLetter.php
+++ protected/modules/OphCoCorrespondence/models/ElementLetter.php
@@ -393,7 +393,7 @@ class ElementLetter extends BaseEventTypeElement
             }
         }
 
-        return $re.', DOB: '.$patient->NHSDate('dob').', Hosp No: '.$patient->hos_num.', NHS No: '.$patient->nhsnum;
+        return $re.', DOB: '.$patient->NHSDate('dob').', Hosp No: '.$patient->hos_num.', '. Yii::app()->params['nhs_num_label'] .' No: '.$patient->nhsnum;
     }
 
     /**
@@ -424,7 +424,7 @@ class ElementLetter extends BaseEventTypeElement
                 }
             }
 
-            $this->re .= ', DOB: '.$patient->NHSDate('dob').', Hosp No: '.$patient->hos_num.', NHS No: '.$patient->nhsnum;
+            $this->re .= ', DOB: '.$patient->NHSDate('dob').', Hosp No: '.$patient->hos_num.', '. Yii::app()->params['nhs_num_label'] .' No: '.$patient->nhsnum;
 
             $user = Yii::app()->session['user'];
             $firm = Firm::model()->with('serviceSubspecialtyAssignment')->findByPk(Yii::app()->session['selected_firm_id']);
