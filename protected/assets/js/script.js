e1d75c52445e7c9a9aef78b0656f644622ac179f
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2018-09-28 16:55:57 +0100

    OE-7664 Fix PR requests

--- protected/assets/js/script.js
+++ protected/assets/js/script.js
@@ -320,19 +320,20 @@ $(document).ready(function () {
   });
 
     (function elementSubgroup() {
-        if( $('.js-element-subgroup-viewstate-btn').length == 0 ) return;
+        let $viewstate_btns = $('.js-element-subgroup-viewstate-btn');
+        if( $viewstate_btns.length === 0 ) return;
 
-        $('.js-element-subgroup-viewstate-btn').each( function(){
+        $viewstate_btns.each( function(){
             new Viewstate( $(this) );
         });
 
         function Viewstate( $icon ){
-            var me = this;
-            var $content = $('#' + $icon.data('subgroup') );
+            let view_state = this;
+            let $content = $('#' + $icon.data('subgroup') );
 
             $icon.click( function( e ){
                 e.preventDefault();
-                me.changeState();
+                view_state.changeState();
             });
 
             this.changeState = function(){
