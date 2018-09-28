65aa1e8bfb3bbfc07f531efaf1539a91371c62c6
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2018-09-28 16:08:45 +0100

    OE-7664 Fix CVI Clinical Info Layout

--- protected/assets/js/script.js
+++ protected/assets/js/script.js
@@ -319,6 +319,31 @@ $(document).ready(function () {
 
   });
 
+    (function elementSubgroup() {
+        if( $('.js-element-subgroup-viewstate-btn').length == 0 ) return;
+
+        $('.js-element-subgroup-viewstate-btn').each( function(){
+            var subgroup = new Viewstate( $(this) );
+        });
+
+        function Viewstate( $icon ){
+            var me = this;
+            var $content = $('#' + $icon.data('subgroup') );
+
+            $icon.click( function( e ){
+                e.preventDefault();
+                me.changeState();
+            });
+
+            this.changeState = function(){
+                $content.toggle();
+                $icon.toggleClass('collapse expand');
+            }
+
+        }
+    })();
+
+
 });
 
 function changeState(wb,sp) {
