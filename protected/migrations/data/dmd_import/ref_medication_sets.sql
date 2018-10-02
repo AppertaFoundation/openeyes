ce2f068942c80c88e4ab794a79dc136aae5728d7
neilsf <feketecsaba@gmail.com>
2018-10-02 09:19:40 +0200

    OE-7023 Fixes for dm+d import (query optimization)

--- protected/migrations/data/dmd_import/ref_medication_sets.sql
+++ protected/migrations/data/dmd_import/ref_medication_sets.sql
@@ -9,21 +9,21 @@ SELECT
     WHEN 'VMP' THEN @vmp_id
     WHEN 'VTM' THEN @vtm_id
   END AS setId,
-  ref_medication_form.id AS formId,
+  rm.id AS formId,
   mr.id AS RouteId
 INTO OUTFILE '/tmp/ref_medication_set.csv'
 FROM openeyes.ref_medication rm
 
-  LEFT JOIN drugs2.f_amp_amps amp2 ON amp2.apid = ref_medication.amp_code
+  LEFT JOIN drugs2.f_amp_amps amp2 ON amp2.apid = rm.amp_code
   LEFT JOIN drugs2.f_vmp_drug_form dft ON dft.vpid = amp2.vpid
   LEFT JOIN drugs2.f_lookup_form fhit ON fhit.cd = dft.formcd
-  LEFT JOIN ref_medication_form mf ON mf.term = fhit.desc AND mf.source_type = 'DM+D'
+  LEFT JOIN openeyes.ref_medication_form mf ON mf.term = fhit.desc AND mf.source_type = 'DM+D'
 
   LEFT JOIN drugs2.f_vmp_drug_route drt ON drt.vpid = amp2.vpid
   LEFT JOIN drugs2.f_lookup_route lr ON lr.cd = drt.routecd
-  LEFT JOIN openeyes.ref_medication_route mr ON mr.term = lr.desc AND mr.source_type = 'DM+D'
+  LEFT JOIN openeyes.ref_medication_route mr ON mr.term COLLATE utf8_general_ci = lr.desc AND mr.source_type = 'DM+D'
 
 WHERE
   rm.source_type='DM+D'
   AND
-  rm.source_subtype != 'VTM';
+  rm.source_subtype != 'VTM'
