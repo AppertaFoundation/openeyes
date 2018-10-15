935045bf27a4b8bb013917724d4dd222f0b10316
Ervin Kovacs <kovacs.ervin87@gmail.com>
2018-10-15 17:58:34 +0200

    Examination print layout fixes

--- protected/modules/OphCiExamination/views/default/print_Element_OphCiExamination_Diagnoses.php
+++ protected/modules/OphCiExamination/views/default/print_Element_OphCiExamination_Diagnoses.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * OpenEyes.
  *
@@ -20,73 +21,74 @@ use OEModule\OphCiExamination\components\ExaminationHelper;
 <?php
 list($right_principals, $left_principals) = ExaminationHelper::getOtherPrincipalDiagnoses($this->episode);
 ?>
-<div class="element-data element-eyes">
-	<div class="js-element-eye right-eye column">
-		<?php if ($principal = OEModule\OphCiExamination\models\OphCiExamination_Diagnosis::model()->find('element_diagnoses_id=? and principal=1 and eye_id in (2,3)', array($element->id))) {
-    ?>
-			<div class="data-group">
-				<div class="data-value">
-					<strong>
-              <?php echo $principal->getDisplayDate()?>
-              <?php echo $principal->eye->adjective?>
-              <?php echo $principal->disorder->term?>
-					</strong>
-				</div>
-			</div>
-		<?php
-        } foreach($right_principals as $disorder) { ?>
+<div class="element-data element-eyes flex-layout">
+    <div class="js-element-eye cols-6 sright-eye">
+        <?php if ($principal = OEModule\OphCiExamination\models\OphCiExamination_Diagnosis::model()->find('element_diagnoses_id=? and principal=1 and eye_id in (2,3)', array($element->id))) {
+            ?>
             <div class="data-group">
                 <div class="data-value">
-                    <?= ($disorder[0]->term) ?><sup>*</sup></span>
+                    <strong>
+                        <?php echo $principal->getDisplayDate() ?>
+                        <?php echo $principal->eye->adjective ?>
+    <?php echo $principal->disorder->term ?>
+                    </strong>
+                </div>
+            </div>
+            <?php } foreach ($right_principals as $disorder) {
+            ?>
+            <div class="data-group">
+                <div class="data-value">
+    <?= ($disorder[0]->term) ?><sup>*</sup></span>
                 </div>
             </div>
             <?php
         }
-		foreach (\OEModule\OphCiExamination\models\OphCiExamination_Diagnosis::model()->findAll('element_diagnoses_id=? and principal=0 and eye_id in (2,3)', array($element->id)) as $diagnosis) {
-    ?>
-			<div class="data-group">
-				<div class="data-value">
-            <?php echo $diagnosis->getDisplayDate()?>
-            <?php echo $diagnosis->eye->adjective?>
-            <?php echo $diagnosis->disorder->term?>
-				</div>
-			</div>
-		<?php
-}?>
-	</div>
-	<div class="js-element-eye left-eye column">
-		<?php if ($principal = \OEModule\OphCiExamination\models\OphCiExamination_Diagnosis::model()->find('element_diagnoses_id=? and principal=1 and eye_id in (1,3)', array($element->id))) {
-    ?>
-			<div class="data-group">
-				<div class="data-value">
-					<strong>
-              <?php echo $principal->getDisplayDate()?>
-              <?php echo $principal->eye->adjective?>
-              <?php echo $principal->disorder->term?>
-					</strong>
-				</div>
-			</div>
-            <?php
-        }
-        foreach($left_principals as $disorder) { ?>
+        foreach (\OEModule\OphCiExamination\models\OphCiExamination_Diagnosis::model()->findAll('element_diagnoses_id=? and principal=0 and eye_id in (2,3)', array($element->id)) as $diagnosis) {
+            ?>
             <div class="data-group">
                 <div class="data-value">
-                    <?= ($disorder[0]->term) ?><sup>*</sup></span>
+                    <?php echo $diagnosis->getDisplayDate() ?>
+                    <?php echo $diagnosis->eye->adjective ?>
+    <?php echo $diagnosis->disorder->term ?>
+                </div>
+            </div>
+            <?php }
+        ?>
+    </div>
+    <div class="js-element-eye cols-6 left-eye">
+        <?php if ($principal = \OEModule\OphCiExamination\models\OphCiExamination_Diagnosis::model()->find('element_diagnoses_id=? and principal=1 and eye_id in (1,3)', array($element->id))) {
+            ?>
+            <div class="data-group">
+                <div class="data-value">
+                    <strong>
+                        <?php echo $principal->getDisplayDate() ?>
+                        <?php echo $principal->eye->adjective ?>
+    <?php echo $principal->disorder->term ?>
+                    </strong>
+                </div>
+            </div>
+            <?php
+        }
+        foreach ($left_principals as $disorder) {
+            ?>
+            <div class="data-group">
+                <div class="data-value">
+            <?= ($disorder[0]->term) ?><sup>*</sup></span>
                 </div>
             </div>
             <?php
         } foreach (\OEModule\OphCiExamination\models\OphCiExamination_Diagnosis::model()->findAll('element_diagnoses_id=? and principal=0 and eye_id in (1,3)', array($element->id)) as $diagnosis) {
-    ?>
-			<div class="data-group">
-				<div class="data-value">
-                    <?php echo $diagnosis->getDisplayDate()?>
-					<?php echo $diagnosis->eye->adjective?>
-					<?php echo $diagnosis->disorder->term?>
-				</div>
-			</div>
-		<?php 
-}?>
-	</div>
+            ?>
+            <div class="data-group">
+                <div class="data-value">
+                    <?php echo $diagnosis->getDisplayDate() ?>
+    <?php echo $diagnosis->eye->adjective ?>
+            <?php echo $diagnosis->disorder->term ?>
+                </div>
+            </div>
+    <?php }
+?>
+    </div>
 </div>
 <?php if (count($left_principals) || count($right_principals)) { ?>
     <p><small><sup>*</sup>This diagnosis is included because it is the principle diagnosis from another ophthalmic subspecialty episode</small></p>
