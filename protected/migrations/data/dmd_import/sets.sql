444c7d54b9e1b3c667b100a7e109378a7833d6af
neilsf <feketecsaba@gmail.com>
2018-10-16 07:44:21 +0200

    OE-7023 Add Formulary rules to DM+D sets

--- protected/migrations/data/dmd_import/sets.sql
+++ protected/migrations/data/dmd_import/sets.sql
@@ -1,4 +1,6 @@
 /*------------------------------------------ SET --------------------------------------------------------*/
+DELETE FROM openeyes.ref_set WHERE `name` IN ('DM+D AMP', 'DM+D VMP', 'DM+D VTM');
+
 INSERT INTO openeyes.ref_set (`name`) VALUES ('DM+D AMP');
 INSERT INTO openeyes.ref_set (`name`) VALUES ('DM+D VMP');
 INSERT INTO openeyes.ref_set (`name`) VALUES ('DM+D VTM');
