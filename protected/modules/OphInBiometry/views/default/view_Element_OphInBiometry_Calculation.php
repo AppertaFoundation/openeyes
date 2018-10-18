0a67912e369ee7fde4b98477b5938535333ae5f1
Arpit Arora <arpit.arora89@live.com>
2018-10-18 16:39:45 +1100

    OE-7316: updating the dom to replicate the idg

--- protected/modules/OphInBiometry/views/default/view_Element_OphInBiometry_Calculation.php
+++ protected/modules/OphInBiometry/views/default/view_Element_OphInBiometry_Calculation.php
@@ -18,25 +18,27 @@
 ?>
 
 <div class="spacer"></div>
-<section class="element view full eye-divider view">
-    <div class="element-both-eyes">
-        <div class="data-value">
-            <?php
-            echo '<b>Device Comments</b>:&nbsp;' . $element->{'comments'};
-            ?>
+    <header class="element-header"><h3 class="element-title">General Comments</h3></header>
+    <div class="element-data eye-divider">
+        <div class="element-both-eyes">
+            <div class="data-value">
+                <?php
+                echo '<b>Device Comments</b>:&nbsp;' . $element->{'comments'};
+                ?>
+            </div>
+        </div>
+        <div class="element-eyes">
+            <?php foreach (['left' => 'right', 'right' => 'left'] as $page_side => $eye_side): ?>
+                <div class="<?= $eye_side ?>-eye">
+                    <?php if ($element->hasEye($eye_side)) {
+                        $this->renderPartial('view_Element_OphInBiometry_Calculation_fields',
+                            array('side' => $eye_side, 'element' => $element));
+                    } else { ?>
+                        <div class="data-value not-recorded">Not recorded</div>
+                    <?php } ?>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
-    <div class="element-eyes">
-        <?php foreach (['left' => 'right', 'right' => 'left'] as $page_side => $eye_side): ?>
-            <div class="<?= $eye_side ?>-eye">
-                <?php if ($element->hasEye($eye_side)) {
-                    $this->renderPartial('view_Element_OphInBiometry_Calculation_fields',
-                        array('side' => $eye_side, 'element' => $element));
-                } else { ?>
-                    <div class="data-value not-recorded">Not recorded</div>
-                <?php } ?>
-            </div>
-        <?php endforeach; ?>
-    </div>
-</section>
+
 <div class="spacer"></div>
