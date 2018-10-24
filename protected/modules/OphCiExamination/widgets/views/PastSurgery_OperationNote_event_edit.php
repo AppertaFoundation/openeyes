469a70f814117d3f64920965530f49dc2398640d
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2018-10-24 15:41:45 +0100

    OE-7808 Be consistent with the method of creating arrays , use triple equals instead of double

--- protected/modules/OphCiExamination/widgets/views/PastSurgery_OperationNote_event_edit.php
+++ protected/modules/OphCiExamination/widgets/views/PastSurgery_OperationNote_event_edit.php
@@ -18,33 +18,33 @@
 
 <?php
 if (!isset($values)) {
-    $values = array(
+    $values = [
         'operation' => $op,
         'side' => $side,
         'date' => $date,
-    );
+    ];
 } ?>
-<tr class="row-<?=$row_count;?>read-only" id="<?= $model_name ?>_operations_<?=$row_count?>">
+<tr class="row-<?= $row_count; ?>read-only" id="<?= $model_name ?>_operations_<?= $row_count ?>">
     <td>
-            <?= $values['operation'] ?>
+        <?= $values['operation'] ?>
     </td>
-    <td class="past-surgery-entry has-operation">
+    <td class="past-surgery-entry has-operation"></td>
+    <td>
+        <?php
+        foreach (['Right', 'Left'] as $eye_side) {
+            if ($values['side'] === $eye_side || $values['side'] === 'Both') { ?>
+                <i class="oe-i laterality <?= substr($values['side'][0] , 0 , 1)?> small pad"></i>
+            <?php } else { ?>
+                <i class="oe-i laterality small pad NA"></i>
+            <?php }
+        } ?>
     </td>
-        <td>
-            <?php
-            foreach(['Right' , 'Left'] as $eye_side){
-                if($values['side'] === $eye_side || $values['side'] == 'Both'){ ?>
-                    <i class="oe-i laterality <?= substr($values['side'] , 0 , 1)?> small pad"></i>
-                <?php } else { ?>
-                        <i class="oe-i laterality small pad NA"></i>
-               <?php }
-                } ?>
-        </td>
     <td></td>
     <td>
-        <?=Helper::formatFuzzyDate($values['date']) ?>
+        <?= Helper::formatFuzzyDate($values['date']) ?>
     </td>
-        <td>read only
-            <i class="js-has-tooltip oe-i info small pad right"
-               data-tooltip-content="This operation is recorded as an Operation Note event in OpenEyes and cannot be edited here"></i></td>
+    <td>read only
+        <i class="js-has-tooltip oe-i info small pad right"
+           data-tooltip-content="This operation is recorded as an Operation Note event in OpenEyes and cannot be edited here"></i>
+    </td>
 </tr>
