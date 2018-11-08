7d16a8f79ade133b5cc2579ed596ccf84f2a5362
Edwin <edwin@fornicode.co.uk>
2018-11-08 15:33:46 +0000

    OE-7834 - Check AutoCompleteSearch is loaded and available otherwise errors will occur where AutoCompleteSearch is being called but not needed

--- protected/modules/OphTrOperationbooking/assets/js/module.js
+++ protected/modules/OphTrOperationbooking/assets/js/module.js
@@ -167,14 +167,17 @@
             }
         });
 
-        OpenEyes.UI.AutoCompleteSearch.init({
-            input: $('#oe-autocompletesearch'),
-            url: '/user/autoComplete',
-            onSelect: function(){
-                AutoCompleteResponse = OpenEyes.UI.AutoCompleteSearch.getResponse();
-                $('#Element_OphTrOperationbooking_Operation_organising_admission_user_id').val(AutoCompleteResponse.id);
-                $('.organising_admission_user').html(AutoCompleteResponse.value + ' (<a href=\"#\" class=\"remove_organising_admission_user\">remove</a>)');
-            }
-        });
+        if(OpenEyes.UI.AutoCompleteSearch != undefined){
+            OpenEyes.UI.AutoCompleteSearch.init({
+                input: $('#oe-autocompletesearch'),
+                url: '/user/autoComplete',
+                onSelect: function(){
+                    AutoCompleteResponse = OpenEyes.UI.AutoCompleteSearch.getResponse();
+                    $('#Element_OphTrOperationbooking_Operation_organising_admission_user_id').val(AutoCompleteResponse.id);
+                    $('.organising_admission_user').html(AutoCompleteResponse.value + ' (<a href=\"#\" class=\"remove_organising_admission_user\">remove</a>)');
+                }
+            });
+        }
+
     });
 }());
