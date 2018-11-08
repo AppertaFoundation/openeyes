976dd91e89711e7ec66d8290ab6edbb07618b899
Zhaolian Zhou <zhaolian.zhou@toukanlabs.com>
2018-11-09 09:33:45 +1100

    cera-13, duplicate patient display format

--- protected/views/patient/crud/_conflicts.php
+++ protected/views/patient/crud/_conflicts.php
@@ -4,21 +4,30 @@
       <p>Duplicate patient detected.</p>
     </td>
     <td>
-          <div class="row field-row">
-            <div> <?php echo Patient::model()->getAttributeLabel('hos_num'); ?></div>
-            <div>Name</div>
-          </div>
-        <?php foreach ($patients as $patient): ?>
-          <div class="row field-row">
-            <div><?php echo CHtml::link($patient->hos_num,
-                Yii::app()->controller->createUrl('patient/view', array('id' => $patient->id)), array('target' => '_blank')); ?></div>
-            <div>
-              <?php echo CHtml::link($patient->getFullName(),
-                Yii::app()->controller->createUrl('patient/view', array('id' => $patient->id)), array('target' => '_blank')); ?>
-            </div>
-          </div>
-        <?php endforeach; ?>
-      </ul>
+      <table class="last-left">
+        <thead>
+        <tr>
+          <th> <?php echo Patient::model()->getAttributeLabel('hos_num'); ?></th>
+          <th>Name</th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <?php foreach ($patients as $patient): ?>
+            <tr>
+              <td><?php echo CHtml::link($patient->hos_num,
+                  Yii::app()->controller->createUrl('patient/view', array('id' => $patient->id)), array('target' => '_blank')); ?></td>
+              <td>
+                <?php echo CHtml::link($patient->getFullName(),
+                  Yii::app()->controller->createUrl('patient/view', array('id' => $patient->id)), array('target' => '_blank')); ?>
+              </td>
+              <td></td>
+            </tr>
+          <?php endforeach; ?>
+        </tr>
+        </tbody>
+      </table>
     </td>
   <?php else: ?>
     <p>No conflicts found.</p>
