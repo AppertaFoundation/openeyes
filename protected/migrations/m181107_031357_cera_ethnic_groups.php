0ef40aad1c7963f0e83acfa2da71fd2c6958fe59
Zhaolian Zhou <zhaolian.zhou@toukanlabs.com>
2018-11-15 14:38:01 +1100

    CERA-14, change ethnic group code

--- protected/migrations/m181107_031357_cera_ethnic_groups.php
+++ protected/migrations/m181107_031357_cera_ethnic_groups.php
@@ -15,15 +15,15 @@ class m181107_031357_cera_ethnic_groups extends OEMigration
     }
 
 
-    $this->insert('ethnic_group', array('name' => 'Australia', 'code' => 'A', 'display_order' => '10', 'tag'=>'CERA'));
-    $this->insert('ethnic_group', array('name' => 'China', 'code' => 'C', 'display_order' => '20', 'tag'=>'CERA'));
-    $this->insert('ethnic_group', array('name' => 'Greece', 'code' => 'G', 'display_order' => '30', 'tag'=>'CERA'));
-    $this->insert('ethnic_group', array('name' => 'Italy', 'code' => 'I', 'display_order' => '40', 'tag'=>'CERA'));
-    $this->insert('ethnic_group', array('name' => 'New Zealand', 'code' => 'N', 'display_order' => '50', 'tag'=>'CERA'));
-    $this->insert('ethnic_group', array('name' => 'United Kingdom', 'code' => 'U', 'display_order' => '60', 'tag'=>'CERA'));
-    $this->insert('ethnic_group', array('name' => 'India', 'code' => 'L', 'display_order' => '70', 'tag'=>'CERA'));
-    $this->insert('ethnic_group', array('name' => 'Other', 'code' => 'O', 'display_order' => '80', 'tag'=>'CERA'));
-    $this->insert('ethnic_group', array('name' => 'Don\'t know', 'code' => 'D', 'display_order' => '90', 'tag'=>'CERA'));
+    $this->insert('ethnic_group', array('name' => 'Indigenous Australia', 'code' => 'S', 'display_order' => '10', 'tag'=>'CERA'));
+    $this->insert('ethnic_group', array('name' => 'China', 'code' => 'R', 'display_order' => '20', 'tag'=>'CERA'));
+    $this->insert('ethnic_group', array('name' => 'Greece', 'code' => 'C', 'display_order' => '30', 'tag'=>'CERA'));
+    $this->insert('ethnic_group', array('name' => 'Italy', 'code' => 'C', 'display_order' => '40', 'tag'=>'CERA'));
+    $this->insert('ethnic_group', array('name' => 'New Zealand', 'code' => 'S', 'display_order' => '50', 'tag'=>'CERA'));
+    $this->insert('ethnic_group', array('name' => 'United Kingdom', 'code' => 'A', 'display_order' => '60', 'tag'=>'CERA'));
+    $this->insert('ethnic_group', array('name' => 'India', 'code' => 'H', 'display_order' => '70', 'tag'=>'CERA'));
+    $this->insert('ethnic_group', array('name' => 'Other', 'code' => 'S', 'display_order' => '80', 'tag'=>'CERA'));
+    $this->insert('ethnic_group', array('name' => 'Don\'t know', 'code' => 'Z', 'display_order' => '90', 'tag'=>'CERA'));
 
 
   }
@@ -31,7 +31,7 @@ class m181107_031357_cera_ethnic_groups extends OEMigration
   public function safeDown()
   {
     $this->dropColumn('ethnic_group', 'tag');
-    $this->delete('ethnic_group', 'name = "Australia"');
+    $this->delete('ethnic_group', 'name = "Indigenous Australia"');
     $this->delete('ethnic_group', 'name = "China"');
     $this->delete('ethnic_group', 'name = "Greece"');
     $this->delete('ethnic_group', 'name = "Italy"');
@@ -40,7 +40,6 @@ class m181107_031357_cera_ethnic_groups extends OEMigration
     $this->delete('ethnic_group', 'name = "India"');
     $this->delete('ethnic_group', 'name = "Other"');
     $this->delete('ethnic_group', 'name = "Don\'t know"');
-
   }
 
 }
