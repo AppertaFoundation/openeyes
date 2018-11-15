70f11d0a0178ffd4adc23dc4516ce277789077b1
Arpit Arora <arpit.arora89@live.com>
2018-11-15 17:51:54 +1100

    CERA29 - v3 ui for view gp page

--- protected/views/gp/view.php
+++ protected/views/gp/view.php
@@ -4,57 +4,63 @@
 
 $this->pageTitle = 'View Practitioner';
 ?>
-
-<h1 class="badge">Practitioner Summary</h1>
-<div class="row data-row">
-  <div class="large-8 column">
-    <section class="box patient-info js-toggle-container">
-      <h3 class="box-title">Contact Information:</h3>
-      <a href="#" class="toggle-trigger toggle-hide js-toggle">
+<div>
+    <div class="oe-full-header flex-layout">
+        <div class="title wordcaps">
+            <b>Practitioner Summary</b>
+        </div>
+    </div>
+    <div class="oe-full-content oe-new-patient flex-layout flex-top">
+        <div class="cols-6 box patient-info js-toggle-container">
+            <h3 class="box-title">Contact Information:</h3>
+            <a href="#" class="toggle-trigger toggle-hide js-toggle">
             <span class="icon-showhide">
                 Show/hide this section
             </span>
-      </a>
-      <div class="js-toggle-body">
-        <div class="row data-row">
-          <div class="large-3 column">
-            <div class="data-label">Name:</div>
-          </div>
-          <div class="large-4 column end">
-            <div class="data-value"><?php echo CHtml::encode($model->getCorrespondenceName()); ?></div>
-          </div>
-        </div>
-        <div class="row data-row">
-          <div class="large-3 column">
-            <div class="data-label">Phone Number:</div>
-          </div>
-          <div class="large-3 column end">
-            <div
-                class="data-value"><?php echo isset($model->contact->primary_phone) ? CHtml::encode($model->contact->primary_phone) : 'Unknown'; ?></div>
-          </div>
-        </div>
-        <div class="row data-row">
-          <div class="large-3 column">
-            <div class="data-label">Role:</div>
-          </div>
-          <div class="large-4 column end">
-            <div class="data-value"><?php echo CHtml::encode(isset($model->contact->label)?$model->contact->label->name:''); ?></div>
-          </div>
-        </div>
-        <!--Add the address row here when GPs get tied directly to practices rather than through patient records.-->
-      </div>
-    </section>
-  </div>
-    <?php if (Yii::app()->user->checkAccess('TaskCreateGp')): ?>
-      <div class="large-4 column end">
-        <div class="box generic">
-          <div class="row">
-            <div class="large-12 column end">
-              <p><?php echo CHtml::link('Update Practitioner Details',
-                      $this->createUrl('/gp/update', array('id' => $model->id))); ?></p>
+            </a>
+            <div class="js-toggle-body">
+                <table class="standard">
+                    <tbody>
+                    <tr>
+                        <td>
+                            Name:
+                        </td>
+                        <td>
+                            <?php echo CHtml::encode($model->getCorrespondenceName()); ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Phone Number:
+                        </td>
+                        <td>
+                            <?php echo isset($model->contact->primary_phone) ? CHtml::encode($model->contact->primary_phone) : 'Unknown'; ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Role:
+                        </td>
+                        <td>
+                            <?php echo CHtml::encode(isset($model->contact->label) ? $model->contact->label->name : ''); ?>
+                        </td>
+                    </tr>
+                    </tbody>
+                    <!--Add the address row here when GPs get tied directly to practices rather than through patient records.-->
+                </table>
             </div>
-          </div>
         </div>
-      </div>
-    <?php endif; ?>
+        <?php if (Yii::app()->user->checkAccess('TaskCreateGp')): ?>
+            <div class="large-4 column end">
+                <div class="box generic">
+                    <div class="row">
+                        <div class="large-12 column end">
+                            <p><?php echo CHtml::link('Update Practitioner Details',
+                                    $this->createUrl('/gp/update', array('id' => $model->id))); ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
 </div>
