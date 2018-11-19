3decf28aa41ef1d1dfd9a7efaa1c08ee74bbe8d5
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2018-11-19 10:35:36 +0000

    OE-7936 Change the container to use Main instead of Div as the div was causing an empty page in the print mode. Improve spacing in third consent print form.

--- protected/modules/OphTrConsent/views/default/print3_English.php
+++ protected/modules/OphTrConsent/views/default/print3_English.php
@@ -15,64 +15,76 @@
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 ?>
-<div class="print-form-div <?php echo $css_class?>">
-	<?php $this->renderPartial('_consent_header')?>
-	<h1 class="print-title">
-		Consent form 3<br/>
-		<?php echo $this->patient->fullName?>, Hospital no: <?php echo $this->patient->hos_num?>
-	</h1>
-	<h3>Patient/parental agreement to investigation or treatment (procedures where consciousness not impaired)</h3>
-	<p>
-		<strong>Procedure(s):</strong>
-		<?php foreach ($elements['Element_OphTrConsent_Procedure']->procedures as $i => $procedure) {
-    if ($i > 0) {
-        echo ', ';
-    }
-    echo \CHtml::encode($procedure->term);
-}?>
-	</p>
-	<p>
-		<strong>Statement of health professional</strong> (to be filled in by health professional with appropriate knowledge of proposed procedure, as specified in consent policy)
-	</p>
-	<p>
-		<strong>I have explained the procedure to the patient/parent. In particular, I have explained:</strong>
-		<br/>
-		<strong>The intended benefits:</strong>
-		<?php echo $elements['Element_OphTrConsent_BenefitsAndRisks']->benefits?>
-		<br/>
-		<strong>Serious, frequently occurring or unavoidable risks:</strong>
-		<?php echo $elements['Element_OphTrConsent_BenefitsAndRisks']->risks?>
-	</p>
-	<?php if (!empty($elements['Element_OphTrConsent_Procedure']->additional_procedures)) {?>
-		<p>Any extra procedures which may become necessary during the procedure(s):</p>
-		<?php echo $this->renderPartial('_proposed_procedures', array('css_class' => 'large', 'procedures' => $elements['Element_OphTrConsent_Procedure']->additional_procedures, 'eye' => $elements['Element_OphTrConsent_Procedure']->eye->adjective))?>
-	<?php }?>
-	<p>
-		I have also discussed what the procedure is likely to involve, the benefits and risks of any available alternative treatments (including no treatment) and any particular concerns of those involved.
-	</p>
+<div class="print-form-div <?php echo $css_class ?>">
+    <?php $this->renderPartial('_consent_header') ?>
+    <h1 class="print-title">
+        Consent form 3<br/>
+        <?php echo $this->patient->fullName ?>, Hospital no: <?php echo $this->patient->hos_num ?>
+    </h1>
+    <h3>Patient/parental agreement to investigation or treatment (procedures where consciousness not impaired)</h3>
+    <p>
+        <strong>Procedure(s):</strong>
+        <?php foreach ($elements['Element_OphTrConsent_Procedure']->procedures as $i => $procedure) {
+            if ($i > 0) {
+                echo ', ';
+            }
+            echo \CHtml::encode($procedure->term);
+        } ?>
+    </p>
+    <p>
+        <strong>Statement of health professional</strong> (to be filled in by health professional with appropriate
+        knowledge of proposed procedure, as specified in consent policy)
+    </p>
+    <p>
+        <strong>I have explained the procedure to the patient/parent. In particular, I have explained:</strong>
+        <br/>
+    </p>
+    <p>
+        <strong>The intended benefits:</strong>
+        <?php echo $elements['Element_OphTrConsent_BenefitsAndRisks']->benefits ?>
+        <br>
+    </p>
+    <p>
+        <strong>Serious, frequently occurring or unavoidable risks:</strong>
+        <?php echo $elements['Element_OphTrConsent_BenefitsAndRisks']->risks ?>
+    </p>
+    <?php if (!empty($elements['Element_OphTrConsent_Procedure']->additional_procedures)) { ?>
+        <p>Any extra procedures which may become necessary during the procedure(s):</p>
+        <?php echo $this->renderPartial('_proposed_procedures', array('css_class' => 'large', 'procedures' => $elements['Element_OphTrConsent_Procedure']->additional_procedures, 'eye' => $elements['Element_OphTrConsent_Procedure']->eye->adjective)) ?>
+    <?php } ?>
+    <p>
+        I have also discussed what the procedure is likely to involve, the benefits and risks of any available
+        alternative treatments (including no treatment) and any particular concerns of those involved.
+    </p>
     <div class="group flex-layout">
                 <span class="nowrap">
                     <span class="checkbox <?php echo $elements['Element_OphTrConsent_Other']->information ? 'checked' : '' ?>"> </span>The following informational leaflets have been provided:
                     <span class="dotted-write"></span>
                 </span>
     </div>
-	<?php echo $this->renderPartial('signature_table1', array('vi' => ($css_class == 'impaired'), 'consultant' => $elements['Element_OphTrConsent_Other']->consultant))?>
+    <?php echo $this->renderPartial('signature_table1', array('vi' => ($css_class == 'impaired'), 'consultant' => $elements['Element_OphTrConsent_Other']->consultant)) ?>
     <div class="break"></div>
-		<?php if ($elements['Element_OphTrConsent_Other']->interpreter_required) {?>
-			<h3>Statement of interpreter</h3>
-			<p>
-				I have interpreted the information above to the patient/parent to the best of my ability and in a way in which I believe s/he/they can understand.
-			</p>
-			<?php echo $this->renderPartial('signature_table3', array('vi' => ($css_class == 'impaired'), 'name' => $elements['Element_OphTrConsent_Other']->interpreter_name))?>
-		<?php }?>
-		<h3>Statement of patient/person with parental responsibility for patient I agree to the procedure described above.</h3>
-		<p>
-			I understand that you cannot give me a guarantee that a particular person will perform the procedure. The person will, however, have appropriate experience. I understand that the procedure will/will not involve local anaesthesia.
-		</p>
-		<?php echo $this->renderPartial('signature_table2', array('vi' => ($css_class == 'impaired')))?>
-		<p>
-			Confirmation of consent (to be completed by a health professional when the patient is admitted for the procedure, if the patient/parent has signed the form in advance) I have confirmed that the patient/parent has no further questions and wishes the procedure to go ahead.
-		</p>
-		<?php echo $this->renderPartial('signature_table1', array('vi' => ($css_class == 'impaired'), 'consultant' => $elements['Element_OphTrConsent_Other']->consultant, 'mask_consultant' => true))?>
-		<h3>Top copy accepted by patient: yes/no <span class="noth3">(please ring)</span></h3>
+    <?php if ($elements['Element_OphTrConsent_Other']->interpreter_required) { ?>
+        <h3>Statement of interpreter</h3>
+        <p>
+            I have interpreted the information above to the patient/parent to the best of my ability and in a way in
+            which I believe s/he/they can understand.
+        </p>
+        <?php echo $this->renderPartial('signature_table3', array('vi' => ($css_class == 'impaired'), 'name' => $elements['Element_OphTrConsent_Other']->interpreter_name)) ?>
+    <?php } ?>
+    <h3>Statement of patient/person with parental responsibility for patient I agree to the procedure described
+        above.</h3>
+    <p>
+        I understand that you cannot give me a guarantee that a particular person will perform the procedure. The person
+        will, however, have appropriate experience. I understand that the procedure will/will not involve local
+        anaesthesia.
+    </p>
+    <?php echo $this->renderPartial('signature_table2', array('vi' => ($css_class == 'impaired'))) ?>
+    <p>
+        Confirmation of consent (to be completed by a health professional when the patient is admitted for the
+        procedure, if the patient/parent has signed the form in advance) I have confirmed that the patient/parent has no
+        further questions and wishes the procedure to go ahead.
+    </p>
+    <?php echo $this->renderPartial('signature_table1', array('vi' => ($css_class == 'impaired'), 'consultant' => $elements['Element_OphTrConsent_Other']->consultant, 'mask_consultant' => true)) ?>
+    <h3>Top copy accepted by patient: yes/no <span class="noth3">(please ring)</span></h3>
 </div>
