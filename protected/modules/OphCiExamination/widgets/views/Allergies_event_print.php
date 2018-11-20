6a4f40ee5647afaa5c8b21bfa32a31658a115239
Edwin <edwin@fornicode.co.uk>
2018-11-20 09:32:29 +0000

    OE-7795 - Update design and content for print version

--- protected/modules/OphCiExamination/widgets/views/Allergies_event_print.php
+++ protected/modules/OphCiExamination/widgets/views/Allergies_event_print.php
@@ -1,4 +1,4 @@
-<?php use OEModule\OphCiExamination\models\AllergyEntry; ?>
+<?php use OEModule\OphCiExamination\models\AllergyEntry; use OEModule\OphCiExamination\models\OphCiExaminationAllergy; ?>
 
 <div class="element-data full-width">
     <div class="flex-layout flex-top">
@@ -21,37 +21,88 @@
                 );
                 ?>
             <div class="flex-layout">
-                <table class="borders cols-12">
-                    <colgroup>
-                        <col class="cols-2">
-                        <col class="cols-5">
-                        <col class="cols-5">
-                    </colgroup>
-                    <tbody>
-                        <tr>
-                            <th></th>
-                            <th>Allergy</th>
-                            <th>Comments</th>
-                        </tr>
+            
+                <div class="cols-2">Present</div>
+            
+                <table class="last-left">
+                  <colgroup>
+                    <col class="cols-4">
+                  </colgroup>
+                <tbody>
+                    <?php if(count($entries[(string)AllergyEntry::$PRESENT]) >0){ ?>
                         <?php for ($i = 0; $i < $max_iter; $i++) :?>
                             <?php if(isset($entries[(string)AllergyEntry::$PRESENT][$i])){?>
                                 <tr>
-                                    <th>Present</th>
-                                    <th><?= $entries[(string)AllergyEntry::$PRESENT][$i]->getDisplayAllergy(); ?></th>
-                                    <th><?= $entries[(string)AllergyEntry::$PRESENT][$i]['comments']; ?></th>
+                                    <td><?= $entries[(string)AllergyEntry::$PRESENT][$i]->getDisplayAllergy(); ?></td>
+                                    <td><?= ($entries[(string)AllergyEntry::$PRESENT][$i]['comments'] !== "" ? $entries[(string)AllergyEntry::$PRESENT][$i]['comments'] : '<span class="none">None</span>'); ?></td>
                                 </tr>
                             <?php } ?>
-                            <?php if(isset($entries[(string)AllergyEntry::$NOT_PRESENT][$i])){?>
+                        <?php endfor; ?>
+                    <?php } else { ?>
+                        <tr>
+                            <td>None</td>
+                            <td><span class="none">None</span></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+              </table>
+            </div><!-- .flex-layout -->
+            
+            <hr class="divider">
+                     <div class="flex-layout">
+                    
+                        <div class="cols-2">Unchecked</div>
+                    
+                        <table class="last-left">
+                          <colgroup>
+                            <col class="cols-4">
+                          </colgroup>
+                        <tbody>
+                            <?php if(count(OphCiExaminationAllergy::unchecked($element->entries[0]['element_id'])) >0){ ?>
+                                <?php foreach (OphCiExaminationAllergy::unchecked($element->entries[0]['element_id']) as $entry) : ?>
+                                    <tr>
+                                        <td><?= $entry->name; ?></td>
+                                        <td><span class="none">None</span></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            <?php } else { ?>
                                 <tr>
-                                    <th>Not Present</th>
-                                    <th><?= $entries[(string)AllergyEntry::$NOT_PRESENT][$i]->getDisplayAllergy(); ?></th>
-                                    <th><?= $entries[(string)AllergyEntry::$NOT_PRESENT][$i]['comments']; ?></th>
+                                    <td>None</td>
+                                    <td><span class="none">None</span></td>
+                                </tr>
+                            <?php } ?> 
+                        </tbody>
+                      </table>
+                    </div><!-- .flex-layout -->
+                    <hr class="divider">
+                    
+                    <div class="flex-layout">
+                    
+                        <div class="cols-2">Absent</div>
+                    
+                        <table class="last-left">
+                          <colgroup>
+                            <col class="cols-4">
+                          </colgroup>
+                        <tbody> 
+                            <?php if(count($entries[(string)AllergyEntry::$NOT_PRESENT]) >0){ ?>
+                                <?php for ($i = 0; $i < $max_iter; $i++) :?>
+                                    <?php if(isset($entries[(string)AllergyEntry::$NOT_PRESENT][$i])){?>
+                                        <tr>
+                                            <td><?= $entries[(string)AllergyEntry::$NOT_PRESENT][$i]->getDisplayAllergy(); ?></td>
+                                            <td><?= ($entries[(string)AllergyEntry::$NOT_PRESENT][$i]['comments'] !== "" ? $entries[(string)AllergyEntry::$NOT_PRESENT][$i]['comments'] : '<span class="none">None</span>'); ?></td>
+                                        </tr>
+                                    <?php } ?>
+                                <?php endfor; ?>
+                            <?php } else { ?>
+                                <tr>
+                                    <td>None</td>
+                                    <td><span class="none">None</span></td>
                                 </tr>
                             <?php } ?>
-                        <?php endfor; ?>                            
-                    </tbody>                    
-                </table>
-            </div>
+                        </tbody>
+                      </table>
+                    </div><!-- .flex-layout -->
             <?php endif; ?>
         </div>
     </div>
