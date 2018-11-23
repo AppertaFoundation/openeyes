710609626d6d05e7f2ebcbadcf6a01ca28db880e
Isaac Jackson <mrisaacjackson@gmail.com>
2018-11-23 12:27:33 +1100

    OE-7978 fixed removal of anaesthetist required column breaking migration

--- protected/modules/OphTrOperationbooking/migrations/m181105_124209_operationbooking_operation_remove_anaesthetist_required_column.php
+++ protected/modules/OphTrOperationbooking/migrations/m181105_124209_operationbooking_operation_remove_anaesthetist_required_column.php
@@ -4,8 +4,13 @@ class m181105_124209_operationbooking_operation_remove_anaesthetist_required_col
 {
     public function up()
     {
-        $this->dropColumn('et_ophtroperationbooking_operation_version' , 'anaesthetist_required');
-        $this->dropColumn('et_ophtroperationbooking_operation' , 'anaesthetist_required');
+        try {
+            $this->dropColumn('et_ophtroperationbooking_operation_version' , 'anaesthetist_required');
+            $this->dropColumn('et_ophtroperationbooking_operation' , 'anaesthetist_required');
+        } catch (Exception $e){
+            echo 'Column anaethetist_required in table (and version) '.
+                'et_ophtropereration_operation_booking didn\'t exist and therefore wasn\'t removed';
+        }
     }
     public function down()
     {
