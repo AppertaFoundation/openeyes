79868f8ecd896a5144a69bfa9bdc2d6ec14600f3
Isaac Jackson <mrisaacjackson@gmail.com>
2018-12-05 08:45:02 +1100

    OE-8042 Fixed an issue where a migration removing group_title would fail
    The migration now checks if the column exists before attempting to remove it.
    Also added some checks to prevent a rare migration problem with group_title

--- protected/migrations/m180416_031716_add_element_group_title_column.php
+++ protected/migrations/m180416_031716_add_element_group_title_column.php
@@ -12,15 +12,17 @@ class m180416_031716_add_element_group_title_column extends OEMigration
         if (!isset(Yii::app()->db->schema->getTable('element_type')->columns['group_title'])) {
             $this->addColumn('element_type_version', 'group_title', 'VARCHAR(255)');
         }
-        // Set the group title to default to the element name
-        $this->update('element_type', array('group_title' => new CDbExpression('name')),
-            array('parent_element_type_id IS NOT NULL'));
+        if (isset(Yii::app()->db->schema->getTable('element_type')->columns['group_title'])) {
+            // Set the group title to default to the element name
+            $this->update('element_type', array('group_title' => new CDbExpression('name')),
+                array('parent_element_type_id IS NOT NULL'));
+        }
 
     }
 
     public function safeDown()
     {
-        if (isset(Yii::app()->db->schema->getTable('myTable')->columns['group_title'])) {
+        if (isset(Yii::app()->db->schema->getTable('element_type')->columns['group_title'])) {
             $this->dropColumn('element_type_version', 'group_title');
             $this->dropColumn('element_type', 'group_title');
         }
