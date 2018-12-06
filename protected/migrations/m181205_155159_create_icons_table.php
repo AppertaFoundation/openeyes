e94be364f99e33d9c7e490ff6864e575d4bcd8c8
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2018-12-06 13:45:48 +0000

    OE-7974 Fix PR requests

--- protected/migrations/m181205_155159_create_icons_table.php
+++ protected/migrations/m181205_155159_create_icons_table.php
@@ -2,25 +2,20 @@
 
 class m181205_155159_create_icons_table extends OEMigration
 {
-	public function up()
-	{
+    public function up()
+    {
         $this->createOETable('icons', [
             'id' => 'pk',
             'class_name' => 'varchar(128) COLLATE utf8_bin NOT NULL']);
 
-        $this->getDbConnection()->getCommandBuilder()->createMultipleInsertCommand(
-            'icons',
-            array(
-                array('class_name' => 'exclamation'),
-                array('class_name' => 'exclamation-green'),
-                array('class_name' => 'exclamation-amber'),
-                array('class_name' => 'exclamation-red'),
-            )
-        )->execute();
-	}
+        $this->insert('icons', array('class_name' => 'exclamation'));
+        $this->insert('icons', array('class_name' => 'exclamation-green'));
+        $this->insert('icons', array('class_name' => 'exclamation-amber'));
+        $this->insert('icons', array('class_name' => 'exclamation-red'));
+    }
 
-	public function down()
-	{
-		$this->dropTable('icons');
-	}
+    public function down()
+    {
+        $this->dropTable('icons');
+    }
 }
--- protected/migrations/m181205_160624_create_icon_id_column_for_nhs_number_status.php
+++ protected/migrations/m181205_160624_create_icon_id_column_for_nhs_number_status.php
@@ -11,14 +11,22 @@ class m181205_160624_create_icon_id_column_for_nhs_number_status extends CDbMigr
 	    $green_icon_id = Icons::model()->find('class_name = ?', ['exclamation-green'])->id;
 	    $red_icon_id = Icons::model()->find('class_name = ?', ['exclamation-red'])->id;
 
-	    $this->update('nhs_number_verification_status' , ['icon_id' => $green_icon_id] , 'code = 01');
-	    $this->update('nhs_number_verification_status' , ['icon_id' => $amber_icon_id] , 'code = 02');
-	    $this->update('nhs_number_verification_status' , ['icon_id' => $red_icon_id] , 'code = 03');
-	    $this->update('nhs_number_verification_status' , ['icon_id' => $red_icon_id] , 'code = 04');
-	    $this->update('nhs_number_verification_status' , ['icon_id' => $red_icon_id] , 'code = 05');
-	    $this->update('nhs_number_verification_status' , ['icon_id' => $amber_icon_id] , 'code = 06');
-	    $this->update('nhs_number_verification_status' , ['icon_id' => $grey_icon_id] , 'code = 07');
-	    $this->update('nhs_number_verification_status' , ['icon_id' => $grey_icon_id] , 'code = 08');
+	    $this->update('nhs_number_verification_status' ,
+            ['icon_id' => $green_icon_id] , 'description = "Number present and verified"');
+	    $this->update('nhs_number_verification_status' ,
+            ['icon_id' => $amber_icon_id] , 'description = "Number present but not traced"');
+	    $this->update('nhs_number_verification_status' ,
+            ['icon_id' => $red_icon_id] , 'description = "Trace required"');
+	    $this->update('nhs_number_verification_status' ,
+            ['icon_id' => $red_icon_id] , 'description = "Trace attempted - No match or multiple match found"');
+	    $this->update('nhs_number_verification_status' ,
+            ['icon_id' => $red_icon_id] , 'description = "Trace needs to be resolved - (NHS Number or PATIENT detail conflict)"');
+	    $this->update('nhs_number_verification_status' ,
+            ['icon_id' => $amber_icon_id] , 'description = "Trace in progress"');
+	    $this->update('nhs_number_verification_status' ,
+            ['icon_id' => $grey_icon_id] , 'description = "Number not present and trace not required"');
+	    $this->update('nhs_number_verification_status' ,
+            ['icon_id' => $grey_icon_id] , 'description = "Trace postponed (baby under six weeks old)"');
 	}
 
 	public function down()
