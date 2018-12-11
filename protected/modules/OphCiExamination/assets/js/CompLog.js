8542aae7724ea1d0322ffe19d8f6650337467f92
adrianbrenton <adrian.r.brenton@gmail.com>
2018-12-11 16:29:07 +0000

    OE-7101 - Refactor COMPLog connection (instantiate connection object)
    This commit wraps the OpenEyes<--->COMPLog connection in a prototype
    (CompLogConnection). A connection attempt is started by instantiating it
    - the object (instance) takes care of creating the connection and
    the associated dialog box.
    
    *Next steps:*
     - Dialog box needs some small alterations to match IDG exactly.
     - Some methods related to the connection cause UI blocking. I have not
       fixed these yet.

--- protected/modules/OphCiExamination/assets/js/CompLog.js
+++ protected/modules/OphCiExamination/assets/js/CompLog.js
@@ -1,10 +1,26 @@
-let dialog = null;
-let compLogCallbackQueue = null;
-let latestHl7Data = null;
+function CompLogConnection(){
+	let self = this;
+	this.initialiseCompLogDialog();
+	this.establishConnection();
+	this.pollForTestResults();
+	this.compLogCallbackQueue.add_function(function(){
+		self.dialog.setTitle('COMPLog test in progress');
+		self.dialog.on('ok', function(){
+			self.importCompLogResults.call(self);
+			$("#complog_iframe").remove(); // need to add this for on cancel too
+			self.dialog.destroy();
+			self.dialog = null;
+		});
+		$('.ok').show();
+	});
+}
 
-function isCompLogConnectedOnWS()
-{
-    var status = false;
+CompLogConnection.dialog = null;
+CompLogConnection.compLogCallbackQueue = null;
+CompLogConnection.latestHl7Data = null;
+
+CompLogConnection.prototype.isCompLogConnectedOnWS = function() {
+    let status = false;
     $.ajax({
         type: 'GET',
         url: "http://localhost:"+OE_COMPLog_port+"/info",
@@ -17,13 +33,11 @@ function isCompLogConnectedOnWS()
         }
     });
     return status;
-}
+};
 
-
-function COMPLogPresetTest()
-{
+CompLogConnection.prototype.COMPLogPresetTest = function() {
     addMessageToFadeContent("Waiting for COMPLog results...");
-    var requestData = {"Message": "MSH|^~\\&|COMPLOG|COMPLOG||COMPLOG|20130510105428.912+0300||ZPT^ZTP^ZPT_ZTP|MSG100|P|2.4\nEVN|ZTP|20050110045502|||||\nPID|||"+OE_patient_hosnum+"||"+OE_patient_firstname+"^"+OE_patient_lastname+"||"+OE_patient_dob+"|"+OE_patient_gender+"-||2106-3|"+OE_patient_address+"|GL||||S||PATID12345001^2^M10|"+OE_patient_id+"|9-87654^NC\nZTP|COMPlogThresholding"};
+    let requestData = {"Message": "MSH|^~\\&|COMPLOG|COMPLOG||COMPLOG|20130510105428.912+0300||ZPT^ZTP^ZPT_ZTP|MSG100|P|2.4\nEVN|ZTP|20050110045502|||||\nPID|||"+OE_patient_hosnum+"||"+OE_patient_firstname+"^"+OE_patient_lastname+"||"+OE_patient_dob+"|"+OE_patient_gender+"-||2106-3|"+OE_patient_address+"|GL||||S||PATID12345001^2^M10|"+OE_patient_id+"|9-87654^NC\nZTP|COMPlogThresholding"};
     $.ajax({
         url: "http://localhost:"+OE_COMPLog_port+"/hl7",
         data: JSON.stringify(requestData),
@@ -38,17 +52,16 @@ function COMPLogPresetTest()
             //alert(x.responseText +"  " +x.status);
         }
     });
+};
 
-}
+CompLogConnection.prototype.updateLatestPolledHl7 = function(newLatest){
+	this.latestHl7Data = newLatest;
+};
 
-function updateLatestPolledHl7(newLatest){
-	latestHl7Data = newLatest;
-	console.log('The latestHl7Data has been updated to' + latestHl7Data.Message);
-}
-
-function COMPLogCheckTestResults()
-{
-    var requestData =  {"Message": "MSH|^~\\&|COMPLOG|COMPLOG||COMPLOG|20130510105428.912+0300||QRY^ZTS|MSG100|P|2.4\nQRD|201311111016|R|I|Q1000|||10^RD|0150311798|RES|ALL||\nPID|||"+OE_patient_hosnum+"||"+OE_patient_firstname+"^"+OE_patient_lastname+"||"+OE_patient_dob+"|"+OE_patient_gender+"-||2106-3|"+OE_patient_address+"|GL||||S||PATID12345001^2^M10|"+OE_patient_id+"|9-87654^NC"};
+/*
+***This method is unused, but was previously left here, so unclear if intended to be used***
+CompLogConnection.prototype.COMPLogCheckTestResults = function() {
+    let requestData =  {"Message": "MSH|^~\\&|COMPLOG|COMPLOG||COMPLOG|20130510105428.912+0300||QRY^ZTS|MSG100|P|2.4\nQRD|201311111016|R|I|Q1000|||10^RD|0150311798|RES|ALL||\nPID|||"+OE_patient_hosnum+"||"+OE_patient_firstname+"^"+OE_patient_lastname+"||"+OE_patient_dob+"|"+OE_patient_gender+"-||2106-3|"+OE_patient_address+"|GL||||S||PATID12345001^2^M10|"+OE_patient_id+"|9-87654^NC"};
     $.ajax({
         url: "http://localhost:"+OE_COMPLog_port+"/hl7",
         data: JSON.stringify(requestData),
@@ -59,17 +72,16 @@ function COMPLogCheckTestResults()
         async: false,
         success: function (data) {
         	let hl7 = data.Message;
-        	console.log('hl7');
         },
         error: function (x, y, z) {
             //alert(x.responseText +"  " +x.status);
         }
     });
+};
+*/
 
-}
-
-function COMPLogDischargePatient() {
-    var requestData = {"Message": "MSH|^~\&|ADT1|COMPLOG|COMPLOG|COMPLOG|198808181126|SECURITY|ADT^A03|MSG00001|P|2.4\nEVN|A01-|198808181123\nPID|||"+OE_patient_hosnum+"||"+OE_patient_firstname+"^"+OE_patient_lastname+"||"+OE_patient_dob+"|"+OE_patient_gender+"-||2106-3|"+OE_patient_address+"|GL||||S||PATID12345001^2^M10|"+OE_patient_id+"|9-87654^NC"};
+CompLogConnection.prototype.COMPLogDischargePatient = function() {
+    let requestData = {"Message": "MSH|^~\&|ADT1|COMPLOG|COMPLOG|COMPLOG|198808181126|SECURITY|ADT^A03|MSG00001|P|2.4\nEVN|A01-|198808181123\nPID|||"+OE_patient_hosnum+"||"+OE_patient_firstname+"^"+OE_patient_lastname+"||"+OE_patient_dob+"|"+OE_patient_gender+"-||2106-3|"+OE_patient_address+"|GL||||S||PATID12345001^2^M10|"+OE_patient_id+"|9-87654^NC"};
     $.ajax({
         url: "http://localhost:"+OE_COMPLog_port+"/hl7",
         data: JSON.stringify(requestData),
@@ -84,27 +96,11 @@ function COMPLogDischargePatient() {
             //alert(x.responseText +"  " +x.status);
         }
     });
-}
+};
 
-function requestHl7TestResults(successCallback=$.noop, errorCallback=$.noop){
-	let requestData =  {"Message": "MSH|^~\\&|COMPLOG|COMPLOG||COMPLOG|20130510105428.912+0300||QRY^ZTR|MSG100|P|2.4\nQRD|201311111016|R|I|Q1000|||10^RD|100437363|RES|ALL||\nPID|||"+OE_patient_hosnum+"||"+OE_patient_firstname+"^"+OE_patient_lastname+"||"+OE_patient_dob+"|"+OE_patient_gender+"-||2106-3|"+OE_patient_address+"|GL||||S||PATID12345001^2^M10|"+OE_patient_id+"|9-87654^NC"};
-	$.ajax({
-		url: "http://localhost:" + OE_COMPLog_port + "/hl7",
-		data: JSON.stringify(requestData),
-		dataType: "json",
-		contentType: "application/json; charset=UTF-8",
-		type: "POST",
-		crossDomain: true,
-		async: false,
-		success: data => successCallback(data),
-		error: (jqXHR, textStatus, errorThrown) => errorCallback(jqXHR, textStatus, errorThrown)
-	});
-}
-
-function convertHl7ToArray(hl7Data){
-	var hl7 = hl7Data.Message;
-	console.log('convertHl7ToArray' + hl7);
-	var parse = function (str) {
+CompLogConnection.prototype.convertHl7ToArray = function(hl7Data){
+	let hl7 = hl7Data.Message;
+	let parse = function (str) {
 		var segments = str.split('\n');
 		return _.map(segments, function (segment) {
 			var fields = segment.split('|');
@@ -114,10 +110,9 @@ function convertHl7ToArray(hl7Data){
 		});
 	};
 
-	var measurements = parse(hl7);
-	var results = [];
-
-	var today = new Date().toJSON().slice(0,10).replace(/-/g,'');
+	let measurements = parse(hl7);
+	let results = [];
+	let today = new Date().toJSON().slice(0,10).replace(/-/g,'');
 
 	for(i=0;i<measurements[0].length;i++){
 		if(measurements[0][i] == "\rZR1") {
@@ -133,25 +128,39 @@ function convertHl7ToArray(hl7Data){
 		}
 	}
 	return results;
-}
+};
 
-function pollForTestResults(){
-	requestHl7TestResults(updateLatestPolledHl7, $.noop);
-	if(dialog){
-		setTimeout(pollForTestResults, 1000);
+CompLogConnection.prototype.pollForTestResults = function(){
+	this.requestHl7TestResults(this.updateLatestPolledHl7.bind(this));
+	if(this.dialog){
+		setTimeout(this.pollForTestResults.bind(this), 1000);
 	}
-}
+};
 
-function importCompLogResults() {
+CompLogConnection.prototype.requestHl7TestResults = function(successCallback=$.noop, errorCallback=$.noop){
+	let requestData =  {"Message": "MSH|^~\\&|COMPLOG|COMPLOG||COMPLOG|20130510105428.912+0300||QRY^ZTR|MSG100|P|2.4\nQRD|201311111016|R|I|Q1000|||10^RD|100437363|RES|ALL||\nPID|||"+OE_patient_hosnum+"||"+OE_patient_firstname+"^"+OE_patient_lastname+"||"+OE_patient_dob+"|"+OE_patient_gender+"-||2106-3|"+OE_patient_address+"|GL||||S||PATID12345001^2^M10|"+OE_patient_id+"|9-87654^NC"};
+	$.ajax({
+		url: "http://localhost:" + OE_COMPLog_port + "/hl7",
+		data: JSON.stringify(requestData),
+		dataType: "json",
+		contentType: "application/json; charset=UTF-8",
+		type: "POST",
+		crossDomain: true,
+		async: false,
+		success: data => successCallback(data),
+		error: (jqXHR, textStatus, errorThrown) => errorCallback(jqXHR, textStatus, errorThrown)
+	});
+};
+
+CompLogConnection.prototype.importCompLogResults = function() {
 	//addMessageToFadeContent("Getting test results from COMPLog...");
-	requestHl7TestResults(updateLatestPolledHl7);
-	saveResultsToOE(convertHl7ToArray(latestHl7Data));
+	this.requestHl7TestResults(this.updateLatestPolledHl7.bind(this));
+	this.saveResultsToOE.call(this, (this.convertHl7ToArray(this.latestHl7Data)));
             //var hl7parser = require("hl7parser");
             //$('.visualAcuityReading ').append(data.Message);
-}
+};
 
-function OphCiExamination_VisualAcuity_getClosestValue(mvalue)
-{
+function OphCiExamination_VisualAcuity_getClosestValue(mvalue) {
     var lastdiff = 10000;
     var previousvalue = {};
 
@@ -168,16 +177,12 @@ function OphCiExamination_VisualAcuity_getClosestValue(mvalue)
                 previousvalue.tooltip = $(this).data("tooltip");
             }
         });
-
     });
     return previousvalue;
-
 }
 
-function OphCiExamination_VisualAcuity_getMethodData(methodName)
-{
-    var method_data = {};
-
+function OphCiExamination_VisualAcuity_getMethodData(methodName) {
+    let method_data = {};
     $('ul[data-id="method"]').each(function() {
         $(this).find('li').each(function(){
             if($(this).data("label") == methodName){
@@ -190,13 +195,10 @@ function OphCiExamination_VisualAcuity_getMethodData(methodName)
     return method_data;
 }
 
-function saveResultsToOE(resultsArray)
-{
+CompLogConnection.prototype.saveResultsToOE = function(resultsArray) {
     unit = $("#visualacuity_unit_change option:selected").html();
-
-    resultsArray.forEach(function(element)
-    {
-        var selected_data = {};
+    resultsArray.forEach(function(element) {
+        let selected_data = {};
         closestValue = OphCiExamination_VisualAcuity_getClosestValue(element.base);
 
         selected_data.reading_value = closestValue.id;
@@ -209,29 +211,23 @@ function saveResultsToOE(resultsArray)
         selected_data.method_display = method_data.label;
 
         OphCiExamination_VisualAcuity_addReading(element.side, selected_data);
-
     });
-    COMPLogDischargePatient();
-}
-
+    this.COMPLogDischargePatient();
+};
 
 function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
+    let start = new Date().getTime();
+    for (let i = 0; i < 1e7; i++) {
         if ((new Date().getTime() - start) > milliseconds){
             break;
         }
     }
 }
 
-var Queue = (function(){
-
+let Queue = (function(){
     function Queue() {}
-
     Queue.prototype.running = false;
-
     Queue.prototype.queue = [];
-
     Queue.prototype.add_function = function(callback) {
         var _this = this;
         //add callback to the queue
@@ -243,12 +239,10 @@ var Queue = (function(){
                 _this.next();
             }
         });
-
         if(!this.running) {
             // if nothing is running, then start the engines!
             this.next();
         }
-
         return this; // for chaining fun!
     };
 
@@ -261,81 +255,69 @@ var Queue = (function(){
             shift();
         }
     };
-
     return Queue;
-
 })();
 
-function openInIframe(url){
+CompLogConnection.prototype.openInIframe = function(url){
     $('body').append('<iframe width="0" height="0" vspace="0" hspace="0" id="complog_iframe" src="'+url+'"></iframe>');
-}
+};
 
+/*
+*** function not used anywhere - still necessary?
 function openInNewTab(url) {
     var win = window.open(url, '_blank');
     win.close();
 }
+*/
 
 function addMessageToFadeContent(msg)
 {
     $(".fadeContent").append(msg+"<br>");
 }
 
-function establishConnection(){
-	compLogCallbackQueue = new Queue;
-	if(!isCompLogConnectedOnWS()){
-		compLogCallbackQueue.add_function(function(){
+CompLogConnection.prototype.establishConnection = function(){
+	let self = this;
+	this.compLogCallbackQueue = new Queue;
+	if(!this.isCompLogConnectedOnWS()){
+		this.compLogCallbackQueue.add_function(function(){
 			// having to use open in new tab, as oelauncher() method doesn't work where
 			// Will mean pop-ups must be enabled on site
 			//openInNewTab("oeLauncher:complog");
-			openInIframe("oeLauncher:complog");
+			self.openInIframe("oeLauncher:complog");
 			sleep(100);
 		});
 	}
 
-	compLogCallbackQueue.add_function(function(){
+	this.compLogCallbackQueue.add_function(function(){
 		setTimeout(function() {
 			var maxRetry = 30;
 			var retry = 0;
-			while((!isCompLogConnectedOnWS()) && (retry < maxRetry)) {
+			while((!self.isCompLogConnectedOnWS()) && (retry < maxRetry)) {
 				sleep(1000);
 				retry++;
 			}
 			sleep(5000);
-			COMPLogDischargePatient(); //makes sure the there is no lingering results
+			self.COMPLogDischargePatient(); //makes sure the there is no lingering results
 			// data from a previous early close of COMPLog
-			compLogCallbackQueue.add_function(COMPLogPresetTest);
+			self.compLogCallbackQueue.add_function(self.COMPLogPresetTest);
 		}, 10);
 	});
-}
+};
 
-function initialiseCompLogDialog(){
-	dialog = new OpenEyes.UI.Dialog.Confirm({
-		title: 'Loading COMPLog test - Please wait...',
+CompLogConnection.prototype.initialiseCompLogDialog = function(){
+	this.dialog = new OpenEyes.UI.Dialog.Confirm({
+		title: 'COMPLog',
 		okButton: 'Pull COMPLog Results',
 		templateSelector: '#dialog-complog-template'});
-	dialog.on('cancel', function(){
+	this.dialog.on('cancel', function(){
 		$("#complog_iframe").remove();
-		dialog.destroy();
-		dialog = null;
+		this.dialog.destroy();
+		this.dialog = null;
 	});
-	dialog.open();
-}
+	this.dialog.open();
+};
 
 $(document).on("click", "#et_complog", function(event){
     event.preventDefault();
-    initialiseCompLogDialog();
-    // try to connect to the WS
-    establishConnection();
-    //need to add a statement to  make sure there is no lingering data in COMPLog from previous early closing
-    pollForTestResults();
-    compLogCallbackQueue.add_function(function(){
-			dialog.setTitle('COMPLog test in progress');
-			dialog.on('ok', function(){
-				importCompLogResults();
-				$("#complog_iframe").remove(); // need to add this for on cancel too
-				dialog.destroy();
-				dialog = null;
-			});
-			$('.ok').show();
-    });
+    let compLogConnection = new CompLogConnection();
 });
