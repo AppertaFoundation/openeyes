b7fd2f5c2d2ac4936e56491944841802d1b10006
Vikas <envikas@gmail.com>
2019-01-02 16:22:13 +1100

    CERA-125 Pull request correction - changed the migration location to be under OETrial module

---
+++ protected/modules/OETrial/migrations/m181228_044926_add_ethics_number_to_trial.php
@@ -0,0 +1,14 @@
+<?php
+
+class m181228_044926_add_ethics_number_to_trial extends OEMigration
+{
+	public function safeUp()
+	{
+        $this->addColumn('trial', 'ethics_number', 'text');
+	}
+
+	public function safeDown()
+	{
+        $this->dropColumn('trial','ethics_number');
+	}
+}
