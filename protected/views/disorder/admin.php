91c81a95afd408975a3a482bb2e1baccda8349fb
Vikas <envikas@gmail.com>
2019-01-09 14:40:42 +1100

    CERA-48 Styled Disorder Admin page

--- protected/views/disorder/admin.php
+++ protected/views/disorder/admin.php
@@ -26,14 +26,12 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Disorders</h1>
-
-<p>
+<p class="description-note note">
 You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
 or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
 </p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('Advanced Search','#',array('class'=>'button large')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
@@ -51,14 +49,24 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'last_modified_user_id',
 		'last_modified_date',
 		'created_user_id',
-		/*
 		'created_date',
 		'specialty_id',
 		'active',
-		*/
 		array(
 			'class'=>'CButtonColumn',
+            'deleteButtonOptions'=>array('class' => 'oe-i trash small hint'),
+            'deleteButtonImageUrl'=> '',
+            'deleteButtonLabel'=>'',
+            'updateButtonOptions'=>array('class' => 'oe-i pencil small'),
+            'updateButtonImageUrl'=> '',
+            'updateButtonLabel'=>'',
+            'viewButtonOptions'=>array('class' => 'oe-i info small'),
+            'viewButtonImageUrl'=> '',
+            'viewButtonLabel'=> '',
 		),
 	),
+    'pager'=>array('class'=>'LinkPager'),
+    'pagerCssClass'=>'pagination',
+    'itemsCssClass'=>'standard highlight-rows',
 )); ?>
-<?= CHtml::link('Add New Disorder', Yii::app()->createURL('disorder/create')) ?>
+<?= CHtml::link('Add New Disorder', Yii::app()->createURL('disorder/create'),array('class' => 'button large')) ?>
