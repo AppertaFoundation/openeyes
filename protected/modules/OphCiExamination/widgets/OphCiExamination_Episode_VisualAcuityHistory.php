221e1fc007c1fa511d0e7e850d7ae617af0a2394
zhaolian-Zhou <zhaolian.zhou@toukanlabs.com>
2019-01-11 16:58:26 +1100

    CERA-135, reduce VA labels in episode

--- protected/modules/OphCiExamination/widgets/OphCiExamination_Episode_VisualAcuityHistory.php
+++ protected/modules/OphCiExamination/widgets/OphCiExamination_Episode_VisualAcuityHistory.php
@@ -72,15 +72,20 @@ class OphCiExamination_Episode_VisualAcuityHistory extends \EpisodeSummaryWidget
      */
     public function configureChart() {
         $va_ticks = $this->getChartTicks();
+        $va_len = sizeof($va_ticks);
+        $step = $va_len/20;
+        $this->va_ticks = array_slice($va_ticks, 0,4);
 
-        $this->va_ticks = $va_ticks;
+        for ($i = 5; $i<=$va_len-$step; $i+=$step){
+            array_push($this->va_ticks, $va_ticks[$i]);
+        }
+
         $this->va_axis = (string)$this->va_unit->name;
-
         $chart = $this->createWidget('FlotChart', array('chart_id' => $this->chart_id))
             ->configureXAxis(array('mode' => 'time'))
             ->configureYAxis(
                 $this->va_axis,
-                array('position' => 'left', 'min' => $this->va_y_min, 'max' => $this->va_y_max, 'ticks' => $va_ticks)
+                array('position' => 'left', 'min' => $this->va_y_min, 'max' => $this->va_y_max, 'ticks' => $this->va_ticks)
             )
             ->configureSeries('Visual Acuity (right)', array('yaxis' => $this->va_axis, 'lines' => array('show' => true), 'points' => array('show' => true)))
             ->configureSeries('Visual Acuity (left)', array('yaxis' => $this->va_axis, 'lines' => array('show' => true), 'points' => array('show' => true)));
