853db5b3b5fea7b34bdc87ab8551eda331780e00
Edwin <edwin@fornicode.co.uk>
2019-01-16 14:02:01 +0000

    OE-8117 - simplify code

--- protected/modules/OphCiExamination/controllers/DefaultController.php
+++ protected/modules/OphCiExamination/controllers/DefaultController.php
@@ -57,8 +57,6 @@ class DefaultController extends \BaseEventTypeController
 
     protected $deletedAllergies = array();
 
-    protected $controller_action;
-
     public function getTitle()
     {
         $title = parent::getTitle();
@@ -81,7 +79,6 @@ class DefaultController extends \BaseEventTypeController
     {
         Yii::app()->assetManager->registerScriptFile('js/spliteventtype.js', null, null, \AssetManager::OUTPUT_SCREEN);
         $this->jsVars['OE_MODEL_PREFIX'] = 'OEModule_OphCiExamination_models_';
-        $this->controller_action = Yii::app()->controller->action->id;
 
         return parent::beforeAction($action);
     }
@@ -1298,7 +1295,7 @@ class DefaultController extends \BaseEventTypeController
         if (!$this->set) {
             /*@TODO: probably the getNextStep() should be able to recognize if there were no steps completed before and return the first step
               Note: getCurrentStep() will return firstStep if there were no steps before */
-            $this->set = $this->getElementSetAssignment() && $this->controller_action != 'update' ? $this->getNextStep() : $this->getCurrentStep();
+            $this->set = $this->getElementSetAssignment() && $this->action->id != 'update' ? $this->getNextStep() : $this->getCurrentStep();
 
             //if $this->set is null than no workflow rule to apply
             $this->mandatoryElements = isset($this->set) ? $this->set->MandatoryElementTypes : null;
