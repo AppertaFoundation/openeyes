bd153a9ee768ed5ee93fa1558d16ed0d92f67c57
Isaac Jackson <mrisaacjackson@gmail.com>
2019-02-05 12:44:48 +1100

    CERA-138 - Removed commented out code and unnecessary maximize

--- features/bootstrap/Pages/Examination.php
+++ features/bootstrap/Pages/Examination.php
@@ -1062,7 +1062,6 @@ class Examination extends OpenEyesPage {
     }
 
     public function addAllergyReading($allergy){
-	    $this->getDriver()->maximizeWindow();
         $this->getElementAtChain(['Allergy', 'addAllergyBtn'])->click();
         $this->elements['Allergy_val'] = array(
             'css' => 'li[data-label='.$allergy.']'
