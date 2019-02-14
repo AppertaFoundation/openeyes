fad4475bd36e7bb1b0cb8d2f8b90408034efcafb
Szabolcs Potondi <szabolcs.potondi@gmail.com>
2019-02-14 16:43:17 +0000

    EDS-27 - adding new 'VitelliformMacularLesion doodle

--- protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_PosteriorPole_OEEyeDraw.php
+++ protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_PosteriorPole_OEEyeDraw.php
@@ -48,6 +48,7 @@ $this->widget('application.modules.eyedraw.OEEyeDrawWidget', array(
             'PreRetinalHaemorrhage',
             'CottonWoolSpot',
             'DiabeticNV',
+            'VitelliformMacularLesion',
             'VitreousOpacity',
             'FibrousProliferation',
             'TractionRetinalDetachment',
