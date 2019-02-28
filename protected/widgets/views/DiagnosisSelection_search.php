468a6fb55535dc5fbe80432a3b4088adba2eb096
sumitshinde559 <45029931+sumitshinde559@users.noreply.github.com>
2019-02-28 14:19:24 +1100

    Shifted Search icon to right on the Injection management section in the Examination page (#2809)

--- protected/widgets/views/DiagnosisSelection_search.php
+++ protected/widgets/views/DiagnosisSelection_search.php
@@ -86,7 +86,7 @@
 			</div>
 			<div class="cols-2 column">
 				<div class="postfix">
-					<button class="oe-i search" id="<?php echo $class.'_'.$field.'_search'?>" style="height: 28px; width: 28px;" type="button">
+					<button class="oe-i search pad-left" id="<?php echo $class.'_'.$field.'_search'?>" style="height: 28px; width: 28px;" type="button">
 						<span class="icon-button-small-search" ></span>
 						<span style="display: none">Search</span>
 					</button>
