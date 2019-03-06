ba008545e1babdf41cc90577ef12a811f4ceb4e5
Yanlin Liu <30500146+louleo@users.noreply.github.com>
2019-03-06 12:54:49 +1100

    fix/CERA-222-Event-date-bug (#2860)

--- protected/models/Event.php
+++ protected/models/Event.php
@@ -106,6 +106,7 @@ class Event extends BaseActiveRecordVersioned
             // Please remove those attributes that should not be searched.
             array('id, episode_id, event_type_id, created_date, event_date, parent_id, pas_visit_id', 'safe', 'on' => 'search'),
             array('event_date', 'OEDateValidatorNotFuture', 'except' => 'allowFutureEvent'),
+            array('event_date','eventDateValidator'),
         );
     }
 
@@ -698,4 +699,21 @@ class Event extends BaseActiveRecordVersioned
             return $this->episode->patient;
         }
     }
+    /**
+     * Validate the event date.
+     */
+    public function eventDateValidator($attribute,$param){
+        $event_date = Helper::mysqlDate2JsTimestamp($this->event_date);
+        if (isset($this->episode)){
+            $episode = $this->episode;
+            if (isset($episode->patient)){
+                $patient = $episode->patient;
+                $event_date_limitation = Helper::mysqlDate2JsTimestamp($patient->dob) - (Helper::EPOCHMONTH*9);
+                if ($event_date < $event_date_limitation){
+                    $this->addError($attribute, 'The event date cannot be earlier than '.date('Y-m-d', ($event_date_limitation/1000)).'.');
+                }
+            }
+        }
+    }
+
 }
