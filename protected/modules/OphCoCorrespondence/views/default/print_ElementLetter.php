cd51a36b24d5d335fe945e9e19b474fcd937bad3
Edwin <edwin@fornicode.co.uk>
2019-03-07 11:25:44 +0000

    OE-8308 - spacing adjustment

--- protected/modules/OphCoCorrespondence/views/default/print_ElementLetter.php
+++ protected/modules/OphCoCorrespondence/views/default/print_ElementLetter.php
@@ -23,7 +23,7 @@
  */
 
 if (!@$no_header) {?>
-	<header class="print-header">
+	<header class="print-header" style="margin-bottom: 70px;">
 	<?php 
         $ccString = $element->getCCString();
         $toAddress = $element->getToAddress();
