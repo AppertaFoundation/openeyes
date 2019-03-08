c55ce31c96635fee6478e9712fef6f2f15f05698
adrianbrenton <adrian.r.brenton@gmail.com>
2019-03-08 15:00:01 +0000

    OE-8341 - Add empty migration

---
+++ protected/modules/OphCiExamination/migrations/m190308_144937_rename_van_herick_thirty_percent.php
@@ -0,0 +1,25 @@
+<?php
+
+class m190308_144937_rename_van_herick_thirty_percent extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m190308_144937_rename_van_herick_thirty_percent does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
