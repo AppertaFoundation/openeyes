cc8466a1c8906e790b5c55ab00c06dd6d580a6f8
adrianbrenton <adrian.r.brenton@gmail.com>
2019-03-11 15:16:02 +0000

    OE-8341 - change van_herick 30% to 40% in database

--- protected/modules/OphCiExamination/migrations/m190308_144937_rename_van_herick_thirty_percent.php
+++ protected/modules/OphCiExamination/migrations/m190308_144937_rename_van_herick_thirty_percent.php
@@ -4,12 +4,14 @@ class m190308_144937_rename_van_herick_thirty_percent extends CDbMigration
 {
 	public function up()
 	{
+		$this->update('ophciexamination_van_herick', ['name' => 'Grade 2 (26-40%)'], 'name = "Grade 2 (26-30%)"');
+		$this->update('ophciexamination_van_herick', ['name' => 'Grade 3 (41-75%)'], 'name = "Grade 3 (31-75%)"');
 	}
 
 	public function down()
 	{
-		echo "m190308_144937_rename_van_herick_thirty_percent does not support migration down.\n";
-		return false;
+		$this->update('ophciexamination_van_herick', ['name' => 'Grade 2 (26-30%)'],  'name = "Grade 2 (26-40%)"');
+		$this->update('ophciexamination_van_herick', ['name' => 'Grade 3 (31-75%)'],  'name = "Grade 3 (41-75%)"');
 	}
 
 	/*
