c328d1cf8abbd376eea24a539331d168ba51cec7
neilsf <feketecsaba@gmail.com>
2019-03-19 14:27:53 +0100

    OE-7023 Removed console logging

--- protected/modules/OphDrPrescription/assets/js/allergicDrugs.js
+++ protected/modules/OphDrPrescription/assets/js/allergicDrugs.js
@@ -33,7 +33,6 @@ OpenEyes.OphDrPrescription = OpenEyes.OphDrPrescription || {};
     };
 
     AllergicDrugsController.prototype.fillAllergicDrugsAndAllergiesArray = function (drug) {
-        console.log(drug['allergies'], this.patientAllergies);
         if(drug['allergies'] === "") {
             return;
         }
