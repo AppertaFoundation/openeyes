15a07d0bbcb995c459159858e209b4b23f07e632
Stefan Toncu <stefan.toncu29@gmail.com>
2019-03-21 14:19:20 +0000

    feature OE-8080 - add historyIOP widget element and model

---
+++ protected/modules/OphCiExamination/migrations/m190321_114813_create_history_iop_element.php
@@ -0,0 +1,25 @@
+<?php
+
+class m190321_114813_create_history_iop_element extends OEMigration
+{
+    public function up()
+    {
+        $this->createOETable('et_ophciexamination_history_iop', array(
+            'id' => 'pk',
+            'event_id' => 'INT(10) UNSIGNED NOT NULL DEFAULT 0',
+            'eye_id' => "int(10) unsigned NOT NULL DEFAULT '3'",
+        ), true);
+        $this->addForeignKey('et_ophciexamination_history_iop_ev_fk', 'et_ophciexamination_history_iop', 'event_id', 'event', 'id');
+        $this->addForeignKey('et_ophciexamination_history_iop_eye_fk', 'et_ophciexamination_history_iop', 'eye_id', 'eye', 'id');
+        $this->createElementType('OphCiExamination', 'History IOP', array(
+            'class_name' => 'OEModule\OphCiExamination\models\HistoryIOP',
+            'display_order' => 1
+        ));
+    }
+    public function down()
+    {
+        $this->delete('element_type', 'class_name = :class_name', array(':class_name' => 'OEModule\OphCiExamination\models\HistoryIOP'));
+        $this->dropOETable('et_ophciexamination_history_iop_version');
+        $this->dropOETable('et_ophciexamination_history_iop');
+    }
+}
