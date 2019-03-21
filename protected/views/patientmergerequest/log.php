7e67d38fa6e82439fee071d2ca03004fb7dae4a9
Zhaolian-Zhou <zhaolian.zhou@fivium.com.au>
2019-03-21 12:45:09 +1100

    CERA 284, patient merge page UI fix (#2928)

--- protected/views/patientmergerequest/log.php
+++ protected/views/patientmergerequest/log.php
@@ -4,27 +4,30 @@
 
 ?>
 
-<div id="patientMergeWrapper" class="container content">
+<div id="patientMergeWrapper" class="container content main-event">
+    <div class="element-fields full-width flex-layout flex-top col-gap element">
 
-  <div class="cols-4 column large-centered text-right large-offset-8">
-    <section class="box dashboard">
-        <?php
-        echo CHtml::link('Back to Patient Merge list', array('patientMergeRequest/index'),
-                array('class' => 'button small')) . ' ';
-        echo CHtml::link('add', array('patientMergeRequest/create'), array('class' => 'button small'));
-        ?>
-    </section>
-  </div>
+        <div class="cols-4">
+            <section class="box dashboard">
+                <?php
+                echo CHtml::link('Back to Patient Merge list', array('patientMergeRequest/index'),
+                        array('class' => 'button small')) . ' ';
+                echo CHtml::link('add', array('patientMergeRequest/create'), array('class' => 'button small'));
+                ?>
+            </section>
+        </div>
+    </div>
 
-  <div class="cols-8 column large-centered">
-      <?php $this->widget('zii.widgets.grid.CGridView', array(
-          'itemsCssClass' => 'standard',
-          'dataProvider' => $data_provider,
-          'summaryText' => '<h3><small> {start}-{end} of {count} </small></h3>',
-          'htmlOptions' => array('id' => 'patientMergeList'),
-          'columns' => array('log'),
-      )); ?>
+    <div class="cols-8">
+        <?php $this->widget('zii.widgets.grid.CGridView', array(
+            'itemsCssClass' => 'standard',
+            'dataProvider' => $data_provider,
+            'summaryText' => '<h3><small> {start}-{end} of {count} </small></h3>',
+            'htmlOptions' => array('id' => 'patientMergeList'),
+            'columns' => array('log'),
+        )); ?>
+        <?= \CHtml::activeTextArea($model, 'comment', array('disabled' => 'disabled')); ?>
+        <br>
+    </div>
 
-      <?=\CHtml::activeTextArea($model, 'comment', array('disabled' => 'disabled')); ?>
-    <br>
-  </div>
+</div>
--- protected/views/patientmergerequest/merge.php
+++ protected/views/patientmergerequest/merge.php
@@ -17,84 +17,82 @@
  */
 ?>
 
-<div id="patientMergeWrapper" class="container content">
-  <div class="cols-3 column large-centered text-right large-offset-9">
-    <section class="box dashboard">
-        <?php
-        echo CHtml::link('Back to Patient Merge list', array('patientMergeRequest/index'),
-            array('class' => 'button small'));
-        ?>
-    </section>
-  </div>
-
-  <form id="grid_header_form"
-        action="<?php echo Yii::app()->createUrl('/patientMergeRequest/merge', array('id' => $model->id)) ?>"
-        method="post">
-    <input type="hidden" name="YII_CSRF_TOKEN" value="<?php echo Yii::app()->request->csrfToken ?>"/>
-      <?=\CHtml::activeHiddenField($model, 'id') ?>
-    <div class="data-group">
-      <div class="large-5 column">
-        <h2 class="secondaryPatient">Secondary</h2>
-          <?php $this->renderPartial('//patientmergerequest/_patient_details',
-              array('model' => $model, 'type' => 'secondary')) ?>
-      </div>
-
-      <div class="cols-2 column text-center">
-        <h2>INTO</h2>
-        <img class="into-arrow"
-             src="<?= Yii::app()->assetManager->createUrl('img/_elements/graphic/right-black-arrow_128_30.png') ?>"
-             alt="OpenEyes logo"/>
-      </div>
-
-      <div class="cols-5 column">
-        <h2 class="primaryPatient">Primary</h2>
-          <?php $this->renderPartial('//patientmergerequest/_patient_details',
-              array('model' => $model, 'type' => 'primary')) ?>
-      </div>
+<div id="patientMergeWrapper" class="container content main-event">
+    <div class="cols-3 column large-centered text-right large-offset-9">
+        <section class="box dashboard">
+            <?php
+            echo CHtml::link('Back to Patient Merge list', array('patientMergeRequest/index'),
+                array('class' => 'button small'));
+            ?>
+        </section>
     </div>
-    <hr>
-    <div class="cols-5 column">Comment:
-        <?=\CHtml::activeTextArea($model, 'comment'); ?>
-    </div>
+
+    <form id="grid_header_form"
+          class="element-fields full-width col-gap"
+          action="<?php echo Yii::app()->createUrl('/patientMergeRequest/merge', array('id' => $model->id)) ?>"
+          method="post">
+        <input type="hidden" name="YII_CSRF_TOKEN" value="<?php echo Yii::app()->request->csrfToken ?>"/>
+        <?= \CHtml::activeHiddenField($model, 'id') ?>
+        <div class="cols-12 flex-layout">
+            <div class="cols-5">
+                <h2 class="secondaryPatient">Secondary</h2>
+                <?php $this->renderPartial('//patientmergerequest/_patient_details',
+                    array('model' => $model, 'type' => 'secondary')) ?>
+            </div>
+
+            <div class="cols-2" style="text-align: center">
+                <h2>INTO</h2>
+                <img class="into-arrow"
+                     src="<?= Yii::app()->assetManager->createUrl('img/_elements/graphic/right-black-arrow_128_30.png') ?>"
+                     alt="OpenEyes logo"/>
+            </div>
+
+            <div class="cols-5">
+                <h2 class="primaryPatient">Primary</h2>
+                <?php $this->renderPartial('//patientmergerequest/_patient_details',
+                    array('model' => $model, 'type' => 'primary')) ?>
+            </div>
+        </div>
+        <hr>
+        <div class="cols-12 element-fields flex-layout flex-top col-gap">
+            <div class="cols-3"></div>
+            <div class="cols-6 element">
+                Comment: <?= \CHtml::activeTextArea($model, 'comment', array('class' => 'cols-9')); ?>
+                <br>
+                <?php if ($personal_details_conflict_confirm && Yii::app()->user->checkAccess('Patient Merge')): ?>
+                    <div id="patientDataConflictConfirmation" class="data-group">
+                        <div class="cols-10 large-offset-1 column alert-box with-icon warning">
+                            <h2> Personal details are conflicting. </h2>
+                            Please confirm you selected the right patients. <br>
+                            Note: the primary patient's personal details will <strong>NOT</strong> be
+                            overwritten.<br><br>
+                            <label>
+                                <?= \CHtml::checkBox('PatientMergeRequest[personal_details_conflict_confirm]', false); ?>
+                                I hereby confirm that I selected the right patients.</label>
+                        </div>
+                    </div>
+                <?php endif; ?>
+                <?php $this->renderPartial('//base/_messages') ?>
+                <?php if (Yii::app()->user->checkAccess('Patient Merge')): ?>
+                    <label>
+                        <?= \CHtml::checkBox('PatientMergeRequest[confirm]', false); ?>
+                        I declare under penalty of perjury I reviewed the details and I would like to proceed to merge.
+                    </label>
+                    <div class="flex-layout">
+                        <div class="cols-6">
+                            <input class="warning" type="submit" value="Merge">
+                        </div>
+                        <div class="cols-6">
+                            <?= \CHtml::link('cancel', array('patientMergeRequest/index'),
+                                array('class' => 'button primary')); ?>
+                        </div>
+                    </div>
+
+                <?php endif; ?>
+            </div>
+            <div class="cols-3"></div>
+        </div>
+
+    </form>
     <br>
-      <?php if ($personal_details_conflict_confirm && Yii::app()->user->checkAccess('Patient Merge')): ?>
-        <div id="patientDataConflictConfirmation" class="data-group">
-          <div class="cols-10 large-offset-1 column alert-box with-icon warning">
-            <h2> Personal details are conflicting. </h2>
-            Please confirm you selected the right patients. <br>
-            Note, the primary patient's personal details will <strong>NOT</strong> be overwritten.<br><br>
-            <label>
-                <?=\CHtml::checkBox('PatientMergeRequest[personal_details_conflict_confirm]', false); ?> I hereby
-              confirm that I selected the right patients.</label>
-          </div>
-        </div>
-        <div class="cols-12 column text-left">
-        </div>
-      <?php endif; ?>
-      <?php $this->renderPartial('//base/_messages') ?>
-      <?php if (Yii::app()->user->checkAccess('Patient Merge')): ?>
-        <div class="data-group">
-          <div class="cols-5 column text-right large-offset-7">
-            <div class="data-group">
-              <div class="cols-9 column text-right">
-                <label>
-                    <?=\CHtml::checkBox('PatientMergeRequest[confirm]', false); ?> I declare under penalty of
-                  perjury I reviewed the details and I would like to proceed to merge.
-                </label>
-              </div>
-              <div class="cols-3 column text-right">
-                <input class="warning" type="submit" value="Merge">
-              </div>
-            </div>
-
-            <div class="cols-3 column text-right large-offset-9">
-                <?=\CHtml::link('cancel', array('patientMergeRequest/index'),
-                    array('class' => 'button primary')); ?>
-            </div>
-
-          </div>
-        </div>
-      <?php endif; ?>
-  </form>
-  <br>
 </div>
