e9d0d56f476882dd4b3c5983dac9a40563939fcd
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-03-26 13:34:21 +0000

    Change the controller for generic procedure data admin page

--- protected/modules/OphTrOperationnote/config/common.php
+++ protected/modules/OphTrOperationnote/config/common.php
@@ -28,7 +28,7 @@ return array(
             'Default Incision Length' => '/OphTrOperationnote/admin/viewIncisionLengthDefaults',
             'Operative Devices' => '/OphTrOperationnote/OperativeDevice/list',
             'Operative Devices Mapping' => '/OphTrOperationnote/OperativeDeviceMapping/list',
-            'Generic Operation Data' => '/OphTrOperationnote/GenericOperation/list',
+            'Generic Operation Data' => '/OphTrOperationnote/GenericProcedureData/list',
         ),
         'reports' => array(
             'Operations' => '/OphTrOperationnote/report/operation',
