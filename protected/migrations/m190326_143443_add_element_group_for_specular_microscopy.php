9c2984e4729501108f5947e1f3d221198a1b4fc8
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-03-26 15:00:17 +0000

    Fix migration

--- protected/migrations/m190326_143443_add_element_group_for_specular_microscopy.php
+++ protected/migrations/m190326_143443_add_element_group_for_specular_microscopy.php
@@ -4,7 +4,13 @@ class m190326_143443_add_element_group_for_specular_microscopy extends CDbMigrat
 {
     public function up()
     {
-        $element_group = ElementGroup::model()->findByAttributes(['name' => 'Anterior Segment']);
+        $event_type = EventType::model()->findByAttributes(['class_name' => 'OphCiExamination']);
+        $element_group = ElementGroup::model()->findByAttributes(
+            [
+                'name' => 'Anterior Segment',
+                'event_type_id' => $event_type->id
+            ]
+        );
         $this->update('element_type', ['element_group_id' => $element_group->id],
             'class_name = :class_name', ['class_name' => 'OEModule\OphCiExamination\models\Element_OphCiExamination_Specular_Microscopy']
         );
