830fb3533309ad893b64e43653bb5cb4a56bad2c
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-03-28 15:22:16 +0000

    Add an admin page for disorders and implement it

---
+++ protected/views/admin/form_Disorder.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * (C) OpenEyes Foundation, 2019
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.openeyes.org.uk
+ *
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (C) 2019, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
+ */
+?>
+
+<div class="cols-5">
+    <div class="row divider">
+        <h2><?php echo $title ?></h2>
+    </div>
+
+    <table class="standard cols-full">
+        <colgroup>
+            <col class="cols-3">
+            <col class="cols-5">
+        </colgroup>
+        <tbody>
+        <tr>
+            <td>Fully Specified Name</td>
+            <td class="cols-full">
+                <?= \CHtml::activeTextArea(
+                    $model,
+                    'fully_specified_name',
+                    ['class' => 'cols-full']
+                ); ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Term</td>
+            <td>
+                <?= \CHtml::activeTextArea(
+                    $model,
+                    'term',
+                    ['class' => 'cols-full']
+                ); ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Aliases</td>
+            <td>
+                <?= \CHtml::activeTextArea(
+                    $model,
+                    'aliases',
+                    ['class' => 'cols-full autosize',
+                        'style' => 'overflow: hidden; ']
+                ); ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Subspecialty</td>
+            <td>
+                <?= \CHtml::activeNumberField(
+                    $model,
+                    'specialty_id',
+                    ['class' => 'cols-full']
+                ); ?>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
