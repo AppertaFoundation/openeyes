d2c06306c63327815cea07478a4911c6929acadb
Peter Toth <peter.toth@abehr.com>
2019-03-28 14:19:22 +0000

    OE-8281 create table ophciexamination_gonioscopy_iris_configuration for storing the configuration options

---
+++ protected/modules/OphCiExamination/migrations/m190328_134435_create_iris_configuration_table.php
@@ -0,0 +1,31 @@
+<?php
+
+class m190328_134435_create_iris_configuration_table extends OEMigration
+{
+	public function up()
+	{
+    $this->createOETable('ophciexamination_gonioscopy_iris_configuration',
+      [
+      'id' => 'pk',
+      'name' => 'varchar(20) NOT NULL',
+      'display_order' => 'tinyint(3) unsigned DEFAULT \'0\'',
+      ],
+      true);
+
+    $this->insert('ophciexamination_gonioscopy_iris_configuration',
+      ['name' => 'Not recorded', 'display_order' => '0',]);
+    $this->insert('ophciexamination_gonioscopy_iris_configuration',
+      ['name' => 'Flat', 'display_order' => '1',]);
+    $this->insert('ophciexamination_gonioscopy_iris_configuration',
+      ['name' => 'Plateau', 'display_order' => '2',]);
+    $this->insert('ophciexamination_gonioscopy_iris_configuration',
+      ['name' => 'Concave', 'display_order' => '3',]);
+    $this->insert('ophciexamination_gonioscopy_iris_configuration',
+      ['name' => 'Bombé', 'display_order' => '4',]);
+	}
+
+	public function down()
+	{
+    $this->dropOETable('ophciexamination_gonioscopy_iris_configuration', true);
+	}
+}
