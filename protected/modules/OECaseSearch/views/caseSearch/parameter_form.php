afc0b41693f02f4a4a2c968fc358d04b6d611f09
ZheWangJames <46304835+ZheWangJames@users.noreply.github.com>
2019-03-29 15:16:43 +1100

    CERA-304 Added AND label to Advanced search (#2957)

--- protected/modules/OECaseSearch/views/caseSearch/parameter_form.php
+++ protected/modules/OECaseSearch/views/caseSearch/parameter_form.php
@@ -5,23 +5,40 @@
 /* @var $form CActiveForm */
 ?>
 <tr class="parameter" id="<?php echo $id; ?>">
-    <td>
-        <div class="<?php echo $model->name; ?> flex-layout"
-             style="padding-bottom: 6px; padding-top: 6px;"
-        >
-            <div class="cols-10">
-                <?php $model->renderParameter($id); ?>
-                <?php echo CHtml::activeHiddenField($model, "[$id]id"); ?>
-            </div>
-            <div class="cols-2">
-                <i id="<?= $id ?>-remove" class="oe-i trash"></i>
-            </div>
-            <hr/>
-        </div>
-    </td>
-</tr>
-<script type="text/javascript">
+  <td>
+    <div class="<?php echo $model->name; ?> flex-layout"
+         style="padding-bottom: 6px; padding-top: 6px;"
+    >
+      <div class="js-case-search-AND-label ">
+        <p class="highlighter"
+           style='margin-right: 8px;'>AND</p>
+      </div>
+      <div class="cols-10">
+          <?php $model->renderParameter($id); ?>
+          <?php echo CHtml::activeHiddenField($model, "[$id]id"); ?>
+      </div>
+      <div class="cols-2">
+        <i id="<?= $id ?>-remove" class="oe-i trash"></i>
+      </div>
+      <hr/>
+    </div>
+  </td>
+  <script type="text/javascript">
+    $(document).ready(function () {
+      $('.parameter').each(function(){
+        if($(this).index() == 0){
+          $(this).find('.js-case-search-AND-label').remove();
+        }
+      });
+    });
     $('#<?= $id?>-remove').on('click', function () {
-        this.closest('.parameter').remove();
+      if ($(this).closest('.parameter').index() == 0){
+        $('.js-case-search-AND-label').first().remove();
+      }
+      this.closest('.parameter').remove();
     })
-</script>
+  </script>
+</tr>
+
+
+
