d6958f41240d59f47d7c3de41dea839dcafdc3ce
Peter Toth <peter.toth@abehr.com>
2019-03-29 12:54:43 +0000

    OE-8281 merge of the migration files in connection with table ophciexamination_gonioscopy_iris

--- protected/modules/OphCiExamination/migrations/m190328_134435_create_iris_table.php
+++ protected/modules/OphCiExamination/migrations/m190328_134435_create_iris_table.php
@@ -19,10 +19,30 @@ class m190328_134435_create_iris_table extends OEMigration
         ['name' => 'Concave', 'display_order' => '3',],
         ['name' => 'Bombé', 'display_order' => '4',],
       ]);
+
+    $this->addColumn('et_ophciexamination_gonioscopy', 'right_iris_id', 'INT(10)');
+    $this->addColumn('et_ophciexamination_gonioscopy', 'left_iris_id', 'INT(10)');
+    $this->addColumn('et_ophciexamination_gonioscopy_version', 'right_iris_id', 'INT(10)');
+    $this->addColumn('et_ophciexamination_gonioscopy_version', 'left_iris_id', 'INT(10)');
+
+    $this->addForeignKey('et_ophciexamination_gonioscopy_right_iris_id_fk',
+      'et_ophciexamination_gonioscopy', 'right_iris_id',
+      'ophciexamination_gonioscopy_iris', 'id');
+    $this->addForeignKey('et_ophciexamination_gonioscopy_left_iris_id_fk',
+      'et_ophciexamination_gonioscopy', 'left_iris_id',
+      'ophciexamination_gonioscopy_iris', 'id');
 	}
 
 	public function down()
 	{
+    $this->dropForeignKey('et_ophciexamination_gonioscopy_left_iris_id_fk', 'et_ophciexamination_gonioscopy');
+    $this->dropForeignKey('et_ophciexamination_gonioscopy_right_iris_id_fk', 'et_ophciexamination_gonioscopy');
+
+    $this->dropColumn('et_ophciexamination_gonioscopy_version', 'left_iris_id');
+    $this->dropColumn('et_ophciexamination_gonioscopy_version', 'right_iris_id');
+    $this->dropColumn('et_ophciexamination_gonioscopy', 'left_iris_id');
+    $this->dropColumn('et_ophciexamination_gonioscopy', 'right_iris_id');
+
     $this->dropOETable('ophciexamination_gonioscopy_iris', true);
 	}
 }
