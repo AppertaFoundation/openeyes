5b0cf8cbd3dc0b49453465feb6d3587584fb6b87
Peter Toth <peter.toth@abehr.com>
2019-03-29 12:58:01 +0000

    OE-8281 small cosmetics

--- protected/modules/OphCiExamination/migrations/m190328_134435_create_iris_table.php
+++ protected/modules/OphCiExamination/migrations/m190328_134435_create_iris_table.php
@@ -4,21 +4,18 @@ class m190328_134435_create_iris_table extends OEMigration
 {
 	public function up()
 	{
-    $this->createOETable('ophciexamination_gonioscopy_iris',
-      [
-      'id' => 'pk',
-      'name' => 'varchar(50) NOT NULL',
-      'display_order' => 'tinyint(3) unsigned DEFAULT \'0\'',
-      ],
-      true);
+    $this->createOETable('ophciexamination_gonioscopy_iris', [
+        'id' => 'pk',
+        'name' => 'varchar(50) NOT NULL',
+        'display_order' => 'tinyint(3) unsigned DEFAULT \'0\'',
+    ], true);
 
-    $this->insertMultiple('ophciexamination_gonioscopy_iris',
-      [
+    $this->insertMultiple('ophciexamination_gonioscopy_iris', [
         ['name' => 'Flat', 'display_order' => '1',],
         ['name' => 'Plateau', 'display_order' => '2',],
         ['name' => 'Concave', 'display_order' => '3',],
         ['name' => 'Bombé', 'display_order' => '4',],
-      ]);
+    ]);
 
     $this->addColumn('et_ophciexamination_gonioscopy', 'right_iris_id', 'INT(10)');
     $this->addColumn('et_ophciexamination_gonioscopy', 'left_iris_id', 'INT(10)');
