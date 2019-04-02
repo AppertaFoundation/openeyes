d211f628b3acc85f1e00e430c4c71b6c14d26648
Peter Toth <peter.toth@abehr.com>
2019-04-02 16:48:21 +0100

    OE-7713 bugfix: the condition is evaluated correctly even if max_complex_procedures is set to 0

--- protected/modules/OphTrOperationbooking/views/booking/_list.php
+++ protected/modules/OphTrOperationbooking/views/booking/_list.php
@@ -26,7 +26,7 @@ if (!$reschedule) {
         if ($session->max_procedures) {
             echo ', ' . $session->getAvailableProcedureCount() . '/' . $session->max_procedures . ' procedures left';
         }
-        if($session->max_complex_procedures) {
+        if(isset($session->max_complex_procedures)) {
             echo ', ' . $session->getAvailableComplexProcedureCount() . '/' . $session->max_complex_procedures . ' complex procedures left';
         } ?>)
     </h3>
