9518e4c684851865edbecdb9ba736c760fb98026
Piyush <piyush.gupta@toukanlabs.com>
2019-04-03 12:21:24 +1100

    CERA-346 Home buttons can be navigated using tab button

--- protected/views/base/_form.php
+++ protected/views/base/_form.php
@@ -71,12 +71,12 @@ if (!Yii::app()->user->isGuest) {
                 </a>
             </li>
             <li class="oe-nav-btn js-hotlist-panel-wrapper">
-                <div class="nav-js-btn" id="js-nav-hotlist-btn"
+                <a href="/" class="nav-js-btn icon-btn" id="js-nav-hotlist-btn" onclick="returnFalse()"
                      data-fixable="<?= $this->fixedHotlist ? 'true' : 'false' ?>">
                     <svg viewBox="0 0 80 40" class="icon hotlist">
                         <use xlink:href="<?= $navIconUrl . '#hotlist-icon' ?>"></use>
                     </svg>
-                </div>
+                </a>
                 <?php $this->renderPartial('//base/_hotlist'); ?>
             </li>
             <li class="oe-nav-btn">
