fb4c3ec2d214c34ebd96480379b1e91e77d2e8b9
zhaolian-Zhou <zhaolian.zhou@toukanlabs.com>
2019-04-04 11:03:30 +1100

    CERA-348, expand practice name length and change field style

--- protected/views/practice/_form.php
+++ protected/views/practice/_form.php
@@ -27,7 +27,7 @@ $address_type_ids = CHtml::listData(AddressType::model()->findAll(), 'id', 'name
                 <?php echo $form->labelEx($contact, 'first_name'); ?>
             </td>
             <td>
-                <?php echo $form->textField($contact, 'first_name', array('size' => 30, 'maxlength' => 50)); ?>
+                <?php echo $form->textArea($contact, 'first_name', array('size' => 30, 'maxlength' => 300)); ?>
                 <?php echo $form->error($contact, 'first_name'); ?>
             </td>
         </tr>
