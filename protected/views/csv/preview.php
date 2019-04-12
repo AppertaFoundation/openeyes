7616406442ab96361e2d6b07605c02755bc6ad07
zhaolian-Zhou <zhaolian.zhou@toukanlabs.com>
2019-04-12 14:49:18 +1000

    CERA-367, fix upload trial and trial patient UI

--- protected/views/csv/preview.php
+++ protected/views/csv/preview.php
@@ -1,35 +1,51 @@
-<?php
-$form = $this->beginWidget(
-    'CActiveForm',
-    array(
-        'id' => 'import-form',
-        'action' => Yii::app()->createURL('csv/import', array('context' => $context)),
-        'enableAjaxValidation' => false,
-        'htmlOptions' => array('enctype' => 'multipart/form-data'),
-    )
-);
-if (!empty($table)): ?>
-  <div style="overflow: auto">
-    <table>
-      <tr>
-          <?php foreach (array_keys($table[0]) as $header): ?>
-            <th>
-                <?php echo $header; ?>
-            </th>
-          <?php endforeach; ?>
-      </tr>
-        <?php foreach ($table as $row): ?>
-          <tr>
-              <?php foreach ($row as $column): ?>
-                <td>
-                    <?php echo $column ?>
-                </td>
-              <?php endforeach; ?>
-          </tr>
-        <?php endforeach; ?>
-    </table>
-  </div>
-<?php endif;
-echo CHtml::submitButton('Import');
-$this->endWidget();
-?>
+<main class="oe-home">
+    <div class="oe-full-header flex-layout">
+        <div class="title wordcaps">Upload</div>
+    </div>
+    <div class="oe-full-content  flex-top">
+        <div class="errorSummary">
+            <?php
+            if (isset($errors) and $errors !== null) {
+                echo '<pre>';
+                echo ArrayHelper::array_dump_html($errors);
+                echo '</pre>';
+            }
+            ?>
+        </div>
+
+        <?php $form = $this->beginWidget(
+            'CActiveForm',
+            array(
+                'id' => 'import-form',
+                'action' => Yii::app()->createURL('csv/import', array('context' => $context)),
+                'enableAjaxValidation' => false,
+                'htmlOptions' => array('enctype' => 'multipart/form-data'),
+            )
+        );
+        if (!empty($table)): ?>
+            <div style="overflow: auto">
+                <table>
+                    <tr>
+                        <?php foreach (array_keys($table[0]) as $header): ?>
+                            <th>
+                                <?php echo $header; ?>
+                            </th>
+                        <?php endforeach; ?>
+                    </tr>
+                    <?php foreach ($table as $row): ?>
+                        <tr>
+                            <?php foreach ($row as $column): ?>
+                                <td>
+                                    <?php echo $column ?>
+                                </td>
+                            <?php endforeach; ?>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+        <?php endif;
+        echo CHtml::submitButton('Import');
+        $this->endWidget();
+        ?>
+    </div>
+</main>
