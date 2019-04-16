1b50ea8f67877fd038984b6a19702b77e6f74354
Peter Toth <peter.toth@abehr.com>
2019-04-16 16:33:24 +0100

    OE-7110 add colour options to admin setting OphCiExamination_default_iris_colour

--- protected/modules/OphCiExamination/migrations/m190411_153415_add_system_setting_for_default_iris_colour.php
+++ protected/modules/OphCiExamination/migrations/m190411_153415_add_system_setting_for_default_iris_colour.php
@@ -8,6 +8,7 @@ class m190411_153415_add_system_setting_for_default_iris_colour extends CDbMigra
       'field_type_id' => SettingFieldType::model()->find('name = ?' , ["Dropdown list"])->id,
       'key' => 'OphCiExamination_default_iris_colour',
       'name' => 'Examination Default Iris Colour',
+      'data' => serialize(['Blue' => 'Blue', 'Brown' => 'Brown', 'Gray' => 'Gray', 'Green' => 'Green']),
       'default_value' => 'Blue',
     ]);
     $this->insert('setting_installation', [
