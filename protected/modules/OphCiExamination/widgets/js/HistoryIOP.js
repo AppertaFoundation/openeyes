212e7e0f949bfb7d3a56f73e7122a4e675009f04
Stefan Toncu <stefan.toncu29@gmail.com>
2019-04-17 13:22:20 +0100

    feature/OE-8080 - remove duplicate code

--- protected/modules/OphCiExamination/widgets/js/HistoryIOP.js
+++ protected/modules/OphCiExamination/widgets/js/HistoryIOP.js
@@ -14,22 +14,3 @@ $(document).ready(function () {
     $("#OEModule_OphCiExamination_models_HistoryIOP_readings_right").on("click", "i.trash", null, deleteReading);
     $("#OEModule_OphCiExamination_models_HistoryIOP_readings_left").on("click", "i.trash", null, deleteReading);
 });
-
-function getScaleDropdownHistoryIOP(element_name, instrument_id, scale_td, index, side){
-    $.ajax({
-        'type': 'GET',
-        'url': baseUrl + '/OphCiExamination/default/getScaleForInstrument?name=' + element_name +
-            '&instrument_id=' + instrument_id + '&side=' + side + '&index=' + index,
-        'success': function (html) {
-            if (html.length > 0) {
-                scale_td.html(html);
-                scale_td.show();
-                scale_td.prev('td').hide();
-            } else {
-                scale_td.html('');
-                scale_td.hide();
-                scale_td.prev('td').show();
-            }
-        }
-    });
-}
--- protected/modules/OphCiExamination/widgets/views/HistoryIOP_event_edit.php
+++ protected/modules/OphCiExamination/widgets/views/HistoryIOP_event_edit.php
@@ -15,6 +15,8 @@
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 
+$url = Yii::app()->assetManager->getPublishedPathOfAlias('application.modules.OphCiExamination.assets');
+\Yii::app()->clientScript->registerScriptFile($url . '/js/IntraocularPressure.js');
 
 ?>
 <script type="text/javascript" src="<?= $this->getJsPublishedPath('HistoryIOP.js') ?>"></script>
