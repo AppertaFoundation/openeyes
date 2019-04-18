8127527dff4d9000a7fd84ceb69f7df5e5447b5a
biskyt <biskyt@users.noreply.github.com>
2019-04-18 17:28:28 +0100

    OE-8248 - renamed menu entry

--- protected/modules/OphTrOperationnote/config/common.php
+++ protected/modules/OphTrOperationnote/config/common.php
@@ -28,7 +28,7 @@ return array(
             'Default Incision Length' => '/OphTrOperationnote/admin/viewIncisionLengthDefaults',
             'Operative Devices' => '/OphTrOperationnote/OperativeDevice/list',
             'Operative Devices Mapping' => '/OphTrOperationnote/OperativeDeviceMapping/list',
-            'Generic Operation Data' => '/OphTrOperationnote/GenericProcedureData/list',
+            'Generic Operation Default Comments' => '/OphTrOperationnote/GenericProcedureData/list',
         ),
         'reports' => array(
             'Operations' => '/OphTrOperationnote/report/operation',
