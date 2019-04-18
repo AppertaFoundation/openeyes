2539775970335d1e08afa7648791062578c53651
Zhe-Wang <zhe.wang@toukanlabs.com>
2019-04-18 15:56:49 +1000

    CERA-378 Fix cannot upload patient under trial

--- protected/components/Helper.php
+++ protected/components/Helper.php
@@ -492,4 +492,14 @@ class Helper
 
         return $eye_id;
     }
+    public static function array_dump_html(array $data){
+        $return_info = "";
+        foreach ($data as $key => $value){
+            if (is_array($value)){
+                $value = Helper::array_dump_html($value);
+            }
+            $return_info .= $value.'<br>';
+        }
+        return $return_info;
+    }
 }
