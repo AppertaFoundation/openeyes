f1676312a929469b42d6cbb82a68ab7f0372f211
Peter Toth <peter.toth@abehr.com>
2019-04-29 14:37:43 +0100

    OE-8080 change Examination->IOP History element name to 'IOP History'

--- protected/modules/OphCiExamination/migrations/m190321_114813_create_history_iop_element.php
+++ protected/modules/OphCiExamination/migrations/m190321_114813_create_history_iop_element.php
@@ -12,20 +12,20 @@ class m190321_114813_create_history_iop_element extends OEMigration
         $this->addForeignKey('et_ophciexamination_history_iop_ev_fk', 'et_ophciexamination_history_iop', 'event_id', 'event', 'id');
         $this->addForeignKey('et_ophciexamination_history_iop_eye_fk', 'et_ophciexamination_history_iop', 'eye_id', 'eye', 'id');
         $this->insert('element_group', array(
-            'name' => 'HistoryIOP',
+            'name' => 'IOP History',
             'event_type_id' => $this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name = ?', array('OphCiExamination'))->queryScalar(),
             'display_order' => 65,
         ));
-        $this->createElementType('OphCiExamination', 'History IOP', [
+        $this->createElementType('OphCiExamination', 'IOP History', [
             'class_name' => 'OEModule\OphCiExamination\models\HistoryIOP',
             'display_order' => 310,
-            'group_name' => 'HistoryIOP'
+            'group_name' => 'IOP History'
         ]);
     }
     public function safeDown()
     {
         $this->delete('element_type', 'class_name = :class_name', [':class_name' => 'OEModule\OphCiExamination\models\HistoryIOP']);
-        $this->delete('element_group', 'name = :element_group_name', [':element_group_name' => 'HistoryIOP']);
+        $this->delete('element_group', 'name = :element_group_name', [':element_group_name' => 'IOP History']);
         $this->dropOETable('et_ophciexamination_history_iop', true);
     }
 }
