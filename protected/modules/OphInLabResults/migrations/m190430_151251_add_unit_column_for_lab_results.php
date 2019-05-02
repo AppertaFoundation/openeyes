2737732836fab84485d5bf2ac1dc4b44c4d990d2
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-05-02 11:07:03 +0100

    OE-6708 Lab results migrations

--- protected/modules/OphInLabResults/migrations/m190430_151251_add_unit_column_for_lab_results.php
+++ protected/modules/OphInLabResults/migrations/m190430_151251_add_unit_column_for_lab_results.php
@@ -4,8 +4,8 @@ class m190430_151251_add_unit_column_for_lab_results extends CDbMigration
 {
 	public function up()
 	{
-	    $this->addColumn('et_ophinlabresults_result_timed_numeric', 'unit', 'varchar(10)');
-	    $this->addColumn('et_ophinlabresults_result_timed_numeric_version', 'unit', 'varchar(10)');
+	    $this->addColumn('et_ophinlabresults_result_timed_numeric', 'unit', 'varchar(30)');
+	    $this->addColumn('et_ophinlabresults_result_timed_numeric_version', 'unit', 'varchar(30)');
 	}
 
 	public function down()
---
+++ protected/modules/OphInLabResults/migrations/m190501_154249_migrate_inr_to_new_format.php
@@ -0,0 +1,20 @@
+<?php
+
+class m190501_154249_migrate_inr_to_new_format extends CDbMigration
+{
+	public function up()
+	{
+	    $number_type_field = OphInLabResults_Field_Type::model()->find('name = "Numeric Field"');
+
+	    $this->update('ophinlabresults_type' ,
+            ['min_range' => 0.1 , 'max_range' => 50, 'show_on_whiteboard' => 1 , 'field_type_id' => $number_type_field->id],
+            'type = :type' , [':type' => 'INR']
+        );
+	}
+
+	public function down()
+	{
+		echo "m190501_154249_migrate_inr_to_new_format does not support migration down.\n";
+		return false;
+	}
+}
