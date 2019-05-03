b783e02a3aebf09101f33c75bab7aa6cb4fdde03
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-05-03 09:16:15 +0100

    Migrations and model for on hold reason and add on hold status and comments to booking element

---
+++ protected/modules/OphTrOperationbooking/migrations/m190502_125723_create_operation_booking_on_hold_reasons.php
@@ -0,0 +1,43 @@
+<?php
+
+class m190502_125723_create_operation_booking_on_hold_reasons extends OEMigration
+{
+	public function up()
+	{
+	    // CREATE OP BOOKING ON HOLD REASONS TABLE
+        $this->createOETable('ophtroperationbooking_operation_on_hold_reason',[
+            'id' => 'pk',
+            'reason' => 'varchar(100) NOT NULL',
+            'display_order' => 'tinyint(3) unsigned DEFAULT \'0\'',
+        ],true);
+
+        $this->insertMultiple('ophtroperationbooking_operation_on_hold_reason', [
+            ['reason' => 'Uncontrolled hypertension', 'display_order' => 1],
+            ['reason' => 'Uncontrolled blood sugar', 'display_order' => 2],
+            ['reason' => 'Recent MI', 'display_order' => 3],
+            ['reason' => 'Recent CVA', 'display_order' => 4],
+            ['reason' => 'Too unwell', 'display_order' => 5],
+            ['reason' => 'Blepharitis', 'display_order' => 6],
+            ['reason' => 'Awaiting surgery elsewhere', 'display_order' => 7],
+            ['reason' => 'Under investigations', 'display_order' => 8],
+            ['reason' => 'Still under post op review - 1st eye', 'display_order' => 9],
+            ['reason' => 'Out of the country', 'display_order' => 10],
+            ['reason' => 'Bereavement', 'display_order' => 11],
+    ]);
+
+        $this->addColumn('ophtroperationbooking_operation_booking', 'on_hold_reason', 'varchar(100) NULL');
+        $this->addColumn('ophtroperationbooking_operation_booking_version', 'on_hold_reason', 'varchar(100) NULL');
+        $this->addColumn('ophtroperationbooking_operation_booking', 'on_hold_comment', 'varchar(200) NULL');
+        $this->addColumn('ophtroperationbooking_operation_booking_version', 'on_hold_comment', 'varchar(200) NULL');
+	}
+
+	public function down()
+	{
+	    $this->dropColumn('ophtroperationbooking_operation_booking', 'on_hold_comment');
+	    $this->dropColumn('ophtroperationbooking_operation_booking_version', 'on_hold_comment');
+	    $this->dropColumn('ophtroperationbooking_operation_booking', 'on_hold_reason');
+	    $this->dropColumn('ophtroperationbooking_operation_booking_version', 'on_hold_reason');
+
+	    $this->dropOETable('ophtroperationbooking_operation_on_hold_reason', true);
+	}
+}
