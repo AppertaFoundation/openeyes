976471047e7bcc4513700e67e6b40c40c74d8f57
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-05-08 13:42:48 +0100

    Remove unnecessary function

--- protected/modules/OphInLabResults/models/OphInLabResults_Field_Type.php
+++ protected/modules/OphInLabResults/models/OphInLabResults_Field_Type.php
@@ -26,16 +26,6 @@
  */
 class OphInLabResults_Field_Type extends BaseActiveRecordVersioned
 {
-    /**
-     * Returns the static model of the specified AR class.
-     *
-     * @return OphInLabResults_Field_Type the static model class
-     */
-    public static function model($className = __CLASS__)
-    {
-        return parent::model($className);
-    }
-
     public function tableName()
     {
         return 'ophinlabresults_field_type';
