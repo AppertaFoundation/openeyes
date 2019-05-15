3b7a7689e94a9d795c1355df945db46aac33c0bb
Peter Toth <peter.toth@abehr.com>
2019-05-15 13:58:56 +0100

    OE-7713 adjust code formatting

--- protected/modules/OphTrOperationbooking/migrations/m190514_145314_add_max_complex_bookings_to_ophtroperation_booking_sequence.php
+++ protected/modules/OphTrOperationbooking/migrations/m190514_145314_add_max_complex_bookings_to_ophtroperation_booking_sequence.php
@@ -4,8 +4,8 @@ class m190514_145314_add_max_complex_bookings_to_ophtroperation_booking_sequence
 {
 	public function safeUp()
 	{
-		$this->addColumn('ophtroperationbooking_operation_sequence', 'max_complex_bookings', 'tinyint DEFAULT NULL');
-    	$this->addColumn('ophtroperationbooking_operation_sequence_version', 'max_complex_bookings', 'tinyint DEFAULT NULL');
+        $this->addColumn('ophtroperationbooking_operation_sequence', 'max_complex_bookings', 'tinyint DEFAULT NULL');
+        $this->addColumn('ophtroperationbooking_operation_sequence_version', 'max_complex_bookings', 'tinyint DEFAULT NULL');
         $this->addColumn('ophtroperationbooking_operation_sequence', 'max_procedures', 'tinyint DEFAULT NULL');
         $this->addColumn('ophtroperationbooking_operation_sequence_version', 'max_procedures', 'tinyint DEFAULT NULL');
 	}
@@ -15,6 +15,6 @@ class m190514_145314_add_max_complex_bookings_to_ophtroperation_booking_sequence
         $this->dropColumn('ophtroperationbooking_operation_sequence_version', 'max_procedures');
         $this->dropColumn('ophtroperationbooking_operation_sequence', 'max_procedures');
         $this->dropColumn('ophtroperationbooking_operation_sequence_version', 'max_complex_bookings');
-		$this->dropColumn('ophtroperationbooking_operation_sequence', 'max_complex_bookings');
+        $this->dropColumn('ophtroperationbooking_operation_sequence', 'max_complex_bookings');
 	}
 }
