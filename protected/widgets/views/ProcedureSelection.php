6be50d9be60ae34fb8c6f7fcc41f52a58326d6dc
Edwin <edwin@fornicode.co.uk>
2019-05-20 14:14:25 +0100

    OE-8245 - Hide results that have already been selected

--- protected/widgets/views/ProcedureSelection.php
+++ protected/widgets/views/ProcedureSelection.php
@@ -221,8 +221,8 @@
             updateProcedureSelect(identifier);
         } else if (popped) {
             // No subsections, so we should be safe to just push it back into the list
-            $('#select_procedure_id_' + identifier).append('<option value="' + popped["id"] + '">' + popped["name"] + '</option>').removeAttr('disabled');
-            sort_selectbox($('#select_procedure_id_' + identifier));
+            $('ul.add-options.js-search-results').append('<option value="' + popped["id"] + '">' + popped["name"] + '</option>').removeAttr('disabled');
+            sort_selectbox($('ul.add-options.js-search-results'));
         }
 
         return false;
@@ -364,13 +364,13 @@
                 }
 
                 // clear out text field
-                $('#autocomplete_procedure_id_' + identifier).val('');
+                $('.js-search-autocomplete').val('');
 
                 // remove selection from the filter box
-                if ($('#select_procedure_id_' + identifier).children().length > 0) {
+                if ($('ul.add-options.js-search-results').children().length > 0) {
                     m = data.match(/<span class="value">(.*?)<\/span>/);
 
-                    $('#select_procedure_id_' + identifier).children().each(function () {
+                    $('ul.add-options.js-search-results').children().each(function () {
                         if ($(this).text() == m[1]) {
                             var id = $(this).val();
                             var name = $(this).text();
@@ -424,6 +424,15 @@
             },
             searchOptions: {
                 searchSource: '/procedure/autocomplete',
+                resultsFilter: function(results) {
+                    let items = '';
+                    $(results).each(function (index, result) {
+                        if($('#procedureList_<?=$identifier ?: ''; ?>').find('span:contains('+result+')').length === 0){
+                            items += '<li data-label="'+result+'"><span class="auto-width">'+result+'</span></li>'
+                        }
+                    });
+                    return $('ul.js-search-results').append(items);
+                }
             }
         });
 
