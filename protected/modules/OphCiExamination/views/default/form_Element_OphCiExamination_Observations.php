885a2e5bdae5c5b669d8a25ea6c616908de8448e
Stefan Toncu <stefan.toncu29@gmail.com>
2019-05-21 10:30:42 +0300

    feature/OE-8298 - restyle Observation element

--- protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_Observations.php
+++ protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_Observations.php
@@ -21,7 +21,10 @@
         <div class="cols-4 data-group">
             <table class="cols-full">
                 <colgroup>
-                    <col class="cols-4">
+                    <col class="cols-6">
+                    <col class="cols-2">
+                    <col class="cols-2">
+                    <col class="cols-2">
                 </colgroup>
                 <tbody>
                 <tr>
@@ -30,33 +33,45 @@
                             <?= $element->getAttributeLabel('blood_pressure') ?>:
                         </label>
                     </td>
-                    <td>
-                        <?= CHtml::activeTextField($element, 'blood_pressure_systolic', array('autocomplete' => Yii::app()->params['html_autocomplete'], 'style' => 'width:50px; display:inline-block;', 'placeholder' => 'mmHg', 'tabindex' => '1')); ?>
+                    <td colspan="2">
+                        <?= CHtml::activeTextField($element, 'blood_pressure_systolic',
+                            ['class' => "cols-5", 'autocomplete' => Yii::app()->params['html_autocomplete'],
+                            'style' => 'display:inline-block;', 'tabindex' => '1']); ?>
                         /
-                        <?= CHtml::activeTextField($element, 'blood_pressure_diastolic', array('autocomplete' => Yii::app()->params['html_autocomplete'], 'style' => 'width:50px; display:inline-block;', 'placeholder' => 'mmHg', 'tabindex' => '2')); ?>
+                        <?= CHtml::activeTextField($element, 'blood_pressure_diastolic',
+                            ['class' => "cols-5", 'autocomplete' => Yii::app()->params['html_autocomplete'],
+                            'style' => 'display:inline-block;', 'tabindex' => '2']); ?>
                     </td>
+                    <td>mmHg/mmHg</td>
                 </tr>
                 <tr>
-                    <td>
+                    <td colspan="2">
                         <label for="<?= CHtml::modelName($element) . '_blood_glucose'; ?>">
                             <?= $element->getAttributeLabel('blood_glucose') ?>:
                         </label>
                     </td>
                     <td>
-                        <?= CHtml::activeTextField($element, 'blood_glucose', array('autocomplete' => Yii::app()->params['html_autocomplete'], 'placeholder' => 'mmol/l', 'tabindex' => '5')); ?>
+                        <?= CHtml::activeTextField($element, 'blood_glucose',
+                            ['class' => "cols-full", 'autocomplete' => Yii::app()->params['html_autocomplete'],
+                            'tabindex' => '5']); ?>
                     </td>
+                    <td>mmol/l</td>
                 </tr>
                 <tr>
-                    <td>
+                    <td colspan="2">
                         <label for="<?= CHtml::modelName($element) . '_weight'; ?>">
                             <?= $element->getAttributeLabel('weight') ?>:
                         </label>
                     </td>
                     <td>
                         <div class="bmi-keyup-event">
-                            <?= CHtml::activeTextField($element, 'weight', array('autocomplete' => Yii::app()->params['html_autocomplete'], 'placeholder' => 'kg', 'tabindex' => '7')); ?>
+                            <?= CHtml::activeTextField($element, 'weight',
+                                ['class' => "cols-full", 'autocomplete' => Yii::app()->params['html_autocomplete'],
+                                'tabindex' => '7']); ?>
                         </div>
                     </td>
+                    <td>kg</td>
+
                 </tr>
                 </tbody>
             </table>
@@ -64,18 +79,21 @@
         <div class="cols-4">
             <table class="cols-full">
                 <colgroup>
-                    <col class="cols-4">
+                    <col class="cols-8">
+                    <col class="cols-2">
+                    <col class="cols-2">
                 </colgroup>
                 <tbody>
                 <tr>
                     <td>
                         <label for="<?= CHtml::modelName($element) . '_o2_sat'; ?>">
-                            <?= $element->getAttributeLabel('o2_sat') ?>:
+                            O<sub>2</sub> Sat (air):
                         </label>
                     </td>
                     <td>
-                        <?= CHtml::activeTextField($element, 'o2_sat', array('autocomplete' => Yii::app()->params['html_autocomplete'], 'placeholder' => '%', 'tabindex' => '3')); ?>
+                        <?= CHtml::activeTextField($element, 'o2_sat', ['class' => "cols-full", 'autocomplete' => Yii::app()->params['html_autocomplete'], 'tabindex' => '3']); ?>
                     </td>
+                    <td>%</td>
                 </tr>
                 <tr>
                     <td>
@@ -84,8 +102,9 @@
                         </label>
                     </td>
                     <td>
-                        <?= CHtml::activeTextField($element, 'hba1c', array('autocomplete' => Yii::app()->params['html_autocomplete'], 'placeholder' => 'mmol/mol', 'tabindex' => '6')); ?>
+                        <?= CHtml::activeTextField($element, 'hba1c', ['class' => "cols-full", 'autocomplete' => Yii::app()->params['html_autocomplete'], 'tabindex' => '6']); ?>
                     </td>
+                    <td>mmol/mol</td>
                 </tr>
                 <tr>
                     <td>
@@ -95,9 +114,12 @@
                     </td>
                     <td>
                         <div class="bmi-keyup-event">
-                            <?= CHtml::activeTextField($element, 'height', array('autocomplete' => Yii::app()->params['html_autocomplete'], 'placeholder' => 'cm', 'tabindex' => '8')); ?>
+                            <?= CHtml::activeTextField($element, 'height',
+                                ['class' => "cols-full", 'autocomplete' => Yii::app()->params['html_autocomplete'],
+                                'tabindex' => '8']); ?>
                         </div>
                     </td>
+                    <td>cm</td>
                 </tr>
 
                 </tbody>
@@ -106,8 +128,9 @@
         <div class="cols-4">
             <table class="cols-full">
                 <colgroup>
-                    <col class="cols-4">
-                </colgroup>
+                    <col class="cols-8">
+                    <col class="cols-2">
+                    <col class="cols-2">                </colgroup>
                 <tbody>
                 <tr>
                     <td>
@@ -116,11 +139,14 @@
                         </label>
                     </td>
                     <td>
-                        <?= CHtml::activeTextField($element, 'pulse', array('autocomplete' => Yii::app()->params['html_autocomplete'], 'placeholder' => 'BPM', 'tabindex' => '4')); ?>
+                        <?= CHtml::activeTextField($element, 'pulse',
+                        ['class' => "cols-full", 'autocomplete' => Yii::app()->params['html_autocomplete'],
+                        'tabindex' => '4']); ?>
                     </td>
+                    <td>BPM</td>
                 </tr>
                 <tr>
-                    <td>
+                    <td colspan="2">
                         <label>
                             BMI:
                         </label>
@@ -145,25 +171,35 @@
         var weightElement = $("#OEModule_OphCiExamination_models_Element_OphCiExamination_Observations_weight");
         height = heightElement.val();
         weight = weightElement.val();
-        getBMI(height, weight);
+        getAndSetBMI(height, weight);
 
         $('.bmi-keyup-event input[type="text"]').keyup(function () {
             height = heightElement.val();
             weight = weightElement.val();
-            getBMI(height, weight);
+            getAndSetBMI(height, weight);
         });
 
-        function getBMI(height, weight) {
+        function getAndSetBMI(height, weight) {
+            let $bmiContainer = $('#bmi-container');
             bmi = 0;
+            let red = "0%";
+            let green = "80%";
             if ((height > 0) && (weight > 0)) {
                 bmi = bmi_calculator(weight, height);
                 result = bmi.toFixed(2) || 'N/A';
+                let resultFloat = parseFloat(result);
+                console.log(typeof parseFloat(result));
+                if (resultFloat < 18.5 || resultFloat >= 30) {
+                    red = "80%";
+                    green = "0%";
+                }
             } else {
                 result = 'N/A';
             }
-            $('#bmi-container').text(result);
+            $bmiContainer.text(result);
+            // console.log($bmiContainer.closest("tr"));
+            $bmiContainer.closest("tr").css("background-color", "rgb("+red+", "+green+", 0%)");
         }
-
     });
 </script>
 
