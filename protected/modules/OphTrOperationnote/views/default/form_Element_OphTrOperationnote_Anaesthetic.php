0e3e2180542db6a99a64db497dd157bf30ac2a3a
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-06-03 15:38:17 +0100

    Use anaesthetic_type instead of assignments as when we're using a booked operation the anaesthetic is coming from there and also it's using a relation with he assignments

--- protected/modules/OphTrOperationnote/views/default/form_Element_OphTrOperationnote_Anaesthetic.php
+++ protected/modules/OphTrOperationnote/views/default/form_Element_OphTrOperationnote_Anaesthetic.php
@@ -20,13 +20,13 @@
 <?php
 $la_hidden = false;
 $sed_hidden = false;
-if (count($element->anaesthetic_type_assignments) == 0) {
+if (count($element->anaesthetic_type) == 0) {
   $la_hidden = true;
   $sed_hidden = true;
-} else if (count($element->anaesthetic_type_assignments) == 1 && ($element->anaesthetic_type_assignments[0]->anaesthetic_type->code == 'GA' || $element->anaesthetic_type_assignments[0]->anaesthetic_type->code == 'NoA')) {
+} else if (count($element->anaesthetic_type) == 1 && ($element->anaesthetic_type[0]->code == 'GA' || $element->anaesthetic_type[0]->code == 'NoA')) {
   $la_hidden = true;
   $sed_hidden = true;
-} else if(count($element->anaesthetic_type_assignments) == 1 && $element->anaesthetic_type_assignments[0]->anaesthetic_type->code === 'Sed'){
+} else if(count($element->anaesthetic_type) == 1 && $element->anaesthetic_type[0]->code === 'Sed'){
   $la_hidden = true;
 }
 ?>
