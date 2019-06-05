8735d49970abc2db419be641d50d533ca8c76b2d
Mike Pargeter <mike.pargeter@abehr.com>
2019-06-05 14:37:43 +0100

    OE-8609 Fixes script to match order in ticket, and adds updates for element_group to re-order the LHS nav.

--- protected/modules/OphCiExamination/migrations/m190605_085113_adjust_IOP_History_display_order.php
+++ protected/modules/OphCiExamination/migrations/m190605_085113_adjust_IOP_History_display_order.php
@@ -2,23 +2,32 @@
 
 class m190605_085113_adjust_IOP_History_display_order extends CDbMigration
 {
-	public function up()
-	{
-	    $this->update('element_type' , ['display_order' => 305],'class_name =:class_name',
+    public function up()
+    {
+        $this->update('element_type', ['display_order' => 310], 'class_name = :class_name',
             [':class_name' => 'OEModule\OphCiExamination\models\HistoryIOP']);
-        $this->update('element_type' , ['display_order' => 300],'class_name =:class_name',
+        $this->update('element_type', ['display_order' => 300], 'class_name = :class_name',
             [':class_name' => 'OEModule\OphCiExamination\models\Element_OphCiExamination_Dilation']);
-        $this->update('element_type' , ['display_order' => 310],'class_name =:class_name',
+        $this->update('element_type', ['display_order' => 305], 'class_name = :class_name',
             [':class_name' => 'OEModule\OphCiExamination\models\Element_OphCiExamination_IntraocularPressure']);
-	}
 
-	public function down()
-	{
-        $this->update('element_type' , ['display_order' => 310],'class_name =:class_name',
+        $this->update('element_group', ['display_order' => 70], 'name =:name', [':name' => 'IOP History']);
+        $this->update('element_group', ['display_order' => 60], 'name =:name', [':name' => 'Drops']);
+        $this->update('element_group', ['display_order' => 65], 'name =:name', [':name' => 'Intraocular Pressure']);
+    }
+
+    public function down()
+    {
+        $this->update('element_type', ['display_order' => 310], 'class_name = :class_name',
             [':class_name' => 'OEModule\OphCiExamination\models\HistoryIOP']);
-        $this->update('element_type' , ['display_order' => 310],'class_name =:class_name',
+        $this->update('element_type', ['display_order' => 310], 'class_name = :class_name',
             [':class_name' => 'OEModule\OphCiExamination\models\Element_OphCiExamination_Dilation']);
-        $this->update('element_type' , ['display_order' => 300],'class_name =:class_name',
+        $this->update('element_type', ['display_order' => 300], 'class_name = :class_name',
             [':class_name' => 'OEModule\OphCiExamination\models\Element_OphCiExamination_IntraocularPressure']);
-	}
+
+        $this->update('element_group', ['display_order' => 65], 'name =:name', [':name' => 'IOP History']);
+        $this->update('element_group', ['display_order' => 70], 'name =:name', [':name' => 'Drops']);
+        $this->update('element_group', ['display_order' => 60], 'name =:name', [':name' => 'Intraocular Pressure']);
+    }
+
 }
