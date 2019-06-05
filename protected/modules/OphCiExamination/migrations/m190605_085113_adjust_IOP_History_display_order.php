f484a64317407b1a6226892749b27c8b9f7fc727
Justin <justinas.cepkauskas@gmail.com>
2019-06-05 10:25:22 +0100

    OE-8609 Adjust display order of IOP History, drops , intraocular pressure

--- protected/modules/OphCiExamination/migrations/m190605_085113_adjust_IOP_History_display_order.php
+++ protected/modules/OphCiExamination/migrations/m190605_085113_adjust_IOP_History_display_order.php
@@ -6,11 +6,19 @@ class m190605_085113_adjust_IOP_History_display_order extends CDbMigration
 	{
 	    $this->update('element_type' , ['display_order' => 305],'class_name =:class_name',
             [':class_name' => 'OEModule\OphCiExamination\models\HistoryIOP']);
+        $this->update('element_type' , ['display_order' => 300],'class_name =:class_name',
+            [':class_name' => 'OEModule\OphCiExamination\models\Element_OphCiExamination_Dilation']);
+        $this->update('element_type' , ['display_order' => 310],'class_name =:class_name',
+            [':class_name' => 'OEModule\OphCiExamination\models\Element_OphCiExamination_IntraocularPressure']);
 	}
 
 	public function down()
 	{
         $this->update('element_type' , ['display_order' => 310],'class_name =:class_name',
             [':class_name' => 'OEModule\OphCiExamination\models\HistoryIOP']);
+        $this->update('element_type' , ['display_order' => 310],'class_name =:class_name',
+            [':class_name' => 'OEModule\OphCiExamination\models\Element_OphCiExamination_Dilation']);
+        $this->update('element_type' , ['display_order' => 300],'class_name =:class_name',
+            [':class_name' => 'OEModule\OphCiExamination\models\Element_OphCiExamination_IntraocularPressure']);
 	}
 }
