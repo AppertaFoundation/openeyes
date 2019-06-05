6cdfe697c36022094ba5a58eca8a8ec91b5a79ff
harpi89 <horvatharpad89@gmail.com>
2019-06-05 10:00:00 +0200

    OE-8452: add view, model, form

---
+++ protected/modules/OphCiExamination/models/Element_OphCiExamination_AccessibleInformationStandards.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * OpenEyes.
+ *
+ * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
+ * (C) OpenEyes Foundation, 2011-2013
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.openeyes.org.uk
+ *
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
+ */
+
+namespace OEModule\OphCiExamination\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "et_ophciexamination_history".
+ *
+ * The followings are the available columns in table:
+ *
+ * @property string $id
+ * @property int $event_id
+ * @property string $description
+ *
+ * The followings are the available model relations:
+ */
+class Element_OphCiExamination_AccessibleInformationStandards extends \BaseEventTypeElement
+{
+    public $service;
+
+    /**
+     * Returns the static model of the specified AR class.
+     *
+     * @return the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'et_ophciexamination_accessible_information_standards';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+                array('correspondence_in_large_letters', 'safe'),
+                array('correspondence_in_large_letters', 'required'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, event_id, correspondence_in_large_letters,anticoagulant ', 'safe', 'on' => 'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+                'eventType' => array(self::BELONGS_TO, 'EventType', 'event_type_id'),
+                'event' => array(self::BELONGS_TO, 'Event', 'event_id'),
+                'user' => array(self::BELONGS_TO, 'User', 'created_user_id'),
+                'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
+        );
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+                'id' => 'ID',
+                'event_id' => 'Event',
+                'correspondence_in_large_letters' => 'Correspondence in large letters',
+        );
+    }
+
+    /**
+     * Set default values for forms on create.
+     */
+    public function setDefaultOptions(\Patient $patient = null)
+    {
+        
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
+
+        $criteria = new \CDbCriteria();
+
+        $criteria->compare('id', $this->id, true);
+        $criteria->compare('event_id', $this->event_id, true);
+
+        $criteria->compare('correspondence_in_large_letters', $this->correspondence_in_large_letters);
+
+        return new \CActiveDataProvider(get_class($this), array(
+                'criteria' => $criteria,
+        ));
+    }
+
+    public function getLetter_string()
+    {
+        return "Correspondence in large letters: $this->correspondence_in_large_letters\n";
+    }
+
+    public function canCopy()
+    {
+        return true;
+    }
+}
