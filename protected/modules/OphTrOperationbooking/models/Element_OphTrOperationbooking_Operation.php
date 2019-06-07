e6d0c8ec7a574d5cad56eb06a0c01fdcaaaec8cd
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-06-07 15:27:00 +0100

    OE-8631 Make pre-assessment booking required a required field

--- protected/modules/OphTrOperationbooking/models/Element_OphTrOperationbooking_Operation.php
+++ protected/modules/OphTrOperationbooking/models/Element_OphTrOperationbooking_Operation.php
@@ -146,7 +146,7 @@ class Element_OphTrOperationbooking_Operation extends BaseEventTypeElement
             array('total_duration', 'validateDuration'),
             array('referral_id', 'validateReferral'),
             array('decision_date', 'OEDateValidatorNotFuture'),
-            array('eye_id, consultant_required, overnight_stay_required_id', 'required'),
+            array('eye_id, consultant_required, overnight_stay_required_id, preassessment_booking_required', 'required'),
             array('anaesthetic_choice_id, stop_medication, complexity', 'required', 'on' => 'insert'),
             array('stop_medication_details', 'RequiredIfFieldValidator', 'field' => 'stop_medication', 'value' => true),
             array('site_id, priority_id, decision_date', 'required'),
