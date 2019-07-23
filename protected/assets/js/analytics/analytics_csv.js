0ef1e9f702761304ab7ce94e1652753375be33c9
Daniel <dwgthomson@gmail.com>
2019-07-23 11:56:33 +1000

    Removed a console log that was previously left in
    removed this for cleanliness

--- protected/assets/js/analytics/analytics_csv.js
+++ protected/assets/js/analytics/analytics_csv.js
@@ -52,7 +52,6 @@ function current_custom_data_to_csv(additional_type,anonymized=false){
         var csv_file = "First Name, Second Name, Hos Num, DOB, Age, Diagnoses, VA-L, "+additional_type+"-L, VA-R,"+additional_type+"-R\n";
     }
     var data = Object.values(window.csv_data_for_report['custom_data']);
-    console.log(data);
     var file_name = "clinical_data";
     data.forEach(function (item) {
         let item_patient = $('#'+item['patient_id']);
