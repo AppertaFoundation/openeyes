2f75a9d3cc125ad2b1fd90e75a83b6c88563b752
Daniel <dwgthomson@gmail.com>
2019-07-29 15:38:01 +1000

    OE-8330 Got the click on graph to kinda work, now to wok on making it elegant

--- protected/views/oescape/oescape_drill_down_list.php
+++ protected/views/oescape/oescape_drill_down_list.php
@@ -1,8 +1,7 @@
 <?php $coreAPI = new CoreAPI();
       $operation_API = new OphTrOperationnote_API();
-      $cataract = isset($event_list); //TODO
       ?>
-<div class="analytics-patient-list" style="display: all;" >
+<div class="analytics-patient-list" style="display: none;" >
     <div class="flex-layout">
         <h3 id="js-list-title">List of Events</h3>
         <button id="js-back-to-chart" class="selected js-plot-display-label" >Back to chart</button>
@@ -16,16 +15,9 @@
             <col style="width: 100px;"><!-- Dilated -->
             <col style="width: 100px;"><!-- Value -->
             <col style="width: 200px;"><!-- Comments -->
-
-           <!-- <?= $cataract?
-               ' <col style="width: 350px;">
-                 <col style="width: 100px;">':
-                '<col style="width: 450px;">
-                 <col style="width: 450px;">';?> -->
         </colgroup>
         <thead>
             <tr>
-            <!-- <?=$cataract? '<th style="vertical-align: center;">Date</th>': '';?> // TODO can be turned off, this is here so I can remeber-->
                 <th class="drill_down_patient_list text-left" style="vertical-align: center;">Event ID</th><!-- Event ID -->
                 <th class="text-left" style="vertical-align: center;">Event Type</th><!-- Event type -->
                 <th class="text-left" style="vertical-align: center;">Eye (left or right)</th><!-- Eye-->
@@ -43,10 +35,7 @@
 <!--TODO <script src="<?= Yii::app()->assetManager->createUrl('js/analytics/analytics_csv.js')?>"></script> -->
 
 <script type="text/javascript">
-    DisplayDrillThroughData(3492971);
-    var iop_plotly_data
-    //DisplayDrillThroughData(3686611);
-
+    var iop_plotly_data;
     // generate links (used for drill through data to event details) based upon the datalink they have
     $('.clickable').click(function () {
         var link = $(this).data('link');
@@ -55,23 +44,21 @@
 
     // to drill through data from chart
     $('.js-plot-display-label').click(function () {
-        $('.analytics-charts').show();
-        $('.analytics-patient-list').hide();
-        $('.analytics-patient-list-row').hide();
+        $('.analytics-patient-list').show();
+        $('#js-back-to-chart').show();      
+        $('#oescape-layout').hide();
     });
     //back to chart from drill through data
     $('#js-back-to-chart').click(function () {
-        $('.analytics-event-list-row').hide();
-        $('.analytics-event-list').hide();
-        $(this).hide();
+        $('.analytics-patient-list').hide();
+        $('#js-back-to-chart').hide();
         $('#oescape-layout').show();
     });
-
     function DisplayDrillThroughData(){
         /// pull list of ids here
-
+        document.getElementById("DrillDownContent").innerHTML = '';
         // loop for each ID
-        iop_plotly_data = <?= CJavaScript::encode(OphCiExamination_Episode_IOPHistory::getDrillthroughIOPDataForEvent(3492971)); ?>//3686611  3492971
+        iop_plotly_data = <?= CJavaScript::encode(OphCiExamination_Episode_IOPHistory::getDrillthroughIOPDataForEvent(3686611)); ?>//3686611  3492971
 
         for (var i=0;i<iop_plotly_data.length;i++){
             var data_row = "<tr  class='clickable' data-link='/OphCi"+iop_plotly_data[i]["event_name"]+"/default/view/"+iop_plotly_data[i]["event_id"]+"'> <!-- Generated Data -->";
