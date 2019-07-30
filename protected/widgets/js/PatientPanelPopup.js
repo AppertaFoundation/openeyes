c19b07bf4eaff89bbed98c227992a63a60078f62
Gurpreet Singh <gurpreet.s.saggu@gmail.com>
2019-07-30 10:05:48 +1000

    CERA-476 Add a function that adjusts the top of the popup if it is outside the viewport.

--- protected/widgets/js/PatientPanelPopup.js
+++ protected/widgets/js/PatientPanelPopup.js
@@ -13,19 +13,23 @@ PatientPanel.patientPopups = {
     // patient popups
     var quicklook = new OpenEyes.UI.NavBtnPopup('quicklook',
         parentElement.find('.js-quicklook-btn'),
-        parentElement.find('.patient-summary-quicklook')
+        parentElement.find('.patient-summary-quicklook'),
+        {asdf: 'hello1'}
     );
     var demographics = new OpenEyes.UI.NavBtnPopup('demographics',
         parentElement.find('.js-demographics-btn'),
-        parentElement.find('.patient-popup-demographics')
+        parentElement.find('.patient-popup-demographics'),
+        {asdf: 'hello2'}
     );
     var management = new OpenEyes.UI.NavBtnPopup('management',
         parentElement.find('.js-management-btn'),
-        parentElement.find('.patient-popup-management')
+        parentElement.find('.patient-popup-management'),
+        {asdf: 'hello3'}
     );
     var risks = new OpenEyes.UI.NavBtnPopup('risks',
         parentElement.find('.js-allergies-risks-btn'),
-        parentElement.find('.patient-popup-allergies-risks')
+        parentElement.find('.patient-popup-allergies-risks'),
+        {asdf: 'hello4'}
     );
 
     var all = [quicklook, demographics, management, risks];
@@ -33,7 +37,8 @@ PatientPanel.patientPopups = {
     if (parentElement.find('.js-trials-btn')) {
       var trials = new OpenEyes.UI.NavBtnPopup('trials',
         parentElement.find('.js-trials-btn'),
-        parentElement.find('.patient-popup-trials')
+        parentElement.find('.patient-popup-trials'),
+          {asdf: 'hello5'}
       );
       all.push(trials);
     }
@@ -69,5 +74,31 @@ PatientPanel.patientPopups = {
       var popup = this.popupBtns[i];
       popup.isLatched = false;
     }
+  },
+
+  /**
+   * This function checks the popup to see if it is going outside the viewport and adjusts the
+   * top of the popup to make it appear above the button.
+   * @param button The ref to the button (image) that opens the popup on mouseover.
+   * @param content The ref to the popup element.
+   */
+  adjustTop: function(button, content) {
+    // height of the icons (such as demographics, management, quicklook)
+    var iconHeight = 35;
+
+    var topButton = $(button).offset().top;
+    var popupHeight = $(content).show().height();
+    var popupTop = $(content).show().offset().top;
+
+    // this variable holds the total length from the top of the window to the button of the popup.
+    var total = popupHeight + popupTop ;
+
+    var windowHeight = $(window).height();
+
+    if( (total + iconHeight)  > windowHeight) {
+      // this property is removed when the mouse leaves the icon.
+      $(content).css({ top: (topButton - popupHeight - iconHeight) + 'px' });
+    }
   }
+
 };
