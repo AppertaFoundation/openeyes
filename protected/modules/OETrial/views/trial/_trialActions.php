ee7f74a2668616bb75c51129a6a6c7d0edbc931a
Gurpreet Singh <gurpreet.s.saggu@gmail.com>
2019-08-01 15:11:35 +1000

    CERA-488 Rename the link from Trial to Trails as it is taking the user back to the trials list.

--- protected/modules/OETrial/views/trial/_trialActions.php
+++ protected/modules/OETrial/views/trial/_trialActions.php
@@ -7,7 +7,7 @@
   <h3>Actions</h3>
   <ul>
         <li>
-            <?=\CHtml::link('Go Back to Trial', Yii::app()-> baseURL.'/OETrial/trial') ?>
+            <?=\CHtml::link('Go Back to Trials', Yii::app()-> baseURL.'/OETrial/trial') ?>
         </li>
       <?php if ($trial->is_open && $permission->can_edit): ?>
         <li>
