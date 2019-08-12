a4cf282ac09677405e5f6c212947deb6fa2c396c
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-08-12 18:08:01 +0300

    OE-8813 use event date instead of event_date_YYYYMMDD

--- protected/modules/OphCiExamination/models/MedicationManagement.php
+++ protected/modules/OphCiExamination/models/MedicationManagement.php
@@ -112,10 +112,9 @@ class MedicationManagement extends BaseMedicationElement
     public function getContinuedEntries()
     {
         $event_date = $this->event->event_date;
-        $event_date_YYYYMMDD = substr($event_date, 0, 4).substr($event_date, 5, 2).substr($event_date, 8, 2);
 
-        return array_filter($this->visible_entries, function($e) use($event_date_YYYYMMDD) {
-            return ($e->start_date < $event_date_YYYYMMDD && is_null($e->end_date));
+        return array_filter($this->visible_entries, function($e) use($event_date) {
+            return ($e->start_date < $event_date && is_null($e->end_date));
         });
     }
 
