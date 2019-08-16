b2a0fe0c00f6d96d62838f4bd8e04d861b6ea5bc
biskyt <biskyt@users.noreply.github.com>
2019-08-16 16:25:05 +0100

    update to 3.3-rc2

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -598,7 +598,7 @@ return array(
         'Greek',
         'Italian'
       ),
-      'oe_version' => '3.3rc1',
+      'oe_version' => '3.3-rc2',
       'gp_label' => 'GP',
       // number of days in the future to retrieve worklists for the automatic dashboard render (0 by default in v3)
       'worklist_dashboard_future_days' => 0,
