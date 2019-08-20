988a51333416aa4557b2669149271eb7a670f05a
AdrianKamulegeya <adriankamulegeya@gmail.com>
2019-08-20 15:52:37 +0100

    OE-8866 Removed unnecessary div tags

--- protected/modules/OphCiExamination/views/default/view_Element_OphCiExamination_PostOpComplications.php
+++ protected/modules/OphCiExamination/views/default/view_Element_OphCiExamination_PostOpComplications.php
@@ -22,51 +22,43 @@ $operation_note_id = \Yii::app()->request->getParam('OphCiExamination_postop_com
     (is_array($operationNoteList) ? key($operationNoteList) : null));
 ?>
 
-<?php if (isset($operation_note_id)) : ?>
-    <div class="element-data element-eyes">
-        <?php foreach (['left' => 'right', 'right' => 'left'] as $page_side => $eye_side) : ?>
-            <div class="js-element-eye <?= $eye_side ?>-eye column">
-                <div class="data-group">
-                    <?php if ($element->hasEye($eye_side)) :
-                        $eye_abbr = $eye_side == 'right' ? 'R' : 'L';
-                        $eye_macro = $eye_side == 'right' ? \Eye::RIGHT : \Eye::LEFT;
-                        ?>
-                        <table id="right-complication-list" class="recorded-postop-complications"
-                               data-sideletter="<?= $eye_abbr ?>">
-                            <tr>
-                                <td colspan="2">
-                                    <?php if ($eye_side === 'right') {
-                                        echo $operationNoteList[$operation_note_id];
-                                    } ?>
-                                </td>
-                            </tr>
-                            <?php foreach ($element->getFullComplicationList($eye_macro) as $value) : ?>
-                                <tr>
-                                    <td class=postop-complication-name><?php echo $value['name']; ?></td>
-                                    <td></td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </table>
-                    <?php else : ?>
-                        <div class="data-value not-recorded">
-                            Not assessed in this examination
-                        </div>
-                    <?php endif; ?>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
-<?php else: ?>
-		<div class="element-data element-eyes">
-			<div class="js-element-eye right-eye">
-				<div class="data-value not-recorded">
-					There are no recorded operations for this patient
+
+<div class="element-data element-eyes">
+		<?php foreach (['left' => 'right', 'right' => 'left'] as $page_side => $eye_side) : ?>
+				<div class="js-element-eye <?= $eye_side ?>-eye column">
+						<?php if (isset($operation_note_id)) : ?>
+								<div class="data-group">
+										<?php if ($element->hasEye($eye_side)) :
+												$eye_abbr = $eye_side == 'right' ? 'R' : 'L';
+												$eye_macro = $eye_side == 'right' ? \Eye::RIGHT : \Eye::LEFT;
+												?>
+												<table id="right-complication-list" class="recorded-postop-complications"
+															 data-sideletter="<?= $eye_abbr ?>">
+														<tr>
+																<td colspan="2">
+																		<?php if ($eye_side === 'right') {
+																				echo $operationNoteList[$operation_note_id];
+																		} ?>
+																</td>
+														</tr>
+														<?php foreach ($element->getFullComplicationList($eye_macro) as $value) : ?>
+																<tr>
+																		<td class=postop-complication-name><?php echo $value['name']; ?></td>
+																		<td></td>
+																</tr>
+														<?php endforeach; ?>
+												</table>
+										<?php else : ?>
+												<div class="data-value not-recorded">
+														Not assessed in this examination
+												</div>
+										<?php endif; ?>
+								</div>
+						<?php else: ?>
+							<div class="data-value not-recorded">
+								There are no recorded operations for this patient
+							</div>
+						<?php endif; ?>
 				</div>
-			</div>
-			<div class="js-element-eye left-eye">
-				<div class="data-value not-recorded">
-					There are no recorded operations for this patient
-				</div>
-			</div>
-		</div>
-<?php endif; ?>
+		<?php endforeach; ?>
+</div>
