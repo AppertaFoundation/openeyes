fc404d0c9c870a41059d4c768240cf7033e8dfa3
Angus Cameron <example@example.com>
2019-08-22 16:44:52 +1000

    CERA-426 Applied recommended codefactor changes (for the second time)

--- protected/models/ImportStatus.php
+++ protected/models/ImportStatus.php
@@ -80,15 +80,4 @@ class ImportStatus extends BaseActiveRecord
 			'criteria'=>$criteria,
 		));
 	}
-
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return ImportStatus the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
 }
