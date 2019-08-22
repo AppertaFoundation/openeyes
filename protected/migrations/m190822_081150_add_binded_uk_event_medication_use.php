e64e7d60ad441c694c270b3739e501a5f57073cd
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-08-22 12:31:24 +0100

    OE-8786 Increase varchar to 20

--- protected/migrations/m190822_081150_add_binded_uk_event_medication_use.php
+++ protected/migrations/m190822_081150_add_binded_uk_event_medication_use.php
@@ -4,8 +4,8 @@ class m190822_081150_add_binded_uk_event_medication_use extends CDbMigration
 {
 	public function up()
 	{
-		$this->addColumn('event_medication_use' , 'binded_key' , 'VARCHAR(10) NULL');
-		$this->addColumn('event_medication_use_version' , 'binded_key' , 'VARCHAR(10) NULL');
+		$this->addColumn('event_medication_use' , 'binded_key' , 'VARCHAR(20) NULL');
+		$this->addColumn('event_medication_use_version' , 'binded_key' , 'VARCHAR(20) NULL');
 	}
 
 	public function down()
