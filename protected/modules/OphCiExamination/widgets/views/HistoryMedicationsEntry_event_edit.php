051f7b1da737977ccda02b9d18f3658d64f92c17
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-08-23 16:44:21 +0100

    OE-8894 Add condition if it's a template and render a mustache tag otherwise output binded_key value

--- protected/modules/OphCiExamination/widgets/views/HistoryMedicationsEntry_event_edit.php
+++ protected/modules/OphCiExamination/widgets/views/HistoryMedicationsEntry_event_edit.php
@@ -74,7 +74,7 @@ $is_posting = Yii::app()->request->getIsPostRequest();
         <input type="hidden" name="<?= $field_prefix ?>[id]" value="<?=$entry->id ?>" />
         <input type="hidden" name="<?= $field_prefix ?>[prescription_item_id]" value="<?=$entry->prescription_item_id ?>" />
         <input type="hidden" name="<?= $field_prefix ?>[to_be_copied]" class="js-to-be-copied" value="<?php echo (int)$to_be_copied; ?>" />
-				<input type="hidden" name="<?= $field_prefix ?>[binded_key]" class="js-binded-key" value="<?= $entry->binded_key ?>">
+				<input type="hidden" name="<?= $field_prefix ?>[binded_key]" class="js-binded-key" value="<?= !isset($entry->binded_key) && isset($is_template) && $is_template ? "{{binded_key}}" : $entry->binded_key ?>">
     </td>
     <td class="dose-frequency-route">
         <div id="<?= $model_name."_entries_".$row_count."_dfrl_error" ?>">
