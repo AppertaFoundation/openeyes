f805a3cf63fb993d3ee284db968f7c533810fe18
codefactor-io <support@codefactor.io>
2019-08-28 09:25:18 +0000

    [CodeFactor] Apply fixes to commit f2399db
    [ci skip] [skip ci]

--- protected/modules/OphInLabResults/views/default/view_Element_OphInLabResults_ResultTimedNumeric.php
+++ protected/modules/OphInLabResults/views/default/view_Element_OphInLabResults_ResultTimedNumeric.php
@@ -13,17 +13,17 @@
       <td>
           <span class="large-text highlighter orange js-lab-result-warning"
                 style="<?php
-              if (isset($element->result)&& $element->resultType->normal_min && $element->resultType->normal_min &&
+                if (isset($element->result)&& $element->resultType->normal_min && $element->resultType->normal_min &&
                  ($element->result > $element->resultType->normal_max || $element->result < $element->resultType->normal_min)) {
                       echo "display:block";
-                  } else {
-                      echo "display:none";
-                  } ?>">
-          <?php if ($element->resultType->custom_warning_message) {
-              echo $element->resultType->custom_warning_message;
-           } else { ?>
+                } else {
+                    echo "display:none";
+                } ?>">
+            <?php if ($element->resultType->custom_warning_message) {
+                echo $element->resultType->custom_warning_message;
+            } else { ?>
               The value is outside the normal range. Normal min: <?= $element->resultType->normal_min ?> Normal max: <?= $element->resultType->normal_max ?>
-          <?php } ?>
+            <?php } ?>
           </span>
       </td>
   </tr>
