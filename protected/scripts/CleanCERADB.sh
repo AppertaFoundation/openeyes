5b9e386a5e82a16447f02c2cec34ece2971fcddf
zhaolian-zhou <zhaolian.zhou@toukanlabs.com>
2019-09-05 05:07:52 +0100

    modify the clean DB bash script for separate DB host

--- protected/scripts/CleanCERADB.sh
+++ protected/scripts/CleanCERADB.sh
@@ -1,2 +1,2 @@
-mysql -u openeyes -popeneyes openeyes < CleanCERANormalTables_2.sql
-mysql -u openeyes -popeneyes openeyes < CleanCERAVersionTables.sql
+mysql -h web_db_1  -u openeyes -popeneyes openeyes < CleanCERANormalTables_2.sql
+mysql -h web_db_1 -u openeyes -popeneyes openeyes < CleanCERAVersionTables.sql
