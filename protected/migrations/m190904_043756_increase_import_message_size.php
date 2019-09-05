69f32af4537cd7f213b9231ba07e49cf82a80328
Angus Cameron <example@example.com>
2019-09-05 15:03:20 +1000

    CERA-426 Modified migration to support longer import messages

--- protected/migrations/m190904_043756_increase_import_message_size.php
+++ protected/migrations/m190904_043756_increase_import_message_size.php
@@ -4,7 +4,7 @@ class m190904_043756_increase_import_message_size extends CDbMigration
 {
 	public function up()
 	{
-		$this->alterColumn('import', 'message', 'text NOT NULL');
+		$this->alterColumn('import', 'message', 'varchar(65535) NOT NULL');
 	}
 
 	public function down()
