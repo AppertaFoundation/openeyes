cf2e46c2b2c1007da430da7e4b5c246d8d995e02
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-09-06 13:06:59 +0100

    OE-8335 Fix transaction bug , check if the current medication is not deleted

--- protected/commands/LocalMedicationToDmdMedicationCommand.php
+++ protected/commands/LocalMedicationToDmdMedicationCommand.php
@@ -1,36 +1,36 @@
 <?php /**
-* OpenEyes
-*
-* (C) OpenEyes Foundation, 2019
-* This file is part of OpenEyes.
-* OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-* OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-* You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
-*
-* @package OpenEyes
-* @link http://www.openeyes.org.uk
-* @author OpenEyes <info@openeyes.org.uk>
-* @copyright Copyright (c) 2019, OpenEyes Foundation
-* @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
-*/
+ * OpenEyes
+ *
+ * (C) OpenEyes Foundation, 2019
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2019, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
+ */
 
 class LocalMedicationToDmdMedicationCommand extends CConsoleCommand
 {
 
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'Update Local Medication with National Codes with existing DM+D medication data.';
-    }
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'Update Local Medication with National Codes with existing DM+D medication data.';
+	}
 
-    /**
-     * @return string
-     */
-    public function getHelp()
-    {
-        return <<<EOH
+	/**
+	 * @return string
+	 */
+	public function getHelp()
+	{
+		return <<<EOH
         
 'Update Local Medication with National Codes with existing DM+D medication data
         
@@ -41,55 +41,55 @@ USAGE
          
 EOH;
 
-    }
+	}
 
-    public function actionIndex()
-    {
+	public function actionIndex()
+	{
+		$drugs_with_national_code = Drug::model()->findAll("national_code is NOT NULL");
+		foreach ($drugs_with_national_code as $drug) {
+			// check for medication ID
+			$current_medication = Medication::model()->find("source_old_id = :old_id AND source_type='LOCAL' AND source_subtype='drug' AND deleted_date is NULL", array(":old_id" => $drug->id));
+			$target_medication = Medication::model()->find("preferred_code = :national_code AND source_type='DM+D' AND deleted_date is NULL", array(":national_code" => $drug->national_code));
 
-        $drugs_with_national_code = Drug::model()->findAll("national_code is NOT NULL");
-        foreach ($drugs_with_national_code as $drug) {
-            // check for medication ID
-            $current_medication = Medication::model()->find("source_old_id = :old_id AND source_type='LOCAL' AND source_subtype='drug'", array(":old_id" => $drug->id));
-            $target_medication = Medication::model()->find("preferred_code = :national_code AND source_type='DM+D' AND deleted_date is NULL", array(":national_code" => $drug->national_code));
 
-						$transaction = Yii::app()->db->beginTransaction();
-            if ($current_medication && $target_medication) {
-                if ($target_medication->source_old_id) {
-                    // Only drug medication has source_old_id if target medication has one it means one drug with
-                    // this national code was already merged in with dmd data
-                    $new_merge = new MedicationMerge();
-                    $new_merge->source_drug_id = $drug->id;
-                    $new_merge->source_medication_id = $current_medication->id;
-                    $new_merge->source_name = $drug->name;
-                    $new_merge->target_code = $drug->national_code;
-                    $new_merge->target_name = $target_medication->preferred_term;
-                    if ($target_medication) {
-                        $new_merge->target_id = $target_medication->id;
-                    }
+			if ($current_medication && $target_medication) {
+				$transaction = Yii::app()->db->beginTransaction();
+				if ($target_medication->source_old_id) {
+					// Only drug medication has source_old_id if target medication has one it means one drug with
+					// this national code was already merged in with dmd data
+					$new_merge = new MedicationMerge();
+					$new_merge->source_drug_id = $drug->id;
+					$new_merge->source_medication_id = $current_medication->id;
+					$new_merge->source_name = $drug->name;
+					$new_merge->target_code = $drug->national_code;
+					$new_merge->target_name = $target_medication->preferred_term;
+					if ($target_medication) {
+						$new_merge->target_id = $target_medication->id;
+					}
 
-                    if (!$new_merge->save()) {
-                        $transaction->rollback();
-                        Yii::log( "ERROR: unable to save drug ".$drug->name."!\n");
-                    } else {
-                        $transaction->commit();
-                    }
-                } else {
-										$transaction = Yii::app()->db->beginTransaction();
-                    $source_old_id = $current_medication->source_old_id;
-                    $current_medication->attributes = $target_medication->attributes;
-                    $current_medication->source_old_id = $source_old_id;
-										$target_medication->deleted_date = date("Y-m-d H:i:s");
+					if (!$new_merge->save()) {
+						$transaction->rollback();
+						Yii::log("ERROR: unable to save drug " . $drug->name . "!\n");
+					} else {
+						$transaction->commit();
+					}
+				} else {
+					$source_old_id = $current_medication->source_old_id;
+					$current_medication->attributes = $target_medication->attributes;
+					$current_medication->source_old_id = $source_old_id;
+					$target_medication->deleted_date = date("Y-m-d H:i:s");
 
-                    if($current_medication->save() && $target_medication->save()) {
-                    	$transaction->commit();
-										} else {
-                    	Yii::log('Unable to update Medication with id :' . $current_medication->id .
-												" with Medication id: " . $target_medication->id . " attributes");
-										}
-                }
-            }
-        }
+					if ($current_medication->save() && $target_medication->save()) {
+						$transaction->commit();
+					} else {
+						$transaction->rollback();
+						Yii::log('Unable to update Medication with id :' . $current_medication->id .
+							" with Medication id: " . $target_medication->id . " attributes");
+					}
+				}
+			}
+		}
 
-        MedicationMerge::model()->mergeAll();
-    }
+		MedicationMerge::model()->mergeAll();
+	}
 }
