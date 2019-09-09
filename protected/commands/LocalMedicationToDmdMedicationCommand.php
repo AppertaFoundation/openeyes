474785b537565025ee01a19c0178565d28a0e05a
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-09-09 11:21:46 +0100

    OE-8335 Refactor some code to smaller methods

--- protected/commands/LocalMedicationToDmdMedicationCommand.php
+++ protected/commands/LocalMedicationToDmdMedicationCommand.php
@@ -51,33 +51,24 @@ EOH;
 			$current_medication = Medication::model()->find("source_old_id = :old_id AND source_type='LOCAL' AND source_subtype='drug' AND deleted_date is NULL", array(":old_id" => $drug->id));
 			$target_medication = Medication::model()->find("preferred_code = :national_code AND source_type='DM+D' AND deleted_date is NULL", array(":national_code" => $drug->national_code));
 
-
 			if ($current_medication && $target_medication) {
-				$transaction = Yii::app()->db->beginTransaction();
-				if ($target_medication->source_old_id) {
 					// Only drug medication has source_old_id if target medication has one it means one drug with
 					// this national code was already merged in with dmd data
-					$new_merge = new MedicationMerge();
-					$new_merge->source_drug_id = $drug->id;
-					$new_merge->source_medication_id = $current_medication->id;
-					$new_merge->source_name = $drug->name;
-					$new_merge->target_code = $drug->national_code;
-					$new_merge->target_name = $target_medication->preferred_term;
-					if ($target_medication) {
-						$new_merge->target_id = $target_medication->id;
-					}
+				if ($target_medication->source_old_id) {
+					if(!$this->activeMedicationMergeExists($current_medication, $target_medication)) {
+						$transaction = Yii::app()->db->beginTransaction();
+						$medication_merge = $this->createMedicationMerge($current_medication, $target_medication, $drug);
 
-					if (!$new_merge->save()) {
-						$transaction->rollback();
-						Yii::log("ERROR: unable to save drug " . $drug->name . "!\n");
-					} else {
-						$transaction->commit();
+						if (!$medication_merge->save()) {
+							$transaction->rollback();
+							Yii::log("ERROR: unable to save merge of drug " . $drug->name . "!\n");
+						} else {
+							$transaction->commit();
+						}
 					}
 				} else {
-					$source_old_id = $current_medication->source_old_id;
-					$current_medication->attributes = $target_medication->attributes;
-					$current_medication->source_old_id = $source_old_id;
-					$target_medication->deleted_date = date("Y-m-d H:i:s");
+					$transaction = Yii::app()->db->beginTransaction();
+					$this->updateLocalMedicationWithDmdMedicationAttributes($current_medication, $target_medication);
 
 					if ($current_medication->save() && $target_medication->save()) {
 						$transaction->commit();
@@ -91,5 +82,64 @@ EOH;
 		}
 
 		MedicationMerge::model()->mergeAll();
+	}
+
+	/**
+	 * @param Medication $source_medication
+	 * @param Medication $target_medication
+	 * @param Drug $source_drug
+	 * @return MedicationMerge
+	 */
+	private function createMedicationMerge($source_medication , $target_medication, $source_drug = null) {
+		$new_merge = new MedicationMerge();
+		if($source_drug) {
+			$new_merge->source_drug_id = $source_drug->id;
+			$new_merge->source_name = $source_drug->national_code;
+		}
+
+		$new_merge->source_medication_id = $source_medication->id;
+		$new_merge->target_code = $target_medication->preferred_code;
+		$new_merge->target_name = $target_medication->preferred_term;
+		$new_merge->target_id = $target_medication->id;
+
+		return $new_merge;
+	}
+
+	/**
+	 * @param Medication $current_medication
+	 * @param Medication $target_medication
+	 * @return bool
+	 */
+	private function activeMedicationMergeExists($source_medication , $target_medication) {
+		$medication_merge_criteria = new CDbCriteria();
+
+		$medication_merge_criteria->addCondition('t.source_medication_id = :source_medication_id');
+		$medication_merge_criteria->addCondition('t.status = :status');
+		$medication_merge_criteria->addCondition('t.target_code = :target_code');
+
+		if ($target_medication) {
+			$medication_merge_criteria->addCondition('t.target_id = :target_id');
+			$medication_merge_criteria->params[':target_id'] = $target_medication->id;
+		}
+
+		$medication_merge_criteria->params[':source_medication_id'] = $source_medication->id;
+		$medication_merge_criteria->params[':status'] = MedicationMerge::$ACTIVE_STATUS;
+		$medication_merge_criteria->params[':target_code'] = $source_medication->preferred_code;
+
+		return MedicationMerge::model()->exists($medication_merge_criteria);
+	}
+
+	/**
+	 * @param Medication $current_medication
+	 * @param Medication $target_medication
+	 */
+	public function updateLocalMedicationWithDmdMedicationAttributes($current_medication, $target_medication)
+	{
+		$source_old_id = $current_medication->source_old_id;
+		$old_id = $current_medication->id;
+		$current_medication->attributes = $target_medication->attributes;
+		$current_medication->source_old_id = $source_old_id;
+		$current_medication->id = $old_id;
+		$target_medication->deleted_date = date("Y-m-d H:i:s");
 	}
 }
