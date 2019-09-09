54139282e0e5cbe066e17e65afc646ade9d32a39
codefactor-io <support@codefactor.io>
2019-09-09 10:43:30 +0000

    [CodeFactor] Apply fixes
    [ci skip] [skip ci]

--- protected/commands/LocalMedicationToDmdMedicationCommand.php
+++ protected/commands/LocalMedicationToDmdMedicationCommand.php
@@ -17,20 +17,20 @@
 class LocalMedicationToDmdMedicationCommand extends CConsoleCommand
 {
 
-	/**
-	 * @return string
-	 */
-	public function getName()
-	{
-		return 'Update Local Medication with National Codes with existing DM+D medication data.';
-	}
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Update Local Medication with National Codes with existing DM+D medication data.';
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getHelp()
-	{
-		return <<<EOH
+    /**
+     * @return string
+     */
+    public function getHelp()
+    {
+        return <<<EOH
         
 'Update Local Medication with National Codes with existing DM+D medication data
         
@@ -41,105 +41,107 @@ USAGE
          
 EOH;
 
-	}
+    }
 
-	public function actionIndex()
-	{
-		$drugs_with_national_code = Drug::model()->findAll("national_code is NOT NULL");
-		foreach ($drugs_with_national_code as $drug) {
-			// check for medication ID
-			$current_medication = Medication::model()->find("source_old_id = :old_id AND source_type='LOCAL' AND source_subtype='drug' AND deleted_date is NULL", array(":old_id" => $drug->id));
-			$target_medication = Medication::model()->find("preferred_code = :national_code AND source_type='DM+D' AND deleted_date is NULL", array(":national_code" => $drug->national_code));
+    public function actionIndex()
+    {
+        $drugs_with_national_code = Drug::model()->findAll("national_code is NOT NULL");
+        foreach ($drugs_with_national_code as $drug) {
+            // check for medication ID
+            $current_medication = Medication::model()->find("source_old_id = :old_id AND source_type='LOCAL' AND source_subtype='drug' AND deleted_date is NULL", array(":old_id" => $drug->id));
+            $target_medication = Medication::model()->find("preferred_code = :national_code AND source_type='DM+D' AND deleted_date is NULL", array(":national_code" => $drug->national_code));
 
-			if ($current_medication && $target_medication) {
-					// Only drug medication has source_old_id if target medication has one it means one drug with
-					// this national code was already merged in with dmd data
-				if ($target_medication->source_old_id) {
-					if(!$this->activeMedicationMergeExists($current_medication, $target_medication)) {
-						$transaction = Yii::app()->db->beginTransaction();
-						$medication_merge = $this->createMedicationMerge($current_medication, $target_medication, $drug);
+            if ($current_medication && $target_medication) {
+                    // Only drug medication has source_old_id if target medication has one it means one drug with
+                    // this national code was already merged in with dmd data
+                if ($target_medication->source_old_id) {
+                    if (!$this->activeMedicationMergeExists($current_medication, $target_medication)) {
+                        $transaction = Yii::app()->db->beginTransaction();
+                        $medication_merge = $this->createMedicationMerge($current_medication, $target_medication, $drug);
 
-						if (!$medication_merge->save()) {
-							$transaction->rollback();
-							Yii::log("ERROR: unable to save merge of drug " . $drug->name . "!\n");
-						} else {
-							$transaction->commit();
-						}
-					}
-				} else {
-					$transaction = Yii::app()->db->beginTransaction();
-					$this->updateLocalMedicationWithDmdMedicationAttributes($current_medication, $target_medication);
+                        if (!$medication_merge->save()) {
+                            $transaction->rollback();
+                            Yii::log("ERROR: unable to save merge of drug " . $drug->name . "!\n");
+                        } else {
+                            $transaction->commit();
+                        }
+                    }
+                } else {
+                    $transaction = Yii::app()->db->beginTransaction();
+                    $this->updateLocalMedicationWithDmdMedicationAttributes($current_medication, $target_medication);
 
-					if ($current_medication->save() && $target_medication->save()) {
-						$transaction->commit();
-					} else {
-						$transaction->rollback();
-						Yii::log('Unable to update Medication with id :' . $current_medication->id .
-							" with Medication id: " . $target_medication->id . " attributes");
-					}
-				}
-			}
-		}
+                    if ($current_medication->save() && $target_medication->save()) {
+                        $transaction->commit();
+                    } else {
+                        $transaction->rollback();
+                        Yii::log('Unable to update Medication with id :' . $current_medication->id .
+                            " with Medication id: " . $target_medication->id . " attributes");
+                    }
+                }
+            }
+        }
 
-		MedicationMerge::model()->mergeAll();
-	}
+        MedicationMerge::model()->mergeAll();
+    }
 
-	/**
-	 * @param Medication $source_medication
-	 * @param Medication $target_medication
-	 * @param Drug $source_drug
-	 * @return MedicationMerge
-	 */
-	private function createMedicationMerge($source_medication , $target_medication, $source_drug = null) {
-		$new_merge = new MedicationMerge();
-		if($source_drug) {
-			$new_merge->source_drug_id = $source_drug->id;
-			$new_merge->source_name = $source_drug->national_code;
-		}
+    /**
+     * @param Medication $source_medication
+     * @param Medication $target_medication
+     * @param Drug $source_drug
+     * @return MedicationMerge
+     */
+    private function createMedicationMerge($source_medication, $target_medication, $source_drug = null)
+    {
+        $new_merge = new MedicationMerge();
+        if ($source_drug) {
+            $new_merge->source_drug_id = $source_drug->id;
+            $new_merge->source_name = $source_drug->national_code;
+        }
 
-		$new_merge->source_medication_id = $source_medication->id;
-		$new_merge->target_code = $target_medication->preferred_code;
-		$new_merge->target_name = $target_medication->preferred_term;
-		$new_merge->target_id = $target_medication->id;
+        $new_merge->source_medication_id = $source_medication->id;
+        $new_merge->target_code = $target_medication->preferred_code;
+        $new_merge->target_name = $target_medication->preferred_term;
+        $new_merge->target_id = $target_medication->id;
 
-		return $new_merge;
-	}
+        return $new_merge;
+    }
 
-	/**
-	 * @param Medication $current_medication
-	 * @param Medication $target_medication
-	 * @return bool
-	 */
-	private function activeMedicationMergeExists($source_medication , $target_medication) {
-		$medication_merge_criteria = new CDbCriteria();
+    /**
+     * @param Medication $current_medication
+     * @param Medication $target_medication
+     * @return bool
+     */
+    private function activeMedicationMergeExists($source_medication, $target_medication)
+    {
+        $medication_merge_criteria = new CDbCriteria();
 
-		$medication_merge_criteria->addCondition('t.source_medication_id = :source_medication_id');
-		$medication_merge_criteria->addCondition('t.status = :status');
-		$medication_merge_criteria->addCondition('t.target_code = :target_code');
+        $medication_merge_criteria->addCondition('t.source_medication_id = :source_medication_id');
+        $medication_merge_criteria->addCondition('t.status = :status');
+        $medication_merge_criteria->addCondition('t.target_code = :target_code');
 
-		if ($target_medication) {
-			$medication_merge_criteria->addCondition('t.target_id = :target_id');
-			$medication_merge_criteria->params[':target_id'] = $target_medication->id;
-		}
+        if ($target_medication) {
+            $medication_merge_criteria->addCondition('t.target_id = :target_id');
+            $medication_merge_criteria->params[':target_id'] = $target_medication->id;
+        }
 
-		$medication_merge_criteria->params[':source_medication_id'] = $source_medication->id;
-		$medication_merge_criteria->params[':status'] = MedicationMerge::$ACTIVE_STATUS;
-		$medication_merge_criteria->params[':target_code'] = $source_medication->preferred_code;
+        $medication_merge_criteria->params[':source_medication_id'] = $source_medication->id;
+        $medication_merge_criteria->params[':status'] = MedicationMerge::$ACTIVE_STATUS;
+        $medication_merge_criteria->params[':target_code'] = $source_medication->preferred_code;
 
-		return MedicationMerge::model()->exists($medication_merge_criteria);
-	}
+        return MedicationMerge::model()->exists($medication_merge_criteria);
+    }
 
-	/**
-	 * @param Medication $current_medication
-	 * @param Medication $target_medication
-	 */
-	public function updateLocalMedicationWithDmdMedicationAttributes($current_medication, $target_medication)
-	{
-		$source_old_id = $current_medication->source_old_id;
-		$old_id = $current_medication->id;
-		$current_medication->attributes = $target_medication->attributes;
-		$current_medication->source_old_id = $source_old_id;
-		$current_medication->id = $old_id;
-		$target_medication->deleted_date = date("Y-m-d H:i:s");
-	}
+    /**
+     * @param Medication $current_medication
+     * @param Medication $target_medication
+     */
+    public function updateLocalMedicationWithDmdMedicationAttributes($current_medication, $target_medication)
+    {
+        $source_old_id = $current_medication->source_old_id;
+        $old_id = $current_medication->id;
+        $current_medication->attributes = $target_medication->attributes;
+        $current_medication->source_old_id = $source_old_id;
+        $current_medication->id = $old_id;
+        $target_medication->deleted_date = date("Y-m-d H:i:s");
+    }
 }
--- protected/models/MedicationMerge.php
+++ protected/models/MedicationMerge.php
@@ -26,123 +26,123 @@
 class MedicationMerge extends BaseActiveRecord
 {
 
-	public static $ACTIVE_STATUS = 1;
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'medication_merge';
-	}
+    public static $ACTIVE_STATUS = 1;
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'medication_merge';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('entry_date_time, source_drug_id, source_medication_id, source_code, source_name, target_id, target_code, target_name, status, merge_date', 'safe'),
-			// The following rule is used by search().
-			array('id, entry_date_time, source_drug_id, source_medication_id, source_code, source_name, target_id, target_code, target_name', 'safe', 'on' => 'search'),
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('entry_date_time, source_drug_id, source_medication_id, source_code, source_name, target_id, target_code, target_name, status, merge_date', 'safe'),
+            // The following rule is used by search().
+            array('id, entry_date_time, source_drug_id, source_medication_id, source_code, source_name, target_id, target_code, target_name', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'eventMedicationUsesDrug' => array(self::HAS_MANY, EventMedicationUse::class, 'source_drug_id'),
-			'eventMedicationUses' => array(self::HAS_MANY, EventMedicationUse::class, 'source_medication_id'),
-			'createdUser' => array(self::BELONGS_TO, 'User', 'created_user_id'),
-			'lastModifiedUser' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
-			'medicationsSource' => array(self::MANY_MANY, Medication::class, array('source_medication_id' => 'id')),
-			'medicationsTarget' => array(self::MANY_MANY, Medication::class, array('target_medication_id' => 'id')),
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'eventMedicationUsesDrug' => array(self::HAS_MANY, EventMedicationUse::class, 'source_drug_id'),
+            'eventMedicationUses' => array(self::HAS_MANY, EventMedicationUse::class, 'source_medication_id'),
+            'createdUser' => array(self::BELONGS_TO, 'User', 'created_user_id'),
+            'lastModifiedUser' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
+            'medicationsSource' => array(self::MANY_MANY, Medication::class, array('source_medication_id' => 'id')),
+            'medicationsTarget' => array(self::MANY_MANY, Medication::class, array('target_medication_id' => 'id')),
+        );
+    }
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return MedicationRoute the static model class
-	 */
-	public static function model($className = __CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return MedicationRoute the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	/**
-	 * Return all entries where status = 1
-	 * @return CActiveDataProvider
-	 */
-	private function getAllPending()
-	{
-		$criteria = new \CDbCriteria();
-		$criteria->addCondition('status = ' . MedicationMerge::$ACTIVE_STATUS);
-		return new CActiveDataProvider('medicationMerge', ['criteria' => $criteria,
-			'pagination' => false]);
-	}
+    /**
+     * Return all entries where status = 1
+     * @return CActiveDataProvider
+     */
+    private function getAllPending()
+    {
+        $criteria = new \CDbCriteria();
+        $criteria->addCondition('status = ' . MedicationMerge::$ACTIVE_STATUS);
+        return new CActiveDataProvider('medicationMerge', ['criteria' => $criteria,
+            'pagination' => false]);
+    }
 
-	/*
-	* Runs all pending merge at once
-	*/
-	public function mergeAll()
-	{
-		foreach ($this->getAllPending()->getData() as $merge_row) {
-			// several cases need to be handled here:
-			// 1. source_drug_id set / target_medication_id not set -> search by target_code
-			// 2. source_medication_id set / target_medication_id set
-			if (!$merge_row->source_medication_id || $merge_row->source_medication_id == '') {
-				if ($merge_row->source_drug_id > 0) {
-					$source_medication = Medication::model()->find("source_old_id = :old_id AND source_type='LOCAL' AND source_subtype='drug'", array(":old_id" => $merge_row->source_drug_id));
-					$merge_row->source_medication_id = $source_medication->id;
-				} else {
-					Yii::log('Source medication cannot be found without a source drug id or a source medication id');
-					continue;
-				}
-			} else {
-				$source_medication = Medication::model()->findByPk($merge_row->source_medication_id);
-			}
+    /*
+    * Runs all pending merge at once
+    */
+    public function mergeAll()
+    {
+        foreach ($this->getAllPending()->getData() as $merge_row) {
+            // several cases need to be handled here:
+            // 1. source_drug_id set / target_medication_id not set -> search by target_code
+            // 2. source_medication_id set / target_medication_id set
+            if (!$merge_row->source_medication_id || $merge_row->source_medication_id == '') {
+                if ($merge_row->source_drug_id > 0) {
+                    $source_medication = Medication::model()->find("source_old_id = :old_id AND source_type='LOCAL' AND source_subtype='drug'", array(":old_id" => $merge_row->source_drug_id));
+                    $merge_row->source_medication_id = $source_medication->id;
+                } else {
+                    Yii::log('Source medication cannot be found without a source drug id or a source medication id');
+                    continue;
+                }
+            } else {
+                $source_medication = Medication::model()->findByPk($merge_row->source_medication_id);
+            }
 
-			if (!$merge_row->target_id || $merge_row->target_id == '') {
-				$target_medication = Medication::model()->find("preferred_code = :national_code AND source_type='DM+D'", array(":national_code" => $merge_row->target_code));
-				if ($target_medication) {
-					$merge_row->target_id = $target_medication->id;
-				} else {
-					Yii::log('No target medication was found , cannot be merged');
-					// we cannot merge without ID!
-					continue;
-				}
-			}
+            if (!$merge_row->target_id || $merge_row->target_id == '') {
+                $target_medication = Medication::model()->find("preferred_code = :national_code AND source_type='DM+D'", array(":national_code" => $merge_row->target_code));
+                if ($target_medication) {
+                    $merge_row->target_id = $target_medication->id;
+                } else {
+                    Yii::log('No target medication was found , cannot be merged');
+                    // we cannot merge without ID!
+                    continue;
+                }
+            }
 
-			foreach (['event_medication_use', 'medication_allergy_assignment', 'medication_attribute_assignment', 'medication_common',
-								 'medication_search_index', 'medication_set_auto_rule_medication', 'medication_set_item'] as $table_with_medication_id) {
-				try {
-					Yii::app()->db->createCommand("UPDATE {$table_with_medication_id} 
+            foreach (['event_medication_use', 'medication_allergy_assignment', 'medication_attribute_assignment', 'medication_common',
+                                 'medication_search_index', 'medication_set_auto_rule_medication', 'medication_set_item'] as $table_with_medication_id) {
+                try {
+                    Yii::app()->db->createCommand("UPDATE {$table_with_medication_id} 
                                          SET medication_id = {$merge_row->target_id} 
                                          WHERE medication_id = {$merge_row->source_medication_id}")->execute();
-				} catch (Exception $exception) {
-					Yii::log("Cannot remap foreign keys for medication: " . $merge_row->target_code . "\n");
-					Yii::log($exception->getMessage());
-				}
-			}
-			$source_medication->deleted_date = date("Y-m-d");
+                } catch (Exception $exception) {
+                    Yii::log("Cannot remap foreign keys for medication: " . $merge_row->target_code . "\n");
+                    Yii::log($exception->getMessage());
+                }
+            }
+            $source_medication->deleted_date = date("Y-m-d");
 
-			if (!$source_medication->save()) {
-				Yii::log("Cannot deactivate medication: " . $source_medication->id . "\n");
-			}
+            if (!$source_medication->save()) {
+                Yii::log("Cannot deactivate medication: " . $source_medication->id . "\n");
+            }
 
-			$merge_row->merge_date = date("Y-m-d H:i:s");
-			$merge_row->status = 0;
-			if (!$merge_row->save()) {
-				Yii::log("Cannot close merge " . $merge_row->id . "\n");
-			}
-		}
-	}
+            $merge_row->merge_date = date("Y-m-d H:i:s");
+            $merge_row->status = 0;
+            if (!$merge_row->save()) {
+                Yii::log("Cannot close merge " . $merge_row->id . "\n");
+            }
+        }
+    }
 }
