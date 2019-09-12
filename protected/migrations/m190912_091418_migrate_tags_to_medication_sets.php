2ca153e72d279d1adfdb47181168e2d69b1ea040
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-09-12 14:03:54 +0100

    OE-8962 Migrate risk tag to medication set and drop all related tag tables

---
+++ protected/migrations/m190912_091418_migrate_tags_to_medication_sets.php
@@ -0,0 +1,34 @@
+<?php
+
+class m190912_091418_migrate_tags_to_medication_sets extends OEMigration
+{
+	public function up()
+	{
+		$this->execute('UPDATE ophciexamination_risk_tag t
+                         JOIN tag tg ON tg.id = t.`tag_id`
+                         SET medication_set_id = (SELECT id FROM medication_set mst WHERE mst.name = tg.name)'
+		);
+
+		$this->dropForeignKey('ophciexamination_risk_tag_tag_fk', 'ophciexamination_risk_tag');
+		$this->dropColumn('ophciexamination_risk_tag', 'tag_id');
+
+
+		$this->dropForeignKey('drug_tags_ti_fk', 'drug_tag');
+		$this->dropForeignKey('drug_type_tags_fk', 'drug_type');
+		$this->dropForeignKey('medication_tags_ti_fk', 'medication_drug_tag');
+
+		$this->dropColumn('drug_type', 'tag_id');
+		$this->dropColumn('drug_type_version', 'tag_id');
+		$this->dropColumn('medication_drug_tag', 'tag_id');
+		$this->dropColumn('medication_drug_tag_version', 'tag_id');
+		$this->dropOETable('drug_type', true);
+		$this->dropOETable('medication_drug_tag', true);
+		$this->dropOETable('drug_tag', true);
+	}
+
+	public function down()
+	{
+		echo "m190912_091418_migrate_tags_to_medication_sets does not support migration down.\n";
+		return false;
+	}
+}
