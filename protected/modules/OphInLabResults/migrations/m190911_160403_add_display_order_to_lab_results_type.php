8da71b2988673cea990ae50fd4bbd8a1b69b895a
codefactor-io <support@codefactor.io>
2019-09-12 09:43:26 +0000

    [CodeFactor] Apply fixes to commit f8b2ff8
    [ci skip] [skip ci]

--- protected/modules/OphInLabResults/migrations/m190911_160403_add_display_order_to_lab_results_type.php
+++ protected/modules/OphInLabResults/migrations/m190911_160403_add_display_order_to_lab_results_type.php
@@ -2,15 +2,15 @@
 
 class m190911_160403_add_display_order_to_lab_results_type extends CDbMigration
 {
-	public function safeUp()
-	{
+    public function safeUp()
+    {
         $this->addColumn('ophinlabresults_type', 'display_order', 'INT(10)');
         $this->addColumn('ophinlabresults_type_version', 'display_order', 'INT(10)');
-	}
+    }
 
-	public function safeDown()
-	{
+    public function safeDown()
+    {
         $this->dropColumn('ophinlabresults_type', 'display_order');
         $this->dropColumn('ophinlabresults_type_version', 'display_order');
-	}
+    }
 }
