43f765e9ab3cdcdf8c009da6bae92efaf3e4c0ee
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2019-09-13 10:55:02 +0100

    OE-8423 create the admin page without using genericAdmin to make 'Medication parameters changed' stop reason not editable

---
+++ protected/modules/OphCiExamination/modules/ExaminationAdmin/controllers/MedicationStopReasonController.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * OpenEyes.
+ *
+ * (C) OpenEyes Foundation, 2019
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.openeyes.org.uk
+ *
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2019, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
+ */
+use OEModule\OphCiExamination\models\HistoryMedicationsStopReason;
+class MedicationStopReasonController extends \ModuleAdminController
+{
+    public function actions() {
+
+    }
+    /**
+     * Lists all models.
+     */
+    public function actionIndex()
+    {
+        $this->group = 'Examination';
+        $asset_manager = Yii::app()->getAssetManager();
+        $asset_manager->registerScriptFile('/js/oeadmin/OpenEyes.admin.js');
+        $asset_manager->registerScriptFile('/js/oeadmin/list.js');
+        $this->render('/medicationstopreason/index', [
+            'model' => HistoryMedicationsStopReason::model(),
+            'model_list' => HistoryMedicationsStopReason::model()->findAll(['order' => 'display_order']),
+        ]);
+    }
+
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate()
+    {
+        $model = new HistoryMedicationsStopReason();
+        $request = Yii::app()->getRequest();
+        if ($request->getPost('OEModule_OphCiExamination_models_HistoryMedicationsStopReason')) {
+            $model->attributes = $request->getPost('OEModule_OphCiExamination_models_HistoryMedicationsStopReason');
+            if ($model->save()) {
+                Audit::add('admin', 'create', serialize($model->attributes), false,
+                    ['model' => 'OEModule_OphCiExamination_models_HistoryMedicationsStopReason']);
+                Yii::app()->user->setFlash('success', 'Medication Stop Reason created');
+                $this->redirect(['index']);
+            } else {
+                $errors = $model->getErrors();
+            }
+        }
+        $this->render('/medicationstopreason/edit', [
+            'model' => $model,
+            'errors' => isset($errors) ? $errors : null,
+        ]);
+    }
+
+    public function actionUpdate()
+    {
+        $request = Yii::app()->getRequest();
+        $model = HistoryMedicationsStopReason::model()->findByPk((int)$request->getParam('id'));
+        if (!$model) {
+            throw new Exception('HistoryMedicationsStopReason not found with id ' . $request->getParam('id'));
+        }
+        if ($request->getPost('OEModule_OphCiExamination_models_HistoryMedicationsStopReason')) {
+            $model->attributes = $request->getPost('OEModule_OphCiExamination_models_HistoryMedicationsStopReason');
+            if ($model->save()) {
+                Yii::app()->user->setFlash('success', 'Medication Stop Reason saved');
+                $this->redirect(['index']);
+            } else {
+                $errors = $model->getErrors();
+            }
+        }
+        $this->render('/medicationstopreason/edit', [
+            'model' => $model,
+            'errors' => isset($errors) ? $errors : null,
+        ]);
+    }
+
+}
---
+++ protected/modules/OphCiExamination/modules/ExaminationAdmin/views/medicationstopreason/index.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * (C) OpenEyes Foundation, 2019
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.openeyes.org.uk
+ *
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (C) 2019, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
+ */
+?>
+<h2>Medication Stop Reason</h2>
+<?php $this->renderPartial('//base/_messages') ?>
+
+<div class="alert-box error with-icon js-admin-errors" style="display:none">
+    <p>Could not be deleted:</p>
+    <div class="js-admin-error-container"></div>
+</div>
+
+<div class="cols-5">
+    <form id="admin_medication_stop_reason">
+        <input type="hidden" name="YII_CSRF_TOKEN" value="<?= Yii::app()->request->csrfToken ?>"/>
+        <input type="hidden" name="page" value="1">
+        <table class="standard generic-admin">
+            <thead>
+            <tr>
+                <th><input type="checkbox" name="selectall" id="selectall"/></th>
+                <th>Name</th>
+                <th>Active</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($model_list as $i => $model) { ?>
+                <tr class="clickable" data-id="<?= $model->id ?>"
+                    data-uri="OphCiExamination/admin/MedicationStopReason/update/<?= $model->id ?>" >
+                    <td><input type="checkbox" name="select[]" value="<?= $model->id ?>"/></td>
+                    <td class="stop-reason-name">
+                        <?= $model->name ?>
+                    </td>
+                    <td>
+                        <?= ($model->active) ?
+                            ('<i class="oe-i tick small"></i>') :
+                            ('<i class="oe-i remove small"></i>'); ?>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
+            <tfoot>
+            <tr>
+                <td colspan="5">
+                    <?= \CHtml::button(
+                        'Add',
+                        [
+                            'class' => 'button large',
+                            'type' => 'button',
+                            'name' => 'add',
+                            'data-uri' => '/OphCiExamination/admin/MedicationStopReason/create',
+                            'id' => 'et_add'
+                        ]
+                    ); ?>
+                </td>
+            </tr>
+            </tfoot>
+        </table>
+    </form>
+</div>
+
+<script>
+    $(document).ready(function(){
+        $('.stop-reason-name').each(function () {
+            if($(this).html().includes('Medication parameters changed')){
+                console.log($(this).html());
+               $(this).parent('tr').removeClass('clickable');
+               $(this).prepend('<i class="js-has-tooltip oe-i info small pad right"\n' +
+                   '                           data-tooltip-content="This element cannot be edited."></i>')
+            }
+        });
+    });
+
+</script>
