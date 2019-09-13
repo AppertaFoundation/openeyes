f65a8c7180cf5e911336878e85d2ac69415836c0
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-09-13 15:36:41 +0100

    OE-8962 Add risk tag model

---
+++ protected/models/OphCiExaminationRiskTag.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * OpenEyes.
+ *
+ * (C) OpenEyes Foundation, 2019
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.openeyes.org.uk
+ *
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2019, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
+ */
+
+
+/**
+ * This is the model class for table "ophciexamination_risk_tag".
+ *
+ *
+ * @property int $risk_id
+ * @property int $medication_set_id
+ */
+class OphCiExaminationRiskTag extends \BaseActiveRecord
+{
+	protected $auto_update_relations = true;
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 *
+	 * @return static
+	 */
+	public static function model($className = __CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'ophciexamination_risk_tag';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('id, risk_id, medication_set_id', 'safe'),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('risk_id, medication_set_id', 'safe', 'on' => 'search'),
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria = new CDbCriteria();
+
+		$criteria->compare('risk_id', $this->risk_id, true);
+		$criteria->compare('medication_set_id', $this->medication_set_id, true);
+
+		return new CActiveDataProvider(get_class($this), array(
+			'criteria' => $criteria,
+		));
+	}
+}
