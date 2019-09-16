1d19ab9a8ea1624f40dad56b4986ea61d12676f9
harikulendran <hari.kulendran@gmail.com>
2019-09-16 09:21:45 +0100

    OE-8808 Added OE headers to edit views.

--- protected/modules/OphDrPrescription/modules/OphDrPrescriptionAdmin/views/Medication/edit_sets.php
+++ protected/modules/OphDrPrescription/modules/OphDrPrescriptionAdmin/views/Medication/edit_sets.php
@@ -1,5 +1,22 @@
 <?php
-    /** @var Medication $medication */
+/**
+ * OpenEyes
+ *
+ * (C) OpenEyes Foundation, 2019
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2019, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
+ */
+?>
+<?php
+/** @var Medication $medication */
 
     $rowkey = 0;
     $sets = array_map(function ($e) {
@@ -29,7 +46,7 @@
 
     $medicationSetItems = $medication->medicationSetItems;
 
-    ?>
+?>
 <script id="set_row_template" type="x-tmpl-mustache">
     <tr data-key="{{ key }}">
         <td>
