34e0c0212f987b6d3a5eade86bc19d8321f82a88
Daniel Thomson <dwgthomson@gmail.com>
2019-09-18 12:17:54 +1000

    merge fix

--- protected/migrations/m190918_012016_add_recent_context_firm_limit_setting.php
+++ protected/migrations/m190918_012016_add_recent_context_firm_limit_setting.php
@@ -10,7 +10,7 @@ class m190918_012016_add_recent_context_firm_limit_setting extends OEMigration
 			$this->insert('setting_metadata', ['element_type_id' => null,
 					'field_type_id' => 4,
 					'key' => 'recent_context_firm_limit',
-					'name' => 'Recent Context results limit',
+					'name' => 'Limit top Recent Context results',
 					'default_value' => '6',
 					'data' => ''
 				]
