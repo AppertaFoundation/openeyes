2c70f3d4ba3df2bbb42930b9bc279cefa9109f8b
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2019-09-19 10:18:56 +0100

    OE-7519 fix headers

--- protected/modules/OphCiExamination/widgets/PupillaryAbnormalities.php
+++ protected/modules/OphCiExamination/widgets/PupillaryAbnormalities.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * OpenEyes.
- *
  * (C) OpenEyes Foundation, 2019
  * This file is part of OpenEyes.
  * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -11,7 +9,7 @@
  * @link http://www.openeyes.org.uk
  *
  * @author OpenEyes <info@openeyes.org.uk>
- * @copyright Copyright (c) 2019, OpenEyes Foundation
+ * @copyright Copyright (C) 2019, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 
