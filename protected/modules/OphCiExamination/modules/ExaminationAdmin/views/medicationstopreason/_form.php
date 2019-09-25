87ddfef4609e9027ff26d0193f90b074eef77dcd
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2019-09-25 14:16:09 +0100

    OE-9014 change request 2

--- protected/modules/OphCiExamination/modules/ExaminationAdmin/views/medicationstopreason/_form.php
+++ protected/modules/OphCiExamination/modules/ExaminationAdmin/views/medicationstopreason/_form.php
@@ -31,15 +31,6 @@
         <tr>
             <td>Name</td>
             <td class="cols-full">
-                <?php
-                if ($model->isNewRecord) {
-                    $max_display_order = $model::model()->find(['order'=>'display_order DESC'])->display_order;
-                    $new_display_order = $max_display_order + 1;
-                    echo \CHtml::hiddenField(
-                        'OEModule_OphCiExamination_models_HistoryMedicationsStopReason[display_order]',
-                        $new_display_order
-                    );
-                } ?>
                 <?=\CHtml::activeTextField(
                     $model,
                     'name',
