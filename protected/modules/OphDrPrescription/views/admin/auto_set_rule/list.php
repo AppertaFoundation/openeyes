655cd7d862bbe832ab7ab95ca533d780e38c0bef
codefactor-io <support@codefactor.io>
2019-09-30 09:26:46 +0000

    [CodeFactor] Apply fixes to commit 3a8a766
    [ci skip] [skip ci]

--- protected/modules/OphDrPrescription/views/admin/auto_set_rule/list.php
+++ protected/modules/OphDrPrescription/views/admin/auto_set_rule/list.php
@@ -36,7 +36,7 @@ if (!isset($uniqueid)) {
 
         <?php if ($admin->isForceFormDisplay()) : ?>
         <form id="generic-admin-list">
-            <?php endif; ?>
+        <?php endif; ?>
 
         <?php
         if ($admin->getSubListParent() && is_array($admin->getSubListParent())) :
@@ -50,9 +50,9 @@ if (!isset($uniqueid)) {
         ?>
         <input type="hidden" name="returnUri" id="returnUri" value="<?= $returnUri ?>"/>
 
-        <?php else: ?>
+    <?php else : ?>
         <form id="generic-admin-list">
-            <?php endif; ?>
+    <?php endif; ?>
             <input type="hidden" name="page" value="<?php echo Yii::app()->request->getParam('page', 1) ?>"/>
             <input type="hidden" name="YII_CSRF_TOKEN" value="<?php echo Yii::app()->request->csrfToken ?>"/>
 
@@ -67,21 +67,23 @@ if (!isset($uniqueid)) {
                                 <?php if ($admin->isSortableColumn($listItem)) : ?>
                                 <a href="/<?php echo $uniqueid ?>/list?<?php echo $admin->sortQuery($listItem, $displayOrder,
                                     Yii::app()->request->getQueryString()) ?>">
-                                    <?php endif;
+                                <?php endif;
                                 ?>
                                     <?php echo $admin->getModel()->getAttributeLabel($listItem); ?>
                                     <?php if ($admin->isSortableColumn($listItem)) : ?>
                                 </a>
-                            <?php endif;
-                            ?>
+                                    <?php endif;
+                                    ?>
                             </th>
-                        <?php else: ?>
+                        <?php else : ?>
                             <th>Action</th>
                         <?php endif;
                     endforeach; ?>
                 </tr>
                 </thead>
-                <tbody <?php if (in_array('display_order', $admin->getListFields())) : echo 'class="sortable"'; endif; ?>>
+                <tbody <?php if (in_array('display_order', $admin->getListFields())) :
+                    echo 'class="sortable"';
+                       endif; ?>>
                 <?php
                 $retrieveResults = $admin->getSearch()->retrieveResults();
                 foreach ($retrieveResults as $i => $row) { ?>
@@ -98,19 +100,19 @@ if (!isset($uniqueid)) {
                                     if (gettype($attr_val) === 'boolean') :
                                         if ($admin->attributeValue($row, $listItem)) :
                                             ?><i class="oe-i tick small"></i><?php
-                                        else:
+                                        else :
                                             ?><i class="oe-i remove small"></i><?php
                                         endif;
                                     elseif (gettype($attr_val) === 'array') :
-                                        echo implode(',', $admin->attributeValue($row, $listItem));
-                                    elseif ($listItem === 'display_order') :
+                                            echo implode(',', $admin->attributeValue($row, $listItem));
+                                        elseif ($listItem === 'display_order') :
                                         ?>
                                         &uarr;&darr;<input type="hidden" name="<?php echo $admin->getModelName(); ?>[display_order][]" value="<?php echo $row->id ?>">
-                                        <?php
-                                    else:
-                                        echo $attr_val;
-                                    endif
-                                    ?>
+                                            <?php
+                                    else :
+                                            echo $attr_val;
+                                        endif
+                                        ?>
                                 </td>
                             <?php endif;
 
@@ -190,7 +192,7 @@ if (!isset($uniqueid)) {
                 <?php endif; ?>
 
         </div>
-    <?php else: ?>
+            <?php else : ?>
         </form>
-    <?php endif; ?>
+            <?php endif; ?>
     </div>
