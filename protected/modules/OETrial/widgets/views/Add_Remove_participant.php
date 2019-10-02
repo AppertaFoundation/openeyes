662d190bc938574bef4fc18fe9ca99ad7ce6c757
Vikas Nagaraju <envikas@gmail.com>
2019-10-02 18:20:03 +1000

    CERA-574 Removed ability to remove patients from trial if they are not in shortlisted for that trial

--- protected/modules/OETrial/widgets/views/Add_Remove_participant.php
+++ protected/modules/OETrial/widgets/views/Add_Remove_participant.php
@@ -7,10 +7,30 @@
       data-patient-id="<?= $this->patient->id ?>"
       data-trial-id="<?= $this->trial->id ?>"
 >
-<button class="js-add-to-trial"
+<button class="js-add-to-trial button blue hint"
    style="<?= $this->isPatientInTrial() ? 'display:none;' : '' ?>"
->Add to trial</button>
-<button class="js-remove-from-trial"
-   style="<?= $this->isPatientInTrial() ? '' : 'display:none;' ?>"
->Remove from trial</button>
+>Add to Shortlist</button>
+<?php
+$shortlisted_code = TrialPatientStatus::SHORTLISTED_CODE;
+$accepted_code = TrialPatientStatus::ACCEPTED_CODE;
+$rejected_code = TrialPatientStatus::REJECTED_CODE;
+$shortlisted_status = TrialPatientStatus::model()->find('code = ?', array($shortlisted_code));
+$accepted_status = TrialPatientStatus::model()->find('code = ?', array($accepted_code));
+$rejected_status = TrialPatientStatus::model()->find('code = ?', array($rejected_code));
+
+$current_patient_status_id = $this->isPatientInTrial() ? $this->getPatientTrialStatusId($this->patient->id, $this->trial->id) :'';
+if ($current_patient_status_id == $shortlisted_status->id){
+?>
+    <button class="js-remove-from-trial button hint" style="<?= $this->isPatientInTrial() ? '': 'display:none;' ?>">Remove from Shortlist</button>
+<?php
+}else if($current_patient_status_id == $accepted_status->id){
+?>
+    <span class = "alert-box success">Accepted</span>
+<?php
+}else if($current_patient_status_id == $rejected_status->id){
+?>
+    <span class = "alert-box error">Rejected</span>
+<?php
+}
+?>
 </span>
