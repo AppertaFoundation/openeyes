e4257eade04bf5da21064f6bfdb01660e7198fc2
harikulendran <hari.kulendran@gmail.com>
2019-10-02 14:45:35 +0100

    OE-9044 Fixed conflicts.

--- protected/modules/OphDrPrescription/controllers/LocalDrugsAdminController.php
+++ protected/modules/OphDrPrescription/controllers/LocalDrugsAdminController.php
@@ -34,8 +34,6 @@ class LocalDrugsAdminController extends RefMedicationAdminController
           'empty' => '-- None --',
           'class' => 'cols-full disabled',
           'disabled' => 'disabled',
-        <<<<<<< Updated upstream
-          'style' => 'background-color:#f0f0f0',
             ),
                 'hidden' => false,
                 'layoutColumns' => array()
@@ -63,33 +61,4 @@ class LocalDrugsAdminController extends RefMedicationAdminController
             ),
         );
     }
-    =======
-        ),
-                'hidden' => false,
-                'layoutColumns' => array()
-            ),
-            'attributes' => array(
-                'widget' => 'CustomView',
-                'viewName' => 'application.modules.OphDrPrescription.views.admin.medication.edit_attributes',
-                'viewArguments' => array(
-                    'medication' => $model
-                )
-            ),
-                'sets' => array(
-                'widget' => 'CustomView',
-                'viewName' => 'application.modules.OphDrPrescription.views.admin.medication.edit_sets',
-                'viewArguments' => array(
-                    'medication' => $model
-                )
-                ),
-                'alternative_terms' =>  array(
-                'widget' => 'CustomView',
-                'viewName' => 'application.modules.OphDrPrescription.views.admin.medication.edit_alternative_terms',
-                'viewArguments' => array(
-                    'medication' => $model
-                )
-                ),
-            );
-}
->>>>>>> Stashed changes
 }
