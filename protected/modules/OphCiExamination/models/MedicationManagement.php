6f4bad674f32603ae51494d56566a4127ced6d27
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-10-08 15:26:20 +0100

    OE-9099 Use saveAttributes approach with modifying isNewRecord to false , save attribute and set it back to true again to workaround the afterSave issue with not able to save attributes when it's a new record

--- protected/modules/OphCiExamination/models/MedicationManagement.php
+++ protected/modules/OphCiExamination/models/MedicationManagement.php
@@ -415,9 +415,17 @@ class MedicationManagement extends BaseMedicationElement
         }
 
         $this->prescription_id = $prescription_creator->elements['Element_OphDrPrescription_Details']->id;
-        // updte only prescription_id - no validation here
-                \Yii::app()->db->createCommand("UPDATE ".$this->tableName()." SET prescription_id=".$this->prescription_id." WHERE id=".$this->id)->execute();
 
+        // To save in afterSave when it's a new record without doing an sql query we have to set the
+				// the isNewRecord to false , before saving the attribute and setting it back to true afterwards
+				// We're also using saveAttributes to avoid any calls to beforeSave and AfterSave
+				if($this->getIsNewRecord()) {
+					$this->setIsNewRecord(false);
+					$this->saveAttributes(['prescription_id']);
+					$this->setIsNewRecord(true);
+				} else {
+					$this->saveAttributes(['prescription_id']);
+				}
     }
 
     private function getPrescriptionItem(\EventMedicationUse $entry)
