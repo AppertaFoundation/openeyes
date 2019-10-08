201700ed3c6e4e6ecc20d51d51b3a80fe82f9550
harikulendran <hari.kulendran@gmail.com>
2019-10-08 16:39:20 +0100

    OE-9067 Merged migration.

--- protected/migrations/m180504_085420_medication_management_tables.php
+++ protected/migrations/m180504_085420_medication_management_tables.php
@@ -183,13 +183,13 @@ class m180504_085420_medication_management_tables extends OEMigration
         $this->addForeignKey('medication_set_rule_ibfk_1', 'medication_set_rule', 'usage_code_id', 'medication_usage_code', 'id');
 
         $this->insertMultiple('medication_usage_code', [
-            ['usage_code' => 'COMMON_OPH', 'name' => 'Common Ophthalmic Drug Sets'],
-            ['usage_code' => 'COMMON_SYSTEMIC', 'name' => 'Common Systemic Drug  Sets'],
-            ['usage_code' => 'PRESCRIPTION_SET', 'name' => 'Prescription Drug Sets'],
-            ['usage_code' => 'DrugTag', 'name' => 'Drug Tags'],
-            ['usage_code' => 'Formulary', 'name' => 'Formulary Drugs'],
-            ['usage_code' => 'MedicationDrug', 'name' => 'Medication Drug'],
-            ['usage_code' => 'Management', 'name' => 'Management'],
+            ['usage_code' => 'COMMON_OPH', 'name' => 'Common Ophthalmic'],
+            ['usage_code' => 'COMMON_SYSTEMIC', 'name' => 'Common Systemic'],
+            ['usage_code' => 'PRESCRIPTION_SET', 'name' => 'Prescription'],
+            ['usage_code' => 'DrugTag', 'name' => 'Drug Tags', 'hidden' => 1, 'active' => 0],
+            ['usage_code' => 'Formulary', 'name' => 'Formulary'],
+            ['usage_code' => 'MedicationDrug', 'name' => 'Medication Drug', 'hidden' => 1, 'active' => 0],
+            ['usage_code' => 'Management', 'name' => 'Management', 'hidden' => 1, 'active' => 0],
         ]);
     }
 
