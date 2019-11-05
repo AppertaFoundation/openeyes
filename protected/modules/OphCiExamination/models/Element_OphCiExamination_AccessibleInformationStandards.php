ea0ca9e265d726a59b462514efd7e20e3bdb0f1e
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2019-11-05 14:49:42 +0000

    OE-8452 update files with latest OE header

--- protected/modules/OphCiExamination/models/Element_OphCiExamination_AccessibleInformationStandards.php
+++ protected/modules/OphCiExamination/models/Element_OphCiExamination_AccessibleInformationStandards.php
@@ -2,8 +2,7 @@
 /**
  * OpenEyes.
  *
- * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
- * (C) OpenEyes Foundation, 2011-2013
+ * (C) OpenEyes Foundation, 2019
  * This file is part of OpenEyes.
  * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
@@ -12,7 +11,7 @@
  * @link http://www.openeyes.org.uk
  *
  * @author OpenEyes <info@openeyes.org.uk>
- * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
+ * @copyright Copyright (c) 2019, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 
@@ -21,15 +20,14 @@ namespace OEModule\OphCiExamination\models;
 use Yii;
 
 /**
- * This is the model class for table "et_ophciexamination_history".
+ * This is the model class for table "et_ophciexamination_accessible_information_standards".
  *
  * The followings are the available columns in table:
  *
  * @property string $id
  * @property int $event_id
- * @property string $description
+ * @property int $correspondence_in_large_letters
  *
- * The followings are the available model relations:
  */
 class Element_OphCiExamination_AccessibleInformationStandards extends \BaseEventTypeElement
 {
@@ -77,7 +75,6 @@ class Element_OphCiExamination_AccessibleInformationStandards extends \BaseEvent
         // NOTE: you may need to adjust the relation name and the related
         // class name for the relations automatically generated below.
         return array(
-                'eventType' => array(self::BELONGS_TO, 'EventType', 'event_type_id'),
                 'event' => array(self::BELONGS_TO, 'Event', 'event_id'),
                 'user' => array(self::BELONGS_TO, 'User', 'created_user_id'),
                 'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
