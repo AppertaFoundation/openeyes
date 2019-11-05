646169ee924b636326df37332b94642c27cbd373
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2019-11-05 14:47:58 +0000

    OE-8452 fix Accessible Information Standards migration

--- protected/modules/OphCiExamination/migrations/m190604_093420_add_element_accessible_information_standards.php
+++ protected/modules/OphCiExamination/migrations/m190604_093420_add_element_accessible_information_standards.php
@@ -8,26 +8,30 @@ class m190604_093420_add_element_accessible_information_standards extends OEMigr
         $this->createOETable('et_ophciexamination_accessible_information_standards', array(
             'id' => 'pk',
             'event_id' => 'int(10) unsigned NOT NULL',
-            'last_modified_user_id' => 'int(10) unsigned',
-            'last_modified_date' => 'datetime',
-            'created_user_id' => 'int(10) unsigned',
-            'created_date' =>'datetime',
             'correspondence_in_large_letters' => 'int(1) unsigned NOT NULL',
         ), true);
 
         $this->addForeignKey('et_ophciexamination_accessible_information_standards_ev_fk', 'et_ophciexamination_accessible_information_standards', 'event_id', 'event', 'id');
 
+        $this->insert('element_group', array(
+            'name' => 'Accessible Information Standards',
+            'event_type_id' => $this->getIdOfEventTypeByClassName('OphCiExamination'),
+            'display_order' => 10,
+        ));
+
         $this->createElementType('OphCiExamination', 'Accessible Information Standards', array(
             'class_name' => 'OEModule\OphCiExamination\models\Element_OphCiExamination_AccessibleInformationStandards',
-            'display_order' => 1
+            'display_order' => 10,
+            'group_id' => $this->getInsertId('element_group'),
         ));
 	}
 
 	public function down()
 	{
+	    $this->delete('element_group', 'name = ?', ['Accessible Information Standards']);
         $id = $this->getIdOfElementTypeByClassName('OEModule\OphCiExamination\models\Element_OphCiExamination_AccessibleInformationStandards');
-        $this->delete('element_type', 'id = ?', array($id));
+        $this->delete('element_type', 'id = ?', [$id]);
+        $this->dropForeignKey('et_ophciexamination_accessible_information_standards_ev_fk', 'et_ophciexamination_accessible_information_standards');
         $this->dropOETable('et_accessible_information_standards');
 	}
-
 }
