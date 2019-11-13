22bdd1b001a7c2b872153bb472bdc522c722381e
Gurpreet Singh <gurpreet.s.saggu@gmail.com>
2019-11-13 15:51:08 +1100

    Resolved the merge issue.

--- protected/modules/OphCoCorrespondence/views/default/view_ElementLetter.php
+++ protected/modules/OphCoCorrespondence/views/default/view_ElementLetter.php
@@ -15,7 +15,6 @@
  * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
-
 Yii::app()->clientScript->registerScriptFile("{$this->assetPath}/js/pages.js", \CClientScript::POS_HEAD);
 Yii::app()->clientScript->registerScriptFile("{$this->assetPath}/js/imageLoader.js", \CClientScript::POS_HEAD);
 $correspondeceApp = Yii::app()->params['ask_correspondence_approval'];
@@ -24,7 +23,7 @@ $is_mobile_or_tablet = preg_match('/(ipad|iphone|android)/i', Yii::app()->getReq
     <div class="cols-3">
         <table class="cols-full">
             <tbody>
-                <?php if ($correspondeceApp === "on") { ?>
+            <?php if ($correspondeceApp === "on") { ?>
                 <tr>
                     <td class="data-label"><?=\CHtml::encode($element->getAttributeLabel('is_signed_off')) . ' '; ?></td>
                     <td>
@@ -41,187 +40,37 @@ $is_mobile_or_tablet = preg_match('/(ipad|iphone|android)/i', Yii::app()->getReq
                         </div>
                     </td>
                 </tr>
-                <?php } ?>
-                <tr>
-                    <td colspan="2">
-                        <small class="fade">To</small><br>
-                        <?php
-                            $ccString = "";
-                            $toAddress = "";
-                        if ($element->document_instance) {
-                            foreach ($element->document_instance as $instance) {
-                                foreach ($instance->document_target as $target) {
-                                    if ($target->ToCc == 'To') {
-                                           $toAddress = $target->contact_name . "\n" . $target->address;
-                                    } else {
-                                        $contact_type = $target->contact_type != Yii::app()->params['gp_label'] ? ucfirst(strtolower($target->contact_type)) : $target->contact_type;
-                                         $ccString .= "CC: " . ($contact_type != "Other" ? $contact_type . ": " : "") . $target->contact_name . ", " . $element->renderSourceAddress($target->address)."<br/>";
-                                    }
-                                }
-                            }
-                        } else {
-                            $toAddress = $element->address;
-                            foreach (explode("\n", trim($element->cc)) as $line) {
-                                if (trim($line)) {
-                                    $ccString .= "CC: " . str_replace(';', ',', $line)."<br/>";
+            <?php } ?>
+            <tr>
+                <td colspan="2">
+                    <small class="fade">To</small><br>
+                    <?php
+                    $ccString = "";
+                    $toAddress = "";
+                    if ($element->document_instance) {
+                        foreach ($element->document_instance as $instance) {
+                            foreach ($instance->document_target as $target) {
+                                if ($target->ToCc == 'To') {
+                                    $toAddress = $target->contact_name . "\n" . $target->address;
+                                } else {
+                                    $contact_type = $target->contact_type != Yii::app()->params['gp_label'] ? ucfirst(strtolower($target->contact_type)) : $target->contact_type;
+                                    $ccString .= "CC: " . ($contact_type != "Other" ? $contact_type . ": " : "") . $target->contact_name . ", " . $element->renderSourceAddress($target->address)."<br/>";
                                 }
                             }
                         }
-                            echo str_replace("\n", '<br/>', CHtml::encode($toAddress))."<br/>".$ccString;
-                        ?>
-                </td>
-            </tr>
-                        <?php if (Yii::app()->params['institution_code'] === 'CERA') { ?>
-            <tr>
-                <td class="data-label">
-                    From
-                </td>
-                <td>
-                            <?php
-                            if (isset($element->site)) {
-                                echo $element->site->getLetterAddress(array(
-                                    'include_name' => true,
-                                    'delimiter' => '<br />',
-                                    'include_telephone' => true,
-                                    'include_fax' => true,
-                                ));
-                            } else {
-                                echo 'No Site Has Been Selected.';
-                            }
-                            ?>
-                </td>
-            </tr>
-            <tr>
-                <td class="data-label">
-                    Direct Line
-                </td>
-                <td>
-                            <?php
-                            echo $element->direct_line
-                            ?>
-                </td>
-            </tr>
-            <tr>
-                <td class="data-label">
-                    Direct Fax
-                </td>
-                <td >
-                            <?php
-                            echo $element->fax
-                            ?>
-                </td>
-            </tr>
-            <tr>
-                <td class="data-label">
-                    Cc
-                </td>
-                <td>
-                            <?php
-                            $ccString = "";
-                            $toAddress = "";
-
-                            if ($element->document_instance) {
-                                foreach ($element->document_instance as $instance) {
-                                    foreach ($instance->document_target as $target) {
-                                        if ($target->ToCc == 'To') {
-                                            $toAddress = $target->contact_name . "\n" . $target->address;
-                                        } else {
-                                            $contact_type = $target->contact_type != Yii::app()->params['gp_label'] ? ucfirst(strtolower($target->contact_type)) : $target->contact_type;
-
-                                            $ccString .= "" . $contact_type . ": " . $target->contact_name . ", " . $element->renderSourceAddress($target->address)."<br/>";
-                                        }
-                                    }
-                                }
-                            } else {
-                                $toAddress = $element->address;
-                                foreach (explode("\n", trim($element->cc)) as $line) {
-                                    if (trim($line)) {
-                                        $ccString .= "" . str_replace(';', ',', $line)."<br/>";
-                                    }
-                                }
-                            }
-                            ?>
-                            <?php
-                            echo $ccString
-                            ?>
-                </td>
-            </tr>
-                    <?php } ?>
-    <div class="spinner-overlay">
-        <i class="spinner"></i>
-        <img src="#"
-             width="<?=Yii::app()->params['lightning_viewer']['blank_image_template']['width']?>"
-             height="<?=Yii::app()->params['lightning_viewer']['blank_image_template']['height']?>"
-             style="background-color: white;"
-        >
-    </div>
-    <div id="correspondence_out"
-         class="wordbreak correspondence-letter<?php if ($element->draft) {
-?> draft<?php }?> cols-full element"
-            <?php
-         // TODO: Remove this section once newblue is updated to include the correspondence-letterdraft style
-            if ($element->draft) {?>
-         style="background-color: white; color: black; display:none;
-                 background-image: url(<?php echo Yii::app()->assetManager->createUrl('img/bg_draft.png', 'application.modules.OphCoCorrespondence.assets') ?>);
-                 background-position-x: center;
-                 background-position-y: top;
-                 background-size: initial;
-                 background-repeat-x: no-repeat;
-                 background-repeat-y: no-repeat;">
-                <?php }?>
-            <header>
-                <?php
-                $ccString = "";
-                $toAddress = "";
-                if ($element->document_instance) {
-                    foreach ($element->document_instance as $instance) {
-                        foreach ($instance->document_target as $target) {
-                            if ($target->ToCc == 'To') {
-                                 $toAddress = $target->contact_name . "\n" . $target->address;
-                            } else {
-                                 $contact_type = $target->contact_type != Yii::app()->params['gp_label'] ? ucfirst(strtolower($target->contact_type)) : $target->contact_type;
-                                 $ccString .= "CC: " . $contact_type . ": " . $target->contact_name . ", " . $element->renderSourceAddress($target->address)."<br/>";
+                    } else {
+                        $toAddress = $element->address;
+                        foreach (explode("\n", trim($element->cc)) as $line) {
+                            if (trim($line)) {
+                                $ccString .= "CC: " . str_replace(';', ',', $line)."<br/>";
                             }
                         }
                     }
-                } else {
-                    $toAddress = $element->address;
-                    foreach (explode("\n", trim($element->cc)) as $line) {
-                        if (trim($line)) {
-                            $ccString .= "CC: " . str_replace(';', ',', $line)."<br/>";
-                        }
-                    }
-                }
-                $this->renderPartial('letter_start', array(
-                'toAddress' => $toAddress,
-                'patient' => $this->patient,
-                'date' => $element->date,
-                'clinicDate' => $element->clinic_date,
-                'element' => $element,
-                ));
-                ?>
-            </header>
-            <?php
-            $this->renderPartial('reply_address', array(
-                'site' => $element->site,
-                'is_internal_referral' => $element->isInternalReferral(),
-            ));
-            $this->renderPartial('print_ElementLetter', array(
-                'element' => $element,
-                'toAddress' => $toAddress,
-                'ccString' => $ccString,
-                'no_header' => true,
-            ));
-            $is_document = isset($element->document_instance);
-            ?>
-            <input type="hidden" name="OphCoCorrespondence_printLetter" id="OphCoCorrespondence_printLetter" value="<?php echo $element->print?>" />
-            <?php if (Yii::app()->user->getState('correspondece_element_letter_saved', true)) : ?>
-                <?php Yii::app()->user->setState('correspondece_element_letter_saved', false); ?>
-                <input type="hidden" name="OphCoCorrespondence_print_checked" id="OphCoCorrespondence_print_checked" value="<?php echo $is_document ? '1' : '0'; ?>" />
-            <?php else : ?>
-                <input type="hidden" name="OphCoCorrespondence_printLetter" id="OphCoCorrespondence_printLetter_all" value="<?php echo $element->print_all?>" />
-            <?php endif; ?>
-        </div>
+                    echo str_replace("\n", '<br/>', CHtml::encode($toAddress))."<br/>".$ccString;
+                    ?>
+                </td>
+            </tr>
+
             </tbody>
         </table>
     </div>
@@ -243,7 +92,6 @@ $is_mobile_or_tablet = preg_match('/(ipad|iphone|android)/i', Yii::app()->getReq
         // OE-8581 Disable lightning image loading due to speed issues
         options['disableAjaxCall'] = <?= ($is_mobile_or_tablet ? 'false' : 'true'); ?>;
         new OpenEyes.OphCoCorrespondence.ImageLoaderController(OE_event_id , options);
-
         if ((String)($('iframe').data('doprint')).charAt(0) === '1') {
             let eventId = $('iframe').data('eventid');
             $.ajax({
