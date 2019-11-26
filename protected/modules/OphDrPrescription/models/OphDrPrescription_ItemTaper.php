a0e6d5228a3f2488335dd3e179db87b48d7c92d8
codefactor-io <support@codefactor.io>
2019-11-26 12:37:06 +0000

    [CodeFactor] Apply fixes
    [ci skip] [skip ci]

--- protected/modules/OphDrPrescription/models/OphDrPrescription_ItemTaper.php
+++ protected/modules/OphDrPrescription/models/OphDrPrescription_ItemTaper.php
@@ -30,120 +30,120 @@
  */
 class OphDrPrescription_ItemTaper extends BaseActiveRecordVersioned
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 *
-	 * @return OphDrPrescription_ItemTaper the static model class
-	 */
-	public static function model($className = __CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     *
+     * @return OphDrPrescription_ItemTaper the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'ophdrprescription_item_taper';
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'ophdrprescription_item_taper';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('frequency_id, duration_id', 'required'),
-			array('dose, item_id, id', 'safe'),
-			array('dose', 'numerical'),
-			//array('', 'required'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, item_id, dose, frequency_id, duration_id', 'safe', 'on' => 'search'),
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('frequency_id, duration_id', 'required'),
+            array('dose, item_id, id', 'safe'),
+            array('dose', 'numerical'),
+            //array('', 'required'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, item_id, dose, frequency_id, duration_id', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'item' => array(self::BELONGS_TO, 'OphDrPrescription_Item', 'item_id'),
-			'duration' => array(self::BELONGS_TO, MedicationDuration::class, 'duration_id'),
-			'frequency' => array(self::BELONGS_TO, MedicationFrequency::class, 'frequency_id'),
-			'user' => array(self::BELONGS_TO, 'User', 'created_user_id'),
-			'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'item' => array(self::BELONGS_TO, 'OphDrPrescription_Item', 'item_id'),
+            'duration' => array(self::BELONGS_TO, MedicationDuration::class, 'duration_id'),
+            'frequency' => array(self::BELONGS_TO, MedicationFrequency::class, 'frequency_id'),
+            'user' => array(self::BELONGS_TO, 'User', 'created_user_id'),
+            'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'duration_id' => 'Duration',
-			'frequency_id' => 'Frequency',
-		);
-	}
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'duration_id' => 'Duration',
+            'frequency_id' => 'Frequency',
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria = new CDbCriteria();
+        $criteria = new CDbCriteria();
 
-		$criteria->compare('id', $this->id, true);
-		$criteria->compare('dose', $this->dose, true);
-		$criteria->compare('item_id', $this->item_id, true);
-		$criteria->compare('duration_id', $this->duration_id, true);
-		$criteria->compare('frequency_id', $this->frequency_id, true);
+        $criteria->compare('id', $this->id, true);
+        $criteria->compare('dose', $this->dose, true);
+        $criteria->compare('item_id', $this->item_id, true);
+        $criteria->compare('duration_id', $this->duration_id, true);
+        $criteria->compare('frequency_id', $this->frequency_id, true);
 
-		return new CActiveDataProvider(get_class($this), array(
-			'criteria' => $criteria,
-		));
-	}
+        return new CActiveDataProvider(get_class($this), array(
+            'criteria' => $criteria,
+        ));
+    }
 
-	public function getDescription()
-	{
-		$return = 'then ' . $this->dose;
-		$return .= ' ' . $this->frequency->term;
-		$return .= ' for ' . $this->duration->name;
+    public function getDescription()
+    {
+        $return = 'then ' . $this->dose;
+        $return .= ' ' . $this->frequency->term;
+        $return .= ' for ' . $this->duration->name;
 
-		return $return;
-	}
+        return $return;
+    }
 
-	public function compareTo(OphDrPrescription_ItemTaper $taper)
-	{
-		$fields = array('frequency_id, duration_id', 'dose');
-		return $this->getAttributes($fields) == $taper->getAttributes($fields);
-	}
+    public function compareTo(OphDrPrescription_ItemTaper $taper)
+    {
+        $fields = array('frequency_id, duration_id', 'dose');
+        return $this->getAttributes($fields) == $taper->getAttributes($fields);
+    }
 
 
-	public function fpTenFrequency()
-	{
-		if (preg_match("/^\d+/", $this->duration->name)) {
-			return "Frequency: {$this->frequency->long_name} for {$this->duration->name}";
-		}
+    public function fpTenFrequency()
+    {
+        if (preg_match("/^\d+/", $this->duration->name)) {
+            return "Frequency: {$this->frequency->long_name} for {$this->duration->name}";
+        }
 
-		return 'Frequency: ' . $this->frequency->long_name . ' ' . strtolower($this->duration->name);
-	}
+        return 'Frequency: ' . $this->frequency->long_name . ' ' . strtolower($this->duration->name);
+    }
 
-	public function fpTenDose()
-	{
-		return 'Dose: ' . (is_numeric($this->dose) ? "{$this->dose} {$this->item->drug->dose_unit}" : $this->dose)
-			. ', ' . $this->item->route->name . ($this->item->laterality ? ' (' . $this->item->laterality->name . ')' : null);
-	}
+    public function fpTenDose()
+    {
+        return 'Dose: ' . (is_numeric($this->dose) ? "{$this->dose} {$this->item->drug->dose_unit}" : $this->dose)
+            . ', ' . $this->item->route->name . ($this->item->laterality ? ' (' . $this->item->laterality->name . ')' : null);
+    }
 }
