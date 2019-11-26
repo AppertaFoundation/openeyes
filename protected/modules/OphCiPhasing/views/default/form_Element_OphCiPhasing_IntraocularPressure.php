8340c9b1caa1242af7940fb84db98011b72d25c3
Gurpreet Singh <gurpreet.s.saggu@gmail.com>
2019-11-26 12:46:32 +1100

    Removed max-height attribute.

--- protected/modules/OphCiPhasing/views/default/form_Element_OphCiPhasing_IntraocularPressure.php
+++ protected/modules/OphCiPhasing/views/default/form_Element_OphCiPhasing_IntraocularPressure.php
@@ -79,7 +79,7 @@ $key = 0;
           </table>
         </div>
       </fieldset>
-        <?php echo $form->textArea($element, 'right_comments', array(), false, array('class' => 'autosize', 'placeholder' => 'Enter comments ...', 'style' => 'max-height:200px'), array('label' => 2, 'field' => 10)) ?>
+        <?php echo $form->textArea($element, 'right_comments', array(), false, array('class' => 'autosize', 'placeholder' => 'Enter comments ...'), array('label' => 2, 'field' => 10)) ?>
     </div>
     <div class="inactive-form" <?= $element->hasRight() ? 'style="display: none;"' : '';?>>
       <div class="add-side">
