aa0064c13aa2d37534020a453ee674c02a76411b
codefactor-io <support@codefactor.io>
2019-12-09 14:34:23 +0000

    [CodeFactor] Apply fixes to commit 733edad
    [ci skip] [skip ci]

--- protected/migrations/m191209_124917_add_comments_to_drops.php
+++ protected/migrations/m191209_124917_add_comments_to_drops.php
@@ -2,19 +2,19 @@
 
 class m191209_124917_add_comments_to_drops extends CDbMigration
 {
-	public function up()
-	{
-	    $this->addColumn('et_ophciexamination_dilation', 'left_comments', 'text');
+    public function up()
+    {
+        $this->addColumn('et_ophciexamination_dilation', 'left_comments', 'text');
         $this->addColumn('et_ophciexamination_dilation_version', 'left_comments', 'text');
         $this->addColumn('et_ophciexamination_dilation', 'right_comments', 'text');
         $this->addColumn('et_ophciexamination_dilation_version', 'right_comments', 'text');
-	}
+    }
 
-	public function down()
-	{
+    public function down()
+    {
         $this->dropColumn('et_ophciexamination_dilation', 'left_comments');
         $this->dropColumn('et_ophciexamination_dilation', 'right_comments');
         $this->dropColumn('et_ophciexamination_dilation_version', 'left_comments');
         $this->dropColumn('et_ophciexamination_dilation_version', 'right_comments');
-	}
+    }
 }
