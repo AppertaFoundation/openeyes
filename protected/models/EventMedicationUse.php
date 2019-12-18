3f1ab977554808bb3f9a7283182ec5fb400313c8
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2019-12-18 10:49:13 +0000

    OE-9335 tooltip content update: add start date for stopped medications

--- protected/models/EventMedicationUse.php
+++ protected/models/EventMedicationUse.php
@@ -397,8 +397,12 @@ class EventMedicationUse extends BaseElement
             $data['Dosage'] = $this->getDoseAndFrequency();
         }
 
-        if ($this->end_date) {
-            $data['Stop date'] = Helper::formatFuzzyDate($this->end_date);
+        if (!is_null($this->end_date)) {
+            if ($this->end_date <= date('Y-m-d')) {
+                $data['Start date'] = Helper::formatFuzzyDate($this->start_date);
+            } else {
+                $data['Stop date'] = Helper::formatFuzzyDate($this->end_date);
+            }
             if ($this->stop_reason_id) {
                 $data['Stop reason'] = $this->stopReason->name;
             }
