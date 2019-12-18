d2ee11ccdae2218d65ee78451bbd6b09c9472233
AdrianKamulegeya <adriankamulegeya@gmail.com>
2019-12-18 12:24:31 +0000

    OE-7696 Separated migration into 2 separate migrations

--- protected/migrations/m191125_091623_create_event_icon_table.php
+++ protected/migrations/m191125_091623_create_event_icon_table.php
@@ -5,7 +5,6 @@ class m191125_091623_create_event_icon_table extends CDbMigration
 
     public function up()
     {
-
         $icon_names = ['i-genCiDiamond', 'i-genCiDoc', 'i-genCiDocHole', 'i-genCiFour', 'i-genCiGrid',
             'i-genCiNut', 'i-genCiSquare', 'i-genCiSquareDot', 'i-genCiSquareSplit', 'i-genCiSquareWin',
             'i-genCiStack', 'i-genCiTarget', 'i-genCoDiamond', 'i-genCoDoc', 'i-genCoDocHole', 'i-genCoFour',
@@ -18,24 +17,6 @@ class m191125_091623_create_event_icon_table extends CDbMigration
             'i-genNuDocHole', 'i-genNuFour', 'i-genNuGrid', 'i-genNuNut', 'i-genNuSquare', 'i-genNuSquareDot',
             'i-genNuSquareSplit', 'i-genNuSquareWin', 'i-genNuStack', 'i-genNuTarget'];
 
-        $sub_types = [
-            'General' => 'i-genCiDocHole',
-            'Biometry Report' => 'i-genImTarget',
-            'Referral Letter' => 'i-genCoNut',
-            'OCT' => 'i-genImSquare',
-            'Electrocardiogram' => 'i-genCiDocHole',
-            'Photograph' => 'i-genImSquareDot',
-            'Consent Form' => 'i-genCoSquareWin',
-            'Visual Field Report' => 'i-genImDiamond',
-            'Lids Photo' => 'i-genCiDocHole',
-            'Orbit Photo' => 'i-genCiDocHole',
-            'Video' => 'i-genCoFour',
-            'Refraction' => 'i-genCiFour',
-            'Retcam' => 'i-genCiDocHole',
-            'Toric IOL Calculation' => 'i-genImSquareSplit',
-            'Ultrasound' => 'i-genImStack',
-        ];
-
         $this->createTable('sub_type_event_icon', [
             'id' => 'pk',
             'name' => 'varchar(64) not null',
@@ -46,24 +27,10 @@ class m191125_091623_create_event_icon_table extends CDbMigration
             $key = ($key + 1) * 10;
             $this->insert('sub_type_event_icon', ['name' => $event_icon, 'display_order' => $key]);
         }
-
-        $this->addColumn('ophcodocument_sub_types', 'sub_type_event_icon_id', 'int(11)');
-        $this->addForeignKey('document_sub_type_event_icon_id_fk', 'ophcodocument_sub_types', 'sub_type_event_icon_id', 'sub_type_event_icon', 'id');
-
-        $this->addColumn('ophcodocument_sub_types_version', 'sub_type_event_icon_id', 'int(10) unsigned');
-
-        foreach ($sub_types as $sub_type => $icon) {  //set default values for document sub types
-            $event_icon = SubTypeEventIcon::model()->find('name = ?', [$icon])->id;
-            $this->update('ophcodocument_sub_types', ['sub_type_event_icon_id' => $event_icon], 'name="'.$sub_type .'"');
-        }
-
     }
 
     public function down()
     {
-        $this->dropForeignKey('document_sub_type_event_icon_id_fk', 'ophcodocument_sub_types');
-        $this->dropColumn('ophcodocument_sub_types', 'sub_type_event_icon_id');
-        $this->dropColumn('ophcodocument_sub_types_version', 'sub_type_event_icon_id');
         $this->dropTable('sub_type_event_icon');
     }
 
---
+++ protected/modules/OphCoDocument/migrations/m191218_121522_add_sub_type_event_icons_to_document_event.php
@@ -0,0 +1,43 @@
+<?php
+
+class m191218_121522_add_sub_type_event_icons_to_document_event extends CDbMigration
+{
+	public function up()
+	{
+        $sub_types = [
+            'General' => 'i-genCiDocHole',
+            'Biometry Report' => 'i-genImTarget',
+            'Referral Letter' => 'i-genCoNut',
+            'OCT' => 'i-genImSquare',
+            'Electrocardiogram' => 'i-genCiDocHole',
+            'Photograph' => 'i-genImSquareDot',
+            'Consent Form' => 'i-genCoSquareWin',
+            'Visual Field Report' => 'i-genImDiamond',
+            'Lids Photo' => 'i-genCiDocHole',
+            'Orbit Photo' => 'i-genCiDocHole',
+            'Video' => 'i-genCoFour',
+            'Refraction' => 'i-genCiFour',
+            'Retcam' => 'i-genCiDocHole',
+            'Toric IOL Calculation' => 'i-genImSquareSplit',
+            'Ultrasound' => 'i-genImStack',
+        ];
+
+        $this->addColumn('ophcodocument_sub_types', 'sub_type_event_icon_id', 'int(11)');
+        $this->addForeignKey('document_sub_type_event_icon_id_fk', 'ophcodocument_sub_types', 'sub_type_event_icon_id', 'sub_type_event_icon', 'id');
+
+        $this->addColumn('ophcodocument_sub_types_version', 'sub_type_event_icon_id', 'int(10) unsigned');
+
+        foreach ($sub_types as $sub_type => $icon) {  //set default values for document sub types
+            $event_icon = SubTypeEventIcon::model()->find('name = ?', [$icon])->id;
+            $this->update('ophcodocument_sub_types', ['sub_type_event_icon_id' => $event_icon], 'name="'.$sub_type .'"');
+        }
+	}
+
+	public function down()
+	{
+        $this->dropForeignKey('document_sub_type_event_icon_id_fk', 'ophcodocument_sub_types');
+        $this->dropColumn('ophcodocument_sub_types', 'sub_type_event_icon_id');
+        $this->dropColumn('ophcodocument_sub_types_version', 'sub_type_event_icon_id');
+	}
+
+}
