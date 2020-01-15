f9cafe6851ba0df5e3465004372676406950076b
Szabolcs Potondi <szabolcs.potondi@gmail.com>
2020-01-15 20:08:52 +0000

    OE-9448 - fix to use usage_code_id instead of usage_code itself

--- protected/migrations/data/dmd_import/ref_medication_sets_load.sql
+++ protected/migrations/data/dmd_import/ref_medication_sets_load.sql
@@ -1,3 +1,8 @@
+#hmm... without this the import fails... still don't know why
+SELECT medication_set_item.`medication_set_id`, id
+FROM medication_set_item
+WHERE medication_set_item.`medication_set_id` NOT IN (SELECT id FROM medication_set);
+
 LOAD DATA INFILE '/tmp/medication_set.csv' INTO TABLE medication_set_item
 (medication_id,medication_set_id,default_form_id,default_route_id)
 SET default_dose = NULL, default_frequency_id = NULL, default_dose_unit_term = NULL, deleted_date = NULL;
