90f664f21871e17d1e621aa188913bbfbd58af7e
@=AdrianKamulegeya <@=adrian.kamulegeya@gmail.com>
2020-01-17 14:42:36 +0000

    OE-9457 added dfrl_validation_error variable

--- protected/modules/OphCiExamination/widgets/views/HistoryMedicationsEntry_event_edit.php
+++ protected/modules/OphCiExamination/widgets/views/HistoryMedicationsEntry_event_edit.php
@@ -44,6 +44,10 @@ $allergy_ids = !is_null($entry->medication_id) ?
     }, $entry->medication->allergies)) :
     [];
 
+$dfrl_validation_error = array_intersect(
+    array("end_date", "stop_reason_id", "laterality"),
+    array_keys($entry->errors));
+
 ?>
 
 <tr
@@ -221,10 +225,10 @@ $allergy_ids = !is_null($entry->medication_id) ?
 
             <span id="<?= $model_name . "_entries_" . $row_count . "_stop_reason_id_error" ?>"
                         class="js-stop-reason-select cols-5"
-                        style="<?=  !($entry->hasErrors('stop_reason_id')) && ($is_new || is_null($entry->end_date)) ? "display:none" : "" ?>">
+                        style="<?=  !$dfrl_validation_error && ($is_new || is_null($entry->end_date)) ? "display:none" : "" ?>">
             <?= CHtml::dropDownList($field_prefix . '[stop_reason_id]', $entry->stop_reason_id, $stop_reason_options, array('empty' => 'Reason stopped?', 'class' => ' js-stop-reason')) ?>
         </span>
-            <div class="js-stop-reason-text" style="<?= $is_new || is_null($entry->end_date) ? "" : "display:none" ?>">
+            <div class="js-stop-reason-text" style="<?=  !$dfrl_validation_error && ($is_new || is_null($entry->end_date)) ? "" : "display:none" ?>">
                 <?= !is_null($entry->stop_reason_id) ? $entry->stopReason->name : ''; ?>
             </div>
         </div>
