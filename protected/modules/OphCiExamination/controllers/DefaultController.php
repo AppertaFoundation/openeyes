1e44fe84b44a2bbd12c18c08387747546f03df91
@=AdrianKamulegeya <@=adrian.kamulegeya@gmail.com>
2020-01-21 11:54:59 +0000

    OE-9452 Fixed error on intial prescription creation, edited audit log to

--- protected/modules/OphCiExamination/controllers/DefaultController.php
+++ protected/modules/OphCiExamination/controllers/DefaultController.php
@@ -1446,15 +1446,24 @@ class DefaultController extends \BaseEventTypeController
                 $reason_other_id = $reason_other->id ?: null;
             }
 
-            $edit_reason = \OphDrPrescriptionEditReasons::model()->findByPk($data['prescription_reason']);
             $prescription = $element->prescription;
-            $prescription->edit_reason_id = $edit_reason->id;
-            $prescription->edit_reason_other .= $data['prescription_reason'] === $reason_other_id ? ': ' .$data['reason_other'] : ': ' .$edit_reason->caption;;
+            $audit_log = 'Reason: ';
+            $audit_prescription_edit_reason = '';
+            if ($data['prescription_reason'] === $reason_other_id) {
+                $prescription->edit_reason_other = $data['reason_other'];
+                $audit_log .= $prescription->edit_reason_other;
+                $audit_prescription_edit_reason = $prescription->edit_reason_other;
+            } else if ($data['prescription_reason']) {
+                $edit_reason = \OphDrPrescriptionEditReasons::model()->findByPk($data['prescription_reason']);
+                $prescription->edit_reason_id = $edit_reason->id;
+                $audit_log .= $edit_reason->caption;
+                $audit_prescription_edit_reason = $edit_reason->caption;
+            }
 
             if (!$prescription->save()) {
                 throw new \Exception("Error while saving prescription: ".print_r($prescription->getErrors(), true));
             }
-            $prescription->event->audit('event', 'update', serialize($prescription->attributes), $prescription->edit_reason_other, array('module' => 'Prescription', 'model' => 'Element_OphDrPrescription_Details'));
+            $prescription->event->audit('event', 'update', serialize(array_merge($prescription->attributes, ['prescription_edit_reason' => $audit_prescription_edit_reason])), $audit_log, array('module' => 'Prescription', 'model' => 'Element_OphDrPrescription_Details'));
         }
     }
 
