bb3e472026067eb4fcf718df7b47d1f16519d205
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2020-01-22 15:41:36 +0000

    OE-9476 use doctor grade model

--- protected/migrations/m200121_154701_add_short_names_to_doctor_grade.php
+++ protected/migrations/m200121_154701_add_short_names_to_doctor_grade.php
@@ -7,11 +7,11 @@ class m200121_154701_add_short_names_to_doctor_grade extends OEMigration
         $short_names = ['Specialty trainee (year 1)' => 'ST1', 'Specialty trainee (year 2)' => 'ST2', 'Specialty trainee (year 3)' => 'ST3', 'Specialty trainee (year 4)' => 'ST4', 'Specialty trainee (year 5)' => 'ST5', 'Specialty trainee (year 6)' => 'ST6'];
         $this->addColumn('doctor_grade', 'short_name', 'varchar(255)');
 
-        foreach ($this->getDbConnection()->createCommand('SELECT grade from doctor_grade')->queryAll() as $row) {
-            if (array_key_exists($row['grade'], $short_names)) {
-                $this->update('doctor_grade', array('short_name' => $short_names[$row['grade']]), "grade='{$row['grade']}'");
+        foreach (DoctorGrade::model()->findAll() as $doctor_grade) {
+            if (array_key_exists($doctor_grade->grade, $short_names)) {
+                $this->update('doctor_grade', array('short_name' => $short_names[$doctor_grade->grade]), "grade='{$doctor_grade->grade}'");
             } else {
-                $this->update('doctor_grade', array('short_name' => $row['grade']), "grade='{$row['grade']}'");
+                $this->update('doctor_grade', array('short_name' => $doctor_grade->grade), "grade='{$doctor_grade->grade}'");
             }
         }
     }
