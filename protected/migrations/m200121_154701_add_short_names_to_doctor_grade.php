bdc091cd62fd52436e8855a61a0e1a8943e444e4
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2020-01-22 16:37:14 +0000

    OE-9476 add new short names

--- protected/migrations/m200121_154701_add_short_names_to_doctor_grade.php
+++ protected/migrations/m200121_154701_add_short_names_to_doctor_grade.php
@@ -4,7 +4,30 @@ class m200121_154701_add_short_names_to_doctor_grade extends OEMigration
 {
     public function up()
     {
-        $short_names = ['Specialty trainee (year 1)' => 'ST1', 'Specialty trainee (year 2)' => 'ST2', 'Specialty trainee (year 3)' => 'ST3', 'Specialty trainee (year 4)' => 'ST4', 'Specialty trainee (year 5)' => 'ST5', 'Specialty trainee (year 6)' => 'ST6'];
+        $short_names = [
+            'Consultant' => 'Cons',
+            'Locum Consultant' => 'Locum Cons',
+            'Associate Specialist' => 'Associate Sp',
+            'Registrar' => 'StR',
+            'Staff Grade' => 'Staff Gr',
+            'Trust Doctor' => 'Trust Dr',
+            'Specialty trainee (year 1)' => 'ST1',
+            'Specialty trainee (year 2)' => 'ST2',
+            'Specialty trainee (year 3)' => 'ST3',
+            'Specialty trainee (year 4)' => 'ST4',
+            'Specialty trainee (year 5)' => 'ST5',
+            'Specialty trainee (year 6)' => 'ST6',
+            'Foundation Year 1 Doctor' => 'F1',
+            'Foundation Year 2 Doctor' => 'F2',
+            'GP with a special interest in ophthalmology' => 'Ophthalmic GP',
+            'Clinical nurse specialist' => 'Specialist nurse',
+            'Health Care Assistant' => 'HCA',
+            'Ophthalmic Technician' => 'OpTech',
+            'Surgical Care Practitioner' => 'SCP',
+            'Clinical Assistant' => 'CA',
+            'Administration staff' => 'Admin',
+        ];
+
         $this->addColumn('doctor_grade', 'short_name', 'varchar(255)');
 
         foreach (DoctorGrade::model()->findAll() as $doctor_grade) {
