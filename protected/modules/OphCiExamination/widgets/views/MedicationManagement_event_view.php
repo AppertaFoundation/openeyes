8819410a029de1b4308a58bde0cf797c2d1f31b7
=biskyt <=biskyt@users.noreply.github.com>
2020-01-22 16:06:21 +0000

    OE-7023 - add data-key for MM view table

--- protected/modules/OphCiExamination/widgets/views/MedicationManagement_event_view.php
+++ protected/modules/OphCiExamination/widgets/views/MedicationManagement_event_view.php
@@ -42,12 +42,13 @@
                             ) as $entry_icon => $method) :
             $entries = $element->$method();
     if (!empty($entries)) : ?>
-                <?php foreach ($entries as $entry) : ?>
+                <?php foreach ($entries as $key => $entry) : ?>
                     <?php echo $this->render(
                         'MedicationManagementEntry_event_view',
                         [
                             'entry' => $entry,
-                            'entry_icon' => $entry_icon
+                            'entry_icon' => $entry_icon,
+                            'row_count' => $key
                         ]
                     ); ?>
                 <?php endforeach; ?>
