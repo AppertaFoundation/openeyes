160475d5fe3000463630bc9851ef0ab9c861fe35
Daniel Thomson <dwgthomson@gmail.com>
2020-02-06 16:58:30 +1100

    OE-9531 changed to Yii's getModule instead of getAPI
    This is so that images for modules without API's will still get generated through the command line, similar to lightning viewer.

--- protected/models/EventImage.php
+++ protected/models/EventImage.php
@@ -107,7 +107,7 @@ class EventImage extends BaseActiveRecord
         // restrict to only include events from modules that are loaded
         return array_filter($events, function($event) {
             /* @var Event $event */
-            return $event->eventType->getApi();
+            return Yii::app()->getModule($event->eventType->class_name);
         });
     }
 
