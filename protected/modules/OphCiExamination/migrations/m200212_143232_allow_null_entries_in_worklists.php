cfff499a6227f6011b2477074f72e40225555439
Hari Kulendran <me@hari.dev>
2020-02-12 14:45:17 +0000

    OE-9560 Fix worklist id issues

---
+++ protected/modules/OphCiExamination/migrations/m200212_143232_allow_null_entries_in_worklists.php
@@ -0,0 +1,18 @@
+<?php
+
+class m200212_143232_allow_null_entries_in_worklists extends CDbMigration
+{
+	public function up()
+  {
+    $this->alterColumn('worklist_definition_display_context', 'worklist_definition_id', 'INTEGER(11) NULL');
+    $this->alterColumn('worklist_definition_mapping', 'worklist_definition_id', 'INTEGER(11) NULL');
+    $this->alterColumn('worklist_definition_mapping_value', 'worklist_definition_mapping_id', 'INTEGER(11) NULL');
+	}
+
+	public function down()
+	{
+    $this->alterColumn('worklist_definition_display_context', 'worklist_definition_id', 'INTEGER(11) NOT NULL');
+    $this->alterColumn('worklist_definition_mapping', 'worklist_definition_id', 'INTEGER(11) NOT NULL');
+    $this->alterColumn('worklist_definition_mapping_value', 'worklist_definition_mapping_id', 'INTEGER(11) NOT NULL');
+	}
+}
