6b7af7c9fb9c3192be15e97f5a18dc9167d96350
Vikas Nagaraju <envikas@gmail.com>
2020-02-26 10:25:27 +1100

    OE-9558 Finding out the performance hit if the index search html is generated on the fly whenever the Examination event is loaded.

--- protected/widgets/IndexSearch.php
+++ protected/widgets/IndexSearch.php
@@ -16,16 +16,28 @@ class IndexSearch extends BaseCWidget
     public function run()
     {
         try {
-//            Fetching the index_search PHP content from the event_type table
-            $criteria = new CDbCriteria();
-            $criteria->select = 'index_search_content';
-            $criteria->addCondition("name =:event_name");
-            $criteria->params[":event_name"] = $this->event_type;
-            $php_output = EventType::model()->find($criteria);
-//            Capturing the contents of the index_search column into a variable
-            $render_content = ($php_output['index_search_content']);
-//            This evaluates the generated php and HTML code obtained from the DB
-            return eval("?>$render_content");
+////            Fetching the index_search PHP content from the event_type table
+//            $criteria = new CDbCriteria();
+//            $criteria->select = 'index_search_content';
+//            $criteria->addCondition("name =:event_name");
+//            $criteria->params[":event_name"] = $this->event_type;
+//            $php_output = EventType::model()->find($criteria);
+////            Capturing the contents of the index_search column into a variable
+//            $render_content = ($php_output['index_search_content']);
+////            This evaluates the generated php and HTML code obtained from the DB
+/*            return eval("?>$render_content");*/
+              $commandPath = Yii::app()->getBasePath().DIRECTORY_SEPARATOR.'commands';
+              $runner = new CConsoleCommandRunner();
+              $runner->addCommands($commandPath);
+              if (Yii::app()->params['index_search_examination'] == null) {
+                  $args = array('yiic', 'eyedrawconfigload', '--filename=' . Yii::app()->getBasePath() . '/config/core/OE_ED_CONFIG.xml');
+                  ob_start();
+                  $runner->run($args);
+                  Yii::log('here');
+              }
+              $render_content = Yii::app()->params['index_search_examination'] ;
+              return eval("?>$render_content");
+
         } catch (Exception $e) {
           //view does not exist in DB
         }
