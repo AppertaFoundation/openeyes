269ea2a002df0075cc4c7f6860ebe9d4dc856944
biskyt <biskyt@users.noreply.github.com>
2020-02-27 08:54:48 +0000

    switch session back to db

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -211,7 +211,7 @@ return array(
                 'services\PatientService',
             ),
         ),
-        /*'session' => array(
+        'session' => array(
             'class' => 'OESession',
             'connectionID' => 'db',
             'sessionTableName' => 'user_session',
@@ -219,7 +219,7 @@ return array(
             /*'cookieParams' => array(
                 'lifetime' => 300,
             ),*/
-        /*),*/
+        ),
         'urlManager' => array(
             'urlFormat' => 'path',
             'showScriptName' => false,
