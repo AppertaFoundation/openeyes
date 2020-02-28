60789f13b5af8e3b4fc93cba009ec3cc54cc95fe
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2020-02-28 09:44:46 +0000

    OE-9623 Do not output address if the patient doesn't have one

--- protected/modules/OphDrPrescription/components/views/form_print_header.php
+++ protected/modules/OphDrPrescription/components/views/form_print_header.php
@@ -22,22 +22,28 @@
                 <div class="fpten-form-row">
                     <?= $this->patient->fullname ?><br/>
                     <br/>
+                    <?php if(isset($this->patient->contact->address)) { ?>
                     <?= $this->patient->contact->address->address1 ?><br/>
                     <?= $this->patient->contact->address->address2 ?><?= $this->patient->contact->address->address2 ? '<br/>' : null ?>
+                    <?php } ?>
                 </div>
                 <div class="fpten-form-row">
                     <table>
                         <tbody>
                         <tr>
                             <td <?= ($form_css_class !== 'wpten') ? 'style="width: 70%"' : ''?>>
+                                <?php if(isset($this->patient->contact->address)) { ?>
                                 <?= $this->patient->contact->address->city ?><br/>
                                 <?= $this->patient->contact->address->county ?><br/><?= $form_css_class !== 'wpten' ? '<br/>' : null ?>
                                 <?= (!$this->patient->contact->address->address2 && $form_css_class !== 'wpten') ? '<br/>' : null ?>
                                 <?= (!$this->patient->contact->address->county && $form_css_class !== 'wpten') ? '<br/>' : null ?>
+                                <?php } ?>
                             </td>
                             <td>
+                                <?php if(isset($this->patient->contact->address)) { ?>
                                 <?= $this->patient->contact->address->county ? '<br/>' : null ?>
                                 <?= $this->patient->contact->address->postcode ?>
+                                <?php } ?>
                             </td>
                         </tr>
                         <?php if ($form_css_class === 'wpten') : ?>
