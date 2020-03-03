9106a24a1ebd63f6607ecca3449830a0d82f387f
Mike Smith <mike.smith@camc-ltd.co.uk>
2020-03-03 14:13:58 +0000

    method symmetry for OE* column operations

--- protected/modules/OphCiExamination/migrations/m191002_003710_add_display_on_whiteboard_column.php
+++ protected/modules/OphCiExamination/migrations/m191002_003710_add_display_on_whiteboard_column.php
@@ -9,7 +9,7 @@ class m191002_003710_add_display_on_whiteboard_column extends OEMigration
 
     public function up()
     {
-        $this->addOEColumn('ophciexamination_risk', 'display_on_whiteboard', 'tinyint(1) DEFAULT 1');
+        $this->addOEColumn('ophciexamination_risk', 'display_on_whiteboard', 'tinyint(1) DEFAULT 1', true);
         $this->alterView('patient_risk_assignment', <<<EOSQL
 select ra.id, latest.patient_id as patient_id, ra.risk_id, ra.other, ra.comments, ra.last_modified_user_id, risk.display_on_whiteboard,
 ra.last_modified_date,
