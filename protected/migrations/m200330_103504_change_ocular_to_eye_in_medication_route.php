3d73dc3969415441eb00c2626401244225baa5c0
codefactor-io <support@codefactor.io>
2020-03-30 10:45:28 +0000

    [CodeFactor] Apply fixes to commit f439075
    [ci skip] [skip ci]

--- protected/migrations/m200330_103504_change_ocular_to_eye_in_medication_route.php
+++ protected/migrations/m200330_103504_change_ocular_to_eye_in_medication_route.php
@@ -2,13 +2,13 @@
 
 class m200330_103504_change_ocular_to_eye_in_medication_route extends CDbMigration
 {
-	public function up()
-	{
+    public function up()
+    {
         $this->update('medication_route', array('term' => 'Eye'), "term = 'Ocular'");
-	}
+    }
 
-	public function down()
-	{
+    public function down()
+    {
         $this->update('medication_route', array('term' => 'Ocular'), "term = 'Eye'");
-	}
+    }
 }
