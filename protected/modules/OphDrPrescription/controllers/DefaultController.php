dba5b78ef5c1280dbc89529cd533211aea0ef1d3
Justin <justinas.cepkauskas@gmail.com>
2020-03-31 11:34:27 +0100

    oe-9736 Delete checking if it's true

--- protected/modules/OphDrPrescription/controllers/DefaultController.php
+++ protected/modules/OphDrPrescription/controllers/DefaultController.php
@@ -720,8 +720,8 @@ class DefaultController extends BaseEventTypeController
                 $item->dose = $source->default_dose;
                 $item->dose_unit_term = $source->default_dose_unit_term ? $source->default_dose_unit_term : $source->medication->default_dose_unit_term;
                 $item->route_id = $source->default_route_id ? $source->default_route_id : $source->medication->default_route_id;
-                $item->dispense_condition_id = $source->default_dispense_condition_id ? $source->default_dispense_condition_id : null;
-                $item->dispense_location_id = $source->default_dispense_location_id ? $source->default_dispense_location_id : null;
+                $item->dispense_condition_id = $source->default_dispense_condition_id;
+                $item->dispense_location_id = $source->default_dispense_location_id;
                 $item->duration_id = $source->default_duration_id;
 
                 if ($source->tapers) {
