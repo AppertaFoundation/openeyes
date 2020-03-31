a2bafccef049d7dfa78d497952b947a651808ecb
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2020-03-31 10:18:18 +0100

    OE-8452 allow i, ul, ol, li tags in letters

--- protected/controllers/BaseController.php
+++ protected/controllers/BaseController.php
@@ -412,7 +412,7 @@ class BaseController extends Controller
     public function sanitizeInput($input)
     {
 
-        $allowable_tags = "<b><div><strong><p><input><option><select><table><thead><tbody><tr><th><td><br>";
+        $allowable_tags = "<b><div><strong><p><input><option><select><table><thead><tbody><tr><th><td><i><ul><ol><li><br>";
         if (count($input) > 0) {
             foreach ($input as $key => $value) {
                 if (is_array($value) || is_object($value)) {
