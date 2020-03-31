bc9e68e4e470fa4875feea5d26c2e6e0748b29d6
codefactor-io <support@codefactor.io>
2020-03-31 14:13:30 +0000

    [CodeFactor] Apply fixes to commit 49d748f
    [ci skip] [skip ci]

--- protected/migrations/m200331_095629_add_default_dose_to_medication.php
+++ protected/migrations/m200331_095629_add_default_dose_to_medication.php
@@ -2,13 +2,13 @@
 
 class m200331_095629_add_default_dose_to_medication extends OEMigration
 {
-	public function up()
-	{
-	    $this->addOEColumn('medication', 'default_dose', 'INT(10) DEFAULT NULL', true);
-	}
+    public function up()
+    {
+        $this->addOEColumn('medication', 'default_dose', 'INT(10) DEFAULT NULL', true);
+    }
 
-	public function down()
-	{
-	    $this->dropOEColumn('medication', 'default_dose', true);
-	}
+    public function down()
+    {
+        $this->dropOEColumn('medication', 'default_dose', true);
+    }
 }
