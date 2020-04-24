c2e77d31228445fcee17aab63f6cf1e22df753e7
Gurpreet Singh <gurpreet.s.saggu@gmail.com>
2020-04-24 15:53:25 +1000

    Replaced the abandoned package with their direct dependencies.

--- protected/modules/OphCoCorrespondence/controllers/DefaultController.php
+++ protected/modules/OphCoCorrespondence/controllers/DefaultController.php
@@ -15,7 +15,6 @@
  * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
-require_once './vendor/setasign/fpdi/pdf_parser.php';
 
 class DefaultController extends BaseEventTypeController
 {
