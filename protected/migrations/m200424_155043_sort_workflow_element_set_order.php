f88feb8542c780ba765bd8754776ed4cfa9e0692
Edwin <edwin@fornicode.co.uk>
2020-04-28 01:24:07 +0100

    OE-9643 - implement savecounters

--- protected/migrations/m200424_155043_sort_workflow_element_set_order.php
+++ protected/migrations/m200424_155043_sort_workflow_element_set_order.php
@@ -7,18 +7,18 @@ class m200424_155043_sort_workflow_element_set_order extends CDbMigration
         $processed_workflow_id = 1;
         $position = 0;
         $criteria = new CDbCriteria();
-        $criteria->order = ' workflow_id asc, position asc';
-        $dataProvider = new CActiveDataProvider('OEModule\OphCiExamination\models\OphCiExamination_ElementSet', ['criteria' => $criteria]);
-        $element_set_iterator = new CDataProviderIterator($dataProvider);
+        $criteria->order = 'workflow_id asc, position asc';
+        $data_provider = new CActiveDataProvider('OEModule\OphCiExamination\models\OphCiExamination_ElementSet', ['criteria' => $criteria]);
+        $element_set_iterator = new CDataProviderIterator($data_provider);
 
         foreach ($element_set_iterator as $element_set) {
             if ($element_set->workflow_id === $processed_workflow_id) {
-                $element_set->position = ++$position;
-                $processed_workflow_id = $element_set->workflow_id;
+                 ++$position;
             } else {
-                $element_set->position = $position = 1;
+                $position = 1;
             }
-            $element_set->save();
+            $element_set->saveCounters(array('position' => -$element_set->position)); //set position in db to 0
+            $element_set->saveCounters(array('position' => $position));
             $processed_workflow_id = $element_set->workflow_id;
         }
     }
