1e563f7f382f76d1ec087aca8ee5086d0eb01914
Hari Kulendran <me@hari.dev>
2020-04-29 15:37:02 +0100

    OE-9631 removed accidental change.

--- protected/controllers/BaseController.php
+++ protected/controllers/BaseController.php
@@ -122,7 +122,7 @@ class BaseController extends Controller
         $assetManager->isAjaxRequest = Yii::app()->getRequest()->getIsAjaxRequest();
         if (!isset(Yii::app()->params['tinymce_default_options']['content_css'])) {
             $newblue_path = Yii::getPathOfAlias('application.assets.newblue');
-            $print_css_path = $assetManager->getPublishedUrl($newblue_path, true).'/css/style_oe3_print.min.css';
+            $print_css_path = $assetManager->getPublishedUrl($newblue_path).'/css/style_oe3_print.min.css';
             $newparams =
                 array_merge_recursive(
                     Yii::app()->getParams()->toArray(),
@@ -406,11 +406,7 @@ class BaseController extends Controller
 
     public function setPageTitle($pageTitle)
     {
-        if ((string)SettingMetadata::model()->getSetting('use_short_page_titles') != "on") {
-            parent::setPageTitle($pageTitle . ' - OE');
-        } else {
-            parent::setPageTitle($pageTitle);
-        }
+        parent::setPageTitle($pageTitle . ' - OE');
     }
 
     public function sanitizeInput($input)
