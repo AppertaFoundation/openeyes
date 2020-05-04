cb6b1a6cab695453368cc0fb4ae270c90c05f2e5
codefactor-io <support@codefactor.io>
2020-05-04 11:20:03 +0000

    [CodeFactor] Apply fixes to commit 88ff362
    [ci skip] [skip ci]

--- protected/modules/OphCiExamination/migrations/m200504_083443_edit_csm_shortcode_for_cataract_surgical_management.php
+++ protected/modules/OphCiExamination/migrations/m200504_083443_edit_csm_shortcode_for_cataract_surgical_management.php
@@ -3,11 +3,11 @@
 class m200504_083443_edit_csm_shortcode_for_cataract_surgical_management extends OEMigration
 {
 
-	public function safeUp()
-	{
+    public function safeUp()
+    {
         $event_type = $this->dbConnection->createCommand()->select('*')->from('event_type')->where('class_name = :class_name', array(':class_name' => 'OphCiExamination'))->queryRow();
 
-	    $this->update('patient_shortcode', ['method' => 'getCataractSurgicalManagementAsText', 'description' => 'Cataract Surgical Management in text readable format'], "code = 'csm'");
+        $this->update('patient_shortcode', ['method' => 'getCataractSurgicalManagementAsText', 'description' => 'Cataract Surgical Management in text readable format'], "code = 'csm'");
 
         $this->insert('patient_shortcode', array(
             'event_type_id' => $event_type['id'],
@@ -16,11 +16,11 @@ class m200504_083443_edit_csm_shortcode_for_cataract_surgical_management extends
             'method' => 'getCataractSurgicalManagementAsTable',
             'description' => 'Cataract Surgical Management in table format',
         ));
-	}
+    }
 
-	public function safeDown()
-	{
+    public function safeDown()
+    {
         $this->delete('patient_shortcode', 'code = :sc', array(':sc' => 'cst'));
-	}
+    }
 
 }
