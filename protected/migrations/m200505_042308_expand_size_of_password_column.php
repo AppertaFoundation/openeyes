6dd687708a5d51b4400d0aaa39cf1cc104d6034e
Andrew Pigram <andrew.pigram@toukanlabs.com>
2020-05-05 15:21:52 +1000

    Disabled down migration for password column expansion migration.

--- protected/migrations/m200505_042308_expand_size_of_password_column.php
+++ protected/migrations/m200505_042308_expand_size_of_password_column.php
@@ -5,6 +5,7 @@
  * column to 255 characters allows future changes to the hashing algorithm used without
  * requiring another migration to further expand column length.
  */
+
 class m200505_042308_expand_size_of_password_column extends CDbMigration
 {
     public function up()
@@ -15,7 +16,7 @@ class m200505_042308_expand_size_of_password_column extends CDbMigration
 
     public function down()
     {
-        $this->alterColumn('user', 'password', 'varchar(40)');
-        $this->alterColumn('user_version', 'password', 'varchar(40)');
+        echo "m200505_042308_expand_size_of_password_column does not support migration down.\n";
+        return false;
     }
 }
