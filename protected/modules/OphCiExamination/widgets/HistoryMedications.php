bb294448ac152eafe7fec8da8f5ebd5a5400efbb
codefactor-io <support@codefactor.io>
2020-05-07 11:04:04 +0000

    [CodeFactor] Apply fixes to commit 43d04b0
    [ci skip] [skip ci]

--- protected/modules/OphCiExamination/widgets/HistoryMedications.php
+++ protected/modules/OphCiExamination/widgets/HistoryMedications.php
@@ -244,7 +244,7 @@ class HistoryMedications extends \BaseEventElementWidget
             $current = array();
             foreach ($this->element->entries as $entry) {
                 $end_date_including_tapers = $entry->prescription_item_id ? $entry->prescription_item->stopDateFromDuration() : $entry->end_date;
-                if ($entry->end_date && $entry->stop_reason_id) { 
+                if ($entry->end_date && $entry->stop_reason_id) {
                     // in case the end date has been manually set in an exam event
                     $end_date_including_tapers = $entry->end_date;
                 }
