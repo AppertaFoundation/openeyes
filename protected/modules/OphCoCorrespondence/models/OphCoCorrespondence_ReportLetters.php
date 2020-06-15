0c05f1d960d3bff31508d25f3c80c278ff75adbb
Hari Kulendran <me@hari.dev>
2020-06-15 10:17:25 +0100

    OE-9906 Fix ambiguous letter reports.
    Could not replicate issue exactly as laid out in ticket, instead when
    a correspondence is 'Save and Print'-ed the report shows two entries for
    the letter in question. One is for the docman output and one is for the
    print output. This is because both print and electronic were selected,
    if only one is selected then there is only one entry in the report. To
    fix the issue of these two entries being ambiguous (as they are
    identical in the report), the output type is appended to the 'Type'
    field of the report.

--- protected/modules/OphCoCorrespondence/models/OphCoCorrespondence_ReportLetters.php
+++ protected/modules/OphCoCorrespondence/models/OphCoCorrespondence_ReportLetters.php
@@ -103,7 +103,8 @@ class OphCoCorrespondence_ReportLetters extends BaseReport
         $select = array(
             'c.first_name', 'c.last_name', 'p.dob', 'p.gender', 'p.hos_num', 'cons.first_name as cons_first_name',
             'cons.last_name as cons_last_name', 'e.created_date', 'ep.patient_id',
-            'IF(output_status IS NULL,IF(l.draft=0," - ","Draft"), LOWER(output_status)) as status');
+            'IF(output_status IS NULL,IF(l.draft=0," - ","Draft"), LOWER(output_status)) as status',
+            'document_output.output_type as output_type');
 
         $data = $this->getDbCommand();
 
@@ -163,7 +164,7 @@ class OphCoCorrespondence_ReportLetters extends BaseReport
     {
         foreach ($data->queryAll() as $i => $row) {
             if (@$row['lid']) {
-                $row['type'] = 'Correspondence';
+                $row['type'] = "Correspondence ({$row['output_type']})";
                 $row['link'] = 'http' . (@$_SERVER['https'] ? 's' : '') . '://' . @$_SERVER['SERVER_NAME'] . '/OphCoCorrespondence/default/view/' . $row['event_id'];
             } else {
                 $row['type'] = 'Legacy letter';
