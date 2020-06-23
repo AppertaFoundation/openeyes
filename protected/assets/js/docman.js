0aa56645a04268d9cc6f8a9aedb544a3537cc1a6
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2020-06-23 09:30:29 +0100

    OE-10106 remove redundant code

--- protected/assets/js/docman.js
+++ protected/assets/js/docman.js
@@ -289,11 +289,6 @@ var docman = (function() {
                 if (other_rowindex !== undefined) {
                     current_type = current_type.toLowerCase();
                     let type = current_type[0].toUpperCase() + current_type.slice(1);
-
-                    if(type !== 'Other' && type !== undefined && type !== '- type -'){
-                        other_id = $("#docman_recipient_" + other_rowindex + " option[value*='" + type + "']" ).val();
-                    }
-
                     other_id = $("#docman_recipient_" + other_rowindex + " option[value*='" + type + "']" ).val();
                 }
 
