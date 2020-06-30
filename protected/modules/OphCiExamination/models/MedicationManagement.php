a062bdb64194d19693937171929d7d4e16693f19
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2020-06-30 16:25:29 +0100

    OE-10093 set end date if the user have prescriber rights

--- protected/modules/OphCiExamination/models/MedicationManagement.php
+++ protected/modules/OphCiExamination/models/MedicationManagement.php
@@ -423,6 +423,12 @@ class MedicationManagement extends BaseMedicationElement
             $class = self::$entry_class;
             $entry = $class::model()->findBypk($item->original_item_id);
             $entry->prescription_item_id = $item->id;
+
+            if ($prescribe_access) {
+                $end_date = $item->stopDateFromDuration(true);
+                $entry->end_date = $end_date ? $end_date->format('Y-m-d') : null;
+            }
+
             $entry->save();
         }
 
