f89f8187aaaa3e41f704c78f77b4f522b6304aa2
biskyt <biskyt@users.noreply.github.com>
2020-07-16 11:11:45 +0100

    update version

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -655,7 +655,7 @@ return array(
         'exclude_admin_structure_param_list' => array(
             //            'Worklist',
         ),
-        'oe_version' => '3.6',
+        'oe_version' => '3.6.1n',
         // Replace the term "GP" in the UI with whatever is specified in gp_label. E.g, in Australia they are called "Practioners", not "GPs"
         'gp_label' => 'GP',
         // number of days in the future to retrieve worklists for the automatic dashboard render (0 by default in v3)
