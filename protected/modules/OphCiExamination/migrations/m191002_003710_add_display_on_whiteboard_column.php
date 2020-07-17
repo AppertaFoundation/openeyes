bb8505a4480cb685fcac7a5a2d3c7b7cd4da70bd
biskyt <biskyt@users.noreply.github.com>
2020-07-17 15:54:31 +0100

    add versioned to migration

--- protected/modules/OphCiExamination/migrations/m191002_003710_add_display_on_whiteboard_column.php
+++ protected/modules/OphCiExamination/migrations/m191002_003710_add_display_on_whiteboard_column.php
@@ -11,7 +11,7 @@ class m191002_003710_add_display_on_whiteboard_column extends OEMigration
     {
         $result = $this->dbConnection->createCommand("SHOW COLUMNS FROM `ophciexamination_risk` LIKE 'display_on_whiteboard'")->query();
         if (count($result) == 0) {
-            $this->addOEColumn('ophciexamination_risk', 'display_on_whiteboard', 'tinyint(1) DEFAULT 1');
+            $this->addOEColumn('ophciexamination_risk', 'display_on_whiteboard', 'tinyint(1) DEFAULT 1', true);
         }
         $this->alterView('patient_risk_assignment', <<<EOSQL
 select ra.id, latest.patient_id as patient_id, ra.risk_id, ra.other, ra.comments, ra.last_modified_user_id, risk.display_on_whiteboard,
