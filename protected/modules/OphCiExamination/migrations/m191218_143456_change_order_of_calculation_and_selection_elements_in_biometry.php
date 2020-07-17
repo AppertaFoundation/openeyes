cdb36a532588d38c8dc004feb97eb7287a52224a
toth2peter <48438323+toth2peter@users.noreply.github.com>
2020-07-17 12:05:54 +0200

    Fix/oe 10226 (#4908)
    * OE-10226 Change order of biometry elements
    
    * OE-10226 made the 'General Comments' box a v3 style bubble icon with autosize
    
    * [CodeFactor] Apply fixes
    
    [ci skip] [skip ci]
    
    Co-authored-by: codefactor-io <support@codefactor.io>
    Co-authored-by: Mally <47662871+MallyDev@users.noreply.github.com>
    Co-authored-by: Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
    Co-authored-by: AdrianKamulegeya <adriankamulegeya@gmail.com>

--- protected/modules/OphCiExamination/migrations/m191218_143456_change_order_of_calculation_and_selection_elements_in_biometry.php
+++ protected/modules/OphCiExamination/migrations/m191218_143456_change_order_of_calculation_and_selection_elements_in_biometry.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Note that this migration does not do its job because of a mistake:
+ * name should be class_name not name
+ * This is fixed in migration m200716_095459_change_order_of_calculation_and_selection_elements_in_biometry
+ */
+
 class m191218_143456_change_order_of_calculation_and_selection_elements_in_biometry extends CDbMigration
 {
     public function up()
