f24f68d36c98d05115e7effb6c285323720798b9
Andrew Pigram <andrew.pigram@toukanlabs.com>
2020-07-21 14:10:53 +1000

    Fully implemented HelperTest for 100% coverage of Helper class. Addressed some inconsistencies in Helper class and added PHPDoc.

--- protected/tests/unit/components/HelperTest.php
+++ protected/tests/unit/components/HelperTest.php
@@ -14,9 +14,9 @@
  */
 require_once __DIR__.DIRECTORY_SEPARATOR.'HelperTestNS.php';
 
-class HelperTest extends PHPUnit_Framework_TestCase
+class HelperTest extends CTestCase
 {
-    private static $tz;
+    private static string $tz;
 
     public static function setUpBeforeClass()
     {
@@ -47,6 +47,8 @@ class HelperTest extends PHPUnit_Framework_TestCase
 
     /**
      * @dataProvider mysqlDate2JsTimestampDataProvider
+     * @param $input
+     * @param $output
      */
     public function testMysqlDateToJsTimestamp($input, $output)
     {
@@ -77,12 +79,20 @@ class HelperTest extends PHPUnit_Framework_TestCase
 
     /**
      * @dataProvider getAgeDataProvider
+     * @param $expected
+     * @param $dob
+     * @param null $date_of_death
+     * @param null $check_date
+     * @throws Exception
      */
     public function testGetAge($expected, $dob, $date_of_death = null, $check_date = null)
     {
         $this->assertEquals($expected, Helper::getAge($dob, $date_of_death, $check_date));
     }
 
+    /**
+     * @throws ReflectionException
+     */
     public function testExtractValues()
     {
         $objects = array(
@@ -142,7 +152,10 @@ class HelperTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('foo, bar and baz', Helper::formatList(array('foo', 'bar', 'baz')));
     }
 
-    public function testgetNSShortname()
+    /**
+     * @throws ReflectionException
+     */
+    public function testGetNSShortname()
     {
         $test = new HelperTestNS\models\HelperTestNS();
 
@@ -152,16 +165,22 @@ class HelperTest extends PHPUnit_Framework_TestCase
     public function getDateForAgeProvider()
     {
         return array(
-                array('2015-05-12', '2004-05-12', 11),
-                array('2015-05-12', '2004-05-12', 11, '2016-04-11'),
-                array(null, '2004-05-12', 11, '2009-04-11'),
+            array('2015-05-12', '2004-05-12', 11),
+            array('2015-05-12', '2004-05-12', 11, '2016-04-11'),
+            array(null, '2004-05-12', 11, '2009-04-11'),
+            array(null, null, 11, '2009-04-11'),
         );
     }
 
     /**
      * @dataProvider getDateForAgeProvider
+     * @param $expected
+     * @param $dob
+     * @param $age
+     * @param null $date_of_death
+     * @throws Exception
      */
-    public function testgetDateForAge($expected, $dob, $age, $date_of_death = null)
+    public function testGetDateForAge($expected, $dob, $age, $date_of_death = null)
     {
         $this->assertEquals($expected, Helper::getDateForAge($dob, $age, $date_of_death));
     }
@@ -183,7 +202,7 @@ class HelperTest extends PHPUnit_Framework_TestCase
      * @dataProvider lineLimitProvider
      * @param $expected
      * @param $args
-     * @params $exception
+     * @param null $exception
      */
     public function testLineLimit($expected, $args, $exception = null)
     {
@@ -222,7 +241,7 @@ class HelperTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($expected, forward_static_call_array(array('Helper', 'elementFinder'), $args));
     }
 
-    public function test_md5Verified()
+    public function testMd5Verified()
     {
         $random_string = '';
         for ($i = 0; $i < 5; $i++) {
@@ -235,6 +254,348 @@ class HelperTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($random_string, Helper::md5Verified($pass_test));
         $fail_test = $random_string . 'a' . $checksum;
         $this->assertNull(Helper::md5Verified($fail_test));
+    }
+
+    public function getWeekdays()
+    {
+        return array(
+            array('day' => 1, 'expected' => 'Monday'),
+            array('day' => 2, 'expected' => 'Tuesday'),
+            array('day' => 3, 'expected' => 'Wednesday'),
+            array('day' => 4, 'expected' => 'Thursday'),
+            array('day' => 5, 'expected' => 'Friday'),
+            array('day' => 6, 'expected' => 'Saturday'),
+            array('day' => 7, 'expected' => 'Sunday'),
+            array('day' => 8, 'expected' => null),
+        );
+    }
+
+    /**
+     * @dataProvider getWeekdays
+     * @param $day
+     * @param $expected
+     */
+    public function testGetWeekdayText($day, $expected)
+    {
+        $this->assertEquals($expected, Helper::getWeekdayText($day));
+    }
+
+    public function testConvertToBytes()
+    {
+        $val = '2KB';
+        $expected = 2048;
+        $this->assertEquals($expected, Helper::convertToBytes($val));
+
+        $val = 8;
+        $expected = 8;
+        $this->assertEquals($expected, Helper::convertToBytes($val));
+
+        $val = 'invalid';
+        $this->assertNull(Helper::convertToBytes($val));
+    }
+
+    public function testConvertDate2NHS()
+    {
+        $expected = '20 Jul 2020';
+        $this->assertEquals($expected, Helper::convertDate2NHS('20-07-2020'));
+        $this->assertEquals($expected, Helper::convertDate2NHS($expected));
+
+        $expected = '-';
+        $this->assertEquals($expected, Helper::convertDate2NHS(null));
+    }
+
+    public function testTimestampToDB()
+    {
+        $date = new DateTime();
+        $expected = date('Y-m-d H:i:s', $date->getTimestamp());
+        $this->assertEquals($expected, Helper::timestampToDB($date->getTimestamp()));
+    }
+
+    public function testReturn_bytes()
+    {
+        $suffixes = array('k', 'm', 'g');
+        $base = 1024;
+        foreach ($suffixes as $i => $suffix) {
+            $expected = pow($base, $i + 1);
+            $this->assertEquals($expected, Helper::return_bytes('1' . $suffix));
+        }
+    }
+
+    public function testConvertFuzzyDate2HTML()
+    {
+        $expected = "<span class='day'>20 </span><span class='mth'>Jul </span><span class='yr'>2020</span>";
+        $this->assertEquals($expected, Helper::convertFuzzyDate2HTML('2020-07-20'));
+
+        $expected = "<span class='day'> </span><span class='mth'>Jul </span><span class='yr'>2020</span>";
+        $this->assertEquals($expected, Helper::convertFuzzyDate2HTML('2020-07-00'));
+
+        $expected = "<span class='day'> </span><span class='mth'> </span><span class='yr'>2020</span>";
+        $this->assertEquals($expected, Helper::convertFuzzyDate2HTML('2020-00-00'));
+    }
+
+    public function getEyeData()
+    {
+        return array(
+            'Both eyes' => array(
+                'eyes' => array(
+                    'left_eye' => array(
+                        'test 1'
+                    ),
+                    'right_eye' => array(
+                        'test 2'
+                    ),
+                ),
+                'expected' => Eye::BOTH
+            ),
+            'Left eye' => array(
+                'eyes' => array(
+                    'left_eye' => array(
+                        'test 1'
+                    ),
+                ),
+                'expected' => Eye::LEFT
+            ),
+            'Right eye' => array(
+                'eyes' => array(
+                    'right_eye' => array(
+                        'test 2'
+                    ),
+                ),
+                'expected' => Eye::RIGHT
+            ),
+            'N/A' => array(
+                'eyes' => array(
+                    'na_eye' => array(
+                        'test 3'
+                    )
+                ),
+                'expected' => -9
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider getEyeData
+     * @param $eyes
+     * @param $expected
+     */
+    public function testGetEyeIdFromArray($eyes, $expected)
+    {
+        $this->assertEquals($expected, Helper::getEyeIdFromArray($eyes));
+    }
+
+    public function testConvertMySQL2NHS()
+    {
+        $data = '2020-07-21';
+        $expected = '21 Jul 2020';
+
+        $this->assertEquals($expected, Helper::convertMySQL2NHS($data));
+
+        $data = null;
+        $expected = '-';
+
+        $this->assertEquals($expected, Helper::convertMySQL2NHS($data));
+    }
+
+    public function getMonths()
+    {
+        return array(
+            array('month' => 1, 'text' => 'January'),
+            array('month' => 2, 'text' => 'February'),
+            array('month' => 3, 'text' => 'March'),
+            array('month' => 4, 'text' => 'April'),
+            array('month' => 5, 'text' => 'May'),
+            array('month' => 6, 'text' => 'June'),
+            array('month' => 7, 'text' => 'July'),
+            array('month' => 8, 'text' => 'August'),
+            array('month' => 9, 'text' => 'September'),
+            array('month' => 10, 'text' => 'October'),
+            array('month' => 11, 'text' => 'November'),
+            array('month' => 12, 'text' => 'December'),
+        );
+    }
+
+    /**
+     * @dataProvider getMonths
+     * @param $month
+     * @param $text
+     */
+    public function testGetMonthText($month, $text)
+    {
+        $expected = substr($text, 0, 3);
+        $this->assertEquals($expected, Helper::getMonthText($month));
+        $this->assertEquals($text, Helper::getMonthText($month, true));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testCombineMySQLDateAndDateTime()
+    {
+        $date = '2020-07-21';
+        $time = '11:00:00';
+        $expected = '2020-07-21 11:00:00';
+
+        $this->assertEquals($expected, Helper::combineMySQLDateAndDateTime($date, $time));
+
+        $date = '21-07-2020';
+        $time = '11:00:00';
+
+        $this->expectException('Exception');
+        Helper::combineMySQLDateAndDateTime($date, $time);
+    }
+
+    public function testFormatFuzzyDate()
+    {
+        $expected = '20 Jul 2020';
+        $this->assertEquals($expected, Helper::formatFuzzyDate('2020-07-20'));
+
+        $expected = 'Jul 2020';
+        $this->assertEquals($expected, Helper::formatFuzzyDate('2020-07'));
+
+        $expected = '2020';
+        $this->assertEquals($expected, Helper::formatFuzzyDate('2020'));
+
+        $expected = 'Undated';
+        $this->assertEquals($expected, Helper::formatFuzzyDate(null));
+    }
+
+    public function testOeDateAsStr()
+    {
+        $expected = '<span class="oe-date"><span class="day">20</span><span class="mth">Jul</span><span class="yr">2020</span></span>';
+        $this->assertEquals($expected, Helper::oeDateAsStr('20 Jul 2020'));
+
+        $expected = '<span class="oe-date"><span class="day"></span><span class="mth">Jul</span><span class="yr">2020</span></span>';
+        $this->assertEquals($expected, Helper::oeDateAsStr('Jul 2020'));
+
+        $expected = '<span class="oe-date"><span class="day"></span><span class="mth"></span><span class="yr">2020</span></span>';
+        $this->assertEquals($expected, Helper::oeDateAsStr('2020'));
+
+        $expected = 'Undated';
+        $this->assertEquals($expected, Helper::oeDateAsStr('Undated'));
+
+        $expected = "";
+        $this->assertEquals($expected, Helper::oeDateAsStr(""));
+    }
+
+    public function testConvertDate2FullYear()
+    {
+        $expected = '20/07/2020';
+        $this->assertEquals($expected, Helper::convertDate2FullYear('20-07-20'));
+        $this->assertEquals($expected, Helper::convertDate2FullYear('20-07-2020'));
+
+        $expected = '-';
+        $this->assertEquals($expected, Helper::convertDate2FullYear(null));
+    }
+
+    public function testGenerateUuid()
+    {
+        $regex = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
+
+        $this->assertRegExp($regex, Helper::generateUuid());
+    }
+
+    public function testPadFuzzyDate()
+    {
+        $day = '21';
+        $month = '7';
+        $year = '2020';
+        $expected = '0021-07-2020';
+        $this->assertEquals($expected, Helper::padFuzzyDate($day, $month, $year));
+
+        $day = null;
+        $month = '7';
+        $year = '2020';
+        $expected = '0000-07-2020';
+        $this->assertEquals($expected, Helper::padFuzzyDate($day, $month, $year));
+
+        $day = null;
+        $month = null;
+        $year = '2020';
+        $expected = '0000-00-2020';
+        $this->assertEquals($expected, Helper::padFuzzyDate($day, $month, $year));
+    }
+
+    public function testConvertDate2HTML()
+    {
+        $expected = '<span class="day">20</span><span class="mth">Jul</span><span class="yr">2020</span>';
+        $this->assertEquals($expected, Helper::convertDate2HTML('20-07-2020'));
+        $this->assertEquals($expected, Helper::convertDate2HTML('20-07-20'));
+
+        $this->assertEquals('-', Helper::convertDate2HTML(null));
+    }
+
+    public function testMysqlDate2JsTimestamp()
+    {
+        $date = '2020-07-21 00:00:00';
+        $expected = 1595289600000;
+
+        $this->assertEquals($expected, Helper::mysqlDate2JsTimestamp($date));
+    }
+
+    public function testArray_dump_html()
+    {
+        $values = array(
+            array('Test 1', 'Test 2'),
+            'Test 3',
+            array(
+                'Test 4',
+                array('Test 5'),
+                array('Test 6')
+            )
+        );
+
+        $expected = 'Test 1<br>Test 2<br><br>Test 3<br>Test 4<br>Test 5<br><br>Test 6<br><br><br>';
+
+        $this->assertEquals($expected, Helper::array_dump_html($values));
+    }
+
+    public function testIsValidDateTime()
+    {
+        $date = '21-07-2020';
+        $this->assertTrue(Helper::isValidDateTime($date) !== false);
+
+        $date = 'invalid';
+        $this->assertTrue(Helper::isValidDateTime($date) === false);
+    }
+
+    public function testConvertDate2Short()
+    {
+        $expected = '20/07/20';
+        $this->assertEquals($expected, Helper::convertDate2Short('20-07-2020'));
+
+        $expected = '-';
+        $this->assertEquals($expected, Helper::convertDate2Short(null));
+    }
+
+    public function testConvertMySQL2HTML()
+    {
+        $date = '2020-07-21';
+        $expected = '<span class="day">21</span><span class="mth">Jul</span><span class="yr">2020</span>';
+        $this->assertEquals($expected, Helper::convertMySQL2HTML($date));
+
+        $date = null;
+        $expected = '-';
+        $this->assertEquals($expected, Helper::convertMySQL2HTML($date));
+    }
+
+    public function testConvertNHS2MySQL()
+    {
+        $date = '21 Jul 2020';
+        $expected = '2020-07-21';
+        $this->assertEquals($expected, Helper::convertNHS2MySQL($date));
+
+        $date = array('21 Jul 2020', '20 Jul 2020');
+        $expected = array('2020-07-21', '2020-07-20');
+        $this->assertEquals($expected, Helper::convertNHS2MySQL($date));
+
+        $date = array(array('21 Jul 2020'), '20 Jul 2020');
+        $expected = array(array('2020-07-21'), '2020-07-20');
+        $this->assertEquals($expected, Helper::convertNHS2MySQL($date));
+
+        $date = null;
+        $expected = null;
+        $this->assertEquals($expected, Helper::convertNHS2MySQL($date));
     }
 
 }
