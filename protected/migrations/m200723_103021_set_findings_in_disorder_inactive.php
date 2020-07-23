a09410bac68ccbd865310529a76342760dd4124e
codefactor-io <support@codefactor.io>
2020-07-23 10:51:51 +0000

    [CodeFactor] Apply fixes to commit b848a9e
    [ci skip] [skip ci]

--- protected/migrations/m200723_103021_set_findings_in_disorder_inactive.php
+++ protected/migrations/m200723_103021_set_findings_in_disorder_inactive.php
@@ -2,16 +2,16 @@
 
 class m200723_103021_set_findings_in_disorder_inactive extends CDbMigration
 {
-	public function safeUp()
-	{
+    public function safeUp()
+    {
         $this->execute("update disorder set active=0 where term like '%O/E%'");
 
-	}
+    }
 
-	public function safeDown()
-	{
+    public function safeDown()
+    {
         $this->execute("update disorder set active=1 where term like '%O/E%'");
-	}
+    }
 
 
 }
