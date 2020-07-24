56eacb46c1b6d994ee5701b64c32b573c8efe4df
Maria Laura Bisogno <mlaura.bisogno@gmail.com>
2020-07-24 09:10:20 +0100

    OE-10284 remove event from parameters

--- protected/modules/OphTrOperationnote/controllers/DefaultController.php
+++ protected/modules/OphTrOperationnote/controllers/DefaultController.php
@@ -1334,9 +1334,8 @@ class DefaultController extends BaseEventTypeController
 
         //event date
         if (isset($data['Event']['event_date'])) {
+            $this->setEventDate($data['Event']['event_date']);
             $event = $this->event;
-            $this->setEventDate($event, $data['Event']['event_date']);
-
             if (!$event->validate()) {
                 foreach ($event->getErrors() as $errormsgs) {
                     foreach ($errormsgs as $error) {
