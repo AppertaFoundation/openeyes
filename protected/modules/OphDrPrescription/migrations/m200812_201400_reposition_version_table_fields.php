631442e18e38c14b56f405efa634264eea5564c8
biskyt <biskyt@users.noreply.github.com>
2020-08-16 19:24:04 +0100

    fix missing auto_increment on event_medication_use_version

--- protected/modules/OphDrPrescription/migrations/m200812_201400_reposition_version_table_fields.php
+++ protected/modules/OphDrPrescription/migrations/m200812_201400_reposition_version_table_fields.php
@@ -5,7 +5,7 @@ class m200812_201400_reposition_version_table_fields extends CDbMigration
     public function up()
     {
         $this->execute("ALTER TABLE `event_medication_use_version` MODIFY `version_date` datetime AFTER `comments` ");
-        $this->execute("ALTER TABLE `event_medication_use_version` MODIFY `version_id` int NULL AFTER `version_date` ");
+        $this->execute("ALTER TABLE `event_medication_use_version` MODIFY `version_id` int NOT NULL AUTO_INCREMENT AFTER `version_date` ");
     }
 
     public function down()
