7024646ea890245dc6a0824bf23e14e02e93e43a
biskyt <biskyt@users.noreply.github.com>
2020-08-18 15:15:27 +0100

    OE-10350 - fix missing column

--- protected/modules/OphCiExamination/migrations/m200813_050735_group_IOP_exam_elements.php
+++ protected/modules/OphCiExamination/migrations/m200813_050735_group_IOP_exam_elements.php
@@ -4,6 +4,13 @@ class m200813_050735_group_IOP_exam_elements extends OEMigration
 {
     public function safeUp()
     {
+        // First check if group_title column exists (for some reason it doesn't in the Cardiff database and possibly others)
+        //if the columns already exist, do nothing
+        if (!isset($this->dbConnection->schema->getTable('element_type')->columns['group_title'])) {
+            // Add the group type column for labelling element groups without using the parent's name
+            $this->addOEColumn('element_type', 'group_title', 'VARCHAR(255) AFTER `tile_size`', true);
+        }
+
         $IOPGroup = $this->dbConnection->createCommand('SELECT id FROM element_group WHERE name = "Intraocular Pressure"')->queryScalar();
 
         $this->update('element_type', array('element_group_id' => $IOPGroup, 'group_title' => 'Intraocular Pressure'), "name = 'IOP History'");
