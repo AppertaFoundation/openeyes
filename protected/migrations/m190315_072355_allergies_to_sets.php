90987602a3af85b4ec3a2c8a37108f0820b0fb5b
Toby Bisco <biskyt@users.noreply.github.com>
2020-08-27 13:13:52 +0100

    Fix some migration issues from v3 (#5034)
    * only import active drug sets
    
    * strip non-numerics from default_dose
    
    * ignore duplicate allergy names

--- protected/migrations/m190315_072355_allergies_to_sets.php
+++ protected/migrations/m190315_072355_allergies_to_sets.php
@@ -2,44 +2,33 @@
 
 class m190315_072355_allergies_to_sets extends OEMigration
 {
-    public function up()
+    public function safeUp()
     {
-        $transaction=$this->getDbConnection()->beginTransaction();
-        try {
-            // Make a link to sets in allergies table
-            $this->addColumn("ophciexamination_allergy", "medication_set_id", "INT null");
-            $this->addColumn("ophciexamination_allergy_version", "medication_set_id", "INT null");
-            $this->addForeignKey("fk_allergy_to_set", "ophciexamination_allergy", "medication_set_id", "medication_set", "id");
+       
+        // Make a link to sets in allergies table
+        $this->addColumn("ophciexamination_allergy", "medication_set_id", "INT null");
+        $this->addColumn("ophciexamination_allergy_version", "medication_set_id", "INT null");
+        $this->addForeignKey("fk_allergy_to_set", "ophciexamination_allergy", "medication_set_id", "medication_set", "id");
 
-            // Create an auto-set for each allergy
-            $this->execute("INSERT INTO medication_set (`name`, `automatic`) SELECT CONCAT('Allergy_', ophciexamination_allergy.`name`), 1 FROM ophciexamination_allergy");
+        // Create an auto-set for each allergy
+        $this->execute("INSERT INTO medication_set (`name`, `automatic`) SELECT CONCAT('Allergy_', ophciexamination_allergy.`name`), 1 FROM ophciexamination_allergy WHERE active = 1 GROUP BY `name`");
 
-            // Link auto-sets with allergies
-            $this->execute("UPDATE ophciexamination_allergy SET medication_set_id = (SELECT id FROM medication_set WHERE `name` = CONCAT('Allergy_', ophciexamination_allergy.`name`))");
+        // Link auto-sets with allergies
+        $this->execute("UPDATE ophciexamination_allergy SET medication_set_id = (SELECT id FROM medication_set WHERE `name` = CONCAT('Allergy_', ophciexamination_allergy.`name`))");
 
-            // Fill auto-set rules with existing medications
-            $this->execute("INSERT INTO medication_set_auto_rule_medication (medication_set_id, medication_id, include_children)
-								SELECT 
-									allergy.medication_set_id,
-									maa.medication_id,
-									1
-								FROM 
-									medication_allergy_assignment maa
-								LEFT JOIN ophciexamination_allergy allergy ON allergy.id = maa.allergy_id
-								");
-
-            $transaction->commit();
-        }
-        catch(Exception $e) {
-            echo "Exception: ".$e->getMessage()."\n";
-            $transaction->rollback();
-            return false;
-        }
-
-        return true;
+        // Fill auto-set rules with existing medications
+        $this->execute("INSERT INTO medication_set_auto_rule_medication (medication_set_id, medication_id, include_children)
+                            SELECT 
+                                allergy.medication_set_id,
+                                maa.medication_id,
+                                1
+                            FROM 
+                                medication_allergy_assignment maa
+                            LEFT JOIN ophciexamination_allergy allergy ON allergy.id = maa.allergy_id
+                            ");
     }
 
-    public function down()
+    public function safeDown()
     {
         $this->execute("UPDATE ophciexamination_allergy SET medication_set_id = NULL ");
         $this->execute("DELETE medication_set_item FROM medication_set_item, medication_set WHERE medication_set_item.medication_set_id = medication_set.id AND  medication_set.`name` LIKE 'Allergy\_%'");
