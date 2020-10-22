efd77055251a1e73d2325efc780df3e313f9e3d4
biskyt <biskyt@users.noreply.github.com>
2020-10-22 12:05:07 +0100

    Handle empty strings in common drug set doses

--- protected/migrations/m180815_071733_add_set_common_subspecialty_medications.php
+++ protected/migrations/m180815_071733_add_set_common_subspecialty_medications.php
@@ -23,7 +23,7 @@ class m180815_071733_add_set_common_subspecialty_medications extends CDbMigratio
             $this->execute("INSERT INTO medication_set_item (`medication_set_id`, `medication_id`, default_form_id, default_dose, default_route_id, default_frequency_id, default_dose_unit_term, default_duration_id)
                             SELECT $ref_set_id, medication.id,
                             drug.form_id,
-                            REGEXP_REPLACE(drug.default_dose, '[a-z -\]', '' ) AS 'default_dose', -- strip non numerics
+                            NULLIF(TRIM(REGEXP_REPLACE(drug.default_dose, '[a-z -\]', '' )), '') AS 'default_dose', -- strip non numerics
                             drug.default_route_id,
                             drug.default_frequency_id,
                             drug.dose_unit,
