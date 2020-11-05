193b367db3508a5b92db73c3960451edded73da6
Maria Laura Bisogno <47662871+MallyDev@users.noreply.github.com>
2020-11-05 10:21:00 +0000

    OE-9158 add colgroup and col to sanitizeInput (#5199)

--- protected/controllers/BaseController.php
+++ protected/controllers/BaseController.php
@@ -410,7 +410,7 @@ class BaseController extends Controller
 
     public function sanitizeInput($input)
     {
-        $allowable_tags = ["b","strong","p","input","option","select","table","thead","tbody","tr","th","td","i","em","span","br","ul","ol","li","div"];
+        $allowable_tags = ["b","strong","p","input","option","select","table","thead","tbody","tr","th","td","i","em","span","br","ul","ol","li","div","col","colgroup"];
         if (count($input) > 0) {
             foreach ($input as $key => $value) {
                 if (is_array($value) || is_object($value)) {
