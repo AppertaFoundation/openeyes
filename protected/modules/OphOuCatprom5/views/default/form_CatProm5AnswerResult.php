e398d9737330114c0e6c4a85215ab80d43f6bc10
Daniel Thomson <daniel.thomson@toukanlabs.com>
2020-11-09 11:58:53 +1100

    OE-10410 Shuffled around a few Elements in the edit screen as requested (#5054)
    * OE-10410 Shuffled around a few Elements in the edit screen as requested
    
    * OE-10410 Neatening up form based on Github  comments
    
    * removed unneeded fieldset
    
    * [CodeFactor] Apply fixes to commit b9d9eda
    
    [ci skip] [skip ci]
    
    * Swapped out height value
    
    Co-authored-by: codefactor-io <support@codefactor.io>
    Co-authored-by: Gurpreet Singh <gurpreet.s.saggu@gmail.com>

--- protected/modules/OphOuCatprom5/views/default/form_CatProm5AnswerResult.php
+++ protected/modules/OphOuCatprom5/views/default/form_CatProm5AnswerResult.php
@@ -15,44 +15,38 @@
                 }
             }
             ?>
-      <tr>
-        <td>
-            <?php
-            if ($index !=0) { // if this isn't the first row, add a divider
-                ?>
-          <hr class="divider">
-            <?php } ?>
-            <?php echo $ques->id.' . '. $ques->question; ?>
-        </td>
-      </tr>
-      <tr style='border-bottom: none;'><!--there doesn't need to be a border after here -->
-        <td>
-            <?php if (isset($answerResult_item)  && isset($answerResult_item->id)) { ?>
-                <?=\CHtml::hiddenField($name_stub .'['. $index .'][id]', @$answerResult_item->id)?>
-            <?php } ?>
-            <?php if (isset($element)  && $element->id) { ?>
-                <?=\CHtml::hiddenField($name_stub .'['. $index .'][element_id]', @$element->id)?>
-            <?php } ?>
-            <?=\CHtml::hiddenField($name_stub .'['. $index .'][question_id]', $ques->id)?>
+        <tr>
+            <td class="flex-layout" style="height: auto;">
+                <div class="flex-layout cols-4" style="padding: 10px;">
+                    <?php echo $ques->id.' . '. $ques->question; ?>
+                </div>
+                <div class="user-input  cols-6" style="padding: 10px;">
+                    <?php if (isset($answerResult_item)  && isset($answerResult_item->id)) { ?>
+                        <?=\CHtml::hiddenField($name_stub .'['. $index .'][id]', @$answerResult_item->id)?>
+                    <?php } ?>
+                    <?php if (isset($element)  && $element->id) { ?>
+                        <?=\CHtml::hiddenField($name_stub .'['. $index .'][element_id]', @$element->id)?>
+                    <?php } ?>
+                    <?=\CHtml::hiddenField($name_stub .'['. $index .'][question_id]', $ques->id)?>
 
-            <?php  foreach ($answers as $answer_item) {?>
-              <label class="inline highlight cols-full">
-                <input class="cat_prom5_answer_score"
-                      id="CatProm5AnswerResult_<?= $answer_item->id?>"
-                      value="<?= $answer_item->id ?>"
-                      data-score="<?= $answer_item->score?>"
-                      type="radio"
-                      name="<?= $name_stub .'['. $index .'][answer_id]'?>"
-                      data-question="<?=$ques->id?>"
-                      required
-                      <?= isset($answerResult_item->answer_id)&& @$answerResult_item->answer_id===$answer_item->id ?'checked':''?>
-                >
-                <span><?= $answer_item->answer ?></span>
-              </label>
-            <?php } ?>
-          </fieldset>
-        </td>
-      </tr>
+                    <?php  foreach ($answers as $answer_item) {?>
+                    <label class="inline highlight cols-full" style='display: inline-block;'>
+                        <input class="cat_prom5_answer_score"
+                            id="CatProm5AnswerResult_<?= $answer_item->id?>"
+                            value="<?= $answer_item->id ?>"
+                            data-score="<?= $answer_item->score?>"
+                            type="radio"
+                            name="<?= $name_stub .'['. $index .'][answer_id]'?>"
+                            data-question="<?=$ques->id?>"
+                            required
+                            <?= isset($answerResult_item->answer_id)&& @$answerResult_item->answer_id===$answer_item->id ?'checked':''?>
+                        />
+                        <span><?= $answer_item->answer ?></span>
+                    </label>
+                    <?php } ?>
+                </div>
+            </td>
+        </tr>
         <?php } ?>
     </tbody>
   </table>
