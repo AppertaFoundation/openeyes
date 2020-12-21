f9789f33ed9c13f96e526d4baf771a47c823776a
biskyt <biskyt@users.noreply.github.com>
2020-12-21 19:01:28 +0000

    another attempt to fix group_title migration bug

--- protected/modules/OphCiExamination/migrations/m200813_050735_group_IOP_exam_elements.php
+++ protected/modules/OphCiExamination/migrations/m200813_050735_group_IOP_exam_elements.php
@@ -6,14 +6,20 @@ class m200813_050735_group_IOP_exam_elements extends OEMigration
     {
         // First check if group_title column exists (for some reason it doesn't in the Cardiff database and possibly others)
         //if the columns already exist, do nothing
-        $exists = isset($this->dbConnection->schema->getTable('element_type')->columns['group_title']);
-        if (!$exists) {
+
+        $result = $this->dbConnection->createCommand("SHOW COLUMNS FROM `element_type` LIKE 'group_title'")->queryScalar();
+        echo "RESULT = " . $result;
+        $not_exists = empty($result);
+
+        if ($not_exists) {
             // Add the group type column for labelling element groups without using the parent's name
             $this->addColumn('element_type', 'group_title', 'VARCHAR(255) AFTER `tile_size`');
         }
         // Check the version table separately - as in one DB, the column existed in version, but not the main table!
-        $exists = isset($this->dbConnection->schema->getTable('element_type')->columns['group_title']);
-        if (!$exists) {
+        $result = $this->dbConnection->createCommand("SHOW COLUMNS FROM `element_type_version` LIKE 'group_title'")->queryScalar();
+        echo "RESULT = " . $result;
+        $not_exists = empty($result);
+        if ($not_exists) {
             $this->addColumn('element_type_version', 'group_title', 'VARCHAR(255) AFTER `tile_size`');
         }
 
