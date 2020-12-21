5cb0b2dd3041989f5b0e6a3f4299d704afef6963
biskyt <biskyt@users.noreply.github.com>
2020-12-21 18:00:32 +0000

    update version number

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -112,7 +112,7 @@ return array(
         ),
         'cacheBuster' => array(
             'class' => 'CacheBuster',
-            'time' => '202007021539',
+            'time' => '202012211800',
         ),
         'clientScript' => array(
             'class' => 'ClientScript',
@@ -691,7 +691,7 @@ return array(
         'exclude_admin_structure_param_list' => array(
             //            'Worklist',
         ),
-        'oe_version' => '3.6.1n',
+        'oe_version' => '3.6.1',
         // Replace the term "GP" in the UI with whatever is specified in gp_label. E.g, in Australia they are called "Practioners", not "GPs"
         'gp_label' => 'GP',
         // number of days in the future to retrieve worklists for the automatic dashboard render (0 by default in v3)
