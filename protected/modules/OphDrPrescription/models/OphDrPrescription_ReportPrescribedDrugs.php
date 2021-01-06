942d14f8b473927571ebef4698db8762aa9b5fa9
stefant29 <stefan.toncu29@gmail.com>
2021-01-06 12:13:47 +0200

    Replace all occurrences of commas in drug tallmanLabel and item dosage to prevent bugs in csv files (#5318)
    Co-authored-by: Justinas Cepkauskas <justinas.cepkauskas@abehr.com>

--- protected/modules/OphDrPrescription/models/OphDrPrescription_ReportPrescribedDrugs.php
+++ protected/modules/OphDrPrescription/models/OphDrPrescription_ReportPrescribedDrugs.php
@@ -104,8 +104,8 @@ class OphDrPrescription_ReportPrescribedDrugs extends BaseReport
             $drug = new Drug();
             $drug->attributes = $item;
             $output .= $item['hos_num'] . ', ' . $item['last_name'] . ', ' . $item['first_name'] . ', ' . ($item['dob'] ? date('j M Y', strtotime($item['dob'])) : 'Unknown') . ', ' . $item['postcode'] . ', ';
-            $output .= (date('j M Y', strtotime($item['created_date'])) . ' ' . (substr($item['created_date'], 11, 5))) . ', ' . $drug->tallmanLabel . ', ';
-            $output .= $item['dose'] . ', ' . $item['frequency'] . ', ' . $item['duration'] . ' ,' . $item['route'] . ' ,' . $item['dispense_condition'] . ' ,' . $item['dispense_location'] . ' ,' . $item['laterality'] . ' ,';
+            $output .= (date('j M Y', strtotime($item['created_date'])) . ' ' . (substr($item['created_date'], 11, 5))) . ', ' . str_replace(',', ';', $drug->tallmanLabel) . ', ';
+            $output .= str_replace(',', ';', $item['dose']) . ', ' . $item['frequency'] . ', ' . $item['duration'] . ' ,' . $item['route'] . ' ,' . $item['dispense_condition'] . ' ,' . $item['dispense_location'] . ' ,' . $item['laterality'] . ' ,';
             $output .= $item['user_first_name'] . ' ' . $item['user_last_name'] . ', ' . $item['role'] . ', ' . (date('j M Y', strtotime($item['event_date'])) . ' ' . (substr($item['event_date'], 11, 5))) . ', ';
             $output .= $item['preservative_free'] ? 'Yes' : 'No';
             $output .= "\n";
