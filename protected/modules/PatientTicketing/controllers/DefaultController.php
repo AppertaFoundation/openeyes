1c9ea1f9bd5cdcf9459c5b951079141750a72da8
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2021-03-05 12:36:43 +0000

    Feature/oe 9105 (#5074)
    * OE-9105 Add data-sort to thead
    
    * OE-9105 Add JS to change the value of sort_by and and sort_by_order when clicking on a filter and submit the form to refres the page
    
    * OE-9105 Add sort_by and sort_by_order inputs in the form
    
    * OE-9105 Add sorting to the patient ticketing view,
    
    every sort_by value has a sort function which is used to sort the tickets array, if the values are desired to be in DESC order then we reverse the array
    
    * OE-9105 Change scope to private
    
    * OE-9105 Check if method exists before trying to call it
    
    * [CodeFactor] Apply fixes
    
    [ci skip] [skip ci]
    
    * OE-9105 Use strnatcmp instead of higher/lower as that will never return 0
    
    * oe-9105 type hinting and docblock
    
    * OE-9105 Delete @ usage with getParam
    
    Co-authored-by: codefactor-io <support@codefactor.io>
    Co-authored-by: toth2peter <48438323+toth2peter@users.noreply.github.com>

--- protected/modules/PatientTicketing/controllers/DefaultController.php
+++ protected/modules/PatientTicketing/controllers/DefaultController.php
@@ -100,7 +100,7 @@ class DefaultController extends \BaseModuleController
 
         // TODO: we probably don't want to have such a gnarly approach to this, we might want to denormalise so that we are able to do eager loading
         // That being said, we might get away with setting together false on the with to do this filtering (multiple query eager loading).
-        $criteria->join = 'JOIN '.models\TicketQueueAssignment::model()->tableName().' cqa ON cqa.ticket_id = t.id and cqa.id = (SELECT id from '.models\TicketQueueAssignment::model()->tableName().' qa2 WHERE qa2.ticket_id = t.id order by qa2.created_date desc limit 1)';
+        $criteria->join = 'JOIN ' . models\TicketQueueAssignment::model()->tableName() . ' cqa ON cqa.ticket_id = t.id and cqa.id = (SELECT id from ' . models\TicketQueueAssignment::model()->tableName() . ' qa2 WHERE qa2.ticket_id = t.id order by qa2.created_date desc limit 1)';
 
         // build queue id list
         $queue_ids = array();
@@ -120,7 +120,7 @@ class DefaultController extends \BaseModuleController
                     $queueset,
                     @$filter_options['closed-tickets'] ? true : false
                 ) as $queue) {
-                            $queue_ids[] = $queue->id;
+                    $queue_ids[] = $queue->id;
                 }
             }
         }
@@ -144,7 +144,7 @@ class DefaultController extends \BaseModuleController
         if (@$filter_options['firm-id']) {
             $criteria->addColumnCondition(array('cqa.assignment_firm_id' => $filter_options['firm-id']));
         } elseif (@$filter_options['subspecialty-id']) {
-            $criteria->join .= 'JOIN '.\Firm::model()->tableName().' f ON f.id = cqa.assignment_firm_id JOIN '.\ServiceSubspecialtyAssignment::model()->tableName().' ssa ON ssa.id = f.service_subspecialty_assignment_id';
+            $criteria->join .= 'JOIN ' . \Firm::model()->tableName() . ' f ON f.id = cqa.assignment_firm_id JOIN ' . \ServiceSubspecialtyAssignment::model()->tableName() . ' ssa ON ssa.id = f.service_subspecialty_assignment_id';
             $criteria->addColumnCondition(array('ssa.subspecialty_id' => $filter_options['subspecialty-id']));
         }
         if (isset($filter_options['patient-ids'])) {
@@ -216,13 +216,63 @@ class DefaultController extends \BaseModuleController
                 'dob' => ($patient->dob) ? $patient->NHSDate('dob') : 'Unknown',
                 'hos_num' => $patient->hos_num,
                 'nhsnum' => $patient->nhsnum,
-                'label' => $patient->first_name.' '.$patient->last_name.' ('.$patient->hos_num.')',
+                'label' => $patient->first_name . ' ' . $patient->last_name . ' (' . $patient->hos_num . ')',
                 'is_deceased' => $patient->is_deceased,
             );
         }
 
         echo CJavaScript::jsonEncode($result);
         Yii::app()->end();
+    }
+
+    /**
+     * @param $a
+     * @param $b
+     * @return int
+     */
+    private function sortBy_list($a, $b): int
+    {
+        return strnatcmp($a->current_queue->name, $b->current_queue->name);
+    }
+
+    /**
+     * @param $a
+     * @param $b
+     * @return int
+     */
+    private function sortBy_patient($a, $b): int
+    {
+        return strnatcmp($a->patient->last_name, $b->patient->last_name);
+    }
+
+    /**
+     * @param $a
+     * @param $b
+     * @return int
+     */
+    private function sortBy_priority($a, $b): int
+    {
+        return strnatcmp($a->priority->display_order, $b->priority->display_order);
+    }
+
+    /**
+     * @param $a
+     * @param $b
+     * @return int
+     */
+    private function sortBy_date($a, $b): int
+    {
+        return strnatcmp($a->created_date, $b->created_date);
+    }
+
+    /**
+     * @param $a
+     * @param $b
+     * @return int
+     */
+    private function sortBy_context($a, $b): int
+    {
+        return strnatcmp($a->getTicketFirm(), $b->getTicketFirm());
     }
 
     /**
@@ -254,7 +304,7 @@ class DefaultController extends \BaseModuleController
 
         $qsc_svc = Yii::app()->service->getService(self::$QUEUESETCATEGORY_SERVICE);
 
-        if (!$category = $qsc_svc->readActive((int) $cat_id)) {
+        if (!$category = $qsc_svc->readActive((int)$cat_id)) {
             throw new \CHttpException(404, 'Invalid category id');
         }
 
@@ -327,6 +377,14 @@ class DefaultController extends \BaseModuleController
 
                 // get tickets that match criteria
                 $tickets = models\Ticket::model()->findAll($criteria);
+                $sort_by = Yii::app()->getRequest()->getParam('sort_by', '');
+                $sort_by_order = Yii::app()->getRequest()->getParam('sort_by_order', '');
+                if ($sort_by != '' && method_exists($this, 'sortBy_' . $sort_by)) {
+                    uasort($tickets, [$this, 'sortBy_' . $sort_by]);
+                    if ($sort_by_order == "DESC") {
+                        $tickets = array_reverse($tickets);
+                    }
+                }
                 \Audit::add('queueset', 'view', $queueset->getId());
             }
         }
@@ -334,14 +392,14 @@ class DefaultController extends \BaseModuleController
         // render
         $this->pageTitle = 'Virtual Clinic';
         $this->render('index', array(
-                'category' => $category,
-                'queueset' => $queueset,
-                'tickets' => $tickets,
-                'patient_filter' => $patient_filter,
-                'pagination' => $pagination,
-                'cat_id' => $cat_id,
-                'patients' => $patient_ids ? \Patient::model()->findAllByPk($patient_ids) : [],
-            ));
+            'category' => $category,
+            'queueset' => $queueset,
+            'tickets' => $tickets,
+            'patient_filter' => $patient_filter,
+            'pagination' => $pagination,
+            'cat_id' => $cat_id,
+            'patients' => $patient_ids ? \Patient::model()->findAllByPk($patient_ids) : [],
+        ));
     }
 
     /**
@@ -373,8 +431,9 @@ class DefaultController extends \BaseModuleController
         }
 
         // if this is for a ticket, then we pass the patient id through for any event creation links
-        if (@$_GET['ticket_id']) {
-            if (!$ticket = models\Ticket::model()->findByPk((int) $_GET['ticket_id'])) {
+        $ticket_id =  \Yii::app()->request->getParam('ticket_id', null);
+        if ($ticket_id) {
+            if (!$ticket = models\Ticket::model()->findByPk((int)$ticket_id)) {
                 throw new \CHttpException(404, 'Invalid ticket id.');
             };
             $template_vars['patient_id'] = $ticket->patient_id;
@@ -445,9 +504,9 @@ class DefaultController extends \BaseModuleController
 
                 $flsh_id .= $queueset->getId();
                 if ($to_queue->outcomes) {
-                    Yii::app()->user->setFlash($flsh_id, $t_svc->getCategoryForTicket($ticket)->name.' - '.$ticket->patient->getHSCICName().' moved to '.$to_queue->name);
+                    Yii::app()->user->setFlash($flsh_id, $t_svc->getCategoryForTicket($ticket)->name . ' - ' . $ticket->patient->getHSCICName() . ' moved to ' . $to_queue->name);
                 } else {
-                    Yii::app()->user->setFlash($flsh_id, $t_svc->getCategoryForTicket($ticket)->name.' - '.$ticket->patient->getHSCICName().' completed');
+                    Yii::app()->user->setFlash($flsh_id, $t_svc->getCategoryForTicket($ticket)->name . ' - ' . $ticket->patient->getHSCICName() . ' completed');
                 }
             } else {
                 throw new Exception('unable to assign ticket to queue');
@@ -532,9 +591,9 @@ class DefaultController extends \BaseModuleController
         $can_process = $qs_svc->isQueueSetPermissionedForUser($queueset, Yii::app()->user->id);
 
         $this->renderPartial('_ticketlist_row', array(
-                    'ticket' => $ticket,
-                    'can_process' => $can_process,
-                ), false, false);
+            'ticket' => $ticket,
+            'can_process' => $can_process,
+        ), false, false);
     }
 
     /**
@@ -553,9 +612,9 @@ class DefaultController extends \BaseModuleController
         $ticket->audit('ticket', 'view-history', $ticket->id);
 
         $this->renderPartial('_ticketlist_history', array(
-                    'ticket' => $ticket,
-                    'assignments' => $ticket->getPastQueueAssignments(),
-                ), false, false);
+            'ticket' => $ticket,
+            'assignments' => $ticket->getPastQueueAssignments(),
+        ), false, false);
     }
 
     /**
@@ -583,7 +642,7 @@ class DefaultController extends \BaseModuleController
         if ($ticket->assignee_user_id) {
             $resp['status'] = 0;
             if ($ticket->assignee_user_id != Yii::app()->user->id) {
-                $resp['message'] = 'Ticket has already been taken by '.$ticket->assignee->getFullName();
+                $resp['message'] = 'Ticket has already been taken by ' . $ticket->assignee->getFullName();
             } else {
                 $resp['message'] = 'Ticket was already taken by you.';
             }
@@ -596,7 +655,7 @@ class DefaultController extends \BaseModuleController
             } else {
                 $resp['status'] = 0;
                 $resp['message'] = 'Unable to take ticket at this time.';
-                Yii::log("Couldn't save ticket to take it: ".print_r($ticket->getErrors(), true), \CLogger::LEVEL_ERROR);
+                Yii::log("Couldn't save ticket to take it: " . print_r($ticket->getErrors(), true), \CLogger::LEVEL_ERROR);
             }
         }
         echo \CJSON::encode($resp);
@@ -638,7 +697,7 @@ class DefaultController extends \BaseModuleController
             } else {
                 $resp['status'] = 0;
                 $resp['message'] = 'Unable to release ticket at this time.';
-                Yii::log("Couldn't save ticket to release it: ".print_r($ticket->getErrors(), true), \CLogger::LEVEL_ERROR);
+                Yii::log("Couldn't save ticket to release it: " . print_r($ticket->getErrors(), true), \CLogger::LEVEL_ERROR);
             }
         }
         echo \CJSON::encode($resp);
@@ -745,7 +804,7 @@ class DefaultController extends \BaseModuleController
      */
     public function actionCollapseTicket($ticket_id)
     {
-        if (!$ticket = models\Ticket::model()->findByPk((int) $ticket_id)) {
+        if (!$ticket = models\Ticket::model()->findByPk((int)$ticket_id)) {
             throw new \CHttpException(404, 'Invalid ticket id.');
         }
         $this->setTicketState($ticket, false);
@@ -760,7 +819,7 @@ class DefaultController extends \BaseModuleController
      */
     public function actionGetPatientAlert($patient_id)
     {
-        if (!$patient = \Patient::model()->findByPk((int) $patient_id)) {
+        if (!$patient = \Patient::model()->findByPk((int)$patient_id)) {
             throw new \CHttpException(404, 'Invalid patient id.');
         }
         $patient->audit('patient', 'view-alert');
@@ -792,7 +851,7 @@ class DefaultController extends \BaseModuleController
         $last_assignment = array_pop($queue_assignments);
 
         if (!$last_assignment->delete()) {
-            throw new \Exception('Unable to remove ticket queue assignment: '.print_r($last_assignment->errors, true));
+            throw new \Exception('Unable to remove ticket queue assignment: ' . print_r($last_assignment->errors, true));
         }
 
         echo '1';
--- protected/modules/PatientTicketing/views/default/ticketlist.php
+++ protected/modules/PatientTicketing/views/default/ticketlist.php
@@ -25,49 +25,80 @@
 
     <?php $flash_message = Yii::app()->user->getFlash('patient-ticketing-' . $queueset->getId()); ?>
     <?php if ($flash_message && $queueset) : ?>
-        <div class="alert-box issue"><b>No tickets match that search criteria for <?=$queueset ? $queueset->name : $category->name;?></b></div>
+        <div class="alert-box issue"><b>No tickets match that search criteria
+                for <?= $queueset ? $queueset->name : $category->name; ?></b></div>
     <?php endif; ?>
 
-  <table class="standard virtual-clinic">
-    <colgroup>
-      <col>
-      <col class="cols-2"> <!-- patient -->
-      <col>
-      <col class="cols-1"><!-- date -->
-      <col class="cols-1">
-      <col class="cols-3"><!-- clinical info -->
-      <col class="cols-3"><!-- notes -->
-      <col>
-    </colgroup>
-    <thead>
-    <tr>
-      <th>List</th>
-      <th>Patient</th>
-      <th></th>
-      <th></th>
-      <th>Context</th>
-      <th>Clinic Info</th>
-      <th>Notes</th>
-      <!--<th>Ticket Owner</th>-->
-      <th></th>
-    </tr>
-    </thead>
-    <tbody id="ticket-list">
-    <?php
-    foreach ($tickets as $i => $t) {
-        $this->renderPartial('_ticketlist_row', array('i' => $i, 'ticket' => $t, 'can_process' => $can_process));
-    }
-    if (!count($tickets)) {
-        echo "<tr><td colspan=8><div class='alert-box issue'>
+
+    <table class="standard virtual-clinic">
+        <colgroup>
+            <col>
+            <col class="cols-2"> <!-- patient -->
+            <col>
+            <col class="cols-1"><!-- date -->
+            <col class="cols-1">
+            <col class="cols-3"><!-- clinical info -->
+            <col class="cols-3"><!-- notes -->
+            <col>
+        </colgroup>
+        <thead>
+        <tr>
+            <th><a href="#" class="column-sort  js-ticket-sort" data-sort="list">List</a></th>
+            <th><a href="#" class="column-sort  js-ticket-sort" data-sort="patient">Patient</a></th>
+            <th><a href="#" class="column-sort js-ticket-sort" data-sort="priority"><i
+                            class="oe-i circle-green small pad"></i></a></th>
+            <th><a href="#" class="column-sort active js-ticket-sort" data-sort="date">Date</a></th>
+            <th><a href="#" class="column-sort js-ticket-sort" data-sort="context">Context</a></th>
+            <th>Clinic Info</th>
+            <th>Notes</th>
+            <!--<th>Ticket Owner</th>-->
+            <th></th>
+        </tr>
+        </thead>
+        <tbody id="ticket-list">
+        <?php
+        foreach ($tickets as $i => $t) {
+            $this->renderPartial('_ticketlist_row', array('i' => $i, 'ticket' => $t, 'can_process' => $can_process));
+        }
+        if (!count($tickets)) {
+            echo "<tr><td colspan=8><div class='alert-box issue'>
                 No patients found.
             </div></td><td></td></tr>";
-    }
-    ?>
-    </tbody>
-    <tfoot class="pagination-container">
+        }
+        ?>
+        </tbody>
+        <tfoot class="pagination-container">
         <td colspan="9">
             <?php $this->widget('LinkPager', ['pages' => $pagination]); ?>
         </td>
-    </tfoot>
-  </table>
+        </tfoot>
+    </table>
 </main>
+<script type="text/javascript">
+    $('document').ready(function(){
+        let current_sort_by_value = $('#ticket_sort_by').val();
+        let current_sort_by_order_value = $('#ticket_sort_by_order').val();
+        let selected_sort = $('*[data-sort="' + current_sort_by_value +'"]');
+        if(selected_sort.length) {
+            if(current_sort_by_order_value == "DESC") {
+                selected_sort.addClass('descend');
+            } else {
+                selected_sort.addClass('ascend');
+            }
+        }
+    })
+    $('.js-ticket-sort').on('click', function () {
+        let current_sort_by_value = $('#ticket_sort_by').val();
+        if (current_sort_by_value == $(this).data('sort')) {
+            if ($('#ticket_sort_by_order').val() == "DESC") {
+                $('#ticket_sort_by_order').val("");
+            } else {
+                $('#ticket_sort_by_order').val("DESC");
+            }
+        } else {
+            $('#ticket_sort_by_order').val("");
+        }
+        $('#ticket_sort_by').val($(this).data('sort'));
+        $('#ticket-filter').submit();
+    })
+</script>
