9e201da460fe457b6ec5e843d0480cc6a097f256
Andrew Pigram <andrew.pigram@toukanlabs.com>
2021-07-05 12:28:11 +1000

    Fix/oe 11432 (#6068)
    * Added Yii command to look up missing hospital numbers from an external PAS using demographics search per patient on NHS number.
    
    * Tightened conditions for patient retrieval so that only patients with a hospital number of 'unknown' will be captured.
    
    * Added newlines to echoed strings to improve output formatting.
    Now echoing exception messages if the command should fail for any reason. The command will also now terminate if it fails.
    
    * [CodeFactor] Apply fixes
    
    * Added short-circuit to command if no patients with unknown hospital numbers have been found.
    
    * [CodeFactor] Apply fixes
    
    * Reworked patient demographic command to apply to all records using the specified block size (block_size) using a single invocation of the command rather than multiple.
    
    * Now outputting patient record validation errors.
    
    * Added condition to filter out existing duplicated records when assigning hospital numbers to patients with unknown hospital numbers.
    
    * Fixed invalid SQL.
    
    * Added a 5 second delay after each callout to the MPI.
    Reduced default block size to 100.
    
    * Added creation of PASAPI assignment record when updating the hospital number of the existing patient record.
    
    * Fixed issue where duplicates could be returned when retrieving the patient model for update.
    
    * Added ID to error message if a patient record or PAS assignment fails to save.
    
    * Ensuring only distinct NHS numbers are selected.
    
    Co-authored-by: codefactor-io <support@codefactor.io>

---
+++ protected/commands/PopulatePatientDemographicsCommand.php
@@ -0,0 +1,126 @@
+<?php
+
+use OEModule\PASAPI\components\XmlHelper;
+use OEModule\PASAPI\models\PasApiAssignment;
+
+class PopulatePatientDemographicsCommand extends CConsoleCommand
+{
+    private const DELAY_SECONDS = 5;
+    /**
+     * @throws Exception
+     */
+    public function actionIndex($block_size = 100)
+    {
+        $query = Yii::app()->db->createCommand()
+            ->selectDistinct('p.nhs_num')
+            ->from('patient p')
+            ->leftJoin('pasapi_assignment pa', 'pa.internal_id = p.id')
+            ->where('pa.id IS NULL AND LOWER(p.hos_num) = \'unknown\'
+            AND p.nhs_num IN (SELECT p1.nhs_num FROM patient p1 GROUP BY p1.nhs_num HAVING COUNT(*) = 1)')
+            ->limit($block_size);
+
+        $total_patients = Yii::app()->db->createCommand()
+            ->select('COUNT(*)')
+            ->from('patient p')
+            ->leftJoin('pasapi_assignment pa', 'pa.internal_id = p.id')
+            ->where('pa.id IS NULL AND LOWER(p.hos_num) = \'unknown\'
+            AND p.nhs_num IN (SELECT p1.nhs_num FROM patient p1 GROUP BY p1.nhs_num HAVING COUNT(*) = 1)')
+            ->queryScalar();
+
+        if ($total_patients === 0) {
+            echo "No patients with missing demographics found. Exiting...\n";
+            Yii::app()->end();
+        }
+
+        echo $total_patients . " patient records with missing demographics found. Commencing demographic lookup...\n";
+        $block_count = 1;
+        $patients_to_update = array();
+
+        do {
+            $transaction = Yii::app()->db->beginTransaction();
+            try {
+                $patients_to_update = $query->queryColumn();
+                echo "Processing patient record block $block_count. Block size: " . count($patients_to_update) . "...";
+                foreach ($patients_to_update as $patient) {
+                    $result = $this->pasRequest($patient);
+                    $xml_helper = new XmlHelper($result);
+                    $xml_handler = $xml_helper->getHandler();
+
+                    while ($xml_handler->read() && $xml_handler->name !== 'Patient');
+
+                    // now that we're at the right depth, hop to the next <patient/> until the end of the tree
+                    while ($xml_handler->name === 'Patient') {
+                        $node = new SimpleXMLElement($xml_handler->readOuterXML());
+
+                        $patient_record = Patient::model()->find('nhs_num = :nhs AND hos_num = \'unknown\'', [':nhs' => $patient]);
+
+                        if (!$patient_record) {
+                            throw new CDbException("Unable to find patient record with NHS: $patient");
+                        }
+
+                        $patient_record->hos_num = $node->HospitalNumber;
+                        if (!$patient_record->save()) {
+                            throw new CDbException("Unable to save demographic changes for patient ID $patient_record->id: " . print_r($patient_record->getErrors(), true));
+                        }
+
+                        $assignment = new PasApiAssignment();
+                        $assignment->resource_id = $node->HospitalNumber;
+                        $assignment->resource_type = 'Patient';
+                        $assignment->internal_type = '\\Patient';
+                        $assignment->internal_id = $patient_record->id;
+
+                        if (!$assignment->save()) {
+                            throw new CDbException("Unable to save demographic changes for patient: $assignment->resource_id" . print_r($assignment->getErrors(), true));
+                        }
+
+                        $xml_handler->next('Patient');
+                    }
+                    sleep(self::DELAY_SECONDS);
+                }
+                $transaction->commit();
+                echo "Done\n";
+                $block_count++;
+            } catch (Exception $e) {
+                $transaction->rollback();
+                echo "PopulatePatientDemographics: " . $e->getMessage();
+                OELog::log("PopulatePatientDemographics: " . $e->getMessage());
+                Yii::app()->end(1);
+            }
+        } while (!empty($patients_to_update));
+
+        echo $total_patients . " patient demographic records updated successfully.\n";
+        OELog::log("PopulatePatientDemographics: " . $total_patients . " patient demographic records updated successfully.");
+    }
+
+    /**
+     * Building the query string and making a GET call to the API
+     * @param $nhs_num
+     * @return bool|string
+     * @throws Exception
+     */
+    private function pasRequest($nhs_num)
+    {
+        $curl = new Curl();
+        $xml = false;
+
+        $url = Yii::app()->params['pasapi']['url'];
+
+        $query = array();
+        $query['nhsnum'] = $nhs_num;
+
+        $error = '';
+        if (!empty($query)) {
+            $xml = $curl->get($url . '?' . http_build_query($query));
+            $ch = $curl->curl;
+
+            if (curl_errno($ch)) {
+                $error = 'PopulatePatientDemographics cURL error occurred on API request. Request error: ' . curl_error($ch) . " ";
+                OELog::log($error);
+            }
+        }
+
+        Audit::add('PopulatePatientDemographics', 'GET request', $error . $xml);
+
+        return $xml;
+    }
+}
