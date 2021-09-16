4d17b1fd396c6c50f22fa7d4a6c7eb302ede9a43
Sabi Potondi <sabi.potondi@abehr.com>
2021-09-16 13:29:40 +0100

    OE-11041 - replace file headers to Copyright Apperta Foundation 2021

--- protected/modules/OphCoCvi/migrations/m191119_115805_create_table_delivery_log.php
+++ protected/modules/OphCoCvi/migrations/m191119_115805_create_table_delivery_log.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * (C) Copyright Apperta Foundation 2021
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.openeyes.org.uk
+ *
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (C) 2021, Apperta Foundation
+ * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
+ */
 
 class m191119_115805_create_table_delivery_log extends CDbMigration
 {
