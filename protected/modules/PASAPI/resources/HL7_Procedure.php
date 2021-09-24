91b78c4c0ac01caf4d011c4e5cdbd6758bd137c1
gabordikan <77804349+gabordikan@users.noreply.github.com>
2021-09-24 10:28:33 +0200

    OE-11158: A03, A11 added to HL7 message types (#6656)
    * OE-11158: A03, A11 added to HL7 message types
    
    * [CodeFactor] Apply fixes to commit 66869f3
    
    * OE-11158: fixed attributes from A04
    
    * OE-11158: fixed attributes from A04
    
    * [CodeFactor] Apply fixes to commit c0b32f4
    
    * OE-11158: fixed attributes from A04
    
    * OE-11158: fixed CF issue
    
    * [CodeFactor] Apply fixes
    
    * OE-11158: fixed date format in proc and diag
    
    * OE-11158: fixed date format in proc and diag
    
    * OE-11158: fixed room data
    
    * OE-11158: removed null from ZU1
    
    Co-authored-by: gabordikan <gabordikan@users.noreply.github.com>
    Co-authored-by: codefactor-io <support@codefactor.io>
    Co-authored-by: Daniel <daniel.thomson@toukanlabs.com>

--- protected/modules/PASAPI/resources/HL7_Procedure.php
+++ protected/modules/PASAPI/resources/HL7_Procedure.php
@@ -79,7 +79,7 @@ class HL7_Procedure extends BaseHL7_Section
             $this->prefix.'.3.1' => $this->procedure_code_identifier,
             $this->prefix.'.3.2' => $this->procedure_code_text,
             $this->prefix.'.3.3' => $this->procedure_code_name_of_coding_system,
-            $this->prefix.'.5' => $this->procedure_date_time,
+            $this->prefix.'.5' => substr(str_replace('-','',str_replace(':','',str_replace(' ','',$this->procedure_date_time))),0,14),
             $this->prefix.'.6' => $this->procedure_functional_type,
             $this->prefix.'.11.1' => $this->clinician_id_number,
             $this->prefix.'.11.2' => $this->clinician_family_name,
