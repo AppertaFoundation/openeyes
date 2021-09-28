0076691524289ccdb24864a229f06d1ed0223758
alec-peecock1 <89386247+alec-peecock1@users.noreply.github.com>
2021-09-28 10:59:14 +0100

    OE-11437-1 - Added visible checkbox to instruments edit (#6436)
    * OE-11437-1 - Added visible checkbox to instruments edit
    
    * OE-11437 - 1 - resolved merge conflicts for v4.3
    
    Co-authored-by: Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
    Co-authored-by: Maria Laura Bisogno <47662871+MallyDev@users.noreply.github.com>

--- protected/modules/OphCiExamination/controllers/AdminController.php
+++ protected/modules/OphCiExamination/controllers/AdminController.php
@@ -109,6 +109,7 @@ class AdminController extends \ModuleAdminController
                 $model->name = $post_attributes['name'];
                 $model->short_name = $post_attributes['short_name'];
                 $model->active = $post_attributes['active'];
+                $model->visible = $post_attributes['visible'];
                 $model->save();
 
                 if (Yii::app()->user->checkAccess('admin')) {
@@ -146,7 +147,7 @@ class AdminController extends \ModuleAdminController
 
         if (isset($_POST[\CHtml::modelName($model)])) {
             $post_attributes = $_POST[\CHtml::modelName($model)];
-            if (Yii::app()->checkAccess('admin')) {
+            if (Yii::app()->user->checkAccess('admin')) {
                 // Only admins can create instances at installation level
                 if (isset($post_attributes['institutions'])) {
                     $institutions = $post_attributes['institutions'];
@@ -161,6 +162,7 @@ class AdminController extends \ModuleAdminController
                 $model->name = $post_attributes['name'];
                 $model->short_name = $post_attributes['short_name'];
                 $model->active = $post_attributes['active'];
+                $model->visible = $post_attributes['visible'];
 
                 $criteria=new CDbCriteria;
                 $criteria->select = 'max(display_order) AS display_order';
