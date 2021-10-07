2e79ee5ec1ab7a5a4d2b3135abc40a45da5bd3d0
Peichun Zhou <peichun.zhou@toukanlabs.com>
2021-10-07 17:49:21 +1100

    fix/OE-12012: Added missing widget back (#6774)
    * fix/OE-12012: Added missing widget back
    
    * [CodeFactor] Apply fixes to commit 02d0db4
    
    Co-authored-by: codefactor-io <support@codefactor.io>
    Co-authored-by: Andrew Pigram <andrew.pigram@toukanlabs.com>

--- protected/modules/PatientTicketing/widgets/TicketAssignOutcome.php
+++ protected/modules/PatientTicketing/widgets/TicketAssignOutcome.php
@@ -1,18 +1,17 @@
 <?php
 /**
- * OpenEyes.
+ * OpenEyes
  *
- * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
- * (C) OpenEyes Foundation, 2011-2014
+ * (C) OpenEyes Foundation, 2021
  * This file is part of OpenEyes.
  * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
  * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
  *
+ * @package OpenEyes
  * @link http://www.openeyes.org.uk
- *
  * @author OpenEyes <info@openeyes.org.uk>
- * @copyright Copyright (c) 2011-2014, OpenEyes Foundation
+ * @copyright Copyright (c) 2021, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 
@@ -67,7 +66,7 @@ class TicketAssignOutcome extends BaseTicketAssignment
     public function extractFormData($form_data)
     {
         $res = array();
-        foreach (array('outcome', 'followup_quantity', 'followup_period', 'clinic_location') as $k) {
+        foreach (array('outcome', 'followup_quantity', 'followup_period', 'clinic_location', 'is_patient_called') as $k) {
             $res[$k] = @$form_data[$k];
         }
 
