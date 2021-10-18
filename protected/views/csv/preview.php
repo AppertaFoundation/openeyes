f119f0a355c5d29390ce6ff6155eea342cc42a1e
Daniel <daniel.thomson@toukanlabs.com>
2021-10-18 16:01:43 +1100

    Fix/oe 12073 (#6861)
    * OE-12073 added back button
    
    * OE-12073 fixed Array helper to Helper
    
    * OE-12073 checked to see if uploaded file is a CSV
    
    * OE-12073 Access Dynamic parameters and methods appropriately
    
    * OE-12073 Updated table references as columns have been moved

--- protected/views/csv/preview.php
+++ protected/views/csv/preview.php
@@ -7,7 +7,7 @@
             <?php
             if (isset($errors) and $errors !== null) {
                 echo '<pre>';
-                echo ArrayHelper::array_dump_html($errors);
+                echo Helper::array_dump_html($errors);
                 echo '</pre>';
             }
             ?>
@@ -18,42 +18,46 @@
                 'id' => 'import-form',
                 'action' => Yii::app()->createURL('csv/import', array('context' => $context, 'csv' => $csv_id)),
                 'enableAjaxValidation' => false,
-                'htmlOptions' => array('enctype' => 'multipart/form-data',
-                'class' => 'oe-full-main',
-                 ),
+                'htmlOptions' => array(
+                    'enctype' => 'multipart/form-data',
+                    'class' => 'oe-full-main',
+                ),
             )
         );
 
-        if(empty($csv_id)) {
-        	?>
-					<div id="import-file-upload-error"> No CSV file found. Please select a file to upload </div>
-					<?php
-				}
-
-        if (!empty($table) && !empty($csv_id)): ?>
-            <div style="overflow: auto">
-                <table class="standard highlight-rows">
-                    <tr>
-                        <?php foreach (array_keys($table[0]) as $header): ?>
-                            <th>
-                                <?php echo $header; ?>
-                            </th>
-                        <?php endforeach; ?>
-                    </tr>
-                    <?php foreach ($table as $row): ?>
+        if (empty($csv_id)) {
+        ?>
+            <div id="import-file-upload-error"> No CSV file found. Please select a CSV file to upload from upload page</div>
+        <?php
+            echo CHtml::link('Back to upload page', '/csv/upload?context=' . $context ,['class' => 'button large',]);
+        }
+        else{
+            if (!empty($table)){ ?>
+                <div style="overflow: auto">
+                    <table class="standard highlight-rows">
                         <tr>
-                            <?php foreach ($row as $column): ?>
-                                <td>
-                                    <?php echo $column ?>
-                                </td>
+                            <?php foreach (array_keys($table[0]) as $header) : ?>
+                                <th>
+                                    <?php echo $header; ?>
+                                </th>
                             <?php endforeach; ?>
                         </tr>
-                    <?php endforeach; ?>
-                </table>
-            </div>
-        <?php
-        echo CHtml::submitButton('Import');
-				endif;
+                        <?php foreach ($table as $row) : ?>
+                            <tr>
+                                <?php foreach ($row as $column) : ?>
+                                    <td>
+                                        <?php echo $column ?>
+                                    </td>
+                                <?php endforeach; ?>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+                <?php
+                echo CHtml::submitButton('Import', [ 'class' => 'button large']);
+            }
+            echo CHtml::link('Cancel', '/csv/upload?context=' . $context, ['class' => 'button large',]);
+        }
         $this->endWidget();
         ?>
     </div>
