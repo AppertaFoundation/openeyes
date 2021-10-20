52fc232a4498ac94f3635afd276a490d0a1dd3b6
Irvine87 <Irvine87@users.noreply.github.com>
2021-10-20 10:49:31 +0100

    OE-12060 - Merged two migration into one file

--- protected/modules/OphTrConsent/migrations/m211018_092600_import_consent_form_types.php
+++ protected/modules/OphTrConsent/migrations/m211018_092600_import_consent_form_types.php
@@ -5,6 +5,8 @@ class m211018_092600_import_consent_form_types extends OEMigration
     private const ARCHIVE_TYPES_TABLE = 'ophtrconsent_type_type_archive';
     private const ARCHIVE_EXTRA_PROCEDURE_ASSESSMENT = 'ophtrconsent_extra_procedure_procedure_type_assignment_archive';
 
+    private const ET_ARCHIVE_TABLE = 'et_ophtrconsent_type_archive';
+
     public function safeUp()
     {
         if ($this->dbConnection->schema->getTable('ophtrconsent_extra_procedure_procedure_type_assignment', true)) {
@@ -26,6 +28,24 @@ class m211018_092600_import_consent_form_types extends OEMigration
                 WHERE ophtrconsent_type_type_id  in (5,6,7,8,9,10)
 		    ");
         }
+
+        if (!$this->dbConnection->schema->getTable(self::ET_ARCHIVE_TABLE, true)) {
+            $this->execute("CREATE TABLE " . self::ET_ARCHIVE_TABLE . " AS SELECT * FROM et_ophtrconsent_type");
+        }
+
+        $this->execute("
+			UPDATE et_ophtrconsent_type
+			SET type_id = CASE
+				WHEN type_id = 5 THEN 1
+				WHEN type_id = 6 THEN 2
+				WHEN type_id = 7 THEN 1
+				WHEN type_id = 8 THEN 4
+				WHEN type_id = 9 THEN 3
+				WHEN type_id = 10 THEN 3
+				ELSE type_id
+			END
+			WHERE type_id  in (5,6,7,8,9,10)
+		");
 
         $this->execute("DELETE FROM ophtrconsent_type_assessment WHERE type_id > 4;");
 
