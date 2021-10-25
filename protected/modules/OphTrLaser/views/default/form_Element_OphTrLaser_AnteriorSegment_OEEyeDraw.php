15d3ec0dddc75ce13d1f124df2dbae2fca46d84e
Andrew Pigram <andrew.pigram@toukanlabs.com>
2021-10-25 16:19:06 +1100

    Added missing Focal Laser EyeDraw doodle to Anterior Segment element for Laser event. (#6929)

--- protected/modules/OphTrLaser/views/default/form_Element_OphTrLaser_AnteriorSegment_OEEyeDraw.php
+++ protected/modules/OphTrLaser/views/default/form_Element_OphTrLaser_AnteriorSegment_OEEyeDraw.php
@@ -18,7 +18,7 @@
 <?php
 $this->widget('application.modules.eyedraw.OEEyeDrawWidget', array(
     'doodleToolBarArray' => array(
-        array('LaserSpot','PI' ,'PosteriorCapsule'),
+        array('LaserSpot', 'FocalLaser', 'PI' ,'PosteriorCapsule'),
     ),
     'onReadyCommandArray' => array(
         array('addDoodle', array('AntSeg')),
