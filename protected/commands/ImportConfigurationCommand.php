9147e121f45665bf7ace11eb4fb9983b024c0316
imas8888 <35904771+imas8888@users.noreply.github.com>
2021-10-28 10:26:02 +0100

    [OE-12138] - Adding Drug set import (#6944)
    * [OE-12138] - Adding Drug set import
    
    * [CodeFactor] Apply fixes to commit 91a1b69
    
    * Update ImportConfigurationCommand.php
    
    * [OE-12138] - Changing insert to save(false)
    
    Co-authored-by: codefactor-io <support@codefactor.io>
    Co-authored-by: Grant Morphett <Grant.Morphett@ToukanLabs.com>

--- protected/commands/ImportConfigurationCommand.php
+++ protected/commands/ImportConfigurationCommand.php
@@ -29,7 +29,7 @@ This command is able to import OpenEyes configuration from an XLSX files
 The file should be named the name of the institution as it appears in the database
 
 The file should have the following tabs (remove any tabs if you do not want them to be imported):
-|Context|Workflows|Workflow Rules|Allergy Reaction|History Macros|Element Attributes|Element Mapping|
+|Context|Workflows|Workflow Rules|Allergy Reaction|History Macros|Element Attributes|Element Mapping|Drug Sets|
 
 The context tab should have the following headers:
 |PAS Code|Context Name|Subspecialty|Consultant|Cost Code|Service Enabled|Context Enabled|Active|
@@ -58,6 +58,10 @@ The element attributes tab should have the following headers:
 The element mapping tab should have the following headers:
 |Attribute Label|Order |Value |Subspecialty|Exclusion    |
 |String         |Number|String|String      |CSV String   |
+
+The drug sets tab should have the following headers:
+|Name  |Hidden|Site       |Usage Code|Subspecialty|Medicine Name|Default Dose|Default Route|Default Frequency|Default Duration|
+|String|Yes/No|String/None|String    |String/None |String       |Number      |String       |String           |String          |
 
 If you want a value to be Null please include 'Blank' in the cell
 If you want all values then please include 'All' in the cell
@@ -101,6 +105,8 @@ EOH;
         $this->elementAttributesImport();
 
         $this->elementMappingImport();
+
+        $this->drugSetImport();
 
         echo "\n[" . (date("Y-m-d H:i:s")) . "] Import Configuration finished ... OK - took: " . (microtime(true) - $t) . "s\n";
     }
@@ -162,8 +168,7 @@ EOH;
             $firm->can_own_an_episode = ($service_enabled == "No") ? 0 : 1;
             $firm->runtime_selectable = ($context_enabled == "No") ? 0 : 1;
             $firm->active = ($active == "No") ? 0 : 1;
-
-            (\Firm::model()->findByAttributes(array('name'=>$context_name)) == null) ? $firm->insert() : $firm->save(false);
+            $firm->save(false);
         }
         echo "\n\t[" . (date("Y-m-d H:i:s")) . "] Context Import finished ... OK - took: " . (microtime(true) - $t) . "s\n";
     }
@@ -219,7 +224,7 @@ EOH;
                 $workflow = new OEModule\OphCiExamination\models\OphCiExamination_Workflow;
                 $workflow->name = ($workflow_name == "Blank") ? null : $workflow_name;
                 $workflow->institution_id = $this->institution_id;
-                $workflow->insert();
+                $workflow->save(false);
                 $workflow_id = $workflow->id;
             } else {
                 $workflow_id = OEModule\OphCiExamination\models\OphCiExamination_Workflow::model()->findByAttributes(array('name' => $workflow_name, 'institution_id' => $this->institution_id))->id;
@@ -231,7 +236,7 @@ EOH;
                 $element_set = new OEModule\OphCiExamination\models\OphCiExamination_ElementSet;
                 $element_set->name = ($step == "Blank") ? null : $step;
                 $element_set->workflow_id = $workflow_id;
-                $element_set->insert();
+                $element_set->save(false);
                 $set_id = $element_set->id;
             } else {
                 $set_id = OEModule\OphCiExamination\models\OphCiExamination_ElementSet::model()->findByAttributes(array('name'=>$step, 'workflow_id'=>$workflow_id))->id;
@@ -252,7 +257,7 @@ EOH;
             $element_set_item->element_type_id = $element_id;
             $element_set_item->display_order = ($order == "Blank") ? null : $order;
             $element_set_item->is_mandatory = ($mandatory == "Yes") ? 1 : 0;
-            $element_set_item->insert();
+            $element_set_item->save(false);
         }
         echo "\n\t[" . (date("Y-m-d H:i:s")) . "] Workflow Import finished ... OK - took: " . (microtime(true) - $t) . "s\n";
     }
@@ -307,7 +312,7 @@ EOH;
             $workflow_rule->subspecialty_id = $subspecialty_id;
             $workflow_rule->firm_id = $firm_id;
             $workflow_rule->episode_status_id = $episode_status_id;
-            $workflow_rule->insert();
+            $workflow_rule->save(false);
         }
         echo "\n\t[" . (date("Y-m-d H:i:s")) . "] Workflow Rules Import finished ... OK - took: " . (microtime(true) - $t) . "s\n";
     }
@@ -333,7 +338,7 @@ EOH;
             if (OphCiExaminationAllergyReaction::model()->findByAttributes(array('name'=>$name)) == null) {
                 $allergy_reaction = new OphCiExaminationAllergyReaction;
                 $allergy_reaction->name = $name;
-                $allergy_reaction->insert();
+                $allergy_reaction->save(false);
             }
         }
         echo "\n\t[" . (date("Y-m-d H:i:s")) . "] Allergy Reaction Import finished ... OK - took: " . (microtime(true) - $t) . "s\n";
@@ -372,8 +377,7 @@ EOH;
             $history_macro->name = $name;
             $history_macro->body = $body;
             $history_macro->active = 1;
-
-            (OEModule\OphCiExamination\models\HistoryMacro::model()->findByAttributes(array('name'=>$name)) == null) ? $history_macro->insert() : $history_macro->save(false);
+            $history_macro->save(false);
 
             $history_macro_id = $history_macro->id;
 
@@ -381,7 +385,7 @@ EOH;
                 $history_macro_subspecialty = new OEModule\OphCiExamination\models\HistoryMacro_Subspecialty;
                 $history_macro_subspecialty->history_macro_id = $history_macro_id;
                 $history_macro_subspecialty->subspecialty_id = $subspecialty_id;
-                $history_macro_subspecialty->insert();
+                $history_macro_subspecialty->save(false);
             }
         }
         echo "\n\t[" . (date("Y-m-d H:i:s")) . "] History Macros Import finished ... OK - took: " . (microtime(true) - $t) . "s\n";
@@ -422,7 +426,7 @@ EOH;
                 $attribute->label = $label;
                 $attribute->is_multiselect = ($multiselect == "Yes") ? 1 : 0;
                 $attribute->institution_id = $this->institution_id;
-                $attribute->insert();
+                $attribute->save(false);
                 $attribute_id = $attribute->id;
             } else {
                 $attribute_id = OEModule\OphCiExamination\models\OphCiExamination_Attribute::model()->findByAttributes(array('name'=>$name, 'label'=>$label, 'institution_id'=>$this->institution_id))->id;
@@ -432,7 +436,7 @@ EOH;
                 $attribute_element = new OEModule\OphCiExamination\models\OphCiExamination_AttributeElement;
                 $attribute_element->attribute_id = $attribute_id;
                 $attribute_element->element_type_id = $element_id;
-                $attribute_element->insert();
+                $attribute_element->save(false);
             }
         }
         echo "\n\t[" . (date("Y-m-d H:i:s")) . "] Element Attributes Import finished ... OK - took: " . (microtime(true) - $t) . "s\n";
@@ -460,11 +464,11 @@ EOH;
             $subspecialty = $row[3];
             $exclusions = explode(",", $row[4]);
 
-            if (OEModule\OphCiExamination\models\OphCiExamination_Attribute::model()->findByAttributes(array('label' => $label)) == null) {
+            if (OEModule\OphCiExamination\models\OphCiExamination_Attribute::model()->findByAttributes(array('label' => $label, 'institution_id' => $this->institution_id)) == null) {
                 echo "\033[0;31mError: \033[0m".$label." is not a valid attribute label\n";
                 exit(8);
             }
-            $attribute_element_id = OEModule\OphCiExamination\models\OphCiExamination_Attribute::model()->findByAttributes(array('label' => $label))->id;
+            $attribute_element_id = OEModule\OphCiExamination\models\OphCiExamination_Attribute::model()->findByAttributes(array('label' => $label, 'institution_id' => $this->institution_id))->id;
 
             if ($subspecialty != "All" && \Subspecialty::model()->findByAttributes(array('name' => $subspecialty)) == null) {
                 echo "\033[0;31mError: \033[0m".$subspecialty." is not a valid subspecialty\n";
@@ -480,8 +484,7 @@ EOH;
             $element_mapping->subspecialty_id = $subspecialty_id;
             $element_mapping->attribute_element_id = $attribute_element_id;
             $element_mapping->display_order = $order;
-
-            (OEModule\OphCiExamination\models\OphCiExamination_AttributeOption::model()->findByAttributes(array('subspecialty_id'=>$subspecialty_id, 'attribute_element_id'=>$attribute_element_id, 'display_order'=>$order)) == null) ? $element_mapping->insert() : $element_mapping->save(false);
+            $element_mapping->save(false);
 
             $option_id = $element_mapping->id;
 
@@ -501,5 +504,112 @@ EOH;
             }
         }
         echo "\n\t[" . (date("Y-m-d H:i:s")) . "] Element Mapping Import finished ... OK - took: " . (microtime(true) - $t) . "s\n";
+    }
+
+    public function drugSetImport()
+    {
+        if ($this->spreadsheet->getSheetByName('Drug Sets') == null) {
+            echo "\n\t Skipping Drug Set Import ... \n";
+            return;
+        }
+        $t = microtime(true);
+        echo "\n\t[" . (date("Y-m-d H:i:s")) . "] Drug Set Import started ... \n";
+
+        foreach ($this->spreadsheet->getSheetByName('Drug Sets')->toArray() as $index => $row) {
+            // Skipping header
+            if ($index == 0) {
+                continue;
+            }
+
+            $name = $row[0];
+            $hidden = $row[1];
+            $site = $row[2];
+            $usage_code = $row[3];
+            $subspecialty = $row[4];
+            $medicine_name = $row[5];
+            $default_dose = $row[6];
+            $default_route = $row[7];
+            $default_frequency = $row[8];
+            $default_duration = $row[9];
+
+            $drug_set = null;
+
+            (\MedicationSet::model()->findByAttributes(array('name'=>$name)) == null) ? $drug_set = new \MedicationSet : $drug_set = \MedicationSet::model()->findByAttributes(array('name'=>$name));
+
+            $drug_set->name = $name;
+            $drug_set->hidden = ($hidden == "Yes") ? 1 : 0;
+            $drug_set->automatic = 1;
+            $drug_set->save(false);
+
+            $drug_set_id = $drug_set->id;
+
+            if ($site != "None" && Site::model()->findByAttributes(array('name' => $site)) == null) {
+                echo "\033[0;31mError: \033[0m".$site." is not a valid Site\n";
+                exit(8);
+            }
+            $site_id = ($site == "None") ? null : \Site::model()->findByAttributes(array('name' => $site))->id;
+
+            if ($subspecialty != "None" && \Subspecialty::model()->findByAttributes(array('name' => $subspecialty)) == null) {
+                echo "\033[0;31mError: \033[0m".$subspecialty." is not a valid Subspecialty\n";
+                exit(8);
+            }
+            $subspecialty_id = ($subspecialty == "None") ? null : \Subspecialty::model()->findByAttributes(array('name' => $subspecialty))->id;
+
+            if ($usage_code != "None" && \MedicationUsageCode::model()->findByAttributes(array('name' => $usage_code)) == null) {
+                echo "\033[0;31mError: \033[0m".$usage_code." is not a valid Usage Code\n";
+                exit(8);
+            }
+            $usage_code_id = ($usage_code == "None") ? null : \MedicationUsageCode::model()->findByAttributes(array('name' => $usage_code))->id;
+
+            $drug_set_rule = null;
+
+            (\MedicationSetRule::model()->findByAttributes(array('medication_set_id'=>$drug_set_id, 'subspecialty_id'=>$subspecialty_id, 'site_id'=>$site_id, 'usage_code_id'=>$usage_code_id)) == null) ? $drug_set_rule = new \MedicationSetRule : $drug_set_rule = \MedicationSetRule::model()->findByAttributes(array('medication_set_id'=>$drug_set_id, 'subspecialty_id'=>$subspecialty_id, 'site_id'=>$site_id, 'usage_code_id'=>$usage_code_id));
+
+            $drug_set_rule->medication_set_id = $drug_set_id;
+            $drug_set_rule->subspecialty_id = $subspecialty_id;
+            $drug_set_rule->site_id = $site_id;
+            $drug_set_rule->usage_code_id = $usage_code_id;
+            $drug_set_rule->save(false);
+
+            if (\Medication::model()->findByAttributes(array('short_term' => $medicine_name)) == null) {
+                echo "\033[0;31mError: \033[0m".$medicine_name." is not a valid Medicine\n";
+                exit(8);
+            }
+            $medication_id = \Medication::model()->findByAttributes(array('short_term' => $medicine_name))->id;
+            $default_dose_unit_term = \Medication::model()->findByAttributes(array('short_term' => $medicine_name))->default_dose_unit_term;
+
+            if (\MedicationRoute::model()->findByAttributes(array('term' => $default_route)) == null) {
+                echo "\033[0;31mError: \033[0m".$default_route." is not a valid Route\n";
+                exit(8);
+            }
+            $default_route_id = \MedicationRoute::model()->findByAttributes(array('term' => $default_route))->id;
+
+            if (\MedicationDuration::model()->findByAttributes(array('name' => $default_duration)) == null) {
+                echo "\033[0;31mError: \033[0m".$default_duration." is not a valid Duration\n";
+                exit(8);
+            }
+            $default_duration_id = \MedicationDuration::model()->findByAttributes(array('name' => $default_duration))->id;
+
+            if (\MedicationFrequency::model()->findByAttributes(array('term' => $default_frequency)) == null) {
+                echo "\033[0;31mError: \033[0m".$default_frequency." is not a valid Frequency\n";
+                exit(8);
+            }
+            $default_frequency_id = \MedicationFrequency::model()->findByAttributes(array('term' => $default_frequency))->id;
+
+            if (\MedicationSetAutoRuleMedication::model()->findByAttributes(array('medication_set_id'=>$drug_set_id, 'medication_id'=>$medication_id, 'default_dose'=>$default_dose, 'default_route_id'=>$default_route_id, 'default_frequency_id'=>$default_frequency_id, 'default_dose_unit_term'=>$default_dose_unit_term, 'default_duration_id'=>$default_duration_id)) == null) {
+                $medication_set_auto_rule_medication = new \MedicationSetAutoRuleMedication;
+                $medication_set_auto_rule_medication->medication_set_id = $drug_set_id;
+                $medication_set_auto_rule_medication->medication_id = $medication_id;
+                $medication_set_auto_rule_medication->default_dose = $default_dose;
+                $medication_set_auto_rule_medication->default_route_id = $default_route_id;
+                $medication_set_auto_rule_medication->default_dose_unit_term = $default_dose_unit_term;
+                $medication_set_auto_rule_medication->default_duration_id = $default_duration_id;
+                $medication_set_auto_rule_medication->default_frequency_id = $default_frequency_id;
+                $medication_set_auto_rule_medication->save(false);
+            }
+        }
+        // Rebuilding all sets
+        \MedicationSet::populateAutoSets();
+        echo "\n\t[" . (date("Y-m-d H:i:s")) . "] Drug Set Import finished ... OK - took: " . (microtime(true) - $t) . "s\n";
     }
 }
