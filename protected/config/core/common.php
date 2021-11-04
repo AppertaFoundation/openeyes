51d67ab73f0fc2f11c4b6b399ad5cc0d119e750c
biskyt <biskyt@users.noreply.github.com>
2021-11-04 17:20:37 +0000

    update version tag

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -127,7 +127,7 @@ $config = array(
         ),
         'cacheBuster' => array(
             'class' => 'CacheBuster',
-            'time' => '202104011656',
+            'time' => '202111041720',
         ),
         'clientScript' => array(
             'class' => 'ClientScript',
@@ -735,7 +735,7 @@ $config = array(
         'default_patient_import_subspecialty' => 'GL',
         //        Add elements that need to be excluded from the admin sidebar in settings
         'exclude_admin_structure_param_list' => getenv('OE_EXCLUDE_ADMIN_STRUCT_LIST') ? explode(",", getenv('OE_EXCLUDE_ADMIN_STRUCT_LIST')) : array(''),
-        'oe_version' => '4.1.2-nightly',
+        'oe_version' => '4.1.2',
         'gp_label' => !empty(trim(getenv('OE_GP_LABEL'))) ? getenv('OE_GP_LABEL') : null,
         'general_practitioner_label' => !empty(trim(getenv('OE_GENERAL_PRAC_LABEL'))) ? getenv('OE_GENERAL_PRAC_LABEL') : null,
         // number of days in the future to retrieve worklists for the automatic dashboard render (0 by default in v3)
