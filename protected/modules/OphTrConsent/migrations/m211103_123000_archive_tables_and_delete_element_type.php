38eef5d7499c29b03dd4690135cf1dd29eaf1add
Arpad Horvath <horvatharpad89@gmail.com>
2021-11-04 13:20:01 +0100

    OE-12060: archive old tables

---
+++ protected/modules/OphTrConsent/migrations/m211103_123000_archive_tables_and_delete_element_type.php
@@ -0,0 +1,42 @@
+<?php
+
+class m211103_123000_archive_tables_and_delete_element_type extends OEMigration
+{
+
+    const OLD_TABLES = array(
+        "et_ophtrconsent_interpreter_signature",
+        "et_ophtrconsent_witness_signature",
+        "et_ophtrconsent_patient_signature",
+        "et_ophtrconsent_parent_signature",
+        "et_ophtrconsent_child_signature",
+        "et_consultant_signature",
+        "ophtrconsent_permissions_images",
+        "et_ophtrconsent_permissions",
+        "et_ophtrconsent_other",
+        "et_ophtrconsent_best_interest_decision_ppl",
+        "et_ophtrconsent_consultant_signature_with_second_opinion",
+        "et_ophtrconsent_best_interest_decision_deputy_signature",
+        );
+
+    public function safeUp()
+    {
+        foreach (self::OLD_TABLES as $table) {
+            if ($this->dbConnection->schema->getTable($table)) {
+                $version_table = $table.'_version';
+                $this->renameTable($table, 'archive_'.$table);
+                $this->renameTable($version_table, 'archive_'.$version_table);
+            }
+        }
+    }
+
+    public function safeDown()
+    {
+        foreach (self::OLD_TABLES as $table) {
+            if ($this->dbConnection->schema->getTable($table)) {
+                $version_table = $table.'_version';
+                $this->renameTable('archive_'.$table, $table);
+                $this->renameTable('archive_'.$version_table, $version_table);
+            }
+        }
+    }
+}
