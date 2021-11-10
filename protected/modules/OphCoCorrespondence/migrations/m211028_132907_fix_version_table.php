5f82cd0fe26b87e5575c616864bab45523e74173
codefactor-io <support@codefactor.io>
2021-11-10 09:31:52 +0000

    [CodeFactor] Apply fixes

--- protected/modules/OphCoCorrespondence/migrations/m211028_132907_fix_version_table.php
+++ protected/modules/OphCoCorrespondence/migrations/m211028_132907_fix_version_table.php
@@ -4,23 +4,23 @@ class m211028_132907_fix_version_table extends OEMigration
 {
     private const TABLE = "et_ophcocorrespondence_letter_version";
 
-	public function up()
-	{
+    public function up()
+    {
         if ($this->dbConnection->schema->getTable(self::TABLE)->getColumn('signatory_id')) {
             $this->dropColumn(self::TABLE, 'signatory_id');
         }
         if ($this->dbConnection->schema->getTable(self::TABLE)->getColumn('created_by')) {
             $this->dropColumn(self::TABLE, 'created_by');
         }
-	}
+    }
 
-	public function down()
-	{
+    public function down()
+    {
         if (!is_null($this->dbConnection->schema->getTable(self::TABLE)->getColumn('created_by'))) {
             $this->addColumn(self::TABLE, "created_by", "INT(10) unsigned NOT NULL");
         }
         if (!is_null($this->dbConnection->schema->getTable(self::TABLE)->getColumn('signatory_id'))) {
             $this->addColumn(self::TABLE, "signatory_id", "INT(10) unsigned NOT NULL");
         }
-	}
+    }
 }
