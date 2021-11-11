27e7f0e2e293f49e0e53eedb8ef6dd508a6109e1
Arpad Horvath <horvatharpad89@gmail.com>
2021-11-11 09:32:43 +0100

    OE-12060: fix consent form 1 label

--- protected/modules/OphTrConsent/migrations/m200920_000000_rename_consent_form_types.php
+++ protected/modules/OphTrConsent/migrations/m200920_000000_rename_consent_form_types.php
@@ -4,7 +4,7 @@ class m200920_000000_rename_consent_form_types extends OEMigration
 {
     public function up()
     {
-        $this->execute("UPDATE ophtrconsent_type_type SET name = '1. Patient agreement to investigation or treatment' WHERE id = 1;");
+        $this->execute("UPDATE ophtrconsent_type_type SET name = '1. Patient agreement to investigation or treatment for adults with mental capacity to give valid consent' WHERE id = 1;");
         $this->execute("UPDATE ophtrconsent_type_type SET name = '2. Parental agreement to investigation or treatment for a child or young person' WHERE id = 2;");
         $this->execute("UPDATE ophtrconsent_type_type SET name = '3. Patient/parental agreement to investigation or treatment (procedures where consciousness not impaired)' WHERE id = 3;");
         $this->execute("UPDATE ophtrconsent_type_type SET name = '4. Form for adults who are unable to consent to investigation or treatment' WHERE id = 4;");
