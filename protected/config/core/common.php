2d25fcee508fbdaac4873af79d854da7d0b983aa
biskyt <biskyt@users.noreply.github.com>
2022-01-06 10:59:20 +0000

    update cahcebuster to force build

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -133,7 +133,7 @@ $config = array(
         ),
         'cacheBuster' => array(
             'class' => 'CacheBuster',
-            'time' => '202201060939',
+            'time' => '202201061059',
         ),
         'clientScript' => array(
             'class' => 'ClientScript',
