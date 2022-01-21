5b688015a695a9e19e9ffc14069d3d8a9166391f
Angus-Cameron <51976505+Angus-Cameron@users.noreply.github.com>
2022-01-21 13:16:33 +1100

    fix/OE-12566 added more informative practice logging and added internal practice id to practice page to allow for locating practice with legacy audit data (#7467)
    Co-authored-by: Andrew Pigram <andrew.pigram@toukanlabs.com>

--- protected/controllers/PracticeController.php
+++ protected/controllers/PracticeController.php
@@ -308,7 +308,7 @@ class PracticeController extends BaseController
                             Audit::add(
                                 'Practice',
                                 $action . '-practice',
-                                "Practice manually [id: $practice->id] {$action}ed."
+                                "Practice manually [id: $practice->id, contact id: $contact->id] {$action}ed."
                             );
                             if (!$isAjax) {
                                 if (count($gpIdProviderNoList) === 0) {
@@ -342,7 +342,7 @@ class PracticeController extends BaseController
                         Audit::add(
                             'Practice',
                             $action . '-practice',
-                            "Practice manually [id: $practice->id] {$action}ed."
+                            "Practice manually [id: $practice->id, contact id: $contact->id] {$action}ed."
                         );
                         if (!$isAjax) {
                             $this->redirect(array('view', 'id' => $practice->id));
