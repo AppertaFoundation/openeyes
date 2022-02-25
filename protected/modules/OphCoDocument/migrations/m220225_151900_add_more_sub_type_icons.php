44fb5b94df5d6e67663ea12510d9bc314a0bc99e
biskyt <biskyt@users.noreply.github.com>
2022-02-25 15:37:08 +0000

    OE-12735 - add some additional icon options to document sub-types

--- protected/modules/OphCoDocument/migrations/m220225_151900_add_more_sub_type_icons.php
+++ protected/modules/OphCoDocument/migrations/m220225_151900_add_more_sub_type_icons.php
@@ -6,11 +6,9 @@ class m220225_151900_add_more_sub_type_icons extends CDbMigration
     {
         $icon_names = ['i-aM', 'i-aW', 'i-aC', 'i-aC', 'i-aD', 'i-aA', 'i-aV'];
 
-        // set start of display order - original migration ended at 1180
-        $key = 1190;
-
         foreach ($icon_names as $key => $event_icon) {
-            $key = ($key + 10);
+            // original migration finished at 1180, so starting this one from 1190
+            $key = ($key + 190) * 10;
             $this->insert('event_icon', ['name' => $event_icon, 'display_order' => $key]);
         }
     }
