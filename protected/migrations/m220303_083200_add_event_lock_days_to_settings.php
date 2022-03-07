d70026ffac21f740ca190796906d65fec6f51be1
Toby Bisco <biskyt@users.noreply.github.com>
2022-03-07 13:04:38 +0000

    OE-12307 - change default lock days to 0 (#7695)

--- protected/migrations/m220303_083200_add_event_lock_days_to_settings.php
+++ protected/migrations/m220303_083200_add_event_lock_days_to_settings.php
@@ -4,7 +4,7 @@ class m220303_083200_add_event_lock_days_to_settings extends OEMigration
 {
     public function safeUp()
     {
-        $this->insert('setting_metadata', ['key' => 'event_lock_days', 'field_type_id' => 4, 'name' => 'Number of full days events can be edited for', 'default_value' => 1, 'lowest_setting_level' => 'INSTALLATION']);
+        $this->insert('setting_metadata', ['key' => 'event_lock_days', 'field_type_id' => 4, 'name' => 'Number of full days events can be edited for', 'default_value' => 0, 'lowest_setting_level' => 'INSTALLATION']);
     }
 
     public function safeDown()
