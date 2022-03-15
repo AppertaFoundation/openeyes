fabd242051855a076265d96f7243024bafa2ab08
biskyt <biskyt@users.noreply.github.com>
2022-03-15 12:42:30 +0000

    improve constarints for PSDPGD tables

--- protected/modules/OphDrPGDPSD/migrations/m220315_114101_add_constraints_for_PSD.php
+++ protected/modules/OphDrPGDPSD/migrations/m220315_114101_add_constraints_for_PSD.php
@@ -4,11 +4,13 @@ class m220315_114101_add_constraints_for_PSD extends OEMigration
 {
     public function safeUp()
     {
-        // Add a unique constraint for team names
+        // Add a unique constraints
         $this->execute("ALTER TABLE team ADD CONSTRAINT team_unique UNIQUE KEY (`name`,`institution_id`,`active`);");
+        $this->execute("ALTER TABLE ophdrpgdpsd_pgdpsd ADD CONSTRAINT psdpgd_unique UNIQUE KEY (`name`,`institution_id`,`type`,`active`);");
 
         // Change active default to 1
         $this->alterOEColumn('team', 'active', 'tinyint(1) DEFAULT 1 NULL', true);
+        $this->alterOEColumn('ophdrpgdpsd_pgdpsd', 'active', 'tinyint(1) DEFAULT 1 NOT NULL', true);
     }
 
     public function safeDown()
