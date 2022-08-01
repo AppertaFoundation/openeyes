8e672bad5b85c0af7136d121b23bd7e5fee0addb
gabordikan <77804349+gabordikan@users.noreply.github.com>
2022-08-01 17:06:47 +0200

    OE-13440: remove criteria when 'All' is selected in the filter box (#8222)
    Co-authored-by: gabordikan <gabordikan@users.noreply.github.com>

--- protected/modules/OphTrOperationbooking/controllers/TheatreDiaryController.php
+++ protected/modules/OphTrOperationbooking/controllers/TheatreDiaryController.php
@@ -304,17 +304,22 @@ class TheatreDiaryController extends BaseModuleController
         if (@$data['emergency_list']) {
             $criteria->addCondition('firm.id IS NULL');
         } else {
-            $criteria->addCondition('theatre.site_id = :siteId and subspecialty_id = :subspecialtyId');
-            $criteria->params[':siteId'] = $data['site-id'];
-            $criteria->params[':subspecialtyId'] = $data['subspecialty-id'];
+            if ($data['site-id'] !== 'All') {
+                $criteria->addCondition('theatre.site_id = :siteId');
+                $criteria->params[':siteId'] = $data['site-id'];
+            }
+            if ($data['subspecialty-id'] !== 'All') {
+                $criteria->addCondition('subspecialty_id = :subspecialtyId');
+                $criteria->params[':subspecialtyId'] = $data['subspecialty-id'];
+            }
         }
 
-        if (@$data['ward-id']) {
+        if (@$data['ward-id'] && $data['ward-id'] !== 'All' ) {
             $criteria->addCondition('ward.id = :wardId');
             $criteria->params[':wardId'] = $data['ward-id'];
         }
 
-        if (@$data['firm-id']) {
+        if (@$data['firm-id'] && $data['firm-id'] !== 'All') {
             $criteria->addCondition('firm.id = :firmId');
             $criteria->params[':firmId'] = $data['firm-id'];
         }
