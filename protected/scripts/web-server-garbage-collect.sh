48ee2a859f151670bd894f1ce3bd5ee5b012a01f
biskyt <biskyt@users.noreply.github.com>
2022-08-12 16:54:15 +0100

    improvements to garbage collection script

--- protected/scripts/web-server-garbage-collect.sh
+++ protected/scripts/web-server-garbage-collect.sh
@@ -1,19 +1,21 @@
 #/bin/bash
 
-seconds=1800
-echo $1
-# allow seconds to be overidden as a command arg
-if [ -n "$1" ]; then seconds=$1; fi
+waitseconds=1800
+stalemins=10
 
-echo "Starting background garbage collection every $seconds"
+# allow seconds to be overidden as a command arg
+if [ -n "$1" ]; then waitseconds=$1; fi
+if [ -n "$2" ]; then stalemins=$2; fi
+
+echo "Starting background garbage collection every $waitseconds seconds"
 
 while [ true ]; do
-    sleep $seconds
+    sleep $waitseconds
     echo "Running garbage collection..."
     beforeP="Before-Processes: $(ps waxu | grep chrome | wc -l)"
     beforeF="Before-Files: $(find /tmp -name 'puppeteer_dev_chrome_profile-*' | wc -l)"
-    killall --older-than 30m chrome
-    find /tmp/puppeteer_dev_chrome_profile-* -mmin +30 -type d -exec rm -rf {} \;
+    killall --older-than 20m chrome
+    find /tmp -maxdepth 1 -name "puppeteer_dev_chrome_profile-*" -mmin +$stalemins -type d -exec rm -rf {} \;
     afterP="After-Processes: $(ps waxu | grep chrome | wc -l)"
     afterF="After-Files: $(find /tmp -name 'puppeteer_dev_chrome_profile-*' | wc -l)"
     echo -e "${beforeP}, ${beforeF}, ${afterP}, ${afterF}" >>/var/log/removePuppeteer.log
