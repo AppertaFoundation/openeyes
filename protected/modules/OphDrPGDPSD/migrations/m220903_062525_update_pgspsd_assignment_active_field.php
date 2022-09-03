8d46916dc0613c9ebf9ad72cfeb0c60b2d392950
codefactor-io <support@codefactor.io>
2022-09-03 06:52:53 +0000

    [CodeFactor] Apply fixes to commit 675de69

--- protected/modules/OphDrPGDPSD/migrations/m220903_062525_update_pgspsd_assignment_active_field.php
+++ protected/modules/OphDrPGDPSD/migrations/m220903_062525_update_pgspsd_assignment_active_field.php
@@ -2,14 +2,14 @@
 
 class m220903_062525_update_pgspsd_assignment_active_field extends OEMigration
 {
-	public function safeUp()
-	{
+    public function safeUp()
+    {
         $this->update('ophdrpgdpsd_assignment', ['active' => 0], "active IS NULL");
-	}
+    }
 
-	public function safeDown()
-	{
-		echo "m220903_062525_update_pgspsd_assignment_active_field does not support migration down.\n";
-		return false;
-	}
+    public function safeDown()
+    {
+        echo "m220903_062525_update_pgspsd_assignment_active_field does not support migration down.\n";
+        return false;
+    }
 }
