db229fc3dc5dfea4be8d080b78f8ba870ff67c9a
splatEric <splatEric@users.noreply.github.com>
2022-09-05 15:02:41 +0100

    OE-13427: ensure contact is displayed after being saved

--- cypress/e2e/modules/examination/eyedraw/elements/contacts.cy.js
+++ cypress/e2e/modules/examination/eyedraw/elements/contacts.cy.js
@@ -23,7 +23,7 @@ describe('contacts widget behaviour', () => {
         cy.get('#add-contacts-btn').click();
 
         cy.get('#contacts-popup')
-            .should('be.visible') 
+            .should('be.visible')
             .within(() => {
                 return cy.getModelByAttributes('ContactLabel', {name: 'Next of Kin'})
                     .then((contactLabel) => {
@@ -37,7 +37,7 @@ describe('contacts widget behaviour', () => {
 
                         cy.get('.add-icon-btn').click();
                     })
-                
+
             })
             .then(() => {
                 cy.get('.oe-popup')
@@ -49,10 +49,20 @@ describe('contacts widget behaviour', () => {
                         cy.get('[data-label="primary_phone"]').type('0123 456 789');
 
                         cy.get('input[type="submit"]').click();
+                    })
+                    .then(() => {
+                        cy.wait('@saveNewContact');
+                        cy.get('#OEModule_OphCiExamination_models_Element_OphCiExamination_Contacts_entry_table')
+                            .within(() => {
+                                cy.get('tbody tr').should('have.length', 1);
+                            })
                     });
-            });        
 
-        cy.wait('@saveNewContact');
+            });
+
+        // it's extremely difficult to reliably assert a request hasn't happened more than once
+        // so here we simply wait a second and then check the interception count is 1
+        cy.wait(1000);
 
         cy.get('@saveNewContact.all').then((interceptions) => {
             expect(interceptions).to.have.length(1);
