7ca8871d666a183fa69f479771059109dcaaac32
Geekybrucy <peichun.zhou@toukanlabs.com>
2022-09-05 21:55:13 +1000

    fix/OE-13479-v6.2.x: Addressed PR review
    [DrugAdministration_event_edit.php]
    * Reverted the $is_active conversion
    
    [DrugAdministration.php]
    * Set assignment active to 0 if there is no 'active' index in the POST data
    
    [m220903_062525_update_pgspsd_assignment_active_field.php]
    * Added update ophdrpgdpsd_assignment_version table
    * Altered the column not to allow null value
    * Added down implementation to roll back table schema
    
    [OphDrPGDPSD_Assignment]
    * Added extra validation rule to active field to set data type and not allow empty

--- protected/modules/OphDrPGDPSD/migrations/m220903_062525_update_pgspsd_assignment_active_field.php
+++ protected/modules/OphDrPGDPSD/migrations/m220903_062525_update_pgspsd_assignment_active_field.php
@@ -2,14 +2,15 @@
 
 class m220903_062525_update_pgspsd_assignment_active_field extends OEMigration
 {
-    public function safeUp()
+    public function up()
     {
         $this->update('ophdrpgdpsd_assignment', ['active' => 0], "active IS NULL");
+        $this->update('ophdrpgdpsd_assignment_version', ['active' => 0], "active IS NULL");
+        $this->alterOEColumn('ophdrpgdpsd_assignment', 'active', 'tinyint(1) DEFAULT 1 NOT NULL', true);
     }
 
-    public function safeDown()
+    public function down()
     {
-        echo "m220903_062525_update_pgspsd_assignment_active_field does not support migration down.\n";
-        return false;
+        $this->alterOEColumn('ophdrpgdpsd_assignment', 'active', 'tinyint(1) DEFAULT 1', true);
     }
 }
