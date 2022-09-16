44d34fb2cbd0c891de5c8af548e26d4102aaefd5
justulka <justinas.cepkauskas@abehr.com>
2022-09-16 17:15:13 +0100

    OE-13645 Create index on worklist patient id

---
+++ protected/migrations/m220916_160353_add_index_to_event_worklist_patient_id.php
@@ -0,0 +1,14 @@
+<?php
+
+class m220916_160353_add_index_to_event_worklist_patient_id extends OEMigration
+{
+	public function up()
+	{
+        $this->createIndex("event_worklist_patient_index", 'event', 'worklist_patient_id');
+	}
+
+	public function down()
+	{
+        $this->dropIndex("event_worklist_patient_index", 'event');
+	}
+}
