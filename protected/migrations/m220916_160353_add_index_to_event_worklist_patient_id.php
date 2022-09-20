b2169baf94c939ec5d97835a3adcf5c1dc03cf81
joseph <joseph.gunnarsson@abehr.com>
2022-09-20 13:26:57 +0100

    OE-13645 added check to see if index already exists

--- protected/migrations/m220916_160353_add_index_to_event_worklist_patient_id.php
+++ protected/migrations/m220916_160353_add_index_to_event_worklist_patient_id.php
@@ -4,11 +4,13 @@ class m220916_160353_add_index_to_event_worklist_patient_id extends OEMigration
 {
     public function up()
     {
-        $this->createIndex("event_worklist_patient_index", 'event', 'worklist_patient_id');
+        if (!$this->dbConnection->createCommand("SHOW INDEX FROM event WHERE Key_name = 'event_ibfk_worklist_patient'")->queryScalar()) {
+            $this->createIndex("event_ibfk_worklist_patient", 'event', 'worklist_patient_id');
+        }
     }
 
     public function down()
     {
-        $this->dropIndex("event_worklist_patient_index", 'event');
+        $this->dropIndex("event_ibfk_worklist_patient", 'event');
     }
 }
