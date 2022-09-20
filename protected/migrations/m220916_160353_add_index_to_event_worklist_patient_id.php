0bf52d62a4ee98d9b641156e8a926ec38f610af5
codefactor-io <support@codefactor.io>
2022-09-20 09:10:05 +0000

    [CodeFactor] Apply fixes to commit 44d34fb

--- protected/migrations/m220916_160353_add_index_to_event_worklist_patient_id.php
+++ protected/migrations/m220916_160353_add_index_to_event_worklist_patient_id.php
@@ -2,13 +2,13 @@
 
 class m220916_160353_add_index_to_event_worklist_patient_id extends OEMigration
 {
-	public function up()
-	{
+    public function up()
+    {
         $this->createIndex("event_worklist_patient_index", 'event', 'worklist_patient_id');
-	}
+    }
 
-	public function down()
-	{
+    public function down()
+    {
         $this->dropIndex("event_worklist_patient_index", 'event');
-	}
+    }
 }
