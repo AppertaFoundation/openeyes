8d57369ab4b0954b7f5a48e5baa67d2d58175e57
1453966+Tomtids <1453966+Tomtids@users.noreply.github.com>
2022-09-21 15:29:22 +0100

    OE-13593: Removing test namespace

--- protected/tests/feature/ProfileTest.php
+++ protected/tests/feature/ProfileTest.php
@@ -13,8 +13,6 @@
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 
-namespace tests\feature;
-
 use Firm;
 use Institution;
 use ModelCollection;
