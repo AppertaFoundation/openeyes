51dd83746d3b8b55e16afad44c68bc572fa64631
Andrew Pigram <andrew.pigram@toukanlabs.com>
2022-09-27 13:45:51 +1000

    Feature/oe 13311 (#8490)
    * Added a setting to determine if optional examination elements should be automatically removed from the event when saved if they are untouched.
    Added a confirmation dialog that will display if the setting has been enabled and there is at least one element dirtied but some optional elements are untouched.
    
    * close_incomplete_exam_elements is now an institution level setting and defaults to 'off' for all institutions.
    
    * JS dialogs no longer require a width to be defined (need to regression test all basic dialogs to ensure no CSS issues). (#8488)
    
    Added support for split-view confirmation dialogs.
    The confirmation dialog when offering to close incomplete examination elements will now list the elements that will be closed.
    Clicking the cancel button in the confirmation dialog will submit the form, which will raise validation errors as per original behaviour.
    
    * Mandatory elements now display an asterisk instead of a disabled trash icon. (#8489)
    
    Added a tooltip to the new icon to indicate the element is mandatory.
    
    * Feature/oe 13619 (#8491)
    
    * Added red asterisk for mandatory elements that fail validation.
    Added error message for mandatory elements if no data has been filled out on them.
    Upgraded to latest newblue to import the red asterisk image.
    
    * Removed afterValidate function override (error message is set elsewhere already).

--- protected/modules/OphCiExamination/assets/js/ExaminationSaveHandler.js
+++ protected/modules/OphCiExamination/assets/js/ExaminationSaveHandler.js
@@ -28,6 +28,7 @@ $(document).ready(() => {
 
     function handleClick(e) {
         e.preventDefault();
+        
         let status;
         let $handler_functions = document.querySelectorAll('.js-save-handler-function');
 
@@ -42,7 +43,68 @@ $(document).ready(() => {
         if (status !== "stop") {
             // get form ends with -create or -update
             let form = document.querySelector('#event-content form[id$="-create"], #event-content form[id$="-update"]');
-            $(form).submit(); // Non JQuery method causes odd behaviour so left for now.
+            if (close_incomplete_exam_elements === 'on') {
+                verifyElements(e, $(form));
+            } else {
+                $(form).submit();
+            }
+        }
+    }
+
+    function verifyElements(e, $form) {
+        let $all_elements = $('section.element');
+        let empty_elements = [];
+        let empty_element_names = [];
+        let dirty_elements = $('input[name^="[element_dirty]"][value="1"]').map(function() { return $(this).parent().data('element-type-class'); }).get();
+        let empty_mandatory_element = false;
+        $all_elements.each(function() {
+            if (!dirty_elements.find((element) => element == $(this).data('element-type-class'))) {
+                if ($(this).data('mandatory') === 'true' && !empty_mandatory_element) {
+                    empty_mandatory_element = true;
+                }
+                empty_elements.push($(this).data('element-type-class'));
+                empty_element_names.push($(this).data('element-type-name'));
+            }
+        });
+
+        if (empty_mandatory_element || dirty_elements.length === 0) {
+            // There should be at least one dirtied element in an examination event, and there must be no empty mandatory elements.
+            $form.submit();
+        } else if (empty_elements.length > 0) {
+            // Need to stop propagation to ensure the OK button is enabled in the confirmation dialog (this handler fires before the base handler that disables all buttons on the screen).
+            e.stopPropagation();
+
+            let list = '<ul class="row-list">';
+            $.each(empty_element_names, function (index, item) {
+                list += '<li>' + item + '</li>';
+            });
+            list += '</ul>';
+            
+            // Display confirmation dialog. When confirmed, wipe out the optional elements.
+            let dialog = new OpenEyes.UI.Dialog.Confirm({
+                title: 'Discard empty elements?',
+                templateSelector: '#dialog-confirm-splitview-template',
+                leftPanelContent: list,
+                rightPanelContent: "The listed elements have received no input or are in-complete, would you like to continue to save and discard them?",
+                okButton: 'Discard & save',
+                okButtonClassList: 'green hint cols-5 ok',
+                cancelButtonClassList: 'cols-5 cancel',
+                popupContentClass: 'oe-popup-content wide'
+            });
+            dialog.on('ok', function () {
+                disableButtons();
+                $.each(empty_elements, function (index, value) {
+                    const $elem = $('section[data-element-type-class="' + value + '"]');
+                    $elem.trigger('element_removed');
+                    removeElement($elem);
+                    $(document).trigger('element_removed');
+                });
+                $form.submit();
+            }.bind(this));
+            dialog.on('cancel', function () {
+                $form.submit();
+            }.bind(this));
+            dialog.open();
         }
     }
     let $save_button = document.getElementById('et_save');
