8f33167dc824e51c3f5ad50ad4c9d093d8b99020
gabordikan <gabordikan@users.noreply.github.com>
2022-09-27 18:36:56 +0100

    fix/OE-13689: some definitions are excluded from the list with union overwritten

--- protected/components/worklist/WorklistManager.php
+++ protected/components/worklist/WorklistManager.php
@@ -307,7 +307,7 @@ class WorklistManager extends CComponent
         $reordered_definitions = [];
         foreach ($definitions as $key => $definition) {
             if ($definition->worklistCount === "0") {
-                $reordered_definitions[] = $definition;
+                $reordered_definitions[$key] = $definition;
                 unset($definitions[$key]);
             }
         }
