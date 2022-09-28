12e282a4d883ab78d133394ecd48792c811d19b4
gabordikan <gabordikan@users.noreply.github.com>
2022-09-28 17:25:27 +0100

    OE-13688: fixed docblock date

--- protected/modules/OphCoCvi/migrations/m220928_091400_add_cvidelivery_settings.php
+++ protected/modules/OphCoCvi/migrations/m220928_091400_add_cvidelivery_settings.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * (C) Copyright Apperta Foundation 2021
+ * (C) Copyright Apperta Foundation 2022
  * This file is part of OpenEyes.
  * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
@@ -9,7 +9,7 @@
  * @link http://www.openeyes.org.uk
  *
  * @author OpenEyes <info@openeyes.org.uk>
- * @copyright Copyright (C) 2021, Apperta Foundation
+ * @copyright Copyright (C) 2022, Apperta Foundation
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
 
