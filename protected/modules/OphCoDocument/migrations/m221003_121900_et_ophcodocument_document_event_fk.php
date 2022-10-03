2a31e8f9f06ed86767c3be78edd6dd579b3fde7c
AdamParkerABEHR <adam.parker@abehr.com>
2022-10-03 13:27:17 +0100

    OE-13708 added event fk to et_ophcodocument_document_ev_fk table

---
+++ protected/modules/OphCoDocument/migrations/m221003_121900_et_ophcodocument_document_event_fk.php
@@ -0,0 +1,25 @@
+<?php
+
+class m221003_121900_et_ophcodocument_document_event_fk extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m221003_121900_et_ophcodocument_document_event_fk does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
