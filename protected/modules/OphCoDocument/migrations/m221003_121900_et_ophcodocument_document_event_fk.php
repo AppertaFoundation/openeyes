a606e0c1dbdac3ae176e0c490c0cd7f431a045cd
AdamParkerABEHR <adam.parker@abehr.com>
2022-10-03 13:34:33 +0100

    OE-13708 added event fk to et_ophcodocument_document_ev_fk table

--- protected/modules/OphCoDocument/migrations/m221003_121900_et_ophcodocument_document_event_fk.php
+++ protected/modules/OphCoDocument/migrations/m221003_121900_et_ophcodocument_document_event_fk.php
@@ -2,24 +2,18 @@
 
 class m221003_121900_et_ophcodocument_document_event_fk extends CDbMigration
 {
-	public function up()
-	{
-	}
-
-	public function down()
-	{
-		echo "m221003_121900_et_ophcodocument_document_event_fk does not support migration down.\n";
-		return false;
-	}
-
-	/*
-	// Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
 	{
+        $this->addForeignKey('et_ophcodocument_document_ev_fk',
+            'et_ophcodocument_document',
+            'event_id',
+            'event',
+            'id');
 	}
 
 	public function safeDown()
 	{
+        $this->dropForeignKey('et_ophcodocument_document_ev_fk',
+            'et_ophcodocument_document');
 	}
-	*/
 }
