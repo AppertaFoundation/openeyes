da1465f97c72822c736f0c7be9683e2ecd622f5a
imas8888 <35904771+imas8888@users.noreply.github.com>
2022-10-08 20:48:07 +0100

    [OE-13416] - Fixing logic of comment

--- protected/views/worklist/step_components/_comment.php
+++ protected/views/worklist/step_components/_comment.php
@@ -7,7 +7,7 @@
 ?>
 <div class="step-comments">
     <?php if (!$partial && $visit) {
-        $comment = ($model instanceof PathwayTypeStep) ? null : $model->comment; ?>
+        $comment = $model && !($model instanceof PathwayTypeStep) ? $model->comment : null; ?>
     <div class="flex js-comments-edit" style="<?= $comment ? 'display: none;' : '' ?>">
         <div class="cols-11">
             <input class="cols-full js-step-comments" type="text" maxlength="80" placeholder="Comments"
@@ -20,10 +20,10 @@
         <i class="oe-i save-plus js-save"></i>
     </div>
     <?php } ?>
-    <div class="flex js-comments-view" style="<?= !$model || !$model->comment ? 'display: none;' : '' ?>">
+    <div class="flex js-comments-view" style="<?= !$comment ? 'display: none;' : '' ?>">
         <div class="cols-11">
             <i class="oe-i comments small pad-right no-click"></i>
-                <em class="comment"><?= $model && !($model instanceof PathwayTypeStep) && $model->comment ? $model->comment->comment : '' ?></em>
+                <em class="comment"><?= $comment ? $comment->comment : '' ?></em>
         </div>
         <?php if (!$partial && $model && !($model instanceof PathwayTypeStep) && $visit->pathway && in_array((int)$visit->pathway->status, Pathway::inProgressStatuses(), true)) { ?>
             <i class="oe-i medium-icon pencil js-edit"></i>
