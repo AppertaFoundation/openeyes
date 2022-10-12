5aa4a4157b7e9860dd34cda51f12c4d2aa788e45
codefactor-io <support@codefactor.io>
2022-10-12 11:50:39 +0000

    [CodeFactor] Apply fixes to commit 311d4b3

--- protected/migrations/m221012_090658_add_pid_to_dicom_file_queue.php
+++ protected/migrations/m221012_090658_add_pid_to_dicom_file_queue.php
@@ -2,13 +2,13 @@
 
 class m221012_090658_add_pid_to_dicom_file_queue extends OEMigration
 {
-	public function safeUp()
-	{
+    public function safeUp()
+    {
         $this->addOEColumn("dicom_file_queue", "pid_type_id", "int(11)");
-	}
+    }
 
-	public function safeDown()
-	{
+    public function safeDown()
+    {
         $this->dropOEColumn("dicom_file_queue", "pid_type_id");
     }
 }
