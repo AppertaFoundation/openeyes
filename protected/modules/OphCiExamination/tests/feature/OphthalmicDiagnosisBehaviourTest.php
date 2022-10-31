b64df48c65c8518310c43935d99f78d02653f4a9
splatEric <splatEric@users.noreply.github.com>
2022-10-31 11:37:20 +1000

    OE-13713: test coverage

--- protected/modules/OphCiExamination/tests/feature/OphthalmicDiagnosisBehaviourTest.php
+++ protected/modules/OphCiExamination/tests/feature/OphthalmicDiagnosisBehaviourTest.php
@@ -15,6 +15,7 @@
 
 namespace OEModule\OphCiExamination\tests\feature;
 use CHtml;
+use OE\factories\models\EventFactory;
 use OEModule\OphCiExamination\models\Element_OphCiExamination_Diagnoses;
 use OEModule\OphCiExamination\models\OphCiExamination_Diagnosis;
 use SecondaryDiagnosis;
@@ -24,25 +25,25 @@ class OphthalmicDiagnosisBehaviourTest extends \OEDbTestCase
     use \HasEventTypeElementAssertions;
     use \MocksSession;
     use \MakesApplicationRequests;
+    use \WithFaker;
     use \WithTransactions;
 
     /** @test */
     public function entries_are_saved_and_reflected_in_patient_record()
     {
-        $test_diagnoses = Element_OphCiExamination_Diagnoses::factory()
-            ->withBilateralDiagnoses(1)
-            ->withRightDiagnoses(1)
-            ->withLeftDiagnoses(1)
-            ->make([
-                'event_id' => null
-            ]);
-
-        list($user, $institution) = $this->createUserWithInstitution();
-
         // set up patient and episode for new event to be attached to
         $episode = \Episode::factory()->create();
         $patient = $episode->patient;
 
+        $test_diagnoses = Element_OphCiExamination_Diagnoses::factory()
+        ->withBilateralDiagnoses(1)
+        ->withRightDiagnoses(1)
+        ->withLeftDiagnoses(1)
+        ->make([
+            'event_id' => null
+        ]);
+
+        list($user, $institution) = $this->createUserWithInstitution();
         $this->mockCurrentContext($episode->firm, null, $institution);
         $form_data = [
             CHtml::modelName($test_diagnoses) => $this->mapElementToFormData($test_diagnoses),
@@ -64,7 +65,61 @@ class OphthalmicDiagnosisBehaviourTest extends \OEDbTestCase
         $this->assertPrincipalDiagnosisRecordedIn($episode, $test_diagnoses->diagnoses);
     }
 
+    /**
+     * Because the secondary diagnoses are only setup via the POST data handling of the
+     * examination module, we have to use the request approach to verify the behaviour
+     *
+     * @test
+     */
     public function deleting_more_recent_examination_reverts_to_previous()
+    {
+        $initial_event = EventFactory::forModule('OphCiExamination')
+        ->create([
+            'event_date' => $this->faker->dateTimeBetween('-4 weeks', '-2 weeks')->format('Y-m-d')
+        ]);
+
+        $initial_diagnoses = Element_OphCiExamination_Diagnoses::factory()
+            ->withBilateralDiagnoses(1)
+            ->withRightDiagnoses(1)
+            ->withLeftDiagnoses(1)
+            ->create(['event_id' => $initial_event->id]);
+        $episode = $initial_event->episode;
+        $patient = $episode->patient;
+
+        list($user, $institution) = $this->createUserWithInstitution();
+        $this->mockCurrentContext($episode->firm, null, $institution);
+
+        $diagnoses_to_be_deleted = Element_OphCiExamination_Diagnoses::factory()
+            ->withBilateralDiagnoses(1)
+            ->withRightDiagnoses(1)
+            ->withLeftDiagnoses(1)
+            ->make(['event_id' => null]);
+
+        $form_data = [
+            CHtml::modelName($diagnoses_to_be_deleted) => $this->mapElementToFormData($diagnoses_to_be_deleted),
+            'principal_diagnosis_row_key' => $this->findPrincipalRowKey($diagnoses_to_be_deleted->diagnoses),
+            'patient_id' => $patient->id
+        ];
+
+        $response = $this->actingAs($user, $institution)
+            ->post('/OphCiExamination/Default/create', $form_data);
+
+        $response->assertRedirectContains('view', 'Expected to redirect to a view of the created event');
+
+        $this->assertSecondaryDiagnosesRecordedFor($patient, $diagnoses_to_be_deleted->diagnoses);
+        $this->assertPrincipalDiagnosisRecordedIn($episode, $diagnoses_to_be_deleted->diagnoses);
+
+        $latest_event = \Event::model()
+            ->findAll(['order' => 'created_date DESC', 'limit' => 1])[0];
+
+        $latest_event->softDelete();
+
+        $this->assertSecondaryDiagnosesRecordedFor($patient, $initial_diagnoses->diagnoses);
+        $this->assertPrincipalDiagnosisRecordedIn($episode, $initial_diagnoses->diagnoses);
+    }
+
+    /** @test */
+    public function patient_diagnoses_unaffected_when_older_examination_is_deleted()
     {
         $this->markTestIncomplete();
     }
