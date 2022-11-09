4d21c5c01031d3c865478ab1293291c324eee556
Geekybrucy <peichun.zhou@toukanlabs.com>
2022-11-09 22:26:32 +1100

    fix/OE-13818
    [protected/widgets/js/EventAutoGenerateCheckboxesWidget.js]
    * Reshuffled code logic
    * - Added a statement to query generate prescription checkbox element
    * - Added generate prescription checkbox element existence check (along with generate gp letter element)
    * - Synced generate gp letter checkbox attribute "checked" and value with generate prescription (as per OE-9363)
    * - Modified the drug set dropdown list display logic. As long as the generate prescription is checked, the dropdown will display

--- protected/widgets/js/EventAutoGenerateCheckboxesWidget.js
+++ protected/widgets/js/EventAutoGenerateCheckboxesWidget.js
@@ -9,19 +9,28 @@ $(document).ready(function() {
         if ($generate_prescription) {
             const $generate_prescription_label = $generate_prescription.closest('label');
 
-            $generate_prescription_label.addEventListener('change', () => {
-                const $generate_gp = document.querySelector(`input[type="checkbox"]#auto_generate_gp_letter_after_${suffix}`);
-                const $dropdown = document.getElementById(`auto_generate_prescription_after_${suffix}_set_id`);
-                if ($generate_gp) {
-                    $generate_gp.click();
+            const $dropdown = document.getElementById(`auto_generate_prescription_after_${suffix}_set_id`);
 
-                    if (!$generate_gp.checked) {
-                        $dropdown.value = "";
-                    }
-                    $dropdown.style.display = $generate_gp.checked ? 'inline-block' : 'none';
-                } else {
+            /**
+             * Binding 'change' event to the Generate prescription checkbox label
+             */
+            $generate_prescription_label.addEventListener('change', function(e) {
+                // get generate gp letter checkbox element
+                const generate_gp = document.querySelector(`input[type="checkbox"]#auto_generate_gp_letter_after_${suffix}`);
+                // get generate prescription checkbox element
+                const generate_px = this.querySelector('input[type="checkbox"]');
+
+                // if there is no generate gp letter checkbox or generate prescription checkbox found
+                // print warning message and exit the callback
+                if (!generate_gp || !generate_px) {
                     console.warn(`Element with id "auto_generate_gp_letter_after_${suffix}" not found`);
+                    return;
                 }
+                // sync generate gp checkbox attribute 'checked' and value with generate prescription checkbox
+                generate_gp.checked = generate_px.checked;
+                generate_gp.value = generate_px.value;
+                // display the drug sets dropdown if the generate prescription checkbox is checked
+                $dropdown.style.display = generate_px.checked ? 'inline-block' : 'none';
             });
         }
     }
