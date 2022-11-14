2e988fb2ace4b140a6e8317e290ed44aab06cff3
splatEric <splatEric@users.noreply.github.com>
2022-11-15 03:51:56 +1000

    OE-13713: clean up

--- protected/controllers/BaseEventTypeController.php
+++ protected/controllers/BaseEventTypeController.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * OpenEyes.
  *
@@ -15,8 +14,6 @@
  * @copyright Copyright (c) 2019, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
-
-use OEModule\OESysEvent\events\EventTypeEventCreated;
 
 /**
  * Class BaseEventTypeController.
