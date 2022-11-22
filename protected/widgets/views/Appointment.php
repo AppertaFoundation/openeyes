821e207fd08cf620eb00f0e971a1cfe8eb3606a6
biskyt <biskyt@users.noreply.github.com>
2022-11-22 15:40:57 +0000

    restrict long appontment lists

--- protected/widgets/views/Appointment.php
+++ protected/widgets/views/Appointment.php
@@ -1,4 +1,5 @@
 <?php
+
     /**
      * OpenEyes.
      *
@@ -19,38 +20,39 @@
 
 <!--Beginning of the side view-->
 <?php if (isset($is_popup)) { ?>
-    <table class="patient-appointments">
-        <colgroup>
-            <col class="cols-1">
-            <col class="cols-6">
-            <col class="cols-2">
-            <col class="cols-3">
-        </colgroup>
-        <tbody>
-        <?php
-        foreach ($worklist_patients as $worklist_patient) {
-            $time = date('H:i', strtotime($worklist_patient->when));
-            $date = \Helper::convertDate2NHS($worklist_patient->worklist->start);
-            $worklist_name = $worklist_patient->worklist->name;
-            $worklist_status = $worklist_patient->getWorklistPatientAttribute('Status');
-            $event = Event::model()->findByAttributes(['worklist_patient_id' => $worklist_patient->id]);
-            ?>
-                <tr>
-                    <td><span class="time"><?= $time ?></span></td>
-                    <td><?= $worklist_name ?></td>
-                    <td><span class="oe-date"><?= $date ?></span></td>
-                    <td>
-                    <?php if (isset($worklist_status)) { ?>
-                            <?= $worklist_status->attribute_value ?>
-                    <?php } elseif ($event && $event->eventType && $event->eventType->class_name === "OphCiDidNotAttend") { ?>
-                            Did not attend.
-                    <?php } ?>
-                    </td>
-                </tr>
-        <?php } ?>
-        </tbody>
-    </table>
-
+    <div class="restrict-data-height rows-10">
+        <table class="patient-appointments">
+            <colgroup>
+                <col class="cols-1">
+                <col class="cols-6">
+                <col class="cols-2">
+                <col class="cols-3">
+            </colgroup>
+            <tbody>
+            <?php
+            foreach ($worklist_patients as $worklist_patient) {
+                $time = date('H:i', strtotime($worklist_patient->when));
+                $date = \Helper::convertDate2NHS($worklist_patient->worklist->start);
+                $worklist_name = $worklist_patient->worklist->name;
+                $worklist_status = $worklist_patient->getWorklistPatientAttribute('Status');
+                $event = Event::model()->findByAttributes(['worklist_patient_id' => $worklist_patient->id]);
+                ?>
+                    <tr>
+                        <td><span class="time"><?= $time ?></span></td>
+                        <td><?= $worklist_name ?></td>
+                        <td><span class="oe-date"><?= $date ?></span></td>
+                        <td>
+                        <?php if (isset($worklist_status)) { ?>
+                                <?= $worklist_status->attribute_value ?>
+                        <?php } elseif ($event && $event->eventType && $event->eventType->class_name === "OphCiDidNotAttend") { ?>
+                                Did not attend.
+                        <?php } ?>
+                        </td>
+                    </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
     <?php if ($past_worklist_patients_count != 0) { ?>
         <div class="collapse-data">
             <div class="collapse-data-header-icon expand js-get-past-appointments" data-patient-id="<?=$this->patient->id?>">
@@ -58,7 +60,7 @@
             </div>
             <div class="collapse-data-content">
                 <div class="restrict-data-shown">
-                    <div class="restrict-data-content rows-10">
+                    <div class="element-data full-width restrict-data-height rows-10"><div class="element-data full-width restrict-data-height rows-10">
                         <!-- restrict data height, overflow will scroll -->
                         <table class="patient-appointments">
                             <colgroup>
@@ -80,18 +82,20 @@
     <!--End of the side view-->
 
 <?php } else { ?>
-    <table class="patient-appointments">
-        <colgroup>
-            <col class="cols-2">
-            <col class="cols-5">
-            <col class="cols-2">
-            <col class="cols-3">
-        </colgroup>
-        <tbody>
-        <?php
-        $this->controller->renderPartial('//default/appointment_entry_tbody', array('worklist_patients' => $worklist_patients))?>
-        </tbody>
-    </table>
+    <div class="restrict-data-height rows-10">
+        <table class="patient-appointments">
+            <colgroup>
+                <col class="cols-2">
+                <col class="cols-5">
+                <col class="cols-2">
+                <col class="cols-3">
+            </colgroup>
+            <tbody>
+            <?php
+            $this->controller->renderPartial('//default/appointment_entry_tbody', array('worklist_patients' => $worklist_patients))?>
+            </tbody>
+        </table>
+    </div>
     <?php if ($past_worklist_patients_count != 0) { ?>
         <div class="collapse-data">
         <div class="collapse-data-header-icon expand js-get-past-appointments" data-patient-id="<?=$this->patient->id?>">
