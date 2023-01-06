2d64e02d713a0148f0e21468ac4ccd94ac07771b
biskyt <biskyt@users.noreply.github.com>
2023-01-06 10:33:53 +0000

    Revert "remove Operation Checklistrs as a default enabled module"
    This reverts commit 26abf36f1ea5967930855eac52a30ec1dcd22365.

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -1027,7 +1027,7 @@ $modules = array(
         'OETrial',
         'SSO',
         'OphOuCatprom5',
-        // 'OphTrOperationchecklists', DEPREACATED and disabled by default, but can be manually enabled for those requiring time to transition the new custom checklists
+        'OphTrOperationchecklists',
         'OphDrPGDPSD',
         'BreakGlass' => array('class' => '\OEModule\BreakGlass\BreakGlassModule'),
         );
