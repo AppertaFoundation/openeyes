770797040781a2d264fed417ea3a1ecabf0c3053
Mike Smith <splatEric@users.noreply.github.com>
2023-02-23 12:39:44 +0000

    Cypress command docs (#8998)
    Co-authored-by: Toby Bisco <biskyt@users.noreply.github.com>

---
+++ cypress/support/yii-commands.d.ts
@@ -0,0 +1,79 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+    interface Chainable<Subject> {
+        /**
+         * Use backend model factories to create a user that can be logged in with
+         *
+         * @param authItems list of RBAC items to assign to the user
+         * @param attributes user model attributes
+         * @param username
+         * @param password
+         */
+        createUser(authitems: ?array, attributes: ?object, username: ?string, password: ?string): Chainable<any>
+        /**
+         * Login to OpenEyes. Defaults to logging in as the admin user
+         *
+         * @param username
+         * @param password
+         */
+        login(username: ?string, password: ?string): Chainable<any>
+        createPatient(states: ?array, attributes: ?object): Chainable<any>
+        /**
+         * Retrieve the URL to create an event for the patient identified by patientId in the given module
+         * @param patientId
+         * @param moduleName
+         */
+        getEventCreationUrl(patientId: Number, moduleName: string): Chainable<any>
+        /**
+         * Use backend model factory to find or create an instance of the given class that matches
+         * all the provided attributes
+         *
+         * @param className
+         * @param attributes
+         */
+        getModelByAttributes(className: string, attributes: ?object): Chainable<any>
+        /**
+         * Use backend model factory to create one or more model instances with the given parameters
+         *
+         * @param className
+         * @param states
+         * @param attributes
+         * @param count
+         * @example
+         * cy.createModels('Disorder', [['forOphthalmology'], ['withICD10']], ['term' => 'A Test Disorder'], 3)
+         */
+        createModels(className: string, states: array[], attributes: array, count: Number): Chainable<any>
+        /**
+         * Run a seeder class in the backend
+         * @param moduleName
+         * @param className
+         * @param additionalData
+         */
+        runSeeder(moduleName: string, className: string, additionalData: object): Chainable<any>
+        /**
+         * Use backend event factory to create an event of the given eventType
+         * @param eventType
+         * @param states
+         * @param attributes
+         * @param count
+         */
+        createEvent(eventType: string, states: array[], attributes: object, count: Number): Chainable<any>
+        /**
+         * Set a system configuration value in the backend
+         * @param settingKey
+         * @param settingValue
+         */
+        setSystemSettingValue(settingKey: string, settingValue: string): Chainable<any>
+        /**
+         * Retrieve the current system setting value from the backend
+         * @param settingKey
+         */
+        getSystemSettingValue(settingKey: string): Chainable<any>
+        /**
+         * Reset the given setting system to default by removing any saved value
+         * @param settingKey
+         */
+        resetSystemSettingValue(settingKey: string): Chainable<any>
+    }
+  }
