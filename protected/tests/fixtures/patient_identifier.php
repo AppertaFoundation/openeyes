5fd85dc2879b34157d710ce447b279b5456b2ba7
preet90 <gurpreet.singh@toukanlabs.com>
2023-03-05 03:40:55 +0000

    reverted the fixture changes as they were breaking other tests. Added a new fixture for the patient identifier to test the new functionality

--- protected/tests/fixtures/patient_identifier.php
+++ protected/tests/fixtures/patient_identifier.php
@@ -4,118 +4,125 @@ return array(
     'patient1ID' => array(
         'patient_id' => $this->getRecord('patient', 'patient1')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
-        'value' => '9998',
+        'value' => '12345',
         'unique_row_string' => "12345-1-ACTIVE",
     ),
     'patient1NHS' => array(
         'patient_id' => $this->getRecord('patient', 'patient1')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'NHS')->id,
-        'value' => '9998',
+        'value' => '54321',
         'unique_row_string' => "54321-2-ACTIVE",
     ),
 
     'patient2ID' => array(
         'patient_id' => $this->getRecord('patient', 'patient2')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
-        'value' => '10001',
+        'value' => '23456',
         'unique_row_string' => "23456-1-ACTIVE",
     ),
     'patient2NHS' => array(
         'patient_id' => $this->getRecord('patient', 'patient2')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'NHS')->id,
-        'value' => '10002',
+        'value' => '65432',
         'unique_row_string' => "65432-2-ACTIVE",
     ),
 
     'patient3ID' => array(
         'patient_id' => $this->getRecord('patient', 'patient3')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
-        'value' => '10003',
+        'value' => '34567',
         'unique_row_string' => "34567-1-ACTIVE",
     ),
     'patient3NHS' => array(
         'patient_id' => $this->getRecord('patient', 'patient3')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'NHS')->id,
-        'value' => '10004',
+        'value' => '76543',
         'unique_row_string' => "76543-2-ACTIVE",
     ),
 
     'patient4ID' => array(
         'patient_id' => $this->getRecord('patient', 'patient4')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
-        'value' => '10005',
+        'value' => '34321',
         'unique_row_string' => "34321-1-ACTIVE",
     ),
     'patient4NHS' => array(
         'patient_id' => $this->getRecord('patient', 'patient4')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'NHS')->id,
-        'value' => '10006',
+        'value' => '76567',
         'unique_row_string' => "76567-2-ACTIVE",
     ),
 
     'patient5ID' => array(
         'patient_id' => $this->getRecord('patient', 'patient5')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
-        'value' => '10007',
+        'value' => '34322',
         'unique_row_string' => "34322-1-ACTIVE",
     ),
     'patient5NHS' => array(
         'patient_id' => $this->getRecord('patient', 'patient5')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'NHS')->id,
-        'value' => '10008',
+        'value' => '76568',
         'unique_row_string' => "76568-2-ACTIVE",
     ),
 
     'patient6ID' => array(
         'patient_id' => $this->getRecord('patient', 'patient6')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
-        'value' => '10009',
+        'value' => '34323',
         'unique_row_string' => "34323-1-ACTIVE",
     ),
     'patient6NHS' => array(
         'patient_id' => $this->getRecord('patient', 'patient6')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'NHS')->id,
-        'value' => '10010',
+        'value' => '76571',
         'unique_row_string' => "76571-2-ACTIVE",
     ),
 
     'patient7ID' => array(
         'patient_id' => $this->getRecord('patient', 'patient7')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
-        'value' => '10011',
+        'value' => '34328',
         'unique_row_string' => "34328-1-ACTIVE",
     ),
     'patient7NHS' => array(
         'patient_id' => $this->getRecord('patient', 'patient7')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'NHS')->id,
-        'value' => '10012',
+        'value' => '76570',
         'unique_row_string' => "76570-2-ACTIVE",
     ),
 
     'patient8ID' => array(
         'patient_id' => $this->getRecord('patient', 'patient8')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
-        'value' => '10013',
+        'value' => '34324',
         'unique_row_string' => "34324-1-ACTIVE",
     ),
     'patient8NHS' => array(
         'patient_id' => $this->getRecord('patient', 'patient8')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'NHS')->id,
-        'value' => '10014',
+        'value' => '76569',
         'unique_row_string' => "76569-2-ACTIVE",
     ),
 
     'patient9ID' => array(
         'patient_id' => $this->getRecord('patient', 'patient9')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
-        'value' => '10015',
+        'value' => '5550101',
         'unique_row_string' => "5550101-1-ACTIVE",
     ),
 
     'patient10ID' => array(
         'patient_id' => $this->getRecord('patient', 'patient10')->id,
         'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
-        'value' => '10016',
+        'value' => '5550102',
         'unique_row_string' => "5550102-1-ACTIVE",
+    ),
+
+    'patient11ID' => array(
+        'patient_id' => $this->getRecord('patient', 'patient10')->id,
+        'patient_identifier_type_id' => $this->getRecord('patient_identifier_type', 'ID')->id,
+        'value' => '9998',
+        'unique_row_string' => "9998-1-ACTIVE",
     ),
 );
