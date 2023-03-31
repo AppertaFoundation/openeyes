aad62eaad389df3db6348dc726055b4670113e3c
Péter Kecskés <kecsokoma@gmail.com>
2023-03-31 15:29:08 +0200

    CVI Filters - the “Consultant Signed By” is displaying a number with 'X' next to it instead of the name of the consultant (#9195)
    ---------
    Co-authored-by: kecsokoma <kecsokoma@users.noreply.github.com>
    Co-authored-by: Mike Smith <splatEric@users.noreply.github.com>

---
+++ cypress/e2e/modules/cvi/CVI-List-filter-behaviour.cy.js
@@ -0,0 +1,42 @@
+function testFilterInput(filterId, searchTerm, expected){
+    const resultId = filterId.split('_')[0]+'_list';
+
+    cy.get(`[data-test=${filterId}]`)
+        .should('be.visible')
+        .type(searchTerm);
+    cy.get('.ui-autocomplete.ui-menu:visible').find('li').contains(expected).click();
+
+    cy.get('#'+resultId).find('li').as('resultLine').should('have.length',1);
+    cy.get('@resultLine').find('.remove').click();
+    cy.get('@resultLine').should('have.length',0);
+}
+
+describe('cvi list filter behaviour', () => {
+    describe('find ', () => {
+
+        beforeEach(() => {
+            cy.login()
+                .then((context) => {
+                    context.visit_url = '/OphCoCvi/Default/list';
+                })
+                .then((context) => {
+                    cy.visit(context.visit_url);
+                });
+        });
+
+
+        it('Created By filter test', function () {
+            testFilterInput('createdby_auto_complete', 'Adm','Mr Admin Admin');
+        });
+
+        it('Consultant signed By filter test', function () {
+            testFilterInput('consultant_auto_complete','Adm','Mr Admin Admin');
+        });
+
+        it('Consultant in charge filter test is case insensitive', function () {
+            let firm = cy.createModels('Firm', [], {name: "Test Firm"});
+            testFilterInput('firm_auto_complete', 'firm', 'Test Firm');
+        });
+
+    });
+});
