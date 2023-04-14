c6d48ae50cefbae3acbe3bf60f1528ea854e510a
biskyt <biskyt@users.noreply.github.com>
2023-04-14 18:05:13 +0100

    supress warning when videos are deleted

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require("cypress");
-// need to install the "del" module as a dependency
-// npm i del --save-dev
 const del = require('del');
 
 module.exports = defineConfig({
@@ -12,9 +10,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       on('after:spec', (spec, results) => {
         if (results && results.stats.failures === 0 && results.video) {
-          // `del()` returns a promise, so it's important to return it to ensure
-          // deleting the video is finished before moving on
-          del(results.video)
+          // delete the video if test is successful and there is a video
+          // This saves a lot of time as otherwise the video is compressed then uploaded to the dashboard
+          return del(results.video)
         }
       })
     },
