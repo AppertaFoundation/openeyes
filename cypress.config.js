c28153596b2a59613e787d61b652d389442c69d8
biskyt <biskyt@users.noreply.github.com>
2023-04-14 17:16:06 +0100

    do not store and compress videos for successful tests

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,13 @@ module.exports = defineConfig({
     viewportWidth: 1280,
     viewportHeight: 737,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('after:spec', (spec, results) => {
+        if (results && results.stats.failures === 0 && results.video) {
+          // `del()` returns a promise, so it's important to return it to ensure
+          // deleting the video is finished before moving on
+          del(results.video)
+        }
+      })
     },
     defaultCommandTimeout: 7000
   },
