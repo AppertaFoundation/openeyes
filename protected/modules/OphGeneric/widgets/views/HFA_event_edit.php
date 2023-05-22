e0619b0bcd3550b81944a7b80dbd2b1873efdfd4
biskyt <biskyt@users.noreply.github.com>
2023-05-22 12:01:33 +0100

    OE-14389 - set min/max input options for Visual Field event inputs

--- protected/modules/OphGeneric/widgets/views/HFA_event_edit.php
+++ protected/modules/OphGeneric/widgets/views/HFA_event_edit.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * OpenEyes.
  *
@@ -41,8 +42,8 @@ use OEModule\OphGeneric\models\HFAEntry;
                             <div class="data-value"><?php echo \CHTML::numberField(
                                 'OEModule_OphGeneric_models_HFA[hfaEntry][' . $index . '][mean_deviation]',
                                 $side ? $side->mean_deviation : '',
-                                ['step' => 'any']
-                                                    ); ?></div>
+                                ['step' => 'any', 'max' => HFAEntry::MEAN_DEVIATION_MAX, 'min' => HFAEntry::MEAN_DEVIATION_MIN]
+                            ); ?></div>
                         </div>
                     </td>
                 </tr>
@@ -55,8 +56,8 @@ use OEModule\OphGeneric\models\HFAEntry;
                             <div class="data-value"><?php echo \CHTML::numberField(
                                 'OEModule_OphGeneric_models_HFA[hfaEntry][' . $index . '][visual_field_index]',
                                 $side ? $side->visual_field_index : '',
-                                ['step' => 'any']
-                                                    ); ?></div>
+                                ['step' => 'any', 'max' => HFAEntry::VISUAL_FIELD_INDEX_MAX, 'min' => HFAEntry::VISUAL_FIELD_INDEX_MIN]
+                            ); ?></div>
                         </div>
                     </td>
                 </tr>
