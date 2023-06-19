22eb90db23b013a8ebeaab2ad2eec25ca4556f13
Edward Desmond-Jones <edward.desmond-jones@toukanlabs.com>
2023-06-19 21:48:07 +1000

    OE-14524 A test for the copying of Visual Acuity Data and its seeder

---
+++ protected/modules/OphCiExamination/seeders/VisualAcuityCopyingSeeder.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * (C) Apperta Foundation, 2023
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.openeyes.org.uk
+ *
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (C) 2023, Apperta Foundation
+ * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
+ */
+
+namespace OEModule\OphCiExamination\seeders;
+
+use OEModule\CypressHelper\resources\SeededEventResource;
+
+use OE\factories\models\EventFactory;
+
+use Patient;
+
+use OEModule\OphCiExamination\models\{
+    Element_OphCiExamination_VisualAcuity,
+    OphCiExamination_VisualAcuity_Reading,
+    OphCiExamination_VisualAcuityUnit
+};
+
+class VisualAcuityCopyingSeeder
+{
+    public function __invoke()
+    {
+        $patient = Patient::factory()->create();
+
+        $existing_event = EventFactory::forModule('OphCiExamination')
+                   ->forPatient($patient)
+                   ->create();
+
+        $existing_va_element = Element_OphCiExamination_VisualAcuity::factory()
+                             ->forEvent($existing_event)
+                             ->bothEyes()
+                             ->create();
+
+        $chosen_unit = OphCiExamination_VisualAcuityUnit::factory()
+                     ->useExisting(['active' => true, 'is_va' => true, 'complex_only' => false])
+                     ->create();
+
+        $lhs_reading = OphCiExamination_VisualAcuity_Reading::factory()
+                     ->forElement($existing_va_element)
+                     ->forSide(OphCiExamination_VisualAcuity_Reading::LEFT)
+                     ->forUnit($chosen_unit)
+                     ->create();
+
+        $rhs_reading = OphCiExamination_VisualAcuity_Reading::factory()
+                     ->forElement($existing_va_element)
+                     ->forSide(OphCiExamination_VisualAcuity_Reading::RIGHT)
+                     ->forUnit($chosen_unit)
+                     ->create();
+
+        $existing_va_element->refresh();
+
+        $lhs_combined = $existing_va_element->getCombined('left');
+        $rhs_combined = $existing_va_element->getCombined('right');
+
+        return [
+            'previousEvent' => SeededEventResource::from($existing_event)->toArray(),
+            'leftEyeCombined' => $lhs_combined,
+            'rightEyeCombined' => $rhs_combined
+        ];
+    }
+}
