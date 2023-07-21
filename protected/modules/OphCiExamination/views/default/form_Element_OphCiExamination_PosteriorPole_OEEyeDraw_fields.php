ac675c610eecafd778a0fe209d452e316bc16cca
biskyt <biskyt@users.noreply.github.com>
2023-07-21 15:11:07 +0100

    Add autosize for macua ED report box

--- protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_PosteriorPole_OEEyeDraw_fields.php
+++ protected/modules/OphCiExamination/views/default/form_Element_OphCiExamination_PosteriorPole_OEEyeDraw_fields.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * OpenEyes.
  *
@@ -15,6 +16,7 @@
  * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/agpl-3.0.html The GNU Affero General Public License V3.0
  */
+
 ?>
 <div class="eyedraw-fields">
     <?=\CHtml::activeHiddenField($element, $side . '_ed_report'); ?>
@@ -31,6 +33,6 @@
     <?=\CHtml::activeTextArea(
         $element,
         $side . '_description',
-        array('rows' => '1', 'placeholder' => $element->getAttributeLabel($side . '_description'))
+        array('rows' => '1', 'placeholder' => $element->getAttributeLabel($side . '_description'), 'class' => 'autosize')
     ) ?>
 </div>
