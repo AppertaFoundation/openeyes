ad4dbd3f45f95068f42ab4f31e52c4348c70be3b
biskyt <biskyt@users.noreply.github.com>
2023-09-22 15:21:18 +0100

    remove cypress-debugger reference

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("cypress");
 const { cloudPlugin } = require("cypress-cloud/plugin");
-const { debuggerPlugin } = require('cypress-debugger');
 
 module.exports = defineConfig({
   e2e: {
