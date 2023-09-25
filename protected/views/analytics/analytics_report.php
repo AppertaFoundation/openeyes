76e4f5598d8db7aff28f631eaf8fe4e42d91c093
biskyt <biskyt@users.noreply.github.com>
2023-09-25 16:42:59 +0100

    fix bad ref to plotly JS

--- protected/views/analytics/analytics_report.php
+++ protected/views/analytics/analytics_report.php
@@ -109,7 +109,7 @@
     <script src="<?= Yii::app()->assetManager->createUrl('js/analytics/analytics_drill_down.js')?>"></script>
     <script src="<?= Yii::app()->assetManager->createUrl('js/analytics/analytics_init.js')?>"></script>
     <script src="<?= Yii::app()->assetManager->createUrl('js/analytics/enhancedPopupFixed.js')?>"></script>
-    <script src="<?= Yii::app()->assetManager->createUrl('newblue/dist/js/plotlyJS/oePlotly_v1.js')?>"></script>
+    <script src="<?= Yii::app()->assetManager->createUrl('nxblu/dist/js/plotlyJS/oePlotly_v1.js')?>"></script>
     <script>
         analytics_init();
     </script>
