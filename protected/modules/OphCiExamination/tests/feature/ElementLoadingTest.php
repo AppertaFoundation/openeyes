258b6f24a72926968f5bf30545c66240705eedd7
edj-toukan <80368411+edj-toukan@users.noreply.github.com>
2023-09-30 00:45:11 +1000

    OE-14262 Use the database to query for elements in the Examination module for its ElementLoadingTest (#9468)
    Co-authored-by: Andrew Pigram <andrew.pigram@toukanlabs.com>

--- protected/modules/OphCiExamination/tests/feature/ElementLoadingTest.php
+++ protected/modules/OphCiExamination/tests/feature/ElementLoadingTest.php
@@ -15,6 +15,8 @@
 
 namespace OEModule\OphCiExamination\tests\feature;
 
+use OEModule\OphCiExamination\components\ExaminationHelper;
+
 /**
  * A safety test that validates all the examination elements can be loaded dynamically
  * @group sample-data
@@ -29,97 +31,30 @@ class ElementLoadingTest extends \OEDbTestCase
 
     public function elementClassListProvider()
     {
-        return [
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_History::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Refraction::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_VisualAcuity::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_AdnexalComorbidity::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_AnteriorSegment::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_IntraocularPressure::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_PosteriorPole::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Diagnoses::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Investigation::class],
-            // [\OEModule\OphCiExamination\models\Element_OphCiExamination_Conclusion::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_CataractSurgicalManagement::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Dilation::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Comorbidities::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Gonioscopy::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_OpticDisc::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_AnteriorSegment_CCT::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Management::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_ClinicOutcome::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_GlaucomaRisk::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Risks::class],
-            [\OEModule\OphCiExamination\models\PupillaryAbnormalities::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_DRGrading::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_LaserManagement::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_InjectionManagement::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_InjectionManagementComplex::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_OCT::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_BlebAssessment::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_ColourVision::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_OverallManagementPlan::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_CurrentManagementPlan::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_FurtherFindings::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_NearVisualAcuity::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_PostOpComplications::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Fundus::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_HistoryRisk::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_OptomComments::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_PcrRisk::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_CXL_History::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Keratometry::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Specular_Microscopy::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Slit_Lamp::class],
-            [\OEModule\OphCiExamination\models\MedicalLids::class],
-            [\OEModule\OphCiExamination\models\FamilyHistory::class],
-            [\OEModule\OphCiExamination\models\Allergies::class],
-            [\OEModule\OphCiExamination\models\SocialHistory::class],
-            [\OEModule\OphCiExamination\models\PastSurgery::class],
-            [\OEModule\OphCiExamination\models\SurgicalLids::class],
-            [\OEModule\OphCiExamination\models\SystemicDiagnoses::class],
-            [\OEModule\OphCiExamination\models\HistoryRisks::class],
-            [\OEModule\OphCiExamination\models\HistoryMedications::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Observations::class],
-            [\OEModule\OphCiExamination\models\VanHerick::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_CVI_Status::class],
-            [\OEModule\OphCiExamination\models\HistoryIOP::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Contacts::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_CommunicationPreferences::class],
-            [\OEModule\OphCiExamination\models\OCT::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_CataractSurgicalManagement::class],
-            // TODO: enable this once the mock user works with ESignPINFieldMedication
-            // [\OEModule\OphCiExamination\models\MedicationManagement::class],
-            [\OEModule\OphCiExamination\models\SystemicSurgery::class],
-            [\OEModule\OphCiExamination\models\PostOpDiplopiaRisk::class],
-            [\OEModule\OphCiExamination\models\BirthHistory::class],
-            [\OEModule\OphCiExamination\models\HeadPosture::class],
-            [\OEModule\OphCiExamination\models\ConvergenceAccommodation::class],
-            [\OEModule\OphCiExamination\models\StereoAcuity::class],
-            [\OEModule\OphCiExamination\models\SensoryFunction::class],
-            [\OEModule\OphCiExamination\models\PrismReflex::class],
-            [\OEModule\OphCiExamination\models\CoverAndPrismCover::class],
-            [\OEModule\OphCiExamination\models\ContrastSensitivity::class],
-            [\OEModule\OphCiExamination\models\Synoptophore::class],
-            [\OEModule\OphCiExamination\models\RedReflex::class],
-            [\OEModule\OphCiExamination\models\NinePositions::class],
-            [\OEModule\OphCiExamination\models\Retinoscopy::class],
-            [\OEModule\OphCiExamination\models\CorrectionGiven::class],
-            [\OEModule\OphCiExamination\models\PrismFusionRange::class],
-            [\OEModule\OphCiExamination\models\StrabismusManagement::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_DR_Retinopathy::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_DR_Maculopathy::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_DrugAdministration::class],
-            [\OEModule\OphCiExamination\models\FreehandDraw::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Pain::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_AE_RedFlags::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Safeguarding::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_ClinicProcedures::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_Triage::class],
-            [\OEModule\OphCiExamination\models\Element_OphCiExamination_NextSteps::class],
-            [\OEModule\OphCiExamination\models\AdviceGiven::class],
-            [\OEModule\OphCiExamination\models\Lacrimal::class]
-        ];
+        $exclude_list = ExaminationHelper::elementFilterList();
+
+        // TODO: enable this once the mock user works with ESignPINFieldMedication
+        $exclude_list = array_merge($exclude_list, [
+            \OEModule\OphCiExamination\models\MedicationManagement::class
+        ]);
+
+        /* This assumes that Yii and its database connection are available at the time PHPUnit is setting up the tests.
+         * Given the current set up of the unit testing, this is the case.
+         *
+         * It only looks at existing data and does not modify the database.
+         * For the existing data it assumes the element_type table is filled with the correct class name entries for
+         * elements in the Examination event.
+         */
+        $class_name_list = \Yii::app()->db->createCommand()
+                         ->select('element_type.class_name')
+                         ->from('element_type')
+                         ->join('event_type', 'element_type.event_type_id = event_type.id')
+                         ->where('event_type.class_name = "OphCiExamination"')
+                         ->queryColumn();
+
+        return array_map(static function ($class_name) {
+            return [$class_name];
+        }, array_diff($class_name_list, $exclude_list));
     }
 
     /**
