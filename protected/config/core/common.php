e196cfd7969baa92a2fbe399bcc0b49a5e2ac6b4
biskyt <biskyt@users.noreply.github.com>
2023-10-09 17:52:35 +0100

    Updated  version,  cachebuster

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -814,7 +814,7 @@ $config = array(
         'default_patient_import_subspecialty' => 'GL',
         //        Add elements that need to be excluded from the admin sidebar in settings
         'exclude_admin_structure_param_list' => getenv('OE_EXCLUDE_ADMIN_STRUCT_LIST') ? explode(",", getenv('OE_EXCLUDE_ADMIN_STRUCT_LIST')) : array(''),
-        'oe_version' => 'UNRELEASED',
+        'oe_version' => '6.7.16',
         'gp_label' => !empty(trim(getenv('OE_GP_LABEL'))) ? getenv('OE_GP_LABEL') : null,
         'general_practitioner_label' => !empty(trim(getenv('OE_GENERAL_PRAC_LABEL'))) ? getenv('OE_GENERAL_PRAC_LABEL') : null,
         // allow duplicate entries on an automatic worklist for a patient (default = false)
@@ -1054,7 +1054,7 @@ if (strtolower(getenv('OE_MODE')) !== 'live') {
 $caches = array(
         'cacheBuster' => array(
             'class' => 'CacheBuster',
-            'time' => '20230825162435',
+            'time' => '20231009175235',
         ),
 );
 
