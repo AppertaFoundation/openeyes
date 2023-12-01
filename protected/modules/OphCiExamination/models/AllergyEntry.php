85c37dab962e14ec450853781ca59d68389458d0
alec-peecock1 <alec.peecock@abehr.com>
2023-12-01 07:01:50 +0000

    OE-14952 - remove unused function isOther.

--- protected/modules/OphCiExamination/models/AllergyEntry.php
+++ protected/modules/OphCiExamination/models/AllergyEntry.php
@@ -41,7 +41,6 @@ class AllergyEntry extends \BaseActiveRecordVersioned
     public static $PRESENT = 1;
     public static $NOT_PRESENT = 0;
     public static $NOT_CHECKED = -9;
-    public static $OTHER_VAL = 17;
 
     protected $auto_update_relations = true;
     protected $auto_validate_relations = true;
@@ -167,22 +166,6 @@ class AllergyEntry extends \BaseActiveRecordVersioned
             $res .= ' (' . $this->comments . ')';
         }
         return $res;
-    }
-
-    /***
-     * Checks if this allergy entry is 'Other' so it can be edited
-     *
-     * @throws Exception if record is new as it does not have a allergy_id to check against
-     * @return bool true if entry is 'Other' (ie not in the standard list so needs editable field)
-     */
-    public function isOther()
-    {
-        if (!$this->isNewRecord) {
-            return $this->allergy->name == "Other";
-        } else {
-            throw new \Exception('Cannot check if new allergy entry is other without proposed allergy id,
-            new records do not have allergy_id set, please use staticIsOther($allergy_id)');
-        }
     }
 
     public function validateOtherAllergies($attribute)
