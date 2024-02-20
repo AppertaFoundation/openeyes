eb087a0e6cfad170ba02b00c25e0096edd2485d1
alec-peecock1 <alec.peecock@abehr.com>
2024-02-20 09:45:49 +0000

    OE-15362 - add check for primary patient has not got 2 episodes with the same firm.

--- protected/tests/unit/components/PatientMergeWithSampleDataTest.php
+++ protected/tests/unit/components/PatientMergeWithSampleDataTest.php
@@ -133,6 +133,10 @@ class PatientMergeWithSampleDataTest extends ModelTestCase
         $primary_patient_episodes = Episode::model()->findAllByAttributes(['patient_id' => $primary_patient->id]);
         $this->assertEquals(count($primary_patient_episodes), 1);
 
+        // Assert primary patient does not have 2 episodes for the same firm
+        $primary_patient_episodes_firm = Episode::model()->findAllByAttributes(['patient_id' => $primary_patient->id, 'firm_id' => $ep1->firm_id]);
+        $this->assertEquals(count($primary_patient_episodes_firm), 1);
+
         // Assert all episodes removed from secondary
         $secondary_patient_episodes = Episode::model()->findAllByAttributes(['patient_id' => $secondary_patient->id]);
         $this->assertEquals(count($secondary_patient_episodes), 0);
