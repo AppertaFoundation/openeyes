538894695cc201e3dc4f8063f21e0aa831ebd12a
sabi1981 <sabi.potondi@toukanlabs.com>
2024-05-03 13:47:39 +0200

    OE-13997 - Glaucoma OEScape Shows medications as current even when they have been stopped (#10242)
    * OE-13997 - refactor getMedicationList() to use the latest HistoryMedications element
    
    Because the latest MH contains the full history, there is no need to loop back through old events

--- protected/modules/OphCiExamination/widgets/OphCiExamination_Episode_Medication.php
+++ protected/modules/OphCiExamination/widgets/OphCiExamination_Episode_Medication.php
@@ -1,4 +1,7 @@
 <?php
+
+use OEModule\OphCiExamination\models\HistoryMedications;
+
 /**
  * OpenEyes
  *
@@ -25,91 +28,25 @@ class OphCiExamination_Episode_Medication extends \EpisodeSummaryWidget
 
     public function getMedicationList()
     {
-        $medication_list = array('right' => array(), 'left' => array());
-        $events = $this->event_type->api->getEvents($this->patient, false);
-        $earliest_date = time() * 1000;
-        $latest_date = time() * 1000;
-        $lateral_routes = array_map(function ($e) {
-            return $e->id;
-        }, MedicationRoute::model()->findAll());
+        $medication_list = ['right' => [], 'left' => []];
 
-        foreach ($events as $event) {
-            $meds = $event->getElementByClass('OEModule\OphCiExamination\models\HistoryMedications');
-            $start_date = '';
-            $end_date = '';
-            if ($meds) {
-                $widget = $this->createWidget('OEModule\OphCiExamination\widgets\HistoryMedications', array(
-                    'patient' => $this->patient,
-                ));
+        $latest_MH_element = $this->event_type->api->getLatestElement(HistoryMedications::class, $this->patient);
 
-                $untracked = $widget->element->getEntriesForUntrackedPrescriptionItems($this->patient);
-                $meds_entries = array_merge($meds->entries, $untracked);
-
-                foreach ($meds_entries as $entry) {
-                    if (!$entry->medication_id) {
-                        continue;
-                    }
-
-                    $is_glaucoma = false;
-                    if ($entry->medication_id) {
-                        foreach ($entry->medication->getSetsByUsageCode('OEScape') as $set) {
-                            foreach ($set->medicationSetRules as $rule) {
-                                if ($rule->subspecialty && $rule->subspecialty->ref_spec === 'GL') {
-                                    $is_glaucoma = true;
-                                }
-                            }
-                        }
-                    }
-
-                    if ( !in_array($entry['route_id'], $lateral_routes) || !$is_glaucoma) {
-                        continue;
-                    }
-
-                    $drug_aliases = $entry->medication->alternativeTerms(true) ? ' ('.$entry->medication->alternativeTerms(true).')': '';
-                    $drug_name = $entry->medication->getLabel(true) . $drug_aliases;
-
-                    if ($entry->start_date !== "0000-00-00" && $entry->start_date !== "") {
-                        $start_date = $this->changeInvalidSqlDateMonthAndDayToOne($entry->start_date, 'start');
-                        $start_date = Helper::mysqlDate2JsTimestamp($start_date);
-
-                        if ($start_date < $earliest_date) {
-                            $earliest_date = $start_date;
-                        }
-                    }
-
-                    $end_date = $this->changeInvalidSqlDateMonthAndDayToOne($entry->end_date, 'end');
-                    $end_date = Helper::mysqlDate2JsTimestamp($end_date);
-
-                    $stop_reason = $entry->stopReason ? $entry->stopReason->name : null;
-
-                     // Construct data to store medication records for left and right eye based on drug name.
-                     // Each medication may have one or multiple apply time.
-                    foreach ([1 => 'left', 2 => 'right'] as $eye_flag => $eye_side) {
-                        if (isset($entry->laterality) && !($entry->laterality & $eye_flag)) {
-                            continue;
-                        }
-                        $new_medi_record = array(
-                            'low' => $start_date,
-                            'high' => $end_date?:$latest_date,
-                            'stop_reason' => $stop_reason
-                        );
-                        if (!in_array($drug_name, array_keys($medication_list[$eye_side]))) {
-                            $medication_list[$eye_side][$drug_name] = [];
-                        }
-                        if (!in_array($new_medi_record, $medication_list[$eye_side][$drug_name]) ) {
-                            $medication_list[$eye_side][$drug_name][] = $new_medi_record;
-                        }
-                    }
-                }
-            }
+        if (!$latest_MH_element) {
+            return $medication_list;
         }
+
+        $widget = $this->createWidget('OEModule\OphCiExamination\widgets\HistoryMedications', array(
+            'patient' => $this->patient,
+        ));
+
+        $untracked = $widget->element->getEntriesForUntrackedPrescriptionItems($this->patient);
+        $meds_entries = array_merge($latest_MH_element->entries, $untracked);
+
+        $medication_list = $this->getMedicationListForOEScapePlotly($meds_entries);
 
         foreach (['left', 'right'] as $side) {
             foreach ($medication_list[$side] as $key => &$med) {
-                if ($med[0]['low'] === '') {
-                    $med[0]['low'] = $earliest_date;
-                }
-
                 if (sizeof($med)>1) {
                     $med = $this->purifyMedicationSeries($med);    //sort and merge each medication's time series
                 }
@@ -175,10 +112,12 @@ class OphCiExamination_Episode_Medication extends \EpisodeSummaryWidget
             $begin = $medication_series[$i]['low'];
             $end = $medication_series[$i]['high'];
             $stop_reason = $medication_series[$i]['stop_reason'];
+            $tooltip = $medication_series[$i]['tooltip'];
             while ($i<sizeof($medication_series)-1 && $medication_series[$i+1]['low']<$end) {
                 if ($medication_series[$i+1]['high']>$end) {
                     $end = $medication_series[$i+1]['high'];
                     $stop_reason = $medication_series[$i+1]['stop_reason'];
+                    $tooltip = $medication_series[$i+1]['tooltip'];
                 }
                 ++$i;
             }
@@ -186,10 +125,80 @@ class OphCiExamination_Episode_Medication extends \EpisodeSummaryWidget
             $out_series[]= array(
                 'low' => $begin,
                 'high' => $end,
-                'stop_reason' => $stop_reason
+                'stop_reason' => $stop_reason,
+                'tooltip' => $tooltip
             );
         }
 
         return $out_series;
+    }
+
+    private function getMedicationListForOEScapePlotly(array $medication_entries): array
+    {
+        $medication_list = ['right' => [], 'left' => []];
+
+        $lateral_routes = array_map(function ($e) {
+            return $e->id;
+        }, MedicationRoute::model()->findAll());
+
+        foreach ($medication_entries as $medication) {
+            if (!$medication->medication_id) {
+                continue;
+            }
+
+            $is_glaucoma = false;
+            if ($medication->medication_id) {
+                foreach ($medication->medication->getSetsByUsageCode('OEScape') as $set) {
+                    foreach ($set->medicationSetRules as $rule) {
+                        if ($rule->subspecialty && $rule->subspecialty->ref_spec === 'GL') {
+                            $is_glaucoma = true;
+                        }
+                    }
+                }
+            }
+
+            if ( !in_array($medication['route_id'], $lateral_routes) || !$is_glaucoma) {
+                continue;
+            }
+
+            $drug_aliases = $medication->medication->alternativeTerms(true) ? ' ('.$medication->medication->alternativeTerms(true).')': '';
+            $drug_name = $medication->medication->getLabel(true);
+
+            $start_date = $medication->start_date;
+            if ($medication->start_date !== "0000-00-00" && $medication->start_date !== "") {
+                $start_date = $this->changeInvalidSqlDateMonthAndDayToOne($medication->start_date, 'start');
+                $start_date = Helper::mysqlDate2JsTimestamp($start_date);
+            }
+
+            $end_date = time() * 1000;
+            if ($medication->end_date !== "0000-00-00" && $medication->end_date !== "") {
+                $end_date = $this->changeInvalidSqlDateMonthAndDayToOne($medication->end_date, 'end');
+                $end_date = Helper::mysqlDate2JsTimestamp($end_date);
+            }
+
+            $stop_reason = $medication->stopReason ? $medication->stopReason->name : null;
+
+            // Construct data to store medication records for left and right eye based on drug name.
+            // Each medication may have one or multiple apply time.
+            foreach ([1 => 'left', 2 => 'right'] as $eye_flag => $eye_side) {
+                if (isset($medication->laterality) && !($medication->laterality & $eye_flag)) {
+                    continue;
+                }
+                $new_medication_record = [
+                    'low' => $start_date,
+                    'high' => $end_date,
+                    'stop_reason' => $stop_reason,
+                    'tooltip' => $drug_name . $drug_aliases,
+                ];
+                if (!in_array($drug_name, array_keys($medication_list[$eye_side]))) {
+                    $medication_list[$eye_side][$drug_name] = [];
+                }
+                if (!in_array($new_medication_record, $medication_list[$eye_side][$drug_name]) ) {
+                    $medication_list[$eye_side][$drug_name][] = $new_medication_record;
+                }
+            }
+        }
+
+        return $medication_list;
     }
 }
