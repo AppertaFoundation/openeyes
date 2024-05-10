a412930ac05d74d774f9bad0520e2af49759b4d6
gabordikan <gabordikan@gmail.com>
2024-05-10 09:12:21 +0200

    login screen changes

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -2,12 +2,13 @@
 $this->pageTitle = ((string)SettingMetadata::model()->getSetting('use_short_page_titles') != "on" ? Yii::app()->name . ' - ' : '') . 'Login';
 $settings = new SettingMetadata();
 $tech_support_provider = Yii::App()->params['tech_support_provider'] ? htmlspecialchars(Yii::App()->params['tech_support_provider']) : htmlspecialchars($settings->getSetting('tech_support_provider'));
-$tech_support_url = Yii::App()->params['tech_support_url'] ? htmlspecialchars(Yii::App()->params['tech_support_url']) : htmlspecialchars($settings->getSetting('tech_support_url'))
+$tech_support_url = Yii::App()->params['tech_support_url'] ? htmlspecialchars(Yii::App()->params['tech_support_url']) : htmlspecialchars($settings->getSetting('tech_support_url'));
 ?>
 
 <div class="oe-login">
+
   <div class="login">
-    <h1>OpenEyes</h1>
+    <h1><b>OpenEyes&#8482;</b></h1>
     <div class="user">
 
       <?php $form = $this->beginWidget('CActiveForm', array(
@@ -42,18 +43,14 @@ $tech_support_url = Yii::App()->params['tech_support_url'] ? htmlspecialchars(Yi
       <!-- user -->
     </div>
     <div class="info">
-      <div class="flex-layout">
-        <span class="large-text"> Need Help?&nbsp;
-          <?php if (Yii::app()->params['helpdesk_phone'] || Yii::app()->params['helpdesk_email']) : ?>
-                <?= Yii::app()->params['helpdesk_phone'] ? "<strong>" . htmlspecialchars(Yii::app()->params['helpdesk_phone']) . "</strong>" : null ?></strong>
-                <?= Yii::app()->params['helpdesk_email'] ? "<br/>" . htmlspecialchars(Yii::app()->params['helpdesk_email']) : null ?>
-                <?= Yii::app()->params['helpdesk_hours'] ? "<br/> (" . htmlspecialchars(Yii::app()->params['helpdesk_hours']) . ")" : null ?>
-          <?php elseif ($tech_support_provider) : ?>
-            <strong><a href="<?= $tech_support_url ?>" target="_blank"><?= $tech_support_provider ?></a></strong>
-          <?php endif; ?>
-        </span>
-        <a href="#" onclick="$('#js-openeyes-btn').click();">About</a>
-      </div>
+      <center>
+      <a href="http://apperta.org" target="_blank"><img src="<?php echo Yii::app()->assetManager->createUrl('img/logo/logo.png') ?>" alt="Apperta Foundation CIC Logo" width="30%" style="padding-right:20px"></a>
+       <a href="https://digitalpublicgoods.net/who-we-are/" target="_blank"><img src="<?php echo Yii::app()->assetManager->createUrl('img/logo/dpga_logo_2.svg') ?>" alt="Digital Public Good Alliance Logo" width="30%" style="padding-right:20px">
+       <a href="https://digitalhealthatlas.org/" target="_blank"><img src="<?php echo Yii::app()->assetManager->createUrl('img/logo/logo-dha.svg') ?>" alt="Digital Health Atlas Logo" width="30%"></a></br></br>
+        Copyright&#169; Apperta Foundation CIC <?= date('Y') ?>
+    </center>
+        <!--<a href="#" onclick="$('#js-openeyes-btn').click();">About</a>-->
+      
       <!-- info -->
     </div>
     <!-- login -->
